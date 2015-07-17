b4e4cdd1b35b3a5ebe8fc331812b786201847069
marco <marco@badshark.io>
2015-07-17 23:32:35 +0200

    Tests commands in Rubo.

--- RoboFile.php
+++ RoboFile.php
@@ -3,4 +3,26 @@ class RoboFile extends \Robo\Tasks {
   function update() {
     $this->_exec('./composer.phar update');
   }
+
+  function testUnit() {
+    $this->_exec('vendor/bin/codecept run unit');
+  }
+
+  function testAcceptance() {
+    $this
+      ->taskExec('phantomjs --webdriver=4444')
+      ->background()
+      ->run();
+    sleep(2);
+    $this->_exec('vendor/bin/codecept run acceptance');
+  }
+
+  function testAll() {
+    $this
+      ->taskExec('phantomjs --webdriver=4444')
+      ->background()
+      ->run();
+    sleep(2);
+    $this->_exec('vendor/bin/codecept run');
+  }
 }
