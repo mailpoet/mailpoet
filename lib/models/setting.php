27c2a4b4025aae4fba75a4f2f0673e1c14ee0178
Vlad <site.github@mrcasual.com>
2015-08-10 18:35:47 -0400

    Fixes #49

--- lib/models/setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace MailPoet\Models;
-
-if (!defined('ABSPATH')) exit;
-
-class Setting extends \Model {
-  public static $_table = MP_SETTINGS_TABLE;
-}
--- lib/models/subscriber.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace MailPoet\Models;
-
-if (!defined('ABSPATH')) exit;
-
-class Subscriber extends \Model {
-  public static $_table = MP_SUBSCRIBERS_TABLE;
-}
