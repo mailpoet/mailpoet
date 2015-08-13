9c80429d0d283264ec6c835732ba726a491f8ee3
MrCasual <site.github@mrcasual.com>
2015-08-13 12:26:14 -0400

    Merge master/resolved conflicts

--- RoboFile.php
+++ RoboFile.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class RoboFile extends \Robo\Tasks {
   function install() {
     $this->_exec('./composer.phar install');
@@ -14,61 +13,85 @@ class RoboFile extends \Robo\Tasks {
   }
 
   function watch() {
-    $files = array(
-      // global admin styles
+    $css_files = array(
       'assets/css/src/admin.styl',
-      // rtl specific styles
       'assets/css/src/rtl.styl'
     );
 
-    $command = array(
-      './node_modules/stylus/bin/stylus -u',
-      ' nib -w' . join(' ', $files) . ' -o assets/css/'
-    );
-    $this->_exec(join(' ', $command));
+    $js_files = glob('assets/js/src/*.js');
+
+    $this->taskWatch()
+      ->monitor($js_files, function() {
+        $this->compileJavascript();
+      })
+      ->monitor($css_files, function() use($css_files) {
+        $this->compileStyles($css_files);
+      })
+      ->run();
+  }
+
+  function compileJavascript() {
+    $this->_exec('./node_modules/webpack/bin/webpack.js');
+  }
+
+  protected function compileStyles($files = array()) {
+    if(empty($files)) { return; }
+
+    $this->_exec(join(' ', array(
+      './node_modules/stylus/bin/stylus',
+      '-u nib',
+      '-w',
+      join(' ', $files),
+      '-o assets/css/'
+    )));
   }
 
   function makepot() {
-    $this->_exec('grunt makepot' . ' --gruntfile '
-      . __DIR__ . '/tasks/makepot/makepot.js'
-      . ' --base_path ' . __DIR__);
+    $this->_exec('grunt makepot'.
+                ' --gruntfile '.__DIR__.'/tasks/makepot/makepot.js'.
+                ' --base_path '.__DIR__
+    );
   }
 
   function pushpot() {
-    $this->_exec('grunt pushpot' . ' --gruntfile '
-      . __DIR__ . '/tasks/makepot/makepot.js'
-      . ' --base_path ' . __DIR__);
+    $this->_exec('grunt pushpot'.
+                ' --gruntfile '.__DIR__.'/tasks/makepot/makepot.js'.
+                ' --base_path '.__DIR__
+    );
   }
 
   function testUnit() {
-    $this->loadEnv();
     $this->_exec('vendor/bin/codecept build');
+    $this->loadEnv();
     $this->_exec('vendor/bin/codecept run unit');
   }
 
-  function testUnitSingle($unit = null) {
-    if (!$unit) {
-      throw new Exception("Your need to specify what you want to test (e.g.: test:unit-single models/SubscriberCest)");
-    }
-    $this->loadEnv();
-    $this->_exec('vendor/bin/codecept build');
-    $this->_exec('vendor/bin/codecept run unit ' . $unit);
-  }
-
   function testAcceptance() {
-    $this->loadEnv();
     $this->_exec('vendor/bin/codecept build');
-    $this->taskExec('phantomjs --webdriver=4444')
+    $this->loadEnv();
+    $this
+      ->taskExec('phantomjs --webdriver=4444')
       ->background()
       ->run();
     sleep(2);
     $this->_exec('vendor/bin/codecept run acceptance');
   }
 
+  function testJavascript() {
+    $this->compileJavascript();
+
+    $this->_exec(join(' ', array(
+      './node_modules/mocha/bin/mocha',
+      '-r tests/javascript/mochaTestHelper.js',
+      'tests/javascript/testBundles/**/*.js'
+    )));
+  }
+
   function testAll() {
-    $this->loadEnv();
+    $this->loadEnv();  
     $this->_exec('vendor/bin/codecept build');
-    $this->taskexec('phantomjs --webdriver=4444')
+    $this
+      ->taskexec('phantomjs --webdriver=4444')
       ->background()
       ->run();
     sleep(2);
@@ -85,13 +108,15 @@ class RoboFile extends \Robo\Tasks {
     $dotenv = new Dotenv\Dotenv(__DIR__);
     $dotenv->load();
 
-    $this->taskWriteToFile('tests/acceptance.suite.yml')
+    $this
+      ->taskWriteToFile('tests/acceptance.suite.yml')
       ->textFromFile('tests/acceptance.suite.src')
       ->run();
 
-    $this->taskReplaceInFile('tests/acceptance.suite.yml')
+    $this
+      ->taskReplaceInFile('tests/acceptance.suite.yml')
       ->regex("/url.*/")
-      ->to('url: ' . "'" . getenv('WP_TEST_URL') . "'")
+      ->to('url: ' . "'" . getenv('WP_TEST_URL'). "'")
       ->run();
   }
 }
