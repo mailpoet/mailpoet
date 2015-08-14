745700e5fe331b5a09a4a7bccafeedb819311d86
marco <marco@badshark.io>
2015-08-14 23:39:38 +0200

    Global bash script with Linux and OSX compatibility.

--- RoboFile.php
+++ RoboFile.php
@@ -48,15 +48,15 @@ class RoboFile extends \Robo\Tasks {
 
   function makepot() {
     $this->_exec('grunt makepot'.
-                ' --gruntfile '.__DIR__.'/tasks/makepot/makepot.js'.
-                ' --base_path '.__DIR__
+      ' --gruntfile '.__DIR__.'/tasks/makepot/makepot.js'.
+      ' --base_path '.__DIR__
     );
   }
 
   function pushpot() {
     $this->_exec('grunt pushpot'.
-                ' --gruntfile '.__DIR__.'/tasks/makepot/makepot.js'.
-                ' --base_path '.__DIR__
+      ' --gruntfile '.__DIR__.'/tasks/makepot/makepot.js'.
+      ' --base_path '.__DIR__
     );
   }
 
@@ -102,53 +102,6 @@ class RoboFile extends \Robo\Tasks {
     $this->_exec('vendor/bin/codecept build');
     $this->loadEnv();
     $this->_exec('vendor/bin/codecept run unit --debug');
-  }
-
-  function zip() {
-    $rootPath = realpath('wysija-newsletters');
-    $zip = new ZipArchive();
-    $zip->open('mailpoet.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
-    $directory = new \RecursiveDirectoryIterator(
-      $rootPath, \FilesystemIterator::FOLLOW_SYMLINKS
-    );
-    $filter = new \RecursiveCallbackFilterIterator(
-      $directory, function ($current, $key, $iterator) {
-        return ($current->getFilename()[0] !== '.');
-      }
-    );
-    $files = new \RecursiveIteratorIterator($filter);
-    foreach ($files as $name => $file) {
-      $filePath = $file->getRealPath();
-      $relativePath = substr($filePath, strlen($rootPath) + 1);
-      $zip->addFile($filePath, $relativePath);
-    }
-    $zip->close();
-  }
-
-  function build() {
-    $this->_exec('mkdir wysija-newsletters');
-    $this->_exec('mkdir wysija-newsletters/vendor');
-    $this->_exec('cp index.php wysija-newsletters/index.php');
-    $this->_exec('cp mailpoet.php wysija-newsletters/mailpoet.php');
-    $this->_exec('cp package.json wysija-newsletters/package.json');
-    $this->_exec('cp composer.* wysija-newsletters/');
-    $this->taskCopyDir(array(
-      'assets' => 'wysija-newsletters/assets',
-      'lang' => 'wysija-newsletters/lang',
-      'lib' => 'wysija-newsletters/lib',
-      'views' => 'wysija-newsletters/views'
-    ))->run();
-    chdir('./wysija-newsletters');
-    $this->taskExecStack()
-      ->stopOnFail()
-      // ->exec('cd wysija-newsletters') does not work as intended
-      ->exec('./composer.phar install --no-dev')
-      ->run();
-    chdir('..');
-    $this->_exec('rm wysija-newsletters/package.json');
-    $this->_exec('rm wysija-newsletters/composer.*');
-    $this->zip();
-    $this->taskDeleteDir('wysija-newsletters')->run();
   }
 
   protected function loadEnv() {
