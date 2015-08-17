6ed4ce0de27d46a03e7a2ebb65a6590e6d9922ac
Tautvidas Sipavičius <t.9b5as@gmail.com>
2015-08-17 14:31:32 +0300

    Swap app code with bundles in `assets/js/src` folder

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,13 +1,21 @@
-define('admin', [
-    'mailpoet',
-    'jquery',
-    'handlebars',
-  ], function(MailPoet, jQuery, Handlebars) {
-  console.log('OVER HERE', MailPoet, jQuery, Handlebars);
-  jQuery(function($) {
-    // dom ready
-    $(function() {
+webpackJsonp([0],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
 
-    });
-  });
-});
+	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
+	    __webpack_require__(1),
+	    __webpack_require__(2),
+	    __webpack_require__(3),
+	  ], __WEBPACK_AMD_DEFINE_RESULT__ = function(MailPoet, jQuery, Handlebars) {
+	  console.log('OVER HERE', MailPoet, jQuery, Handlebars);
+	  jQuery(function($) {
+	    // dom ready
+	    $(function() {
+
+	    });
+	  });
+	}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+
+
+/***/ }
+]);
--- assets/js/src/admin.js
+++ assets/js/src/admin.js
@@ -1,21 +1,13 @@
-webpackJsonp([0],[
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
+define('admin', [
+    'mailpoet',
+    'jquery',
+    'handlebars',
+  ], function(MailPoet, jQuery, Handlebars) {
+  console.log('admin.js', MailPoet, jQuery, Handlebars);
+  jQuery(function($) {
+    // dom ready
+    $(function() {
 
-	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
-	    __webpack_require__(1),
-	    __webpack_require__(2),
-	    __webpack_require__(3),
-	  ], __WEBPACK_AMD_DEFINE_RESULT__ = function(MailPoet, jQuery, Handlebars) {
-	  console.log('OVER HERE', MailPoet, jQuery, Handlebars);
-	  jQuery(function($) {
-	    // dom ready
-	    $(function() {
-
-	    });
-	  });
-	}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
-
-
-/***/ }
-]);
+    });
+  });
+});
