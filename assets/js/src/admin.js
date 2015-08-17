cf3695157a677a15fe4cf076a709e0f48ba454ea
Jonathan Labreuille <jonathan@mailpoet.com>
2015-08-17 15:04:30 +0200

    Update Robo task to compile assets - renamed compile:javascript to compile:js - renamed compile:styles to compile:css - added compile:all to compile all of our assets - added compiled js & css to gitignore - removed console.log from src/admin.js

--- assets/js/src/admin.js
+++ assets/js/src/admin.js
@@ -3,7 +3,6 @@ define('admin', [
     'jquery',
     'handlebars',
   ], function(MailPoet, jQuery, Handlebars) {
-  console.log('admin.js', MailPoet, jQuery, Handlebars);
   jQuery(function($) {
     // dom ready
     $(function() {
