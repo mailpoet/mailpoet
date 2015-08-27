703fd17932a9402657b882d77288f3eb6a77c663
Jonathan Labreuille <jonathan@mailpoet.com>
2015-08-27 14:11:42 +0200

    fix sorting indicator not showing for sorted column

--- assets/js/src/listing/header.jsx
+++ assets/js/src/listing/header.jsx
@@ -12,42 +12,6 @@ define('listing.header', ['react', 'classnames'], function(React, classNames) {
           label: (string) displayed label
         }
   */
-  var ListingColumn = React.createClass({
-    handleSort: function() {
-      var sort_by = this.props.column.name,
-          sort_order = (this.props.column.sorted === 'asc') ? 'desc' : 'asc';
-      this.props.onSort(sort_by, sort_order);
-    },
-    render: function() {
-      var classes = classNames(
-        'manage-column',
-        { 'sortable': this.props.column.sortable },
-        this.props.column.sorted
-      );
-
-      var label;
-
-      if(this.props.column.sortable === true) {
-        label = (
-          <a onClick={this.handleSort}>
-            <span>{ this.props.column.label }</span>
-            <span className="sorting-indicator"></span>
-          </a>
-        );
-      } else {
-        label = this.props.column.label;
-      }
-      return (
-        <th
-          className={ classes }
-          id={this.props.column.name }
-          scope="col">
-          {label}
-        </th>
-      );
-    }
-  });
-
   var ListingHeader = React.createClass({
     handleSelectAll: function() {
       return this.props.onSelectAll(
@@ -78,6 +42,43 @@ define('listing.header', ['react', 'classnames'], function(React, classNames) {
           </td>
           {columns}
         </tr>
+      );
+    }
+  });
+
+  var ListingColumn = React.createClass({
+    handleSort: function() {
+      var sort_by = this.props.column.name,
+          sort_order = (this.props.column.sorted === 'asc') ? 'desc' : 'asc';
+      this.props.onSort(sort_by, sort_order);
+    },
+    render: function() {
+      var classes = classNames(
+        'manage-column',
+        { 'sortable': this.props.column.sortable },
+        this.props.column.sorted,
+        { 'sorted': (this.props.sort_by === this.props.column.name) }
+      );
+
+      var label;
+
+      if(this.props.column.sortable === true) {
+        label = (
+          <a onClick={this.handleSort}>
+            <span>{ this.props.column.label }</span>
+            <span className="sorting-indicator"></span>
+          </a>
+        );
+      } else {
+        label = this.props.column.label;
+      }
+      return (
+        <th
+          className={ classes }
+          id={this.props.column.name }
+          scope="col">
+          {label}
+        </th>
       );
     }
   });
