9804b2fa7a3d211dcc3c5d0ec48af99f3c8b5041
marco <marco@badshark.io>
2015-09-25 15:25:48 +0200

    Remove PhantomJS shim and refactor subscribers edit.

--- integration/tests/test/subscribers_edit.rb
+++ integration/tests/test/subscribers_edit.rb
@@ -8,26 +8,14 @@ describe 'subscribers edit' do
     within '#subscribers' do
       click_on 'New'
       fill_in('E-mail', with: 'test@mailpoet.com')
+      select('Subscribed', from: 'field_status')
       fill_in('Firstname', with: 'Test')
       fill_in('Lastname', with: 'Last')
-      select('Unconfirmed', from: 'field_status')
       click_on 'Save'
     end
-    click_on('Subscribers')
   end
 
   it 'can edit a subscriber' do
-    find('#subscribers a', text: 'test@mailpoet.com').hover
-    find('#subscribers a', text: 'Edit').click
-
-    page.must_have_field('E-mail', with: 'test@mailpoet.com')
-    fill_in('Firstname', with: 'First')
-    select('Unsubscribed', from: 'field_status')
-    execute_script("$('#field_status').trigger('change'");
-
-    click_on 'Save'
-
-      page.must_have_content 'Subscriber succesfully updated!'
     within '#subscribers tbody' do
       page.must_have_content 'Unsubscribed'
     end
