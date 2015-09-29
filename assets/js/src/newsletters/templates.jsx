37bace359a83bc1e5394b240f32253bf6eaa26ad
Jonathan Labreuille <jonathan@mailpoet.com>
2015-09-29 17:29:31 +0200

    Template selection - templates are now loaded via ajax - added "template" deletion

--- assets/js/src/newsletters/templates.jsx
+++ assets/js/src/newsletters/templates.jsx
@@ -13,16 +13,86 @@ define(
       mixins: [
         Router.Navigation
       ],
-      handleSelectTemplate: function(template) {
-        console.log('select '+template);
+      getInitialState: function() {
+        return {
+          loading: false,
+          templates: []
+        };
       },
-      handlePreviewTemplate: function(template) {
-        console.log('preview '+template);
+      componentDidMount: function() {
+        this.getTemplates();
       },
-      handleDeleteTemplate: function(template) {
-        console.log('delete '+template);
+      getTemplates: function() {
+        this.setState({ loading: true });
+
+        MailPoet.Ajax.post({
+          endpoint: 'newslettertemplates',
+          action: 'getAll',
+        }).done(function(response) {
+          if(this.isMounted()) {
+            this.setState({
+              templates: response,
+              loading: false
+            });
+          }
+        }.bind(this));
+      },
+      handleSelectTemplate: function(id) {
+        console.log('select '+id);
+      },
+      handlePreviewTemplate: function(id) {
+        console.log('preview '+id);
+      },
+      handleDeleteTemplate: function(id) {
+        this.setState({ loading: true });
+
+        MailPoet.Ajax.post({
+          endpoint: 'newslettertemplates',
+          action: 'delete',
+          data: id
+        }).done(function(response) {
+          this.getTemplates();
+        }.bind(this));
       },
       render: function() {
+        var templates = this.state.templates.map(function(template, index) {
+          return (
+            <li key={ 'template-'+index }>
+              <div className="mailpoet_thumbnail">
+              </div>
+
+              <div className="mailpoet_description">
+                  <h3>{ template.name }</h3>
+                  <p>{ template.description }</p>
+              </div>
+
+              <div className="mailpoet_actions">
+                  <a
+                    className="button button-primary"
+                    onClick={ this.handleSelectTemplate.bind(null, template.id) }
+                  >
+                    Select
+                  </a>
+                  &nbsp;
+                  <a
+                    className="button button-secondary"
+                    onClick={ this.handlePreviewTemplate.bind(null, template.id) }
+                  >
+                    Preview
+                  </a>
+              </div>
+              <div className="mailpoet_delete">
+                <a
+                  href="javascript:;"
+                  onClick={ this.handleDeleteTemplate.bind(null, template.id) }
+                >
+                  Delete
+                </a>
+              </div>
+            </li>
+          );
+        }.bind(this));
+
         return (
           <div>
             <h1>Select a template</h1>
@@ -38,38 +108,7 @@ define(
 
 
             <ul className="mailpoet_boxes clearfix">
-              <li>
-                <div className="mailpoet_thumbnail">
-                </div>
-
-                <div className="mailpoet_description">
-                    <h3>MailPoet&#39;s Guide</h3>
-
-                    <p>This is the standard template that comes with MailPoet.</p>
-                </div>
-
-                <div className="mailpoet_actions">
-                    <a
-                      className="button button-primary"
-                      onClick={ this.handleSelectTemplate.bind(null, 'default') }
-                    >
-                      Select
-                    </a>
-                    &nbsp;
-                    <a
-                      className="button button-secondary"
-                      onClick={ this.handlePreviewTemplate.bind(null, 'default') }
-                    >
-                      Preview
-                    </a>
-                </div>
-                <div className="mailpoet_delete">
-                  <a onClick={ this.handleDeleteTemplate.bind(null, 'default') }
-                  >
-                    Delete
-                  </a>
-                </div>
-              </li>
+              { templates }
             </ul>
           </div>
         );
