7ac386a8e2ebc8b9b3e2b371ee06826816840991
Jonathan Labreuille <jonathan@mailpoet.com>
2015-11-20 13:37:52 +0100

    fixed welcome/update pages display logic

--- lib/Config/Changelog.php
+++ lib/Config/Changelog.php
@@ -4,24 +4,58 @@ use \MailPoet\Models\Setting;
 
 class Changelog {
   function init() {
+    $doing_ajax = (bool)(defined('DOING_AJAX') && DOING_AJAX);
+
+    // don't run any check when it's an ajax request
+    if($doing_ajax) {
+      return;
+    }
+
+    // don't run any check when we're not on our pages
+    if(
+      !(isset($_GET['page']))
+      or
+      (isset($_GET['page']) && strpos($_GET['page'], 'mailpoet') !== 0)
+    ) {
+      return;
+    }
+
     add_action(
-       'admin_init',
-      array($this, 'setup')
+      'admin_init',
+      array($this, 'check')
     );
   }
 
-  function setup() {
+  function check() {
     $version = Setting::getValue('version', null);
+    $redirect_url = null;
+
     if($version === null) {
       // new install
-      Setting::setValue('version', Env::$version);
-      wp_redirect(admin_url('admin.php?page=mailpoet-welcome'));
-      exit;
+      $redirect_url = admin_url('admin.php?page=mailpoet-welcome');
     } else if($version !== Env::$version) {
       // update
+      $redirect_url = admin_url('admin.php?page=mailpoet-update');
+    }
+
+    if($redirect_url !== null) {
+      // save version number
       Setting::setValue('version', Env::$version);
-      wp_redirect(admin_url('admin.php?page=mailpoet-update'));
-      exit;
+
+      global $wp;
+      $current_url = home_url(add_query_arg($wp->query_string, $wp->request));
+
+      if($redirect_url !== $current_url) {
+        wp_safe_redirect(
+          add_query_arg(
+            array(
+              'mailpoet_redirect' => urlencode($current_url)
+            ),
+            $redirect_url
+          )
+        );
+        exit;
+      }
     }
   }
 }
