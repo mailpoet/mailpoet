67fa9e0993b6fbe21d41209534590c9e32f25860
MrCasual <site.github@mrcasual.com>
2015-11-30 18:38:28 -0500

    - Adds router methods to add/delete/et queue status

--- lib/Router/Queue.php
+++ lib/Router/Queue.php
@@ -3,6 +3,7 @@ namespace MailPoet\Router;
 
 use MailPoet\Queue\Daemon;
 use MailPoet\Queue\Supervisor;
+use MailPoet\Util\Helpers;
 
 if(!defined('ABSPATH')) exit;
 
@@ -44,5 +45,128 @@ class Queue {
   function getDaemonStatus() {
     $daemon = new \MailPoet\Queue\BootStrapMenu();
     wp_send_json($daemon->bootStrap());
+  }
+
+  function addQueue($data) {
+    $queue = \MailPoet\Models\Queue::create();
+    $queue->newsletter_id = $data['newsletter_id'];
+    $queue->subscribers = json_encode(
+      array(
+        'to_process' => $data['subscribers']
+      )
+    );
+    $queue->count_total = $queue->count_to_process = count($data['subscribers']);
+    $queue->save();
+    wp_send_json(
+      !$queue->save() ?
+        array(
+          'result' => false,
+          'error' => 'Queue could not be created.'
+        ) :
+        array(
+          'result' => true,
+          'data' => array($queue->id)
+        )
+    );
+  }
+
+  function addQueues($data) {
+    $result = array_map(function ($queueData) {
+      $queue = \MailPoet\Models\Queue::create();
+      $queue->newsletter_id = $queueData['newsletter_id'];
+      $queue->subscribers = json_encode(
+        array(
+          'to_process' => $queueData['subscribers']
+        )
+      );
+      $queue->count_total = $queue->count_to_process = count($queueData['subscribers']);
+      $queue->save();
+      return array(
+        'newsletter_id' => $queue->newsletter_id,
+        'queue_id' => $queue->id
+      );
+    }, $data);
+    $result = Helpers::arrayColumn($result, 'queue_id', 'newsletter_id');
+    wp_send_json(
+      count($data) != count($result) ?
+        array(
+          'result' => false,
+          'error' => __('Some queues could not be created.'),
+          'data' => $result
+        ) :
+        array(
+          'result' => true,
+          'data' => $result
+        )
+    );
+  }
+
+  function deleteQueue($data) {
+    $queue = \MailPoet\Models\Queue::whereNull('deleted_at')
+      ->findOne($data['queue_id']);
+    if(!$queue) {
+      wp_send_json(
+        array(
+          'result' => false,
+          'error' => __('Queue not found.')
+        )
+      );
+    }
+    $queue->deleted_at = 'Y-m-d H:i:s';
+    $queue->save();
+    wp_send_json(array('result' => true));
+  }
+
+  function deleteQueues($data) {
+    $queues = \MailPoet\Models\Queue::whereNull('deleted_at')
+      ->whereIn('id', $data['queue_ids'])
+      ->findResultSet();
+    if(!$queues->count()) {
+      wp_send_json(
+        array(
+          'result' => false,
+          'error' => __('Queues not found.')
+        )
+      );
+    }
+    foreach($queues as $queue) {
+      $queue->deleted_at = 'Y-m-d H:i:s';
+      $queue->save();
+    }
+    wp_send_json(array('result' => true));
+  }
+
+  function getQueueStatus($data) {
+    $queue = \MailPoet\Models\Queue::whereNull('deleted_at')
+      ->findOne($data['queue_id'])
+      ->asArray();
+    wp_send_json(
+      !$queue ?
+        array(
+          'result' => false,
+          'error' => __('Queue not found.')
+        ) :
+        array(
+          'result' => true,
+          'data' => $queue
+        )
+    );
+  }
+
+  function getQueuesStatus($data) {
+    $queues = \MailPoet\Models\Queue::whereNull('deleted_at')
+      ->whereIn('id', $data['queue_ids'])
+      ->findArray();
+    wp_send_json(
+      !$queues ?
+        array(
+          'result' => false,
+          'error' => __('Queue not found.')
+        ) :
+        array(
+          'result' => true,
+          'data' => $queues
+        )
+    );
   }
 }
