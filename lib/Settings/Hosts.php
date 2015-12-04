beb939df9ecfc6ad3e8daf2d795458865b480bc2
Jonathan Labreuille <jonathan@mailpoet.com>
2015-12-04 15:31:04 +0100

    updated send with tab

--- lib/Settings/Hosts.php
+++ lib/Settings/Hosts.php
@@ -5,27 +5,43 @@ class Hosts {
   private static $_smtp = array(
     'AmazonSES' => array(
       'name' => 'Amazon SES',
-      'api' => false,
       'emails' => 100,
-      'interval' => 5
+      'interval' => 5,
+      'fields' => array(
+        'region',
+        'access_key',
+        'secret_key'
+      ),
+      'regions' => array(
+        'US East (N. Virginia)' => 'us-east-1.amazonaws.com',
+        'US West (Oregon)' => 'us-west-2.amazonaws.com',
+        'EU (Ireland)' => 'eu-west-1.amazonaws.com'
+      )
     ),
     'ElasticEmail' => array(
       'name' => 'ElasticEmail',
-      'api' => true,
       'emails' => 100,
-      'interval' => 5
+      'interval' => 5,
+      'fields' => array(
+        'api_key'
+      )
     ),
     'MailGun' => array(
       'name' => 'MailGun',
-      'api' => false,
       'emails' => 100,
-      'interval' => 5
+      'interval' => 5,
+      'fields' => array(
+        'domain',
+        'api_key'
+      )
     ),
     'SendGrid' => array(
       'name' => 'SendGrid',
-      'api' => true,
       'emails' => 100,
-      'interval' => 5
+      'interval' => 5,
+      'fields' => array(
+        'api_key'
+      )
     )
   );
 
