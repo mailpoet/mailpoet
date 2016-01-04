d1c09c015a3d4cc53734665c5d8e280f1677701f
Tautvidas Sipavičius <t.9b5as@gmail.com>
2016-01-04 13:19:40 +0200

    Remove remote sticky-kit dep, use static local patched package instead

--- webpack.config.js
+++ webpack.config.js
@@ -253,7 +253,7 @@ config.push(_.extend({}, baseConfig, {
       'tests/javascript/newsletter_editor'
     ],
     alias: {
-      'sticky-kit': 'sticky-kit/jquery.sticky-kit',
+      'sticky-kit': 'vendor_static/jquery.sticky-kit.js',
       'backbone.marionette': 'backbone.marionette/lib/backbone.marionette',
       'backbone.supermodel$': 'backbone.supermodel/build/backbone.supermodel.js',
       'blob$': 'blob/Blob.js',
