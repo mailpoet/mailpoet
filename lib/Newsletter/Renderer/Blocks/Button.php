16661af8c34c2a7bc2ad640b774d71516e07375c
Vlad <vlad@mailpoet.com>
2016-01-04 10:27:07 -0500

    - Updates text alignemnt for buttons

--- lib/Newsletter/Renderer/Blocks/Button.php
+++ lib/Newsletter/Renderer/Blocks/Button.php
@@ -9,9 +9,9 @@ class Button {
       <tr>
         <td class="mailpoet_padded" valign="top">
           <div>
-            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="border-spacing:0;mso-table-lspace:0;mso-table-rspace:0;text-align:' . $element['styles']['block']['textAlign'] . ';">
+            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="border-spacing:0;mso-table-lspace:0;mso-table-rspace:0;">
               <tr>
-                <td class="mailpoet_button-container" style="padding:8px 0;"><!--[if mso]>
+                <td class="mailpoet_button-container" style="padding:8px 0;text-align:' . $element['styles']['block']['textAlign'] . ';"><!--[if mso]>
                   <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word"
                     href="' . $element['url'] . '"
                     style="height:' . $element['styles']['block']['lineHeight'] . ';
