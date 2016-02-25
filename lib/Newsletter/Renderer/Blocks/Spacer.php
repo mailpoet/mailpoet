f9028d28c0df7af14e37c793bda4b119e7bf5a0c
Vlad <vlad@mailpoet.com>
2016-02-25 11:31:38 -0500

    - Adds background color to spacer

--- lib/Newsletter/Renderer/Blocks/Spacer.php
+++ lib/Newsletter/Renderer/Blocks/Spacer.php
@@ -3,9 +3,13 @@ namespace MailPoet\Newsletter\Renderer\Blocks;
 
 class Spacer {
   static function render($element) {
+    $height = $element['styles']['block']['height'];
+    $background_color = $element['styles']['block']['backgroundColor'];
     $template = '
       <tr>
-        <td class="mailpoet_spacer" height="' . (int) $element['styles']['block']['height'] . '" valign="top"></td>
+        <td class="mailpoet_spacer" ' .
+      (($background_color !== 'transparent') ? 'bgcolor="' . $background_color . '" ' : ' ') .
+      'height="' . $height . '" valign="top"></td>
       </tr>';
     return $template;
   }
