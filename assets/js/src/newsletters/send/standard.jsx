f8b1e153bef66ddd3ff86ab79f16683c3cac8117
Tautvidas Sipavičius <t.9b5as@gmail.com>
2016-05-13 13:15:55 +0300

    Make date picker translatable

--- assets/js/src/newsletters/send/standard.jsx
+++ assets/js/src/newsletters/send/standard.jsx
@@ -2,6 +2,7 @@ define(
   [
     'react',
     'jquery',
+    'underscore',
     'mailpoet',
     'form/fields/checkbox.jsx',
     'form/fields/select.jsx',
@@ -10,6 +11,7 @@ define(
   function(
     React,
     jQuery,
+    _,
     MailPoet,
     Checkbox,
     Select,
@@ -21,6 +23,68 @@ define(
         defaultDateTime = window.mailpoet_current_date + ' ' + '00:00:00';
         timeOfDayItems = window.mailpoet_schedule_time_of_day;
 
+    var datepickerTranslations = {
+      closeText: MailPoet.I18n.t('close'),
+      currentText: MailPoet.I18n.t('today'),
+      nextText: MailPoet.I18n.t('next'),
+      prevText: MailPoet.I18n.t('previous'),
+      monthNames: [
+        MailPoet.I18n.t('january'),
+        MailPoet.I18n.t('february'),
+        MailPoet.I18n.t('march'),
+        MailPoet.I18n.t('april'),
+        MailPoet.I18n.t('may'),
+        MailPoet.I18n.t('june'),
+        MailPoet.I18n.t('july'),
+        MailPoet.I18n.t('august'),
+        MailPoet.I18n.t('september'),
+        MailPoet.I18n.t('october'),
+        MailPoet.I18n.t('november'),
+        MailPoet.I18n.t('december')
+      ],
+      monthNamesShort: [
+        MailPoet.I18n.t('januaryShort'),
+        MailPoet.I18n.t('februaryShort'),
+        MailPoet.I18n.t('marchShort'),
+        MailPoet.I18n.t('aprilShort'),
+        MailPoet.I18n.t('mayShort'),
+        MailPoet.I18n.t('juneShort'),
+        MailPoet.I18n.t('julyShort'),
+        MailPoet.I18n.t('augustShort'),
+        MailPoet.I18n.t('septemberShort'),
+        MailPoet.I18n.t('octoberShort'),
+        MailPoet.I18n.t('novemberShort'),
+        MailPoet.I18n.t('decemberShort')
+      ],
+      dayNames: [
+        MailPoet.I18n.t('sunday'),
+        MailPoet.I18n.t('monday'),
+        MailPoet.I18n.t('tuesday'),
+        MailPoet.I18n.t('wednesday'),
+        MailPoet.I18n.t('thursday'),
+        MailPoet.I18n.t('friday'),
+        MailPoet.I18n.t('saturday')
+      ],
+      dayNamesShort: [
+        MailPoet.I18n.t('sundayShort'),
+        MailPoet.I18n.t('mondayShort'),
+        MailPoet.I18n.t('tuesdayShort'),
+        MailPoet.I18n.t('wednesdayShort'),
+        MailPoet.I18n.t('thursdayShort'),
+        MailPoet.I18n.t('fridayShort'),
+        MailPoet.I18n.t('saturdayShort')
+      ],
+      dayNamesMin: [
+        MailPoet.I18n.t('sundayMin'),
+        MailPoet.I18n.t('mondayMin'),
+        MailPoet.I18n.t('tuesdayMin'),
+        MailPoet.I18n.t('wednesdayMin'),
+        MailPoet.I18n.t('thursdayMin'),
+        MailPoet.I18n.t('fridayMin'),
+        MailPoet.I18n.t('saturdayMin')
+      ],
+    };
+
     var isScheduledField = {
       name: 'isScheduled',
     };
@@ -30,8 +94,9 @@ define(
         var $element = jQuery(this.refs.dateInput),
             that = this;
         if ($element.datepicker) {
-          $element.datepicker({
+          $element.datepicker(_.extend({
             dateFormat: "yy-mm-dd",
+            isRTL: false,
             onSelect: function(value) {
               that.props.onChange({
                 target: {
@@ -40,7 +105,7 @@ define(
                 },
               });
             }
-          });
+          }, datepickerTranslations));
 
           this.datepickerInitialized = true;
         }
