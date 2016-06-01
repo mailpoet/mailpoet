61987a204edb556d437d77c0c5b5f29b34fb24e5
Vlad <vlad@mailpoet.com>
2016-06-01 09:48:01 -0400

    - Fixes custom field shortcode matching logic

--- lib/Newsletter/Shortcodes/Categories/Subscriber.php
+++ lib/Newsletter/Shortcodes/Categories/Subscriber.php
@@ -1,8 +1,8 @@
 <?php
 namespace MailPoet\Newsletter\Shortcodes\Categories;
 
-use MailPoet\Models\SubscriberCustomField;
 use MailPoet\Models\Subscriber as SubscriberModel;
+use MailPoet\Models\SubscriberCustomField;
 
 require_once(ABSPATH . 'wp-includes/pluggable.php');
 
@@ -16,34 +16,36 @@ class Subscriber {
     switch($action) {
       case 'firstname':
         return ($subscriber) ? $subscriber['first_name'] : $default_value;
-      break;
+        break;
       case 'lastname':
         return ($subscriber) ? $subscriber['last_name'] : $default_value;
-      break;
+        break;
       case 'email':
         return ($subscriber) ? $subscriber['email'] : false;
-      break;
+        break;
       case 'displayname':
         if($subscriber && $subscriber['wp_user_id']) {
           $wp_user = get_userdata($subscriber['wp_user_id']);
           return $wp_user->user_login;
         }
         return $default_value;
-      break;
+        break;
       case 'count':
-        return SubscriberModel::filter('subscribed')->count();
-      break;
-      case preg_match('/cf_(\d+)/', $action, $custom_field) ? true : false:
-        if(empty($subscriber['id'])) return false;
-        $custom_field = SubscriberCustomField
-          ::where('subscriber_id', $subscriber['id'])
-          ->where('custom_field_id', $custom_field[1])
-          ->findOne();
-        return ($custom_field) ? $custom_field->value : false;
-      break;
+        return SubscriberModel::filter('subscribed')
+          ->count();
+        break;
       default:
+        if(preg_match('/cf_(\d+)/', $action, $custom_field) &&
+          !empty($subscriber['id'])
+        ) {
+          $custom_field = SubscriberCustomField
+            ::where('subscriber_id', $subscriber['id'])
+            ->where('custom_field_id', $custom_field[1])
+            ->findOne();
+          return ($custom_field) ? $custom_field->value : false;
+        }
         return false;
-      break;
+        break;
     }
   }
 }
