bc1fb235d3cff4e94531f693856f8a71e303103b
Vlad <vlad@mailpoet.com>
2016-06-07 18:47:00 -0400

    - Removes unused method from the base model. Closes #511

--- lib/Models/Model.php
+++ lib/Models/Model.php
@@ -152,18 +152,6 @@ class Model extends \Sudzy\ValidModel {
     }
   }
 
-  static function filterSearchCustomFields($orm, $searchCriteria = array(), $searchCondition = 'AND', $searchSymbol = '=') {
-    $havingFields = array_filter(
-      array_map(function ($customField) use ($searchSymbol) {
-        return sprintf('`%s` %s ?', $customField['name'], $searchSymbol);
-      }, $searchCriteria)
-    );
-    $havingValues = array_map(function ($customField) use ($searchSymbol) {
-      return (strtolower($searchSymbol) === 'like') ? '%' . $customField['value'] . '%' : $customField['value'];
-    }, $searchCriteria);
-    return $orm->having_raw(implode(' ' . $searchCondition . ' ', $havingFields), array_values($havingValues));
-  }
-
   static function getPublished() {
     return static::whereNull('deleted_at');
   }
