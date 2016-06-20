22dfb372ec70738ef3f4e999b0fe1952d507d222
Vlad <vlad@mailpoet.com>
2016-06-20 10:34:41 -0400

    - Updates bulk insert logic

--- lib/Models/StatisticsNewsletters.php
+++ lib/Models/StatisticsNewsletters.php
@@ -10,18 +10,27 @@ class StatisticsNewsletters extends Model {
     parent::__construct();
   }
 
-  static function createMultiple($data) {
-    if (count($data) % 3 !== 0) {
-      return false;
+  static function createMultiple(array $data) {
+    $values = array();
+    foreach($data as $value) {
+      if(!empty($value['newsletter_id']) &&
+         !empty($value['subscriber_id']) &&
+         !empty($value['queue_id'])
+      ) {
+        $values[] = $value['newsletter_id'];
+        $values[] = $value['subscriber_id'];
+        $values[] = $value['queue_id'];
+      }
     }
+    if (!count($values)) return false;
     return self::rawExecute(
       'INSERT INTO `' . self::$_table . '` ' .
       '(newsletter_id, subscriber_id, queue_id) ' .
       'VALUES ' . rtrim(
-        str_repeat('(?,?,?), ', count($data)/3),
+        str_repeat('(?,?,?), ', count($values) / 3),
         ', '
       ),
-      $data
+      $values
     );
   }
 }
