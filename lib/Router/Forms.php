362ee49ce4ac36b22e57a2840a3b8c49cafee9c5
Jonathan Labreuille <jonathan@mailpoet.com>
2016-07-19 17:38:45 +0200

    Let the statisticsForms model return the total signups instead of the form model - added unit test for getTotalSignups() method

--- lib/Router/Forms.php
+++ lib/Router/Forms.php
@@ -1,6 +1,7 @@
 <?php
 namespace MailPoet\Router;
 use \MailPoet\Models\Form;
+use \MailPoet\Models\StatisticsForms;
 use \MailPoet\Form\Renderer as FormRenderer;
 use \MailPoet\Listing;
 use \MailPoet\Form\Util;
@@ -29,9 +30,9 @@ class Forms {
 
     // fetch segments relations for each returned item
     foreach($listing_data['items'] as $key => $form) {
-      $form = $form
-        ->withSignups()
-        ->asArray();
+      $form = $form->asArray();
+
+      $form['signups'] = StatisticsForms::getTotalSignups($form['id']);
 
       $form['segments'] = (
         !empty($form['settings']['segments'])
