03f3a6080c327cb25ce89d1980c33dc0a89569f7
Vlad <vlad@mailpoet.com>
2016-08-18 09:28:53 -0400

    - Returns empty response or an image in all cases

--- lib/Statistics/Track/Opens.php
+++ lib/Statistics/Track/Opens.php
@@ -7,7 +7,7 @@ if(!defined('ABSPATH')) exit;
 
 class Opens {
   static function track($data, $display_image = true) {
-    if(!$data) return self::displayImage();
+    if(!$data) return self::returnResponse($display_image);
     $subscriber = $data->subscriber;
     $queue = $data->queue;
     $newsletter = $data->newsletter;
@@ -20,12 +20,11 @@ class Opens {
         $queue->id
       );
     }
-    return ($display_image) ?
-      self::displayImage() :
-      true;
+    return self::returnResponse($display_image);
   }
 
-  static function displayImage() {
+  static function returnResponse($display_image) {
+    if(!$display_image) return;
     // return 1x1 pixel transparent gif image
     header('Content-Type: image/gif');
     echo "\x47\x49\x46\x38\x37\x61\x1\x0\x1\x0\x80\x0\x0\xfc\x6a\x6c\x0\x0\x0\x2c\x0\x0\x0\x0\x1\x0\x1\x0\x0\x2\x2\x44\x1\x0\x3b";
