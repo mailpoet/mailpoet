2490d8c9198c0c026b699022d359467cafd69074
Vlad <vlad@mailpoet.com>
2016-08-17 21:15:42 -0400

    - Refactors browser preview

--- lib/Newsletter/ViewInBrowser.php
+++ lib/Newsletter/ViewInBrowser.php
@@ -14,13 +14,13 @@ class ViewInBrowser {
 
   static function view($data) {
     $data = self::processData($data);
-    if(!$data) return false;
+    if(!$data) self::abort();
     $rendered_newsletter =
       self::getAndRenderNewsletter(
-        $data['newsletter'],
-        $data['subscriber'],
-        $data['queue'],
-        $data['preview']
+        $data->newsletter,
+        $data->subscriber,
+        $data->queue,
+        $data->preview
       );
     header('Content-Type: text/html; charset=utf-8');
     echo $rendered_newsletter;
@@ -28,60 +28,42 @@ class ViewInBrowser {
   }
 
   static function processData($data) {
-    if(empty($data['subscriber_id']) ||
-      empty($data['subscriber_token']) ||
-      empty($data['newsletter_id'])
+    $data = (object)$data;
+    if(empty($data->subscriber_id) ||
+       empty($data->subscriber_token) ||
+       empty($data->newsletter_id)
     ) {
       return false;
     }
-    $data['newsletter'] = self::getNewsletter($data['newsletter_id']);
-    $data['subscriber'] = self::getSubscriber($data['subscriber_id']);
-    $data['queue'] = self::getQueue($data['queue_id']);
-    $data_processed_successfully =
-      ($data['subscriber'] && $data['newsletter']);
-    return ($data_processed_successfully) ?
-      self::validateData($data) :
+    $data->newsletter = Newsletter::findOne($data->newsletter_id);
+    $data->subscriber = Subscriber::findOne($data->subscriber_id);
+    $data->queue = ($data->queue_id) ?
+      SendingQueue::findOne($data->queue_id) :
       false;
+    return self::validateData($data);
   }
 
   static function validateData($data) {
-    if(!$data['subscriber']) return false;
+    if(!$data->subscriber || !$data->newsletter) return false;
     $subscriber_token_match =
-      Subscriber::verifyToken($data['subscriber']['email'], $data['subscriber_token']);
-    // return if this is an administrator user previewing the newsletter
-    if($data['subscriber']['wp_user_id'] && $subscriber_token_match && $data['preview']) {
-      return ($subscriber_token_match) ? $data : false;
+      Subscriber::verifyToken($data->subscriber->email, $data->subscriber_token);
+    if(!$subscriber_token_match) return false;
+    // return if this is a WP user previewing the newsletter
+    if($data->subscriber->isWPUser() && $data->preview) {
+      return $data;
     }
     // if queue exists, check if the newsletter was sent to the subscriber
-    if($data['queue'] && $data['subscriber']) {
-      $is_valid_subscriber =
-        (!empty($data['queue']['subscribers']['processed']) &&
-          in_array($data['subscriber']['id'], $data['queue']['subscribers']['processed']));
-      $data = ($is_valid_subscriber && $subscriber_token_match) ? $data : false;
-    } else {
-      $data = ($subscriber_token_match) ? $data : false;
+    if($data->queue) {
+      $data = ($data->queue->isSubscriberProcessed($data->subscriber->id)) ?
+        $data :
+        false;
     }
     return $data;
   }
 
-  static function getNewsletter($newsletter_id) {
-    $newsletter = Newsletter::findOne($newsletter_id);
-    return ($newsletter) ? $newsletter->asArray() : $newsletter;
-  }
-
-  static function getQueue($queue_id) {
-    $queue = SendingQueue::findOne($queue_id);
-    return ($queue) ? $queue->asArray() : $queue;
-  }
-
-  static function getSubscriber($subscriber_id) {
-    $subscriber = Subscriber::findOne($subscriber_id);
-    return ($subscriber) ? $subscriber->asArray() : $subscriber;
-  }
-
   static function getAndRenderNewsletter($newsletter, $subscriber, $queue, $preview) {
-    if($queue && $queue['newsletter_rendered_body']) {
-      $newsletter_body = json_decode($queue['newsletter_rendered_body'], true);
+    if($queue && $queue->newsletter_rendered_body) {
+      $newsletter_body = $queue->getRenderedNewsletterBody();
     } else {
       $renderer = new Renderer($newsletter, $preview);
       $newsletter_body = $renderer->render();
@@ -94,8 +76,8 @@ class ViewInBrowser {
     $rendered_newsletter = $shortcodes->replace($newsletter_body['html']);
     if($queue && (boolean)Setting::getValue('tracking.enabled')) {
       $rendered_newsletter = Links::replaceSubscriberData(
-        $subscriber['id'],
-        $queue['id'],
+        $subscriber->id,
+        $queue->id,
         $rendered_newsletter,
         $preview
       );
