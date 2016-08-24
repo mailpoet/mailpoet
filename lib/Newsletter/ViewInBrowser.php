80fddd6c589a42c9e597314ba740db443d85990b
Vlad <vlad@mailpoet.com>
2016-08-23 23:42:26 -0400

    - Refactors view in browser

--- lib/Newsletter/ViewInBrowser.php
+++ lib/Newsletter/ViewInBrowser.php
@@ -1,67 +1,32 @@
 <?php
 namespace MailPoet\Newsletter;
 
-use MailPoet\Models\Newsletter;
-use MailPoet\Models\SendingQueue;
 use MailPoet\Models\Setting;
-use MailPoet\Models\Subscriber;
 use MailPoet\Newsletter\Links\Links;
 use MailPoet\Newsletter\Renderer\Renderer;
 use MailPoet\Newsletter\Shortcodes\Shortcodes;
 
 class ViewInBrowser {
-  static function view($data) {
-    $data = self::preProcessData($data);
-    if(!self::validateData($data)) self::abort();
+  function view($data) {
+    if(!$data) {
+      return $this->abort();
+    }
+    $wp_user_preview = ($data->preview && $data->subscriber->isWPUser());
     $rendered_newsletter =
-      self::getAndRenderNewsletter(
+      $this->renderNewsletter(
         $data->newsletter,
         $data->subscriber,
         $data->queue,
-        $data->preview
+        $wp_user_preview
       );
-    header('Content-Type: text/html; charset=utf-8');
-    echo $rendered_newsletter;
-    exit;
+    return $this->displayNewsletter($rendered_newsletter);
   }
 
-  static function preProcessData($data) {
-    $data = (object)$data;
-    if(empty($data->subscriber_id) ||
-       empty($data->subscriber_token) ||
-       empty($data->newsletter_id)
-    ) {
-      return false;
-    }
-    $data->newsletter = Newsletter::findOne($data->newsletter_id);
-    $data->subscriber = Subscriber::findOne($data->subscriber_id);
-    $data->queue = ($data->queue_id) ?
-      SendingQueue::findOne($data->queue_id) :
-      false;
-    return $data;
-  }
-
-  static function validateData($data) {
-    if(!$data || !$data->subscriber || !$data->newsletter) return false;
-    $subscriber_token_match =
-      Subscriber::verifyToken($data->subscriber->email, $data->subscriber_token);
-    if(!$subscriber_token_match) return false;
-    // return if this is a WP user previewing the newsletter
-    if($data->subscriber->isWPUser() && $data->preview) {
-      return $data;
-    }
-    // if queue exists, check if the newsletter was sent to the subscriber
-    if($data->queue && !$data->queue->isSubscriberProcessed($data->subscriber->id)) {
-      $data = false;
-    }
-    return $data;
-  }
-
-  static function getAndRenderNewsletter($newsletter, $subscriber, $queue, $preview) {
+  function renderNewsletter($newsletter, $subscriber, $queue, $wp_user_preview) {
     if($queue && $queue->newsletter_rendered_body) {
       $newsletter_body = $queue->getRenderedNewsletterBody();
     } else {
-      $renderer = new Renderer($newsletter, $preview);
+      $renderer = new Renderer($newsletter, $wp_user_preview);
       $newsletter_body = $renderer->render();
     }
     $shortcodes = new Shortcodes(
@@ -75,13 +40,19 @@ class ViewInBrowser {
         $subscriber->id,
         $queue->id,
         $rendered_newsletter,
-        $preview
+        $wp_user_preview
       );
     }
     return $rendered_newsletter;
   }
 
-  private static function abort() {
+  function displayNewsletter($rendered_newsletter) {
+    header('Content-Type: text/html; charset=utf-8');
+    echo $rendered_newsletter;
+    exit;
+  }
+
+  function abort() {
     status_header(404);
     exit;
   }
--- lib/Router/Endpoints/ViewInBrowser.php
+++ lib/Router/Endpoints/ViewInBrowser.php
@@ -1,6 +1,9 @@
 <?php
 namespace MailPoet\Router\Endpoints;
 
+use MailPoet\Models\Newsletter;
+use MailPoet\Models\SendingQueue;
+use MailPoet\Models\Subscriber;
 use MailPoet\Newsletter\ViewInBrowser as NewsletterViewInBrowser;
 
 if(!defined('ABSPATH')) exit;
@@ -10,6 +13,39 @@ class ViewInBrowser {
   const ACTION_VIEW = 'view';
 
   static function view($data) {
-    NewsletterViewInBrowser::view($data);
+    $view_in_browser = new NewsletterViewInBrowser();
+    $view_in_browser->view(self::_processBrowserPreviewData($data));
+  }
+
+  static function _processBrowserPreviewData($data) {
+    $data = (object)$data;
+    if(empty($data->subscriber_id) ||
+      empty($data->subscriber_token) ||
+      empty($data->newsletter_id)
+    ) {
+      return false;
+    }
+    $data->newsletter = Newsletter::findOne($data->newsletter_id);
+    $data->subscriber = Subscriber::findOne($data->subscriber_id);
+    $data->queue = ($data->queue_id) ?
+      SendingQueue::findOne($data->queue_id) :
+      false;
+    return self::_validateBrowserPreviewData($data);
+  }
+
+  static function _validateBrowserPreviewData($data) {
+    if(!$data || !$data->subscriber || !$data->newsletter) return false;
+    $subscriber_token_match =
+      Subscriber::verifyToken($data->subscriber->email, $data->subscriber_token);
+    if(!$subscriber_token_match) return false;
+    // return if this is a WP user previewing the newsletter
+    if($data->subscriber->isWPUser() && $data->preview) {
+      return $data;
+    }
+    // if queue exists, check if the newsletter was sent to the subscriber
+    if($data->queue && !$data->queue->isSubscriberProcessed($data->subscriber->id)) {
+      $data = false;
+    }
+    return $data;
   }
 }
