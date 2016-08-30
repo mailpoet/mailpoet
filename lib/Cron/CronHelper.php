fb433dd25adaf68c180df2a50f1ebd3a0f7b190b
Vlad <site.github@mrcasual.com>
2016-08-30 09:07:49 -0400

    - Updates site URL detection function for hosts that are behind   proxy/running on port that's different from what's returned by   home_url()

--- lib/Cron/CronHelper.php
+++ lib/Cron/CronHelper.php
@@ -60,6 +60,7 @@ class CronHelper {
       QueueEndpoint::ACTION_RUN,
       $data
     );
+    $url = str_replace(home_url(), self::getSiteUrl(), $url);
     $args = array(
       'blocking' => false,
       'sslverify' => false,
@@ -70,20 +71,20 @@ class CronHelper {
     return wp_remote_retrieve_body($result);
   }
 
-  private static function getSiteUrl() {
-    // additional check for some sites running on a virtual machine or behind
+  static function getSiteUrl() {
+    // additional check for some sites running inside a virtual machine or behind
     // proxy where there could be different ports (e.g., host:8080 => guest:80)
-
-    // if the site URL does not contain a port, return the URL
-    if(!preg_match('!^https?://.*?:\d+!', site_url())) return site_url();
-    preg_match('!://(?P<host>.*?):(?P<port>\d+)!', site_url(), $server);
-    // connect to the URL with port
-    $fp = @fsockopen($server['host'], $server['port'], $errno, $errstr, 1);
-    if($fp) return site_url();
-    // connect to the URL without port
-    $fp = @fsockopen($server['host'], $server['port'], $errno, $errstr, 1);
-    if($fp) return preg_replace('!(?=:\d+):\d+!', '$1', site_url());
-    // throw an error if all connection attempts failed
+    $url = parse_url(home_url());
+    // 1. if the site URL does not contain a port, return the URL
+    if(empty($url['port'])) return home_url();
+    // 2. connect to the URL with specified port
+    $fp = @fsockopen($url['host'], $url['port'], $errno, $errstr, 1);
+    if($fp) return home_url();
+    $port = (strtolower($url['scheme']) === 'http') ? 80 : 443;
+    // 3. connect to the URL with default port
+    $fp = fsockopen($url['host'], $port, $errno, $errstr, 1);
+    if($fp) return sprintf('%s://%s', $url['scheme'], $url['host']);
+    // 4. throw an error if all connection attempts failed
     throw new \Exception(__('Site URL is unreachable.'));
   }
 
