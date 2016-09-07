f17c8228cdb995b771b6c6ad76b17f419be5667d
Vlad <site.github@mrcasual.com>
2016-09-07 18:21:31 -0400

    - Adds units tests for cron triggers

---
+++ tests/unit/Cron/Triggers/MailPoetTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use MailPoet\API\Endpoints\Cron;
+use MailPoet\Cron\CronHelper;
+use MailPoet\Cron\Triggers\MailPoet;
+use MailPoet\Models\Setting;
+
+class MailPoetCronTriggerTest extends MailPoetTest {
+  function _before() {
+    // cron trigger is by default set to 'WordPress'; when it runs and does not
+    // detect any queues to process, it deletes the daemon setting, so Supervisor that's
+    // called by the MailPoet cron trigger does not work. for that matter, we need to set
+    // the trigger setting to anything but 'WordPress'.
+    Setting::setValue('cron_trigger', array(
+      'method' => 'none'
+    ));
+  }
+
+  function testItCanRun() {
+    expect(Setting::getValue(CronHelper::DAEMON_SETTING))->null();
+    MailPoet::run();
+    expect(Setting::getValue(CronHelper::DAEMON_SETTING))->notEmpty();
+  }
+
+  function _after() {
+    ORM::raw_execute('TRUNCATE ' . Setting::$_table);
+  }
+}
