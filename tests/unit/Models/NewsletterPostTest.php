1a85914c1bd10b8f9166ba2e46134c5d5e75cb2e
Vlad <site.github@mrcasual.com>
2016-09-20 20:08:44 -0400

    - Adds unit test

---
+++ tests/unit/Models/NewsletterPostTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Carbon\Carbon;
+use MailPoet\Models\NewsletterPost as NewsletterPost;
+
+class NewsletterPostTest extends MailPoetTest {
+  function testItCanGetLatestNewsletterPost() {
+    foreach(range(1, 5) as $index) {
+      $newsletter_post = NewsletterPost::create();
+      $newsletter_post->newsletter_id = 1;
+      $newsletter_post->post_id = $index;
+      $newsletter_post->save();
+      $newsletter_post->created_at = Carbon::now()
+        ->addMinutes($index);
+      $newsletter_post->save();
+    }
+    $latest_newsletter_post = NewsletterPost::getNewestNewsletterPost(1);
+    expect($latest_newsletter_post->post_id)->equals(5);
+  }
+
+  function _after() {
+    ORM::for_table(NewsletterPost::$_table)
+      ->deleteMany();
+  }
+}
