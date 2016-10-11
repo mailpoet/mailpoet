bf894fc26f603bc1448dd2da93c00eea545f91e7
Tautvidas Sipavičius <t.9b5as@gmail.com>
2016-10-11 13:02:08 +0300

    Bump up release version to 0.0.48, update changelog

--- mailpoet.php
+++ mailpoet.php
@@ -4,7 +4,7 @@ if(!defined('ABSPATH')) exit;
 use \MailPoet\Config\Initializer;
 /*
  * Plugin Name: MailPoet
- * Version: 0.0.47
+ * Version: 0.0.48
  * Plugin URI: http://www.mailpoet.com
  * Description: MailPoet Newsletters.
  * Author: MailPoet
@@ -22,7 +22,7 @@ use \MailPoet\Config\Initializer;
 
 require 'vendor/autoload.php';
 
-define('MAILPOET_VERSION', '0.0.47');
+define('MAILPOET_VERSION', '0.0.48');
 
 $initializer = new Initializer(array(
   'file' => __FILE__,
