d5a9b84ca32b29f940b16f3aeed1c6b4fda1b14e
Vlad <site.github@mrcasual.com>
2016-11-21 18:06:47 -0500

    - Updates "tested up to" WP version - Includes a check for missing autoloader and displays an error notice

--- mailpoet.php
+++ mailpoet.php
@@ -1,7 +1,8 @@
 <?php
 if(!defined('ABSPATH')) exit;
 
-use \MailPoet\Config\Initializer;
+use MailPoet\Config\Initializer;
+
 /*
  * Plugin Name: MailPoet
  * Version: 3.0.0-beta.4
@@ -10,7 +11,7 @@ use \MailPoet\Config\Initializer;
  * Author: MailPoet
  * Author URI: http://www.mailpoet.com
  * Requires at least: 4.0
- * Tested up to: 4.0
+ * Tested up to: 4.6.1
  *
  * Text Domain: mailpoet
  * Domain Path: /lang/
@@ -20,12 +21,20 @@ use \MailPoet\Config\Initializer;
  * @since 0.0.8
  */
 
-require 'vendor/autoload.php';
-
-define('MAILPOET_VERSION', '3.0.0-beta.4');
-
-$initializer = new Initializer(array(
-  'file' => __FILE__,
-  'version' => MAILPOET_VERSION
-));
-$initializer->init();
+$mailpoet_loader = dirname(__FILE__) . '/vendor/autoload.php';
+if(file_exists($mailpoet_loader)) {
+  require $mailpoet_loader;
+  define('MAILPOET_VERSION', '3.0.0-beta.4');
+  $initializer = new Initializer(
+    array(
+      'file' => __FILE__,
+      'version' => MAILPOET_VERSION
+    )
+  );
+  $initializer->init();
+} else {
+  add_action('admin_notices', function() {
+    $notice = __('MailPoet cannot start because it is missing core files. Please reinstall the plugin.', 'mailpoet');
+    printf('<div class="error"><p>%1$s</p></div>', $notice);
+  });
+}
