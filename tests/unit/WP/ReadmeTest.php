4ae55230da721c5c4dbc030aa56d19240b71cbbb
Alexey Stoletniy <stoletniy@gmail.com>
2016-12-12 10:04:36 +0300

    Display changelog from readme.txt on Update page [MAILPOET-708]

---
+++ tests/unit/WP/ReadmeTest.php
@@ -0,0 +1,28 @@
+<?php
+use \MailPoet\WP\Readme;
+
+class ReadmeTest extends MailPoetTest {
+  function _before() {
+    // Sample taken from https://wordpress.org/plugins/about/readme.txt
+    $this->data = file_get_contents(dirname(__FILE__) . '/ReadmeTestData.txt');
+  }
+
+  function testItParsesChangelog() {
+    $result = Readme::parseChangelog($this->data);
+    expect(count($result))->equals(2);
+    expect(count($result[0]['changes']))->equals(2);
+    expect(count($result[1]['changes']))->equals(1);
+  }
+
+  function testItRespectsLimitOfParsedItems() {
+    $result = Readme::parseChangelog($this->data, 1);
+    expect(count($result))->equals(1);
+  }
+
+  function testItReturnsFalseOnMalformedData() {
+    $result = Readme::parseChangelog("");
+    expect($result)->false();
+    $result = Readme::parseChangelog("== Changelog ==\n\n\n=\n==");
+    expect($result)->false();
+  }
+}
