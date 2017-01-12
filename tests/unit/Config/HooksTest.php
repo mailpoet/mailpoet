b758284df79b2f856ad7c118e57f36911d9a8245
Vlad <site.github@mrcasual.com>
2017-01-12 11:12:08 -0500

    - Fixes unit test

--- tests/unit/Config/HooksTest.php
+++ tests/unit/Config/HooksTest.php
@@ -2,20 +2,9 @@
 
 class HooksTest extends MailPoetTest {
   function testItHooksSchedulerToMultiplePostTypes() {
-    global $wp_filter;
     $post_types = get_post_types();
-    $hook_count = 0;
     foreach($post_types as $post_type) {
-      expect(!empty($wp_filter['publish_' . $post_type]))->true();
-      $filter = $wp_filter['publish_' . $post_type];
-      $is_hooked = false;
-      foreach($filter->callbacks[10] as $name => $hook) {
-        if(!preg_match('/schedulePostNotification/', $name)) continue;
-        $is_hooked = true;
-        $hook_count++;
-      }
-      expect($is_hooked)->true();
+      expect(has_filter('publish_' . $post_type, '\MailPoet\Newsletter\Scheduler\Scheduler::schedulePostNotification'))->notEmpty();
     }
-    expect($hook_count)->equals(count($post_types));
   }
 }
