a4a654cfd5a69e4573d67876805f9d124da23fb9
Vlad <site.github@mrcasual.com>
2017-01-19 19:38:13 -0500

    - Removes the use of namespace to ensure PHP 5.2 compatibility - Adds a PHP version check and disabled the plugin if the check fails - Adds a check for Initializer and autoload core files - Moves plugin initialization logic outside to the Initializer class

--- mailpoet.php
+++ mailpoet.php
@@ -1,7 +1,6 @@
 <?php
-if(!defined('ABSPATH')) exit;
 
-use MailPoet\Config\Initializer;
+if(!defined('ABSPATH')) exit;
 
 /*
  * Plugin Name: MailPoet
@@ -18,23 +17,49 @@ use MailPoet\Config\Initializer;
  *
  * @package WordPress
  * @author MailPoet
- * @since 0.0.8
+ * @since 3.0.0-beta.1
  */
 
-$mailpoet_loader = dirname(__FILE__) . '/vendor/autoload.php';
-if(file_exists($mailpoet_loader)) {
-  require $mailpoet_loader;
-  define('MAILPOET_VERSION', '3.0.0-beta.14');
-  $initializer = new Initializer(
-    array(
-      'file' => __FILE__,
-      'version' => MAILPOET_VERSION
-    )
-  );
-  $initializer->init();
-} else {
+$mailpoet_plugin = array(
+  'version' => '3.0.0-beta.14',
+  'filename' => __FILE__,
+  'path' => dirname(__FILE__),
+  'loader' => dirname(__FILE__) . '/vendor/autoload.php',
+  'initializer' => dirname(__FILE__) . '/lib/Config/Initializer.php'
+);
+
+// Check for the minimum PHP version
+if(version_compare(phpversion(), '5.3.0', '<')) {
+  add_action('admin_notices', function() {
+    $notice = str_replace(
+      '[link]',
+      '<a href="//docs.mailpoet.com/article/152-minimum-requirements-for-mailpoet-3#php_version" target="_blank">',
+      __('MailPoet plugin requires PHP version 5.3 or newer. Please read our [link]instructions[/link] on how to resolve this issue.', 'mailpoet')
+    );
+    $notice = str_replace('[/link]', '</a>', $notice);
+    printf('<div class="error"><p>%1$s</p></div>', $notice);
+  });
+  // deactivate the plugin
+  add_action('admin_init', function() {
+    deactivate_plugins(plugin_basename(__FILE__));
+  });
+  if(!empty($_GET['activate'])) {
+    unset($_GET['activate']);
+  }
+  return;
+}
+
+// Check for core dependencies
+if(!file_exists($mailpoet_plugin['loader']) && !file_exists($mailpoet_plugin['initializer'])) {
   add_action('admin_notices', function() {
     $notice = __('MailPoet cannot start because it is missing core files. Please reinstall the plugin.', 'mailpoet');
     printf('<div class="error"><p>%1$s</p></div>', $notice);
   });
+  return;
 }
+
+// Initialize the plugin
+define('MAILPOET_VERSION', $mailpoet_plugin['version']);
+define('MAILPOET_FILENAME', $mailpoet_plugin['filename']);
+define('MAILPOET_PATH', $mailpoet_plugin['path']);
+require_once($mailpoet_plugin['initializer']);
