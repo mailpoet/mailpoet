063c271e401a38592303fced8524c5b70c769230
Vlad <site.github@mrcasual.com>
2017-01-27 18:39:45 -0500

    Fixes PDO extension check condition

--- lib/Config/RequirementsChecker.php
+++ lib/Config/RequirementsChecker.php
@@ -78,14 +78,12 @@ class RequirementsChecker {
   }
 
   function checkPDOExtension() {
-    if(!extension_loaded('pdo') && !extension_loaded('pdo_mysql')) {
-      $error = Helpers::replaceLinkTags(
-        __('This plugin requires PDO_MYSQL PHP extension. Please read our [link]instructions[/link] on how to resolve this issue.', 'mailpoet'),
-        '//docs.mailpoet.com/article/152-minimum-requirements-for-mailpoet-3#php_extension'
-      );
-      return $this->processError($error);
-    }
-    return true;
+    if(extension_loaded('pdo') && extension_loaded('pdo_mysql')) return true;
+    $error = Helpers::replaceLinkTags(
+      __('This plugin requires PDO_MYSQL PHP extension. Please read our [link]instructions[/link] on how to resolve this issue.', 'mailpoet'),
+      '//docs.mailpoet.com/article/152-minimum-requirements-for-mailpoet-3#php_extension'
+    );
+    return $this->processError($error);
   }
 
   function checkMbstringExtension() {
