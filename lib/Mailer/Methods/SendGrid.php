b539eae7f99a83e2712c1b2639251941f4391a70
Alexey Stoletniy <stoletniy@gmail.com>
2017-02-13 10:35:21 +0300

    Fix SendGrid error message assignment [MAILPOET-810]

--- lib/Mailer/Methods/SendGrid.php
+++ lib/Mailer/Methods/SendGrid.php
@@ -28,7 +28,7 @@ class SendGrid {
     if(wp_remote_retrieve_response_code($result) !== 200) {
       $response = json_decode($result['body'], true);
       $response = (!empty($response['errors'][0])) ?
-        $response['errors'] :
+        $response['errors'][0] :
         sprintf(__('%s has returned an unknown error.', 'mailpoet'), Mailer::METHOD_SENDGRID);
       return Mailer::formatMailerSendErrorResult($response);
     }
