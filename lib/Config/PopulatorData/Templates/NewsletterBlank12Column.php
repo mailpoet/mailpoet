b3abf46604c691319bcd86c423b42e7bfd9cad0c
Tautvidas Sipavičius <t.9b5as@gmail.com>
2017-02-15 20:12:53 +0200

    Update outdated newsletter template translation strings

--- lib/Config/PopulatorData/Templates/NewsletterBlank12Column.php
+++ lib/Config/PopulatorData/Templates/NewsletterBlank12Column.php
@@ -116,7 +116,7 @@ class NewsletterBlank12Column {
                   ),
                   array(
                     "type" => "text",
-                    "text" => __("<h1 style=\"text-align: center;\"><strong>Let's Get Started!</strong></h1>\n<p></p>\n<p>It's time to design your newsletter! In the right sidebar, you'll find 4 menu items that will help you customize your newsletter:</p>\n<ol>\n<li>Content</li>\n<li>Layout</li>\n<li>Styles</li>\n<li>Preview</li>\n</ol>", 'mailpoet')
+                    "text" => __("<h1 style=\"text-align: center;\"><strong>Let's Get Started!</strong></h1>\n<p></p>\n<p>It's time to design your newsletter! In the right sidebar, you'll find 4 menu items that will help you customize your newsletter:</p>\n<ol>\n<li>Content</li>\n<li>Columns</li>\n<li>Styles</li>\n<li>Preview</li>\n</ol>", 'mailpoet')
                   ),
                   array(
                     "type" => "divider",
