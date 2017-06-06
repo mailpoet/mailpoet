95072a9ac5ad1399def16b4f476467d207ebcd61
stoletniy <stoletniy@users.noreply.github.com>
2017-06-06 19:46:34 +0300

    Restore empty license key warnings [MAILPOET-933]

--- lib/Config/ServicesChecker.php
+++ lib/Config/ServicesChecker.php
@@ -58,7 +58,7 @@ class ServicesChecker {
     $premium_plugin_active = License::getLicense();
     $premium_key = Setting::getValue(Bridge::PREMIUM_KEY_STATE_SETTING_NAME);
 
-    if(!$premium_plugin_active || !$premium_key_specified) {
+    if(!$premium_plugin_active) {
       $display_error_notice = false;
     }
 
