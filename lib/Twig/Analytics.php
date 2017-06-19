c6d0f0f90fb6449747bcc907f8608d6f0be7a839
Pavel Dohnal <pavel@mailpoet.com>
2017-06-19 17:05:33 +0100

    Fix lint errors
    I don't know how did I do those errors

--- lib/Twig/Analytics.php
+++ lib/Twig/Analytics.php
@@ -9,22 +9,22 @@ if(!defined('ABSPATH')) exit;
 class Analytics extends \Twig_Extension {
 
   public function getName() {
-return 'analytics';
-}
+    return 'analytics';
+  }
 
-public function getFunctions() {
-  $analytics = new AnalyticsGenerator(new Reporter());
-  return array(
-    new \Twig_SimpleFunction(
-      'get_analytics_data',
-      array($analytics, 'generateAnalytics'),
-      array('is_safe' => array('all'))
-    ),
-    new \Twig_SimpleFunction(
-      'is_analytics_enabled',
-      array($analytics, 'isEnabled'),
-      array('is_safe' => array('all'))
-    ),
-  );
-}
+  public function getFunctions() {
+    $analytics = new AnalyticsGenerator(new Reporter());
+    return array(
+      new \Twig_SimpleFunction(
+        'get_analytics_data',
+        array($analytics, 'generateAnalytics'),
+        array('is_safe' => array('all'))
+      ),
+      new \Twig_SimpleFunction(
+        'is_analytics_enabled',
+        array($analytics, 'isEnabled'),
+        array('is_safe' => array('all'))
+      ),
+    );
+  }
 }
