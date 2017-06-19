df7d24d395dc861b9a942685a895e170407bd233
Pavel Dohnal <pavel@mailpoet.com>
2017-06-19 15:11:01 +0100

    Fix fully qualified class name
    [MAILPOET-686]

--- lib/Twig/Analytics.php
+++ lib/Twig/Analytics.php
@@ -2,29 +2,29 @@
 namespace MailPoet\Twig;
 
 use MailPoet\Analytics\Reporter;
-use MailPoet\Models\Setting;
+use MailPoet\Analytics\Analytics as AnalyticsGenerator;
 
 if(!defined('ABSPATH')) exit;
 
 class Analytics extends \Twig_Extension {
 
   public function getName() {
-    return 'analytics';
-  }
+return 'analytics';
+}
 
-  public function getFunctions() {
-    $analytics = new \MailPoet\Analytics\Analytics(new Reporter());
-    return array(
-      new \Twig_SimpleFunction(
-        'get_analytics_data',
-        array($analytics, 'generateAnalytics'),
-        array('is_safe' => array('all'))
-      ),
-      new \Twig_SimpleFunction(
-        'is_analytics_enabled',
-        array($analytics, 'isEnabled'),
-        array('is_safe' => array('all'))
-      ),
-    );
-  }
+public function getFunctions() {
+  $analytics = new AnalyticsGenerator(new Reporter());
+  return array(
+    new \Twig_SimpleFunction(
+      'get_analytics_data',
+      array($analytics, 'generateAnalytics'),
+      array('is_safe' => array('all'))
+    ),
+    new \Twig_SimpleFunction(
+      'is_analytics_enabled',
+      array($analytics, 'isEnabled'),
+      array('is_safe' => array('all'))
+    ),
+  );
+}
 }
