145acf05c783cf80e3a4e0218e35bfbd7111e5b0
Pavel Dohnal <pavel@mailpoet.com>
2017-06-20 08:57:22 +0100

    Enable dynamic mixpanel initialisation
    [MAILPOET-686]

--- assets/js/src/analytics_event.js
+++ assets/js/src/analytics_event.js
@@ -1,32 +1,18 @@
-define(
-  ['mailpoet'],
-  function(MailPoet) {
-
-    MailPoet.trackEvent = cacheEvent;
-    MailPoet.forceTrackEvent = cacheEvent;
-
-    initializeMixpanelWhenLoaded();
-  }
-);
+/*
+ * This creates two functions and adds them to MailPoet object
+ * - `trackEvent` which should be used in normal circumstances.
+ *   This function tracks an event and sends it to mixpanel.
+ *   This function does nothing if analytics is disabled.
+ * - `forceTrackEvent` which sends given event to analytics
+ *   even if it has been disabled.
+ */
 
 var eventsCache = [];
-function cacheEvent(name, data) {
-  eventsCache.push({
-    name: name,
-    data: data
-  });
-}
-
-function initializeMixpanelWhenLoaded() {
-  if (typeof window.mixpanel === "object" && typeof window.mixpanel.track === "function") {
-    exportMixpanel(MailPoet);
-    trackCachedEvents(eventsCache);
-  } else {
-    setTimeout(initializeMixpanelWhenLoaded, 100);
-  }
-}
 
 function track(name, data){
+  if (typeof window.mixpanel.track !== "function") {
+    window.mixpanel.init(window.mixpanelTrackingId);
+  }
   window.mixpanel.track(name, data);
 }
 
@@ -40,10 +26,38 @@ function exportMixpanel(MailPoet) {
   }
 }
 
-function trackCachedEvents(eventsCache) {
-  if (window.mailpoet_analytics_enabled) {
-    eventsCache.map(function (event) {
+function trackCachedEvents() {
+  eventsCache.map(function (event) {
+    if (window.mailpoet_analytics_enabled || event.forced) {
       window.mixpanel.track(event.name, event.data)
-    });
+    }
+  });
+}
+
+function initializeMixpanelWhenLoaded() {
+  if (typeof window.mixpanel === "object") {
+    exportMixpanel(MailPoet);
+    trackCachedEvents();
+  } else {
+    setTimeout(initializeMixpanelWhenLoaded, 100);
   }
 }
+
+function cacheEvent(forced, name, data) {
+  eventsCache.push({
+    name: name,
+    data: data,
+    forced: forced,
+  });
+}
+
+define(
+  ['mailpoet', 'underscore'],
+  function(MailPoet, _) {
+
+    MailPoet.trackEvent = _.partial(cacheEvent, false);
+    MailPoet.forceTrackEvent = _.partial(cacheEvent, true);
+
+    initializeMixpanelWhenLoaded();
+  }
+);
