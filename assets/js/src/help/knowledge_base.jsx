a7504136a2ceb9dc0770fd074e56d2874674ed8e
Pavel Dohnal <pavel@mailpoet.com>
2017-06-28 15:36:51 +0100

    Add knowledge base page content
    [MAILPOET-949]

--- assets/js/src/help/knowledge_base.jsx
+++ assets/js/src/help/knowledge_base.jsx
@@ -13,7 +13,18 @@ function KnowledgeBase() {
 
       <Tabs tab="knowledgeBase" />
 
-      <div>asdfasdf</div>
+      <p>{MailPoet.I18n.t('knowledgeBaseIntro')}</p>
+      <ul>
+        <li><a target="_blank" href="http://beta.docs.mailpoet.com/category/116-common-problems">Common Problems</a></li>
+        <li><a target="_blank" href="http://beta.docs.mailpoet.com/category/165-newsletters">Newsletters</a></li>
+        <li><a target="_blank" href="http://beta.docs.mailpoet.com/category/156-migration-questions">Migration Questions</a></li>
+        <li><a target="_blank" href="http://beta.docs.mailpoet.com/category/149-sending-methods">Sending Methods</a></li>
+        <li><a target="_blank" href="http://beta.docs.mailpoet.com/category/139-subscription-forms">Subscription Forms</a></li>
+        <li><a target="_blank" href="http://beta.docs.mailpoet.com/category/114-getting-started">Getting Started</a></li>
+        <li><a target="_blank" href="http://beta.docs.mailpoet.com/category/123-newsletter-designer">Newsletter Designer</a></li>
+        <li><a target="_blank" href="http://beta.docs.mailpoet.com/category/121-subscribers-and-lists">Subscribers and Lists</a></li>
+      </ul>
+      <a href="http://beta.docs.mailpoet.com/" className="button button-primary">{MailPoet.I18n.t('knowledgeBaseButton')}</a>
     </div>
   );
 };
