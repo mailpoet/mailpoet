fbe5c39b157b096b3fafc2f977d8657caac4edd3
Tautvidas Sipavičius <t.9b5as@gmail.com>
2017-07-04 16:02:22 +0300

    Switch to using CircleCI 2.0
    We end up using Docker images for PHP 5.6 and MySQL, handle our own
    Composer and NPM caches.

--- .circle_ci/fake-sendmail.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-path = "/tmp"
-Dir.mkdir(path) if !File.exists?(path)
-File.open("#{path}/mailpoet-#{Time.now.to_f}.txt", "w") do |f|
-    sleep 5
-    f.puts ARGV.inspect
-    $stdin.each_line { |line| f.puts line }
-end
