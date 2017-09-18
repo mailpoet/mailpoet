d831b2df5505603e8ac97f17665ffa82c5a36d25
Vlad <site.github@mrcasual.com>
2017-09-18 10:38:57 -0400

    Explicitly sets up hooks instead of assuming they are already set up

--- tests/unit/Config/HooksTest.php
+++ tests/unit/Config/HooksTest.php
@@ -1,8 +1,12 @@
 <?php
 namespace MailPoet\Test\Config;
 
+use MailPoet\Config\Hooks;
+
 class HooksTest extends \MailPoetTest {
   function testItHooksSchedulerToMultiplePostTypes() {
+    $hooks = new Hooks();
+    $hooks->setupPostNotifications();
     $post_types = get_post_types();
     foreach($post_types as $post_type) {
       expect(has_filter('publish_' . $post_type, '\MailPoet\Newsletter\Scheduler\Scheduler::schedulePostNotification'))->notEmpty();
