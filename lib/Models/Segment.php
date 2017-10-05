b7555aa640b775296b6713a2599857ffc0dac63c
Pavel Dohnal <pavel@mailpoet.com>
2017-10-05 10:42:30 +0100

    Show only default segments in listings
    [PREMIUM-38]

--- lib/Models/Segment.php
+++ lib/Models/Segment.php
@@ -152,6 +152,7 @@ class Segment extends Model {
 
   static function getSegmentsWithSubscriberCount($type = self::TYPE_DEFAULT) {
     $query = self::selectMany(array(self::$_table.'.id', self::$_table.'.name'))
+      ->whereIn('type', array(Segment::TYPE_WP_USERS, Segment::TYPE_DEFAULT))
       ->selectExpr(
         self::$_table.'.*, ' .
         'COUNT(IF('.
@@ -207,6 +208,13 @@ class Segment extends Model {
       'HAVING subscribers) ' .
       'ORDER BY name'
     )->findArray();
+  }
+
+  static function listingQuery(array $data = array()) {
+    $query = self::select('*');
+    $query->whereIn('type', array(Segment::TYPE_WP_USERS, Segment::TYPE_DEFAULT));
+    return $query
+      ->filter('groupBy', $data);
   }
 
   static function createOrUpdate($data = array()) {
