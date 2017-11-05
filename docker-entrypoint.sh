46c42c1bb4d2516dbec0448afd4cb888838028f1
Vlad <site.github@mrcasual.com>
2017-11-05 13:22:02 -0500

    Re-adds vendor folder installation (this is needed for Alex's setup)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -28,12 +28,29 @@ if ! $(wp-su core is-installed); then
     echo "Configuring WordPress"
     # The development version of Gravity Flow requires SCRIPT_DEBUG
     wp-su core config --dbhost=mysql --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --extra-php="define( 'SCRIPT_DEBUG', true );" --force
+fi
 
+# Load Composer dependencies
+# Set KEEP_DEPS environment flag to not redownload them on each run, only for the 1st time, useful for development.
+# Example: docker-compose run -e KEEP_DEPS=1 codeception ...
+# Don't forget to restore your original /vendor folder from /vendor_backup manually or by running acceptance tests without this flag.
+if [[ -z "${KEEP_DEPS}" ]]; then
+  rm -rf /project/vendor_backup
+fi
+if [ ! -d "/project/vendor_backup" ]; then
+  mv /project/vendor /project/vendor_backup
+  cd /project
+  php composer.phar install
 fi
 
 cd /wp-core/wp-content/plugins/mailpoet
 
 /project/vendor/bin/codecept run acceptance -c codeception.acceptance.yml $@
 exitcode=$?
+
+if [[ -z "${KEEP_DEPS}" ]]; then
+  rm -rf /project/vendor
+  mv /project/vendor_backup /project/vendor
+fi
 
 exit $exitcode
