83d84e67d6c02485bc83b5bd7c89a28d7b354e05
Vlad <site.github@mrcasual.com>
2017-11-05 19:55:16 -0500

    Adds test for iframe form subscription

--- tests/acceptance/SubscriptionFormCest.php
+++ tests/acceptance/SubscriptionFormCest.php
@@ -4,10 +4,12 @@ namespace MailPoet\Test\Acceptance;
 
 class SubscriptionFormCest {
   function _before(\AcceptanceTester $I) {
-    $I->cli('widget add mailpoet_form sidebar-1 2 --form=1 --title="Subscribe to Our Newsletter" --allow-root');
+    // prevent throttling from consecutive subscriptions
+    $I->loginAsAdmin();
   }
 
   function subscriptionFormWidget(\AcceptanceTester $I) {
+    $I->cli('widget add mailpoet_form sidebar-1 2 --form=1 --title="Subscribe to Our Newsletter" --allow-root');
     $I->wantTo('Subscribe using form widget');
 
     $I->amOnPage('/');
@@ -15,9 +17,19 @@ class SubscriptionFormCest {
     $I->click('.mailpoet_submit');
     $I->waitForText('Check your inbox or spam folder to confirm your subscription.', 3, '.mailpoet_validate_success');
     $I->seeNoJSErrors();
+    $I->cli('widget reset sidebar-1 --allow-root');
   }
 
-  function _after(\AcceptanceTester $I) {
-    $I->cli('widget reset sidebar-1 --allow-root');
+  function subscriptionFormIframe(\AcceptanceTester $I) {
+    $I->wantTo('Subscribe using iframe form');
+
+    $I->amOnPage('/form-test');
+    $I->seeElement('iframe.mailpoet_form_iframe');
+    $I->executeJS('jQuery(".mailpoet_form_iframe").attr("id", "iframe_form")');
+    $I->switchToIframe('iframe_form');
+    $I->fillField('input[title="Email"]', 'test-email@example.com');
+    $I->click('.mailpoet_submit');
+    $I->waitForText('Check your inbox or spam folder to confirm your subscription.', 3, '.mailpoet_validate_success');
+    $I->seeNoJSErrors();
   }
 }
