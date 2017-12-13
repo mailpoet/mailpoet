5808e7ce1e36a0080564b9b9d57f017c67b78c2b
Vlad <site.github@mrcasual.com>
2017-12-12 21:32:30 -0500

    Tests statistics column

--- tests/acceptance/NewslettersListingCest.php
+++ tests/acceptance/NewslettersListingCest.php
@@ -18,4 +18,22 @@ class NewslettersListingCest {
     $I->click('Post Notifications', '[data-automation-id="newsletters_listing_tabs"]');
     $I->waitForText('Post notification', 5, '[data-automation-id="listing_item_3"]');
   }
+
+  function statisticsColumn(\AcceptanceTester $I) {
+    $I->wantTo('Check if statistics column is visible depending on tracking option');
+
+    $I->loginAsAdmin();
+
+    // column is hidden when tracking is not enabled
+    $I->cli('db query "UPDATE mp_mailpoet_settings SET value = null WHERE name = \'tracking\'" --allow-root');
+    $I->amOnMailpoetPage('Emails');
+    $I->waitForText('Subject', 5);
+    $I->dontSee('Opened, Clicked');
+
+    // column is visible when tracking is enabled
+    $I->cli('db query "UPDATE mp_mailpoet_settings SET value = \'a:1:{s:7:\"enabled\";s:1:\"1\";}\' WHERE name = \'tracking\'" --allow-root');
+    $I->reloadPage();
+    $I->waitForText('Subject', 5);
+    $I->see('Opened, Clicked');
+  }
 }
