f658fd7776b80383c7f580e626fe585375bfd5e6
Tautvidas Sipavičius <t.9b5as@gmail.com>
2018-01-16 16:38:06 +0200

    Release 3.3.5

--- mailpoet.php
+++ mailpoet.php
@@ -4,7 +4,7 @@ if(!defined('ABSPATH')) exit;
 
 /*
  * Plugin Name: MailPoet 3 (New)
- * Version: 3.3.4
+ * Version: 3.3.5
  * Plugin URI: http://www.mailpoet.com
  * Description: Create and send newsletters, post notifications and welcome emails from your WordPress.
  * Author: MailPoet
@@ -20,7 +20,7 @@ if(!defined('ABSPATH')) exit;
  */
 
 $mailpoet_plugin = array(
-  'version' => '3.3.4',
+  'version' => '3.3.5',
   'filename' => __FILE__,
   'path' => dirname(__FILE__),
   'autoloader' => dirname(__FILE__) . '/vendor/autoload.php',
