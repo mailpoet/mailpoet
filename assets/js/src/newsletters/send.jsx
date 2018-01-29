43fbbca0507026b3c12622e0410cc9dc4a5db4a8
Amine Ben hammou <amine@mailpoet.com>
2018-01-29 15:58:18 +0000

    moving width to css

--- assets/js/src/newsletters/send.jsx
+++ assets/js/src/newsletters/send.jsx
@@ -92,30 +92,27 @@ define(
       },
       saveTemplate: function (response, done) {
         const iframe = document.createElement('iframe');
-        iframe.width = 660;
         iframe.src = response.meta.preview_url;
         iframe.onload = () => {
-          setTimeout(() => {
-            html2canvas(iframe.contentDocument.documentElement).then((thumbnail) => {
-              document.body.removeChild(iframe);
-              MailPoet.Ajax.post({
-                api_version: window.mailpoet_api_version,
-                endpoint: 'newsletterTemplates',
-                action: 'save',
-                data: {
-                  newsletter_id: response.data.id,
-                  name: response.data.subject,
-                  description: response.data.preheader,
-                  thumbnail: thumbnail.toDataURL('image/jpeg'),
-                  body: JSON.stringify(response.data.body),
-                  categories: '["recent"]',
-                },
-              }).then(done).fail(this.showError);
-            });
-          }, 500);
+          html2canvas(iframe.contentDocument.documentElement).then((thumbnail) => {
+            document.body.removeChild(iframe);
+            MailPoet.Ajax.post({
+              api_version: window.mailpoet_api_version,
+              endpoint: 'newsletterTemplates',
+              action: 'save',
+              data: {
+                newsletter_id: response.data.id,
+                name: response.data.subject,
+                description: response.data.preheader,
+                thumbnail: thumbnail.toDataURL('image/jpeg'),
+                body: JSON.stringify(response.data.body),
+                categories: '["recent"]',
+              },
+            }).then(done).fail(this.showError);
+          });
         };
         // just to hide the iframe
-        iframe.style.cssText ='position: absolute; opacity:0; z-index: -9999';
+        iframe.className ='mailpoet_template_iframe';
         document.body.appendChild(iframe);
       },
       handleSend: function (e) {
