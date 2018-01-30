17a25120d4d4e3a7eb97e8c262c676632ed4ced8
Pavel Dohnal <pavel@mailpoet.com>
2018-01-30 10:16:12 +0000

    Add conflict HTTP code
    [PREMIUM-42]

--- lib/API/JSON/Response.php
+++ lib/API/JSON/Response.php
@@ -9,6 +9,7 @@ abstract class Response {
   const STATUS_UNAUTHORIZED = 401;
   const STATUS_FORBIDDEN = 403;
   const STATUS_NOT_FOUND = 404;
+  const STATUS_CONFLICT = 409;
   const STATUS_UNKNOWN = 500;
 
   public $status;
