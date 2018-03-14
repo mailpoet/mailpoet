1f072757882c5dbcb2bf6a6f02106a7a4f90bc77
Vlad <site.github@mrcasual.com>
2018-03-14 19:35:02 -0400

    Sends welcome email by default when adding subscribing to list(s)

--- tests/unit/API/MP/APITest.php
+++ tests/unit/API/MP/APITest.php
@@ -174,6 +174,45 @@ class APITest extends \MailPoetTest {
     expect($result['subscriptions'][0]['segment_id'])->equals($segment->id);
   }
 
+  function testItSchedulesWelcomeNotificationByDefaultAfterSubscriberSubscriberToLists() {
+    $API = Stub::makeEmptyExcept(
+      new \MailPoet\API\MP\v1\API(),
+      'subscribeToLists',
+      array(
+        '_scheduleWelcomeNotification' => Stub::once()
+      ), $this);
+    $subscriber = Subscriber::create();
+    $subscriber->hydrate(Fixtures::get('subscriber_template'));
+    $subscriber->save();
+    $segment = Segment::createOrUpdate(
+      array(
+        'name' => 'Default',
+        'type' => Segment::TYPE_DEFAULT
+      )
+    );
+    $API->subscribeToLists($subscriber->id, array($segment->id));
+  }
+
+  function testItDoesNotScheduleWelcomeNotificationAfterSubscribingSubscriberToListsWhenDisabledByOption() {
+    $API = Stub::makeEmptyExcept(
+      new \MailPoet\API\MP\v1\API(),
+      'subscribeToLists',
+      array(
+        '_scheduleWelcomeNotification' => Stub::never()
+      ), $this);
+    $subscriber = Subscriber::create();
+    $subscriber->hydrate(Fixtures::get('subscriber_template'));
+    $subscriber->save();
+    $segment = Segment::createOrUpdate(
+      array(
+        'name' => 'Default',
+        'type' => Segment::TYPE_DEFAULT
+      )
+    );
+    $options = array('schedule_welcome_email' => false);
+    $API->subscribeToLists($subscriber->id, array($segment->id), $options);
+  }
+
   function testItGetsSegments() {
     $segment = Segment::createOrUpdate(
       array(
