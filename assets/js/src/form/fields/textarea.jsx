d638273f477b5a8c82d088cb6fe82c1dd9fe64dc
Amine Ben hammou <amine@mailpoet.com>
2018-04-18 15:59:24 +0200

    ES6 assets/js/src/form/fields/textarea.jsx

--- assets/js/src/form/fields/textarea.jsx
+++ assets/js/src/form/fields/textarea.jsx
@@ -1,26 +1,17 @@
-define([
-  'react',
-],
-(
-  React
-) => {
-  const FormFieldTextarea = React.createClass({
-    render: function render() {
-      return (
-        <textarea
-          type="text"
-          className="regular-text"
-          name={this.props.field.name}
-          id={`field_${this.props.field.name}`}
-          value={this.props.item[this.props.field.name]}
-          placeholder={this.props.field.placeholder}
-          defaultValue={this.props.field.defaultValue}
-          onChange={this.props.onValueChange}
-          {...this.props.field.validation}
-        />
-      );
-    },
-  });
+import React from 'react';
 
-  return FormFieldTextarea;
-});
+const FormFieldTextarea = () => (
+  <textarea
+    type="text"
+    className="regular-text"
+    name={this.props.field.name}
+    id={`field_${this.props.field.name}`}
+    value={this.props.item[this.props.field.name]}
+    placeholder={this.props.field.placeholder}
+    defaultValue={this.props.field.defaultValue}
+    onChange={this.props.onValueChange}
+    {...this.props.field.validation}
+  />
+);
+
+export default FormFieldTextarea;
