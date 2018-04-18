861d4ea9e1205da57481728732cbde9852ecde25
Pavel Dohnal <pavel@mailpoet.com>
2018-04-18 17:15:52 +0100

    Fix eslint rules
    [MAILPOET-1348]

--- assets/js/src/newsletters/breadcrumb.jsx
+++ assets/js/src/newsletters/breadcrumb.jsx
@@ -1,76 +1,65 @@
-define(
-  [
-    'react',
-    'react-router',
-    'classnames',
-    'mailpoet',
-  ],
-  (
-    React,
-    Router,
-    classNames,
-    MailPoet
-  ) => {
-    const Link = Router.Link;
+import React from 'react';
+import classNames from 'classnames';
+import { Link } from 'react-router';
+import MailPoet from 'mailpoet';
 
-    const Breadcrumb = React.createClass({
-      getInitialState: function getInitialState() {
-        const steps = this.props.steps || [
-          {
-            name: 'type',
-            label: MailPoet.I18n.t('selectType'),
-            link: '/new',
-          },
-          {
-            name: 'template',
-            label: MailPoet.I18n.t('template'),
-          },
-          {
-            name: 'editor',
-            label: MailPoet.I18n.t('designer'),
-          },
-          {
-            name: 'send',
-            label: MailPoet.I18n.t('send'),
-          },
-        ];
-        return {
-          step: null,
-          steps,
-        };
+const Breadcrumb = React.createClass({
+  getInitialState: function getInitialState() {
+    const steps = this.props.steps || [
+      {
+        name: 'type',
+        label: MailPoet.I18n.t('selectType'),
+        link: '/new',
       },
-      render: function render() {
-        const steps = this.state.steps.map((step, index) => {
-          const stepClasses = classNames(
-            { mailpoet_current: (this.props.step === step.name) }
-          );
+      {
+        name: 'template',
+        label: MailPoet.I18n.t('template'),
+      },
+      {
+        name: 'editor',
+        label: MailPoet.I18n.t('designer'),
+      },
+      {
+        name: 'send',
+        label: MailPoet.I18n.t('send'),
+      },
+    ];
+    return {
+      step: null,
+      steps,
+    };
+  },
+  render: function render() {
+    const steps = this.state.steps.map((step, index) => {
+      const stepClasses = classNames(
+        { mailpoet_current: (this.props.step === step.name) }
+      );
 
-          let label = step.label;
+      let label = step.label;
 
-          if (step.link !== undefined && this.props.step !== step.name) {
-            label = (
-              <Link to={step.link}>{ step.label }</Link>
-            );
-          }
-
-          return (
-            <span key={`step-${index}`}>
-              <span className={stepClasses}>
-                { label }
-              </span>
-              { (index < (this.state.steps.length - 1)) ? ' > ' : '' }
-            </span>
-          );
-        });
-
-        return (
-          <p className="mailpoet_breadcrumb">
-            { steps }
-          </p>
+      if (step.link !== undefined && this.props.step !== step.name) {
+        label = (
+          <Link to={step.link}>{ step.label }</Link>
         );
-      },
+      }
+
+      return (
+        <span key={`step-${step.label}`}>
+          <span className={stepClasses}>
+            { label }
+          </span>
+          { (index < (this.state.steps.length - 1)) ? ' > ' : '' }
+        </span>
+      );
     });
 
-    return Breadcrumb;
-  }
-);
+    return (
+      <p className="mailpoet_breadcrumb">
+        { steps }
+      </p>
+    );
+  },
+});
+
+module.exports = Breadcrumb;
+
