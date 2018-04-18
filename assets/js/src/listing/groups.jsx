1b98604c90d1ae3471e7f5f8a81d88ef2c0e3a75
Amine Ben hammou <amine@mailpoet.com>
2018-04-18 18:34:12 +0200

    ESLint rules

--- assets/js/src/listing/groups.jsx
+++ assets/js/src/listing/groups.jsx
@@ -1,41 +1,60 @@
-define(['react', 'classnames'], (React, classNames) => {
-  const ListingGroups = React.createClass({
-    handleSelect: function handleSelect(group) {
-      return this.props.onSelectGroup(group);
-    },
-    render: function render() {
-      const groups = this.props.groups.map((group, index) => {
-        if (group.name === 'trash' && group.count === 0) {
-          return false;
-        }
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-        const classes = classNames(
-            { current: (group.name === this.props.group) }
-          );
+class ListingGroups extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSelect = this.handleSelect.bind(this);
+  }
 
-        return (
-          <li key={index}>
-            {(index > 0) ? ' |' : ''}
-            <a
-              href="javascript:;"
-              className={classes}
-              onClick={this.handleSelect.bind(this, group.name)}
-            >
-              {group.label}
-              <span className="count">({ parseInt(group.count, 10).toLocaleString() })</span>
-            </a>
-          </li>
+  handleSelect(group) {
+    return this.props.onSelectGroup(group);
+  }
+
+  render() {
+    const groups = this.props.groups.map((group, index) => {
+      if (group.name === 'trash' && group.count === 0) {
+        return false;
+      }
+
+      const classes = classNames(
+          { current: (group.name === this.props.group) }
         );
-      });
 
       return (
-        <ul className="subsubsub">
-          { groups }
-        </ul>
+        <li key={group.name}>
+          {(index > 0) ? ' |' : ''}
+          <a
+            href="javascript:;"
+            className={classes}
+            onClick={() => this.handleSelect(group.name)}
+          >
+            {group.label}
+            <span className="count">({ parseInt(group.count, 10).toLocaleString() })</span>
+          </a>
+        </li>
       );
-    },
-  });
+    });
 
-  return ListingGroups;
+    return (
+      <ul className="subsubsub">
+        { groups }
+      </ul>
+    );
+  }
 }
-);
+
+ListingGroups.propTypes = {
+  onSelectGroup: PropTypes.func.isRequired,
+  groups: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    count: PropTypes.number,
+  })).isRequired,
+  group: PropTypes.shape({
+    name: PropTypes.string,
+    count: PropTypes.number,
+  }).isRequired,
+};
+
+export default ListingGroups;
