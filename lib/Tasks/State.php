7186f4b13ae5990606be8ea7873844a9777151f7
Rostislav Wolny <rosta@mailpoet.com>
2018-07-26 13:05:20 +0200

    Add fetching of latest sending tasks for help page
    [MAILPOET-1460]

--- lib/Tasks/State.php
+++ lib/Tasks/State.php
@@ -2,8 +2,11 @@
 
 namespace MailPoet\Tasks;
 
-
+use Carbon\Carbon;
+use MailPoet\Cron\Workers\Scheduler;
 use MailPoet\Models\ScheduledTask;
+use MailPoet\Models\SendingQueue;
+use MailPoet\Newsletter\Url as NewsletterUrl;
 
 class State
 {
@@ -31,5 +34,65 @@ class State
       $stats[$count->status] = (int)$count->value;
     }
     return $stats;
+  }
+
+  /**
+   * @return array
+   */
+  function getLatestTasks(
+    $type = null,
+    $statuses = [
+      ScheduledTask::STATUS_COMPLETED,
+      ScheduledTask::STATUS_SCHEDULED,
+      ScheduledTask::VIRTUAL_STATUS_RUNNING,
+    ],
+    $limit = Scheduler::TASK_BATCH_SIZE) {
+    $tasks = [];
+    foreach($statuses as $status) {
+      $query = ScheduledTask::orderByDesc('created_at')
+        ->whereNull('deleted_at')
+        ->limit($limit);
+      if($type) {
+        $query = $query->where('type', $type);
+      }
+      if($status === ScheduledTask::VIRTUAL_STATUS_RUNNING) {
+        $query = $query->whereNull('status');
+      } else {
+        $query = $query->where('status', $status);
+      }
+      $tasks = array_merge($tasks, $query->findMany());
+    }
+
+    return array_map([$this, 'buildTaskData'], $tasks);
+  }
+
+  /**
+   * @return array
+   */
+  private function buildTaskData(ScheduledTask $task) {
+    $queue = $newsletter = null;
+    if($task->type === Sending::TASK_TYPE) {
+      $queue = SendingQueue::where('task_id', $task->id)->findOne();
+      $newsletter = $queue ? $queue->newsletter()->findOne() : null;
+    }
+    return [
+      'id' => (int)$task->id,
+      'type' => $task->type,
+      'priority' => (int)$task->priority,
+      'updated_at' => Carbon::createFromTimeString($task->updated_at)->timestamp,
+      'scheduled_at' => $task->scheduled_at ? Carbon::createFromTimeString($task->scheduled_at)->timestamp : null,
+      'status' => $task->status,
+      'newsletter' => $queue && $newsletter ? [
+        'newsletter_id' => (int)$queue->newsletter_id,
+        'queue_id' => (int)$queue->id,
+        'subject' => $queue->newsletter_rendered_subject ?: $newsletter->subject,
+        'preview_url' => NewsletterUrl::getViewInBrowserUrl(
+          NewsletterUrl::TYPE_LISTING_EDITOR,
+          $newsletter,
+          null,
+          $queue
+        ),
+      ] : null,
+    ];
   }
 }
