1f88e56f1ed706e2942b57448d226cbb5fe5e24f
Pavel Dohnal <pavel@mailpoet.com>
2018-08-14 15:55:06 +0200

    Skip video if already seen
    [MAILPOET-1447]

--- assets/js/src/newsletter_editor/initializer.jsx
+++ assets/js/src/newsletter_editor/initializer.jsx
@@ -18,15 +18,18 @@ const renderBreadcrumb = (newsletterType) => {
 
 function displayTutorial() {
   const key = `user_seen_editor_tutorial${window.config.currentUserId}`;
+  if (window.config.dragDemoUrlSettings) {
+    return;
+  }
   MailPoet.Modal.popup({
     title: MailPoet.I18n.t('tutorialVideoTitle'),
-    template: '<video style="height:640px;" src=' + window.config.dragDemoUrl + ' controls autoplay></video>',
+    template: `<video style="height:640px;" src="${window.config.dragDemoUrl}" controls autoplay></video>`,
     onCancel: () => {
       MailPoet.Ajax.post({
         api_version: window.mailpoet_api_version,
         endpoint: 'settings',
         action: 'set',
-        data: {[key]: 1},
+        data: { [key]: 1 },
       });
     },
   });
