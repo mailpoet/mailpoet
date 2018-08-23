c68f58a7c0f09801ec9c9073ab3226b6b7560cad
Tautvidas Sipavičius <t.9b5as@gmail.com>
2018-08-23 15:06:21 +0300

    Display PHP version warning via notice filter instead of stdout

--- lib/WP/Notice.php
+++ lib/WP/Notice.php
@@ -11,41 +11,44 @@ class Notice {
   private $type;
   private $message;
 
-  function __construct($type, $message) {
+  function __construct($type, $message, $classes = '', $transient = '') {
     $this->type = $type;
     $this->message = $message;
+    $this->classes = $classes;
+    $this->transient = $transient;
   }
 
-  static function displayError($message) {
+  static function displayError($message, $classes = '', $transient = '') {
     $message = sprintf(
       "<b>%s </b> %s",
       __('MailPoet Error:', 'mailpoet'),
       $message
     );
-    self::createNotice(self::TYPE_ERROR, $message);
+    self::createNotice(self::TYPE_ERROR, $message, $classes, $transient);
   }
 
-  static function displayWarning($message) {
-    self::createNotice(self::TYPE_WARNING, $message);
+  static function displayWarning($message, $classes = '', $transient = '') {
+    self::createNotice(self::TYPE_WARNING, $message, $classes, $transient);
   }
 
-  static function displaySuccess($message) {
-    self::createNotice(self::TYPE_SUCCESS, $message);
+  static function displaySuccess($message, $classes = '', $transient = '') {
+    self::createNotice(self::TYPE_SUCCESS, $message, $classes, $transient);
   }
 
-  static function displayInfo($message) {
-    self::createNotice(self::TYPE_INFO, $message);
+  static function displayInfo($message, $classes = '', $transient = '') {
+    self::createNotice(self::TYPE_INFO, $message, $classes, $transient);
   }
 
-  protected static function createNotice($type, $message) {
-    $notice = new Notice($type, $message);
+  protected static function createNotice($type, $message, $classes, $transient) {
+    $notice = new Notice($type, $message, $classes, $transient);
     add_action('admin_notices', array($notice, 'displayWPNotice'));
   }
 
   function displayWPNotice() {
-    $class = sprintf('notice notice-%s mailpoet_notice_server', $this->type);
+    $class = sprintf('notice notice-%s mailpoet_notice_server %s', $this->type, $this->classes);
     $message = nl2br($this->message);
+    $transient = !empty($this->transient) ? sprintf('data-notice="%s"', $this->transient) : '';
 
-    printf('<div class="%1$s"><p>%2$s</p></div>', $class, $message);
+    printf('<div class="%1$s" %3$s><p>%2$s</p></div>', $class, $message, $transient);
   }
 }
