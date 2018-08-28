60ce501c20873d1d7a0c4ed22e0a8825771271d9
Pavel Dohnal <pavel@mailpoet.com>
2018-08-28 10:42:44 +0200

    Purge old logs
    [MAILPOET-570]

--- lib/Logging/LogHandler.php
+++ lib/Logging/LogHandler.php
@@ -2,11 +2,22 @@
 
 namespace MailPoet\Logging;
 
+use Carbon\Carbon;
 use MailPoet\Dependencies\Monolog\Handler\AbstractProcessingHandler;
 use MailPoet\Models\Log;
 
 class LogHandler extends AbstractProcessingHandler {
 
+  /**
+   * Percentage value, what is the probability of running purge routine
+   * @var int
+   */
+  CONST LOG_PURGE_PROBABILITY = 5;
+
+  /**
+   * Logs older than this many days will be deleted
+   */
+  CONST DAYS_TO_KEEP_LOGS = 30;
 
   protected function write(array $record) {
     $model = Log::create();
@@ -17,6 +28,15 @@ class LogHandler extends AbstractProcessingHandler {
       'created_at' => $record['datetime']->format('Y-m-d H:i:s'),
     ]);
     $model->save();
+
+    if(rand(0, 100) <= self::LOG_PURGE_PROBABILITY) {
+      $this->purgeOldLogs();
+    }
+  }
+
+  private function purgeOldLogs() {
+    Log::whereLt('created_at', Carbon::create()->subDays(self::DAYS_TO_KEEP_LOGS)->toDateTimeString())
+       ->deleteMany();
   }
 
 }
