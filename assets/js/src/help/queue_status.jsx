1e5ad9d5cda7f15c6f391ed4d44b1cd66ae1b38b
Rostislav Wolny <rosta@mailpoet.com>
2018-09-03 16:58:46 +0200

    Refactor key value table to accept data content as a property
    [MAILPOET-1517]

--- assets/js/src/help/queue_status.jsx
+++ assets/js/src/help/queue_status.jsx
@@ -9,40 +9,42 @@ const QueueStatus = (props) => {
   return (
     <div>
       <h2>{MailPoet.I18n.t('systemStatusQueueTitle')}</h2>
-      <KeyValueTable max_width={'400px'}>{
-        [{
-          key: MailPoet.I18n.t('status'),
-          value: status.status === 'paused' ? MailPoet.I18n.t('paused') : MailPoet.I18n.t('running'),
-        }, {
-          key: MailPoet.I18n.t('startedAt'),
-          value: status.started ? MailPoet.Date.full(status.started * 1000) : MailPoet.I18n.t('unknown'),
-        }, {
-          key: MailPoet.I18n.t('sentEmails'),
-          value: status.sent || 0,
-        }, {
-          key: MailPoet.I18n.t('retryAttempt'),
-          value: status.retry_attempt || MailPoet.I18n.t('none'),
-        }, {
-          key: MailPoet.I18n.t('retryAt'),
-          value: status.retry_at ? MailPoet.Date.full(status.retry_at * 1000) : MailPoet.I18n.t('none'),
-        }, {
-          key: MailPoet.I18n.t('error'),
-          value: status.error || MailPoet.I18n.t('none'),
-        }, {
-          key: MailPoet.I18n.t('totalCompletedTasks'),
-          value: status.tasksStatusCounts.completed,
-        }, {
-          key: MailPoet.I18n.t('totalRunningTasks'),
-          value: status.tasksStatusCounts.running,
-        }, {
-          key: MailPoet.I18n.t('totalPausedTasks'),
-          value: status.tasksStatusCounts.paused,
-        }, {
-          key: MailPoet.I18n.t('totalScheduledTasks'),
-          value: status.tasksStatusCounts.scheduled,
-        }]}
-      </KeyValueTable>
-
+      <KeyValueTable
+        max_width={'400px'}
+        rows={[
+          {
+            key: MailPoet.I18n.t('status'),
+            value: status.status === 'paused' ? MailPoet.I18n.t('paused') : MailPoet.I18n.t('running'),
+          }, {
+            key: MailPoet.I18n.t('startedAt'),
+            value: status.started ? MailPoet.Date.full(status.started * 1000) : MailPoet.I18n.t('unknown'),
+          }, {
+            key: MailPoet.I18n.t('sentEmails'),
+            value: status.sent || 0,
+          }, {
+            key: MailPoet.I18n.t('retryAttempt'),
+            value: status.retry_attempt || MailPoet.I18n.t('none'),
+          }, {
+            key: MailPoet.I18n.t('retryAt'),
+            value: status.retry_at ? MailPoet.Date.full(status.retry_at * 1000) : MailPoet.I18n.t('none'),
+          }, {
+            key: MailPoet.I18n.t('error'),
+            value: status.error || MailPoet.I18n.t('none'),
+          }, {
+            key: MailPoet.I18n.t('totalCompletedTasks'),
+            value: status.tasksStatusCounts.completed,
+          }, {
+            key: MailPoet.I18n.t('totalRunningTasks'),
+            value: status.tasksStatusCounts.running,
+          }, {
+            key: MailPoet.I18n.t('totalPausedTasks'),
+            value: status.tasksStatusCounts.paused,
+          }, {
+            key: MailPoet.I18n.t('totalScheduledTasks'),
+            value: status.tasksStatusCounts.scheduled,
+          },
+        ]}
+      />
       <h4>{MailPoet.I18n.t('scheduledTasks')}</h4>
       <TasksList show_scheduled_at tasks={status.latestTasks.filter(task => (task.status === 'scheduled'))} />
 
