24ba6b8d796ca6ba17a68a493af44ed54d98f7c8
Michelle Shull <mashull@yahoo.com>
2018-09-09 13:30:24 -0400

    Preview newsletteer in browser from editor [MQ-42]

--- tests/acceptance/BrowserPreviewNewsletterCest.php
+++ tests/acceptance/BrowserPreviewNewsletterCest.php
@@ -10,17 +10,16 @@ class BrowserPreviewNewsletterCest {
     $I->wantTo('Preview a newsletter in the browser from the newsletter editor page');
     $newsletter_title = 'Preview Newsletter';
 
-    // step 1 - Prepare post notification data
     $newsletterFactory = new Newsletter();
     $newsletter = $newsletterFactory->withSubject($newsletter_title)
       ->withType('standard')
       ->create();
     
-    // step 2 - Go to editor
     $I->login();
     $I->amEditingNewsletter($newsletter->id);
     $I->click(['css' => '.mailpoet_region.mailpoet_preview_region']);
-    $I->click(['View in browser']);
+    $I->wait(5);
+    $I->click('View in browser');
     $I->wait (15);
     $I->waitForText('Newsletter Preview');
     }
