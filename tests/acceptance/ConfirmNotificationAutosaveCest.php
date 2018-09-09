9105c8734d404cb6621f129c73d943fc06bf5610
Michelle Shull <mashull@yahoo.com>
2018-09-09 14:04:13 -0400

    Confirm Notification Autosave Test [MQ-34]

---
+++ tests/acceptance/ConfirmNotificationAutosaveCest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MailPoet\Test\Acceptance;
+
+use MailPoet\Test\DataFactories\Newsletter;
+
+require_once __DIR__ . '/../DataFactories/Newsletter.php';
+
+class ConfirmNotificationAutosaveCest {
+  function confirmNotificationAutosave(\AcceptanceTester $I) {
+    $I->wantTo('Confirm Post Notification Autosave');
+
+    $newsletter_title = 'Notification Autosave Test';
+
+
+    // step 1 - Prepare post notification data
+    $newsletterFactory = new Newsletter();
+    $newsletter = $newsletterFactory->withSubject($newsletter_title)
+      ->withType('notification')
+      ->withPostNoticationOptions()
+      ->create();
+
+    // step 2 - Go to editor
+    $I->login();
+    $I->amEditingNewsletter($newsletter->id);
+  
+    // step 3 - Add subject, wait for Autosave
+    $title_element = '[data-automation-id=\'newsletter_title\']';
+    $I->waitForElement($title_element);
+    $I->seeInCurrentUrl('mailpoet-newsletter-editor');
+    $I->fillField($title_element, $newsletter_title);
+    $I->wait(10);
+    $I->see('Autosaved');
+  }
+}
