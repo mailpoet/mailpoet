2eb68ed561fb9f7c8ebd516cf69b66e5b656d6cc
Michelle Shull <mashull@yahoo.com>
2018-09-10 11:40:36 -0400

    Fixed spacing issues [MQ-34]

--- tests/acceptance/ConfirmNotificationAutosaveCest.php
+++ tests/acceptance/ConfirmNotificationAutosaveCest.php
@@ -9,21 +9,16 @@ require_once __DIR__ . '/../DataFactories/Newsletter.php';
 class ConfirmNotificationAutosaveCest {
   function confirmNotificationAutosave(\AcceptanceTester $I) {
     $I->wantTo('Confirm Post Notification Autosave');
-
     $newsletter_title = 'Notification Autosave Test';
-
-
     // step 1 - Prepare post notification data
     $newsletterFactory = new Newsletter();
     $newsletter = $newsletterFactory->withSubject($newsletter_title)
       ->withType('notification')
       ->withPostNoticationOptions()
       ->create();
-
     // step 2 - Go to editor
     $I->login();
-    $I->amEditingNewsletter($newsletter->id);
-  
+    $I->amEditingNewsletter($newsletter->id);  
     // step 3 - Add subject, wait for Autosave
     $title_element = '[data-automation-id=\'newsletter_title\']';
     $I->waitForElement($title_element);
