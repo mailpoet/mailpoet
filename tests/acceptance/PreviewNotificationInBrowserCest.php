96c2ff2745247e2f7dc9628be0e14251dfeb9017
Michelle Shull <mashull@yahoo.com>
2018-09-10 12:39:38 -0400

    fixed formatting and syntax issues [MQ-43]

--- tests/acceptance/PreviewNotificationInBrowserCest.php
+++ tests/acceptance/PreviewNotificationInBrowserCest.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace MailPoet\Test\Acceptance;
- use MailPoet\Test\DataFactories\Newsletter;
- require_once __DIR__ . '/../DataFactories/Newsletter.php';
- class BrowserPreviewNotificationCest {
+
+use MailPoet\Test\DataFactories\Newsletter;
+
+require_once __DIR__ . '/../DataFactories/Newsletter.php';
+
+class BrowserPreviewNotificationCest {
   function previewNotificationInBrowser(\AcceptanceTester $I) {
-    
     //step one - create notifcation data
     $I->wantTo('Preview a notification in the browser from the newsletter editor page');
     $newsletter_title = 'Preview Notification';
@@ -13,15 +16,12 @@ namespace MailPoet\Test\Acceptance;
       ->withType('notification')
       ->withPostNoticationOptions()
       ->create();
-    
     //step two - and now for some fancy previewing
     $I->login();
     $I->amEditingNewsletter($newsletter->id);
     $I->click(['css' => '.mailpoet_region.mailpoet_preview_region']);
-    $I->wait(5);
     $I->click('View in browser');
-    $I->wait (10);
-    $I->waitForText('Newsletter Preview');
-    }
+    $I->waitForText('Newsletter Preview',10);
+  }
     
 }
