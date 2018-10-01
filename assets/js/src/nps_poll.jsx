da059ad4b245eeb26952f7bc7a83c157c674e2fa
Pavel Dohnal <pavel@mailpoet.com>
2018-10-01 08:36:47 +0200

    Prefix all global variables
    [MAILPOET-1539]

--- assets/js/src/nps_poll.jsx
+++ assets/js/src/nps_poll.jsx
@@ -1,11 +1,11 @@
 function displayPoll() {
-  if (window.display_nps_poll && window.satismeter) {
+  if (window.mailpoet_display_nps_poll && window.satismeter) {
     window.satismeter({
       writeKey: '6L479eVPXk7pBn6S',
-      userId: window.current_wp_user.ID + window.site_url,
+      userId: window.mailpoet_current_wp_user.ID + window.mailpoet_site_url,
       traits: {
-        name: window.current_wp_user.user_nicename,
-        email: window.current_wp_user.user_email,
+        name: window.mailpoet_current_wp_user.user_nicename,
+        email: window.mailpoet_current_wp_user.user_email,
         createdAt: window.mailpoet_settings.installed_at,
       },
     });
