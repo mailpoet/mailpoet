e8f0a0067d184bdb3dba796faad6be4b0f5c4895
Pavel Dohnal <pavel@mailpoet.com>
2018-10-02 14:53:36 +0200

    Exit with error on failure
    [MAILPOET-1538]

--- mailpoet-cron.php
+++ mailpoet-cron.php
@@ -5,13 +5,13 @@ error_reporting(E_ALL);
 
 if(!isset($argv[1]) || !$argv[1]) {
   echo 'You need to pass a WordPress root as an argument.';
-  die;
+  exit(1);
 }
 
 $wp_load_file = $argv[1] . '/wp-load.php';
 if(!file_exists($wp_load_file)) {
   echo 'WordPress root argument is not valid.';
-  die;
+  exit(1);
 }
 
 if(!defined('ABSPATH')) {
@@ -21,13 +21,13 @@ if(!defined('ABSPATH')) {
 
 if(!is_plugin_active('mailpoet/mailpoet.php')) {
   echo 'MailPoet plugin is not active';
-  die;
+  exit(1);
 }
 
 // Check for minimum supported PHP version
 if(version_compare(phpversion(), '5.5.0', '<')) {
   echo 'MailPoet requires PHP version 5.6 or newer (version 7 recommended).';
-  die;
+  exit(1);
 }
 
 $trigger = new \MailPoet\Cron\Triggers\MailPoet();
