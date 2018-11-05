91e66476d34092d509852ca31632e21252049da4
Michelle Shull <mashull@yahoo.com>
2018-11-05 12:20:52 -0500

    added data-automation-ids, fixed typo. [MQ-83][MQ-84]

--- tests/acceptance/SettingsSubscriptionPageCest.php
+++ tests/acceptance/SettingsSubscriptionPageCest.php
@@ -3,11 +3,11 @@
 namespace MailPoet\Test\Acceptance;
 
 class SettingsSubscriptionPageCest {
-  function previvewDefaultSubscriptionPage(\AcceptanceTester $I) {
+  function previewDefaultSubscriptionPage(\AcceptanceTester $I) {
     $I->wantTo('Preview default MailPoet page from MP Settings page');
     $I->login();
     $I->amOnMailPoetPage('Settings');
-    $I->click(['xpath'=>'//*[@id="mailpoet_settings_form"]/div[2]/table/tbody/tr[5]/td/p[1]/a']);
+    $I->click('[data-automation-id="preview_manage_subscription_page_link"]');
     $I->waitForText('Manage your subscription');
   }
   function createNewSubscriptionPage(\AcceptanceTester $I) {
@@ -19,7 +19,7 @@ class SettingsSubscriptionPageCest {
     $I->cli('post create --allow-root --post_type=page --post_title=' . $page_title . '--post_content=' . $page_content);
     $I->click(['css'=>'#subscription_manage_page.mailpoet_page_selection']);
     $I->checkOption('select#subscription_manage_page', $page_title);
-    $I->click(['xpath'=>'//*[@id="mailpoet_settings_form"]/div[2]/table/tbody/tr[5]/td/p[1]/a']);
+    $I->click('[data-automation-id="preview_manage_subscription_page_link"]');
     $I->waitForText('Manage your subscription');
 
   }
