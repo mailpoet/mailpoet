b54d31c797d2d14c6fa9552876714f55cde6a3aa
Michelle Shull <mashull@yahoo.com>
2018-11-05 08:38:48 -0500

    added data-automation-id for robustness [MQ-100]

--- tests/acceptance/SettingsUnsubscribePageCest.php
+++ tests/acceptance/SettingsUnsubscribePageCest.php
@@ -7,7 +7,7 @@ class SettingsUnsubscribePageCest {
     $I->wantTo('Preview default MailPoet Unsubscribe page from MP Settings page');
     $I->login();
     $I->amOnMailPoetPage('Settings');
-    $I->click(['xpath'=>'//*[@id="mailpoet_settings_form"]/div[2]/table/tbody/tr[6]/td/p/a']);
+    $I->click('[data-automation-id="unsubscribe_page_preview_link"]');
     $I->switchToNextTab();
     $I->waitForElement(['css'=>'.entry-title'], 20);
     $I->seeInCurrentUrl('&action=unsubscribe');
