cf9f38cdb4eda8c608ef42ac0eeee85c0d8b253c
Ján Mikláš <neosinner@gmail.com>
2018-12-11 21:19:52 +0100

    Fix NewsletterCreationCest - wait for select2 to render before selecting value

--- tests/acceptance/NewsletterCreationCest.php
+++ tests/acceptance/NewsletterCreationCest.php
@@ -81,6 +81,7 @@ class NewsletterCreationCest {
     $send_form_element = '[data-automation-id="newsletter_send_form"]';
     $I->waitForElement($send_form_element);
     $I->seeInCurrentUrl('mailpoet-newsletters#/send/');
+    $I->waitForElement('.select2');
     $I->selectOptionInSelect2('WordPress Users');
     $I->click('Send');
   }
