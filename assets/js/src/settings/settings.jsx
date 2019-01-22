889712af5c6ee21d7e96ffc040c5e1f2981505e0
Pavel Dohnal <pavel@mailpoet.com>
2019-01-22 14:50:08 +0100

    Not render settings if not on settings page
    [MAILPOET-1573]

--- assets/js/src/settings/settings.jsx
+++ assets/js/src/settings/settings.jsx
@@ -2,12 +2,16 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const DefaultSender = require('settings/default_sender.jsx').default;
 
-ReactDOM.render(
-  React.createElement(DefaultSender, {
-    senderAddress: window.mailpoet_settings_sender_adddress,
-    senderName: window.mailpoet_settings_sender_name,
-    replyToAddress: window.mailpoet_settings_reply_to_address,
-    replyToName: window.mailpoet_settings_reply_to_name,
-  }),
-  document.getElementById('settings_sender_container')
-);
+const container = document.getElementById('settings_sender_container');
+
+if (container) {
+  ReactDOM.render(
+    React.createElement(DefaultSender, {
+      senderAddress: window.mailpoet_settings_sender_adddress,
+      senderName: window.mailpoet_settings_sender_name,
+      replyToAddress: window.mailpoet_settings_reply_to_address,
+      replyToName: window.mailpoet_settings_reply_to_name,
+    }),
+    container
+  );
+}
