4fb4e5a37a7d92e512439a2377b2f4c46aa32aba
Pavel Dohnal <pavel@mailpoet.com>
2019-01-22 14:23:19 +0100

    Display sender warning for other newsletter types
    [MAILPOET-1573]

--- assets/js/src/newsletters/send/welcome.jsx
+++ assets/js/src/newsletters/send/welcome.jsx
@@ -1,6 +1,7 @@
 import MailPoet from 'mailpoet';
 import Hooks from 'wp-js-hooks';
 import Scheduling from 'newsletters/types/welcome/scheduling.jsx';
+import SenderField from 'newsletters/send/sender_address_field.jsx';
 
 let fields = [
   {
@@ -34,7 +35,8 @@ let fields = [
       },
       {
         name: 'sender_address',
-        type: 'text',
+        type: 'reactComponent',
+        component: SenderField,
         placeholder: MailPoet.I18n.t('senderAddressPlaceholder'),
         validation: {
           'data-parsley-required': true,
