086a8b1956eded55f95cfb79c9baaf05358d9e56
Pavel Dohnal <pavel@mailpoet.com>
2019-01-29 14:40:12 +0100

    Fix broken test

--- tests/acceptance/SubscriptionFormCest.php
+++ tests/acceptance/SubscriptionFormCest.php
@@ -24,6 +24,7 @@ class SubscriptionFormCest {
     $settings = new Settings();
     $settings
       ->withConfirmationEmailSubject()
+      ->withConfirmationEmailBody()
       ->withConfirmationEmailEnabled();
   }
 
