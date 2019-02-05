5cf75bcd60200a3447191459edda56ea1e2eb733
Pavel Dohnal <pavel@mailpoet.com>
2019-02-05 13:35:03 +0100

    Add sender warning to sign-up confirmation page
    [MAILPOET-1769]

--- assets/js/src/settings/settings.jsx
+++ assets/js/src/settings/settings.jsx
@@ -1,10 +1,11 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const DefaultSender = require('settings/default_sender.jsx').default;
+const SignupConfirmationSender = require('settings/signup_confirmation_sender.jsx').default;
 
-const container = document.getElementById('settings_sender_container');
+const settingsSenderContainer = document.getElementById('settings_sender_container');
 
-if (container) {
+if (settingsSenderContainer) {
   ReactDOM.render(
     React.createElement(DefaultSender, {
       senderAddress: window.mailpoet_settings_sender_adddress,
@@ -12,6 +13,18 @@ if (container) {
       replyToAddress: window.mailpoet_settings_reply_to_address,
       replyToName: window.mailpoet_settings_reply_to_name,
     }),
-    container
+    settingsSenderContainer
+  );
+}
+
+const settingsSignupConfirmationSenderContainer = document.getElementById('settings_signup_confirmation_sender_container');
+
+if (settingsSignupConfirmationSenderContainer) {
+  ReactDOM.render(
+    React.createElement(SignupConfirmationSender, {
+      senderAddress: window.mailpoet_settings_sender_adddress,
+      senderName: window.mailpoet_settings_sender_name,
+    }),
+    settingsSignupConfirmationSenderContainer
   );
 }
