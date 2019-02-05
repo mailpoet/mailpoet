c67e76916f440346cc53834682902cdcc41d4ae1
Michelle Shull <mashull@yahoo.com>
2019-02-05 14:38:20 -0500

    [MQ-174] Beamer acceptance test

---
+++ tests/acceptance/BeamerAcceptanceCest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MailPoet\Test\Acceptance;
+
+class BeamerAcceptanceCest {
+  function openAndCloseBeamer(\AcceptanceTester $I) {
+    $I->wantTo('Open and close Beamer sidebar');
+    $I->login();
+    $I->amOnMailPoetPage('Emails');
+    $I->click('.mailpoet_feature_announcement_icon');
+    $I->waitForElement('#beamerNews');
+    $I->switchToIframe('beamerNews');
+    $I->click('.headerClose');
+    $I->dontSeeElement('#beamerNews');
+  }
+}
