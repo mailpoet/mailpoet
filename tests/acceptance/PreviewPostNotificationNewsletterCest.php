e8f1b1843553e8767615e645f6773809ae8944e0
Michelle Shull <mashull@yahoo.com>
2019-02-05 18:14:04 -0500

    [MQ-43] Preview Post Notification

---
+++ tests/acceptance/PreviewPostNotificationNewsletterCest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MailPoet\Test\Acceptance;
+
+use MailPoet\Test\DataFactories\Newsletter;
+
+require_once __DIR__ . '/../DataFactories/Newsletter.php';
+
+class PreviewPostNotificationNewsletterCest {
+  function previewStandardNewsletter(\AcceptanceTester $I) {
+    $newsletterName = 'Preview in Browser Post Notification';
+    $newsletter = new Newsletter();
+    $newsletter->withSubject($newsletterName)->withPostNotificationsType()->create();
+    $I->wantTo('Preview a post notification');
+    $I->login();
+    $I->amOnMailpoetPage('Emails');
+    $I->click('Post Notifications', '[data-automation-id="newsletters_listing_tabs"]');
+    $I->waitForText($newsletterName);
+    $I->clickItemRowActionByItemName($newsletterName, 'Preview');
+    $I->switchToNextTab();
+    $I->waitForElement('.mailpoet_template');
+  }
+}
