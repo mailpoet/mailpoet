d2240ead1bc3b79744be307b49358ddc2c355897
Michelle Shull <mashull@yahoo.com>
2019-02-05 13:47:30 -0500

    [MQ-174] Acceptance test for Beamer to confirm sidebar opens and closes

--- tests/acceptance/BeamerOpenAndCloseCest.php
+++ tests/acceptance/BeamerOpenAndCloseCest.php
@@ -4,13 +4,13 @@ namespace MailPoet\Test\Acceptance;
 
 class BeamerOpenAndCloseCest {
   function openAndCloseBeamer(\AcceptanceTester $I){
-    //$beamer_carrot = '[data-automation-id="beamer-icon"]';
     $I->wantTo('Open and close Beamer sidebar');
     $I->login();
     $I->amOnMailPoetPage('Emails');
-    $I->click('.beamer-selector');
-    $I->waitForText('What\'s new in MailPoet');
+    $I->click('.mailpoet_feature_announcement_icon');
+    $I->waitForElement('#beamerNews');
+    $I->switchToIframe('beamerNews');
     $I->click('.headerClose');
-    $I->dontSeeElement('.headerTitle');
+    $I->dontSeeElement('#beamerNews');
   }
 }
