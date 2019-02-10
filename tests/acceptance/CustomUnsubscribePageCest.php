7bc61add139bf74d1311a713edf74b422ceba14a
Michelle Shull <mashull@yahoo.com>
2019-02-10 14:26:45 -0500

    [MQ-102] removed concatenation, fixed formatting, added escapeshellarg, some shine

--- tests/acceptance/CustomUnsubscribePageCest.php
+++ tests/acceptance/CustomUnsubscribePageCest.php
@@ -5,17 +5,17 @@ namespace MailPoet\Test\Acceptance;
 class CustomUnsubscribePageCest {
   function createCustomUnsubscribePage(\AcceptanceTester $I) {
     $I->wantTo('Create page with MP subscriber shortcode');
-    $pageTitle='SorryToSeeYouGo';
-    $pageContent='[mailpoet_manage\ text=\"Manage\ your\ subscription\"]';
-    $I->cli('post create --allow-root --post_type=page --post_title=' . $pageTitle . '  --post_content=' . $pageContent);
+    $pageTitle = 'SorryToSeeYouGo';
+    $pageText = 'Manage your subscription';
+    $pageContent = escapeshellarg("[mailpoet_manage text=\"$pageText\"]");
+    $I->cli("post create --allow-root --post_type=page --post_status=publish --post_title=$pageTitle --post_content=$pageContent");
     $I->login();
     $I->amOnPage('/wp-admin/edit.php?post_type=page');
     $I->waitForText($pageTitle);
     $I->click($pageTitle);
-    $I->click('Publish');
     //see live page with shortcode output
-    $I->click('View page');
+    $I->click('View Page');
     $I->waitForText($pageTitle);
-    $I->waitForText('Manage your subscription');
+    $I->waitForText($pageText);
   }
 }
