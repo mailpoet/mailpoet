4fea19ea8dbd46ad618f82980e2219489cd082b9
Michelle Shull <mashull@yahoo.com>
2019-02-12 16:34:42 -0500

    [MQ-89][MQ-90]Test MailPoet default and custom confirmation pages.

---
+++ tests/acceptance/ConfirmConfirmationPageCest.php
@@ -0,0 +1,32 @@
+<?php
+
+ namespace MailPoet\Test\Acceptance;
+
+class ConfirmConfirmationPageCest {
+  function confirmDefaultConfirmationPage(\AcceptanceTester $I) {
+    $I->wantTo('Confirm link to default confirmation page works correctly');
+    $I->login();
+    $I->amOnMailPoetPage('Settings');
+    $I->click('[data-automation-id="signup_settings_tab"]');
+    $I->waitForText('Enable sign-up confirmation');
+    $I->waitForText('MailPoet Page');
+    $I->click('[data-automation-id="preview_page_link"]');
+    $I->switchToNextTab();
+    $I->waitForText('You have subscribed to: demo 1, demo 2');
+    $pageTitle = 'MailPoetConfirmationPage';
+    $postContent = 'BobsYourUncle';
+    $I->cli("post create --allow-root --post_type=page --post_status=publish --post_title=$pageTitle --post_content=$postContent");
+    $I->amOnMailPoetPage('Settings');
+    $I->click('[data-automation-id="signup_settings_tab"]');
+    $I->waitForText('Enable sign-up confirmation');
+    $I->waitForText('MailPoet Page');
+    $I->selectOption('[data-automation-id="page_selection"]', $pageTitle);
+    //$I->click('Save settings');
+    //$I->waitForElementClickable('[data-automation-id="preview_page_link"]');
+    $I->click('[data-automation-id="preview_page_link"]');
+    $I->switchToNextTab();
+    $I->waitForText($postContent);
+
+  }
+}
+
