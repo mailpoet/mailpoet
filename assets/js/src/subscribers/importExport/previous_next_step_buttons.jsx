d531cd8678a3b27998a1dd4804e0d30ec179e175
Pavel Dohnal <pavel@mailpoet.com>
2019-02-12 16:12:45 +0100

    Refactor buttons to reusable component
    [MAILPOET-1626]

---
+++ assets/js/src/subscribers/importExport/previous_next_step_buttons.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import MailPoet from 'mailpoet';
+
+const PreviousNextStepButtons = ({ canGoNext, onPreviousAction, onNextAction }) => {
+  const nextStepClasses = classNames(
+    'button-primary',
+    'wysija',
+    { 'button-disabled': !canGoNext },
+  );
+  return (
+    <div className="import_step_buttons">
+      <button
+        className="button-primary wysija button"
+        type="button"
+        onClick={() => onPreviousAction()}
+      >
+        {MailPoet.I18n.t('previousStep')}
+      </button>
+      &nbsp;&nbsp;
+      <button
+        type="button"
+        className={nextStepClasses}
+        onClick={() => {
+          if (canGoNext) {
+            onNextAction();
+          }
+        }}
+      >
+        {MailPoet.I18n.t('nextStep')}
+      </button>
+    </div>
+  );
+};
+
+PreviousNextStepButtons.propTypes = {
+  canGoNext: PropTypes.bool.isRequired,
+  onPreviousAction: PropTypes.func.isRequired,
+  onNextAction: PropTypes.func.isRequired,
+};
+
+export default PreviousNextStepButtons;
