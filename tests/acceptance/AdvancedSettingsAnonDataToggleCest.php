646ce60bdbcc5082f8bef127c304138f712ee700
Michelle Shull <mashull@yahoo.com>
2019-02-13 13:39:36 -0500

    [MQ-94]Fixed naming, added better assertions

--- tests/acceptance/AdvancedSettingsAnonDataToggleCest.php
+++ tests/acceptance/AdvancedSettingsAnonDataToggleCest.php
@@ -2,18 +2,24 @@
 
 namespace MailPoet\Test\Acceptance;
 
-class AdvancedSettingsAnonDataToggleCest {
+class AdvancedSettingsAnonymousDataToggleCest {
   function toggleAnonDataSetting(\AcceptanceTester $I) {
-    $I->wantTo('Confirm anon data settings can be toggled');
-    $noAnonData = '[data-automation-id="analytics-no"]';
-    $yesAnonData = '[data-automation-id="analytics-yes"]';
+    $I->wantTo('Confirm anonymous data settings can be toggled');
+    $noAnonymousData = '[data-automation-id="analytics-no"]';
+    $yesAnonymousData = '[data-automation-id="analytics-yes"]';
+    $submitButton = '[data-automation-id="settings-submit-button"]';
     $I->login();
     $I->amOnMailPoetPage('Settings');
     $I->click('[data-automation-id="settings-advanced-tab"]');
     $I->waitForText('Bounce email address');
-    $I->selectOption($noAnonData, 'No');
-    $I->seeOptionIsSelected($noAnonData, 'No');
-    $I->selectOption($yesAnonData, 'Yes');
-    $I->seeOptionIsSelected($yesAnonData, 'Yes');
+    $I->selectOption($noAnonymousData, 'No');
+    $I->click($submitButton);
+    $I->waitForElementClickable($submitButton);
+    $I->seeOptionIsSelected($noAnonymousData, 'No');
+    $I->reloadPage();
+    $I->selectOption($yesAnonymousData, 'Yes');
+    $I->click($submitButton);
+    $I->waitForElementClickable($submitButton);
+    $I->seeOptionIsSelected($yesAnonymousData, 'Yes');
   }
 }
