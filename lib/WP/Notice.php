fb830e1b321f4d005df2ee646366959382f3b751
Ján Mikláš <neosinner@gmail.com>
2019-02-19 12:36:01 +0100

    Allow rendering WP notices without paragraph element
    [MAILPOET-1787]

--- lib/WP/Notice.php
+++ lib/WP/Notice.php
@@ -13,36 +13,37 @@ class Notice {
   private $classes;
   private $data_notice_name;
 
-  function __construct($type, $message, $classes = '', $data_notice_name = '') {
+  function __construct($type, $message, $classes = '', $data_notice_name = '', $render_in_paragraph = true) {
     $this->type = $type;
     $this->message = $message;
     $this->classes = $classes;
     $this->data_notice_name = $data_notice_name;
+    $this->render_in_paragraph = $render_in_paragraph;
   }
 
-  static function displayError($message, $classes = '', $data_notice_name = '') {
+  static function displayError($message, $classes = '', $data_notice_name = '', $render_in_paragraph = true) {
     $message = sprintf(
       "<b>%s </b> %s",
       __('MailPoet Error:', 'mailpoet'),
       $message
     );
-    self::createNotice(self::TYPE_ERROR, $message, $classes, $data_notice_name);
+    self::createNotice(self::TYPE_ERROR, $message, $classes, $data_notice_name, $render_in_paragraph);
   }
 
-  static function displayWarning($message, $classes = '', $data_notice_name = '') {
-    self::createNotice(self::TYPE_WARNING, $message, $classes, $data_notice_name);
+  static function displayWarning($message, $classes = '', $data_notice_name = '', $render_in_paragraph = true) {
+    self::createNotice(self::TYPE_WARNING, $message, $classes, $data_notice_name, $render_in_paragraph);
   }
 
-  static function displaySuccess($message, $classes = '', $data_notice_name = '') {
-    self::createNotice(self::TYPE_SUCCESS, $message, $classes, $data_notice_name);
+  static function displaySuccess($message, $classes = '', $data_notice_name = '', $render_in_paragraph = true) {
+    self::createNotice(self::TYPE_SUCCESS, $message, $classes, $data_notice_name, $render_in_paragraph);
   }
 
-  static function displayInfo($message, $classes = '', $data_notice_name = '') {
-    self::createNotice(self::TYPE_INFO, $message, $classes, $data_notice_name);
+  static function displayInfo($message, $classes = '', $data_notice_name = '', $render_in_paragraph = true) {
+    self::createNotice(self::TYPE_INFO, $message, $classes, $data_notice_name, $render_in_paragraph);
   }
 
-  protected static function createNotice($type, $message, $classes, $data_notice_name) {
-    $notice = new Notice($type, $message, $classes, $data_notice_name);
+  protected static function createNotice($type, $message, $classes, $data_notice_name, $render_in_paragraph) {
+    $notice = new Notice($type, $message, $classes, $data_notice_name, $render_in_paragraph);
     add_action('admin_notices', array($notice, 'displayWPNotice'));
   }
 
@@ -51,6 +52,10 @@ class Notice {
     $message = nl2br($this->message);
     $data_notice_name = !empty($this->data_notice_name) ? sprintf('data-notice="%s"', $this->data_notice_name) : '';
 
-    printf('<div class="%1$s" %3$s><p>%2$s</p></div>', $class, $message, $data_notice_name);
+    if ($this->render_in_paragraph) {
+      printf('<div class="%1$s" %3$s><p>%2$s</p></div>', $class, $message, $data_notice_name);
+    } else {
+      printf('<div class="%1$s" %3$s>%2$s</div>', $class, $message, $data_notice_name);
+    }
   }
 }
