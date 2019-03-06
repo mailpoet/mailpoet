823c86fadf2e29a8b4db1d730bcff3f5218c586d
Amine Ben hammou <amine@mailpoet.com>
2019-03-06 16:55:46 +0100

    Refactor Database::defineTables [MAILPOET-1677]

--- lib/Config/Database.php
+++ lib/Config/Database.php
@@ -61,61 +61,38 @@ class Database {
 
   function defineTables() {
     if (!defined('MP_SETTINGS_TABLE')) {
-      $settings = Env::$db_prefix . 'settings';
-      $segments = Env::$db_prefix . 'segments';
-      $forms = Env::$db_prefix . 'forms';
-      $custom_fields = Env::$db_prefix . 'custom_fields';
-      $subscribers = Env::$db_prefix . 'subscribers';
-      $subscriber_segment = Env::$db_prefix . 'subscriber_segment';
-      $subscriber_custom_field = Env::$db_prefix . 'subscriber_custom_field';
-      $subscriber_ips = Env::$db_prefix . 'subscriber_ips';
-      $newsletter_segment = Env::$db_prefix . 'newsletter_segment';
-      $scheduled_tasks = Env::$db_prefix . 'scheduled_tasks';
-      $scheduled_task_subscribers = Env::$db_prefix . 'scheduled_task_subscribers';
-      $sending_queues = Env::$db_prefix . 'sending_queues';
-      $newsletters = Env::$db_prefix . 'newsletters';
-      $newsletter_templates = Env::$db_prefix . 'newsletter_templates';
-      $newsletter_option_fields = Env::$db_prefix . 'newsletter_option_fields';
-      $newsletter_option = Env::$db_prefix . 'newsletter_option';
-      $newsletter_links = Env::$db_prefix . 'newsletter_links';
-      $newsletter_posts = Env::$db_prefix . 'newsletter_posts';
-      $statistics_newsletters = Env::$db_prefix . 'statistics_newsletters';
-      $statistics_clicks = Env::$db_prefix . 'statistics_clicks';
-      $statistics_opens = Env::$db_prefix . 'statistics_opens';
-      $statistics_unsubscribes = Env::$db_prefix . 'statistics_unsubscribes';
-      $statistics_forms = Env::$db_prefix . 'statistics_forms';
-      $mapping_to_external_entities = Env::$db_prefix . 'mapping_to_external_entities';
-      $log = Env::$db_prefix . 'log';
-      $stats_notifications = Env::$db_prefix . 'stats_notifications';
-      $user_flags = Env::$db_prefix . 'user_flags';
-
-      define('MP_SETTINGS_TABLE', $settings);
-      define('MP_SEGMENTS_TABLE', $segments);
-      define('MP_FORMS_TABLE', $forms);
-      define('MP_CUSTOM_FIELDS_TABLE', $custom_fields);
-      define('MP_SUBSCRIBERS_TABLE', $subscribers);
-      define('MP_SUBSCRIBER_SEGMENT_TABLE', $subscriber_segment);
-      define('MP_SUBSCRIBER_CUSTOM_FIELD_TABLE', $subscriber_custom_field);
-      define('MP_SUBSCRIBER_IPS_TABLE', $subscriber_ips);
-      define('MP_SCHEDULED_TASKS_TABLE', $scheduled_tasks);
-      define('MP_SCHEDULED_TASK_SUBSCRIBERS_TABLE', $scheduled_task_subscribers);
-      define('MP_SENDING_QUEUES_TABLE', $sending_queues);
-      define('MP_NEWSLETTERS_TABLE', $newsletters);
-      define('MP_NEWSLETTER_TEMPLATES_TABLE', $newsletter_templates);
-      define('MP_NEWSLETTER_SEGMENT_TABLE', $newsletter_segment);
-      define('MP_NEWSLETTER_OPTION_FIELDS_TABLE', $newsletter_option_fields);
-      define('MP_NEWSLETTER_LINKS_TABLE', $newsletter_links);
-      define('MP_NEWSLETTER_POSTS_TABLE', $newsletter_posts);
-      define('MP_NEWSLETTER_OPTION_TABLE', $newsletter_option);
-      define('MP_STATISTICS_NEWSLETTERS_TABLE', $statistics_newsletters);
-      define('MP_STATISTICS_CLICKS_TABLE', $statistics_clicks);
-      define('MP_STATISTICS_OPENS_TABLE', $statistics_opens);
-      define('MP_STATISTICS_UNSUBSCRIBES_TABLE', $statistics_unsubscribes);
-      define('MP_STATISTICS_FORMS_TABLE', $statistics_forms);
-      define('MP_MAPPING_TO_EXTERNAL_ENTITIES_TABLE', $mapping_to_external_entities);
-      define('MP_LOG_TABLE', $log);
-      define('MP_STATS_NOTIFICATIONS_TABLE', $stats_notifications);
-      define('MP_USER_FLAGS_TABLE', $user_flags);
+      $tables = [
+        'MP_SETTINGS_TABLE' => 'settings',
+        'MP_SEGMENTS_TABLE' => 'segments',
+        'MP_FORMS_TABLE' => 'forms',
+        'MP_CUSTOM_FIELDS_TABLE' => 'custom_fields',
+        'MP_SUBSCRIBERS_TABLE' => 'subscribers',
+        'MP_SUBSCRIBER_SEGMENT_TABLE' => 'subscriber_segment',
+        'MP_SUBSCRIBER_CUSTOM_FIELD_TABLE' => 'subscriber_custom_field',
+        'MP_SUBSCRIBER_IPS_TABLE' => 'subscriber_ips',
+        'MP_NEWSLETTER_SEGMENT_TABLE' => 'newsletter_segment',
+        'MP_SCHEDULED_TASKS_TABLE' => 'scheduled_tasks',
+        'MP_SCHEDULED_TASK_SUBSCRIBERS_TABLE' => 'scheduled_task_subscribers',
+        'MP_SENDING_QUEUES_TABLE' => 'sending_queues',
+        'MP_NEWSLETTERS_TABLE' => 'newsletters',
+        'MP_NEWSLETTER_TEMPLATES_TABLE' => 'newsletter_templates',
+        'MP_NEWSLETTER_OPTION_FIELDS_TABLE' => 'newsletter_option_fields',
+        'MP_NEWSLETTER_OPTION_TABLE' => 'newsletter_option',
+        'MP_NEWSLETTER_LINKS_TABLE' => 'newsletter_links',
+        'MP_NEWSLETTER_POSTS_TABLE' => 'newsletter_posts',
+        'MP_STATISTICS_NEWSLETTERS_TABLE' => 'statistics_newsletters',
+        'MP_STATISTICS_CLICKS_TABLE' => 'statistics_clicks',
+        'MP_STATISTICS_OPENS_TABLE' => 'statistics_opens',
+        'MP_STATISTICS_UNSUBSCRIBES_TABLE' => 'statistics_unsubscribes',
+        'MP_STATISTICS_FORMS_TABLE' => 'statistics_forms',
+        'MP_MAPPING_TO_EXTERNAL_ENTITIES_TABLE' => 'mapping_to_external_entities',
+        'MP_LOG_TABLE' => 'log',
+        'MP_STATS_NOTIFICATIONS_TABLE' => 'stats_notifications',
+        'MP_USER_FLAGS_TABLE' => 'user_flags',
+      ];
+      foreach ($tables as $constant => $name) {
+        define($constant, Env::$db_prefix . $name);
+      }
     }
   }
 }
