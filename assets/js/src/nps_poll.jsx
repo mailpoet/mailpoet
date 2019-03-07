429671f78f321727ee8cf9cff18e5904b789dc22
Rostislav Wolny <rosta@mailpoet.com>
2019-03-07 15:19:26 +0100

    Add new items to NPS traits
    [MAILPOET-1815]

--- assets/js/src/nps_poll.jsx
+++ assets/js/src/nps_poll.jsx
@@ -21,19 +21,31 @@ const showReviewRequestModal = () => MailPoet.Modal.popup({
 
 function displayPoll() {
   if (
-    window.mailpoet_display_nps_poll
+    (window.mailpoet_display_nps_poll || true)
     && window.satismeter
     && window.mailpoet_installed_at_isoFormat
+    && window.mailpoet_analytics_tracking_data
   ) {
     const newUsersPollId = '6L479eVPXk7pBn6S';
     const oldUsersPollId = 'k0aJAsQAWI2ERyGv';
     window.satismeter({
+      forceSurvey: true,
       writeKey: window.mailpoet_is_new_user ? newUsersPollId : oldUsersPollId,
       userId: window.mailpoet_current_wp_user.ID + window.mailpoet_site_url,
       traits: {
         name: window.mailpoet_current_wp_user.user_nicename,
         email: window.mailpoet_current_wp_user.user_email,
         createdAt: window.mailpoet_installed_at_isoFormat,
+        mailpoetVersion: window.mailpoet_version,
+        mailpoetPremiumIsActive: window.mailpoet_premium_active,
+        newslettersSent: window.mailpoet_analytics_tracking_data.newslettersSent,
+        welcomeEmails: window.mailpoet_analytics_tracking_data.welcomeEmails,
+        postnotificationEmails: window.mailpoet_analytics_tracking_data.postnotificationEmails,
+        woocommerceEmails: window.mailpoet_analytics_tracking_data.woocommerceEmails,
+        subscribers: window.mailpoet_analytics_tracking_data.subscribers,
+        lists: window.mailpoet_analytics_tracking_data.lists,
+        sendingMethod: window.mailpoet_analytics_tracking_data.sendingMethod,
+        woocommerceIsInstalled: window.mailpoet_analytics_tracking_data.woocommerceIsInstalled,
       },
       events: {
         submit: (response) => {
