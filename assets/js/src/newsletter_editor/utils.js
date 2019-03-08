d445495cba17dfcc291b4fb4e0284845de53fcdc
Rostislav Wolny <rosta@mailpoet.com>
2019-03-08 15:46:31 +0100

    Refactor event within view detection into utils
    [MAILPOET-1819]

---
+++ assets/js/src/newsletter_editor/utils.js
@@ -0,0 +1,13 @@
+export const isEventInsideElement = (event, $el) => {
+  var offset = $el.offset();
+  var height = $el.height();
+  var width = $el.width();
+  if (event.pageX < offset.left
+    || event.pageX > offset.left + width
+    || event.pageY < offset.top
+    || event.pageY > offset.top + height
+  ) {
+    return false;
+  }
+  return true;
+};
