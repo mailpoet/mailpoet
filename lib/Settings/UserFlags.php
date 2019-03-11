33847397f391ac01049ebf6238a4e058546d7b56
Amine Ben hammou <amine@mailpoet.com>
2019-03-11 14:25:29 +0100

    Add UserFlags controller [MAILPOET-1677]

---
+++ lib/Settings/UserFlags.php
@@ -0,0 +1,79 @@
+<?php
+namespace MailPoet\Settings;
+
+use MailPoet\Models\UserFlag;
+use MailPoet\WP\Functions as WPFunctions;
+
+class UserFlags {
+
+  /**
+   * @var array|null
+   */
+  private static $data = null;
+
+  public function getDefaults() {
+    return [
+      'last_announcement_seen' => false,
+      'editor_tutorial_seen' => false,
+    ];
+  }
+
+  public function load() {
+    $current_user_id = WPFunctions::get()->getCurrentUserId();
+    $flags = UserFlag::where('user_id', $current_user_id)->findMany();
+    self::$data = [];
+    foreach ($flags as $flag) {
+      self::$data[$flag->name] = $flag->value;
+    }
+  }
+
+  public function isLoaded() {
+    return self::$data !== null;
+  }
+
+  public function ensureLoaded() {
+    if (!$this->isLoaded()) {
+      $this->load();
+    }
+  }
+
+  public function get(string $name) {
+    $this->ensureLoaded();
+    if (empty(self::$data[$name])) {
+      $defaults = $this->getDefaults();
+      return $defaults[$name];
+    }
+    return self::$data[$name];
+  }
+
+  public function getAll() {
+    $this->ensureLoaded();
+    return array_merge($this->getDefaults(), self::$data);
+  }
+
+  public function set(string $name, $value) {
+    $current_user_id = WPFunctions::get()->getCurrentUserId();
+    UserFlag::createOrUpdate([
+      'user_id' => $current_user_id,
+      'name' => $name,
+      'value' => $value
+    ]);
+    if ($this->isLoaded()) {
+      self::$data[$name] = $value;
+    }
+  }
+
+  public function delete(string $name) {
+    $current_user_id = WPFunctions::get()->getCurrentUserId();
+    UserFlag::where('user_id', $current_user_id)
+      ->where('name', $name)
+      ->deleteMany();
+    if ($this->isLoaded()) {
+      unset(self::$data[$name]);
+    }
+  }
+
+  public static function clear() {
+    self::$data = null;
+  }
+}
---
+++ tests/integration/Settings/UserFlagsTest.php
@@ -0,0 +1,109 @@
+<?php
+namespace MailPoet\Test\Settings;
+
+use Codeception\Stub;
+use MailPoet\Models\UserFlag;
+use MailPoet\Settings\UserFlags;
+use MailPoet\WP\Functions as WPFunctions;
+
+class UserFlagsTest extends \MailPoetTest {
+
+  /** @var UserFlags */
+  private $user_flags;
+
+  /** @var int */
+  private $current_user_id;
+
+  /** @var int */
+  private $other_user_id;
+
+  function _before() {
+    parent::_before();
+    
+    $current_user_id = 1;
+    $other_user_id = 2;
+    WPFunctions::set(Stub::make(new WPFunctions, [
+      'getCurrentUserId' => $current_user_id,
+    ]));
+    $this->current_user_id = $current_user_id;
+    $this->user_flags = Stub::make(new UserFlags(), [
+      'getDefaults' => function() {
+        return [
+          'flag_1' => 'default_value_1',
+          'flag_2' => 'default_value_2',
+          'flag_3' => 'default_value_3',
+        ];
+      }
+    ]);
+    UserFlag::createOrUpdate([
+      'user_id' => $this->current_user_id,
+      'name' => 'flag_1',
+      'value' => 'value_1',
+    ]);
+    UserFlag::createOrUpdate([
+      'user_id' => $this->current_user_id,
+      'name' => 'flag_3',
+      'value' => 'value_3',
+    ]);
+    UserFlag::createOrUpdate([
+      'user_id' => $other_user_id,
+      'name' => 'flag_1',
+      'value' => 'other_value_1',
+    ]);
+    UserFlag::createOrUpdate([
+      'user_id' => $other_user_id,
+      'name' => 'flag_2',
+      'value' => 'other_value_2',
+    ]);
+  }
+
+  function testItGetsFlagsOfCurrentUser() {
+    expect($this->user_flags->get('flag_1'))->equals('value_1');
+    expect($this->user_flags->get('flag_2'))->equals('default_value_2');
+    expect($this->user_flags->getAll())->equals([
+      'flag_1' => 'value_1',
+      'flag_2' => 'default_value_2',
+      'flag_3' => 'value_3',
+    ]);
+  }
+
+  function testItLoadsDataOnlyOnceWhenNeeded() {
+    UserFlag::createOrUpdate([
+      'user_id' => $this->current_user_id,
+      'name' => 'flag_1',
+      'value' => 'new_value_1',
+    ]);
+    expect($this->user_flags->get('flag_1'))->equals('new_value_1');
+    UserFlag::createOrUpdate([
+      'user_id' => $this->current_user_id,
+      'name' => 'flag_1',
+      'value' => 'newer_value_1',
+    ]);
+    expect($this->user_flags->get('flag_1'))->equals('new_value_1');
+  }
+
+  function testItSetsNewFlagValue() {
+    expect($this->user_flags->get('flag_1'))->equals('value_1');
+    $this->user_flags->set('flag_1', 'updated_value');
+    expect($this->user_flags->get('flag_1'))->equals('updated_value');
+    $flag = UserFlag::where('user_id', $this->current_user_id)
+      ->where('name', 'flag_1')
+      ->findOne();
+    expect($flag->value)->equals('updated_value');
+  }
+
+  function testItDeletesAFlag() {
+    expect($this->user_flags->get('flag_1'))->equals('value_1');
+    $this->user_flags->delete('flag_1');
+    expect($this->user_flags->get('flag_1'))->equals('default_value_1');
+    $flag = UserFlag::where('user_id', $this->current_user_id)
+      ->where('name', 'flag_1')
+      ->findOne();
+    expect($flag)->equals(false);
+  }
+
+  function _after() {
+    UserFlags::clear();
+    \ORM::raw_execute('TRUNCATE ' . UserFlag::$_table);
+  }
+}
