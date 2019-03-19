16cf972cc8b2745044e8fb8124116f700ff05688
Amine Ben hammou <amine@mailpoet.com>
2019-03-19 17:16:31 +0100

    Fix small bugs

--- lib/DI/ContainerConfigurator.php
+++ lib/DI/ContainerConfigurator.php
@@ -93,7 +93,7 @@ class ContainerConfigurator implements IContainerConfigurator {
     // Settings
     $container->autowire(\MailPoet\Settings\SettingsController::class)->setPublic(true);
     // User Flags
-    $container->autowire(\MailPoet\Settings\UserFlagsController::class)->setPublic(false);
+    $container->autowire(\MailPoet\Settings\UserFlagsController::class);
     // Subscription
     $container->autowire(\MailPoet\Subscription\Comment::class)->setPublic(true);
     $container->autowire(\MailPoet\Subscription\Form::class)->setPublic(true);
