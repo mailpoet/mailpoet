e6e5eaa6c2ff28d4c5645dd4bfa3f52aa2331e4d
Amine Ben hammou <amine@mailpoet.com>
2019-03-27 17:26:05 +0100

    Fix PHPStan warnings

--- lib/WooCommerce/Helper.php
+++ lib/WooCommerce/Helper.php
@@ -5,25 +5,16 @@ class Helper {
   function isWooCommerceActive() {
     return class_exists('WooCommerce');
   }
+
   function wcGetCustomerOrderCount($user_id) {
-    if (! is_callable('wc_get_customer_order_count')) {
-      throw new \Exception("function 'wc_get_customer_order_count' not found!");
-    }
     return wc_get_customer_order_count($user_id);
   }
-  function wcGetOrder($order = false) {
-    if (! is_callable('wc_get_order')) {
-      throw new \Exception("function 'wc_get_order' not found!");
 
-    }
+  function wcGetOrder($order = false) {
     return wc_get_order($order);
   }
 
   function wcPrice($price, array $args = array()) {
-    if (! is_callable('wc_price')) {
-      throw new \Exception("function 'wc_price' not found!");
-
-    }
     return wc_price($price, $args);
   }
 }
