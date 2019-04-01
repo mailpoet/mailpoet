9d8866237334379c43c337befc0ca0a7e749b589
Rostislav Wolny <rosta@mailpoet.com>
2019-04-01 11:33:35 +0200

    Add comments and remove unnecessary checks from a WC list acceptance test
    [MAILPOET-1732]

--- tests/acceptance/WooCommerceListImportPageCest.php
+++ tests/acceptance/WooCommerceListImportPageCest.php
@@ -14,9 +14,6 @@ use MailPoet\Test\DataFactories\WooCommerceProduct;
 
 class WooCommerceListImportPageCest {
 
-  /** @var WooCommerceProduct */
-  private $product_factory;
-
   /** @var WooCommerceCustomer */
   private $customer_factory;
 
@@ -25,13 +22,47 @@ class WooCommerceListImportPageCest {
 
   function _before(\AcceptanceTester $I) {
     $I->activateWooCommerce();
-    $this->product_factory = new WooCommerceProduct($I);
     $this->customer_factory = new WooCommerceCustomer($I);
     $this->order_factory = new WooCommerceOrder($I);
     // Cleanup
     $this->customer_factory->deleteAll();
-    $this->product_factory->deleteAll();
     $this->order_factory->deleteAll();
+  }
+
+  function importListPageImportTest(\AcceptanceTester $I) {
+    $settings_factory = new Settings();
+    $settings_factory->withWooCommerceListImportPageDisplayed(false);
+    $order = $this->order_factory->create();
+    $guest_user_data = $order['billing'];
+    $registered_customer = $this->customer_factory->withEmail('customer1@email.com')->create();
+    $I->login();
+    $I->amOnPage('wp-admin/admin.php?page=mailpoet-woocommerce-list-import');
+    $subscribed_radio = '[data-automation-id="import_as_subscribed"]';
+    $submit_button = '[data-automation-id="submit_woo_commerce_list_import"]';
+    $I->selectOption($subscribed_radio, 'subscribed');
+    $I->click($submit_button);
+    $I->seeNoJSErrors();
+    $I->waitForElement('[data-automation-id="newsletters_listing_tabs"]');
+    $I->amOnMailpoetPage('Lists');
+    $I->waitForText('WooCommerce Customers');
+    $I->moveMouseOver('[data-automation-id="segment_name_WooCommerce Customers"]');
+    $I->click('[data-automation-id="view_subscribers_WooCommerce Customers"]');
+    $I->waitForListingItemsToLoad();
+    $I->canSee($registered_customer['email']);
+    $I->reloadPage();
+    // It takes more time to sync guest user
+    // So we reload page several times and check for guest customer email
+    for ($i = 0; $i < 15; $i++) {
+      try {
+        $I->wait(2);
+        $I->reloadPage();
+        $I->see($guest_user_data['email']);
+        return;
+      } catch (\PHPUnit_Framework_Exception $e) {
+        continue;
+      }
+    }
+    $I->see($guest_user_data['email']);
   }
 
   function importPageFormBehaviourTest(\AcceptanceTester $I) {
@@ -51,9 +82,13 @@ class WooCommerceListImportPageCest {
     $I->click($submit_button);
     $I->seeNoJSErrors();
     $I->waitForElement('[data-automation-id="newsletters_listing_tabs"]');
-    $I->seeInCurrentUrl('wp-admin/admin.php?page=mailpoet-newsletters');
   }
 
+  /**
+   * Test that admin is always redirected to WooCommerce list import page and
+   * can't go to another page unless he submits the form
+   * @param \AcceptanceTester $I
+   */
   function importListPageRedirectionTest(\AcceptanceTester $I) {
     $settings_factory = new Settings();
     $settings_factory->withWooCommerceListImportPageDisplayed(false);
@@ -64,43 +99,6 @@ class WooCommerceListImportPageCest {
     $I->amOnMailpoetPage('Emails');
     $I->seeInCurrentUrl('wp-admin/admin.php?page=mailpoet-woocommerce-list-import');
     $this->order_factory->delete($order['id']);
-  }
-
-  function importListPageImportTest(\AcceptanceTester $I) {
-    $settings_factory = new Settings();
-    $settings_factory->withWooCommerceListImportPageDisplayed(false);
-    $order = $this->order_factory->create();
-    $guest_user_data = $order['billing'];
-    $registered_customer = $this->customer_factory->withEmail('customer1@email.com')->create();
-    $I->login();
-    $I->amOnPage('wp-admin/admin.php?page=mailpoet-woocommerce-list-import');
-    $subscribed_radio = '[data-automation-id="import_as_subscribed"]';
-    $submit_button = '[data-automation-id="submit_woo_commerce_list_import"]';
-    $I->selectOption($subscribed_radio, 'subscribed');
-    $I->click($submit_button);
-    $I->seeNoJSErrors();
-    $I->waitForElement('[data-automation-id="newsletters_listing_tabs"]');
-    $I->seeInCurrentUrl('wp-admin/admin.php?page=mailpoet-newsletters');
-    $I->amOnMailpoetPage('Lists');
-    $I->waitForText('WooCommerce Customers');
-    $I->moveMouseOver('[data-automation-id="segment_name_WooCommerce Customers"]');
-    $I->click('[data-automation-id="view_subscribers_WooCommerce Customers"]');
-    $I->waitForListingItemsToLoad();
-    $I->canSee($registered_customer['email']);
-    $I->reloadPage();
-    // It takes more time to sync guest user
-    // So we reload page several times and check for guest customer email
-    for ($i = 0; $i < 15; $i++) {
-      try {
-        $I->wait(1);
-        $I->reloadPage();
-        $I->see($guest_user_data['email']);
-        return;
-      } catch (\PHPUnit_Framework_Exception $e) {
-        continue;
-      }
-    }
-    $I->see($guest_user_data['email']);
   }
 
   function _after(\AcceptanceTester $I) {
