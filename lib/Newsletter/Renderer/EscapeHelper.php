36608e1066da185104a9232cbed8bdba5c0dfd40
Rostislav Wolny <rosta@mailpoet.com>
2019-04-08 16:36:18 +0200

    Add php doc to block render helpers
    [MAILPOET-1891]

--- lib/Newsletter/Renderer/EscapeHelper.php
+++ lib/Newsletter/Renderer/EscapeHelper.php
@@ -2,22 +2,44 @@
 namespace MailPoet\Newsletter\Renderer;
 
 class EscapeHelper {
+  /**
+   * @param string $string
+   * @return string
+   */
   static function escapeHtmlText($string) {
     return htmlspecialchars((string)$string, ENT_NOQUOTES, 'UTF-8');
   }
 
+  /**
+   * @param string $string
+   * @return string
+   */
   static function escapeHtmlAttr($string) {
     return htmlspecialchars((string)$string, ENT_QUOTES, 'UTF-8', true);
   }
 
+  /**
+   * Similar to escapeHtmlAttr just this one keeps single quotes since some email clients
+   * (e.g. Yahoo webmail) don't support encoded quoted font names
+   * @param string $string
+   * @return string
+   */
   static function escapeHtmlStyleAttr($string) {
     return htmlspecialchars((string)$string, ENT_COMPAT, 'UTF-8', true);
   }
 
+  /**
+   * @param string $string
+   * @return string
+   */
   static function unescapeHtmlStyleAttr($string) {
     return htmlspecialchars_decode((string)$string, ENT_COMPAT);
   }
 
+  /**
+   * @param string $string
+   * @return string
+   */
   static function escapeHtmlLinkAttr($string) {
     $string = self::escapeHtmlAttr($string);
     if (preg_match('~^javascript:|^data:text|^data:application~i', $string) === 1) {
