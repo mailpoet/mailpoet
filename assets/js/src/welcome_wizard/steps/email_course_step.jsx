f6192f77a165ea231c49d967778ba71bf37b7397
Ján Mikláš <neosinner@gmail.com>
2019-04-09 13:38:48 +0200

    Updated wizard content styles
    [MAILPOET-1927]

--- assets/js/src/welcome_wizard/steps/email_course_step.jsx
+++ assets/js/src/welcome_wizard/steps/email_course_step.jsx
@@ -6,19 +6,17 @@ const WelcomeWizardEmailCourseStep = props => (
   <div className="mailpoet_welcome_wizard_step_content">
     <h1>{MailPoet.I18n.t('welcomeWizardEmailCourseTitle')}</h1>
     <p>{MailPoet.I18n.t('welcomeWizardEmailCourseText')}</p>
-    <div className="mailpoet_welcome_wizard_course_form_box">
-      <iframe
-        id="mailpoet_form_iframe"
-        width="100%"
-        scrolling="no"
-        frameBorder="0"
-        title="Apply to course"
-        src="https://newsletters.mailpoet.com?mailpoet_form_iframe=13"
-        className="mailpoet_form_iframe"
-        marginWidth="0"
-        marginHeight="0"
-      />
-    </div>
+    <iframe
+      id="mailpoet_form_iframe"
+      width="100%"
+      scrolling="no"
+      frameBorder="0"
+      title="Apply to course"
+      src="https://newsletters.mailpoet.com?mailpoet_form_iframe=13"
+      className="mailpoet_form_iframe"
+      marginWidth="0"
+      marginHeight="0"
+    />
     <button type="button" className="button button-primary" onClick={props.next}>{MailPoet.I18n.t('next')}</button>
   </div>
 );
