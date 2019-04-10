b07975b91b7969f59eceaa810d5d12287321a480
Rostislav Wolny <rosta@mailpoet.com>
2019-04-10 18:19:50 +0200

    Fix pQuery classes to respect psr-4

---
+++ lib/Util/pQuery/Html5Parser.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MailPoet\Util\pQuery;
+
+class Html5Parser extends \pQuery\HtmlParser {
+  /** @var string|\pQuery\DomNode */
+  public $root = 'MailPoet\Util\pQuery\DomNode';
+}
--- lib/Util/pQuery/pQuery.php
+++ lib/Util/pQuery/pQuery.php
@@ -9,20 +9,3 @@ class pQuery extends \pQuery {
     return $parser->root;
   }
 }
-
-class Html5Parser extends \pQuery\HtmlParser {
-  /** @var string|\pQuery\DomNode */
-  public $root = 'MailPoet\Util\pQuery\DomNode';
-}
-
-class DomNode extends \pQuery\DomNode {
-  public $childClass = 'MailPoet\Util\pQuery\DomNode';
-
-  function getInnerText() {
-    return html_entity_decode($this->toString(true, true, 1), ENT_NOQUOTES, 'UTF-8');
-  }
-
-  function getOuterText() {
-    return html_entity_decode($this->toString(), ENT_NOQUOTES, 'UTF-8');
-  }
-}
