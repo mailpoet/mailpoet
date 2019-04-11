3fbdd861b2373d9139f7adf44b41f51d16e3d8c5
Michelle Shull <mashull@yahoo.com>
2019-04-11 17:13:32 -0400

    [MQ-109] tests text block can be added to editor

---
+++ tests/acceptance/EditorTextBlockCest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MailPoet\Test\Acceptance;
+
+use MailPoet\Test\DataFactories\Newsletter;
+
+require_once __DIR__ . '/../DataFactories/Newsletter.php';
+
+class EditorTextBlockCest {
+  function addText(\AcceptanceTester $I) {
+    $I->wantTo('add Text block to newsletter');
+    $newsletterTitle = 'Text Block Newsletter';
+    $textInEditor = ('[data-automation-id="text_block_in_editor"]');
+    (new Newsletter())
+      ->withSubject($newsletterTitle)
+      ->loadBodyFrom('newsletterWithText.json')
+      ->create();
+    $I->login();
+    $I->amOnMailpoetPage('Emails');
+    $I->waitForText($newsletterTitle);
+    $I->clickItemRowActionByItemName($newsletterTitle, 'Edit');
+    // Create Text block
+    $I->waitForText('Text');
+    $I->wait(1); // just to be sure
+    $I->dragAndDrop('#automation_editor_block_text', '#mce_1');
+    $I->waitForText('Edit this to insert text.');
+  }
+
+}
