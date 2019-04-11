f885d862fac63fe66aed7de5fc382bc824048e0e
Pavel Dohnal <pavel@mailpoet.com>
2019-04-11 14:35:50 +0200

    Create mailchimp component in react
    [MAILPOET-1808]

--- assets/js/src/subscribers/importExport/import/step_method_selection/method_mailchimp.jsx
+++ assets/js/src/subscribers/importExport/import/step_method_selection/method_mailchimp.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import MailPoet from 'mailpoet';
 
-const MethodMailChimp = ({ setInputValid }) => {
+const MethodMailChimp = ({ setInputValid, setInputInvalid, onValueChange }) => {
+  const [key, setKey] = useState('');
+
   return (
-    <div>
-      MethodMailChimp
-    </div>
+    <>
+      <label htmlFor="paste_input" className="import_method_paste">
+        <div>
+          <span className="import_heading">{MailPoet.I18n.t('methodMailChimpLabel')}</span>
+        </div>
+        <input
+          id="paste_input"
+          type="text"
+          onChange={setKey}
+        />
+        <button className="button" type="button">
+          {MailPoet.I18n.t('methodMailChimpVerify')}
+        </button>
+        <span className="mailpoet_mailchimp-key-status"></span>
+      </label>
+    </>
   );
 };
 
 MethodMailChimp.propTypes = {
   setInputValid: PropTypes.func,
+  setInputInvalid: PropTypes.func,
+  onValueChange: PropTypes.func.isRequired,
 };
 
 MethodMailChimp.defaultProps = {
   setInputValid: () => {},
+  setInputInvalid: () => {},
 };
 
 export default MethodMailChimp;
