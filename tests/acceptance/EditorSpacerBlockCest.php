4b2b74dbe22f7c014498a7978bfd91f9684886fb
Michelle Shull <mashull@yahoo.com>
2019-04-11 14:12:06 -0400

    [MQ-104] tests adding spacer block to editor

---
+++ tests/acceptance/EditorSpacerBlockCest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MailPoet\Test\Acceptance;
+
+use MailPoet\Test\DataFactories\Newsletter;
+
+require_once __DIR__ . '/../DataFactories/Newsletter.php';
+
+class EditorSpacerBlockCest {
+  function addSpacer(\AcceptanceTester $I) {
+    $I->wantTo('add spacer block to newsletter');
+    $newsletterTitle = 'Spacer Block Newsletter';
+    $spacerResizeHandle = ('[data-automation-id="spacer_resize_handle"]');
+    $spacerInEditor = ('[data-automation-id="spacer"]');
+    $spacerSettingsAssertion = ('[data-automation-id="spacer_done_button"]');
+    (new Newsletter())
+      ->withSubject($newsletterTitle)
+      ->loadBodyFrom('newsletterWithText.json')
+      ->create();
+    $I->login();
+    $I->amOnMailpoetPage('Emails');
+    $I->waitForText($newsletterTitle);
+    $I->clickItemRowActionByItemName($newsletterTitle, 'Edit');
+    // Create divider block
+    $I->waitForText('Spacer');
+    $I->wait(1); // just to be sure
+    $I->dragAndDrop('#automation_editor_block_spacer', '#mce_1');
+    //$I->wait(2); // just to be sure again
+    //Open settings by clicking on block
+    $I->moveMouseOver($spacerInEditor, 3, 2);
+    $I->waitForElementVisible($spacerResizeHandle);
+    $I->click($spacerInEditor);
+    $I->waitForElementVisible($spacerSettingsAssertion);
+    $I->click($spacerSettingsAssertion);
+  }
+
+}
