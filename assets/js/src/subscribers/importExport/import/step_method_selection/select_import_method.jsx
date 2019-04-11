55360c5c7b7b68e1cbec3dd52e987f96343e3451
Pavel Dohnal <pavel@mailpoet.com>
2019-04-11 10:11:28 +0200

    Move method selction form to component
    [MAILPOET-1808]

---
+++ assets/js/src/subscribers/importExport/import/step_method_selection/select_import_method.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import MailPoet from 'mailpoet';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import ImportContext from '../context.jsx';
+
+function SelectImportMethod({
+  activeMethod,
+  onMethodChange,
+}) {
+
+  const renderSelection = () => (
+    <form>
+      {MailPoet.I18n.t('methodSelectionHead')}
+      <label htmlFor="import-paste-method">
+        <input
+          type="radio"
+          name="select_method"
+          data-automation-id="import-paste-method"
+          id="import-paste-method"
+        />
+        {MailPoet.I18n.t('methodPaste')}
+      </label>
+      <label htmlFor="import-csv-method">
+        <input
+          type="radio"
+          name="select_method"
+          data-automation-id="import-csv-method"
+          id="import-csv-method"
+        />
+        {MailPoet.I18n.t('methodUpload')}
+      </label>
+      <label htmlFor="import-mailchimp-method">
+        <input
+          type="radio"
+          name="select_method"
+          data-automation-id="import-mailchimp-method"
+          id="import-mailchimp-method"
+        />
+        {MailPoet.I18n.t('methodMailChimp')}
+      </label>
+    </form>
+  );
+
+  return (
+    <ImportContext.Consumer>
+      {({ isNewUser }) => {
+        const badgeClasses = classNames(
+          'mailpoet_badge',
+          'mailpoet_badge_video',
+          { mailpoet_badge_video_grey: isNewUser }
+        );
+        return (
+          <>
+            {renderSelection()}
+            <a
+              className={badgeClasses}
+              href="https://beta.docs.mailpoet.com/article/242-video-guide-importing-subscribers-using-a-csv-file"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="dashicons dashicons-format-video" />
+              {MailPoet.I18n.t('seeVideo')}
+            </a>
+          </>
+        );
+      }}
+    </ImportContext.Consumer>
+  );
+}
+
+SelectImportMethod.propTypes = {
+  activeMethod: PropTypes.string,
+  onMethodChange: PropTypes.func.isRequired,
+};
+
+SelectImportMethod.defaultProps = {
+  activeMethod: undefined,
+};
+
+export default SelectImportMethod;
