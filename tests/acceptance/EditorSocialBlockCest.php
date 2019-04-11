7cb861b16afbb2b8b9585be98848a607fe0d0631
Michelle Shull <mashull@yahoo.com>
2019-04-11 15:10:55 -0400

    [MQ-105] tests adding social block to editor

---
+++ tests/acceptance/EditorSocialBlockCest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MailPoet\Test\Acceptance;
+
+use MailPoet\Test\DataFactories\Newsletter;
+
+require_once __DIR__ . '/../DataFactories/Newsletter.php';
+
+class EditorSocialBlockCest {
+  function addSocialBlock(\AcceptanceTester $I) {
+    $I->wantTo('add social block to newsletter');
+    $newsletterTitle = 'Social Block Newsletter';
+    $socialBlockInEditor = ('[data-automation-id="socialBlock"]');
+    $socialBlockSettingsAssertion = ('[data-automation-id="social_select_another_network"]');
+    (new Newsletter())
+      ->withSubject($newsletterTitle)
+      ->loadBodyFrom('newsletterWithText.json')
+      ->create();
+    $I->login();
+    $I->amOnMailpoetPage('Emails');
+    $I->waitForText($newsletterTitle);
+    $I->clickItemRowActionByItemName($newsletterTitle, 'Edit');
+    // Create social block
+    $I->waitForText('Social');
+    $I->wait(1); // just to be sure
+    $I->dragAndDrop('#automation_editor_block_social', '#mce_1');
+    //Open settings by clicking on block
+    $I->moveMouseOver($socialBlockInEditor, 3, 2);
+    $I->click($socialBlockInEditor);
+    $I->waitForElementVisible($socialBlockSettingsAssertion);
+    $I->click('Done');
+  }
+
+}
