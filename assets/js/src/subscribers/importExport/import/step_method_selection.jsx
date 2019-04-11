92ca2c3633539fead54af7e07808f6d600086506
Pavel Dohnal <pavel@mailpoet.com>
2019-04-11 10:51:28 +0200

    Display componets for inputs
    [MAILPOET-1808]

--- assets/js/src/subscribers/importExport/import/step_method_selection.jsx
+++ assets/js/src/subscribers/importExport/import/step_method_selection.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import PreviousNextStepButtons from './previous_next_step_buttons.jsx';
 import SelectMethod from './step_method_selection/select_import_method.jsx';
+import MethodPaste from './step_method_selection/method_paste.jsx';
+import MethodUpload from './step_method_selection/method_upload.jsx';
+import MethodMailChimp from './step_method_selection/method_mailchimp.jsx';
 
 function StepMethodSelection({
   navigate,
@@ -28,6 +31,21 @@ function StepMethodSelection({
         activeMethod={method}
         onMethodChange={setMethod}
       />
+      { method === 'paste-method'
+        ? (
+          <MethodPaste />
+        ) : null
+      }
+      { method === 'csv-method'
+        ? (
+          <MethodUpload />
+        ) : null
+      }
+      { method === 'mailchimp-method'
+        ? (
+          <MethodMailChimp />
+        ) : null
+      }
       {showNextButton()}
     </>
   );
---
+++ assets/js/src/subscribers/importExport/import/step_method_selection/method_upload.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MailPoet from 'mailpoet';
+
+const MethodUpload = () => {
+  return (
+    <div>
+      MethodUpload
+    </div>
+  );
+};
+
+MethodUpload.propTypes = {
+};
+
+MethodUpload.defaultProps = {
+};
+
+export default MethodUpload;
