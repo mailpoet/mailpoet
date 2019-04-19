f0977871afd21c9b371d9073b0977a112411165a
Rostislav Wolny <rosta@mailpoet.com>
2019-04-19 12:29:52 +0200

    Add WooCommerce list import scheduling
    [MAILPOET-1732]

--- assets/js/src/wizard/woocommerce_import_controller.jsx
+++ assets/js/src/wizard/woocommerce_import_controller.jsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import MailPoet from 'mailpoet';
 import WooCommerceImportListStep from './steps/woo_commerce_import_list_step.jsx';
@@ -6,13 +5,14 @@ import WooCommerceImportListStep from './steps/woo_commerce_import_list_step.jsx
 class WooCommerceImportController extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       loading: false,
     };
     this.updateSettings = this.updateSettings.bind(this);
+    this.scheduleImport = this.scheduleImport.bind(this);
+    this.finishWizard = this.finishWizard.bind(this);
+    this.submit = this.submit.bind(this);
   }
-
 
   finishWizard() {
     this.setState({ loading: true });
@@ -20,12 +20,27 @@ class WooCommerceImportController extends React.Component {
   }
 
   updateSettings(data) {
-    this.setState({ loading: true });
     return MailPoet.Ajax.post({
       api_version: window.mailpoet_api_version,
       endpoint: 'settings',
       action: 'set',
       data,
+    }).fail((response) => {
+      this.setState({ loading: false });
+      if (response.errors.length > 0) {
+        MailPoet.Notice.error(
+          response.errors.map(error => error.message),
+          { scroll: true }
+        );
+      }
+    });
+  }
+
+  scheduleImport() {
+    return MailPoet.Ajax.post({
+      api_version: window.mailpoet_api_version,
+      endpoint: 'importExport',
+      action: 'setupWooCommerceInitialImport',
     }).then(() => this.setState({ loading: false })).fail((response) => {
       this.setState({ loading: false });
       if (response.errors.length > 0) {
@@ -37,13 +52,22 @@ class WooCommerceImportController extends React.Component {
     });
   }
 
+  submit(importType) {
+    this.setState({ loading: true });
+    const settings = {
+      'woocommerce.import_screen_displayed': 1,
+      'mailpoet_subscribe_old_woocommerce_customers.enabled': importType === 'subscribed' ? 1 : 0,
+    };
+    this.updateSettings(settings).then(this.scheduleImport).then(this.finishWizard);
+  }
+
   render() {
     return (
       <div className="mailpoet_welcome_wizard_steps mailpoet_welcome_wizard_centered_column">
         <div className="mailpoet_welcome_wizard_header">
           <img src={window.mailpoet_logo_url} width="200" height="87" alt="MailPoet logo" />
         </div>
-        <WooCommerceImportListStep loading={this.state.loading} />
+        <WooCommerceImportListStep loading={this.state.loading} submitForm={this.submit} />
       </div>
     );
   }
