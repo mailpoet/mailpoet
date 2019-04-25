765d21ed6b6ba91905eac7d14a7d4d5420b68b95
Jan Jakeš <jan@jakes.pro>
2019-04-25 13:38:22 +0200

    Add driver option & wait_timeout settings according to Database.php
    [MAILPOET-2014]

--- tests/integration/Doctrine/ConnectionFactoryTest.php
+++ tests/integration/Doctrine/ConnectionFactoryTest.php
@@ -24,4 +24,49 @@ class ConnectionFactoryTest extends \MailPoetTest {
     expect($connection->getParams()['charset'])->equals(Env::$db_charset);
     expect($connection->getDatabase())->equals(Env::$db_name);
   }
+
+  function testItSetsDriverOptions() {
+    $connection_factory = new ConnectionFactory();
+    $connection = $connection_factory->createConnection();
+
+    $result = $connection->executeQuery('
+      SELECT
+        @@session.sql_mode,
+        @@session.time_zone,
+        @@session.wait_timeout,
+        @@session.character_set_client,
+        @@session.character_set_connection,
+        @@session.character_set_results,
+        @@session.collation_connection
+    ')->fetch();
+
+    // check timezone, SQL mode, wait timeout
+    expect($result['@@session.sql_mode'])->notContains('ONLY_FULL_GROUP_BY');
+    expect($result['@@session.time_zone'])->equals(Env::$db_timezone_offset);
+    expect($result['@@session.wait_timeout'])->greaterOrEquals(60);
+
+    // check "SET NAMES ... COLLATE ..."
+    expect($result['@@session.character_set_client'])->equals(Env::$db_charset);
+    expect($result['@@session.character_set_connection'])->equals(Env::$db_charset);
+    expect($result['@@session.character_set_results'])->equals(Env::$db_charset);
+    expect($result['@@session.collation_connection'])->equals(Env::$db_collation);
+  }
+
+  function testItSelectivelyUpdatesWaitTimeoutOption() {
+    // timeout will be kept from DB defaults
+    $connection_factory = $this->make(ConnectionFactory::class, [
+      'min_wait_timeout' => 1,
+    ]);
+    $connection = $connection_factory->createConnection();
+    $current = $connection->executeQuery('SELECT @@session.wait_timeout')->fetchColumn();
+    expect($current)->greaterThan(1);
+
+    // timeout will be set to higher value
+    $connection_factory = $this->make(ConnectionFactory::class, [
+      'min_wait_timeout' => 999999,
+    ]);
+    $connection = $connection_factory->createConnection();
+    $current = $connection->executeQuery('SELECT @@session.wait_timeout')->fetchColumn();
+    expect($current)->equals(999999);
+  }
 }
