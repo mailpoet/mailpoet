45c2464e5090446869147789c7b20ac53ec4f940
Jan Jakeš <jan@jakes.pro>
2019-04-29 14:56:10 +0200

    Use site-specific form field obfuscation hash
    [MAILPOET-1999]

--- tests/unit/Form/Util/FieldNameObfuscatorTest.php
+++ tests/unit/Form/Util/FieldNameObfuscatorTest.php
@@ -2,7 +2,18 @@
 
 namespace MailPoet\Form\Util;
 
+use Codeception\Stub;
+use MailPoet\WP\Functions as WPFunctions;
+
 class FieldNameObfuscatorTest extends \MailPoetUnitTest {
+  function _before() {
+    parent::_before();
+    WPFunctions::set(
+      Stub::make(WPFunctions::class, [
+        'homeUrl' => 'http://example.com',
+      ])
+    );
+  }
 
   public function testObfuscateWorks() {
     $obfuscator = new FieldNameObfuscator();
@@ -27,5 +38,10 @@ class FieldNameObfuscatorTest extends \MailPoetUnitTest {
       'regularField' => 'regularValue',
       'email' => 'obfuscatedFieldValue',
     ));
+  }
+
+  function _after() {
+    parent::_after();
+    WPFunctions::set(new WPFunctions());
   }
 }
