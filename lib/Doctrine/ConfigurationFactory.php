ae45bf246d46285421781a9e84ae1e2721009df9
Jan Jakeš <jan@jakes.pro>
2019-05-09 15:00:51 +0200

    Add Doctrine metadata storage configuration
    [MAILPOET-2014]

--- lib/Doctrine/ConfigurationFactory.php
+++ lib/Doctrine/ConfigurationFactory.php
@@ -2,11 +2,14 @@
 
 namespace MailPoet\Doctrine;
 
+use MailPoetVendor\Doctrine\Common\Annotations\SimpleAnnotationReader;
+use MailPoetVendor\Doctrine\Common\Cache\FilesystemCache;
 use MailPoetVendor\Doctrine\ORM\Configuration;
 use MailPoetVendor\Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
 
 class ConfigurationFactory {
   const ENTITY_DIR = __DIR__ . '/../Doctrine/Entities';
+  const METADATA_DIR = __DIR__ . '/../../generated/doctrine-metadata';
 
   /** @var bool */
   private $is_dev_mode;
@@ -18,11 +21,24 @@ class ConfigurationFactory {
   function createConfiguration() {
     $configuration = new Configuration();
     $configuration->setNamingStrategy(new UnderscoreNamingStrategy());
-    $configuration->setMetadataDriverImpl($configuration->newDefaultAnnotationDriver([self::ENTITY_DIR]));
+
+    $this->configureMetadata($configuration);
 
     if (!$this->is_dev_mode) {
       $configuration->ensureProductionSettings();
     }
     return $configuration;
+  }
+
+  private function configureMetadata(Configuration $configuration) {
+    // metadata cache (for production cache is pre-generated at build time)
+    $metadata_storage = new MetadataCache(self::METADATA_DIR);
+    $configuration->setMetadataCacheImpl($metadata_storage);
+
+    // register annotation reader if doctrine/annotations package is installed
+    // (i.e. in dev environment, on production metadata is dumped in the build)
+    if (class_exists(SimpleAnnotationReader::class)) {
+      $configuration->setMetadataDriverImpl($configuration->newDefaultAnnotationDriver([self::ENTITY_DIR]));
+    }
   }
 }
