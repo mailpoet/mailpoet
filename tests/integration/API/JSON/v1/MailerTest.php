e7b2b9aee999a27f11103574b332391a0045a8c6
Rostislav Wolny <rosta@mailpoet.com>
2019-05-10 10:07:57 +0200

    Add authorized emails check to resume sending endpoint
    [MAILPOET-1987]

--- tests/integration/API/JSON/v1/MailerTest.php
+++ tests/integration/API/JSON/v1/MailerTest.php
@@ -1,9 +1,12 @@
 <?php
 namespace MailPoet\Test\API\JSON\v1;
 
+use Codeception\Stub\Expected;
 use MailPoet\API\JSON\v1\Mailer;
 use MailPoet\API\JSON\Response as APIResponse;
 use MailPoet\Mailer\MailerLog;
+use MailPoet\Services\Bridge;
+use MailPoet\Settings\SettingsController;
 
 class MailerTest extends \MailPoetTest {
   function testItResumesSending() {
@@ -12,11 +15,21 @@ class MailerTest extends \MailPoetTest {
     MailerLog::updateMailerLog($mailer_log);
     $mailer_log = MailerLog::getMailerLog();
     expect($mailer_log['status'])->equals(MailerLog::STATUS_PAUSED);
+    $settings = new SettingsController();
+    $bridge = $this->makeEmpty(Bridge::class, ['checkAuthorizedEmailAddresses' => Expected::never()]);
     // resumeSending() method should clear the mailer log's status
-    $mailer_endpoint = new Mailer();
+    $mailer_endpoint = new Mailer($bridge, $settings);
     $response = $mailer_endpoint->resumeSending();
     expect($response->status)->equals(APIResponse::STATUS_OK);
     $mailer_log = MailerLog::getMailerLog();
     expect($mailer_log['status'])->null();
+  }
+
+  function testItRunsAuhtorizedEmailsCheckIfErrorIsPresent() {
+    $settings = new SettingsController();
+    $settings->set(Bridge::AUTHORIZED_EMAIL_ADDRESSES_ERROR_SETTING_NAME, ['invalid_sender_address' => 'a@b.c']);
+    $bridge = $this->makeEmpty(Bridge::class, ['checkAuthorizedEmailAddresses' => Expected::once()]);
+    $mailer_endpoint = new Mailer($bridge, $settings);
+    $mailer_endpoint->resumeSending();
   }
 }
