2a5f0cbb52857aeacaacd2bf95504b13c978bd41
Pavel Dohnal <pavel@mailpoet.com>
2019-05-14 08:57:43 +0200

    Ask for permissions step
    [MAILPOET-1856]

--- assets/js/src/wizard/revenue_tracking_permission.jsx
+++ assets/js/src/wizard/revenue_tracking_permission.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MailPoet from 'mailpoet';
+import RevenueTrackingPermissionStep from './steps/revenue_tracking_permission_step.jsx';
 
 function RevenueTrackingPermission() {
+  const [loading, setLoading] = useState(false);
+
+  const handleApiError = (response) => {
+    setLoading(false);
+    let errorMessage = MailPoet.I18n.t('unknownError');
+    if (response && response.errors && response.errors.length > 0) {
+      errorMessage = response.errors.map(error => error.message);
+    }
+    MailPoet.Notice.error(errorMessage, { scroll: true });
+  };
+
+  const updateSettings = (data) => {
+    return MailPoet.Ajax.post({
+      api_version: window.mailpoet_api_version,
+      endpoint: 'settings',
+      action: 'set',
+      data,
+    }).fail(handleApiError);
+  };
+
+  const finishWizard = () => {
+    window.location = window.finish_wizard_url;
+  };
+
+  const submit = (allowed) => {
+    setLoading(true);
+    const settings = {
+      accept_cookie_revenue_tracking: allowed ? 1 : 0,
+    };
+    updateSettings(settings).then(finishWizard);
+  };
+
   return (
     <div className="mailpoet_welcome_wizard_steps mailpoet_welcome_wizard_centered_column">
       <div className="mailpoet_welcome_wizard_header">
-        <img src={window.mailpoet_logo_url} width="200" height="87" alt="MailPoet logo"/>
+        <img src={window.mailpoet_logo_url} width="200" height="87" alt="MailPoet logo" />
       </div>
-      <h1>This is the page</h1>
+      <RevenueTrackingPermissionStep loading={loading} submitForm={submit} />
     </div>
   );
 }
