a4ae41cad527e859a3a3f3b0dc817bf33d4db0a8
Pavel Dohnal <pavel@mailpoet.com>
2019-05-15 15:09:14 +0200

    Expose help tooltip
    [PREMIUM-115]

--- webpack.config.js
+++ webpack.config.js
@@ -197,6 +197,13 @@ const baseConfig = {
         ]
       },
       {
+        include: path.resolve(__dirname, 'assets/js/src/help-tooltip.jsx'),
+        use: [
+          'expose-loader?' + globalPrefix + '.HelpTooltip',
+          'babel-loader',
+        ]
+      },
+      {
         include: path.resolve(__dirname, 'assets/js/src/newsletters/types/automatic_emails/breadcrumb.jsx'),
         use: [
           'expose-loader?' + globalPrefix + '.AutomaticEmailsBreadcrumb',
@@ -247,6 +254,7 @@ const adminConfig = {
       'react-string-replace',
       'prop-types',
       'classnames',
+      'help-tooltip.jsx',
       'form/form.jsx',
       'listing/listing.jsx',
       'newsletters/badges/stats.jsx',
