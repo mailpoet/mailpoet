9c0d39e74b3047475369f1041f2eff26fce4d678
Rostislav Wolny <rosta@mailpoet.com>
2019-05-16 16:11:29 +0200

    Add test for spacer renderer
    [MAILPOET-2086]

---
+++ tests/unit/Newsletter/Renderer/Blocks/SpacerTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace MailPoet\Newsletter\Renderer\Blocks;
+
+class SpacerTest extends \MailPoetUnitTest {
+
+  private $block = [
+    'type' => 'spacer',
+    'styles' => [
+      'block' => [
+        'backgroundColor' => 'transparent',
+        'height' => '13px',
+      ],
+    ],
+  ];
+
+  function testItRendersCorrectly() {
+    $output = Spacer::render($this->block);
+    $expected_result = '
+      <tr>
+        <td class="mailpoet_spacer" height="13" valign="top"></td>
+      </tr>';
+    expect($output)->equals($expected_result);
+  }
+
+  function testsItRendersWithBackground() {
+    $this->block['styles']['block']['backgroundColor'] = "#ffffff";
+    $output = Spacer::render($this->block);
+    $expected_result = '
+      <tr>
+        <td class="mailpoet_spacer" bgcolor="#ffffff" height="13" valign="top"></td>
+      </tr>';
+    expect($output)->equals($expected_result);
+  }
+}
