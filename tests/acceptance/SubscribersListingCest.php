eb650a95a0c1f4967d8336683276ac0eb3f5d2ea
Jan Jakeš <jan@jakes.pro>
2019-05-16 14:27:27 +0200

    Fix flaky SubscriberListingCest
    [MAILPOET-2008]

--- tests/acceptance/SubscribersListingCest.php
+++ tests/acceptance/SubscribersListingCest.php
@@ -2,15 +2,25 @@
 
 namespace MailPoet\Test\Acceptance;
 
+use MailPoet\Test\DataFactories\Subscriber;
+
+require_once __DIR__ . '/../DataFactories/Subscriber.php';
+
 class SubscribersListingCest {
 
   function subscribersListing(\AcceptanceTester $I) {
     $I->wantTo('Open subscribers listings page');
 
+    $subscriber = (new Subscriber())
+      ->withEmail('wp@example.com')
+      ->create();
+
     $I->login();
     $I->amOnMailpoetPage('Subscribers');
     $I->searchFor('wp@example.com', 2);
     $I->waitForText('wp@example.com');
+
+    $subscriber->delete();
   }
 
 }
