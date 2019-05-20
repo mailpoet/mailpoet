8be7768176cc16729a42432eaef9411080d809db
wxa <wxa@users.noreply.github.com>
2019-05-20 21:32:44 +0300

    Log plugin updates [MAILPOET-2083]

--- lib/Config/Activator.php
+++ lib/Config/Activator.php
@@ -21,7 +21,7 @@ class Activator {
 
     $populator = new Populator();
     $populator->up();
-    $this->settings->set('db_version', Env::$version);
+    $this->updateDbVersion();
 
     $caps = new Capabilities();
     $caps->setupWPCapabilities();
@@ -33,5 +33,26 @@ class Activator {
 
     $caps = new Capabilities();
     $caps->removeWPCapabilities();
+  }
+
+  function updateDbVersion() {
+    try {
+      $current_db_version = $this->settings->get('db_version');
+    } catch (\Exception $e) {
+      $current_db_version = null;
+    }
+
+    $this->settings->set('db_version', Env::$version);
+
+    // if current db version and plugin version differ, log an update
+    if (version_compare($current_db_version, Env::$version) !== 0) {
+      $updates_log = (array)$this->settings->get('updates_log', []);
+      $updates_log[] = [
+        'previous_version' => $current_db_version,
+        'new_version' => Env::$version,
+        'date' => date('Y-m-d H:i:s'),
+      ];
+      $this->settings->set('updates_log', $updates_log);
+    }
   }
 }
