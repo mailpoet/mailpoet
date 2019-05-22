6bc983cc79b23c74111045b050f4a3e0947bd982
wxa <wxa@users.noreply.github.com>
2019-05-22 14:49:02 +0300

    Use the official WP JS hooks library [MAILPOET-2081]

---
+++ assets/js/src/hooks.js
@@ -0,0 +1,8 @@
+import { createHooks } from '@wordpress/hooks';
+
+// Make sure the hooks library is available globally
+// because it is used in views (e.g. newsletter/editor.html)
+window.wp = window.wp || {};
+window.wp.hooks = window.wp.hooks || createHooks();
+
+export default window.wp.hooks;
