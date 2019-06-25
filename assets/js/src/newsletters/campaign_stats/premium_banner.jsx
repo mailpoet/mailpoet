89794e3d095007655b5f87cf8f7e636f0068ed46
Ján Mikláš <neosinner@gmail.com>
2019-06-25 14:30:26 +0200

    Create premium banner component for stats page
    [MAILPOET-2104]

---
+++ assets/js/src/newsletters/campaign_stats/premium_banner.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import MailPoet from 'mailpoet';
+
+const PremiumBanner = () => {
+  if (window.mailpoet_premium_active) {
+    return null;
+  }
+
+  let ctaButton = null;
+  if (window.mailpoet_subscribers_count <= window.mailpoet_free_premium_subscribers_limit) {
+    ctaButton = (
+      <a
+        className="button"
+        href="https://www.mailpoet.com/free-plan/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {MailPoet.I18n.t('premiumBannerCtaFree')}
+      </a>
+    );
+  } else {
+    ctaButton = (
+      <a
+        className="button"
+        href={`https://www.mailpoet.com/pricing/?subscribers=${window.mailpoet_subscribers_count}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {MailPoet.I18n.t('premiumBannerCtaPremium')}
+      </a>
+    );
+  }
+
+  return (
+    <div className="mailpoet_stats_premium_banner">
+      <h1>{MailPoet.I18n.t('premiumBannerTitle')}</h1>
+      <p>{ctaButton}</p>
+      <a href="admin.php?page=mailpoet-premium">{MailPoet.I18n.t('premiumBannerLink')}</a>
+    </div>
+  );
+};
+
+export default PremiumBanner;
