a61c4e7a45ad45139eeca4cd9af9dc327dd7f862
Amine Ben hammou <amine@mailpoet.com>
2019-06-28 18:37:25 +0100

    Add APIErrorsNotice

---
+++ assets/js/src/notices/api_errors_notice.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Notice from 'notices/notice.jsx';
+
+const APIErrorsNotice = ({ errors }) => {
+  if (errors.length < 1) return null;
+  return <Notice type="error">{errors.map(err => <p key={err.message}>{err.message}</p>)}</Notice>;
+};
+APIErrorsNotice.propTypes = {
+  errors: PropTypes.arrayOf(PropTypes.shape({
+    message: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+export default APIErrorsNotice;
