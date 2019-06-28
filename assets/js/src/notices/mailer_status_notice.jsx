146f22ccc1d11605e651e9d11a1c2d727fb09d3e
Amine Ben hammou <amine@mailpoet.com>
2019-06-28 19:04:32 +0100

    Add MailerStatusNotice

---
+++ assets/js/src/notices/mailer_status_notice.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Notice from 'notices/notice.jsx';
+
+const MailerStatusNotice = ({ error }) => {
+  if (!error || error.operation !== 'authorization') return null;
+  return <Notice type="error" timeout={false}><p>{error.error_message}</p></Notice>;
+};
+MailerStatusNotice.propTypes = {
+  error: PropTypes.shape({
+    operation: PropTypes.string,
+    error_message: PropTypes.string,
+  }),
+};
+MailerStatusNotice.defaultProps = {
+  error: null,
+};
+
+export default MailerStatusNotice;
