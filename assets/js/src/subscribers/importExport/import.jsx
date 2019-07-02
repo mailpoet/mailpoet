3101365849c22fc1b927486b09b16d1e6d20f799
Pavel Dohnal <pavel@mailpoet.com>
2019-07-02 13:23:02 +0200

    Add data manipulation step
    [MAILPOET-1809]

--- assets/js/src/subscribers/importExport/import.jsx
+++ assets/js/src/subscribers/importExport/import.jsx
@@ -6,6 +6,7 @@ import {
 
 import StepMethodSelection from './import/step_method_selection.jsx';
 import StepInputValidation from './import/step_input_validation.jsx';
+import StepDataManipulation from './import/step_data_manipulation.jsx';
 
 const container = document.getElementById('import_container');
 
@@ -16,6 +17,7 @@ const ImportSubscribers = () => {
       <Switch>
         <Route path="/step_method_selection" render={props => <StepMethodSelection {...props} setStepMethodSelection={setStepMethodSelection} />} />
         <Route path="/step_input_validation" render={props => <StepInputValidation {...props} stepMethodSelection={stepMethodSelection} />} />
+        <Route path="/step_data_manipulation" render={props => <StepDataManipulation {...props} stepMethodSelection={stepMethodSelection} />} />
         <Route path="*" render={() => <Redirect to="/step_method_selection" />} />
       </Switch>
     </HashRouter>
