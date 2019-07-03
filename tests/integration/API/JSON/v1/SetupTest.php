5a8f400d4f2e6dbf55a7c2b3a79363c9ee0516db
wxa <wxa@users.noreply.github.com>
2019-07-03 12:59:06 +0300

    Update the captcha setting [MAILPOET-2015]

--- tests/integration/API/JSON/v1/SetupTest.php
+++ tests/integration/API/JSON/v1/SetupTest.php
@@ -8,6 +8,7 @@ use MailPoet\WP\Functions as WPFunctions;
 use Helper\WordPressHooks as WPHooksHelper;
 use MailPoet\API\JSON\Response as APIResponse;
 use MailPoet\Settings\SettingsController;
+use MailPoet\Subscription\Captcha;
 
 class SetupTest extends \MailPoetTest {
   function _before() {
@@ -28,6 +29,13 @@ class SetupTest extends \MailPoetTest {
     $settings = new SettingsController();
     $signup_confirmation = $settings->fetch('signup_confirmation.enabled');
     expect($signup_confirmation)->true();
+
+    $captcha = $settings->fetch('captcha');
+    $subscription_captcha = new Captcha;
+    $captcha_type = $subscription_captcha->isSupported() ? Captcha::TYPE_BUILTIN : Captcha::TYPE_DISABLED;
+    expect($captcha['type'])->equals($captcha_type);
+    expect($captcha['recaptcha_site_token'])->equals('');
+    expect($captcha['recaptcha_secret_token'])->equals('');
 
     $woocommerce_optin_on_checkout = $settings->fetch('woocommerce.optin_on_checkout');
     expect($woocommerce_optin_on_checkout['enabled'])->true();
