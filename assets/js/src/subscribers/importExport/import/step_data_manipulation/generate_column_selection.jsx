72fa226cee147380a893d3a33014ad3e7d19d9bb
Pavel Dohnal <pavel@mailpoet.com>
2019-07-03 14:05:59 +0200

    Add column selection to data table
    [MAILPOET-1809]

---
+++ assets/js/src/subscribers/importExport/import/step_data_manipulation/generate_column_selection.jsx
@@ -0,0 +1,120 @@
+import jQuery from 'jquery';
+import MailPoet from 'mailpoet';
+
+export default (selectColumnType) => {
+  jQuery('select.mailpoet_subscribers_column_data_match')
+    .select2({
+      data: window.mailpoetColumnsSelect2,
+      width: '15em',
+      templateResult(item) {
+        return item.name;
+      },
+      templateSelection(item) {
+        return item.name;
+      },
+    })
+    .on('select2:selecting', (selectEvent) => {
+      const selectElement = selectEvent.currentTarget;
+      const selectedOptionId = selectEvent.params.args.data.id;
+      // CREATE CUSTOM FIELD
+      if (selectedOptionId === 'create') {
+        selectEvent.preventDefault();
+        jQuery(selectElement).select2('close');
+        MailPoet.Modal.popup({
+          title: MailPoet.I18n.t('addNewField'),
+          template: jQuery('#form_template_field_form').html(),
+        });
+        jQuery('#form_field_new').parsley().on('form:submit', () => {
+          // get data
+          const data = jQuery('#form_field_new').mailpoetSerializeObject();
+
+          // save custom field
+          MailPoet.Ajax.post({
+            api_version: window.mailpoet_api_version,
+            endpoint: 'customFields',
+            action: 'save',
+            data,
+          }).done((response) => {
+            const newColumnData = {
+              id: response.data.id,
+              name: response.data.name,
+              type: response.data.type,
+              params: response.data.params,
+              custom: true,
+            };
+            // if this is the first custom column, create an "optgroup"
+            if (window.mailpoetColumnsSelect2.length === 2) {
+              window.mailpoetColumnsSelect2.push({
+                name: MailPoet.I18n.t('userColumns'),
+                children: [],
+              });
+            }
+            window.mailpoetColumnsSelect2[2].children.push(newColumnData);
+            window.mailpoetColumns.push(newColumnData);
+            jQuery('select.mailpoet_subscribers_column_data_match')
+              .each(() => {
+                jQuery(selectElement)
+                  .html('')
+                  .select2('destroy')
+                  .select2({
+                    data: window.mailpoetColumnsSelect2,
+                    width: '15em',
+                    templateResult(item) {
+                      return item.name;
+                    },
+                    templateSelection(item) {
+                      return item.name;
+                    },
+                  });
+              });
+            jQuery(selectElement).data('column-id', newColumnData.id);
+            jQuery(selectElement).data('validation-rule', false);
+            const columnIndex = jQuery(selectElement).data('column-index');
+            selectColumnType(newColumnData.id, columnIndex);
+            // close popup
+            MailPoet.Modal.close();
+          }).fail((response) => {
+            if (response.errors.length > 0) {
+              MailPoet.Notice.error(
+                response.errors.map(error => error.message),
+                { positionAfter: '#field_name' }
+              );
+            }
+          });
+          return false;
+        });
+      } else {
+        // CHANGE COLUMN
+        // check for duplicate values in all select options
+        jQuery('select.mailpoet_subscribers_column_data_match')
+          .each(() => {
+            const element = selectElement;
+            const elementId = jQuery(element).val();
+            // if another column has the same value and it's not an 'ignore',
+            // prompt user
+            if (elementId === selectedOptionId
+              && elementId !== 'ignore') {
+              if (confirm(`${MailPoet.I18n.t('selectedValueAlreadyMatched')} ${MailPoet.I18n.t('confirmCorrespondingColumn')}`)) { // eslint-disable-line
+                jQuery(element).data('column-id', 'ignore');
+              } else {
+                selectEvent.preventDefault();
+                jQuery(selectElement).select2('close');
+              }
+            }
+          });
+      }
+    })
+    .on('select2:select', (selectEvent) => {
+      const selectElement = selectEvent.currentTarget;
+      const selectedOptionId = selectEvent.params.data.id;
+      jQuery(selectElement).data('column-id', selectedOptionId);
+      const columnIndex = jQuery(selectElement).data('column-index');
+      selectColumnType(selectedOptionId, columnIndex);
+    });
+  jQuery.map(
+    jQuery('.mailpoet_subscribers_column_data_match'), (element) => {
+      const columnId = jQuery(element).data('column-id');
+      jQuery(element).val(columnId).trigger('change');
+    }
+  );
+};
--- assets/js/src/subscribers/importExport/import/step_data_manipulation/match_table.jsx
+++ assets/js/src/subscribers/importExport/import/step_data_manipulation/match_table.jsx
@@ -1,106 +1,120 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import PropTypes from 'prop-types';
 import MailPoet from 'mailpoet';
+import _ from 'underscore';
 
+import generateColumnSelection from './generate_column_selection.jsx';
 import matchColumns from './match_columns.jsx';
 
 const MAX_SUBSCRIBERS_SHOWN = 10;
-
-function ColumnDataMatch({ columnTypes }) {
-  return (
-    <tr>
-      <th>{MailPoet.I18n.t('matchData')}</th>
-      {/* eslint-disable-next-line react/no-array-index-key */}
-      {columnTypes.map((columnType, i) => <th key={columnType.column_id + i}>{columnType.column_id}</th>)}
-    </tr>
-  );
-}
-
-ColumnDataMatch.propTypes = {
-  columnTypes: PropTypes.arrayOf(PropTypes.shape({ column_id: PropTypes.string })).isRequired,
-};
-
-function Header({ header }) {
-  return (
-    <tr className="mailpoet_header">
-      <td />
-      {header.map(headerName => <td key={headerName}>{headerName}</td>)}
-    </tr>
-  );
-}
-
-Header.propTypes = {
-  header: PropTypes.arrayOf(PropTypes.string).isRequired,
-};
-
-function Subscriber({ subscriber, index }) {
-  return (
-    <>
-      <td>{index}</td>
-      {/* eslint-disable-next-line react/no-array-index-key */}
-      {subscriber.map((field, i) => <td key={field + index + i}>{field}</td>)}
-    </>
-  );
-}
-
-Subscriber.propTypes = {
-  subscriber: PropTypes.arrayOf(PropTypes.string).isRequired,
-  index: PropTypes.node.isRequired,
-};
-
-function Subscribers({ subscribers, subscribersCount }) {
-  const filler = '. . .';
-  const fillerArray = Array(subscribers[0].length).fill(filler);
-  return (
-    <>
-      {
-        subscribers
-          .slice(0, MAX_SUBSCRIBERS_SHOWN)
-          .map((subscriber, i) => (
-            <tr key={subscriber[0]}>
-              <Subscriber subscriber={subscriber} index={i + 1} />
-            </tr>
-          ))
-      }
-      {
-        subscribersCount > MAX_SUBSCRIBERS_SHOWN + 1
-          ? <tr key="filler"><Subscriber subscriber={fillerArray} index={filler} /></tr>
-          : null
-      }
-      {
-        subscribersCount > MAX_SUBSCRIBERS_SHOWN
-          ? (
-            <tr key={subscribers[subscribersCount - 1][0]}>
-              <Subscriber subscriber={subscribers[subscribersCount - 1]} index={subscribersCount} />
-            </tr>
-          )
-          : null
-      }
-    </>
-  );
-}
-
-Subscribers.propTypes = {
-  subscribersCount: PropTypes.number.isRequired,
-  subscribers: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)).isRequired,
-};
 
 function MatchTable({
   subscribersCount,
   subscribers,
   header,
 }) {
-  const matchedColumnTypes = matchColumns(subscribers, header);
+  let selectedColumns = [];
+
+  useLayoutEffect(() => {
+    generateColumnSelection((selectedOptionId, columnIndex) => {
+      selectedColumns[columnIndex] = selectedOptionId;
+    });
+  });
+
+  function ColumnDataMatch() {
+    const matchedColumnTypes = matchColumns(subscribers, header);
+    selectedColumns = _.pluck(matchedColumnTypes, 'column_id');
+    return (
+      <tr>
+        <th>{MailPoet.I18n.t('matchData')}</th>
+        {
+          matchedColumnTypes.map((columnType, i) => {
+            return (
+              // eslint-disable-next-line react/no-array-index-key
+              <th key={columnType.column_id + i}>
+                <select
+                  className="mailpoet_subscribers_column_data_match"
+                  data-column-id={columnType.column_id}
+                  data-validation-rule="false"
+                  data-column-index={i}
+                  id={`column_${i}`}
+                />
+              </th>
+            );
+          })
+        }
+      </tr>
+    );
+  }
+
+  function Header() {
+    return (
+      <tr className="mailpoet_header">
+        <td />
+        {header.map(headerName => <td key={headerName}>{headerName}</td>)}
+      </tr>
+    );
+  }
+
+  function Subscriber({ subscriber, index }) {
+    return (
+      <>
+        <td>{index}</td>
+        {/* eslint-disable-next-line react/no-array-index-key */}
+        {subscriber.map((field, i) => <td key={field + index + i}>{field}</td>)}
+      </>
+    );
+  }
+
+  Subscriber.propTypes = {
+    subscriber: PropTypes.arrayOf(PropTypes.string).isRequired,
+    index: PropTypes.node.isRequired,
+  };
+
+  function Subscribers() {
+    const filler = '. . .';
+    const fillerArray = Array(subscribers[0].length).fill(filler);
+    return (
+      <>
+        {
+          subscribers
+            .slice(0, MAX_SUBSCRIBERS_SHOWN)
+            .map((subscriber, i) => (
+              <tr key={subscriber[0]}>
+                <Subscriber subscriber={subscriber} index={i + 1} />
+              </tr>
+            ))
+        }
+        {
+          subscribersCount > MAX_SUBSCRIBERS_SHOWN + 1
+            ? <tr key="filler"><Subscriber subscriber={fillerArray} index={filler} /></tr>
+            : null
+        }
+        {
+          subscribersCount > MAX_SUBSCRIBERS_SHOWN
+            ? (
+              <tr key={subscribers[subscribersCount - 1][0]}>
+                <Subscriber
+                  subscriber={subscribers[subscribersCount - 1]}
+                  index={subscribersCount}
+                />
+              </tr>
+            )
+            : null
+        }
+      </>
+    );
+  }
 
   return (
     <div className="subscribers_data">
       <table className="mailpoet_subscribers widefat fixed">
         <thead>
-          <ColumnDataMatch columnTypes={matchedColumnTypes} />
+          <ColumnDataMatch />
         </thead>
         <tbody>
-          <Header header={header} />
-          <Subscribers subscribers={subscribers} subscribersCount={subscribersCount} />
+          <Header />
+          <Subscribers />
         </tbody>
       </table>
     </div>
