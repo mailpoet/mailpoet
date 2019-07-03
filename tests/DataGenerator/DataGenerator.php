358bcc5d38f2344168d014cf7a279ebce64726b4
Rostislav Wolny <rosta@mailpoet.com>
2019-07-03 14:27:02 +0200

    Add runtime for data generators
    [MAILPOET-2140]

---
+++ tests/DataGenerator/DataGenerator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace MailPoet\Test\DataGenerator;
+
+use Codeception\Lib\Console\Output;
+use MailPoet\Test\DataGenerator\Generators\GeneratorHelper;
+use MailPoet\Test\DataGenerator\Generators\WooCommercePastRevenues;
+
+class DataGenerator {
+
+  const PAST_REVENUES_GENERATOR = 'past_revenues';
+
+  /** @var Output */
+  private $console;
+
+  function __construct(Output $console) {
+    $this->console = $console;
+  }
+
+  function run($generator_name) {
+    $timer = time();
+    try {
+      $generator = $this->createGenerator($generator_name);
+      foreach ($generator->generate() as $message) {
+        $this->log($timer, $message);
+      }
+    } catch (\Exception $e) {
+      $this->console->exception($e);
+    }
+    $this->log($timer, 'DONE!');
+  }
+
+  private function createGenerator($generator_name) {
+    switch ($generator_name) {
+      default:
+        throw new \Exception("Missing or unknown generator name. Possible values: \n " . self::PAST_REVENUES_GENERATOR);
+    }
+  }
+
+  private function log($timer, $message) {
+    $duration = time() - $timer;
+    $this->console->message("[{$duration}s] $message")->writeln();
+  }
+}
---
+++ tests/DataGenerator/_bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+// Turn off transaction emails by defining dummy wp_mail
+if (!function_exists('wp_mail')) {
+  function wp_mail($to, $subject, $message, $headers = '', $attachments = []) {
+    return true;
+  }
+}
+
+// Load WP
+require_once(getenv('WP_ROOT') . '/wp-load.php');
