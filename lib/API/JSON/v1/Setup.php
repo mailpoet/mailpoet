80d0e7e0f74062edcb72631af793e8bfc2d3b963
Jan Jakeš <jan@jakes.pro>
2019-07-05 17:42:56 +0200

    Use DI for Activator in Setup API
    [MAILPOET-2014]

--- lib/API/JSON/v1/Setup.php
+++ lib/API/JSON/v1/Setup.php
@@ -6,29 +6,29 @@ use MailPoet\API\JSON\Endpoint as APIEndpoint;
 use MailPoet\WP\Functions as WPFunctions;
 use MailPoet\Config\AccessControl;
 use MailPoet\Config\Activator;
-use MailPoet\Config\Populator;
-use MailPoet\Settings\SettingsController;
-use MailPoet\Subscription\Captcha;
 
 if (!defined('ABSPATH')) exit;
 
 class Setup extends APIEndpoint {
-  private $wp;
   public $permissions = [
     'global' => AccessControl::PERMISSION_MANAGE_SETTINGS,
   ];
 
-  function __construct(WPFunctions $wp) {
+  /** @var WPFunctions */
+  private $wp;
+
+  /** @var Activator */
+  private $activator;
+
+  function __construct(WPFunctions $wp, Activator $activator) {
     $this->wp = $wp;
+    $this->activator = $activator;
   }
 
   function reset() {
     try {
-      $settings = new SettingsController();
-      $captcha = new Captcha();
-      $activator = new Activator($settings, new Populator($settings, $this->wp, $captcha));
-      $activator->deactivate();
-      $activator->activate();
+      $this->activator->deactivate();
+      $this->activator->activate();
       $this->wp->doAction('mailpoet_setup_reset');
       return $this->successResponse();
     } catch (\Exception $e) {
