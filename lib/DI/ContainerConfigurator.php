f644b3e010e081debd2a61617ad61061105d4e3a
Rostislav Wolny <rosta@mailpoet.com>
2019-07-15 16:46:54 +0200

    Add Subscribers page class
    [MAILPOET-2200]

--- lib/DI/ContainerConfigurator.php
+++ lib/DI/ContainerConfigurator.php
@@ -36,6 +36,7 @@ class ContainerConfigurator implements IContainerConfigurator {
     $container->autowire(\MailPoet\AdminPages\Pages\MP2Migration::class);
     $container->autowire(\MailPoet\AdminPages\Pages\RevenueTrackingPermission::class);
     $container->autowire(\MailPoet\AdminPages\Pages\Settings::class);
+    $container->autowire(\MailPoet\AdminPages\Pages\Subscribers::class);
     $container->autowire(\MailPoet\AdminPages\Pages\Update::class);
     $container->autowire(\MailPoet\AdminPages\Pages\WelcomeWizard::class);
     $container->autowire(\MailPoet\AdminPages\Pages\WooCommerceListImport::class);
