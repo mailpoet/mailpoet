793b31e597f459dda85a9aa84ff442a7cb737813
Pavel Dohnal <pavel@mailpoet.com>
2019-07-22 13:19:50 +0200

    Remove all contents from outdated import step
    [MAILPOET-2129]

--- assets/js/src/subscribers/importExport/import/step_input_validation.jsx
+++ assets/js/src/subscribers/importExport/import/step_input_validation.jsx
@@ -1,130 +1,30 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import ReactStringReplace from 'react-string-replace';
-import MailPoet from 'mailpoet';
 import PreviousNextStepButtons from './previous_next_step_buttons.jsx';
 
-const ServicesMessage = () => {
-  let message = ReactStringReplace(MailPoet.I18n.t('useServices'), '%1$s', () => (
-    <a
-      href="https://www.briteverify.com"
-      target="_blank"
-      rel="noreferrer noopener"
-      key="BriteVerify"
-    >
-      BriteVerify
-    </a>
-  ));
-  message = ReactStringReplace(message, '%2$s', () => (
-    <a
-      href="https://www.datavalidation.com/"
-      target="_blank"
-      rel="noreferrer noopener"
-      key="DataValidation"
-    >
-      DataValidation
-    </a>
-  ));
-  message = ReactStringReplace(message, '%3$s', () => (
-    <a
-      href="https://neverbounce.com/"
-      target="_blank"
-      rel="noreferrer noopener"
-      key="NeverBounce"
-    >
-      NeverBounce
-    </a>
-  ));
-  return message;
-};
+function StepInputValidation({ stepMethodSelectionData, history }) {
+  useEffect(
+    () => {
+      if (typeof (stepMethodSelectionData) === 'undefined') {
+        history.replace('step_method_selection');
+      }
+    },
+    [stepMethodSelectionData, history],
+  );
 
-class StepInputValidation extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      subscribersAgreed: false,
-      sentOnceLastYear: false,
-      understand: false,
-    };
+  function isFormValid() {
+    return false;
   }
-
-  componentDidMount() {
-    if (typeof (this.props.stepMethodSelectionData) === 'undefined') {
-      this.props.history.replace('step_method_selection');
-    }
-  }
-
-  isFormValid() {
-    return this.state.subscribersAgreed
-      && this.state.sentOnceLastYear
-      && this.state.understand;
-  }
-
-  render() {
-    return (
-      <div className="mailpoet_import_validation_step">
-        <label htmlFor="subscribers_agreed">
-          <input
-            type="checkbox"
-            checked={this.state.subscribersAgreed}
-            onChange={e => this.setState({ subscribersAgreed: e.target.checked })}
-            name="subscribers_agreed"
-            id="subscribers_agreed"
-            data-automation-id="subscribers_agreed"
-          />
-          {MailPoet.I18n.t('subscribersAgreed')}
-        </label>
-        <p className="description">
-          {MailPoet.I18n.t('dontEmailSubscribers')}
-          {' '}
-          <a
-            href="https://kb.mailpoet.com/article/127-checklist-before-importing-subscribers"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            {MailPoet.I18n.t('readSupportArticle')}
-          </a>
-        </p>
-        <label htmlFor="sent_once_last_year">
-          <input
-            type="checkbox"
-            checked={this.state.sentOnceLastYear}
-            onChange={e => this.setState({ sentOnceLastYear: e.target.checked })}
-            name="sent_once_last_year"
-            id="sent_once_last_year"
-            data-automation-id="sent_once_last_year"
-          />
-          {MailPoet.I18n.t('sentOnceYear')}
-        </label>
-        <p className="description">
-          {MailPoet.I18n.t('emailAddressesWillBounce')}
-        </p>
-        <p className="description">
-          <ServicesMessage />
-        </p>
-        <label htmlFor="understand">
-          <input
-            type="checkbox"
-            checked={this.state.understand}
-            onChange={e => this.setState({ understand: e.target.checked })}
-            name="understand"
-            id="understand"
-            data-automation-id="input_understand"
-          />
-          {MailPoet.I18n.t('youUnderstand')}
-        </label>
-        <p className="description">
-          {MailPoet.I18n.t('weWillSuspend')}
-        </p>
-        <PreviousNextStepButtons
-          canGoNext={this.isFormValid()}
-          onPreviousAction={() => this.props.history.push('step_method_selection')}
-          onNextAction={() => this.props.history.push('step_data_manipulation')}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="mailpoet_import_validation_step">
+      <PreviousNextStepButtons
+        canGoNext={isFormValid()}
+        onPreviousAction={() => history.push('step_method_selection')}
+        onNextAction={() => history.push('step_data_manipulation')}
+      />
+    </div>
+  );
 }
 
 StepInputValidation.propTypes = {
