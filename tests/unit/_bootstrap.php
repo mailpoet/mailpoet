38cf82d203fb8cc19317cef3aa8f8c1b46b98cdd
Rostislav Wolny <rosta@mailpoet.com>
2019-07-24 13:56:03 +0200

    Add fix to unit tests to enable mocking WPFunctions
    [MAILPOET-2203]

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -4,6 +4,12 @@ function __($text) {
   return $text;
 }
 
+// Fix for mocking WPFunctions
+// [PHPUnit_Framework_Exception] Use of undefined constant OBJECT - assumed 'OBJECT' (this will throw an Error in a future version of PHP)
+if (!defined('OBJECT')) {
+  define( 'OBJECT', 'OBJECT' );
+}
+
 define('ABSPATH', '/');
 
 $console = new \Codeception\Lib\Console\Output([]);
