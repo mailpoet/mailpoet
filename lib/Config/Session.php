98ecb2d2ee9d232f406220270ac43c173fb9fe8c
Rostislav Wolny <rosta@mailpoet.com>
2019-08-02 18:31:28 +0200

    Replace PHP Session with minimalist MailPoet session
    [MAILPOET-2248]

--- lib/Config/Session.php
+++ lib/Config/Session.php
@@ -2,12 +2,39 @@
 
 namespace MailPoet\Config;
 
-if (!defined('ABSPATH')) exit;
+use MailPoet\Util\Security;
 
-class Session {
+class Session
+{
+  const COOKIE_NAME = 'MAILPOET_SESSION';
+  const KEY_LENGTH = 32;
+  const COOKIE_EXPIRATION = 84600; // day
+
+  function getId() {
+    return isset($_COOKIE[self::COOKIE_NAME]) ? $_COOKIE[self::COOKIE_NAME] : null;
+  }
+
   function init() {
-    if (!session_id()) {
-      return session_start();
+    $id = $this->getId() ?: Security::generateRandomString(self::KEY_LENGTH);
+    if (!headers_sent()) {
+      $this->setCookie($id);
     }
   }
+
+  function destroy() {
+    if ($this->getId() === null) {
+      return;
+    }
+    unset($_COOKIE[self::COOKIE_NAME]);
+  }
+
+  private function setCookie($id) {
+    setcookie(
+      self::COOKIE_NAME,
+      $id,
+      time() + self::COOKIE_EXPIRATION,
+      "/"
+    );
+  }
 }
+
