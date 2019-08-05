6a494a2faea826630d6da9997c4379637ddbb11f
Rostislav Wolny <rosta@mailpoet.com>
2019-08-05 08:53:53 +0200

    Introduce captcha session service
    [MAILPOET-2248]

--- lib/DI/ContainerConfigurator.php
+++ lib/DI/ContainerConfigurator.php
@@ -163,6 +163,7 @@ class ContainerConfigurator implements IContainerConfigurator {
     // Subscription
     $container->autowire(\MailPoet\Subscription\Captcha::class)->setPublic(true);
     $container->autowire(\MailPoet\Subscription\CaptchaRenderer::class)->setPublic(true);
+    $container->autowire(\MailPoet\Subscription\CaptchaSession::class);
     $container->autowire(\MailPoet\Subscription\Comment::class)->setPublic(true);
     $container->autowire(\MailPoet\Subscription\Form::class)->setPublic(true);
     $container->autowire(\MailPoet\Subscription\Manage::class)->setPublic(true);
