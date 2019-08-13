013a49318e2e1f4e7e93bc0de27aec7567088c22
Rostislav Wolny <rosta@mailpoet.com>
2019-08-13 13:16:54 +0200

    Use JS URL api to manipulate URL in referral id decorator
    [MAILPOET-2182]

--- assets/js/src/referral_url_decorator.jsx
+++ assets/js/src/referral_url_decorator.jsx
@@ -2,15 +2,9 @@ const addReferralId = (url) => {
   if (!window.mailpoet_referral_id) {
     return url;
   }
-  const parser = document.createElement('a');
-  parser.href = url;
-  parser.search += parser.search ? '&' : '?';
-  parser.search += `ref=${encodeURIComponent(window.mailpoet_referral_id)}`;
-  return parser.toString();
-
-  // const refUrl = new URL(url);
-  // refUrl.searchParams.set('ref', window.mailpoet_referral_id);
-  // return refUrl.toString();
+  const refUrl = new URL(url);
+  refUrl.searchParams.set('ref', window.mailpoet_referral_id);
+  return refUrl.toString();
 };
 
 export default addReferralId;
