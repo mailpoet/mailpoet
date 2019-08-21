ae04953d1f8dd9c3e0fb97bfa5fcda7d90c895e4
Pavel Dohnal <pavel@mailpoet.com>
2019-08-21 08:57:43 +0200

    Create function createOrUpdate
    [MAILPOET-2219]

--- lib/Features/FeatureFlagsRepository.php
+++ lib/Features/FeatureFlagsRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MailPoet\Settings;
+namespace MailPoet\Features;
 
 use MailPoet\Entities\FeatureFlagEntity;
 use MailPoet\Doctrine\Repository;
@@ -14,5 +14,34 @@ use MailPoet\Doctrine\Repository;
 class FeatureFlagsRepository extends Repository {
   protected function getEntityClassName() {
     return FeatureFlagEntity::class;
+  }
+
+  /**
+   * @param array $data
+   * @throws \RuntimeException
+   * @throws \InvalidArgumentException
+   * @return FeatureFlagEntity
+   */
+  public function createOrUpdate(array $data = []) {
+    if (!$data['name']) {
+      throw new \InvalidArgumentException('Missing name');
+    }
+    $feature_flag = $this->findOneBy([
+      'name' => $data['name'],
+    ]);
+    if (!$feature_flag) {
+      $feature_flag = new FeatureFlagEntity($data['name']);
+      $this->persist($feature_flag);
+    }
+    if (array_key_exists('value', $data)) {
+      $feature_flag->setValue($data['value']);
+    }
+
+    try {
+      $this->flush();
+    } catch (\Exception $e) {
+      throw new \RuntimeException("Error when saving feature " . $data['name']);
+    }
+    return $feature_flag;
   }
 }
