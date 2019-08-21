25329cd571c83f920c5f66fa813ca653b0513349
Jan Jakeš <jan@jakes.pro>
2019-08-21 16:30:15 +0200

    Remove forms table from acceptance dump
    [MAILPOET-2209]

--- tests/acceptance/FormsListingCest.php
+++ tests/acceptance/FormsListingCest.php
@@ -2,15 +2,21 @@
 
 namespace MailPoet\Test\Acceptance;
 
+use MailPoet\Test\DataFactories\Form;
+
 class FormsListingCest {
 
   function formsListing(\AcceptanceTester $I) {
+    $form = new Form();
+    $form->withName('Test Form');
+    $form->create();
+
     $I->wantTo('Open forms listings page');
 
     $I->login();
     $I->amOnMailpoetPage('Forms');
 
-    $I->waitForText('Test Form', 5, '[data-automation-id="listing_item_1"]');
+    $I->waitForText('Test Form', 5, '.mailpoet_listing_table');
     $I->seeNoJSErrors();
   }
 
