8666bbc7cf76b84e6aea24421ccefd66d7615c1a
Jan Jakeš <jan@jakes.pro>
2019-08-22 11:09:34 +0200

    Add extension to associate errors with tests
    [MAILPOET-2209]

---
+++ tests/_support/ErrorsExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+use Codeception\Event\TestEvent;
+use Codeception\Events;
+use Codeception\Extension;
+
+class ErrorsExtension extends Extension {
+  const ERROR_LOG_PATH = __DIR__ . '/../_output/exceptions/error.log';
+
+  private $errors = [];
+
+  static $events = [
+    Events::TEST_BEFORE => 'checkErrorsBeforeTests',
+    Events::TEST_AFTER => 'checkErrorsAfterTests',
+  ];
+
+  function checkErrorsBeforeTests(TestEvent $e) {
+    if (!file_exists(self::ERROR_LOG_PATH)) {
+      return;
+    }
+    $this->errors = file(self::ERROR_LOG_PATH, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+  }
+
+  function checkErrorsAfterTests(TestEvent $e) {
+    if (!file_exists(self::ERROR_LOG_PATH)) {
+      return;
+    }
+
+    $errors = file(self::ERROR_LOG_PATH, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    if (count($this->errors) === count($errors)) {
+      return;
+    }
+
+    foreach (array_slice($errors, count($this->errors)) as $error) {
+      $this->output->writeln("<error>$error</error>");
+    }
+  }
+}
