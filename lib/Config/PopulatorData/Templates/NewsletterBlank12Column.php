5954b80ae6cacdf29731714258617125d42decb8
Rami Yushuvaev <r_a_m_i@hotmail.com>
2019-08-27 18:52:24 +0300

    Update

--- lib/Config/PopulatorData/Templates/NewsletterBlank12Column.php
+++ lib/Config/PopulatorData/Templates/NewsletterBlank12Column.php
@@ -163,7 +163,7 @@ class NewsletterBlank12Column {
                 "blocks" => [
                   [
                     "type" => "text",
-                    "text" => WPFunctions::get()->'<h2>'.__('This template has...', 'mailpoet').'</h2>',
+                    "text" => '<h2>' . WPFunctions::get()->__('This template has...', 'mailpoet') . '</h2>',
                   ],
                   [
                     "type" => "text",
@@ -182,7 +182,7 @@ class NewsletterBlank12Column {
                 "blocks" => [
                   [
                     "type" => "text",
-                    "text" => WPFunctions::get()->'<h2>'.__('... a 2-column layout.', 'mailpoet').'</h2>',
+                    "text" => '<h2>' . WPFunctions::get()->__('... a 2-column layout.', 'mailpoet') . '</h2>',
                   ],
                   [
                     "type" => "text",
