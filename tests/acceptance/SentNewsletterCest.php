f61f6c248d54fec825f65708de166362174aeb57
Ján Mikláš <neosinner@gmail.com>
2019-08-28 14:00:53 +0200

    Add acceptance test for disabled Next button for sent newsletter
    [MAILPOET-2195]

---
+++ tests/acceptance/SentNewsletterCest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MailPoet\Test\Acceptance;
+
+use MailPoet\Test\DataFactories\Newsletter;
+
+class SentNewsletterCest {
+
+  function disableLastStep(\AcceptanceTester $I) {
+    $I->wantTo('See that last step is disabled fot sent standard email');
+
+    $newsletter = (new Newsletter())
+      ->withSentStatus()
+      ->withSubject('Sent newsletter')
+      ->create();
+
+    $I->login();
+    $I->amEditingNewsletter($newsletter->id);
+    $I->waitForElement('.mailpoet_save_next.button-disabled');
+    $I->see('This email has already been sent.');
+    $I->see('It can be edited, but not sent again.');
+    $I->see('Duplicate this email if you want to send it again.');
+  }
+
+}
