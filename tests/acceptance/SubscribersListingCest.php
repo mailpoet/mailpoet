a4c42c797372e16948402dd79624ff4c7fd51cd4
Ján Mikláš <neosinner@gmail.com>
2019-09-03 11:16:02 +0200

    Add acceptance test for resending email
    [MAILPOET-2316]

--- tests/acceptance/SubscribersListingCest.php
+++ tests/acceptance/SubscribersListingCest.php
@@ -2,6 +2,7 @@
 
 namespace MailPoet\Test\Acceptance;
 
+use MailPoet\Subscribers\ConfirmationEmailMailer;
 use MailPoet\Test\DataFactories\Subscriber;
 
 class SubscribersListingCest {
@@ -17,6 +18,37 @@ class SubscribersListingCest {
     $I->amOnMailpoetPage('Subscribers');
     $I->searchFor('wp@example.com');
     $I->waitForText('wp@example.com');
+  }
+
+  function sendConfirmationEmail(\AcceptanceTester $I) {
+    $I->wantTo('Send confirmation email');
+
+    $disallowed_email = 'disallowed@example.com';
+    $allowed_email = 'allowed@example.com';
+
+    $subscriber_resend_disallowed = (new Subscriber())
+      ->withEmail($disallowed_email)
+      ->withStatus('unconfirmed')
+      ->withCountConfirmations(ConfirmationEmailMailer::MAX_CONFIRMATION_EMAILS)
+      ->create();
+
+    $subscriber_resend_allowed = (new Subscriber())
+      ->withEmail($allowed_email)
+      ->withStatus('unconfirmed')
+      ->withCountConfirmations(0)
+      ->create();
+
+    $I->login();
+    $I->amOnMailpoetPage('Subscribers');
+
+    $I->moveMouseOver(['xpath' => '//*[text()="' . $disallowed_email . '"]//ancestor::tr']);
+    $I->dontSee('Resend confirmation email', '//*[text()="' . $disallowed_email . '"]//ancestor::tr');
+
+    $I->clickItemRowActionByItemName($allowed_email, 'Resend confirmation email');
+    $I->waitForText('1 confirmation email has been sent.');
+
+    $I->amOnMailboxAppPage();
+    $I->waitForText('Confirm your subscription');
   }
 
 }
