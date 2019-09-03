00ee90075afa88f0e0b11ef70ed80c3d0fc4e9f3
Amine Ben hammou <amine@mailpoet.com>
2019-09-03 17:16:18 +0100

    Add Cron task to generate link_tokens for existing subscribers

---
+++ lib/Cron/Workers/SubscriberLinkTokens.php
@@ -0,0 +1,34 @@
+<?php
+namespace MailPoet\Cron\Workers;
+
+use MailPoet\Cron\CronHelper;
+use MailPoet\Models\Subscriber;
+use MailPoet\Models\ScheduledTask;
+
+if (!defined('ABSPATH')) exit;
+
+class SubscriberLinkTokens extends SimpleWorker {
+  const TASK_TYPE = 'subscriber_link_tokens';
+  const BATCH_SIZE = 1000;
+
+  function processTaskStrategy(ScheduledTask $task) {
+    $count = $this->addTokens();
+    while ($count === self::BATCH_SIZE) {
+      CronHelper::enforceExecutionLimit($this->timer);
+      $count = $this->addTokens();
+    };
+    if ($count > 0) {
+      self::schedule();
+    }
+    return true;
+  }
+
+  public function addTokens() {
+    $instances = Subscriber::whereNull('link_token')->limit(self::BATCH_SIZE)->findMany();
+    foreach ($instances as $instance) {
+      $instance->set('link_token', Subscriber::generateToken($instance->email));
+      $instance->save();
+    }
+    return count($instances);
+  }
+}
