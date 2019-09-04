72c62ba937b866b2e99fae7e8c5b42b83542dedf
Pavel Dohnal <pavel@mailpoet.com>
2019-09-04 13:32:35 +0200

    Hide feature behind feature flag
    [MAILPOET-2307]

--- lib/Mailer/WordPress/WordpressMailerReplacer.php
+++ lib/Mailer/WordPress/WordpressMailerReplacer.php
@@ -2,12 +2,24 @@
 
 namespace MailPoet\Mailer\WordPress;
 
+use MailPoet\Features\FeaturesController;
+
 class WordpressMailerReplacer {
-  
+
+  /** @var FeaturesController */
+  private $features_controller;
+
+  function __construct(FeaturesController $features_controller) {
+    $this->features_controller = $features_controller;
+  }
+
   public function replaceWordPressMailer() {
     global $phpmailer;
 
-    return $this->replaceWithCustomPhpMailer($phpmailer);
+    if ($this->features_controller->isSupported(FeaturesController::SEND_WORDPRESS_MAILS_WITH_MP3)) {
+      return $this->replaceWithCustomPhpMailer($phpmailer);
+    }
+    return $phpmailer;
   }
 
   private function replaceWithCustomPhpMailer(&$obj = null) {
