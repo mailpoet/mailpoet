3dc23cc75efe7b4a45a663858f47b0a6c2dad64e
Amine Ben hammou <amine@mailpoet.com>
2019-09-04 15:09:40 +0100

    Schedule the task only when needed
    [MAILPOET-2313]

--- lib/Cron/Workers/SubscriberLinkTokens.php
+++ lib/Cron/Workers/SubscriberLinkTokens.php
@@ -10,6 +10,7 @@ if (!defined('ABSPATH')) exit;
 class SubscriberLinkTokens extends SimpleWorker {
   const TASK_TYPE = 'subscriber_link_tokens';
   const BATCH_SIZE = 1000;
+  const AUTOMATIC_SCHEDULING = false;
 
   function processTaskStrategy(ScheduledTask $task) {
     $count = $this->addTokens();
@@ -23,12 +24,17 @@ class SubscriberLinkTokens extends SimpleWorker {
     return true;
   }
 
-  public function addTokens() {
+  private function addTokens() {
     $instances = Subscriber::whereNull('link_token')->limit(self::BATCH_SIZE)->findMany();
     foreach ($instances as $instance) {
       $instance->set('link_token', Subscriber::generateToken($instance->email));
       $instance->save();
     }
     return count($instances);
+  }
+
+  static function getNextRunDate() {
+    $wp = new WPFunctions();
+    return Carbon::createFromTimestamp($wp->currentTime('timestamp'));
   }
 }
