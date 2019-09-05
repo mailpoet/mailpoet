dcb9b124c8c2fa95d1edaec7989652a912da29ca
Pavel Dohnal <pavel@mailpoet.com>
2019-09-05 12:42:24 +0200

    Send text wordpress emails
    [MAILPOET-2307]

--- lib/Mailer/WordPress/WordPressMailer.php
+++ lib/Mailer/WordPress/WordPressMailer.php
@@ -9,8 +9,47 @@ if (!class_exists('PHPMailer')) {
 
 class WordPressMailer extends \PHPMailer {
 
-  public function send() {
+  /** @var Mailer */
+  private $mailer;
 
+  function __construct(Mailer $mailer) {
+    parent::__construct(true);
+    $this->mailer = $mailer;
+  }
+
+  function send() {
+    // We need this so that the \PHPMailer class will correctly prepare all the headers.
+    $this->Mailer = 'mail';
+
+    // Prepare everything (including the message) for sending.
+    if (!$this->preSend()) {
+      return false;
+    }
+
+    $result = $this->mailer->send($this->getEmail(), $this->formatAddress($this->getToAddresses()));
+    // TODO return boolean depending on the result and throw
+  }
+
+  private function getEmail() {
+    $email = [
+      'subject' => $this->Subject,
+      'body' => [],
+    ];
+    if ($this->ContentType === 'text/plain' ) {
+      $email['body']['text'] = $this->Body;
+    }
+    return $email;
+  }
+
+  private function formatAddress($wordpress_address) {
+    $data = $wordpress_address[0];
+    $result = [
+      'address' => $data[0],
+    ];
+    if (!empty($data[1])) {
+      $result['full_name'] = $data[1];
+    }
+    return $result;
   }
 
 }
