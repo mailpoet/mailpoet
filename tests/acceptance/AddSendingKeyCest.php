0d738c8123ff0a961e8c602aa12049e2d26a7b1b
Jan Jakeš <jan@jakes.pro>
2019-09-12 10:20:19 +0200

    Skip sending key test when sending key env variable not set
    [MAILPOET-2319]

--- tests/acceptance/AddSendingKeyCest.php
+++ tests/acceptance/AddSendingKeyCest.php
@@ -2,10 +2,17 @@
 
 namespace MailPoet\Test\Acceptance;
 
+use Codeception\Scenario;
+
 class AddSendingKeyCest {
-  function addMailPoetSendingKey(\AcceptanceTester $I) { 
+  function addMailPoetSendingKey(\AcceptanceTester $I, Scenario $scenario) {
     $I->wantTo('Add a mailpoet sending key');
+
     $mailPoetSendingKey = getenv('WP_TEST_MAILER_MAILPOET_API');
+    if (!$mailPoetSendingKey) {
+      $scenario->skip("Skipping, 'WP_TEST_MAILER_MAILPOET_API' not set.");
+    }
+
     $keyActivationTab = '[data-automation-id="activation_settings_tab"]';
     $I->login();
     $I->amOnMailPoetPage('Settings');
