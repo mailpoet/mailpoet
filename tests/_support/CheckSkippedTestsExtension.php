03cb02caacc1f7633021489b2930639deaf2b103
Pavel Dohnal <pavel@mailpoet.com>
2019-09-19 09:48:02 +0200

    Disable acceptance tests skipping on master
    [MAILPOET-2358]

---
+++ tests/_support/CheckSkippedTestsExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+use Codeception\Event\SuiteEvent;
+use Codeception\Events;
+use Codeception\Extension;
+
+class CheckSkippedTestsExtension extends Extension {
+  static $events = [
+    Events::SUITE_AFTER => 'checkErrorsAfterTests',
+  ];
+
+  function checkErrorsAfterTests(SuiteEvent $e) {
+    $branch = getenv('CIRCLE_BRANCH');
+    $skipped = $e->getResult()->skipped();
+    if (in_array($branch, ['master', 'release']) && (count($skipped) !== 0)) {
+      throw new PHPUnit_Framework_ExpectationFailedException("Failed, Cannot skip tests on branch $branch.");
+    }
+  }
+}
