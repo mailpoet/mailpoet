b90b0964938a938df7dccb1f9fdd474fa22bee6f
Jan Jakeš <jan@jakes.pro>
2019-09-25 10:11:35 +0200

    Implement skipping for already downloaded PHAR tools
    [MAILPOET-2405]

--- tools/download.php
+++ tools/download.php
@@ -13,11 +13,20 @@ if (!$is_dev_mode) {
 
 // download all tools
 foreach ($tools as $url => $path) {
+  $phar_path = __DIR__ . "/$path";
+  $phar_info_path = "$phar_path.info";
+
   fwrite(STDERR, "Downloading '$url'...");
+  if (file_exists($phar_path) && file_exists($phar_info_path) && file_get_contents($phar_info_path) === $url) {
+    fwrite(STDERR, " skipped (already exists).\n");
+    continue;
+  }
+
   $resource = fopen($url, 'r');
   if ($resource === false) {
     throw new \RuntimeException("Could not connect to '$url'");
   }
-  file_put_contents(__DIR__ . "/$path", $resource);
+  file_put_contents($phar_path, $resource);
+  file_put_contents($phar_info_path, $url);
   fwrite(STDERR, " done.\n");
 }
