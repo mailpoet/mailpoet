fca7f2b2c78a2b32a81379527a2ca4babeab0dc9
Ján Mikláš <neosinner@gmail.com>
2019-09-25 12:36:01 +0200

    Rename `de` translation as `de_DE-formal`
    [MAILPOET-2384]

--- tasks/pack_translations.sh
+++ tasks/pack_translations.sh
@@ -7,4 +7,11 @@ echo "Generating MO files..."
 for file in `find ./lang/ -name "*.po"` ; do
   msgfmt -o ${file/.po/.mo} $file
 done
+
+echo "Creating german formal translation..."
+if [[ -f ./lang/mailpoet-de.mo && -f ./lang/mailpoet-de.po ]]; then
+	mv ./lang/mailpoet-de.mo ./lang/mailpoet-de_DE-formal.mo
+	mv ./lang/mailpoet-de.po ./lang/mailpoet-de_DE-formal.po
+fi
+
 echo "Done"
