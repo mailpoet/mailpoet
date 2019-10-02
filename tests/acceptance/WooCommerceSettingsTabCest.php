ed215782a81e88d0515dc35c8309b6fabbe63c68
Ján Mikláš <neosinner@gmail.com>
2019-10-02 14:52:00 +0200

    Add acceptance test for disabling WooCommerce email settings
    [MAILPOET-2283]

--- tests/acceptance/WooCommerceSettingsTabCest.php
+++ tests/acceptance/WooCommerceSettingsTabCest.php
@@ -2,9 +2,21 @@
 
 namespace MailPoet\Test\Acceptance;
 
+use MailPoet\Features\FeaturesController;
+use MailPoet\Test\DataFactories\Features;
 use MailPoet\Test\DataFactories\Settings;
 
 class WooCommerceSettingsTabCest {
+
+  const CUSTOMIZE_SELECTOR = '[data-automation-id="mailpoet_woocommerce_customize"]';
+  const DISABLE_SELECTOR = '[data-automation-id="mailpoet_woocommerce_disable"]';
+
+  /** @var Features */
+  private $features;
+
+  protected function _inject(Features $features) {
+    $this->features = $features;
+  }
 
   function _before(\AcceptanceTester $I) {
     $I->activateWooCommerce();
@@ -28,5 +40,26 @@ class WooCommerceSettingsTabCest {
     $I->amOnMailpoetPage('Settings');
     $I->dontSeeElement('[data-automation-id="woocommerce_settings_tab"]');
     $I->seeNoJSErrors();
+  }
+
+  function checkWooCommercePluginSettingsAreDisabled(\AcceptanceTester $I) {
+    $this->features->withFeatureEnabled(FeaturesController::WC_TRANSACTIONAL_EMAILS_CUSTOMIZER);
+    $this->settings_factory->withWooCommerceEmailCustomizerEnabled();
+
+    $I->wantTo('Check WooCommerce plugin email settings are overlayed with link to MailPoet');
+
+    $I->login();
+    $I->amOnPage("/wp-admin/admin.php?page=wc-settings&tab=general");
+    $I->dontSeeElementInDOM(self::CUSTOMIZE_SELECTOR);
+
+    $I->amOnPage("/wp-admin/admin.php?page=wc-settings&tab=email");
+    $I->scrollTo(self::CUSTOMIZE_SELECTOR);
+    $href = $I->grabAttributeFrom(self::CUSTOMIZE_SELECTOR, 'href');
+    expect($href)->contains('?page=mailpoet-newsletter-editor&id=');
+    $href = $I->grabAttributeFrom(self::DISABLE_SELECTOR, 'href');
+    expect($href)->contains('?page=mailpoet-settings#woocommerce');
+
+    $I->amOnPage("/wp-admin/admin.php?page=wc-settings&tab=email&section=wc_email_new_order");
+    $I->dontSeeElementInDOM(self::CUSTOMIZE_SELECTOR);
   }
 }
