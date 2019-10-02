2eb0133422041e77134012af9e2c1caa5ba010a0
Amine Ben hammou <amine@mailpoet.com>
2019-10-02 22:53:00 +0100

    Create WoocommerceContent block
    [MAILPOET-2279]

---
+++ assets/js/src/newsletter_editor/blocks/woocommerceContent.js
@@ -0,0 +1,52 @@
+/* eslint-disable func-names */
+import App from 'newsletter_editor/App';
+import BaseBlock from 'newsletter_editor/blocks/base';
+
+const BlockModel = BaseBlock.BlockModel.extend({
+  defaults: function () {
+    return {
+      type: 'woocommerceContent',
+    };
+  },
+});
+
+const BlockToolsView = BaseBlock.BlockToolsView.extend({
+  tools: { move: true },
+});
+
+const WidgetView = BaseBlock.WidgetView.extend({
+  className: BaseBlock.WidgetView.prototype.className + ' mailpoet_droppable_layout_block',
+  getTemplate: function () { return window.templates.woocommerceContentInsertion; },
+  behaviors: {
+    DraggableBehavior: {
+      cloneOriginal: true,
+      drop: function () {
+        return new BlockModel({}, { parse: true });
+      },
+    },
+  },
+});
+
+const BlockView = BaseBlock.BlockView.extend({
+  className: 'mailpoet_block mailpoet_woocommerce_content_block mailpoet_droppable_block',
+  getTemplate: function () { return window.templates.woocommerceContentBlock; },
+  regions: {
+    toolsRegion: '.mailpoet_tools',
+  },
+  onDragSubstituteBy: function () { return WidgetView; },
+  onRender: function () {
+    this.toolsView = new BlockToolsView({ model: this.model });
+    this.showChildView('toolsRegion', this.toolsView);
+  },
+});
+
+App.on('before:start', function (BeforeStartApp) {
+  BeforeStartApp.registerBlockType('woocommerceContent', {
+    blockModel: BlockModel,
+    blockView: BlockView,
+  });
+});
+
+export default {
+  BlockModel, BlockView, BlockToolsView, WidgetView,
+};
