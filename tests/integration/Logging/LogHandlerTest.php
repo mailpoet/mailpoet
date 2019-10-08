10eb34ce53cac549bf62508020199f4ce8871984
Rostislav Wolny <rosta@mailpoet.com>
2019-10-08 11:56:11 +0200

    Remove aspect mock from LogHandler test
    [MAILPOET-2444]

--- tests/integration/Logging/LogHandlerTest.php
+++ tests/integration/Logging/LogHandlerTest.php
@@ -2,35 +2,24 @@
 
 namespace MailPoet\Logging;
 
-use AspectMock\Test as Mock;
 use Carbon\Carbon;
-use Codeception\Stub;
-use Codeception\Stub\Expected;
 use MailPoet\Models\Log;
 
 class LogHandlerTest extends \MailPoetTest {
 
   public function testItCreatesLog() {
-    $log_model = Stub::makeEmpty(Log::class, [
-      'save' => Expected::once(),
-      'hydrate' => Expected::once(),
-    ], $this);
-
     $log_handler = new LogHandler();
-
-    $log_handler = Mock::double($log_handler, [
-      'createNewLogModel' => function () use ($log_model) {
-        return $log_model;
-      },
-    ]);
-
+    $time = new \DateTime();
     $log_handler->handle([
       'level' => \MailPoetVendor\Monolog\Logger::EMERGENCY,
       'extra' => [],
       'context' => [],
       'channel' => 'name',
-      'datetime' => new \DateTime(),
+      'datetime' => $time,
     ]);
+
+    $log = Log::where('name', 'name')->orderByDesc('id')->findOne();
+    expect($log->created_at)->equals($time->format('Y-m-d H:i:s'));
 
   }
 
@@ -87,7 +76,6 @@ class LogHandlerTest extends \MailPoetTest {
   }
 
   function _after() {
-    Mock::clean();
     \ORM::raw_execute('TRUNCATE ' . Log::$_table);
   }
 
