1e491a6e4e928d3b5701faccfec5edd39ea0d064
Ján Mikláš <neosinner@gmail.com>
2019-10-08 14:28:28 +0200

    Rewrite acceptance tests to use actual email ID
    [MAILPOET-2408]

--- tests/acceptance/WooCommerceEmailCustomizationCest.php
+++ tests/acceptance/WooCommerceEmailCustomizationCest.php
@@ -14,18 +14,30 @@ class WooCommerceEmailCustomizationCest {
   /** @var Features */
   private $features;
 
-  function _before() {
+  /** @var int */
+  private $woocommerce_email_template_id;
+
+  function _before(\AcceptanceTester $I) {
+    $I->activateWooCommerce();
     $this->features = new Features;
     $this->settings = new Settings();
     $this->features->withFeatureEnabled(FeaturesController::WC_TRANSACTIONAL_EMAILS_CUSTOMIZER);
+
+    // TODO: remove next 4 lines when WC_TRANSACTIONAL_EMAILS_CUSTOMIZER flag is removed
+    $I->login();
+    $I->amOnPluginsPage();
+    $I->deactivatePlugin('mailpoet');
+    $I->activatePlugin('mailpoet');
+
+    $woocommerce_settings = $I->grabFromDatabase(MP_SETTINGS_TABLE, 'value', ['name' => 'woocommerce']);
+    $woocommerce_settings = unserialize($woocommerce_settings);
+    $this->woocommerce_email_template_id = $woocommerce_settings['transactional_email_id'];
   }
 
   function openEmailCustomizerWhenSettingIsEnabled(\AcceptanceTester $I) {
     $I->wantTo('Open WooCommerce email customizer while setting is enabled');
 
-    $I->activateWooCommerce();
     $this->settings->withWooCommerceEmailCustomizerEnabled();
-    $this->settings->withWooCommerceTransactionalEmailId(11);
 
     $I->login();
     $I->amOnMailpoetPage('Emails');
@@ -36,15 +48,15 @@ class WooCommerceEmailCustomizationCest {
     $I->waitForText('Customize', 30);
     $I->dontSee('Activate', $button_selector);
     $I->click($button_selector);
-    $I->seeInCurrentUrl('?page=mailpoet-newsletter-editor&id=11');
+
+    $I->waitForText('Edit template for WooCommerce emails');
+    $I->seeInCurrentUrl('?page=mailpoet-newsletter-editor&id=' . $this->woocommerce_email_template_id);
   }
 
   function openEmailCustomizerWhenSettingIsDisabled(\AcceptanceTester $I) {
     $I->wantTo('Open WooCommerce email customizer while setting is disabled');
 
-    $I->activateWooCommerce();
     $this->settings->withWooCommerceEmailCustomizerDisabled();
-    $this->settings->withWooCommerceTransactionalEmailId(11);
 
     $I->login();
     $I->amOnMailpoetPage('Emails');
@@ -54,27 +66,18 @@ class WooCommerceEmailCustomizationCest {
     $I->seeNoJSErrors();
     $I->waitForText('Activate & Customize', 30);
     $I->click($button_selector);
-    $I->seeInCurrentUrl('?page=mailpoet-newsletter-editor&id=11');
+
+    $I->waitForText('Edit template for WooCommerce emails');
+    $I->seeInCurrentUrl('?page=mailpoet-newsletter-editor&id=' . $this->woocommerce_email_template_id);
   }
 
   function showNoticeWhenWooCommerceCustomizerIsDisabled(\AcceptanceTester $I) {
-    $I->activateWooCommerce();
-    $this->settings->withWooCommerceEmailCustomizerDisabled();
-
-    // TODO: remove next 4 lines when WC_TRANSACTIONAL_EMAILS_CUSTOMIZER flag is removed
-    $I->login();
-    $I->amOnPluginsPage();
-    $I->deactivatePlugin('mailpoet');
-    $I->activatePlugin('mailpoet');
-
-    $woocommerce_settings = $I->grabFromDatabase(MP_SETTINGS_TABLE, 'value', ['name' => 'woocommerce']);
-    $woocommerce_settings = unserialize($woocommerce_settings);
-    $woocommerce_email_template_id = $woocommerce_settings['transactional_email_id'];
-
     $I->wantTo('Show notice when WooCommerce Customizer is disabled');
 
+    $this->settings->withWooCommerceEmailCustomizerDisabled();
+
     $I->login();
-    $I->amOnPage('/wp-admin/admin.php?page=mailpoet-newsletter-editor&id=' . $woocommerce_email_template_id);
+    $I->amOnPage('/wp-admin/admin.php?page=mailpoet-newsletter-editor&id=' . $this->woocommerce_email_template_id);
     $I->waitForText('You need to enable MailPoet email customizer for WooCommerce if you want to access to the customizer.');
     $I->seeInCurrentUrl('?page=mailpoet-settings&enable-customizer-notice#woocommerce');
   }
