3c3baeb9a96520fc896bd5c70eb1bac8753dd4f0
Jan Jakeš <jan@jakes.pro>
2019-10-09 10:16:23 +0200

    Prefix Swiftmailer class strings missed by PHPScoper, add tests
    [MAILPOET-2448]

--- prefixer/fix-swiftmailer.php
+++ prefixer/fix-swiftmailer.php
@@ -14,6 +14,8 @@ foreach ($files as $file) {
     $data = preg_replace("/'(Swift_[^']*?::)/", "'MailPoetVendor\\\\\\\\$1", $data);
     $data = preg_replace("/InstanceOf\('(Swift_[^']*?')/", "InstanceOf('MailPoetVendor\\\\\\\\$1", $data);
     $data = preg_replace("/registerAutoload\('(_swift[^']*?')/", "registerAutoload('MailPoetVendor\\\\\\\\$1", $data);
+    $data = preg_replace("/'(Swift_[^']*?Listener)/", "'MailPoetVendor\\\\\\\\$1", $data);
+    $data = str_replace("'Swift_CharacterReader_", "'MailPoetVendor\\\\Swift_CharacterReader_", $data);
     $data = str_replace('SWIFT_INIT_LOADED', 'MAILPOET_SWIFT_INIT_LOADED', $data);
 
     // require-once 'swift_init.php' in classes since prefixed version won't use Swift's autoloader
