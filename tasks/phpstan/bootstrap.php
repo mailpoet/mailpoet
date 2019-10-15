078223cd645abf704d0166110d330710e3fc302c
Jan Jakeš <jan@jakes.pro>
2019-10-15 16:58:38 +0200

    Fix path usage in PHPStan setup
    [MAILPOET-2429]

--- tasks/phpstan/bootstrap.php
+++ tasks/phpstan/bootstrap.php
@@ -4,10 +4,10 @@ define('ABSPATH', getenv('WP_ROOT') . '/');
 
 require_once ABSPATH . 'wp-load.php';
 require_once ABSPATH . 'wp-admin/includes/admin.php';
-require_once ABSPATH . '/wp-includes/class-phpmailer.php';
+require_once ABSPATH . 'wp-includes/class-phpmailer.php';
 
 if (!class_exists('\MailPoet\Premium\DI\ContainerConfigurator')) {
-  require_once './PremiumContainerConfigurator.php';
+  require_once __DIR__ . '/PremiumContainerConfigurator.php';
 }
 
 if (!class_exists(WooCommerce::class)) {
