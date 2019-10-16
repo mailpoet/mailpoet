d51b61ec17905b618892c134fc0992c50f79c2e0
Rostislav Wolny <rosta@mailpoet.com>
2019-10-16 13:52:34 +0200

    Mark current form editor code as legacy
    [MAILPOET-2450]

--- assets/js/src/form_editor_legacy/webpack_index.jsx
+++ assets/js/src/form_editor_legacy/webpack_index.jsx
@@ -4,6 +4,6 @@
 // This is to avoid undefined import order & messy WebPack config.
 // Code can be gradually refactored to avoid side effects completely.
 
-import 'form_editor/form_editor.js'; // side effect - calls document.observe()
+import 'form_editor_legacy/form_editor.js'; // side effect - calls document.observe()
 import 'codemirror'; // side effect - has to be loaded here, used in 'editor.html'
 import 'codemirror/mode/css/css'; // side effect - has to be loaded here, used in 'editor.html'
