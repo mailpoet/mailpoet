d3d1307fef302ac11ef7b5e78e2b191bb9d4a0a6
Jan Jakeš <jan@jakes.pro>
2019-10-16 13:20:54 +0200

    Implement Codeception extension for PHPStan
    [MAILPOET-2429]

---
+++ tasks/phpstan/extensions/CodeceptionExtension/Type/StubDynamicReturnTypeExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace MailPoet\PHPStan\Extensions\CodeceptionExtension\Type;
+
+use Codeception\Stub;
+use PhpParser\Node\Expr\StaticCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\DynamicStaticMethodReturnTypeExtension;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+
+class StubDynamicReturnTypeExtension implements DynamicStaticMethodReturnTypeExtension {
+
+  public function getClass(): string {
+    return Stub::class;
+  }
+
+  public function isStaticMethodSupported(MethodReflection $reflection): bool {
+    return in_array($reflection->getName(), [
+      'make',
+      'makeEmpty',
+      'makeEmptyExcept',
+      'construct',
+      'constructEmpty',
+      'constructEmptyExcept',
+      'copy',
+    ], true);
+  }
+
+  public function getTypeFromStaticMethodCall(MethodReflection $reflection, StaticCall $call, Scope $scope): Type {
+    $type = $scope->getType($call->args[0]->value);
+    if ($type instanceof ConstantStringType) {
+      return new ObjectType($type->getValue()); // $type is class name
+    }
+    return $type;
+  }
+}
