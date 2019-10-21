92696949c24853923ad31b1d28be17cb273c142f
Jan Jakeš <jan@jakes.pro>
2019-10-21 14:48:08 +0200

    Install symfony\validator to vendor-prefixed
    [MAILPOET-2437]

---
+++ prefixer/fix-validator.php
@@ -0,0 +1,25 @@
+<?php
+
+// throw exception if anything fails
+set_error_handler(function ($severity, $message, $file, $line) {
+  throw new ErrorException($message, 0, $severity, $file, $line);
+});
+
+// cleanup unused Validator paths
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/validator/DataCollector');
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/validator/DependencyInjection');
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/validator/Resources');
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/validator/Test');
+
+// cleanup unused Translator paths
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/translation/Catalogue');
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/translation/Command');
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/translation/DataCollector');
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/translation/DependencyInjection');
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/translation/Dumper');
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/translation/Extractor');
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/translation/Formatter');
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/translation/Reader');
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/translation/Loader');
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/translation/Resources');
+exec('rm -r ' . __DIR__ . '/../vendor-prefixed/symfony/translation/Writer');
