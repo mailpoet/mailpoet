40f2818af43c5c9d38ffbc67d7acee78393c62e9
Jan Jakeš <jan@jakes.pro>
2019-10-22 15:16:11 +0200

    Add test for Doctrine entity validation
    [MAILPOET-2437]

---
+++ tests/integration/Doctrine/EventListeners/ValidatedEntity.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MailPoet\Test\Doctrine\EventListeners;
+
+use MailPoetVendor\Doctrine\ORM\Mapping as ORM;
+use MailPoetVendor\Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="test_validated_entity")
+ */
+class ValidatedEntity {
+  /**
+   * @ORM\Column(type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue
+   * @var int|null
+   */
+  private $id;
+
+  /**
+   * @ORM\Column(type="string")
+   * @Assert\NotBlank()
+   * @Assert\Length(min=3)
+   * @var string
+   */
+  private $name;
+
+  /** @return int */
+  function getId() {
+    return $this->id;
+  }
+
+  /** @return string */
+  public function getName() {
+    return $this->name;
+  }
+
+  /** @param string $name */
+  public function setName($name) {
+    $this->name = $name;
+  }
+}
