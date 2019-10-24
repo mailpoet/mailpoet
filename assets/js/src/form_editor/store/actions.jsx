cb68de4384e1975684a0ea70215547720453602e
Rostislav Wolny <rosta@mailpoet.com>
2019-10-24 17:22:53 +0200

    Refactor store actions from single object to extra functions
    [MAILPOET-2450]

--- assets/js/src/form_editor/store/actions.jsx
+++ assets/js/src/form_editor/store/actions.jsx
@@ -1,14 +1,14 @@
-export default {
-  toggleSidebar(toggleTo) {
-    return {
-      type: 'TOGGLE_SIDEBAR',
-      toggleTo,
-    };
-  },
-  changeFormName(name) {
-    return {
-      type: 'CHANGE_FORM_NAME',
-      name,
-    };
-  },
-};
+
+export function toggleSidebar(toggleTo) {
+  return {
+    type: 'TOGGLE_SIDEBAR',
+    toggleTo,
+  };
+}
+
+export function changeFormName(name) {
+  return {
+    type: 'CHANGE_FORM_NAME',
+    name,
+  };
+}
