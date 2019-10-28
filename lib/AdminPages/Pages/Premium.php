51193359c7db66d3b0f0a4d8f5ba93efd0a5f445
Pavel Dohnal <pavel@mailpoet.com>
2019-10-28 15:20:21 +0100

    Remove new premium page feature flag
    [MAILPOET-2493]

--- lib/AdminPages/Pages/Premium.php
+++ lib/AdminPages/Pages/Premium.php
@@ -3,35 +3,20 @@
 namespace MailPoet\AdminPages\Pages;
 
 use MailPoet\AdminPages\PageRenderer;
-use MailPoet\Config\Menu;
-use MailPoet\Features\FeaturesController;
 use MailPoet\Models\Subscriber;
 
 class Premium {
   /** @var PageRenderer */
   private $page_renderer;
 
-  /** @var FeaturesController */
-  private $features_controller;
-
-  function __construct(PageRenderer $page_renderer, FeaturesController $features_controller) {
+  function __construct(PageRenderer $page_renderer) {
     $this->page_renderer = $page_renderer;
-    $this->features_controller = $features_controller;
   }
 
   function render() {
-    if ($this->features_controller->isSupported(FeaturesController::NEW_PREMIUM_PAGE)) {
-      $data = [
-        'subscriber_count' => Subscriber::getTotalSubscribers(),
-      ];
-      $this->page_renderer->displayPage('premium.html', $data);
-    } else {
-      $data = [
-        'subscriber_count' => Subscriber::getTotalSubscribers(),
-        'sub_menu' => Menu::MAIN_PAGE_SLUG,
-        'display_discount' => time() <= strtotime('2018-11-30 23:59:59'),
-      ];
-      $this->page_renderer->displayPage('premium_old.html', $data);
-    }
+    $data = [
+      'subscriber_count' => Subscriber::getTotalSubscribers(),
+    ];
+    $this->page_renderer->displayPage('premium.html', $data);
   }
 }
