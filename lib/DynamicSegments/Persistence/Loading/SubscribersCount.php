1e2381293addcb0b1b5f087e2087e608b67c0ea9
Pavel Dohnal <pavel@mailpoet.com>
2019-10-29 11:59:02 +0100

    Check requirements when counting subscribers
    [MAILPOET-2486]

--- lib/DynamicSegments/Persistence/Loading/SubscribersCount.php
+++ lib/DynamicSegments/Persistence/Loading/SubscribersCount.php
@@ -2,10 +2,22 @@
 
 namespace MailPoet\DynamicSegments\Persistence\Loading;
 
+use MailPoet\DynamicSegments\RequirementsChecker;
 use MailPoet\Models\DynamicSegment;
 use MailPoet\Models\Subscriber;
+use MailPoet\WooCommerce\Helper as WooCommerceHelper;
 
 class SubscribersCount {
+
+  /** @var RequirementsChecker */
+  private $requirements_checker;
+
+  function __construct(RequirementsChecker $requirements_checker = null) {
+    if (!$requirements_checker) {
+      $requirements_checker = new RequirementsChecker(new WooCommerceHelper());
+    }
+    $this->requirements_checker = $requirements_checker;
+  }
 
   /**
    * @param DynamicSegment $dynamic_segment
@@ -14,6 +26,9 @@ class SubscribersCount {
    */
   function getSubscribersCount(DynamicSegment $dynamic_segment) {
     $orm = Subscriber::selectExpr('count(distinct ' . Subscriber::$_table . '.id) as cnt');
+    if ($this->requirements_checker->shouldSkipSegment($dynamic_segment)) {
+      return 0;
+    }
     foreach ($dynamic_segment->getFilters() as $filter) {
       $orm = $filter->toSql($orm);
     }
