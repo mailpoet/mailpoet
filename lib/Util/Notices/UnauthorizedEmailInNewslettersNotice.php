1095cba1f1fbef5e5e38359def5e5b7d9eea6e66
Pavel Dohnal <pavel@mailpoet.com>
2019-10-29 13:10:05 +0100

    Add missing translation domains
    [MAILPOET-2494]

--- lib/Util/Notices/UnauthorizedEmailInNewslettersNotice.php
+++ lib/Util/Notices/UnauthorizedEmailInNewslettersNotice.php
@@ -46,14 +46,14 @@ class UnauthorizedEmailInNewslettersNotice {
   }
 
   private function getMessageText() {
-    $message = $this->wp->__('<b>Your automatic emails have been paused,</b> because some email addresses haven’t been authorized yet.');
+    $message = $this->wp->__('<b>Your automatic emails have been paused,</b> because some email addresses haven’t been authorized yet.', 'mailpoet');
     return "<p>$message</p>";
   }
 
   private function getNewslettersLinks($validation_error) {
     $links = '';
     foreach ($validation_error['invalid_senders_in_newsletters'] as $error) {
-      $link_text = $this->wp->_x('Update the from address of %s', '%s will be replaced by a newsletter subject');
+      $link_text = $this->wp->_x('Update the from address of %s', '%s will be replaced by a newsletter subject', 'mailpoet');
       $link_text = str_replace('%s', EscapeHelper::escapeHtmlText($error['subject']), $link_text);
       $link_url = $this->wp->adminUrl('admin.php?page=' . Menu::MAIN_PAGE_SLUG . '#/send/' . $error['newsletter_id']);
       $link = Helpers::replaceLinkTags("[link]{$link_text}[/link]", $link_url, ['target' => '_blank']);
@@ -65,16 +65,16 @@ class UnauthorizedEmailInNewslettersNotice {
   private function getAuthorizationLink($validation_error) {
     $emails = array_unique(array_column($validation_error['invalid_senders_in_newsletters'], 'sender_address'));
     if (count($emails) > 1) {
-      $authorize_link = $this->wp->_x('Authorize %1$s and %2$s', 'Link for user to authorize their email address');
+      $authorize_link = $this->wp->_x('Authorize %1$s and %2$s', 'Link for user to authorize their email address', 'mailpoet');
       $authorize_link = str_replace('%2$s', EscapeHelper::escapeHtmlText(array_pop($emails)), $authorize_link);
       $authorize_link = str_replace('%1$s', EscapeHelper::escapeHtmlText(implode(', ', $emails)), $authorize_link);
     } else {
-      $authorize_link = $this->wp->_x('Authorize %s', 'Link for user to authorize their email address');
+      $authorize_link = $this->wp->_x('Authorize %s', 'Link for user to authorize their email address', 'mailpoet');
       $authorize_link = str_replace('%s', EscapeHelper::escapeHtmlText($emails[0]), $authorize_link);
     }
 
     $authorize_link = Helpers::replaceLinkTags("[link]{$authorize_link}[/link]", 'https://account.mailpoet.com/authorization', ['target' => '_blank']);
-    $html = '<p><b>' . $this->wp->_x('OR', 'User has to choose between two options') . '</b></p>';
+    $html = '<p><b>' . $this->wp->_x('OR', 'User has to choose between two options', 'mailpoet') . '</b></p>';
     $html .= "<p>$authorize_link</p>";
     return $html;
   }
