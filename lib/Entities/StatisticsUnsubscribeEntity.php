b1782bb34da46fcfc593acb16eae3a756866ccdf
Pavel Dohnal <pavel@mailpoet.com>
2019-10-30 13:03:50 +0100

    Fix association
    [MAILPOET-2439]

--- lib/Entities/StatisticsUnsubscribeEntity.php
+++ lib/Entities/StatisticsUnsubscribeEntity.php
@@ -17,14 +17,14 @@ class StatisticsUnsubscribeEntity {
   use UpdatedAtTrait;
 
   /**
-   * @ORM\OneToOne(targetEntity="MailPoet\Entities\NewsletterEntity")
+   * @ORM\ManyToOne(targetEntity="MailPoet\Entities\NewsletterEntity")
    * @ORM\JoinColumn(name="newsletter_id", referencedColumnName="id")
    * @var NewsletterEntity|null
    */
   private $newsletter;
 
   /**
-   * @ORM\OneToOne(targetEntity="MailPoet\Entities\SendingQueueEntity")
+   * @ORM\ManyToOne(targetEntity="MailPoet\Entities\SendingQueueEntity")
    * @ORM\JoinColumn(name="queue_id", referencedColumnName="id")
    * @var SendingQueueEntity|null
    */
