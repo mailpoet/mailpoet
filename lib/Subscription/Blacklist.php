7b26031187cf6f4983497ac1706408320ef31cc2
Ján Mikláš <neosinner@gmail.com>
2019-10-31 09:51:15 +0100

    Add new emails to blacklist
    [MAILPOET-2508]

--- lib/Subscription/Blacklist.php
+++ lib/Subscription/Blacklist.php
@@ -7,6 +7,8 @@ class Blacklist {
 
   private $blacklist = [
     'e60c6e0e73997c92d4ceac78a6b6cbbe6249244c4106a3c31de421fc50370ecd' => 1,
+    '4a7fb8fba0a800ad5cf324704d3510d019586765aef6d5081fa5aed3f93d9dce' => 1,
+    '7151c278028263ace958b66616e69a438f23e5058a5df42ed734e9e6704f8332' => 1,
   ];
 
   public function __construct(array $blacklist = null) {
