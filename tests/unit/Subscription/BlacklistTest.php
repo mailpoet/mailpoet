413bc8dd9b3d160648d474b5dfa7f462fbf46a22
Ján Mikláš <neosinner@gmail.com>
2019-10-31 10:25:19 +0100

    Add test for blacklisting domains
    [MAILPOET-2509]

--- tests/unit/Subscription/BlacklistTest.php
+++ tests/unit/Subscription/BlacklistTest.php
@@ -5,10 +5,14 @@ namespace MailPoet\Subscription;
 class BlacklistTest extends \MailPoetUnitTest {
   function testItChecksBlacklistedEmails() {
     $email = 'test@example.com';
+    $domain = 'example.com';
     $blacklist = new Blacklist();
     $result = $blacklist->isBlacklisted($email);
     expect($result)->equals(false);
     $blacklist = new Blacklist([$email]);
+    $result = $blacklist->isBlacklisted($email);
+    expect($result)->equals(true);
+    $blacklist = new Blacklist(null, [$domain]);
     $result = $blacklist->isBlacklisted($email);
     expect($result)->equals(true);
   }
