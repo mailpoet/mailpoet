323e656f7375e447cc99f291cf6f1ea483fd554c
Rostislav Wolny <rosta@mailpoet.com>
2019-11-05 14:03:29 +0100

    Add comments for devs
    [MAILPOET-2450]

--- assets/js/src/form_editor/components/editor.jsx
+++ assets/js/src/form_editor/components/editor.jsx
@@ -6,6 +6,13 @@ import Sidebar from './sidebar.jsx';
 import FormTitle from './form_title.jsx';
 import Notices from './notices.jsx';
 
+/**
+ * This component renders the form editor app.
+ * Class names and organization of elements are done based on Gutenberg's edit-post package.
+ * (https://github.com/WordPress/gutenberg/tree/master/packages/edit-post).
+ * The goal is to render the same DOM for layout as Gutenberg post/page editor
+ * so that we can reuse it's CSS.
+ */
 export default () => {
   const sidebarOpened = useSelect(
     (select) => select('mailpoet-form-editor').getSidebarOpened(),
