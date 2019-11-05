5574bda028f1e9f5fa1c31df9c0db12fc7a34b1e
Rostislav Wolny <rosta@mailpoet.com>
2019-11-05 15:39:08 +0100

    Remove unused random id for notices
    [MAILPOET-2450]

--- assets/js/src/form_editor/store/reducers/addNotice.jsx
+++ assets/js/src/form_editor/store/reducers/addNotice.jsx
@@ -1,7 +1,7 @@
 export default (state, action) => {
   const notices = state.notices.filter((item) => item.id !== action.id);
   const notice = {
-    id: action.id ? action.id : Math.random().toString(36).substr(2, 9),
+    id: action.id,
     content: action.content,
     isDismissible: action.isDismissible,
     status: action.status,
