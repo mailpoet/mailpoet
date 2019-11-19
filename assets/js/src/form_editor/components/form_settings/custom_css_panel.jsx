0ba2e15a1c482a4db865116872436d178d76ae87
Rostislav Wolny <rosta@mailpoet.com>
2019-11-19 16:29:49 +0100

    Use codeMirror editor for Custom CSS
    [MAILPOET-2455]

--- assets/js/src/form_editor/components/form_settings/custom_css_panel.jsx
+++ assets/js/src/form_editor/components/form_settings/custom_css_panel.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
   Panel,
   PanelBody,
-  TextareaControl,
 } from '@wordpress/components';
 import MailPoet from 'mailpoet';
 import { useSelect, useDispatch } from '@wordpress/data';
+import CodeMirror from './codemirror_wrap.jsx';
 
 export default () => {
   const styles = useSelect(
     (select) => select('mailpoet-form-editor').getFormStyles(),
     []
   );
+  const options = useRef({
+    lineNumbers: true,
+    tabMode: 'indent',
+    matchBrackets: true,
+    theme: 'neo',
+    mode: 'css',
+  });
+
   const { changeFormStyles } = useDispatch('mailpoet-form-editor');
+
   return (
     <Panel>
       <PanelBody title={MailPoet.I18n.t('customCss')} initialOpen={false}>
-        <TextareaControl
+        <CodeMirror
           value={styles}
-          rows={10}
+          options={options.current}
           onChange={changeFormStyles}
         />
       </PanelBody>
