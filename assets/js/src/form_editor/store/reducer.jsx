554142e87cdf48d774256e5fe2011778c644743e
Rostislav Wolny <rosta@mailpoet.com>
2019-11-19 16:06:00 +0100

    Add custom css panel
    [MAILPOET-2455]

--- assets/js/src/form_editor/store/reducer.jsx
+++ assets/js/src/form_editor/store/reducer.jsx
@@ -1,6 +1,7 @@
 import addNotice from './reducers/addNotice.jsx';
 import toggleSidebar from './reducers/toggleSidebar.jsx';
 import changeFormName from './reducers/changeFormName.jsx';
+import changeFormStyles from './reducers/changeFormStyles.jsx';
 import saveFormStarted from './reducers/saveFormStarted.jsx';
 import saveFormDone from './reducers/saveFormDone.jsx';
 import removeNotice from './reducers/removeNotice.jsx';
@@ -9,6 +10,7 @@ export default (defaultState) => (state = defaultState, action) => {
   switch (action.type) {
     case 'ADD_NOTICE': return addNotice(state, action);
     case 'CHANGE_FORM_NAME': return changeFormName(state, action);
+    case 'CHANGE_FORM_STYLES': return changeFormStyles(state, action);
     case 'REMOVE_NOTICE': return removeNotice(state, action);
     case 'SAVE_FORM_DONE': return saveFormDone(state);
     case 'SAVE_FORM_STARTED': return saveFormStarted(state);
---
+++ assets/js/src/form_editor/store/reducers/changeFormStyles.jsx
@@ -0,0 +1,7 @@
+export default (state, action) => ({
+  ...state,
+  formData: {
+    ...state.formData,
+    styles: action.styles,
+  },
+});
