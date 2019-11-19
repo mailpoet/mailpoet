672b0c272e79a217b842552ecc71d8991384e989
Pavel Dohnal <pavel@mailpoet.com>
2019-11-19 12:55:12 +0100

    Check type for PHPStan
    [MAILPOET-2535]

--- lib/Util/pQuery/pQuery.php
+++ lib/Util/pQuery/pQuery.php
@@ -6,6 +6,12 @@ namespace MailPoet\Util\pQuery;
 class pQuery extends \pQuery {
   public static function parseStr($html) {
     $parser = new Html5Parser($html);
+
+    if (!$parser->root instanceof \pQuery\DomNode) {
+      // this condition shouldn't happen it is here only for PHPStan
+      throw new \Exception('Renderer is not configured correctly');
+    }
+
     return $parser->root;
   }
 }
