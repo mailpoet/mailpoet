25b467613765d81ec0e231cb469f4f961fce15d6
Amine Ben hammou <amine@mailpoet.com>
2019-11-20 10:39:24 +0100

    Fix shortcode being URL encoded
    [MAILPOET-2540]

--- tests/integration/Statistics/GATrackingTest.php
+++ tests/integration/Statistics/GATrackingTest.php
@@ -23,8 +23,8 @@ class GATrackingTest extends \MailPoetTest {
     $this->ga_campaign = 'Spring email';
     $this->link = add_query_arg(['foo' => 'bar', 'baz' => 'xyz'], 'http://www.mailpoet.com/');
     $this->rendered_newsletter = [
-      'html' => '<p><a href="' . $this->link . '">Click here</a>. <a href="http://somehost.com/fff/?abc=123">Do not process this</a> [link:some_link_shortcode]</p>',
-      'text' => '[Click here](' . $this->link . '). [Do not process this](http://somehost.com/fff/?abc=123) [link:some_link_shortcode]',
+      'html' => '<p><a href="' . $this->link . '">Click here</a>. <a href="http://somehost.com/fff/?abc=123&email=[subscriber:email]">Do not process this</a> [link:some_link_shortcode]</p>',
+      'text' => '[Click here](' . $this->link . '). [Do not process this](http://somehost.com/fff/?abc=123&email=[subscriber:email]) [link:some_link_shortcode]',
     ];
   }
 
@@ -67,6 +67,16 @@ class GATrackingTest extends \MailPoetTest {
     $result = $tracking->applyGATracking($this->rendered_newsletter, $newsletter, $this->internal_host);
     expect($result['text'])->contains('utm_campaign=' . urlencode($this->ga_campaign));
     expect($result['html'])->contains('utm_campaign=' . urlencode($this->ga_campaign));
+  }
+
+  function testItKeepsShorcodes() {
+    $tracking = new GATracking();
+    $newsletter = Newsletter::createOrUpdate([
+      'ga_campaign' => $this->ga_campaign,
+    ]);
+    $result = $tracking->applyGATracking($this->rendered_newsletter, $newsletter, $this->internal_host);
+    expect($result['text'])->contains('email=[subscriber:email]');
+    expect($result['html'])->contains('email=[subscriber:email]');
   }
 
   function _after() {
