924486e9148f7d92bfdf6888b3f19cd83757c98a
Amine Ben hammou <amine@mailpoet.com>
2019-11-26 21:47:00 +0100

    Add SubscribersRepository
    [MAILPOET-2394]

---
+++ lib/Subscribers/SubscribersRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MailPoet\Subscribers;
+
+use MailPoet\Doctrine\Repository;
+use MailPoet\Entities\SubscriberEntity;
+
+/**
+ * @method SubscriberEntity[] findBy(array $criteria, array $order_by = null, int $limit = null, int $offset = null)
+ * @method SubscriberEntity|null findOneBy(array $criteria, array $order_by = null)
+ * @method SubscriberEntity|null findOneById(mixed $id)
+ * @method void persist(SubscriberEntity $entity)
+ * @method void remove(SubscriberEntity $entity)
+ */
+class SubscribersRepository extends Repository {
+  protected function getEntityClassName() {
+    return SubscriberEntity::class;
+  }
+
+  /**
+   * @return int
+   */
+  public function getTotalSubscribers() {
+    $query = $this->entity_manager
+      ->createQueryBuilder()
+      ->select('count(n.id)')
+      ->from(SubscriberEntity::class, 'n')
+      ->where('n.status IN (:statuses)')
+      ->setParameter(':statuses', [
+        SubscriberEntity::STATUS_SUBSCRIBED,
+        SubscriberEntity::STATUS_UNCONFIRMED,
+        SubscriberEntity::STATUS_INACTIVE,
+      ])
+      ->getQuery();
+    return (int)$query->getSingleScalarResult();
+  }
+}
