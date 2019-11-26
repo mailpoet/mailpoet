c2c7986b55ac4aaa57a4465008d512f965072252
Rostislav Wolny <rosta@mailpoet.com>
2019-11-26 15:07:21 +0100

    Add list input highlight on save if no list selected
    [MAILOET-2455]

--- assets/js/src/form_editor/store/reducers/save_form_started.jsx
+++ assets/js/src/form_editor/store/reducers/save_form_started.jsx
@@ -4,6 +4,7 @@ export default (state) => {
   // remove all form saving related notices
   const notices = state.notices.filter((notice) => !['save-form', 'missing-lists'].includes(notice.id));
   const hasMissingLists = state.formErrors.includes('missing-lists');
+  const sidebarOpenedPanels = [...state.sidebar.openedPanels];
   if (hasMissingLists) {
     notices.push({
       id: 'missing-lists',
@@ -11,10 +12,19 @@ export default (state) => {
       isDismissible: true,
       status: 'error',
     });
+    if (!sidebarOpenedPanels.includes('basic-settings')) {
+      sidebarOpenedPanels.push('basic-settings');
+    }
   }
+
   return {
     ...state,
     isFormSaving: !hasMissingLists,
+    sidebar: {
+      ...state.sidebar,
+      activeTab: hasMissingLists ? 'form' : state.sidebar.activeTab,
+      openedPanels: sidebarOpenedPanels,
+    },
     notices,
   };
 };
