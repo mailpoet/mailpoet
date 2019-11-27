f8b74aef967e69196ad9225ceb398f1bb5422fb0
Jan Jakeš <jan@jakes.pro>
2019-11-27 14:25:55 +0100

    Check errors from both error.log and exception.log
    [MAILPOET-2551]

--- tests/_support/ErrorsExtension.php
+++ tests/_support/ErrorsExtension.php
@@ -1,38 +1,38 @@
 <?php
 
-use Codeception\Event\TestEvent;
 use Codeception\Events;
 use Codeception\Extension;
 
 class ErrorsExtension extends Extension { // phpcs:ignore PSR1.Classes.ClassDeclaration
-  const ERROR_LOG_PATH = __DIR__ . '/../_output/exceptions/error.log';
-
-  private $errors = [];
-
-  static $events = [
-    Events::TEST_BEFORE => 'checkErrorsBeforeTests',
-    Events::TEST_AFTER => 'checkErrorsAfterTests',
+  const ERROR_LOG_PATHS = [
+    __DIR__ . '/../_output/exceptions/error.log',
+    __DIR__ . '/../_output/exceptions/exception.log',
   ];
 
-  function checkErrorsBeforeTests(TestEvent $e) {
-    if (!file_exists(self::ERROR_LOG_PATH)) {
-      return;
+  private $known_error_counts = [];
+
+  static $events = [
+    Events::SUITE_BEFORE => 'loadErrorCountsBeforeSuite',
+    Events::TEST_AFTER => 'checkErrorsAfterTest',
+  ];
+
+  function loadErrorCountsBeforeSuite() {
+    foreach (self::ERROR_LOG_PATHS as $path) {
+      $this->known_error_counts[$path] = count($this->readFileToArray($path));
     }
-    $this->errors = file(self::ERROR_LOG_PATH, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
   }
 
-  function checkErrorsAfterTests(TestEvent $e) {
-    if (!file_exists(self::ERROR_LOG_PATH)) {
-      return;
+  function checkErrorsAfterTest() {
+    foreach (self::ERROR_LOG_PATHS as $path) {
+      $errors = $this->readFileToArray($path);
+      foreach (array_slice($errors, $this->known_error_counts[$path]) as $error) {
+        $this->output->writeln("<error>$error</error>");
+        $this->known_error_counts[$path]++;
+      }
     }
+  }
 
-    $errors = file(self::ERROR_LOG_PATH, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    if (count($this->errors) === count($errors)) {
-      return;
-    }
-
-    foreach (array_slice($errors, count($this->errors)) as $error) {
-      $this->output->writeln("<error>$error</error>");
-    }
+  private function readFileToArray($path) {
+    return file_exists($path) ? file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) : [];
   }
 }
