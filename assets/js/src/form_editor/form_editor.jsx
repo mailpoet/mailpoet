0b17a6ec853f4826f5d5d8aedf7b692b49055de0
Amine Ben hammou <amine@mailpoet.com>
2019-11-27 14:11:09 +0100

    Add context to form editor entrypoint
    [MAILPOET-2389]

--- assets/js/src/form_editor/form_editor.jsx
+++ assets/js/src/form_editor/form_editor.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GlobalContext, useGlobalContextValue } from 'context/index.jsx';
 import Editor from './components/editor.jsx';
 import initStore from './store/store.jsx';
+
+const App = () => (
+  <GlobalContext.Provider value={useGlobalContextValue(window)}>
+    <Editor />
+  </GlobalContext.Provider>
+);
 
 const appElement = document.querySelector('#mailpoet_form_edit');
 
 if (appElement) {
   initStore();
   ReactDOM.render(
-    <Editor />,
+    <App />,
     appElement
   );
 }
