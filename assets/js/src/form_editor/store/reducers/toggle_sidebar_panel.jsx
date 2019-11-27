cd0cce07a6367b17326073ead4b0de36ee72a984
Rostislav Wolny <rosta@mailpoet.com>
2019-11-27 12:38:52 +0100

    Refactor panel toggle reducer to be more readable
    [MAILPOET-2455]

--- assets/js/src/form_editor/store/reducers/toggle_sidebar_panel.jsx
+++ assets/js/src/form_editor/store/reducers/toggle_sidebar_panel.jsx
@@ -1,13 +1,34 @@
 import { remove } from 'lodash';
 
-export default (state, action) => {
-  const openedPanels = [...state.sidebar.openedPanels];
-  const isOpenedCurrent = openedPanels.includes(action.id);
-  const isOpenedFinal = action.isOpened !== undefined ? action.isOpened : !isOpenedCurrent;
-  if (isOpenedFinal && !isOpenedCurrent) {
-    openedPanels.push(action.id);
+const getRequiredAction = (openedPanels, panelId, toggleTo) => {
+  const isPanelOpened = openedPanels.includes(panelId);
+  let requestedToggleState = toggleTo;
+  if (requestedToggleState === undefined) {
+    requestedToggleState = isPanelOpened ? 'closed' : 'opened';
   }
-  if (!isOpenedFinal && isOpenedCurrent) {
+  if (isPanelOpened && requestedToggleState === 'closed') {
+    return 'close';
+  }
+  if (!isPanelOpened && requestedToggleState === 'opened') {
+    return 'open';
+  }
+  return null;
+};
+
+/**
+ * @param {object} state
+ * @param {{toggleTo: string|undefined, id: string, type: string}} action
+ * @return {object} Modified state object
+ */
+export default (state, action) => {
+  if (action.toggleTo !== undefined && !['opened', 'closed'].includes(action.toggleTo)) {
+    throw new Error(`Unexpected toggleTo value "${action.toggleTo}"`);
+  }
+  const openedPanels = [...state.sidebar.openedPanels];
+  const requiredAction = getRequiredAction(openedPanels, action.id, action.toggleTo);
+  if (requiredAction === 'open') {
+    openedPanels.push(action.id);
+  } else if (requiredAction === 'close') {
     remove(openedPanels, (item) => item === action.id);
   }
   return {
