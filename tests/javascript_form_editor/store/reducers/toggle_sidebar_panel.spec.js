bb0c10837e1a50ec52be27013448370cd73a494d
Rostislav Wolny <rosta@mailpoet.com>
2019-11-27 12:39:22 +0100

    Add test for panel toggle reducer
    [MAILPOET-2455]

---
+++ tests/javascript_form_editor/store/reducers/toggle_sidebar_panel.spec.js
@@ -0,0 +1,69 @@
+import { describe, it, beforeEach } from 'mocha';
+import { expect } from 'chai';
+import reducer from '../../../../assets/js/src/form_editor/store/reducers/toggle_sidebar_panel.jsx';
+
+describe('Toggle Sidebar Panel Reducer', () => {
+  let initialState = null;
+  beforeEach(() => {
+    initialState = {
+      something: 'value',
+      sidebar: {
+        activeTab: 'form',
+        openedPanels: [],
+      },
+    };
+  });
+
+  it('Should keep unrelated state properties untouched', () => {
+    const action = {
+      type: 'TOGGLE_SIDEBAR_PANEL',
+      id: 'nice-panel',
+    };
+    const finalState = reducer(initialState, action);
+    expect(finalState.something).to.equal('value');
+    expect(finalState.sidebar.activeTab).to.equal('form');
+  });
+
+  it('Should open closed panel', () => {
+    const action = {
+      type: 'TOGGLE_SIDEBAR_PANEL',
+      id: 'nice-panel',
+    };
+    const finalState = reducer(initialState, action);
+    expect(finalState.sidebar.openedPanels).to.include('nice-panel');
+  });
+
+  it('Should close opened panel', () => {
+    const action = {
+      type: 'TOGGLE_SIDEBAR_PANEL',
+      id: 'nice-panel',
+    };
+    initialState.sidebar.openedPanels.push('nice-panel');
+    const finalState = reducer(initialState, action);
+    expect(finalState.sidebar.openedPanels).to.not.include('nice-panel');
+  });
+
+  it('Should toggle panel to required state', () => {
+    const action = {
+      type: 'TOGGLE_SIDEBAR_PANEL',
+      id: 'nice-panel',
+      toggleTo: 'opened',
+    };
+    reducer(initialState, action);
+    let finalState = reducer(initialState, action);
+    expect(finalState.sidebar.openedPanels).to.include('nice-panel');
+    action.toggleTo = 'closed';
+    finalState = reducer(initialState, action);
+    expect(finalState.sidebar.openedPanels).to.not.include('nice-panel');
+  });
+
+  it('Should throw an error for unexpected toggleTo value', () => {
+    const action = {
+      type: 'TOGGLE_SIDEBAR_PANEL',
+      id: 'nice-panel',
+      toggleTo: 'nonsense',
+    };
+    expect(() => (reducer(initialState, action)))
+      .to.throw('Unexpected toggleTo value "nonsense"');
+  });
+});
