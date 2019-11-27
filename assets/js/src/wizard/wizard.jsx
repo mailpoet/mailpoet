de08105dff7cfdebb44e107fddbc54cf64858112
Amine Ben hammou <amine@mailpoet.com>
2019-11-27 14:03:08 +0100

    Add context to wizard entrypoint
    [MAILPOET-2389]

--- assets/js/src/wizard/wizard.jsx
+++ assets/js/src/wizard/wizard.jsx
@@ -6,27 +6,35 @@ import {
   Redirect,
   Switch,
 } from 'react-router-dom';
+import { GlobalContext, useGlobalContextValue } from 'context/index.jsx';
 import WelcomeWizardStepsController from './welcome_wizard_controller.jsx';
 import WooCommerceImportController from './woocommerce_import_controller.jsx';
 import RevenueTrackingPermissionController from './revenue_tracking_permission.jsx';
 
-const container = document.getElementById('mailpoet_wizard_container');
-
-if (container) {
+const App = () => {
   let basePath = '/steps/1';
   if (window.location.search.includes('revenue-tracking-permission')) {
     basePath = '/revenue-tracking-permission';
   } else if (window.location.search.includes('woocommerce-list-import')) {
     basePath = '/import';
   }
-  ReactDOM.render((
-    <HashRouter>
-      <Switch>
-        <Route path="/steps/:step" component={WelcomeWizardStepsController} />
-        <Route path="/import" component={WooCommerceImportController} />
-        <Route path="/revenue-tracking-permission" component={RevenueTrackingPermissionController} />
-        <Route render={() => <Redirect to={basePath} />} />
-      </Switch>
-    </HashRouter>
-  ), container);
+  const contextValue = useGlobalContextValue(window);
+  return (
+    <GlobalContext.Provider value={contextValue}>
+      <HashRouter>
+        <Switch>
+          <Route path="/steps/:step" component={WelcomeWizardStepsController} />
+          <Route path="/import" component={WooCommerceImportController} />
+          <Route path="/revenue-tracking-permission" component={RevenueTrackingPermissionController} />
+          <Route render={() => <Redirect to={basePath} />} />
+        </Switch>
+      </HashRouter>
+    </GlobalContext.Provider>
+  );
+};
+
+const container = document.getElementById('mailpoet_wizard_container');
+
+if (container) {
+  ReactDOM.render(<App />, container);
 }
