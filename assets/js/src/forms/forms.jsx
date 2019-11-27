3345903eaae965063b64df5c6cb326377abe1a05
Amine Ben hammou <amine@mailpoet.com>
2019-11-27 14:12:45 +0100

    Add context to forms entrypoint
    [MAILPOET-2389]

--- assets/js/src/forms/forms.jsx
+++ assets/js/src/forms/forms.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, HashRouter } from 'react-router-dom';
+import { GlobalContext, useGlobalContextValue } from 'context/index.jsx';
 import FormList from './list.jsx';
+
+const App = () => (
+  <GlobalContext.Provider value={useGlobalContextValue(window)}>
+    <HashRouter>
+      <Route path="*" component={FormList} />
+    </HashRouter>
+  </GlobalContext.Provider>
+);
 
 const container = document.getElementById('forms_container');
 
 if (container) {
   ReactDOM.render(
-    <HashRouter>
-      <Route path="*" component={FormList} />
-    </HashRouter>,
+    <App />,
     container
   );
 }
