14974710ae69330072a3a682644050c44bd3a601
Amine Ben hammou <amine@mailpoet.com>
2019-11-27 13:53:26 +0100

    Add context to settings entrypoint
    [MAILPOET-2389]

--- assets/js/src/settings/settings.jsx
+++ assets/js/src/settings/settings.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DefaultSender from 'settings/default_sender.jsx';
+import { GlobalContext, useGlobalContextValue } from 'context/index.jsx';
+
+const App = () => (
+  <GlobalContext.Provider value={useGlobalContextValue(window)}>
+    <DefaultSender
+      senderAddress={window.mailpoet_settings_sender_adddress}
+      senderName={window.mailpoet_settings_sender_name}
+      replyToAddress={window.mailpoet_settings_reply_to_address}
+      replyToName={window.mailpoet_settings_reply_to_name}
+      mssActive={window.mailpoet_mss_active}
+    />
+  </GlobalContext.Provider>
+);
 
 const settingsSenderContainer = document.getElementById('settings_sender_container');
 
 if (settingsSenderContainer) {
-  ReactDOM.render(
-    React.createElement(DefaultSender, {
-      senderAddress: window.mailpoet_settings_sender_adddress,
-      senderName: window.mailpoet_settings_sender_name,
-      replyToAddress: window.mailpoet_settings_reply_to_address,
-      replyToName: window.mailpoet_settings_reply_to_name,
-      mssActive: window.mailpoet_mss_active,
-    }),
-    settingsSenderContainer
-  );
+  ReactDOM.render(<App />, settingsSenderContainer);
 }
