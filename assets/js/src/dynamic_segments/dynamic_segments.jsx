1b5e371177c0613343f71ab58cd39426090c91d1
Amine Ben hammou <amine@mailpoet.com>
2019-11-27 14:04:44 +0100

    Add context to dynamic_segments entrypoint
    [MAILPOET-2389]

--- assets/js/src/dynamic_segments/dynamic_segments.jsx
+++ assets/js/src/dynamic_segments/dynamic_segments.jsx
@@ -4,19 +4,22 @@ import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import DynamicSegmentList from 'dynamic_segments/list.jsx';
 import DynamicSegmentForm from 'dynamic_segments/form.jsx';
+import { GlobalContext, useGlobalContextValue } from 'context/index.jsx';
+
+const App = () => (
+  <GlobalContext.Provider value={useGlobalContextValue(window)}>
+    <HashRouter>
+      <Switch>
+        <Route path="/new" component={DynamicSegmentForm} />
+        <Route path="/edit/:id" component={DynamicSegmentForm} />
+        <Route path="*" component={DynamicSegmentList} />
+      </Switch>
+    </HashRouter>
+  </GlobalContext.Provider>
+);
 
 const container = document.getElementById('dynamic_segments_container');
 
 if (container) {
-  ReactDOM.render(
-    (
-      <HashRouter>
-        <Switch>
-          <Route path="/new" component={DynamicSegmentForm} />
-          <Route path="/edit/:id" component={DynamicSegmentForm} />
-          <Route path="*" component={DynamicSegmentList} />
-        </Switch>
-      </HashRouter>
-    ), container
-  );
+  ReactDOM.render(<App />, container);
 }
