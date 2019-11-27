32bf0c66d36d45a5b1d85678da754616cf25d671
Amine Ben hammou <amine@mailpoet.com>
2019-11-27 13:32:01 +0100

    Refactor newsletters entry point
    [MAILPOET-2389]

--- assets/js/src/newsletters/newsletters.jsx
+++ assets/js/src/newsletters/newsletters.jsx
@@ -5,7 +5,6 @@ import {
 } from 'react-router-dom';
 import Hooks from 'wp-js-hooks';
 import _ from 'underscore';
-import PropTypes from 'prop-types';
 
 import NewsletterTypes from 'newsletters/types.jsx';
 import NewsletterTemplates from 'newsletters/templates.jsx';
@@ -22,18 +21,6 @@ import NewsletterListNotificationHistory from 'newsletters/listings/notification
 import NewsletterSendingStatus from 'newsletters/sending_status.jsx';
 import CampaignStatsPage from 'newsletters/campaign_stats/page.jsx';
 
-class App extends React.Component {
-  render() {
-    return this.props.children;
-  }
-}
-
-App.propTypes = {
-  children: PropTypes.element.isRequired,
-};
-
-const container = document.getElementById('newsletters_container');
-
 const getAutomaticEmailsRoutes = () => {
   if (!window.mailpoet_automatic_emails) return null;
 
@@ -47,86 +34,89 @@ const getAutomaticEmailsRoutes = () => {
   }));
 };
 
+const routes = Hooks.applyFilters('mailpoet_newsletters_before_router', [
+  /* Listings */
+  {
+    path: '/standard/(.*)?',
+    component: NewsletterListStandard,
+  },
+  {
+    path: '/welcome/(.*)?',
+    component: NewsletterListWelcome,
+  },
+  {
+    path: '/notification/history/:parent_id/(.*)?',
+    component: NewsletterListNotificationHistory,
+  },
+  {
+    path: '/notification/(.*)?',
+    component: NewsletterListNotification,
+  },
+  /* New newsletter: types */
+  {
+    path: '/new/standard',
+    component: NewsletterTypeStandard,
+  },
+  {
+    path: '/new/notification',
+    component: NewsletterTypeNotification,
+  },
+  {
+    path: '/new/welcome',
+    component: NewsletterTypeWelcome,
+  },
+  /* Newsletter: type selection */
+  {
+    path: '/new',
+    component: NewsletterTypes,
+  },
+  /* Template selection */
+  {
+    name: 'template',
+    path: '/template/:id',
+    component: NewsletterTemplates,
+  },
+  /* congratulate */
+  {
+    path: '/send/congratulate/:id',
+    component: NewsletterCongratulate,
+  },
+  /* Sending options */
+  {
+    path: '/send/:id',
+    component: NewsletterSend,
+  },
+  {
+    path: '/sending-status/:id/(.*)?',
+    component: NewsletterSendingStatus,
+  },
+  {
+    path: '/stats/:id/(.*)?',
+    component: CampaignStatsPage,
+  },
+  ...getAutomaticEmailsRoutes(),
+]);
+
+const App = () => (
+  <HashRouter>
+    <Switch>
+      <Route exact path="/" render={() => <Redirect to="/standard" />} />
+      {routes.map((route) => (
+        <Route
+          key={route.path}
+          path={route.path}
+          component={route.component}
+          name={route.name || null}
+          data={route.data || null}
+          render={route.render}
+        />
+      ))}
+    </Switch>
+  </HashRouter>
+);
+
+const container = document.getElementById('newsletters_container');
 if (container) {
-  let routes = [
-    /* Listings */
-    {
-      path: '/standard/(.*)?',
-      component: NewsletterListStandard,
-    },
-    {
-      path: '/welcome/(.*)?',
-      component: NewsletterListWelcome,
-    },
-    {
-      path: '/notification/history/:parent_id/(.*)?',
-      component: NewsletterListNotificationHistory,
-    },
-    {
-      path: '/notification/(.*)?',
-      component: NewsletterListNotification,
-    },
-    /* New newsletter: types */
-    {
-      path: '/new/standard',
-      component: NewsletterTypeStandard,
-    },
-    {
-      path: '/new/notification',
-      component: NewsletterTypeNotification,
-    },
-    {
-      path: '/new/welcome',
-      component: NewsletterTypeWelcome,
-    },
-    /* Newsletter: type selection */
-    {
-      path: '/new',
-      component: NewsletterTypes,
-    },
-    /* Template selection */
-    {
-      name: 'template',
-      path: '/template/:id',
-      component: NewsletterTemplates,
-    },
-    /* congratulate */
-    {
-      path: '/send/congratulate/:id',
-      component: NewsletterCongratulate,
-    },
-    /* Sending options */
-    {
-      path: '/send/:id',
-      component: NewsletterSend,
-    },
-    {
-      path: '/sending-status/:id/(.*)?',
-      component: NewsletterSendingStatus,
-    },
-    {
-      path: '/stats/:id/(.*)?',
-      component: CampaignStatsPage,
-    },
-  ];
-
-  routes = Hooks.applyFilters('mailpoet_newsletters_before_router', [...routes, ...getAutomaticEmailsRoutes()]);
-
-  window.mailpoet_listing = ReactDOM.render(( // eslint-disable-line react/no-render-return-value
-    <HashRouter>
-      <Switch>
-        <Route exact path="/" render={() => <Redirect to="/standard" />} />
-        {routes.map((route) => (
-          <Route
-            key={route.path}
-            path={route.path}
-            component={route.component}
-            name={route.name || null}
-            data={route.data || null}
-            render={route.render}
-          />
-        ))}
-      </Switch>
-    </HashRouter>
-  ), container);
+  // eslint-disable-line react/no-render-return-value
+  window.mailpoet_listing = ReactDOM.render(<App />, container);
 }
