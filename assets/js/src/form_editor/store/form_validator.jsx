a356805591ce51c1c30a4369daad8e157ffb6150
Rostislav Wolny <rosta@mailpoet.com>
2019-12-02 17:20:50 +0100

    Add validation of missing obligatory blocks
    [MAILPOET-2451]

--- assets/js/src/form_editor/store/form_validator.jsx
+++ assets/js/src/form_editor/store/form_validator.jsx
@@ -1,7 +1,15 @@
-export default (formData) => {
+export default (formData, formBlocks) => {
   const errors = [];
   if (!formData.settings.segments || formData.settings.segments.length === 0) {
     errors.push('missing-lists');
+  }
+  const emailInput = formBlocks.filter((block) => (block.attributes.id === 'email'));
+  const submit = formBlocks.filter((block) => (block.attributes.id === 'submit'));
+  if (!emailInput) {
+    errors.push('missing-email-input');
+  }
+  if (!submit) {
+    errors.push('missing-submit');
   }
   return errors;
 };
--- assets/js/src/form_editor/store/reducers/save_form_started.jsx
+++ assets/js/src/form_editor/store/reducers/save_form_started.jsx
@@ -2,7 +2,7 @@ import MailPoet from 'mailpoet';
 
 export default (state) => {
   // remove all form saving related notices
-  const notices = state.notices.filter((notice) => !['save-form', 'missing-lists'].includes(notice.id));
+  const notices = state.notices.filter((notice) => !['save-form', 'missing-lists', 'missing-block'].includes(notice.id));
   const hasMissingLists = state.formErrors.includes('missing-lists');
   const sidebarOpenedPanels = [...state.sidebar.openedPanels];
   if (hasMissingLists) {
@@ -15,6 +15,17 @@ export default (state) => {
     if (!sidebarOpenedPanels.includes('basic-settings')) {
       sidebarOpenedPanels.push('basic-settings');
     }
+  }
+
+  const hasMissingEmail = state.formErrors.includes('missing-email-input');
+  const hasMissingSubmit = state.formErrors.includes('missing-submit');
+  if (hasMissingEmail || hasMissingSubmit) {
+    notices.push({
+      id: 'missing-block',
+      content: MailPoet.I18n.t('missingObligatoryBlock'),
+      isDismissible: true,
+      status: 'error',
+    });
   }
 
   return {
