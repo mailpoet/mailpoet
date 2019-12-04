612f436dbad621b455e62a077aaecdd37b18cc77
Pavel Dohnal <pavel@mailpoet.com>
2019-12-04 10:21:12 +0100

    Register first name block
    [MAILPOET-2452]

--- assets/js/src/form_editor/blocks/blocks.jsx
+++ assets/js/src/form_editor/blocks/blocks.jsx
@@ -1,13 +1,17 @@
 import { registerBlockType, setCategories } from '@wordpress/blocks';
+import MailPoet from 'mailpoet';
 
 import * as email from './email/email.jsx';
 import * as submit from './submit/submit.jsx';
+import * as firstName from './first_name/first_name.jsx';
 
 export default () => {
   setCategories([
     { slug: 'obligatory', title: '' }, // Blocks from this category are not in block insert popup
+    { slug: 'fields', title: MailPoet.I18n.t('fieldsBlocksCategory') },
   ]);
 
   registerBlockType(email.name, email.settings);
   registerBlockType(submit.name, submit.settings);
+  registerBlockType(firstName.name, firstName.settings);
 };
