147f27152712052a7a7d09dc640273db2512ada1
Pavel Dohnal <pavel@mailpoet.com>
2019-12-10 13:46:14 +0100

    Remove beautiful drag and drop
    [MAILPOET-2452]

--- assets/js/src/form_editor/blocks/segment_select/settings_preview.jsx
+++ assets/js/src/form_editor/blocks/segment_select/settings_preview.jsx
@@ -2,25 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CheckboxControl, Dashicon } from '@wordpress/components';
 import { partial } from 'lodash';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-
-const getItemStyle = (isDragging, draggableStyle) => ({
-  userSelect: 'none',
-  background: isDragging ? 'lightblue' : 'rgba(0,0,0,0)',
-  ...draggableStyle,
-});
-
-const getListStyle = (isDraggingOver) => ({
-  background: isDraggingOver ? 'lightgrey' : 'rgba(0,0,0,0)',
-});
-
-const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-
-  return result;
-};
 
 const Preview = ({
   segments,
@@ -38,63 +19,25 @@ const Preview = ({
     updateSegment(segment);
   };
 
-  const onDragEnd = (result) => {
-    if (!result.destination) {
-      return;
-    }
-
-    onSegmentsReorder(reorder(
-      segments,
-      result.source.index,
-      result.destination.index
-    ));
-  };
-
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="droppable">
-        {(providedDroppable, snapshotDroppable) => (
-          <div
-            {...providedDroppable.droppableProps}
-            ref={providedDroppable.innerRef}
-            style={getListStyle(snapshotDroppable.isDraggingOver)}
-          >
-            {segments.map((segment, index) => (
-              <Draggable key={segment.id} draggableId={segment.id} index={index}>
-                {(providedDraggable, snapshotDraggable) => (
-                  <div
-                    className="mailpoet-form-segments-settings-list"
-                    key={segment.id}
-                    ref={providedDraggable.innerRef}
-                    {...providedDraggable.draggableProps}
-                    {...providedDraggable.dragHandleProps}
-                    style={getItemStyle(
-                      snapshotDraggable.isDragging,
-                      providedDraggable.draggableProps.style
-                    )}
-                  >
-                    <CheckboxControl
-                      label={segment.name}
-                      checked={!!segment.isChecked}
-                      onChange={partial(onCheck, segment.id)}
-                      key={`check-${segment.id}`}
-                    />
-                    <Dashicon
-                      icon="no-alt"
-                      color="#900"
-                      className="mailpoet-form-segments-segment-remove"
-                      onClick={partial(removeSegment, segment.id)}
-                    />
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {providedDroppable.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
-  );
+  return segments.map((segment) => (
+    <div
+      className="mailpoet-form-segments-settings-list"
+      key={segment.id}
+    >
+      <CheckboxControl
+        label={segment.name}
+        checked={!!segment.isChecked}
+        onChange={partial(onCheck, segment.id)}
+        key={`check-${segment.id}`}
+      />
+      <Dashicon
+        icon="no-alt"
+        color="#900"
+        className="mailpoet-form-segments-segment-remove"
+        onClick={partial(removeSegment, segment.id)}
+      />
+    </div>
+  ));
 };
 
 Preview.propTypes = {
