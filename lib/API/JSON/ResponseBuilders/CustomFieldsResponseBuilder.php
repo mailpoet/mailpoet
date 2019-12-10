f47cec32d9cc12a7d395e57aacb32126038b8a1b
Pavel Dohnal <pavel@mailpoet.com>
2019-12-10 17:27:24 +0100

    Send custom fields into template
    [MAILPOET-2453]

---
+++ lib/API/JSON/ResponseBuilders/CustomFieldsResponseBuilder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MailPoet\API\JSON\ResponseBuilders;
+
+use MailPoet\Entities\CustomFieldEntity;
+
+class CustomFieldsResponseBuilder {
+
+  /**
+   * @param CustomFieldEntity[] $custom_fields
+   * @return array
+   */
+  function buildBatch(array $custom_fields) {
+    return array_map([$this, 'build'], $custom_fields);
+  }
+
+  /**
+   * @param CustomFieldEntity $custom_field
+   * @return array
+   */
+  function build(CustomFieldEntity $custom_field) {
+    return [
+      'id' => $custom_field->getId(),
+      'name' => $custom_field->getName(),
+      'type' => $custom_field->getType(),
+      'params' => $custom_field->getParams(),
+      'created_at' => $custom_field->getCreatedAt()->format(DATE_ATOM),
+      'updated_at' => $custom_field->getUpdatedAt()->format(DATE_ATOM),
+    ];
+  }
+}
