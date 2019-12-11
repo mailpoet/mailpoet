22452af49450acd90eb574cd90872bc073eec2dc
Pavel Dohnal <pavel@mailpoet.com>
2019-12-11 16:32:19 +0100

    Add textarea custom field
    [MAILPOET-2453]

---
+++ assets/js/src/form_editor/blocks/custom_textarea/custom_textarea.jsx
@@ -0,0 +1,44 @@
+import Icon from '../custom_text/icon.jsx';
+import Edit from './edit.jsx';
+
+export const name = 'mailpoet-form/custom-textarea';
+
+export function getSettings(customField) {
+  return {
+    title: customField.name,
+    description: '',
+    icon: Icon,
+    category: 'custom-fields',
+    attributes: {
+      label: {
+        type: 'string',
+        default: customField.name,
+      },
+      labelWithinInput: {
+        type: 'boolean',
+        default: true,
+      },
+      mandatory: {
+        type: 'boolean',
+        default: false,
+      },
+      validate: {
+        type: 'string',
+        default: '',
+      },
+      customFieldId: {
+        type: 'string',
+        default: customField.id,
+      },
+    },
+    supports: {
+      html: false,
+      customClassName: false,
+      multiple: false,
+    },
+    edit: Edit,
+    save() {
+      return null;
+    },
+  };
+}
