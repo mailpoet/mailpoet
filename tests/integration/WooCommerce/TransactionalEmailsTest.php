fa81c24825142aaf86ae3f5f4f9491956bbed6a7
wxa <wxa@users.noreply.github.com>
2019-12-12 15:11:05 +0300

    Sync WC customizer styles to WooCommerce settings [MAILPOET-2569]

--- tests/integration/WooCommerce/TransactionalEmailsTest.php
+++ tests/integration/WooCommerce/TransactionalEmailsTest.php
@@ -84,6 +84,92 @@ class TransactionalEmailsTest extends \MailPoetTest {
     expect(json_encode($email->getBody()))->contains('my-awesome-image-url');
   }
 
+  function testItSynchronizesEmailSettingsToWooCommerce() {
+    $newsletter = new NewsletterEntity;
+    $newsletter->setType(Newsletter::TYPE_WC_TRANSACTIONAL_EMAIL);
+    $newsletter->setSubject('WooCommerce Transactional Email');
+    $newsletter->setBody([
+      'globalStyles' => [
+        'text' =>
+         [
+          'fontColor' => '#111111',
+          'fontFamily' => 'Arial',
+          'fontSize' => '14px',
+          'lineHeight' => '1.6',
+         ],
+        'h1' =>
+         [
+          'fontColor' => '#222222',
+          'fontFamily' => 'Source Sans Pro',
+          'fontSize' => '36px',
+          'lineHeight' => '1.6',
+         ],
+        'h2' =>
+         [
+          'fontColor' => '#333333',
+          'fontFamily' => 'Verdana',
+          'fontSize' => '24px',
+          'lineHeight' => '1.6',
+         ],
+        'h3' =>
+         [
+          'fontColor' => '#444444',
+          'fontFamily' => 'Trebuchet MS',
+          'fontSize' => '22px',
+          'lineHeight' => '1.6',
+         ],
+        'link' =>
+         [
+          'fontColor' => '#555555',
+          'textDecoration' => 'underline',
+         ],
+        'wrapper' =>
+         [
+          'backgroundColor' => '#666666',
+         ],
+        'body' =>
+         [
+          'backgroundColor' => '#777777',
+         ],
+        'woocommerce' =>
+         [
+          'brandingColor' => '#888888',
+          'headingFontColor' => '#999999',
+         ],
+      ],
+    ]);
+    $this->newsletters_repository->persist($newsletter);
+    $this->newsletters_repository->flush();
+
+    $options = [];
+    $wp = Stub::make(new WPFunctions, [
+      'updateOption' => function($name, $value) use (&$options) {
+        $options[$name] = $value;
+      },
+      'getOption' => function($name) use (&$options) {
+        return $options[$name];
+      },
+    ]);
+
+    $transactional_emails = new TransactionalEmails(
+      $wp,
+      $this->settings,
+      ContainerWrapper::getInstance()->get(Template::class),
+      ContainerWrapper::getInstance()->get(Renderer::class),
+      Stub::makeEmpty(WooCommerceHelper::class),
+      $this->newsletters_repository
+    );
+    $transactional_emails->enableEmailSettingsSyncToWooCommerce();
+
+    $newsletter = Newsletter::findOne($newsletter->getId());
+    $wp->doAction('mailpoet_api_newsletters_save_after', $newsletter);
+
+    expect($wp->getOption('woocommerce_email_background_color'))->equals('#777777');
+    expect($wp->getOption('woocommerce_email_base_color'))->equals('#888888');
+    expect($wp->getOption('woocommerce_email_body_background_color'))->equals('#666666');
+    expect($wp->getOption('woocommerce_email_text_color'))->equals('#111111');
+  }
+
   function testUseTemplateForWCEmails() {
     $added_actions = [];
     $removed_actions = [];
