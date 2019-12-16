6e74851850e288bffdc3666c9c0d5c5fab27ff26
Jan Jakeš <jan@jakes.pro>
2019-12-16 14:57:13 +0100

    Remove no longer relevant test
    [MAILPOET-2539]

--- tests/integration/Cron/Workers/AuthorizedSendingEmailsCheckTest.php
+++ tests/integration/Cron/Workers/AuthorizedSendingEmailsCheckTest.php
@@ -6,17 +6,11 @@ use Codeception\Stub;
 use MailPoet\Cron\Workers\AuthorizedSendingEmailsCheck;
 use MailPoet\Models\ScheduledTask;
 use MailPoet\Services\AuthorizedEmailsController;
-use MailPoet\Settings\SettingsController;
 use MailPoetVendor\Idiorm\ORM;
 
 class AuthorizedSendingEmailsCheckTest extends \MailPoetTest {
-
-  /** @var SettingsController */
-  private $settings;
-
   function _before() {
     parent::_before();
-    $this->settings = SettingsController::getInstance();
     ORM::raw_execute('TRUNCATE ' . ScheduledTask::$_table);
   }
 
@@ -24,18 +18,5 @@ class AuthorizedSendingEmailsCheckTest extends \MailPoetTest {
     $bridge_mock = $this->makeEmpty(AuthorizedEmailsController::class, ['checkAuthorizedEmailAddresses' => Stub\Expected::once()]);
     $worker = new AuthorizedSendingEmailsCheck($bridge_mock);
     $worker->processTaskStrategy(ScheduledTask::createOrUpdate([]), microtime(true));
-  }
-
-  function testItDoesNotScheduleAutomatically() {
-    $this->settings->set('mta_group', 'mailpoet');
-    $this->settings->set('mta.method', 'MailPoet');
-    $bridge_mock = $this->makeEmpty(AuthorizedEmailsController::class, ['checkAuthorizedEmailAddresses' => Stub\Expected::never()]);
-    $worker = new AuthorizedSendingEmailsCheck($bridge_mock);
-    $worker->process();
-
-    $task = ScheduledTask::where('type', AuthorizedSendingEmailsCheck::TASK_TYPE)
-      ->where('status', ScheduledTask::STATUS_SCHEDULED)
-      ->findOne();
-    expect($task)->false();
   }
 }
