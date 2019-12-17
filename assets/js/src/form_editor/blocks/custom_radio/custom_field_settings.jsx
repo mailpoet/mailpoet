d550ba78a0d775661ad3adeba4b4cc1e5411abff
Pavel Dohnal <pavel@mailpoet.com>
2019-12-17 16:43:39 +0100

    Add options to radio custom field
    [MAILPOET-2453]

--- assets/js/src/form_editor/blocks/custom_radio/custom_field_settings.jsx
+++ assets/js/src/form_editor/blocks/custom_radio/custom_field_settings.jsx
@@ -6,20 +6,40 @@ import {
 import PropTypes from 'prop-types';
 import MailPoet from 'mailpoet';
 
+import SettingsPreview from './settings_preview.jsx';
+
 const CustomFieldSettings = ({
   mandatory,
+  values,
   isSaving,
   onSave,
 }) => {
   const [localMandatory, setLocalMandatory] = useState(mandatory);
+  const [localValues, setLocalValues] = useState(values);
+
+  const update = (value) => {
+    setLocalValues(localValues.map((valueInSelection) => {
+      if (value.id !== valueInSelection.id) {
+        return valueInSelection;
+      }
+      return value;
+    }));
+  };
+
+  const remove = (valueId) => {
+    setLocalValues(
+      localValues.filter((value) => valueId !== value.id)
+    );
+  };
 
   return (
-    <>
+    <div className="custom-field-settings">
       <Button
         isPrimary
         isDefault
         onClick={() => onSave({
           mandatory: localMandatory,
+          values: localValues,
         })}
         isBusy={isSaving}
         disabled={isSaving}
@@ -27,17 +47,40 @@ const CustomFieldSettings = ({
       >
         {MailPoet.I18n.t('customFieldSaveCTA')}
       </Button>
+      <SettingsPreview
+        remove={remove}
+        update={update}
+        values={localValues}
+        onReorder={setLocalValues}
+      />
+      <Button
+        isLink
+        onClick={() => setLocalValues([
+          ...localValues,
+          {
+            id: `${Math.random() * 1000}-${Date.now()}`,
+            name: `Option ${localValues.length + 1}`,
+          },
+        ])}
+        className="button-on-top"
+      >
+        {MailPoet.I18n.t('customFieldAddItem')}
+      </Button>
       <ToggleControl
         label={MailPoet.I18n.t('blockMandatory')}
         checked={localMandatory}
         onChange={setLocalMandatory}
       />
-    </>
+    </div>
   );
 };
 
 CustomFieldSettings.propTypes = {
   mandatory: PropTypes.bool,
+  values: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+  })),
   onSave: PropTypes.func.isRequired,
   isSaving: PropTypes.bool,
 };
@@ -45,6 +88,7 @@ CustomFieldSettings.propTypes = {
 CustomFieldSettings.defaultProps = {
   mandatory: false,
   isSaving: false,
+  values: [],
 };
 
 export default CustomFieldSettings;
