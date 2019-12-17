aef6832ca0183b5593adef668017c1d308019d7c
Rostislav Wolny <rosta@mailpoet.com>
2019-12-17 17:32:41 +0100

    Add custom date block with simple text inputs settings
    [MAILPOET-2453]

---
+++ assets/js/src/form_editor/blocks/custom_date/custom_date.jsx
@@ -0,0 +1,48 @@
+import Icon from '../custom_text/icon.jsx';
+import Edit from './edit.jsx';
+
+export const name = 'mailpoet-form/custom-date';
+
+export function getSettings(customField) {
+  return {
+    title: customField.name,
+    description: '',
+    icon: Icon,
+    category: 'custom-fields',
+    attributes: {
+      label: {
+        type: 'string',
+        default: customField.name,
+      },
+      mandatory: {
+        type: 'boolean',
+        default: false,
+      },
+      defaultToday: {
+        type: 'boolean',
+        default: false,
+      },
+      dateType: {
+        type: 'string',
+        default: customField.params.date_type,
+      },
+      dateFormat: {
+        type: 'string',
+        default: customField.params.date_format,
+      },
+      customFieldId: {
+        type: 'string',
+        default: customField.id,
+      },
+    },
+    supports: {
+      html: false,
+      customClassName: false,
+      multiple: false,
+    },
+    edit: Edit,
+    save() {
+      return null;
+    },
+  };
+}
