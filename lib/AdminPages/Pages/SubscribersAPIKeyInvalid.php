7dca67abf76a429aba8a853754c354c52fd2152c
wxa <wxa@users.noreply.github.com>
2019-12-18 08:55:44 +0300

    Link to MSS signup with prefilled email address [MAILPOET-2484]

--- lib/AdminPages/Pages/SubscribersAPIKeyInvalid.php
+++ lib/AdminPages/Pages/SubscribersAPIKeyInvalid.php
@@ -4,17 +4,23 @@ namespace MailPoet\AdminPages\Pages;
 
 use MailPoet\AdminPages\PageRenderer;
 use MailPoet\Models\Subscriber;
+use MailPoet\WP\Functions as WPFunctions;
 
 class SubscribersAPIKeyInvalid {
   /** @var PageRenderer */
   private $page_renderer;
 
-  function __construct(PageRenderer $page_renderer) {
+  /** @var WPFunctions */
+  private $wp;
+
+  function __construct(PageRenderer $page_renderer, WPFunctions $wp) {
     $this->page_renderer = $page_renderer;
+    $this->wp = $wp;
   }
 
   function render() {
     $this->page_renderer->displayPage('invalidkey.html', [
+      'current_wp_user' => $this->wp->wpGetCurrentUser()->to_array(),
       'subscriber_count' => Subscriber::getTotalSubscribers(),
     ]);
   }
