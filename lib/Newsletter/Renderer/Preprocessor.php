7a940ecb392d64a08094e02276bebb4f10287f44
wxa <wxa@users.noreply.github.com>
2019-12-18 14:52:44 +0300

    Fix WC heading padding in rendering [MAILPOET-2569]

--- lib/Newsletter/Renderer/Preprocessor.php
+++ lib/Newsletter/Renderer/Preprocessor.php
@@ -10,6 +10,15 @@ class Preprocessor {
   const WC_HEADING_PLACEHOLDER = '[mailpet_woocommerce_heading_placeholder]';
   const WC_CONTENT_PLACEHOLDER = '[mailpet_woocommerce_content_placeholder]';
 
+  const WC_HEADING_BEFORE = '
+    <table width="100%" border="0" cellpadding="0" cellspacing="0" style="border-spacing:0;mso-table-lspace:0;mso-table-rspace:0">
+            <tr>
+              <td class="mailpoet_text" valign="top" style="padding-top:20px;padding-bottom:20px;word-break:break-word;word-wrap:break-word;">';
+  const WC_HEADING_AFTER = '
+        </td>
+      </tr>
+    </table>';
+
   /** @var BlocksRenderer */
   private $blocks_renderer;
 
@@ -47,7 +56,7 @@ class Preprocessor {
         return $this->blocks_renderer->automatedLatestContentTransformedPosts($block);
       case 'woocommerceHeading':
         $wc_email_settings = $this->transactional_emails->getWCEmailSettings();
-        $content = '<h1 style="color:' . $wc_email_settings['base_text_color'] . ';">' . self::WC_HEADING_PLACEHOLDER . '</h1>';
+        $content = self::WC_HEADING_BEFORE . '<h1 style="color:' . $wc_email_settings['base_text_color'] . ';">' . self::WC_HEADING_PLACEHOLDER . '</h1>' . self::WC_HEADING_AFTER;
         return $this->placeholder($content, ['backgroundColor' => $wc_email_settings['base_color']]);
       case 'woocommerceContent':
         return $this->placeholder(self::WC_CONTENT_PLACEHOLDER);
