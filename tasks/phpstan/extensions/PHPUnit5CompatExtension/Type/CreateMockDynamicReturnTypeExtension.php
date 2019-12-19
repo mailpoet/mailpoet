b9779f0632878c1d311da8bf64052f8f311c1045
Jan Jakeš <jan@jakes.pro>
2019-12-19 13:40:04 +0100

    Upgrade PHPUnit5CompatExtension
    [MAILPOET-2588]

--- tasks/phpstan/extensions/PHPUnit5CompatExtension/Type/CreateMockDynamicReturnTypeExtension.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MailPoet\PHPStan\Extensions\PHPUnit5CompatExtension\Type;
-
-class CreateMockDynamicReturnTypeExtension extends \PHPStan\Type\PHPUnit\CreateMockDynamicReturnTypeExtension {
-  public function getClass(): string {
-    return 'PHPUnit_Framework_TestCase';
-  }
-}
--- tasks/phpstan/extensions/PHPUnit5CompatExtension/Type/GetMockBuilderDynamicReturnTypeExtension.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MailPoet\PHPStan\Extensions\PHPUnit5CompatExtension\Type;
-
-class GetMockBuilderDynamicReturnTypeExtension extends \PHPStan\Type\PHPUnit\GetMockBuilderDynamicReturnTypeExtension {
-  public function getClass(): string {
-    return 'PHPUnit_Framework_TestCase';
-  }
-}
