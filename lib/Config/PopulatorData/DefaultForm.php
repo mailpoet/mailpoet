462e38054a879a4f8dc43df54ad27c979f8c6a39
Pavel Dohnal <pavel@mailpoet.com>
2020-01-07 16:08:45 +0100

    Update default form copy
    [MAILPOET-2592]

--- lib/Config/PopulatorData/DefaultForm.php
+++ lib/Config/PopulatorData/DefaultForm.php
@@ -16,7 +16,7 @@ class DefaultForm {
   }
 
   public function getName() {
-    return WPFunctions::get()->_x('A GDPR friendly form', 'default name of form (GDPR friendly) to capture emails', 'mailpoet');
+    return WPFunctions::get()->_x('My first form', 'default name of form (GDPR friendly) to capture emails', 'mailpoet');
   }
 
   public function getBody() {
@@ -40,25 +40,13 @@ class DefaultForm {
         'position' => '2',
       ],
       [
-        'type' => 'html',
-        'name' => WPFunctions::get()->_x('Custom text or HTML', 'Form label', 'mailpoet'),
-        'id' => 'html',
-        'unique' => '0',
-        'static' => '0',
-        'params' => [
-          'text' => WPFunctions::get()->__('We keep your data private and share your data only with third parties that make this service possible. See our Privacy Policy for more information.', 'mailpoet'),
-          'nl2br' => '0',
-        ],
-        'position' => '3',
-      ],
-      [
         'type' => 'submit',
         'name' => WPFunctions::get()->_x('Submit', 'Form label', 'mailpoet'),
         'id' => 'submit',
         'unique' => '0',
         'static' => '1',
         'params' => ['label' => WPFunctions::get()->_x('Subscribe!', 'Form label', 'mailpoet')],
-        'position' => '4',
+        'position' => '3',
       ],
     ];
   }
