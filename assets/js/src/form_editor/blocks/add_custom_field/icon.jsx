d50cecc7a08c7c64e9e28a23c3666b2074ce3ada
Rostislav Wolny <rosta@mailpoet.com>
2020-01-09 15:12:15 +0100

    Add dummy add custom field block
    [MAILPOET-2463]

---
+++ assets/js/src/form_editor/blocks/add_custom_field/icon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { G, Path, SVG } from '@wordpress/components';
+
+export default (
+  <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 694 694">
+    <G>
+      <Path d="M347 0c31 0 56 25 56 55v237h236c30 0 55 25 55 55 0 31-25 56-55 56H403v236c0 30-25 55-56 55-30 0-55-25-55-55V403H55c-30 0-55-25-55-56 0-30 25-55 55-55h237V55c0-30 25-55 55-55z" fill-rule="evenodd" clip-rule="evenodd"/>
+    </G>
+  </SVG>
+);
