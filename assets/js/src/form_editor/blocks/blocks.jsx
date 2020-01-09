2a91f8a1b5bb9e37a644f03bb86d908b93ad8359
Pavel Dohnal <pavel@mailpoet.com>
2020-01-09 15:10:29 +0100

    Unregister block on custom field delete
    [MAILPOET-2463]

--- assets/js/src/form_editor/blocks/blocks.jsx
+++ assets/js/src/form_editor/blocks/blocks.jsx
@@ -2,6 +2,7 @@ import { registerBlockType, setCategories } from '@wordpress/blocks';
 import { select } from '@wordpress/data';
 import MailPoet from 'mailpoet';
 import formatCustomFieldBlockName from './format_custom_field_block_name.jsx';
+import getCustomFieldBlocksSettings from './custom_fields_blocks.jsx';
 
 import * as divider from './divider/divider.jsx';
 import * as email from './email/email.jsx';
@@ -11,40 +12,9 @@ import * as lastName from './last_name/last_name.jsx';
 import * as segmentSelect from './segment_select/segment_select.jsx';
 import * as html from './html/html.jsx';
 
-import * as customDate from './custom_date/custom_date.jsx';
-import * as customText from './custom_text/custom_text.jsx';
-import * as customTextArea from './custom_textarea/custom_textarea.jsx';
-import * as customRadio from './custom_radio/custom_radio.jsx';
-import * as customCheckbox from './custom_checkbox/custom_checkbox.jsx';
-import * as customSelect from './custom_select/custom_select.jsx';
 
 const registerCustomFieldBlock = (customField) => {
-  const namesMap = {
-    date: {
-      name: customDate.name,
-      settings: customDate.getSettings(customField),
-    },
-    text: {
-      name: customText.name,
-      settings: customText.getSettings(customField),
-    },
-    textarea: {
-      name: customTextArea.name,
-      settings: customTextArea.getSettings(customField),
-    },
-    radio: {
-      name: customRadio.name,
-      settings: customRadio.getSettings(customField),
-    },
-    checkbox: {
-      name: customCheckbox.name,
-      settings: customCheckbox.getSettings(customField),
-    },
-    select: {
-      name: customSelect.name,
-      settings: customSelect.getSettings(customField),
-    },
-  };
+  const namesMap = getCustomFieldBlocksSettings(customField);
 
   if (!namesMap[customField.type]) return;
 
---
+++ assets/js/src/form_editor/blocks/custom_fields_blocks.jsx
@@ -0,0 +1,35 @@
+import * as customDate from './custom_date/custom_date.jsx';
+import * as customText from './custom_text/custom_text.jsx';
+import * as customTextArea from './custom_textarea/custom_textarea.jsx';
+import * as customRadio from './custom_radio/custom_radio.jsx';
+import * as customCheckbox from './custom_checkbox/custom_checkbox.jsx';
+import * as customSelect from './custom_select/custom_select.jsx';
+
+export default function getCustomFieldBlocksSettings(customField) {
+  return {
+    date: {
+      name: customDate.name,
+      settings: customDate.getSettings(customField),
+    },
+    text: {
+      name: customText.name,
+      settings: customText.getSettings(customField),
+    },
+    textarea: {
+      name: customTextArea.name,
+      settings: customTextArea.getSettings(customField),
+    },
+    radio: {
+      name: customRadio.name,
+      settings: customRadio.getSettings(customField),
+    },
+    checkbox: {
+      name: customCheckbox.name,
+      settings: customCheckbox.getSettings(customField),
+    },
+    select: {
+      name: customSelect.name,
+      settings: customSelect.getSettings(customField),
+    },
+  };
+}
