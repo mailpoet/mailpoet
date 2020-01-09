f49757bd4e129d44102a932246c5e892d362054d
Jan Jakeš <jan@jakes.pro>
2020-01-09 15:29:31 +0100

    Convert variables not caught by Code Sniffer to camel case
    [MAILPOET-1796]

--- tests/DataFactories/WooCommerceOrder.php
+++ tests/DataFactories/WooCommerceOrder.php
@@ -20,7 +20,7 @@ class WooCommerceOrder {
   const STATUS_REFUNDED = 'refunded';
 
   public function __construct(\AcceptanceTester $tester) {
-    $unique_id = bin2hex(random_bytes(7)); // phpcs:ignore
+    $uniqueId = bin2hex(random_bytes(7)); // phpcs:ignore
     $this->tester = $tester;
     $this->data = [
       'customer_id' => self::GUEST_CUSTOMER_ID,
