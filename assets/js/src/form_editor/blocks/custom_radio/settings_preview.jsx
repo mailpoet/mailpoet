0994755f8ca0447a7bcadcba163e290a6a6fac83
Rostislav Wolny <rosta@mailpoet.com>
2020-01-14 20:46:41 +0100

    Use simple sortable library
    [MAILPOET-2463]

--- assets/js/src/form_editor/blocks/custom_radio/settings_preview.jsx
+++ assets/js/src/form_editor/blocks/custom_radio/settings_preview.jsx
@@ -1,104 +1,41 @@
 import React, {
-  useRef,
-  useCallback,
   useState,
   useEffect,
 } from 'react';
 import PropTypes from 'prop-types';
 import { Dashicon } from '@wordpress/components';
 import { partial } from 'lodash';
-import { useDrag, useDrop } from 'react-dnd';
-import DndProvider from '../dnd_provider.jsx';
+import { ReactSortable } from 'react-sortablejs';
 
 const PreviewItem = ({
   value,
-  index,
-  moveItem,
   remove,
   onUpdate,
   onCheck,
-  dragFinished,
-}) => {
-  const ref = useRef(null);
-  const [, drop] = useDrop({
-    accept: 'item',
-    hover(item, monitor) {
-      if (!ref.current) {
-        return;
-      }
-      const dragIndex = item.index;
-      const hoverIndex = index;
-      // Don't replace items with themselves
-      if (dragIndex === hoverIndex) {
-        return;
-      }
-      // Determine rectangle on screen
-      const hoverBoundingRect = ref.current.getBoundingClientRect();
-      // Get vertical middle
-      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-      // Determine mouse position
-      const clientOffset = monitor.getClientOffset();
-      // Get pixels to the top
-      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-      // Only perform the move when the mouse has crossed half of the items height
-      // When dragging downwards, only move when the cursor is below 50%
-      // When dragging upwards, only move when the cursor is above 50%
-      // Dragging downwards
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return;
-      }
-      // Dragging upwards
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return;
-      }
-      // Time to actually perform the action
-      moveItem(dragIndex, hoverIndex);
-      // Note: we're mutating the monitor item here!
-      // Generally it's better to avoid mutations,
-      // but it's good here for the sake of performance
-      // to avoid expensive index searches.
-      // eslint-disable-next-line no-param-reassign
-      item.index = hoverIndex;
-    },
-  });
-  const [{ isDragging }, drag] = useDrag({
-    item: { type: 'item', id: value.id, index },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-    end() {
-      dragFinished();
-    },
-  });
-  const opacity = isDragging ? 0.2 : 1;
-  drag(drop(ref));
-  return (
-    <div
-      className="mailpoet-form-segments-settings-list"
-      key={value.id}
-      ref={ref}
-      style={{ opacity }}
-    >
-      <input
-        type="checkbox"
-        checked={value.isChecked || false}
-        onChange={(event) => onCheck(value.id, event.target.checked)}
-        key={`check-${value.id}`}
-      />
-      <input
-        type="text"
-        value={value.name}
-        onChange={(event) => onUpdate(value.id, event.target.value)}
-      />
-      <Dashicon
-        icon="no-alt"
-        color="#900"
-        className="mailpoet-form-segments-segment-remove"
-        onClick={partial(remove, value.id)}
-      />
-    </div>
-  );
-};
+}) => (
+  <div
+    className="mailpoet-form-segments-settings-list"
+    key={value.id}
+  >
+    <input
+      type="checkbox"
+      checked={value.isChecked || false}
+      onChange={(event) => onCheck(value.id, event.target.checked)}
+      key={`check-${value.id}`}
+    />
+    <input
+      type="text"
+      value={value.name}
+      onChange={(event) => onUpdate(value.id, event.target.value)}
+    />
+    <Dashicon
+      icon="no-alt"
+      color="#900"
+      className="mailpoet-form-segments-segment-remove"
+      onClick={partial(remove, value.id)}
+    />
+  </div>
+);
 
 PreviewItem.propTypes = {
   value: PropTypes.shape({
@@ -108,10 +45,7 @@ PreviewItem.propTypes = {
   }).isRequired,
   onUpdate: PropTypes.func.isRequired,
   onCheck: PropTypes.func.isRequired,
-  moveItem: PropTypes.func.isRequired,
   remove: PropTypes.func.isRequired,
-  index: PropTypes.number.isRequired,
-  dragFinished: PropTypes.func.isRequired,
 };
 
 const Preview = ({
@@ -121,16 +55,6 @@ const Preview = ({
   onReorder,
 }) => {
   const [valuesWhileMoved, setValues] = useState(values);
-  const moveItem = useCallback(
-    (dragIndex, hoverIndex) => {
-      const result = Array.from(valuesWhileMoved);
-      const [removed] = result.splice(dragIndex, 1);
-      result.splice(hoverIndex, 0, removed);
-
-      setValues(result);
-    },
-    [valuesWhileMoved, setValues],
-  );
 
   useEffect(() => {
     setValues(values);
@@ -160,22 +84,20 @@ const Preview = ({
   };
 
   return (
-    <DndProvider>
-      <div className="mailpoet-dnd-items-list">
+    <div className="mailpoet-dnd-items-list">
+      <ReactSortable list={valuesWhileMoved} setList={onReorder}>
         {valuesWhileMoved.map((value, index) => (
           <PreviewItem
             key={value.id}
             index={index}
             value={value}
-            moveItem={moveItem}
             remove={remove}
             onCheck={onCheck}
             onUpdate={onUpdate}
-            dragFinished={() => onReorder(valuesWhileMoved)}
           />
         ))}
-      </div>
-    </DndProvider>
+      </ReactSortable>
+    </div>
   );
 };
 
--- assets/js/src/form_editor/blocks/segment_select/settings_preview.jsx
+++ assets/js/src/form_editor/blocks/segment_select/settings_preview.jsx
@@ -1,98 +1,35 @@
 import React, {
-  useRef,
-  useCallback,
   useState,
   useEffect,
 } from 'react';
 import PropTypes from 'prop-types';
 import { CheckboxControl, Dashicon } from '@wordpress/components';
 import { partial } from 'lodash';
-import { useDrag, useDrop } from 'react-dnd';
-import DndProvider from '../dnd_provider.jsx';
+import { ReactSortable } from 'react-sortablejs';
 
 const PreviewItem = ({
   segment,
-  index,
-  moveItem,
   removeSegment,
   onCheck,
-  dragFinished,
-}) => {
-  const ref = useRef(null);
-  const [, drop] = useDrop({
-    accept: 'item',
-    hover(item, monitor) {
-      if (!ref.current) {
-        return;
-      }
-      const dragIndex = item.index;
-      const hoverIndex = index;
-      // Don't replace items with themselves
-      if (dragIndex === hoverIndex) {
-        return;
-      }
-      // Determine rectangle on screen
-      const hoverBoundingRect = ref.current.getBoundingClientRect();
-      // Get vertical middle
-      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-      // Determine mouse position
-      const clientOffset = monitor.getClientOffset();
-      // Get pixels to the top
-      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-      // Only perform the move when the mouse has crossed half of the items height
-      // When dragging downwards, only move when the cursor is below 50%
-      // When dragging upwards, only move when the cursor is above 50%
-      // Dragging downwards
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return;
-      }
-      // Dragging upwards
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return;
-      }
-      // Time to actually perform the action
-      moveItem(dragIndex, hoverIndex);
-      // Note: we're mutating the monitor item here!
-      // Generally it's better to avoid mutations,
-      // but it's good here for the sake of performance
-      // to avoid expensive index searches.
-      // eslint-disable-next-line no-param-reassign
-      item.index = hoverIndex;
-    },
-  });
-  const [{ isDragging }, drag] = useDrag({
-    item: { type: 'item', id: segment.id, index },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-    end() {
-      dragFinished();
-    },
-  });
-  const opacity = isDragging ? 0.2 : 1;
-  drag(drop(ref));
-  return (
-    <div
-      className="mailpoet-form-segments-settings-list"
-      key={segment.id}
-      ref={ref}
-      style={{ opacity }}
-    >
-      <CheckboxControl
-        label={segment.name}
-        checked={!!segment.isChecked}
-        onChange={partial(onCheck, segment.id)}
-        key={`check-${segment.id}`}
-      />
-      <Dashicon
-        icon="no-alt"
-        color="#900"
-        className="mailpoet-form-segments-segment-remove"
-        onClick={partial(removeSegment, segment.id)}
-      />
-    </div>
-  );
-};
+}) => (
+  <div
+    className="mailpoet-form-segments-settings-list"
+    key={segment.id}
+  >
+    <CheckboxControl
+      label={segment.name}
+      checked={!!segment.isChecked}
+      onChange={partial(onCheck, segment.id)}
+      key={`check-${segment.id}`}
+    />
+    <Dashicon
+      icon="no-alt"
+      color="#900"
+      className="mailpoet-form-segments-segment-remove"
+      onClick={partial(removeSegment, segment.id)}
+    />
+  </div>
+);
 
 PreviewItem.propTypes = {
   segment: PropTypes.shape({
@@ -101,10 +38,7 @@ PreviewItem.propTypes = {
     id: PropTypes.string.isRequired,
   }).isRequired,
   onCheck: PropTypes.func.isRequired,
-  moveItem: PropTypes.func.isRequired,
   removeSegment: PropTypes.func.isRequired,
-  index: PropTypes.number.isRequired,
-  dragFinished: PropTypes.func.isRequired,
 };
 
 const Preview = ({
@@ -114,16 +48,6 @@ const Preview = ({
   onSegmentsReorder,
 }) => {
   const [segmentsWhileMoved, setSegments] = useState(segments);
-  const moveItem = useCallback(
-    (dragIndex, hoverIndex) => {
-      const result = Array.from(segmentsWhileMoved);
-      const [removed] = result.splice(dragIndex, 1);
-      result.splice(hoverIndex, 0, removed);
-
-      setSegments(result);
-    },
-    [segmentsWhileMoved, setSegments],
-  );
 
   useEffect(() => {
     setSegments(segments);
@@ -140,19 +64,17 @@ const Preview = ({
   };
 
   return (
-    <DndProvider>
+    <ReactSortable list={segmentsWhileMoved} setList={onSegmentsReorder}>
       {segmentsWhileMoved.map((segment, index) => (
         <PreviewItem
           key={segment.id}
           index={index}
           segment={segment}
-          moveItem={moveItem}
           onCheck={onCheck}
           removeSegment={removeSegment}
-          dragFinished={() => onSegmentsReorder(segmentsWhileMoved)}
         />
       ))}
-    </DndProvider>
+    </ReactSortable>
   );
 };
 
