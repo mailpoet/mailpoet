9c2e7235dee82cacf5380b2e7d62156907065dc9
Rostislav Wolny <rosta@mailpoet.com>
2020-01-21 10:49:37 +0100

    Make old form editor test run using old editor
    [MAILPOET-2549]

--- tests/acceptance/SubscriptionFormEditCest.php
+++ tests/acceptance/SubscriptionFormEditCest.php
@@ -12,13 +12,11 @@ class SubscriptionFormEditCest {
     $formName = 'Testing Form Edit';
     $formEditedName = 'Testing Form Edited';
     $formFactory = new Form();
-    $formFactory->withName($formName)->create();
+    $form = $formFactory->withName($formName)->create();
     //Step two - Edit the form title
     $i->wantTo('Edit a form');
     $i->login();
-    $i->amOnMailpoetPage('Forms');
-    $i->waitForText($formName);
-    $i->clickItemRowActionByItemName($formName, 'Edit');
+    $i->amOnPage('/wp-admin/admin.php?page=mailpoet-form-editor&legacy=1&id=' . $form->id);
     $titleElement = '[data-automation-id="mailpoet_form_name_input"]';
     $i->waitForElement($titleElement);
     $i->fillField($titleElement, $formEditedName);
