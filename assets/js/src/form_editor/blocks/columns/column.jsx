5f2aa1addaa7e4a164e3b12846c6870392187eda
Rostislav Wolny <rosta@mailpoet.com>
2020-01-27 17:43:50 +0100

    Add core\columns and core\column blocks to form editor
    [MAILPOET-2610]

---
+++ assets/js/src/form_editor/blocks/columns/column.jsx
@@ -0,0 +1,17 @@
+import * as column from '@wordpress/block-library/build-module/column/index.js';
+
+export const name = 'core/column';
+
+const settingsReset = {
+  name,
+  examples: null,
+  deprecated: null,
+  save: () => null,
+};
+const settings = {
+  ...column.metadata,
+  ...column.settings,
+  ...settingsReset,
+  category: 'layout',
+};
+export { settings };
