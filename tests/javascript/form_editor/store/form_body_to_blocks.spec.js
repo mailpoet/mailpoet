97f76717eb01cf893de35f0831cb82c6ff557188
Rostislav Wolny <rosta@mailpoet.com>
2020-01-28 15:32:08 +0100

    Move test data from form to blocks test to separate file
    [MAILPOET-2609]

--- tests/javascript/form_editor/store/form_body_to_blocks.spec.js
+++ tests/javascript/form_editor/store/form_body_to_blocks.spec.js
@@ -1,182 +1,20 @@
 import { expect } from 'chai';
 import { formBodyToBlocks } from '../../../../assets/js/src/form_editor/store/form_body_to_blocks.jsx';
 
-const emailInput = {
-  type: 'text',
-  name: 'Email',
-  id: 'email',
-  unique: '0',
-  static: '1',
-  params: {
-    label: 'Email',
-  },
-  position: null,
-};
-const firstNameInput = {
-  type: 'text',
-  name: 'First name',
-  id: 'first_name',
-  unique: '1',
-  static: '0',
-  params: {
-    label: 'First Name',
-  },
-  position: null,
-};
-const lastNameInput = {
-  type: 'text',
-  name: 'Last name',
-  id: 'last_name',
-  unique: '1',
-  static: '0',
-  params: {
-    label: 'Last Name',
-  },
-  position: null,
-};
-const segmentsInput = {
-  type: 'segment',
-  name: 'List selection',
-  id: 'segments',
-  unique: '1',
-  static: '0',
-  params: {
-    label: 'Select list(s):',
-    values: [
-      {
-        id: '6',
-        name: 'Unicorn Truthers',
-      },
-      {
-        id: '24',
-        is_checked: '1',
-        name: 'Carrots are lit',
-      },
-      {
-        id: '29',
-        name: 'Daily',
-      },
-    ],
-  },
-  position: null,
-};
-const submitInput = {
-  type: 'submit',
-  name: 'Submit',
-  id: 'submit',
-  unique: '0',
-  static: '1',
-  params: {
-    label: 'Subscribe!',
-  },
-  position: null,
-};
-const customTextInput = {
-  type: 'text',
-  name: 'Street name',
-  id: '1',
-  unique: '1',
-  static: '0',
-  params: {
-    required: '',
-    validate: 'alphanum',
-    label: 'Name of the street',
-    label_within: '1',
-  },
-  position: null,
-};
-const customRadioInput = {
-  type: 'radio',
-  name: 'Options',
-  id: '3',
-  unique: '1',
-  static: '0',
-  params: {
-    required: '',
-    label: 'Options',
-    hide_label: '1',
-    values: [
-      {
-        value: 'option 1',
-      },
-    ],
-  },
-  position: null,
-};
-const customSelectInput = {
-  type: 'select',
-  name: 'Custom select',
-  id: '5',
-  unique: '1',
-  static: '0',
-  params: {
-    required: '',
-    label: 'Select',
-    label_within: '1',
-    values: [
-      {
-        value: 'option 1',
-      },
-    ],
-  },
-  position: null,
-};
-const customCheckboxInput = {
-  type: 'checkbox',
-  name: 'Custom check',
-  id: '4',
-  unique: '1',
-  static: '0',
-  params: {
-    required: '',
-    label: 'Check this',
-    hide_label: '',
-    values: [
-      {
-        value: 'Check',
-        is_checked: '1',
-      },
-    ],
-  },
-  position: null,
-};
-const customDateInput = {
-  type: 'date',
-  name: 'Custom date',
-  id: '6',
-  unique: '1',
-  static: '0',
-  params: {
-    required: '1',
-    label: 'Date',
-    date_type: 'month_year',
-    date_format: 'MM/YYYY',
-    is_default_today: true,
-  },
-  position: null,
-};
-const divider = {
-  type: 'divider',
-  name: 'Divider',
-  id: 'divider',
-  unique: '0',
-  static: '0',
-  params: '',
-  position: null,
-};
-
-const customHtml = {
-  type: 'html,',
-  name: 'Custom text or HTML',
-  id: 'html',
-  unique: '0',
-  static: '0',
-  params: {
-    text: 'test',
-    nl2br: '1',
-  },
-  position: null,
-};
+import {
+  emailInput,
+  firstNameInput,
+  lastNameInput,
+  segmentsInput,
+  submitInput,
+  customTextInput,
+  customRadioInput,
+  customSelectInput,
+  customCheckboxInput,
+  customDateInput,
+  customHtml,
+  divider,
+} from './form_to_block_test_data.js';
 
 const checkBlockBasics = (block) => {
   expect(block.clientId).to.be.a('string');
