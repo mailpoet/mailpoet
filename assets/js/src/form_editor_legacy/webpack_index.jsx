f1fdaf117f75d03c9e421a3eab8838279cffde3e
Pavel Dohnal <pavel@mailpoet.com>
2020-01-29 13:53:36 +0100

    Remove the old form editor
    [MAILPOET-2625]

--- assets/js/src/form_editor_legacy/webpack_index.jsx
+++
@@ -1,9 +0,0 @@
-// Initialize Editor dependencies that have side effect (meaning they
-// not only define module but also modify/register something on load).
-
-// This is to avoid undefined import order & messy WebPack config.
-// Code can be gradually refactored to avoid side effects completely.
-
-import 'form_editor_legacy/form_editor.js'; // side effect - calls document.observe()
-import 'codemirror'; // side effect - has to be loaded here, used in 'editor.html'
-import 'codemirror/mode/css/css'; // side effect - has to be loaded here, used in 'editor.html'
