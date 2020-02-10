072c6e48a0b45ae60a0678d82965811c706247c8
Rostislav Wolny <rosta@mailpoet.com>
2020-02-10 17:03:31 +0100

    Add basic checkbox test
    [MAILPOET-265]

--- tests/unit/Form/HtmlParser.php
+++ tests/unit/Form/HtmlParser.php
@@ -9,4 +9,23 @@ class HtmlParser extends \MailPoetUnitTest {
     $value = (new \DOMXPath($dom))->query($xpath);
     return $value ?: new \DOMNodeList();
   }
+
+  public function getElementByXpath(string $html, string $xpath, int $index = 0): \DOMElement {
+    $value = $this->findByXpath($html, $xpath);
+    $element = $value->item($index);
+    assert($element instanceof \DOMElement);
+    return $element;
+  }
+
+  public function getChildElement(\DOMElement $element, string $tagName, int $index = 0): \DOMElement {
+    $result = $element->getElementsByTagName($tagName)->item($index);
+    assert($result instanceof \DOMElement);
+    return $result;
+  }
+
+  public function getAttribute(\DOMElement $element, string $attrNam): \DOMAttr {
+    $attr = $element->attributes->getNamedItem($attrNam);
+    assert($attr instanceof \DOMAttr);
+    return $attr;
+  }
 }
