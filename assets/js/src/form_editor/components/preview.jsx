ed5e98a30628218e8093824b23683dd6b328eb78
Pavel Dohnal <pavel@mailpoet.com>
2020-02-10 13:57:38 +0100

    Display preview
    [MAILPOET-2602]

--- assets/js/src/form_editor/components/preview.jsx
+++ assets/js/src/form_editor/components/preview.jsx
@@ -1,12 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import MailPoet from 'mailpoet';
 import { Spinner } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
 
 import Preview from '../../common/preview.jsx';
 import Modal from '../../common/modal/modal.jsx';
+import blocksToFormBody from '../store/blocks_to_form_body.jsx';
 
 const FormPreview = () => {
   const [form, setForm] = useState(null);
+
+  const formBlocks = useSelect(
+    (select) => select('mailpoet-form-editor').getFormBlocks(),
+    []
+  );
+  const customFields = useSelect(
+    (select) => select('mailpoet-form-editor').getAllAvailableCustomFields(),
+    []
+  );
 
   const { hidePreview } = useDispatch('mailpoet-form-editor');
   const isPreview = useSelect(
@@ -14,29 +25,53 @@ const FormPreview = () => {
     []
   );
 
-  function loadFormPreviewFromServer() {
-
-  }
+  const loadFormPreviewFromServer = useCallback(() => {
+    MailPoet.Ajax.post({
+      api_version: window.mailpoet_api_version,
+      endpoint: 'forms',
+      action: 'previewEditor',
+      data: {
+        body: blocksToFormBody(formBlocks, customFields),
+      },
+    }).done((response) => {
+      setForm(response.data);
+    });
+  }, [formBlocks, customFields]);
 
   useEffect(() => {
     if (isPreview) {
       loadFormPreviewFromServer();
     }
-  }, [isPreview]);
+  }, [isPreview, loadFormPreviewFromServer]);
 
   if (!isPreview) return null;
 
+  function onClose() {
+    setForm(null);
+    hidePreview();
+  }
+
   return (
     <Modal
-      title="Form Preview"
-      onRequestClose={hidePreview}
+      title={MailPoet.I18n.t('formPreview')}
+      onRequestClose={onClose}
     >
       <Preview>
         {form === null && (
           <Spinner />
         )}
         {form !== null && (
-          <div dangerouslySetInnerHTML={form} />
+          <div>
+            <style type="text/css" id="mailpoet_form_preview_styles">
+              {'.mailpoet_hp_email_label { display: none }' }
+              {form.css}
+            </style>
+            <div className="mailpoet_message">
+              <p className="mailpoet_validate_success">{MailPoet.I18n.t('successMessage')}</p>
+              <p className="mailpoet_validate_error">{MailPoet.I18n.t('errorMessage')}</p>
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: form.html }} />
+          </div>
         )}
       </Preview>
     </Modal>
