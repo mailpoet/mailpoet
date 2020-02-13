4afc47463f6227b9fc5e97be867a61e5bbebb044
Rostislav Wolny <rosta@mailpoet.com>
2020-02-13 11:24:45 +0100

    Add form renderer integration test
    [MAILPOET-2665]

---
+++ tests/integration/Form/RendererTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MailPoet\Form;
+
+use Codeception\Util\Fixtures;
+use MailPoet\DI\ContainerWrapper;
+
+class RendererTest extends \MailPoetTest {
+  public function testItRendersFormBody() {
+    $formBody = Fixtures::get('form_body_template');
+    $renderer = ContainerWrapper::getInstance()->get(Renderer::class);
+    assert($renderer instanceof Renderer);
+    $formHtml = $renderer->renderBlocks($formBody);
+    expect($formHtml)->contains('<input type="email" name="data[email]"/>'); // honey pot
+    expect($formHtml)->contains('input type="submit" class="mailpoet_submit" value="Subscribe!"'); // Subscribe button
+  }
+}
