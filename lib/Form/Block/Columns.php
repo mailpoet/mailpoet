9624509284ca0329ff56c270ef3dfbea26248ff4
Rostislav Wolny <rosta@mailpoet.com>
2020-02-18 16:21:19 +0100

    Add vertical alignment support to columns renderer
    [MAILPOET-2609]

--- lib/Form/Block/Columns.php
+++ lib/Form/Block/Columns.php
@@ -4,6 +4,15 @@ namespace MailPoet\Form\Block;
 
 class Columns {
   public function render(array $block, string $content): string {
-    return "<div class=\"mailpoet_form_columns\">$content</div>";
+    return "<div {$this->getClass($block['params'] ?? [])}>$content</div>";
+  }
+
+  private function getClass(array $params): string {
+    $classes = ['mailpoet_form_columns'];
+    if (!empty($params['vertical_alignment'])) {
+      $classes[] = "mailpoet_vertically_align_{$params['vertical_alignment']}";
+    }
+    $classes = implode(' ', $classes);
+    return "class=\"$classes\"";
   }
 }
--- tests/unit/Form/Block/ColumnsTest.php
+++ tests/unit/Form/Block/ColumnsTest.php
@@ -3,10 +3,16 @@
 namespace MailPoet\Test\Form\Block;
 
 use MailPoet\Form\Block\Columns;
+use MailPoet\Test\Form\HtmlParser;
+
+require_once __DIR__ . '/../HtmlParser.php';
 
 class ColumnsTest extends \MailPoetUnitTest {
   /** @var Columns */
   private $columns;
+
+  /** @var HtmlParser */
+  private $htmlParser;
 
   private $block = [
     'position' => '1',
@@ -16,10 +22,20 @@ class ColumnsTest extends \MailPoetUnitTest {
   public function _before() {
     parent::_before();
     $this->columns = new Columns();
+    $this->htmlParser = new HtmlParser();
   }
 
   public function testItShouldRenderColumns() {
     $html = $this->columns->render($this->block, 'content');
     expect($html)->equals('<div class="mailpoet_form_columns">content</div>');
+  }
+
+  public function testItShouldRenderVerticalAlignClass() {
+    $block = $this->block;
+    $block['params']['vertical_alignment'] = 'top';
+    $html = $this->columns->render($block, 'content');
+    $column = $this->htmlParser->getElementByXpath($html, '//div[1]');
+    $class = $this->htmlParser->getAttribute($column, 'class');
+    expect($class->textContent)->contains('mailpoet_vertically_align_top');
   }
 }
