4f057cca18edc52bcf2b3ae597893bb26d0906d4
Pavel Dohnal <pavel@mailpoet.com>
2020-02-25 09:52:15 +0100

    Add Form Styles Panel
    [MAILPOET-2600]

--- assets/js/src/form_editor/components/form_settings/form_settings.jsx
+++ assets/js/src/form_editor/components/form_settings/form_settings.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { partial } from 'lodash';
 import BasicSettingsPanel from './basic_settings_panel.jsx';
+import StylesSettingsPanel from './styles_settings_panel.jsx';
 import FormPlacementPanel from './form_placement_panel.jsx';
 import CustomCssPanel from './custom_css_panel.jsx';
 
@@ -18,6 +19,10 @@ export default () => {
       <BasicSettingsPanel
         isOpened={openedPanels.includes('basic-settings')}
         onToggle={partial(toggleSidebarPanel, 'basic-settings')}
+      />
+      <StylesSettingsPanel
+        isOpened={openedPanels.includes('styles-settings')}
+        onToggle={partial(toggleSidebarPanel, 'styles-settings')}
       />
       <FormPlacementPanel
         isOpened={openedPanels.includes('form-placement')}
