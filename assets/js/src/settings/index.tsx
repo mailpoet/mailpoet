408437af70a71590f095634f413659982836b255
Amine Ben hammou <amine@mailpoet.com>
2020-03-02 19:42:21 +0100

    Inject all settings
    [MAILPOET-2676]

--- assets/js/src/settings/index.tsx
+++ assets/js/src/settings/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <h1 className="title">Settings</h1>;
+const App = () => (
+  <>
+    <h1 className="title">Settings</h1>
+    <pre><code>{JSON.stringify((window as any).mailpoet_settings)}</code></pre>
+  </>
+);
 
 const container = document.getElementById('settings_container');
 if (container) {
