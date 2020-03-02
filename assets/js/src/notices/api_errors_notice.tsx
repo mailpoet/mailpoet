698c523d255cd00e9b2ecdd9f1a0a3686b3c64b0
Amine Ben hammou <amine@mailpoet.com>
2020-03-02 13:28:31 +0100

    Remove React.FC
    [MAILPOET-2658]

--- assets/js/src/notices/api_errors_notice.tsx
+++ assets/js/src/notices/api_errors_notice.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from 'react';
+import React from 'react';
 import Notice from 'notices/notice';
 
 type Props = {
   errors: Array<{ message: string }>
 }
 
-const APIErrorsNotice: FC<Props> = ({ errors }) => {
+const APIErrorsNotice = ({ errors }: Props) => {
   if (errors.length < 1) return null;
   return <Notice type="error" closable={false}>{errors.map((err) => <p key={err.message}>{err.message}</p>)}</Notice>;
 };
