c3fa66b11253e097f3e884116a84b1a38c7435f1
Amine Ben hammou <amine@mailpoet.com>
2020-03-05 01:19:57 +0100

    Remove temporary code
    [MAILPOET-2676]

--- assets/js/src/settings/index.tsx
+++ assets/js/src/settings/index.tsx
@@ -1,32 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { initStore } from './store';
-import {
-  useSelector, useActions, useSetting, useSettingSetter,
-} from './store/hooks';
 
-const App = () => {
-  const isSaving = useSelector('isSaving')();
-  const error = useSelector('getError')();
-  const settings = useSelector('getSettings')();
-  const email = useSetting('sender', 'address');
-  const setEmail = useSettingSetter('sender', 'address');
-  const actions = useActions();
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  };
-  const save = () => {
-    actions.saveSettings(settings);
-  };
-  return (
-    <>
-      <h1 className="title">Settings</h1>
-      <p>{JSON.stringify({ email, isSaving, error })}</p>
-      <input type="text" value={email} onChange={onChange} />
-      <button type="button" onClick={save}>Save</button>
-    </>
-  );
-};
+const App = () => null;
 
 const container = document.getElementById('settings_container');
 if (container) {
