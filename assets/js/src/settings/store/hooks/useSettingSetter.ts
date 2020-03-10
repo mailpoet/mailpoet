1026b07495189539ba4592c8db8ebd03d4ec63d6
Amine Ben hammou <amine@mailpoet.com>
2020-03-10 13:48:10 +0100

    Add comments about TS keyof and overloads
    [MAILPOET-2676]

--- assets/js/src/settings/store/hooks/useSettingSetter.ts
+++ assets/js/src/settings/store/hooks/useSettingSetter.ts
@@ -1,6 +1,14 @@
 import { Settings, Action } from '../types';
 import { useAction } from './useActions';
 
+/**
+ * Takes the path of a setting (ie. key1, key2, key3,...) and returns its setter.
+ * Here we are declaring the signatures in case it takes 1, 2, and 3 keys.
+ * Additional overloaded signatures can be added to go as deep as we want.
+ * See:
+ *  keyof: http://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-1.html#keyof-and-lookup-types
+ *  overloading functions: http://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html#function-overloads
+ */
 export function useSettingSetter<Key1 extends keyof Settings, Value extends Settings[Key1]>
   (key1: Key1): ((value: Value) => Promise<Action>);
 export function useSettingSetter<
