4d35b4b79ba28a0c4cb61e9a3d66d5586a101fce
Amine Ben hammou <amine@mailpoet.com>
2020-03-10 17:29:37 +0100

    Refactoring the control to be reusable
    [MAILPOET-2676]

---
+++ assets/js/src/common/controls/call_api.ts
@@ -0,0 +1,16 @@
+import MailPoet from 'mailpoet';
+
+export default async function ({ endpoint, action, data }) {
+  try {
+    const res = await MailPoet.Ajax.post({
+      api_version: (window as any).mailpoet_api_version,
+      endpoint,
+      action,
+      data,
+    });
+    return { success: true, res };
+  } catch (res) {
+    const error = res.errors.map((e) => e.message);
+    return { success: false, error };
+  }
+}
