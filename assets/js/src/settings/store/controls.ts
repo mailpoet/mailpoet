8846040993cd44dd18a90d0e60c5eb8cf4b8a479
Amine Ben hammou <amine@mailpoet.com>
2020-03-11 13:28:34 +0100

    Fix save and track saved settings
    [MAILPOET-2677]

--- assets/js/src/settings/store/controls.ts
+++ assets/js/src/settings/store/controls.ts
@@ -1,5 +1,34 @@
+import MailPoet from 'mailpoet';
+import { select } from '@wordpress/data';
+import { STORE_NAME } from '.';
+
 export { default as CALL_API } from 'common/controls/call_api';
 
-export function TRACK_SETTINGS_SAVED({ data }) {
-  // ...
+export function TRACK_SETTINGS_SAVED() {
+  const settings = select(STORE_NAME).getSettings();
+  const { mailpoet_version, mailpoet_woocommerce_active } = window as any;
+  const data = {
+    'MailPoet Free version': mailpoet_version,
+    'Sending method type': settings.mta_group || null,
+    'Sending frequency (emails)': (
+      settings.mta_group !== 'mailpoet'
+      && settings.mta
+      && settings.mta.frequency
+      && settings.mta.frequency.emails
+    ),
+    'Sending frequency (interval)': (
+      settings.mta_group !== 'mailpoet'
+      && settings.mta
+      && settings.mta.frequency
+      && settings.mta.frequency.interval
+    ),
+    'Sending provider': settings.mta_group === 'smtp' && settings.smtp_provider,
+    'Sign-up confirmation enabled': settings.signup_confirmation && settings.signup_confirmation.enabled,
+    'Bounce email is present': settings.bounce && settings.bounce.address !== '',
+    'Newsletter task scheduler method': (settings.cron_trigger && settings.cron_trigger.method),
+  };
+  if (mailpoet_woocommerce_active) {
+    data['WooCommerce email customizer enabled'] = settings.woocommerce && settings.woocommerce.use_mailpoet_editor;
+  }
+  MailPoet.trackEvent('User has saved Settings', data);
 }
