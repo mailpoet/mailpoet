060a6839c76b8a2bc983b4f942b94f8959fbacd5
Rostislav Wolny <rosta@mailpoet.com>
2020-03-12 16:27:59 +0100

    Render form block custom classes on frontend
    [MAILPOET-2746]

--- lib/Form/Block/Divider.php
+++ lib/Form/Block/Divider.php
@@ -3,7 +3,8 @@
 namespace MailPoet\Form\Block;
 
 class Divider {
-  public function render(): string {
-    return '<hr class="mailpoet_divider" />';
+  public function render($block): string {
+    $classes = isset($block['params']['class_name']) ? " " . $block['params']['class_name'] : '';
+    return '<hr class="mailpoet_divider' . $classes . '" />';
   }
 }
--- tests/unit/Form/BlockWrapperRendererTest.php
+++ tests/unit/Form/BlockWrapperRendererTest.php
@@ -12,4 +12,12 @@ class BlockWrapperRendererTest extends \MailPoetUnitTest {
     $result = $renderer->render($block, 'content');
     expect($result)->equals('<div class="mailpoet_paragraph">content</div>');
   }
+
+  public function testItShouldWrapRenderCustomClasses() {
+    $renderer = new BlockWrapperRenderer();
+    $block = Fixtures::get('simple_form_body')[0];
+    $block['params']['class_name'] = 'class1 class2';
+    $result = $renderer->render($block, 'content');
+    expect($result)->equals('<div class="mailpoet_paragraph class1 class2">content</div>');
+  }
 }
