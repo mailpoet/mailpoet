10a3ce4b983870b161815f88f6138e3f2ee61343
Amine Ben hammou <amine@mailpoet.com>
2020-03-12 01:04:55 +0100

    Add stats notifications
    [MAILPOET-2677]

---
+++ assets/js/src/settings/pages/basics/stats_notifications.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { t, onToggle, onChange } from 'settings/utils';
+import { useSetting, useAction } from 'settings/store/hooks';
+import { Label, Inputs } from 'settings/components';
+
+export default function StatsNotifications() {
+  const [enabled, setEnabled] = useSetting('stats_notifications', 'enabled');
+  const [automated, setAutomated] = useSetting('stats_notifications', 'automated');
+  const [email, setEmail] = useSetting('stats_notifications', 'address');
+  const setErrorFlag = useAction('setErrorFlag');
+  const hasError = (enabled === '1' || automated === '1') && email.trim() === '';
+  React.useEffect(() => {
+    setErrorFlag(hasError);
+  }, [hasError, setErrorFlag]);
+
+  return (
+    <>
+      <Label
+        title={t`statsNotifsTitle`}
+        description={t`statsNotifsDescription`}
+        htmlFor="stats-enabled"
+      />
+      <Inputs>
+        <input
+          type="checkbox"
+          id="stats-enabled"
+          checked={enabled === '1'}
+          onChange={onToggle(setEnabled)}
+        />
+        <label htmlFor="stats-enabled">{t`newslettersAndPostNotifs`}</label>
+        <br />
+        <input
+          type="checkbox"
+          id="stats-automated"
+          checked={automated === '1'}
+          onChange={onToggle(setAutomated)}
+        />
+        <label htmlFor="stats-automated">{t`welcomeAndWcEmails`}</label>
+        <br />
+        <input type="email" value={email} onChange={onChange(setEmail)} placeholder="me@mydomain.com" />
+        {hasError && (
+          <div className="mailpoet_error_item mailpoet_error">
+            {t`pleaseFillEmail`}
+          </div>
+        )}
+      </Inputs>
+    </>
+  );
+}
