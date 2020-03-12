30d9a9c1089b78b0427600c38c09e9cd3f327eaf
Amine Ben hammou <amine@mailpoet.com>
2020-03-12 01:58:21 +0100

    Add GDPR guide
    [MAILPOET-2677]

---
+++ assets/js/src/settings/pages/basics/gdpr_compliant.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { t } from 'settings/utils';
+import { Label, Inputs } from 'settings/components';
+
+export default function GdprCompliant() {
+  return (
+    <>
+      <Label
+        title={t`gdprTitle`}
+        description={t`gdprDescription`}
+        htmlFor="gdpr-compliant"
+      />
+      <Inputs>
+        <a
+          href="https://kb.mailpoet.com/article/246-guide-to-conform-to-gdpr"
+          data-beacon-article="5a9e8cdd04286374f7089a8c"
+          title={t`readGuide`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t`readGuide`}
+        </a>
+      </Inputs>
+    </>
+  );
+}
