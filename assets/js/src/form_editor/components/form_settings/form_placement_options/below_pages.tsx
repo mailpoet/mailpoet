af69fae5566b9f8e7e3c5fa4a610fc5047036b3d
Pavel Dohnal <pavel@mailpoet.com>
2020-03-16 13:30:02 +0100

    Active state for form placement component
    [MAILPOET-2738]

--- assets/js/src/form_editor/components/form_settings/form_placement_options/below_pages.tsx
+++ assets/js/src/form_editor/components/form_settings/form_placement_options/below_pages.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import MailPoet from 'mailpoet';
+import { useSelect } from '@wordpress/data';
 
 import FormPlacementOption from './form_placement_option';
 import Icon from './below_pages_icon';
 
-const BelowPages = () => (
-  <FormPlacementOption
-    label={MailPoet.I18n.t('placeFormBellowPages')}
-    icon={Icon}
-  />
-);
+const BelowPages = () => {
+  const placeFormBellowAllPages = useSelect(
+    (select) => select('mailpoet-form-editor').placeFormBellowAllPages(),
+    []
+  );
+
+  const placeFormBellowAllPosts = useSelect(
+    (select) => select('mailpoet-form-editor').placeFormBellowAllPosts(),
+    []
+  );
+
+  return (
+    <FormPlacementOption
+      label={MailPoet.I18n.t('placeFormBellowPages')}
+      icon={Icon}
+      active={placeFormBellowAllPages || placeFormBellowAllPosts}
+    />
+  );
+};
 
 export default BelowPages;
