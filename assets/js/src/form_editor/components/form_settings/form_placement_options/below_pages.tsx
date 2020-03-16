f9aca4fe577b6a7f80a08a03d76a8a7b09894c13
Pavel Dohnal <pavel@mailpoet.com>
2020-03-16 15:01:34 +0100

    Open settings modal
    [MAILPOET-2738]

--- assets/js/src/form_editor/components/form_settings/form_placement_options/below_pages.tsx
+++ assets/js/src/form_editor/components/form_settings/form_placement_options/below_pages.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MailPoet from 'mailpoet';
 import { useSelect } from '@wordpress/data';
 
 import FormPlacementOption from './form_placement_option';
 import Icon from './below_pages_icon';
+import Modal from '../../../../common/modal/modal.jsx';
 
 const BelowPages = () => {
+  const [displaySettings, setDisplaySettings] = useState(false);
+
   const placeFormBellowAllPages = useSelect(
     (select) => select('mailpoet-form-editor').placeFormBellowAllPages(),
     []
@@ -17,11 +20,27 @@ const BelowPages = () => {
   );
 
   return (
-    <FormPlacementOption
-      label={MailPoet.I18n.t('placeFormBellowPages')}
-      icon={Icon}
-      active={placeFormBellowAllPages || placeFormBellowAllPosts}
-    />
+    <>
+      <FormPlacementOption
+        label={MailPoet.I18n.t('placeFormBellowPages')}
+        icon={Icon}
+        active={placeFormBellowAllPages || placeFormBellowAllPosts}
+        onClick={() => setDisplaySettings(true)}
+      />
+      {
+        displaySettings
+        && (
+          <Modal
+            title={MailPoet.I18n.t('placeFormBellowPages')}
+            onRequestClose={() => setDisplaySettings(false)}
+          >
+            <p>
+              {MailPoet.I18n.t('placeFormBellowPagesDescription')}
+            </p>
+          </Modal>
+        )
+      }
+    </>
   );
 };
 
