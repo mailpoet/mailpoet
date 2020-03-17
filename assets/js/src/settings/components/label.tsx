b149c3148bf984a76df83a8cbd43eedc95eb442f
Amine Ben hammou <amine@mailpoet.com>
2020-03-17 13:06:17 +0100

    Make description optional
    [MAILPOET-2678]

--- assets/js/src/settings/components/label.tsx
+++ assets/js/src/settings/components/label.tsx
@@ -2,13 +2,13 @@ import React, { ReactNode } from 'react';
 
 type Props = {
   title: string
-  description: ReactNode
+  description?: ReactNode
   htmlFor: string
 }
 
 export default ({ title, description, htmlFor }: Props) => (
   <div className="mailpoet-settings-label">
     <label className="mailpoet-settings-label-title" htmlFor={htmlFor}>{title}</label>
-    <p className="description">{description}</p>
+    {description && <p className="description">{description}</p>}
   </div>
 );
