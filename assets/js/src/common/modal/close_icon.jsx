db47fdf22f73ac25409cbbd5b0d99b4680ff04f8
Pavel Dohnal <pavel@mailpoet.com>
2020-03-17 14:55:49 +0100

    Use a new close button
    [MAILPOET-2738]

--- assets/js/src/common/modal/close_icon.jsx
+++ assets/js/src/common/modal/close_icon.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Path, SVG } from '@wordpress/components';
+import { Path, SVG, G } from '@wordpress/components';
 
 export default (
-  <SVG viewBox="0 0 23 23" xmlns="http://www.w3.org/2000/svg">
-    <Path d="M21.454 1.546L1.546 21.454M1.546 1.546L21.454 21.454" strokeWidth="3" strokeLinecap="round" />
+  <SVG viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <G id="icons/ic_close" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <Path d="M8,2 C8.55228475,2 9,2.44771525 9,3 L9,7 L13,7 C13.5522847,7 14,7.44771525 14,8 C14,8.55228475 13.5522847,9 13,9 L9,9 L9,13 C9,13.5522847 8.55228475,14 8,14 C7.44771525,14 7,13.5522847 7,13 L7,9 L3,9 C2.44771525,9 2,8.55228475 2,8 C2,7.44771525 2.44771525,7 3,7 L7,7 L7,3 C7,2.44771525 7.44771525,2 8,2 Z" id="Combined-Shape" fill="#FF5301" fillRule="nonzero" transform="translate(8.000000, 8.000000) rotate(45.000000) translate(-8.000000, -8.000000) " />
+    </G>
   </SVG>
 );
