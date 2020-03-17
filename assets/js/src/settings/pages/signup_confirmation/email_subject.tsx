390a8619ffec5089c0cfa1c541a9f3c15480eb11
Amine Ben hammou <amine@mailpoet.com>
2020-03-17 15:08:01 +0100

    Add email subject field
    [MAILPOET-2678]

---
+++ assets/js/src/settings/pages/signup_confirmation/email_subject.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { t, onChange } from 'common/functions';
+import { Label, Inputs } from 'settings/components';
+import { useSetting } from 'settings/store/hooks';
+
+export default function EmailSubject() {
+  const [enabled] = useSetting('signup_confirmation', 'enabled');
+  const [subject, setSubject] = useSetting('signup_confirmation', 'subject');
+
+  if (!enabled) return null;
+  return (
+    <>
+      <Label
+        title={t('emailSubject')}
+        htmlFor="signup_confirmation-subject"
+      />
+      <Inputs>
+        <input
+          type="text"
+          size={50}
+          id="signup_confirmation-subject"
+          data-automation-id="signup_confirmation_email_subject"
+          value={subject}
+          onChange={onChange(setSubject)}
+        />
+      </Inputs>
+    </>
+  );
+}
--- assets/js/src/settings/pages/signup_confirmation/index.tsx
+++ assets/js/src/settings/pages/signup_confirmation/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import EnableSignupConfirmation from './enable_signup_confirmation';
+import EmailSubject from './email_subject';
 
 export default function SignupConfirmation() {
   return (
     <div className="mailpoet-settings-grid">
       <EnableSignupConfirmation />
+      <EmailSubject />
     </div>
   );
 }
