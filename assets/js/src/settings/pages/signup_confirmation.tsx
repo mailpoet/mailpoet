8c5d9b88657460fc7ce0ee460311bcbfc1b1ea5d
Amine Ben hammou <amine@mailpoet.com>
2020-03-17 15:02:30 +0100

    Add EnableSignupConfirmation
    [MAILPOET-2678]

--- assets/js/src/settings/pages/signup_confirmation.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function SignupConfirmation() {
-  return <p>Signup Confirmation!</p>;
-}
---
+++ assets/js/src/settings/pages/signup_confirmation/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import EnableSignupConfirmation from './enable_signup_confirmation';
+
+export default function SignupConfirmation() {
+  return (
+    <div className="mailpoet-settings-grid">
+      <EnableSignupConfirmation />
+    </div>
+  );
+}
