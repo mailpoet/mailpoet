2db03512105aaf1c69f2ab7c9397b443e2e3d64e
Amine Ben hammou <amine@mailpoet.com>
2020-03-17 15:15:40 +0100

    Add confirmation page field
    [MAILPOET-2678]

---
+++ assets/js/src/settings/pages/signup_confirmation/confirmation_page.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { t } from 'common/functions';
+import { Label, Inputs, PagesSelect } from 'settings/components';
+import { useSetting } from 'settings/store/hooks';
+
+export default function ConfirmationPage() {
+  const [enabled] = useSetting('signup_confirmation', 'enabled');
+  const [page, setPage] = useSetting('subscription', 'pages', 'confirmation');
+
+  if (!enabled) return null;
+  return (
+    <>
+      <Label
+        title={t('confirmationPage')}
+        description={t('confirmationPageDescription')}
+        htmlFor="subscription-pages-confirmation"
+      />
+      <Inputs>
+        <PagesSelect
+          value={page}
+          preview="confirm"
+          setValue={setPage}
+          id="subscription-pages-confirmation"
+          linkAutomationId="preview_page_link"
+        />
+      </Inputs>
+    </>
+  );
+}
