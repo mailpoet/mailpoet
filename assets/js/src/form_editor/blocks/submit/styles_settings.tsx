b07600ff3506b9083da541fdcf3e1d4eaa62e8c9
Rostislav Wolny <rosta@mailpoet.com>
2020-03-18 17:12:52 +0100

    Add other button style settings to panel
    [MAILPOET-2604]

--- assets/js/src/form_editor/blocks/submit/styles_settings.tsx
+++ assets/js/src/form_editor/blocks/submit/styles_settings.tsx
@@ -3,14 +3,24 @@ import MailPoet from 'mailpoet';
 import {
   Panel,
   PanelBody,
+  RangeControl,
   ToggleControl,
 } from '@wordpress/components';
 import { partial } from 'lodash';
+
+import ColorSettings from 'form_editor/components/color_settings';
+import FontSizeSetting from 'form_editor/components/font_size_settings';
 
 type Styles = {
   fullWidth: boolean,
   inheritFromTheme: boolean,
   bold?: boolean,
+  backgroundColor?: string,
+  fontColor?: string,
+  fontSize?: number,
+  borderSize?: number,
+  borderRadius?: number,
+  borderColor?: string,
 }
 
 type Props = {
@@ -24,12 +34,14 @@ const StylesSettings = ({
 }: Props) => {
   const localStylesRef = useRef(styles);
   const localStyles = localStylesRef.current;
+
   const updateStyles = (property, value) => {
     const updated = { ...localStylesRef.current };
     updated[property] = value;
     onChange(updated);
     localStylesRef.current = updated;
   };
+
   return (
     <Panel className="mailpoet-automation-input-styles-panel">
       <PanelBody title={MailPoet.I18n.t('formSettingsStyles')} initialOpen={false}>
@@ -46,12 +58,51 @@ const StylesSettings = ({
             className="mailpoet-automation-inherit-theme-toggle"
           />
           {!localStyles.inheritFromTheme ? (
-            <ToggleControl
-              label={MailPoet.I18n.t('formSettingsBold')}
-              checked={localStyles.bold || false}
-              onChange={partial(updateStyles, 'bold')}
-              className="mailpoet-automation-styles-bold-toggle"
-            />
+            <>
+              <ColorSettings
+                name={MailPoet.I18n.t('formSettingsStylesBackgroundColor')}
+                value={styles.backgroundColor}
+                onChange={partial(updateStyles, 'backgroundColor')}
+              />
+              <ColorSettings
+                name={MailPoet.I18n.t('formSettingsStylesFontColor')}
+                value={styles.fontColor}
+                onChange={partial(updateStyles, 'fontColor')}
+              />
+              <FontSizeSetting
+                name={MailPoet.I18n.t('formSettingsStylesFontSize')}
+                value={styles.fontSize}
+                onChange={partial(updateStyles, 'fontSize')}
+              />
+              <ToggleControl
+                label={MailPoet.I18n.t('formSettingsBold')}
+                checked={localStyles.bold || false}
+                onChange={partial(updateStyles, 'bold')}
+                className="mailpoet-automation-styles-bold-toggle"
+              />
+              <RangeControl
+                label={MailPoet.I18n.t('formSettingsBorderSize')}
+                value={localStyles.borderSize || 1}
+                min={0}
+                max={10}
+                allowReset
+                onChange={partial(updateStyles, 'borderSize')}
+                className="mailpoet-automation-styles-border-size"
+              />
+              <RangeControl
+                label={MailPoet.I18n.t('formSettingsBorderRadius')}
+                value={localStyles.borderRadius || 1}
+                min={0}
+                max={40}
+                allowReset
+                onChange={partial(updateStyles, 'borderRadius')}
+              />
+              <ColorSettings
+                name={MailPoet.I18n.t('formSettingsBorderColor')}
+                value={localStyles.borderColor}
+                onChange={partial(updateStyles, 'borderColor')}
+              />
+            </>
           ) : null}
         </div>
       </PanelBody>
