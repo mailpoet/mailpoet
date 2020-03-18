02fe614b27f488911d47b12bb86443caf1cf69a4
Pavel Dohnal <pavel@mailpoet.com>
2020-03-18 15:26:01 +0100

    Split component into two
    [MAILPOET-2738]

--- assets/js/src/form_editor/components/form_settings/form_placement_options/below_pages.tsx
+++ assets/js/src/form_editor/components/form_settings/form_placement_options/below_pages.tsx
@@ -1,16 +1,11 @@
 import React, { useState } from 'react';
 import MailPoet from 'mailpoet';
-import { Button } from '@wordpress/components';
 import { useSelect, useDispatch } from '@wordpress/data';
 
-import FormPlacementOption from './form_placement_option';
-import Icon from './below_pages_icon';
-import Modal from '../../../../common/modal/modal.jsx';
+import FormPlacementSettings from './form_placement_settings';
 import Toggle from '../../../../common/toggle';
 
 const BelowPages = () => {
-  const [displaySettings, setDisplaySettings] = useState(false);
-
   const placeFormBellowAllPages = useSelect(
     (select) => select('mailpoet-form-editor').placeFormBellowAllPages(),
     []
@@ -32,64 +27,40 @@ const BelowPages = () => {
   ] = useState(placeFormBellowAllPosts);
 
   const save = () => {
-    setDisplaySettings(false);
     setPlaceFormBellowAllPages(localPlaceFormBellowAllPages);
     setPlaceFormBellowAllPosts(localPlaceFormBellowAllPosts);
   };
 
   return (
-    <>
-      <FormPlacementOption
-        label={MailPoet.I18n.t('placeFormBellowPages')}
-        icon={Icon}
-        active={placeFormBellowAllPages || placeFormBellowAllPosts}
-        onClick={() => setDisplaySettings(true)}
-      />
-      {
-        displaySettings
-        && (
-          <Modal
-            title={MailPoet.I18n.t('placeFormBellowPages')}
-            onRequestClose={() => setDisplaySettings(false)}
-            contentClassName="form-placement-settings"
-          >
-            <p>
-              {MailPoet.I18n.t('placeFormBellowPagesDescription')}
-            </p>
-            <div className="mailpoet-toggle-list">
-              <div className="mailpoet-toggle-list-description">
-                {MailPoet.I18n.t('placeFormBellowAllPages')}
-              </div>
-              <div className="mailpoet-toggle-list-toggle">
-                <Toggle
-                  name="localPlaceFormBellowAllPages"
-                  checked={localPlaceFormBellowAllPages}
-                  onCheck={setLocalPlaceFormBellowAllPages}
-                />
-              </div>
-              <div className="mailpoet-toggle-list-description">
-                {MailPoet.I18n.t('placeFormBellowAllPosts')}
-              </div>
-              <div className="mailpoet-toggle-list-toggle" data-automation-id="place-form-bellow-all-posts-toggle">
-                <Toggle
-                  name="localPlaceFormBellowAllPosts"
-                  checked={localPlaceFormBellowAllPosts}
-                  onCheck={setLocalPlaceFormBellowAllPosts}
-                />
-              </div>
-            </div>
-            <div className="mailpoet-form-placement-save">
-              <Button
-                onClick={save}
-                className="mailpoet-save-button automation-id-save-form-below-pages"
-              >
-                {MailPoet.I18n.t('formPlacementSave')}
-              </Button>
-            </div>
-          </Modal>
-        )
-      }
-    </>
+    <FormPlacementSettings
+      active={placeFormBellowAllPages || placeFormBellowAllPosts}
+      onSave={save}
+      description={MailPoet.I18n.t('placeFormBellowPagesDescription')}
+      label={MailPoet.I18n.t('placeFormBellowPages')}
+    >
+      <div className="mailpoet-toggle-list">
+        <div className="mailpoet-toggle-list-description">
+          {MailPoet.I18n.t('placeFormBellowAllPages')}
+        </div>
+        <div className="mailpoet-toggle-list-toggle">
+          <Toggle
+            name="localPlaceFormBellowAllPages"
+            checked={localPlaceFormBellowAllPages}
+            onCheck={setLocalPlaceFormBellowAllPages}
+          />
+        </div>
+        <div className="mailpoet-toggle-list-description">
+          {MailPoet.I18n.t('placeFormBellowAllPosts')}
+        </div>
+        <div className="mailpoet-toggle-list-toggle" data-automation-id="place-form-bellow-all-posts-toggle">
+          <Toggle
+            name="localPlaceFormBellowAllPosts"
+            checked={localPlaceFormBellowAllPosts}
+            onCheck={setLocalPlaceFormBellowAllPosts}
+          />
+        </div>
+      </div>
+    </FormPlacementSettings>
   );
 };
 
