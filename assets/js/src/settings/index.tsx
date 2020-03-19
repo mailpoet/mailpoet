88b12dc1cc774f1b10130a5b8e9df4fc042fe7bb
Amine Ben hammou <amine@mailpoet.com>
2020-03-20 00:42:26 +0100

    Focus settings menu item
    [MAILPOET-2677]

--- assets/js/src/settings/index.tsx
+++ assets/js/src/settings/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import jQuery from 'jquery';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import { GlobalContext, useGlobalContextValue } from 'context';
@@ -16,5 +17,8 @@ const Entry = () => (
 const container = document.getElementById('settings_container');
 if (container) {
   initStore(window);
+  // Select the settings submenu
+  // This is temporary and should be removed at the end of settings refactoring
+  jQuery('ul.wp-submenu > li:nth-child(7)').addClass('current');
   ReactDOM.render(<Entry />, container);
 }
