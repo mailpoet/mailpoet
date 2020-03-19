8628bae9839f403b7c2af281ded7f1e7984dd37b
Amine Ben hammou <amine@mailpoet.com>
2020-03-19 19:23:04 +0100

    Add bounce address field
    [MAILPOET-2680]

--- assets/js/src/settings/pages/advanced.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Advanced() {
-  return <p>Advanced!</p>;
-}
---
+++ assets/js/src/settings/pages/advanced/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import BounceAddress from './bounce_address';
+
+export default function Advanced() {
+  return (
+    <div className="mailpoet-settings-grid">
+      <BounceAddress />
+    </div>
+  );
+}
