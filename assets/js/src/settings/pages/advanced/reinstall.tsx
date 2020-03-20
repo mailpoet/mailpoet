9054e86b203eb8202a2f18e22636282e430763af
Amine Ben hammou <amine@mailpoet.com>
2020-03-20 04:10:31 +0100

    Add reinstall button

---
+++ assets/js/src/settings/pages/advanced/reinstall.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { t } from 'common/functions';
+import { GlobalContext } from 'context';
+import { useAction } from 'settings/store/hooks';
+import { Label, Inputs } from 'settings/components';
+
+export default function Reinstall() {
+  const reinstall = useAction('reinstall');
+  const { notices } = React.useContext<any>(GlobalContext);
+  const onClick = async () => {
+    if (window.confirm(t('reinstallConfirmation'))) { // eslint-disable-line
+      type Result = { type: 'SAVE_FAILED' | 'SAVE_DONE', error?: any }
+      const action = (await reinstall()) as any as Result;
+      if (action.type === 'SAVE_FAILED') {
+        notices.error(action.error.map((err) => <p>{err}</p>), { scroll: true });
+      } else {
+        window.location.href = 'admin.php?page=mailpoet-newsletters';
+      }
+    }
+  };
+  return (
+    <>
+      <Label
+        title={t('reinstallTitle')}
+        description={t('reinstallDescription')}
+        htmlFor=""
+      />
+      <Inputs>
+        <button type="button" className="button" onClick={onClick}>{t('reinstallNow')}</button>
+      </Inputs>
+    </>
+  );
+}
--- assets/js/src/settings/store/actions.ts
+++ assets/js/src/settings/store/actions.ts
@@ -188,3 +188,18 @@ export function* installPremiumPlugin() {
   }
   return yield* activatePremiumPlugin(true);
 }
+
+export function* reinstall() {
+  MailPoet.Modal.loading(true);
+  const { success, error } = yield {
+    type: 'CALL_API',
+    endpoint: 'setup',
+    action: 'reset',
+  };
+  MailPoet.Modal.loading(false);
+  if (!success) {
+    return { type: 'SAVE_FAILED', error };
+  }
+  yield { type: 'TRACK_REINSTALLED' };
+  return { type: 'SAVE_DONE' };
+}
