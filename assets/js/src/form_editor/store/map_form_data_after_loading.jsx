4e94a34932c82f996b275db8e04af761d5e31d5c
Pavel Dohnal <pavel@mailpoet.com>
2020-03-24 14:40:16 +0100

    Add delay settings
    [MAILPOET-2741]

--- assets/js/src/form_editor/store/map_form_data_after_loading.jsx
+++ assets/js/src/form_editor/store/map_form_data_after_loading.jsx
@@ -1,4 +1,9 @@
 export default function mapFormDataAfterLoading(data) {
+  let popupFormDelay = parseInt(data.settings.popup_form_delay, 10);
+  if (Number.isNaN(popupFormDelay)) {
+    popupFormDelay = undefined;
+  }
+
   return {
     ...data,
     settings: {
@@ -7,6 +12,7 @@ export default function mapFormDataAfterLoading(data) {
       placeFormBellowAllPosts: data.settings.place_form_bellow_all_posts === '1',
       placePopupFormOnAllPages: data.settings.place_popup_form_on_all_pages === '1',
       placePopupFormOnAllPosts: data.settings.place_popup_form_on_all_posts === '1',
+      popupFormDelay,
     },
   };
 }
