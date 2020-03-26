36524efb492158ba5a53fffad06a15984f1211ee
Amine Ben hammou <amine@mailpoet.com>
2020-03-26 05:02:12 +0100

    Normalize settings values
    [MAILPOET-2823]

--- assets/js/src/settings/store/types.ts
+++ assets/js/src/settings/store/types.ts
@@ -37,9 +37,131 @@ export type Settings = {
     enabled: '' | '1'
     address: string
   }
-  // ...
-}
+  cron_trigger: {
+    method: 'WordPress' | 'MailPoet' | 'Linux Cron'
+  }
+  tracking: {
+    enabled: '' | '1'
+  }
+  send_transactional_emails: '' | '1'
+  deactivate_subscriber_after_inactive_days: '' | '90' | '180' | '365'
+  analytics: {
+    enabled: '' | '1'
+  }
+  captcha: {
+    type: 'built-in' | 'recaptcha' | ''
+    recaptcha_site_token: string
+    recaptcha_secret_token: string
+  }
+  logging: 'everything' | 'errors' | 'nothing'
+  mta_group: 'mailpoet' | 'website' | 'smtp'
+  mta: {
+    method: 'MailPoet' | 'AmazonSES' | 'SendGrid' | 'PHPMail' | 'SMTP'
+    frequency: {
+      emails: string
+      interval: string
+    }
+    mailpoet_api_key: string
+    host: string
+    port: string
+    region:
+    | 'us-east-1'
+    | 'us-west-2'
+    | 'eu-west-1'
+    | 'eu-central-1'
+    | 'ap-south-1'
+    | 'ap-southeast-2'
+    access_key: string
+    secret_key: string
+    api_key: string
+    login: string
+    password: string
+    encryption: string
+    authentication: '1' | '-1'
+    mailpoet_api_key_state: {
+      state:
+      | 'valid'
+      | 'invalid'
+      | 'expiring'
+      | 'already_used'
+      | 'check_error'
+      data: any
+    }
+  }
+  mailpoet_smtp_provider: 'server' | 'manual' | 'AmazonSES' | 'SendGrid'
+  smtp_provider: 'server' | 'manual' | 'AmazonSES' | 'SendGrid',
+  web_host:
+  | 'manual'
+  | '1and1'
+  | 'bluehost'
+  | 'df'
+  | 'dreamhost'
+  | 'free'
+  | 'froghost'
+  | 'godaddy'
+  | 'goneo'
+  | 'googleapps'
+  | 'greengeeks'
+  | 'hawkhost'
+  | 'hivetec'
+  | 'hostgator'
+  | 'hosting2go'
+  | 'hostmonster'
+  | 'infomaniak'
+  | 'justhost'
+  | 'laughingsquid'
+  | 'lunarpages'
+  | 'mediatemple'
+  | 'netfirms'
+  | 'netissime'
+  | 'one'
+  | 'ovh'
+  | 'phpnet'
+  | 'planethoster'
+  | 'rochen'
+  | 'site5'
+  | 'siteground'
+  | 'synthesis'
+  | 'techark'
+  | 'vexxhost'
+  | 'vps'
+  | 'webcity'
+  | 'westhost'
+  | 'wpwebhost'
+  mailpoet_sending_frequency: 'auto' | 'manual'
 
+  signup_confirmation: {
+    enabled: '1' | ''
+    subject: string
+    body: string
+  }
+  woocommerce: {
+    transactional_email_id: string,
+    optin_on_checkout: {
+      enabled: '1' | ''
+      message: string
+    }
+    accept_cookie_revenue_tracking: {
+      enabled: '1' | '',
+      set: '1' | ''
+    }
+  }
+  mailpoet_subscribe_old_woocommerce_customers: {
+    enabled: '1' | ''
+  }
+  premium: {
+    premium_key: string
+    premium_key_state: {
+      state:
+      | 'valid'
+      | 'invalid'
+      | 'expiring'
+      | 'already_used'
+      | 'check_error'
+      data: any
+    }
+  }
+}
 type Segment = {
   id: string
   name: string
