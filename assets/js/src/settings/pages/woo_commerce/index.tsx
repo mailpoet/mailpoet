ec86425357a15de6ed11720c4d55744ade70821f
Amine Ben hammou <amine@mailpoet.com>
2020-04-01 04:38:42 +0200

    Add checkout opt-in fields
    [MAILPOET-2708]

--- assets/js/src/settings/pages/woo_commerce/index.tsx
+++ assets/js/src/settings/pages/woo_commerce/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { SaveButton } from 'settings/components';
 import EmailCustomizer from './email_customizer';
+import CheckoutOptin from './checkout_optin';
 
 export default function WooCommerce() {
   return (
     <div className="mailpoet-settings-grid">
       <EmailCustomizer />
+      <CheckoutOptin />
       <SaveButton />
     </div>
   );
