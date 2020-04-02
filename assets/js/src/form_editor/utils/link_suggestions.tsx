3aaf5a3ab1f5a2cf999475df8ccc34373c351122
Rostislav Wolny <rosta@mailpoet.com>
2020-04-02 16:11:11 +0200

    Add support for image links autocomplete
    [MAILPOET-2750]

---
+++ assets/js/src/form_editor/utils/link_suggestions.tsx
@@ -0,0 +1,27 @@
+import apiFetch from '@wordpress/api-fetch';
+import { addQueryArgs } from '@wordpress/url';
+import { decodeEntities } from '@wordpress/html-entities';
+import MailPoet from 'mailpoet';
+
+/**
+ * This is based on function used in post editor
+ * @see https://github.com/WordPress/gutenberg/blob/5941c924425f1d09bc724652cc316f5df90d4d32/packages/editor/src/components/provider/index.js#L31
+ */
+const fetchLinkSuggestions = async (search: string, { perPage = 20 } = {}) => {
+  const posts = await apiFetch({
+    path: addQueryArgs('/wp/v2/search', {
+      search,
+      per_page: perPage,
+      type: 'post',
+    }),
+  });
+
+  return posts.map((post) => ({
+    id: post.id,
+    url: post.url,
+    title: decodeEntities(post.title) || `(${MailPoet.I18n.t('noName')})`,
+    type: post.subtype || post.type,
+  }));
+};
+
+export default fetchLinkSuggestions;
