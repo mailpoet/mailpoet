1722eaefed2f76b21d52b2ea3dc032ebe970eabe
Amine Ben hammou <amine@mailpoet.com>
2020-04-06 07:48:03 +0200

    Make filenames consistent with other files

--- assets/js/src/settings/pages/index.tsx
+++ assets/js/src/settings/pages/index.tsx
@@ -1,5 +1,5 @@
-export { default as Advanced } from './advanced';
 export { default as Basics } from './basics/basics';
+export { default as Advanced } from './advanced/advanced';
 export { default as KeyActivation } from './key_activation/key_activation';
 export { default as SendWith } from './send_with';
 export { default as SignupConfirmation } from './signup_confirmation/signup_confirmation';
