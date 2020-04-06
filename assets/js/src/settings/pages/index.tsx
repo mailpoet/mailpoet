bc26618f3894f1aa7b01f0f90536f3435602aa48
Amine Ben hammou <amine@mailpoet.com>
2020-04-06 07:56:17 +0200

    Make filenames consistent with other files
    [MAILPOET-2681]

--- assets/js/src/settings/pages/index.tsx
+++ assets/js/src/settings/pages/index.tsx
@@ -1,6 +1,6 @@
 export { default as Advanced } from './advanced';
 export { default as Basics } from './basics';
-export { default as KeyActivation } from './key_activation';
+export { default as KeyActivation } from './key_activation/key_activation';
 export { default as SendWith } from './send_with';
 export { default as SignupConfirmation } from './signup_confirmation';
 export { default as WooCommerce } from './woo_commerce';
