98803e1c040e6b098b58a815cdb717ea06732f69
Amine Ben hammou <amine@mailpoet.com>
2020-04-09 05:33:04 +0200

    Add SPF description
    [MAILPOET-2693]

---
+++ assets/js/src/settings/pages/send_with/other/spf.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { t } from 'common/functions';
+import { Label, Inputs } from 'settings/components';
+
+export default function SPF() {
+  return (
+    <>
+      <Label title={t('spfTitle')} description={t('spfDescription')} htmlFor="" />
+      <Inputs>
+        {t('spfSetup')}
+      </Inputs>
+    </>
+  );
+}
