cbac68d3a1b3b7ad22293a3c2946bc1e4e13cb3e
Amine Ben hammou <amine@mailpoet.com>
2020-04-09 05:33:04 +0200

    Add SPF description
    [MAILPOET-2693]

--- assets/js/src/settings/pages/send_with/other/other_sending_methods.tsx
+++ assets/js/src/settings/pages/send_with/other/other_sending_methods.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import SendingMethod from './sending_method';
+import SPF from './spf';
 
 export default function OtherSendingMethods() {
   return (
     <div className="mailpoet-settings-grid">
       <SendingMethod />
+
+      <SPF />
     </div>
   );
 }
