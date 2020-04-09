9111e18e35e01da67c0c730129d548ee8dc42539
Amine Ben hammou <amine@mailpoet.com>
2020-04-09 05:25:13 +0200

    Add sending method field
    [MAILPOET-2693]

---
+++ assets/js/src/settings/pages/send_with/other/other_sending_methods.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SendingMethod from './sending_method';
+
+export default function OtherSendingMethods() {
+  return (
+    <div className="mailpoet-settings-grid">
+      <SendingMethod />
+    </div>
+  );
+}
