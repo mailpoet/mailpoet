639fa9f0e242d90ecfef7e6a42619cc625376041
Amine Ben hammou <amine@mailpoet.com>
2020-04-13 06:53:21 +0200

    Add PHPMail fields
    [MAILPOET-2693]

--- assets/js/src/settings/pages/send_with/other/other_sending_methods.tsx
+++ assets/js/src/settings/pages/send_with/other/other_sending_methods.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useSetting } from 'settings/store/hooks';
 import SendingMethod from './sending_method';
 import SPF from './spf';
 import TestSending from './test_sending';
 import ActivateOrCancel from './activate_or_cancel';
+import PHPMailFields from './php_mail_fields';
 
 export default function OtherSendingMethods() {
+  const [method] = useSetting('mta', 'method');
   return (
     <div className="mailpoet-settings-grid">
       <SendingMethod />
-
+      {method === 'PHPMail' && <PHPMailFields />}
       <SPF />
       <TestSending />
       <ActivateOrCancel />
---
+++ assets/js/src/settings/pages/send_with/other/sending_frequency.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import ReactStringReplace from 'react-string-replace';
+
+import { Label, Inputs } from 'settings/components';
+import { t, onChange } from 'common/functions';
+import { useSetting } from 'settings/store/hooks';
+
+const MINUTES_PER_DAY = 1440;
+const SECONDS_PER_DAY = 86400;
+
+type Props = {
+  recommendedEmails: string
+  recommendedInterval: string
+}
+export default function SendingFrequency({ recommendedEmails, recommendedInterval }: Props) {
+  const [frequency, setFrequency] = useSetting('mailpoet_sending_frequency');
+  const [frequencyEmails, setFrequencyEmails] = useSetting('mta', 'frequency', 'emails');
+  const [frequencyInterval, setFrequencyInterval] = useSetting('mta', 'frequency', 'interval');
+  React.useEffect(() => {
+    if (frequency === 'auto') {
+      setFrequencyEmails(recommendedEmails);
+      setFrequencyInterval(recommendedInterval);
+    }
+  }, [frequency, recommendedEmails, recommendedInterval, setFrequencyEmails, setFrequencyInterval]);
+
+  const dailyEmails = Math.floor(
+    (MINUTES_PER_DAY * parseInt(frequencyEmails, 10)) / parseInt(frequencyInterval, 10)
+  );
+  const emailsPerSecond = Math.floor((dailyEmails / SECONDS_PER_DAY) * 10) / 10;
+
+  return (
+    <>
+      <Label title={t('sendingFrequency')} htmlFor="mailpoet_sending_frequency" />
+      <Inputs>
+        <select id="mailpoet_sending_frequency" value={frequency} onChange={onChange(setFrequency)}>
+          <option value="auto">{t('recommendedTitle')}</option>
+          <option value="manual">{t('ownFrequency')}</option>
+        </select>
+        {frequency === 'manual' && (
+          <>
+            <br />
+            <input
+              id="other_frequency_emails"
+              type="number"
+              min="1"
+              max="1000"
+              value={frequencyEmails}
+              onChange={onChange(setFrequencyEmails)}
+            />
+            {' '}
+            {t('emails')}
+            <select id="other_frequency_interval" value={frequencyInterval} onChange={onChange(setFrequencyInterval)}>
+              <option value="1">every minute</option>
+              <option value="2">every 2 minutes</option>
+              <option value="5">every 5 minutes (recommended)</option>
+              <option value="10">every 10 minutes</option>
+              <option value="15">every 15 minutes</option>
+              <option value="30">every 30 minutes</option>
+            </select>
+          </>
+        )}
+        {frequency === 'auto' && (
+          <span>
+            {t('xEmails').replace('%1$s', frequencyEmails)}
+            {' '}
+            {formatInterval(frequencyInterval)}
+            {'. '}
+          </span>
+        )}
+        <span>
+          {ReactStringReplace(
+            t('thatsXEmailsPerDay').replace('%1$s', dailyEmails.toLocaleString()),
+            /<strong>(.*?)<\/strong>/g,
+            (match, i) => <strong key={i}>{match}</strong>
+          )}
+        </span>
+        <br />
+        {emailsPerSecond > 1 && (
+          <>
+            <br />
+            <span className="mailpoet_emails_per_second_warning">
+              {ReactStringReplace(
+                t('thatsXEmailsPerSecond').replace('%1$s', emailsPerSecond.toLocaleString()),
+                /<strong>(.*?)<\/strong>/g,
+                (match, i) => <strong key={i}>{match}</strong>
+              )}
+            </span>
+            <br />
+          </>
+        )}
+        {frequency === 'manual' && (
+          <>
+            <br />
+            <span>
+              {ReactStringReplace(
+                t('frequencyWarning').replace('%1$s', emailsPerSecond.toLocaleString()),
+                /<strong>(.*?)<\/strong>/g,
+                (match, i) => <strong key={i}>{match}</strong>
+              )}
+            </span>
+          </>
+        )}
+      </Inputs>
+    </>
+  );
+}
+
+function formatInterval(minutes: string): string {
+  const value = Math.floor(parseInt(minutes, 10));
+  if (value > 60) return t('everyHours').replace('%1$d', `${value / 60}`);
+  if (value === 60) return t('everyHour');
+  if (value > 1) return t('everyMinutes').replace('%1$d', `${value}`);
+  return t('everyMinute');
+}
