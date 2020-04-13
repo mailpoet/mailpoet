81942d03bfca7b9e255be143d3b9606ecbb93c00
Jan Jakeš <jan@jakes.pro>
2020-04-13 12:13:53 +0200

    Add congratulatory MSS sending email & endpoint to send it
    [MAILPOET-2831]

--- lib/DI/ContainerConfigurator.php
+++ lib/DI/ContainerConfigurator.php
@@ -238,6 +238,7 @@ class ContainerConfigurator implements IContainerConfigurator {
     // Services
     $container->autowire(\MailPoet\Services\Bridge::class)->setPublic(true);
     $container->autowire(\MailPoet\Services\AuthorizedEmailsController::class);
+    $container->autowire(\MailPoet\Services\CongratulatoryMssEmailController::class);
     $container->autowire(\MailPoet\Services\SPFCheck::class)->setPublic(true);
     // Tasks
     $container->autowire(\MailPoet\Tasks\State::class);
