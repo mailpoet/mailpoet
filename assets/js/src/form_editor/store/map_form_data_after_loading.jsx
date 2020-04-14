a9d1bb78978caba291de300970bd7f397b594b1b
Pavel Dohnal <pavel@mailpoet.com>
2020-04-14 10:54:47 +0200

    Save slide in form to server
    [MAILPOET-2742]

--- assets/js/src/form_editor/store/map_form_data_after_loading.jsx
+++ assets/js/src/form_editor/store/map_form_data_after_loading.jsx
@@ -1,13 +1,13 @@
-export default function mapFormDataAfterLoading(data) {
-  let popupFormDelay = parseInt(data.settings.popup_form_delay, 10);
-  if (Number.isNaN(popupFormDelay)) {
-    popupFormDelay = undefined;
+function asNum(num) {
+  const numI = parseInt(num, 10);
+  if (Number.isNaN(numI)) {
+    return undefined;
   }
-  let fixedBarFormDelay = parseInt(data.settings.fixed_bar_form_delay, 10);
-  if (Number.isNaN(fixedBarFormDelay)) {
-    fixedBarFormDelay = undefined;
-  }
+  return numI;
+}
 
+
+export default function mapFormDataAfterLoading(data) {
   return {
     ...data,
     settings: {
@@ -16,11 +16,15 @@ export default function mapFormDataAfterLoading(data) {
       placeFormBellowAllPosts: data.settings.place_form_bellow_all_posts === '1',
       placePopupFormOnAllPages: data.settings.place_popup_form_on_all_pages === '1',
       placePopupFormOnAllPosts: data.settings.place_popup_form_on_all_posts === '1',
-      popupFormDelay,
+      popupFormDelay: asNum(data.settings.popup_form_delay),
       placeFixedBarFormOnAllPages: data.settings.place_fixed_bar_form_on_all_pages === '1',
       placeFixedBarFormOnAllPosts: data.settings.place_fixed_bar_form_on_all_posts === '1',
-      fixedBarFormDelay,
+      fixedBarFormDelay: asNum(data.settings.fixed_bar_form_delay),
       fixedBarFormPosition: data.settings.fixed_bar_form_position,
+      placeSlideInFormOnAllPages: data.settings.place_slide_in_form_on_all_pages === '1',
+      placeSlideInFormOnAllPosts: data.settings.place_slide_in_form_on_all_posts === '1',
+      slideInFormDelay: asNum(data.settings.slide_in_form_delay),
+      slideInFormPosition: data.settings.slide_in_form_position,
     },
   };
 }
