223c8cf67f8bf5fd716b92bb4e0faae9ec861347
Amine Ben hammou <amine@mailpoet.com>
2020-04-14 09:03:52 +0200

    Separate actions into multiple files
    [MAILPOET-2682]

---
+++ assets/js/src/settings/store/actions/send_test_email.ts
@@ -0,0 +1,30 @@
+import MailPoet from 'mailpoet';
+
+import { t } from 'common/functions';
+import { Settings } from 'settings/store/types';
+
+export default function* sendTestEmail(recipient: string, mailer: Settings['mta']) {
+  if (!recipient) {
+    return { type: 'TEST_EMAIL_FAILED', error: [t('cantSendEmail')] };
+  }
+  yield { type: 'START_TEST_EMAIL_SENDING' };
+  const res = yield {
+    type: 'CALL_API',
+    endpoint: 'mailer',
+    action: 'send',
+    data: {
+      mailer,
+      newsletter: {
+        subject: t('testEmailSubject'),
+        body: {
+          html: `<p>${t('testEmailBody')}</p>`,
+          text: t('testEmailBody'),
+        },
+      },
+      subscriber: recipient,
+    },
+  };
+  yield { type: 'TRACK_TEST_EMAIL_SENT', success: res.success, method: mailer.method };
+  if (!res.success) return { type: 'TEST_EMAIL_FAILED', error: res.error };
+  return { type: 'TEST_EMAIL_SUCCESS' };
+}
---
+++ assets/js/src/settings/store/actions/settings.ts
@@ -0,0 +1,50 @@
+import { select } from '@wordpress/data';
+
+import { STORE_NAME } from 'settings/store';
+import { Action } from 'settings/store/types';
+import { updateKeyActivationState } from './mss_and_premium';
+
+export function setSetting(path: string[], value: any): Action {
+  return { type: 'SET_SETTING', path, value };
+}
+
+export function setSettings(value: any): Action {
+  return { type: 'SET_SETTINGS', value };
+}
+
+export function setErrorFlag(value: boolean): Action {
+  return { type: 'SET_ERROR_FLAG', value };
+}
+
+export function* saveSettings() {
+  yield { type: 'SAVE_STARTED' };
+  const data = select(STORE_NAME).getSettings();
+  const { success, error, res } = yield {
+    type: 'CALL_API',
+    endpoint: 'settings',
+    action: 'set',
+    data,
+  };
+  if (!success) {
+    return { type: 'SAVE_FAILED', error };
+  }
+  yield { type: 'TRACK_SETTINGS_SAVED' };
+  yield updateKeyActivationState({
+    congratulatoryMssEmailSentTo: null,
+    fromAddressModalCanBeShown: false,
+  });
+  yield setSettings(res.data);
+  return { type: 'SAVE_DONE' };
+}
+
+export function* loadSettings() {
+  const { success, error, res } = yield {
+    type: 'CALL_API',
+    endpoint: 'settings',
+    action: 'get',
+  };
+  if (!success) {
+    return { type: 'SAVE_FAILED', error };
+  }
+  return setSettings(res.data);
+}
