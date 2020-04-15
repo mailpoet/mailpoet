49ec2ed294817e91bd92ec3303ede4017a0d7c28
Rostislav Wolny <rosta@mailpoet.com>
2020-04-15 18:01:25 +0200

    Change controls of preview to icons and add callback
    [MAILPOET-2743]

--- assets/js/src/common/preview.jsx
+++ assets/js/src/common/preview.jsx
@@ -2,36 +2,44 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import MailPoet from 'mailpoet';
 import classnames from 'classnames';
+import MobileIcon from './preview/mobile_icon';
+import DesktopIcon from './preview/desktop_icon';
 
 function Preview({
   children,
+  onChange,
+  selectedType,
 }) {
-  const [checked, setChecked] = useState('desktop');
-
-
+  const [checked, setChecked] = useState(selectedType);
+  const changeType = (type) => {
+    setChecked(type);
+    onChange(type);
+  };
   return (
     <div className="mailpoet_browser_preview">
       <div className="mailpoet_browser_preview_toggle">
-        <label>
-          <input
-            type="radio"
-            name="mailpoet_browser_preview_type"
-            className="mailpoet_browser_preview_type"
-            checked={checked === 'desktop'}
-            onChange={() => setChecked('desktop')}
-          />
-          {MailPoet.I18n.t('formPreviewDesktop')}
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="mailpoet_browser_preview_type"
-            className="mailpoet_browser_preview_type"
-            checked={checked !== 'desktop'}
-            onChange={() => setChecked('mobile')}
-          />
-          {MailPoet.I18n.t('formPreviewMobile')}
-        </label>
+        <a
+          className={classnames('mailpoet_browser_preview_icon', { mailpoet_active: checked === 'desktop' })}
+          onClick={(e) => {
+            e.preventDefault();
+            changeType('desktop');
+          }}
+          title={MailPoet.I18n.t('formPreviewDesktop')}
+          href="#"
+        >
+          <DesktopIcon />
+        </a>
+        <a
+          className={classnames('mailpoet_browser_preview_icon', { mailpoet_active: checked === 'mobile' })}
+          onClick={(e) => {
+            e.preventDefault();
+            changeType('mobile');
+          }}
+          title={MailPoet.I18n.t('formPreviewMobile')}
+          href="#"
+        >
+          <MobileIcon />
+        </a>
       </div>
       <div
         className={classnames(
@@ -50,6 +58,13 @@ function Preview({
 
 Preview.propTypes = {
   children: PropTypes.node.isRequired,
+  onChange: PropTypes.func,
+  selectedType: PropTypes.string,
+};
+
+Preview.defaultProps = {
+  onChange: () => {},
+  selectedType: 'desktop',
 };
 
 export default Preview;
