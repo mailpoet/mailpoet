4cb7ba242439c27f89eea5887b74d3ee7b9b5eec
Rostislav Wolny <rosta@mailpoet.com>
2020-04-16 09:31:35 +0200

    Add different rendering for sidebar widget preview type
    [MAILPOET-2743]

---
+++ lib/Form/PreviewWidget.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MailPoet\Form;
+
+class PreviewWidget extends \WP_Widget {
+
+  /** @var string */
+  private $formHtml;
+
+  public function __construct($formHtml) {
+    $this->formHtml = $formHtml;
+    parent::__construct(
+      'mailpoet_form_preview',
+      'Dummy form form preview',
+      []
+    );
+  }
+
+  /**
+   * Output the widget itself.
+   */
+  public function widget($args, $instance = null) {
+    echo $this->formHtml;
+  }
+}
