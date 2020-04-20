dfe9e78e4b0af22fe684b6520a5273e9f3485615
Pavel Dohnal <pavel@mailpoet.com>
2020-04-20 15:24:28 +0200

    Display border around the whole form
    pMAILPOET-2809]

--- assets/js/src/form_editor/components/form_styling_background.jsx
+++ assets/js/src/form_editor/components/form_styling_background.jsx
@@ -3,20 +3,38 @@ import PropTypes from 'prop-types';
 import { useSelect } from '@wordpress/data';
 
 const FormStylingBackground = ({ children }) => {
-  const { fontColor, backgroundColor, fontSize } = useSelect(
+  const {
+    fontColor,
+    backgroundColor,
+    fontSize,
+    borderRadius,
+    borderSize,
+    borderColor,
+  } = useSelect(
     (select) => {
       const settings = select('mailpoet-form-editor').getFormSettings();
+
       return {
         backgroundColor: settings.backgroundColor,
         fontColor: settings.fontColor,
         fontSize: settings.fontSize,
+        borderRadius: settings.borderRadius,
+        borderSize: settings.borderSize,
+        borderColor: settings.borderColor,
       };
     },
     []
   );
 
+  let borderStyle;
+  if (borderSize && borderColor) {
+    borderStyle = 'solid';
+  }
+
   let font;
   if (fontSize) font = Number(fontSize);
+  let radius;
+  if (borderRadius) radius = Number(borderRadius);
   return (
     <div
       style={{
@@ -24,6 +42,10 @@ const FormStylingBackground = ({ children }) => {
         color: fontColor,
         fontSize: font,
         lineHeight: 1.2,
+        borderRadius: radius,
+        borderWidth: borderSize,
+        borderColor,
+        borderStyle,
       }}
     >
       {children}
