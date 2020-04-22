f92a7c18b1c8c7ad6b74523f9d92d0d52eb69b9c
Ján Mikláš <neosinner@gmail.com>
2020-04-22 21:21:12 +0200

    Allow loading images in stories
    [MAILPOET-2769]

--- assets/js/src/storybook_demo/_stories/button.tsx
+++ assets/js/src/storybook_demo/_stories/button.tsx
@@ -7,7 +7,12 @@ export default {
   component: Button,
 };
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+export const Text = () => (
+  <Button onClick={action('clicked')}>
+    Hello Button
+    <img src="/assets/img/handle.png" alt="" />
+  </Button>
+);
 
 export const Emoji = () => (
   <Button onClick={action('clicked')}>
