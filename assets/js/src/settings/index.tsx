df3a429c94e07b36f3a12692043de7245adf4761
Jan Jakeš <jan@jakes.pro>
2020-04-27 12:16:10 +0200

    Remove temporary Settings menu selection
    [MAILPOET-2682]

--- assets/js/src/settings/index.tsx
+++ assets/js/src/settings/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import jQuery from 'jquery';
 import 'parsleyjs';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
@@ -18,8 +17,5 @@ const Entry = () => (
 const container = document.getElementById('settings_container');
 if (container) {
   initStore(window);
-  // Select the settings submenu
-  // This is temporary and should be removed at the end of settings refactoring
-  jQuery('ul.wp-submenu > li:nth-child(7)').addClass('current');
   ReactDOM.render(<Entry />, container);
 }
