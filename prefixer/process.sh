1e8a18ed5be006ae84c549349f0fbe770b07e6e1
Jan Jakeš <jan@jakes.pro>
2020-04-28 17:46:03 +0200

    Avoid warning when vendor-prefixed doesn't exist
    [MAILPOET-2580]

--- prefixer/process.sh
+++ prefixer/process.sh
@@ -2,11 +2,14 @@
 
 BASEDIR=$(dirname "$0")
 COMPOSER_BIN=$BASEDIR/../tools/vendor/composer.phar
+VENDOR_PREFIXED_DIR=$BASEDIR/../vendor-prefixed
 DRY_RUN_CHECK=$($COMPOSER_BIN install --working-dir=$BASEDIR --dry-run 2>&1 | grep 'Nothing to install or update')
 
 if [[ $DRY_RUN_CHECK != "Nothing to install or update" ]]; then
     rm -rf $BASEDIR/build
 fi
-if [[ ! -e $BASEDIR/build || -z $(ls -A $BASEDIR/../vendor-prefixed) ]]; then
+
+mkdir -p $VENDOR_PREFIXED_DIR
+if [[ ! -e $BASEDIR/build || -z $(ls -A $VENDOR_PREFIXED_DIR) ]]; then
     $COMPOSER_BIN install --working-dir=$BASEDIR
 fi
