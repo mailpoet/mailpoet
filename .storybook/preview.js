b4a9b942f3386e0f26a0e05269ddb9b31f3073a3
Ján Mikláš <neosinner@gmail.com>
2020-04-29 13:58:03 +0200

    Load plugin styles in Storybook preview
    [MAILPOET-2770]

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { withPerformance } from 'storybook-addon-performance';
+import '../assets/dist/css/mailpoet-plugin.css';
 
 addDecorator(withPerformance);
+addDecorator(story => <div id="wpcontent">{story()}</div>);
