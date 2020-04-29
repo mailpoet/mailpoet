8797b7427a8b982c8a7af3012a1691500afd3c1a
Pavel Dohnal <pavel@mailpoet.com>
2020-04-29 12:25:17 +0200

    Render divider with params
    [MAILPOET-2835]

--- tests/unit/Form/Block/DividerTest.php
+++ tests/unit/Form/Block/DividerTest.php
@@ -23,14 +23,49 @@ class DividerTest extends \MailPoetUnitTest {
     $this->divider = new Divider();
   }
 
-  public function testItShouldRenderDivider() {
-    $html = $this->divider->render($this->block);
-    expect($html)->equals('<hr class="mailpoet_divider" />');
+  public function testItRendersOldDividerWithNoParams() {
+    $result = $this->divider->render($this->block);
+    expect($result)->contains('height: 1px;');
+    expect($result)->contains("class='mailpoet_divider");
+    expect($result)->contains("class='mailpoet_spacer");
+    expect($result)->contains("border-top-style: solid;");
+    expect($result)->contains("border-top-width: 1px;");
+    expect($result)->contains("border-top-color: black;");
+    expect($result)->contains("width: 100%");
   }
 
-  public function testItShouldRenderCustomClass() {
-    $this->block['params']['class_name'] = 'my_class';
-    $html = $this->divider->render($this->block);
-    expect($html)->equals('<hr class="mailpoet_divider my_class" />');
+  public function testItRendersClassName() {
+    $block = $this->block;
+    $block['params']['class_name'] = 'abc';
+    $result = $this->divider->render($block);
+    expect($result)->contains("class='mailpoet_spacer abc");
+  }
+
+  public function testItRendersSpacer() {
+    $block = $this->block;
+    $block['params']['type'] = 'spacer';
+    $block['params']['height'] = '10';
+    $result = $this->divider->render($block);
+    expect($result)->equals("<div class='mailpoet_spacer ' style='height: 10px;'></div>");
+  }
+
+  public function testItRendersDivider() {
+    $block = $this->block;
+    $block['params']['type'] = 'divider';
+    $block['params']['height'] = '12';
+    $block['params']['style'] = 'dotted';
+    $block['params']['divider_height'] = '10';
+    $block['params']['divider_width'] = '50';
+    $block['params']['color'] = 'red';
+
+    $result = $this->divider->render($block);
+
+    expect($result)->contains('height: 12px;');
+    expect($result)->contains("class='mailpoet_divider");
+    expect($result)->contains("class='mailpoet_spacer");
+    expect($result)->contains("border-top-style: dotted;");
+    expect($result)->contains("border-top-width: 10px;");
+    expect($result)->contains("border-top-color: red;");
+    expect($result)->contains("width: 50%");
   }
 }
