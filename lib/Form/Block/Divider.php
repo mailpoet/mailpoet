ce170c0f5722cec1bc808fe527e815d91fa82095
Pavel Dohnal <pavel@mailpoet.com>
2020-04-29 12:25:17 +0200

    Render divider with params
    [MAILPOET-2835]

--- lib/Form/Block/Divider.php
+++ lib/Form/Block/Divider.php
@@ -3,8 +3,41 @@
 namespace MailPoet\Form\Block;
 
 class Divider {
+  const DEFAULT_ATTRIBUTES = [
+    'height' => 1,
+    'type' => 'divider',
+    'style' => 'solid',
+    'dividerHeight' => 1,
+    'dividerWidth' => 100,
+    'color' => 'black',
+  ];
+
   public function render($block): string {
-    $classes = isset($block['params']['class_name']) ? " " . $block['params']['class_name'] : '';
-    return '<hr class="mailpoet_divider' . $classes . '" />';
+    $classes = isset($block['params']['class_name']) ? $block['params']['class_name'] : '';
+    $height = $block['params']['height'] ?? self::DEFAULT_ATTRIBUTES['height'];
+    return "<div class='mailpoet_spacer $classes' style='height: {$height}px;'>"
+    . $this->renderDivider($block)
+    . '</div>';
+  }
+
+  private function renderDivider(array $block): string {
+    if (isset($block['params']['type']) && $block['params']['type'] === 'spacer') {
+      return '';
+    }
+    $width = $block['params']['divider_width'] ?? self::DEFAULT_ATTRIBUTES['dividerWidth'];
+    $style = $block['params']['style'] ?? self::DEFAULT_ATTRIBUTES['style'];
+    $dividerHeight = $block['params']['divider_height'] ?? self::DEFAULT_ATTRIBUTES['dividerHeight'];
+    $color = $block['params']['color'] ?? self::DEFAULT_ATTRIBUTES['color'];
+    $height = $block['params']['height'] ?? self::DEFAULT_ATTRIBUTES['height'];
+
+    $dividerStyles = [
+      "border-top-style: $style",
+      "border-top-width: {$dividerHeight}px",
+      "border-top-color: $color",
+      "height: {$height}px",
+      "width: $width%",
+    ];
+    $style = implode(";", $dividerStyles);
+    return "<div class='mailpoet_divider' style='$style'></div>";
   }
 }
