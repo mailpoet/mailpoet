f186001f38621d040aa4eb09d32437881efa4a1f
Ján Mikláš <neosinner@gmail.com>
2020-04-30 14:57:13 +0200

    Add <Heading> stories
    [MAILPOET-2770]

---
+++ assets/js/src/common/typography/heading/_stories/heading.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Heading from '../heading';
+
+export default {
+  title: 'Typography',
+};
+
+export const Headings = () => (
+  <>
+    <Heading level={1}>Level 1 heading</Heading>
+    <Heading level={2}>Level 2 heading</Heading>
+    <Heading level={3}>Level 3 heading</Heading>
+    <Heading level={4}>Level 4 heading</Heading>
+    <Heading level={5}>Level 5 heading</Heading>
+  </>
+);
