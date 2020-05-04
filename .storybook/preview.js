cfece4449484207cd7dde6cb974ada2aaf952b76
Pavel Dohnal <pavel@mailpoet.com>
2020-05-04 12:14:50 +0200

    Add story for form option
    [MAILPOET-2873]

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { withPerformance } from 'storybook-addon-performance';
 import '../assets/dist/css/mailpoet-plugin.css';
+import '../assets/dist/css/mailpoet-form-editor.css';
 
 addDecorator(withPerformance);
 addDecorator(story => <div id="wpcontent">{story()}</div>);
---
+++ assets/js/src/form_editor/components/form_settings/form_placement_options/_stories/form_placement_option.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Option from '../form_placement_option';
+import Icon from '../icons/sidebar_icon';
+
+export default {
+  title: 'Form Placement Options',
+};
+
+export const Options = () => (
+  <>
+    <Option
+      label="Active option"
+      icon={Icon}
+      active
+      onClick={action('active option click')}
+    />
+    <Option
+      label="Inactive option"
+      icon={Icon}
+      active={false}
+      onClick={action('inactive option click')}
+    />
+    <Option
+      label="Always inactive"
+      icon={Icon}
+      active
+      canBeActive={false}
+      onClick={action('inactive option click')}
+    />
+  </>
+);
+
+export const OptionsListInSidebar = () => (
+  <div className="edit-post-sidebar mailpoet_form_editor_sidebar">
+    <div className="form-placement-option-list">
+      <Option
+        label="Active option"
+        icon={Icon}
+        active
+        onClick={action('active option click')}
+      />
+      <Option
+        label="Inactive option"
+        icon={Icon}
+        active={false}
+        onClick={action('inactive option click')}
+      />
+      <Option
+        label="Always inactive"
+        icon={Icon}
+        active
+        canBeActive={false}
+        onClick={action('inactive option click')}
+      />
+    </div>
+  </div>
+);
