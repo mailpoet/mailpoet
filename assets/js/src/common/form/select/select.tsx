064fccd54171f7ffecc46cd7967426a5b2f5ae1f
Ján Mikláš <neosinner@gmail.com>
2020-05-07 11:55:50 +0200

    Add correct types for Input and Select components
    [MAILPOET-2772]

--- assets/js/src/common/form/select/select.tsx
+++ assets/js/src/common/form/select/select.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { SelectHTMLAttributes } from 'react';
 import classnames from 'classnames';
 
-type Props = {
+type Props = SelectHTMLAttributes<HTMLSelectElement> & {
   children?: React.ReactNode,
   dimension?: 'small',
   isFullWidth?: boolean,
   iconStart?: JSX.Element,
-  [attribute: string]: any, // any HTML attributes, e.g. name, id
 };
 
 const Select = ({
