bfbbfc40c4c2546cecbac829db244ab43b0ff643
wxa <wxa@users.noreply.github.com>
2020-05-11 16:35:19 +0300

    Release 3.46.14

--- mailpoet.php
+++ mailpoet.php
@@ -2,7 +2,7 @@
 
 /*
  * Plugin Name: MailPoet 3 (New)
- * Version: 3.46.13
+ * Version: 3.46.14
  * Plugin URI: http://www.mailpoet.com
  * Description: Create and send newsletters, post notifications and welcome emails from your WordPress.
  * Author: MailPoet
@@ -16,7 +16,7 @@
  */
 
 $mailpoetPlugin = [
-  'version' => '3.46.13',
+  'version' => '3.46.14',
   'filename' => __FILE__,
   'path' => dirname(__FILE__),
   'autoloader' => dirname(__FILE__) . '/vendor/autoload.php',
