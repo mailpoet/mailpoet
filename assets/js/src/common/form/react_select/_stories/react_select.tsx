e3515a733f4f21f7700556384307693f59b5f927
Ján Mikláš <neosinner@gmail.com>
2020-05-13 13:37:01 +0200

    Add story for ReactSelect component
    [MAILPOET-2772]

---
+++ assets/js/src/common/form/react_select/_stories/react_select.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Select from '../react_select';
+import Heading from '../../../typography/heading/heading';
+import icon from './assets/icon';
+
+export default {
+  title: 'Form',
+  component: Select,
+};
+
+export const ReactSelect = () => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+
+  return (
+    <>
+      <Heading level={3}>Small select boxes</Heading>
+      <div>
+        <Select
+          dimension="small"
+          options={options}
+          placeholder="Select single value"
+          onChange={action('small single')}
+        />
+        <div className="mailpoet-gap" />
+        <Select
+          dimension="small"
+          iconStart={icon}
+          options={options}
+          isMulti
+          placeholder="Select multiple values"
+          onChange={action('small multi')}
+        />
+      </div>
+      <br />
+      <Heading level={3}>Regular select boxes</Heading>
+      <div>
+        <Select
+          options={options}
+          placeholder="Select single value"
+          onChange={action('regular single')}
+        />
+        <div className="mailpoet-gap" />
+        <Select
+          iconStart={icon}
+          options={options}
+          defaultValue={options[2]}
+          onChange={action('regular single')}
+        />
+        <div className="mailpoet-gap" />
+        <Select
+          options={options}
+          placeholder="Select multiple values"
+          isMulti
+          onChange={action('regular multi')}
+        />
+        <div className="mailpoet-gap" />
+        <Select
+          iconStart={icon}
+          options={options}
+          isMulti
+          defaultValue={[options[2], options[1]]}
+          onChange={action('regular multi')}
+        />
+      </div>
+      <br />
+      <Heading level={3}>Full-width select boxes</Heading>
+      <div>
+        <Select
+          isFullWidth
+          options={options}
+          isMulti
+          placeholder="Select multiple values"
+          onChange={action('full-width multi')}
+        />
+        <div className="mailpoet-gap" />
+        <Select
+          isFullWidth
+          iconStart={icon}
+          options={options}
+          placeholder="Select single value"
+          onChange={action('full-width single')}
+        />
+      </div>
+      <br />
+    </>
+  );
+};
