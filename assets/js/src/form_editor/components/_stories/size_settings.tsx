60d70ad453938e684bb420a27e878dda3aa187e7
Rostislav Wolny <rosta@mailpoet.com>
2020-05-14 18:06:50 +0200

    Add component for size settings
    [MAILPOET-2811]

---
+++ assets/js/src/form_editor/components/_stories/size_settings.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { SizeSettings } from '../size_settings';
+
+export default {
+  title: 'FormEditor/Size Settings',
+};
+
+export const Settings = () => (
+  <>
+    <SizeSettings
+      label="Basic Size Settings"
+      value={{
+        unit: 'pixel',
+        value: 200,
+      }}
+      onChange={action('on change no action')}
+    />
+    <SizeSettings
+      label="Size Settings With All Props"
+      value={{
+        unit: 'pixel',
+        value: 200,
+      }}
+      defaultPercentValue={25}
+      defaultPixelValue={150}
+      maxPercents={80}
+      maxPixels={500}
+      minPercents={20}
+      minPixels={100}
+      onChange={action('on change no action')}
+    />
+  </>
+);
+
+
+export const SettingsInSidebar = () => (
+  <div className="edit-post-sidebar mailpoet_form_editor_sidebar">
+    <SizeSettings
+      label="Basic Size Settings"
+      value={{
+        unit: 'pixel',
+        value: 200,
+      }}
+      onChange={action('on change no action')}
+    />
+    <SizeSettings
+      label="Size Settings in %"
+      value={{
+        unit: 'percent',
+        value: 100,
+      }}
+      maxPercents={100}
+      minPercents={10}
+      onChange={action('on change no action')}
+    />
+  </div>
+);
