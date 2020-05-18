01a26d37a891a8ce5a3d8dc99fe4ae0b76ee86dd
Ján Mikláš <neosinner@gmail.com>
2020-05-18 16:08:57 +0200

    Add Checkbox and CheckboxGroup component
    [MAILPOET-2773]

---
+++ assets/js/src/common/form/checkbox/group.tsx
@@ -0,0 +1,59 @@
+import React, { InputHTMLAttributes, useState } from 'react';
+import Checkbox from './checkbox';
+
+type CheckboxValueType = string | string[] | number;
+
+type CheckboxProps = InputHTMLAttributes<HTMLInputElement> & {
+  label: string,
+}
+
+type Props = {
+  name: string,
+  options: CheckboxProps[],
+  defaultValue?: CheckboxValueType[]
+  isFullWidth?: boolean,
+  onChange?: (values: CheckboxValueType[]) => void,
+};
+
+const CheckboxGroup = ({
+  name,
+  options,
+  defaultValue,
+  isFullWidth,
+  onChange,
+}: Props) => {
+  const [values, setValues] = useState(defaultValue || []);
+
+  const handleChange = (value: CheckboxValueType, isChecked: boolean) => {
+    const index = values.indexOf(value);
+    let newValues;
+    if (isChecked && index === -1) {
+      newValues = values.concat([value]);
+    }
+    if (!isChecked && index !== -1) {
+      newValues = values.filter((val: CheckboxValueType) => val !== value);
+    }
+    setValues(newValues);
+    onChange(newValues);
+  };
+
+  return (
+    <div>
+      {options.map(({ label, value, ...attributes }: CheckboxProps) => (
+        <Checkbox
+          checked={values.indexOf(value) !== -1}
+          key={label}
+          name={name}
+          value={value}
+          onChange={(isChecked) => handleChange(value, isChecked)}
+          isFullWidth={isFullWidth}
+          {...attributes} // eslint-disable-line react/jsx-props-no-spreading
+        >
+          {label}
+        </Checkbox>
+      ))}
+    </div>
+  );
+};
+
+export default CheckboxGroup;
