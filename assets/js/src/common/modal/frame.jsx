039ea68b60fcf71cc1e292389dae32a525f7227e
Jan Jakeš <jan@jakes.pro>
2020-05-28 16:55:35 +0200

    Split modal to overlay and frame
    [MAILPOET-2777]

--- assets/js/src/common/modal/frame.jsx
+++ assets/js/src/common/modal/frame.jsx
@@ -1,94 +1,39 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const ESCAPE = 27;
-
 function ModalFrame({
-  shouldCloseOnClickOutside,
-  onRequestClose,
-  shouldCloseOnEsc,
-  overlayClassName,
   children,
   className,
   role,
   fullScreen,
 }) {
-  const wrapperRef = useRef(null);
-  function onClose(event) {
-    if (onRequestClose) {
-      onRequestClose(event);
-    }
-  }
-
-  function handleFocusOutside(event) {
-    if (shouldCloseOnClickOutside
-      && wrapperRef.current
-      && !wrapperRef.current.contains(event.target) // filter clicks inside the modal window
-    ) {
-      onClose(event);
-    }
-  }
-
-  function handleEscapeKeyDown(event) {
-    if (shouldCloseOnEsc) {
-      event.stopPropagation();
-      onClose(event);
-    }
-  }
-
-  function handleKeyDown(event) {
-    if (event.keyCode === ESCAPE) {
-      handleEscapeKeyDown(event);
-    }
-  }
-
   return (
     <div
       className={classnames(
-        'mailpoet-modal-screen-overlay',
-        overlayClassName
+        'mailpoet-modal-frame',
+        { 'mailpoet-modal-full-screen': fullScreen },
+        className
       )}
-      onKeyDown={handleKeyDown}
-      onClick={handleFocusOutside}
-      role="button"
-      tabIndex="0"
+      role={role}
+      tabIndex="-1"
     >
-      <div
-        ref={wrapperRef}
-        className={classnames(
-          'mailpoet-modal-frame',
-          { 'mailpoet-modal-full-screen': fullScreen },
-          className
-        )}
-        role={role}
-        tabIndex="-1"
-      >
-        {children}
-      </div>
+      {children}
     </div>
   );
 }
 
 ModalFrame.propTypes = {
-  onRequestClose: PropTypes.func,
-  shouldCloseOnEsc: PropTypes.bool,
   fullScreen: PropTypes.bool,
-  shouldCloseOnClickOutside: PropTypes.bool,
   role: PropTypes.string,
   className: PropTypes.string,
-  overlayClassName: PropTypes.string,
   children: PropTypes.node.isRequired,
 };
 
 ModalFrame.defaultProps = {
-  onRequestClose: () => {},
   role: 'dialog',
-  shouldCloseOnEsc: true,
   fullScreen: false,
-  shouldCloseOnClickOutside: true,
   className: '',
-  overlayClassName: '',
 };
 
 export default ModalFrame;
---
+++ assets/js/src/common/modal/overlay.jsx
@@ -0,0 +1,73 @@
+import React, { useRef } from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+const ESCAPE = 27;
+
+function ModalOverlay({
+  onRequestClose,
+  shouldCloseOnEsc,
+  shouldCloseOnClickOutside,
+  className,
+  children,
+}) {
+  const overlayRef = useRef(null);
+  function onClose(event) {
+    if (onRequestClose) {
+      onRequestClose(event);
+    }
+  }
+
+  function handleFocusOutside(event) {
+    // filter only to clicks on overlay
+    if (shouldCloseOnClickOutside && overlayRef.current === event.target) {
+      onClose(event);
+    }
+  }
+
+  function handleEscapeKeyDown(event) {
+    if (shouldCloseOnEsc) {
+      event.stopPropagation();
+      onClose(event);
+    }
+  }
+
+  function handleKeyDown(event) {
+    if (event.keyCode === ESCAPE) {
+      handleEscapeKeyDown(event);
+    }
+  }
+
+  return (
+    <div
+      ref={overlayRef}
+      className={classnames(
+        'mailpoet-modal-screen-overlay',
+        className
+      )}
+      onKeyDown={handleKeyDown}
+      onClick={handleFocusOutside}
+      role="button"
+      tabIndex="0"
+    >
+      {children}
+    </div>
+  );
+}
+
+ModalOverlay.propTypes = {
+  onRequestClose: PropTypes.func,
+  shouldCloseOnEsc: PropTypes.bool,
+  shouldCloseOnClickOutside: PropTypes.bool,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+ModalOverlay.defaultProps = {
+  onRequestClose: () => {},
+  shouldCloseOnEsc: true,
+  shouldCloseOnClickOutside: true,
+  className: '',
+};
+
+export default ModalOverlay;
