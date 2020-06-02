b5ed7e3e9d7ccef1282aff51d5f114f99d85c5d4
Rostislav Wolny <rosta@mailpoet.com>
2020-06-02 13:14:23 +0200

    Update context segments when a user add one in import
    [MAILPOET-2919]

--- assets/js/src/context/useSegmentsContext.jsx
+++ assets/js/src/context/useSegmentsContext.jsx
@@ -3,5 +3,7 @@ import React from 'react';
 export default function useSegmentsContext(data) {
   return React.useMemo(() => ({
     all: data.mailpoetSegments,
-  }), [data]);
+    // eslint-disable-next-line no-param-reassign
+    updateAll: (segments) => { data.mailpoetSegments = segments; },
+  }), [data.mailpoetSegments]);
 }
