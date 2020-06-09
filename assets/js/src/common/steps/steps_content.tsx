e294fc76cbc25e417c1e81632f635a5d6ca370bb
Amine Ben hammou <amine@mailpoet.com>
2020-06-09 17:46:07 +0200

    Add StepsContent component

---
+++ assets/js/src/common/steps/steps_content.tsx
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import React from 'react';
+
+type Props = {
+  children: React.ReactNode,
+};
+
+const StepsContent = ({ children }: Props) => (
+  <div className="mailpoet-steps-content">{children}</div>
+);
+
+export default StepsContent;
