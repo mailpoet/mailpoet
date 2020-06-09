ce87e3e86079ec23444192c323e476211ca19e34
Amine Ben hammou <amine@mailpoet.com>
2020-06-09 17:46:22 +0200

    Add StepsContent component to Storybook

--- assets/js/src/common/steps/_stories/steps.tsx
+++ assets/js/src/common/steps/_stories/steps.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Steps from '../steps';
+import StepsContent from '../steps_content';
 
 export default {
   title: 'Steps',
@@ -8,15 +9,57 @@ export default {
 export const StepsWithoutTitles = () => (
   <>
     <Steps count={5} current={1} />
+    <StepsContent>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
+      consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
+      odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
+    </StepsContent>
+
+    <div className="mailpoet-gap" />
+
     <Steps count={5} current={3} />
+    <StepsContent>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
+      consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
+      odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
+    </StepsContent>
+
+    <div className="mailpoet-gap" />
+
     <Steps count={5} current={5} />
+    <StepsContent>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
+      consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
+      odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
+    </StepsContent>
   </>
 );
 
 export const StepsWithTitles = () => (
   <>
     <Steps count={4} current={1} titles={['First', 'Second', 'Third', 'Fourth']} />
+    <StepsContent>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
+      consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
+      odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
+    </StepsContent>
+
+    <div className="mailpoet-gap" />
+
     <Steps count={4} current={3} titles={['First', 'Second', 'Third', 'Fourth']} />
+    <StepsContent>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
+      consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
+      odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
+    </StepsContent>
+
+    <div className="mailpoet-gap" />
+
     <Steps count={4} current={4} titles={['First', 'Second', 'Third', 'Fourth']} />
+    <StepsContent>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
+      consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
+      odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
+    </StepsContent>
   </>
 );
