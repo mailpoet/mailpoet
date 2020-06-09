dffc768176f9f6fce619caa1fcc36c626a54f045
Amine Ben hammou <amine@mailpoet.com>
2020-06-09 17:29:30 +0200

    Add Steps component

---
+++ assets/js/src/common/steps/steps.tsx
@@ -0,0 +1,32 @@
+import _ from 'lodash';
+import React from 'react';
+import classNames from 'classnames';
+
+type Props = {
+  count: number,
+  current: number,
+  titles?: string[]
+};
+
+const Steps = ({ count, current, titles }: Props) => (
+  <div className="mailpoet-steps">
+    {_.range(1, count + 1).map((i) => (
+      <div
+        key={i}
+        className={classNames('mailpoet-step', {
+          'mailpoet-step-done': i < current,
+          'mailpoet-step-active': i === current,
+        })}
+      >
+        <div className="mailpoet-step-badge">{i >= current ? i : ''}</div>
+        <div className="mailpoet-step-title">{titles[i - 1] || ''}</div>
+      </div>
+    ))}
+  </div>
+);
+
+Steps.defaultProps = {
+  titles: [],
+};
+
+export default Steps;
