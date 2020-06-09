5fe4c32eb8b3c5cdcf8c55967340733b0d2d7c43
Pavel Dohnal <pavel@mailpoet.com>
2020-06-09 14:03:50 +0200

    Add form settings type
    [MAILPOET-2911]

--- assets/js/src/form_editor/components/form_settings/form_settings.tsx
+++ assets/js/src/form_editor/components/form_settings/form_settings.tsx
@@ -2,13 +2,49 @@ import React from 'react';
 
 import { useDispatch, useSelect } from '@wordpress/data';
 import { partial } from 'lodash';
-import BasicSettingsPanel from './basic_settings_panel.jsx';
-import StylesSettingsPanel from './styles_settings_panel.jsx';
-import FormPlacementPanel from './form_placement_panel.jsx';
-import CustomCssPanel from './custom_css_panel.jsx';
+import BasicSettingsPanel from './basic_settings_panel';
+import StylesSettingsPanel from './styles_settings_panel';
+import FormPlacementPanel from './form_placement_panel';
+import CustomCssPanel from './custom_css_panel';
+
+type PlacementStyles = {
+  width: {
+    unit: string
+    value: number
+  }
+}
 
 export type FormSettingsType = {
-  inputPadding: number,
+  alignment: string
+  backgroundImageDisplay?: string
+  backgroundImageUrl?: string
+  belowPostStyles: PlacementStyles
+  borderColor?: string
+  borderRadius: number
+  borderSize: number
+  errorValidationColor?: string
+  fixedBarFormDelay: number
+  fixedBarFormPosition: string
+  fixedBarStyles: PlacementStyles
+  fontFamily?: string
+  formPadding: number
+  inputPadding: number
+  otherStyles: PlacementStyles
+  placeFixedBarFormOnAllPages: boolean
+  placeFixedBarFormOnAllPosts: boolean
+  placeFormBellowAllPages: boolean
+  placeFormBellowAllPosts: boolean
+  placePopupFormOnAllPages: boolean
+  placePopupFormOnAllPosts: boolean
+  placeSlideInFormOnAllPages: boolean
+  placeSlideInFormOnAllPosts: boolean
+  popupFormDelay: number
+  popupStyles: PlacementStyles
+  segments: Array<string>
+  slideInFormDelay: number
+  slideInFormPosition: string
+  slideInStyles: PlacementStyles
+  successValidationColor?: string
 };
 
 export default () => {
