763135268c418aea8fa38d32719ba9657a7677b1
Amine Ben hammou <amine@mailpoet.com>
2020-06-10 12:30:50 +0200

    Add steps to welcome wizard

--- assets/js/src/wizard/layout/step_layout_body.jsx
+++ assets/js/src/wizard/layout/step_layout_body.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import SteppedProgressBar from '../../common/stepped_progess_bar.jsx';
 
 const WelcomeWizardStepLayoutBody = (props) => (
   <div className="mailpoet_welcome_wizard_flex">
@@ -8,10 +7,6 @@ const WelcomeWizardStepLayoutBody = (props) => (
       <img src={props.illustrationUrl} alt="" />
     </div>
     <div className="mailpoet_welcome_wizard_step">
-      { props.displayProgressBar && (props.step <= props.stepsCount)
-        ? (
-          <SteppedProgressBar steps_count={props.stepsCount} step={props.step} />
-        ) : null}
       {props.children}
     </div>
   </div>
@@ -19,19 +14,10 @@ const WelcomeWizardStepLayoutBody = (props) => (
 
 WelcomeWizardStepLayoutBody.propTypes = {
   illustrationUrl: PropTypes.string.isRequired,
-  displayProgressBar: PropTypes.bool,
-  step: PropTypes.number,
-  stepsCount: PropTypes.number,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]).isRequired,
-};
-
-WelcomeWizardStepLayoutBody.defaultProps = {
-  displayProgressBar: true,
-  step: 0,
-  stepsCount: -1,
 };
 
 export default WelcomeWizardStepLayoutBody;
