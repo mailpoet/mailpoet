1de099fb72836b97db6e15faff348b4cb36725d7
Pavel Dohnal <pavel@mailpoet.com>
2020-06-10 15:39:42 +0200

    Add custom font selection to rich text
    [MAILPOET-2965]

--- assets/js/src/form_editor/rich_text/init.ts
+++ assets/js/src/form_editor/rich_text/init.ts
@@ -1,5 +1,7 @@
 import '@wordpress/format-library';
+import { registerFormatType } from '@wordpress/rich-text';
+import * as FontSelectionFormat from './font_selection_format';
 
 export default function () {
-
+  registerFormatType(FontSelectionFormat.name, FontSelectionFormat.settings);
 }
