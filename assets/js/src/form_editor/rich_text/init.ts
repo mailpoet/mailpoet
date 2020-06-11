1cb7756862815d175a53b30e30d16db8acea86b9
Pavel Dohnal <pavel@mailpoet.com>
2020-06-11 12:37:06 +0200

    Add code comment
    [MAILPOET-2965]

--- assets/js/src/form_editor/rich_text/init.ts
+++ assets/js/src/form_editor/rich_text/init.ts
@@ -1,4 +1,4 @@
-import '@wordpress/format-library';
+import '@wordpress/format-library'; // load default formats (bold, italic, ...)
 import { registerFormatType } from '@wordpress/rich-text';
 import * as FontSelectionFormat from './font_selection_format';
 
