78b79853303d248796ddae64d7b6f84c9a87b4b5
Rostislav Wolny <rosta@mailpoet.com>
2020-06-17 16:15:58 +0200

    Fix image block rendering in form editor
    [MAILPOET-2976]

--- assets/js/src/form_editor/rich_text/font_selection_format.tsx
+++ assets/js/src/form_editor/rich_text/font_selection_format.tsx
@@ -2,11 +2,57 @@ import React from 'react';
 import { applyFormat } from '@wordpress/rich-text';
 import MailPoet from 'mailpoet';
 import { Fill } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
 
 import FontFamilySettings from '../components/font_family_settings';
 
 const name = 'mailpoet-form/font-selection';
 const title = 'Font Selection';
+
+const supportedBlocks = [
+  'core/paragraph',
+  'core/heading',
+];
+
+const Edit = ({
+  value,
+  onChange,
+  activeAttributes,
+}) => {
+  const selectedBlock = useSelect(
+    (sel) => sel('core/block-editor').getSelectedBlock(),
+    []
+  );
+
+  if (!supportedBlocks.includes(selectedBlock.name)) {
+    return null;
+  }
+
+  return (
+    <>
+      <Fill name="BlockFormatControls">
+        <div className="mailpoet_toolbar_item">
+          <FontFamilySettings
+            value={activeAttributes.font}
+            onChange={(font) => {
+              onChange(
+                applyFormat(value, {
+                  type: 'mailpoet-form/font-selection',
+                  attributes: {
+                    style: `font-family: ${font}`,
+                    font,
+                  },
+                })
+              );
+            }}
+            name={MailPoet.I18n.t('formSettingsStylesFontFamily')}
+            hideLabelFromVision
+          />
+        </div>
+      </Fill>
+    </>
+  );
+};
 
 const settings = {
   name,
@@ -17,36 +63,7 @@ const settings = {
     style: 'style',
     font: 'data-font',
   },
-  edit({
-    value,
-    onChange,
-    activeAttributes,
-  }) {
-    return (
-      <>
-        <Fill name="BlockFormatControls">
-          <div className="mailpoet_toolbar_item">
-            <FontFamilySettings
-              value={activeAttributes.font}
-              onChange={(font) => {
-                onChange(
-                  applyFormat(value, {
-                    type: 'mailpoet-form/font-selection',
-                    attributes: {
-                      style: `font-family: ${font}`,
-                      font,
-                    },
-                  })
-                );
-              }}
-              name={MailPoet.I18n.t('formSettingsStylesFontFamily')}
-              hideLabelFromVision
-            />
-          </div>
-        </Fill>
-      </>
-    );
-  },
+  edit: Edit,
 };
 
 export { name, settings };
