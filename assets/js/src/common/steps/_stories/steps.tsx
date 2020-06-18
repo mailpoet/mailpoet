454b8cb3501a6ef793c52a027dee4756717d1082
Ján Mikláš <neosinner@gmail.com>
2020-06-18 17:25:49 +0200

    Allow going through steps in Steps story
    [MAILPOET-2776]

--- assets/js/src/common/steps/_stories/steps.tsx
+++ assets/js/src/common/steps/_stories/steps.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../../button/button';
+import Heading from '../../typography/heading/heading';
 import Steps from '../steps';
 import StepsContent from '../steps_content';
 
@@ -6,60 +8,76 @@ export default {
   title: 'Steps',
 };
 
-export const StepsWithoutTitles = () => (
-  <>
-    <Steps count={5} current={1} />
-    <StepsContent>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
-      consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
-      odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
-    </StepsContent>
+export const StepsWithoutTitles = () => {
+  const [step, setStep] = useState(1);
 
-    <div className="mailpoet-gap" />
+  const nextStep = () => setStep(step + 1);
+  const previousStep = () => setStep(step - 1);
 
-    <Steps count={5} current={3} />
-    <StepsContent>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
-      consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
-      odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
-    </StepsContent>
+  return (
+    <>
+      <Steps count={5} current={step} />
+      <StepsContent>
+        <Heading level={3}>{`Step ${step}`}</Heading>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
+          consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
+          odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
+        </p>
+        <div>
+          <Button
+            onClick={previousStep}
+            dimension="small"
+            isDisabled={step === 1}
+          >
+            Previous step
+          </Button>
+          <Button
+            onClick={nextStep}
+            dimension="small"
+            isDisabled={step === 5}
+          >
+            Next step
+          </Button>
+        </div>
+      </StepsContent>
+    </>
+  );
+};
 
-    <div className="mailpoet-gap" />
+export const StepsWithTitles = () => {
+  const [step, setStep] = useState(1);
 
-    <Steps count={5} current={5} />
-    <StepsContent>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
-      consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
-      odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
-    </StepsContent>
-  </>
-);
+  const nextStep = () => setStep(step + 1);
+  const previousStep = () => setStep(step - 1);
 
-export const StepsWithTitles = () => (
-  <>
-    <Steps count={4} current={1} titles={['First', 'Second', 'Third', 'Fourth']} />
-    <StepsContent>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
-      consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
-      odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
-    </StepsContent>
-
-    <div className="mailpoet-gap" />
-
-    <Steps count={4} current={3} titles={['First', 'Second', 'Third', 'Fourth']} />
-    <StepsContent>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
-      consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
-      odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
-    </StepsContent>
-
-    <div className="mailpoet-gap" />
-
-    <Steps count={4} current={4} titles={['First', 'Second', 'Third', 'Fourth']} />
-    <StepsContent>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
-      consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
-      odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
-    </StepsContent>
-  </>
-);
+  return (
+    <>
+      <Steps count={5} current={step} titles={['First', 'Second', 'Third', 'Fourth', 'Fifth']} />
+      <StepsContent>
+        <Heading level={3}>{`Step ${step}`}</Heading>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta natus
+          consequuntur saepe harum nesciunt eum, a nulla facilis architecto incidunt
+          odio voluptas praesentium, ipsa laboriosam animi! Officiis atque odio nulla.
+        </p>
+        <div>
+          <Button
+            onClick={previousStep}
+            dimension="small"
+            isDisabled={step === 1}
+          >
+            Previous step
+          </Button>
+          <Button
+            onClick={nextStep}
+            dimension="small"
+            isDisabled={step === 5}
+          >
+            Next step
+          </Button>
+        </div>
+      </StepsContent>
+    </>
+  );
+};
