05a1de62f8534623223ff23ad5b1a2a1485f0519
Pavel Dohnal <pavel@mailpoet.com>
2020-06-22 14:20:49 +0200

    Render list of templates
    [MAILPOET-2987]

--- assets/js/src/form_editor/templates/selection.tsx
+++ assets/js/src/form_editor/templates/selection.tsx
@@ -2,8 +2,34 @@ import React from 'react';
 import Heading from 'common/typography/heading/heading';
 import MailPoet from 'mailpoet';
 
-export default () => (
+type Template = {
+  id: string
+  name: string
+};
+
+type Props = {
+  templates: Array<Template>
+  formEditorUrl: string
+};
+
+export default ({ formEditorUrl, templates }: Props) => (
   <div className="template-selection">
     <Heading level={1}>{MailPoet.I18n.t('heading')}</Heading>
+    <ol>
+      <li>
+        <a href={formEditorUrl}>
+          {MailPoet.I18n.t('blankTemplate')}
+        </a>
+      </li>
+      {templates.map((template) => (
+        <li
+          key={template.id}
+        >
+          <a href={`${formEditorUrl}${template.id}`}>
+            {template.name}
+          </a>
+        </li>
+      ))}
+    </ol>
   </div>
 );
