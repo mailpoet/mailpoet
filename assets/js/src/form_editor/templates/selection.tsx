ddf79e01afeabe3e9646d5f5fbed5b7601985406
Pavel Dohnal <pavel@mailpoet.com>
2020-06-23 10:40:35 +0200

    Add automation Id
    [MAILPOET-2987]

--- assets/js/src/form_editor/templates/selection.tsx
+++ assets/js/src/form_editor/templates/selection.tsx
@@ -13,19 +13,21 @@ type Props = {
 };
 
 export default ({ formEditorUrl, templates }: Props) => (
-  <div className="template-selection">
+  <div className="template-selection" data-automation-id="template_selection_list">
     <Heading level={1}>{MailPoet.I18n.t('heading')}</Heading>
     <ol>
-      <li>
+      <li
+        data-automation-id="blank_template"
+      >
         <a href={formEditorUrl}>
           {MailPoet.I18n.t('blankTemplate')}
         </a>
       </li>
-      {templates.map((template) => (
+      {templates.map((template, index) => (
         <li
           key={template.id}
         >
-          <a href={`${formEditorUrl}${template.id}`}>
+          <a href={`${formEditorUrl}${template.id}`} data-automation-id={`template_index_${index}`}>
             {template.name}
           </a>
         </li>
