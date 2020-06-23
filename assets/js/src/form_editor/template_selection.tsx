2ecca72102732a11f2c914fcb16fc92a2c7c244b
Pavel Dohnal <pavel@mailpoet.com>
2020-06-23 10:35:23 +0200

    Remove unneeded code
    [MAILPOET-2987]

--- assets/js/src/form_editor/template_selection.tsx
+++ assets/js/src/form_editor/template_selection.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import apiFetch from '@wordpress/api-fetch';
 import { GlobalContext, useGlobalContextValue } from 'context/index.jsx';
 import Notices from 'notices/notices.jsx';
 import Selection from './templates/selection';
@@ -20,9 +19,6 @@ const App = () => (
 window.addEventListener('DOMContentLoaded', () => {
   const appElement = document.querySelector('#mailpoet_form_edit_templates');
   if (appElement) {
-    // Initialize WP API
-    apiFetch.use(apiFetch.createRootURLMiddleware((window as any).wpApiSettings.root));
-    apiFetch.use(apiFetch.createNonceMiddleware((window as any).wpApiSettings.nonce));
     ReactDOM.render(
       <React.StrictMode>
         <App />
