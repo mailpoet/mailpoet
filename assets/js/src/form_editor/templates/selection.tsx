754172982a6d36bee7c708477d846f62056aaf86
Pavel Dohnal <pavel@mailpoet.com>
2020-06-23 11:15:53 +0200

    Use a store for template selection
    [MAILPOET-2987]

--- assets/js/src/form_editor/templates/selection.tsx
+++ assets/js/src/form_editor/templates/selection.tsx
@@ -1,37 +1,40 @@
 import React from 'react';
 import Heading from 'common/typography/heading/heading';
 import MailPoet from 'mailpoet';
+import { useSelect } from '@wordpress/data';
 
-type Template = {
-  id: string
-  name: string
-};
+import { TemplateType } from './types';
 
-type Props = {
-  templates: Array<Template>
-  formEditorUrl: string
-};
-
-export default ({ formEditorUrl, templates }: Props) => (
-  <div className="template-selection" data-automation-id="template_selection_list">
-    <Heading level={1}>{MailPoet.I18n.t('heading')}</Heading>
-    <ol>
-      <li
-        data-automation-id="blank_template"
-      >
-        <a href={formEditorUrl}>
-          {MailPoet.I18n.t('blankTemplate')}
-        </a>
-      </li>
-      {templates.map((template, index) => (
+export default () => {
+  const templates: Array<TemplateType> = useSelect(
+    (select) => select('mailpoet-form-editor-templates').getTemplates(),
+    []
+  );
+  const formEditorUrl: string = useSelect(
+    (select) => select('mailpoet-form-editor-templates').getFormEditorUrl(),
+    []
+  );
+  return (
+    <div className="template-selection" data-automation-id="template_selection_list">
+      <Heading level={1}>{MailPoet.I18n.t('heading')}</Heading>
+      <ol>
         <li
-          key={template.id}
+          data-automation-id="blank_template"
         >
-          <a href={`${formEditorUrl}${template.id}`} data-automation-id={`template_index_${index}`}>
-            {template.name}
+          <a href={formEditorUrl}>
+            {MailPoet.I18n.t('blankTemplate')}
           </a>
         </li>
-      ))}
-    </ol>
-  </div>
-);
+        {templates.map((template, index) => (
+          <li
+            key={template.id}
+          >
+            <a href={`${formEditorUrl}${template.id}`} data-automation-id={`template_index_${index}`}>
+              {template.name}
+            </a>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
