2d79748d6e4780c00584473165aa136db7b7c735
Ján Mikláš <neosinner@gmail.com>
2020-06-25 17:13:41 +0200

    Simplify NewsletterStatus story
    [MAILPOET-2999]

--- assets/js/src/common/listings/_stories/newsletter_status.tsx
+++ assets/js/src/common/listings/_stories/newsletter_status.tsx
@@ -2,68 +2,48 @@ import React from 'react';
 import { addHours, subHours } from 'date-fns';
 import MailPoet from 'mailpoet';
 import NewsletterStatus from '../newsletter_status';
+import Heading from '../../typography/heading/heading';
 
 MailPoet.I18n.add('notSentYet', 'Not sent yet!');
 
 export default {
   title: 'Listing',
+  component: NewsletterStatus,
 };
 
-export const NewsletterStatusNotSentYet = () => (<NewsletterStatus />);
-
-export const NewsletterStatusScheduledInTheFuture = () => {
+export const NewsletterStatuses = () => {
   const now = new Date();
   const inOneHour = addHours(now, 1);
   const inSixHours = addHours(now, 6);
   const inTwelveHours = addHours(now, 12);
   const inOneDay = addHours(now, 24);
+  const inPast = subHours(now, 24);
+
   return (
     <>
-      <div>
-        <h2>Scheduled in 1 hour</h2>
-        <NewsletterStatus scheduledFor={inOneHour} />
-      </div>
-      <div>
-        <h2>Scheduled in 6 hours</h2>
-        <NewsletterStatus scheduledFor={inSixHours} />
-      </div>
-      <div>
-        <h2>Scheduled in 12 hours</h2>
-        <NewsletterStatus scheduledFor={inTwelveHours} />
-      </div>
-      <div>
-        <h2>Scheduled in 1 day</h2>
-        <NewsletterStatus scheduledFor={inOneDay} />
-      </div>
+      <Heading level={3}>Not sent yet</Heading>
+      <NewsletterStatus />
+
+      <div className="mailpoet-gap" />
+
+      <Heading level={3}>Scheduled in the future</Heading>
+      <NewsletterStatus scheduledFor={inOneHour} />
+      <NewsletterStatus scheduledFor={inSixHours} />
+      <NewsletterStatus scheduledFor={inTwelveHours} />
+      <NewsletterStatus scheduledFor={inOneDay} />
+
+      <div className="mailpoet-gap" />
+
+      <Heading level={3}>Sending in progress</Heading>
+      <NewsletterStatus total={200} processed={0} />
+      <NewsletterStatus total={400} processed={150} />
+      <NewsletterStatus scheduledFor={inPast} total={300} processed={270} />
+
+      <div className="mailpoet-gap" />
+
+      <Heading level={3}>Sent</Heading>
+      <NewsletterStatus total={500} processed={500} />
+      <NewsletterStatus total={1000} processed={1200} />
     </>
   );
 };
-
-export const NewsletterStatusSendingInProgress = () => {
-  const scheduledFor = subHours(new Date(), 1);
-  return (
-    <>
-      <div>
-        <h2>Progress at 0%</h2>
-        <NewsletterStatus scheduledFor={scheduledFor} total={100} processed={0} />
-      </div>
-      <div>
-        <h2>Progress at 30%</h2>
-        <NewsletterStatus scheduledFor={scheduledFor} total={100} processed={30} />
-      </div>
-      <div>
-        <h2>Progress at 90%</h2>
-        <NewsletterStatus scheduledFor={scheduledFor} total={100} processed={90} />
-      </div>
-    </>
-  );
-};
-
-
-export const NewsletterStatusSent = () => (
-  <NewsletterStatus
-    scheduledFor={subHours(new Date(), 1)}
-    total={100}
-    processed={100}
-  />
-);
