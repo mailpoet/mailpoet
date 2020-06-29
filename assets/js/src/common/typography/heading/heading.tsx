02873b3aa4b310d189f1ef642b49d0b71bb0f7de
Ján Mikláš <neosinner@gmail.com>
2020-06-29 11:32:36 +0200

    Unify how we create components
    [MAILPOET-2784]

--- assets/js/src/common/typography/heading/heading.tsx
+++ assets/js/src/common/typography/heading/heading.tsx
@@ -8,12 +8,14 @@ type Props = {
 const Heading = ({
   children,
   level,
-}: Props) => (
-  React.createElement(
-    `h${level || 1}`,
-    { className: `mailpoet-h${level}` },
-    children
-  )
-);
+}: Props) => {
+  // This is written like this because of linter errors on React's IntrinsicAttributes
+  const Element = level === 5 ? 'h5' : level === 4 ? 'h4' : level === 3 ? 'h3' : level === 2 ? 'h2' : 'h1'; // eslint-disable-line no-nested-ternary
+  return (
+    <Element className={`mailpoet-h${level}`}>
+      {children}
+    </Element>
+  );
+};
 
 export default Heading;
