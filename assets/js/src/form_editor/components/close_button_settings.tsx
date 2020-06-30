de6862c84094b243e1e912fbd077e8bda5746e59
Pavel Dohnal <pavel@mailpoet.com>
2020-06-30 12:46:42 +0200

    Add component for close button selection
    [MAILPOET-2963]

--- assets/js/src/form_editor/components/close_button_settings.tsx
+++ assets/js/src/form_editor/components/close_button_settings.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
 
+import SelectionItem from './form_settings/selection_item';
+
+type CloseButtonProps = {
+  label: string
+  active: boolean
+  onClick: () => any
+  icon: string
+}
+
+const CloseButton = ({
+  label,
+  active,
+  onClick,
+  icon,
+}: CloseButtonProps) => (
+  <SelectionItem
+    label={label}
+    onClick={onClick}
+    active={active}
+    canBeActive
+    className="close-button-selection-item"
+  >
+    <div>{icon}</div>
+  </SelectionItem>
+);
+
+
 type Props = {
-  name: string,
+  name: string
   value?: number|undefined
   onChange: (value: string|undefined) => any
 }
 
-const CloseButtonSettings = ({
+const CloseButtonsSettings = ({
   name,
   value,
   onChange,
@@ -15,7 +42,42 @@ const CloseButtonSettings = ({
     <h3 className="mailpoet-styles-settings-heading">
       {name}
     </h3>
+    <div className="close-button-selection-item-list">
+      <CloseButton
+        label="kjk1"
+        active={false}
+        icon="abc"
+        onClick={() => onChange('abcv')}
+      />
+      <CloseButton
+        label="kjk2"
+        active
+        icon="def"
+        onClick={() => onChange('abcv')}
+      />
+      <CloseButton
+        label="kjk3"
+        active={false}
+        icon="gha"
+        onClick={() => onChange('abcv')}
+      />
+      <CloseButton
+        label="kjk4"
+        active={false}
+        icon="ijk"
+        onClick={() => onChange('abcv')}
+      />
+      <CloseButton
+        label="kjk5"
+        active={false}
+        icon="lmn"
+        onClick={() => onChange('abcv')}
+      />
+    </div>
   </div>
 );
 
-export default CloseButtonSettings;
+export {
+  CloseButtonsSettings,
+  CloseButton,
+};
