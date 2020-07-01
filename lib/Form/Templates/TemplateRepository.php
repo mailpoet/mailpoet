8114715b8726b59209c7869d3b852245cf0cc430
Rostislav Wolny <rosta@mailpoet.com>
2020-07-01 09:18:08 +0200

    Add correct success message to form created from template
    [MAILPOET-2985]

--- lib/Form/Templates/TemplateRepository.php
+++ lib/Form/Templates/TemplateRepository.php
@@ -3,6 +3,7 @@
 namespace MailPoet\Form\Templates;
 
 use MailPoet\Entities\FormEntity;
+use MailPoet\Settings\SettingsController;
 use MailPoet\UnexpectedValueException;
 
 class TemplateRepository {
@@ -13,6 +14,13 @@ class TemplateRepository {
     'demo_form' => DemoForm::class,
   ];
 
+  /** @var SettingsController */
+  private $settings;
+
+  public function __construct(SettingsController $settings) {
+    $this->settings = $settings;
+  }
+
   public function getFormEntityForTemplate($templateId): FormEntity {
     if (!isset($this->templates[$templateId])) {
       throw UnexpectedValueException::create()
@@ -22,8 +30,17 @@ class TemplateRepository {
     $template = new $this->templates[$templateId]();
     $formEntity = new FormEntity($template->getName());
     $formEntity->setBody($template->getBody());
-    $formEntity->setSettings($template->getSettings());
+    $settings = $formEntity->getSettings();
+    $settings['success_message'] = $this->getDefaultSuccessMessage();
+    $formEntity->setSettings($settings);
     $formEntity->setStyles($template->getStyles());
     return $formEntity;
+  }
+
+  private function getDefaultSuccessMessage() {
+    if ($this->settings->get('signup_confirmation.enabled')) {
+      return __('Check your inbox or spam folder to confirm your subscription.', 'mailpoet');
+    }
+    return __('You’ve been successfully subscribed to our newsletter!', 'mailpoet');
   }
 }
--- tests/unit/Form/Templates/TemplatesRepositoryTest.php
+++ tests/unit/Form/Templates/TemplatesRepositoryTest.php
@@ -4,21 +4,41 @@ namespace MailPoet\Test\Form\Templates;
 
 use MailPoet\Entities\FormEntity;
 use MailPoet\Form\Templates\TemplateRepository;
+use MailPoet\Settings\SettingsController;
+use PHPUnit\Framework\MockObject\MockObject;
 
 class TemplatesRepositoryTest extends \MailPoetUnitTest {
   /** @var TemplateRepository */
   private $repository;
 
+  /** @var SettingsController & MockObject */
+  private $settingsMock;
+
   public function _before() {
     parent::_before();
-    $this->repository = new TemplateRepository();
+    $this->settingsMock = $this->createMock(SettingsController::class);
+    $this->repository = new TemplateRepository($this->settingsMock);
   }
 
   public function testItCanBuildFormEntity() {
+    $this->settingsMock->method('get')->willReturn(true);
     $formEntity = $this->repository->getFormEntityForTemplate(TemplateRepository::INITIAL_FORM_TEMPLATE);
     expect($formEntity)->isInstanceOf(FormEntity::class);
     expect($formEntity->getStyles())->notEmpty();
     expect($formEntity->getBody())->notEmpty();
     expect($formEntity->getSettings())->notEmpty();
+  }
+
+  public function testItSetsDefaultMessage() {
+    $this->settingsMock->method('get')->willReturnOnConsecutiveCalls([true, false]);
+    $formEntity = $this->repository->getFormEntityForTemplate(TemplateRepository::INITIAL_FORM_TEMPLATE);
+    expect($formEntity)->isInstanceOf(FormEntity::class);
+    $settings = $formEntity->getSettings();
+    expect($settings['success_message'])->equals('Check your inbox or spam folder to confirm your subscription.');
+
+    $formEntity = $this->repository->getFormEntityForTemplate(TemplateRepository::INITIAL_FORM_TEMPLATE);
+    expect($formEntity)->isInstanceOf(FormEntity::class);
+    $settings = $formEntity->getSettings();
+    expect($settings['success_message'])->equals('You’ve been successfully subscribed to our newsletter!');
   }
 }
