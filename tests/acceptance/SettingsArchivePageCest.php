7e0c165c459e8342aa295fac78803eb93b504a5d
Veljko V <veljano@yahoo.com>
2020-07-06 13:23:03 +0200

    Add scenario various status newsletters
    MAILPOET-3042

--- tests/acceptance/SettingsArchivePageCest.php
+++ tests/acceptance/SettingsArchivePageCest.php
@@ -37,4 +37,42 @@ class SettingsArchivePageCest {
     $i->waitForText($pageTitle2);
     $i->waitForText('SentNewsletter');
   }
+
+  public function createArchivePageWithVariousStatusNewsletters(\AcceptanceTester $i) {
+    $i->wantTo('Create page with MP archive shortcode, showing only sent newsletters but having various in database');
+    $segmentFactory = new Segment();
+    $segment3 = $segmentFactory->withName('SentNewsletters')->create();
+    $newsletterFactory = new Newsletter();
+    $newsletterFactory->withSubject('SentNewsletter')->withSentStatus()->withSendingQueue()->withSegments([$segment3])->create();
+    $newsletterFactory->withSubject('DraftNewsletter')->withDraftStatus()->withScheduledQueue()->withSegments([$segment3])->create();
+    $newsletterFactory->withSubject('ScheduledNewsletter')->withScheduledStatus()->withScheduledQueue()->withSegments([$segment3])->create();
+    $pageTitle3 = 'SentNewsletterArchive';
+    $pageContent3 = "[mailpoet_archive segments=\"$segment3->id\"]";
+    $i->cli(['post', 'create', '--post_type=page', '--post_status=publish', "--post_title=$pageTitle3", "--post_content=$pageContent3"]);
+    $i->login();
+    $i->amOnPage('/wp-admin/edit.php?post_type=page');
+    $i->waitForText($pageTitle3);
+    $i->clickItemRowActionByItemName($pageTitle3, 'View');
+    $i->waitForText($pageTitle3);
+    $i->waitForText('SentNewsletter');
+    $i->dontSee('DraftNewsletter');
+    $i->dontSee('ScheduledNewsletter');
+    $newsletterFactory->withSubject('SentNewsletter2')->withDraftStatus()->withSendingQueue()->withSegments([$segment3])->create();
+    $newsletterFactory->withSubject('SentNewsletter3')->withDraftStatus()->withSendingQueue()->withSegments([$segment3])->create();
+    $i->reloadPage();
+    $i->waitForText('SentNewsletter');
+    $i->waitForText('SentNewsletter2');
+    $i->waitForText('SentNewsletter3');
+    $i->amOnMailpoetPage('Emails');
+    $i->waitForText('SentNewsletter3');
+    $i->clickItemRowActionByItemName('SentNewsletter3', 'Move to trash');
+    $i->waitForText('1 email was moved to the trash.');
+    $i->amOnPage('/wp-admin/edit.php?post_type=page');
+    $i->waitForText($pageTitle3);
+    $i->clickItemRowActionByItemName($pageTitle3, 'View');
+    $i->waitForText($pageTitle3);
+    $i->waitForText('SentNewsletter');
+    $i->waitForText('SentNewsletter2');
+    $i->dontSee('SentNewsletter3');
+  }
 }
