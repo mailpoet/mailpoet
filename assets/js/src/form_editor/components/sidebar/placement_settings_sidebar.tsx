5568e06c0bed59a05fc1945eb75a87b429826b44
Rostislav Wolny <rosta@mailpoet.com>
2020-07-08 15:18:58 +0200

    Add placement settings sidebar component
    [MAILPOET-3030]

---
+++ assets/js/src/form_editor/components/sidebar/placement_settings_sidebar.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSelect } from '@wordpress/data';
+import MailPoet from 'mailpoet';
+import PlacementSettingsPanel from 'form_editor/components/form_settings/form_placement_options/settings_panel';
+import SidebarHeader from './sidebar_header';
+
+type Props = {
+  onClose: () => any,
+}
+
+export default ({ onClose }:Props) => {
+  const previewSettings = useSelect(
+    (select) => select('mailpoet-form-editor').getPreviewSettings(),
+    []
+  );
+  return (
+    <>
+      <SidebarHeader closeSidebar={onClose}>
+        <h3 className="mailpoet-sidebar-header-heading">
+          {previewSettings.formType === 'others' && MailPoet.I18n.t('placeFormOthers')}
+          {previewSettings.formType === 'below_post' && MailPoet.I18n.t('placeFormBellowPages')}
+          {previewSettings.formType === 'fixed_bar' && MailPoet.I18n.t('placeFixedBarFormOnPages')}
+          {previewSettings.formType === 'popup' && MailPoet.I18n.t('placePopupFormOnPages')}
+          {previewSettings.formType === 'slide_in' && MailPoet.I18n.t('placeSlideInFormOnPages')}
+        </h3>
+      </SidebarHeader>
+      <PlacementSettingsPanel activePanel={previewSettings.formType} />
+    </>
+  );
+};
