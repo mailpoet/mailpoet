858b1e10de136c6a9c1755c0dc110dbbd3a3b2e0
Rostislav Wolny <rosta@mailpoet.com>
2020-07-08 13:36:20 +0200

    Refactor placement settings to reusable panel
    [MAILPOET-3030]

---
+++ assets/js/src/form_editor/components/form_settings/form_placement_options/settings_panel.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import BelowPostsSettings from './settings_panels/below_posts_settings';
+import PopUpSettings from './settings_panels/popup_settings';
+import OtherSettings from './settings_panels/other_settings';
+import FixedBarSettings from './settings_panels/fixed_bar_settings';
+import SlideInSettings from './settings_panels/slide_in_settings';
+
+type Props = {
+  activePanel: string
+}
+
+const SettingsPanel = ({ activePanel }: Props) => (
+  <>
+    {activePanel === 'others' && <OtherSettings />}
+    {activePanel === 'below_post' && <BelowPostsSettings />}
+    {activePanel === 'fixed_bar' && <FixedBarSettings />}
+    {activePanel === 'popup' && <PopUpSettings />}
+    {activePanel === 'slide_in' && <SlideInSettings />}
+  </>
+);
+
+export default SettingsPanel;
