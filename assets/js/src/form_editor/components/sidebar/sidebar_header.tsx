f3715bcd321212bdc5ecda86df389c668235f421
Rostislav Wolny <rosta@mailpoet.com>
2020-07-08 14:24:04 +0200

    Refactor sidebar header to an extra file
    [MAILPOET-3030]

---
+++ assets/js/src/form_editor/components/sidebar/sidebar_header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button } from '@wordpress/components';
+
+type Props = {
+  closeSidebar: () => any,
+  children: React.ReactNode | React.ReactNode[],
+}
+
+const SidebarHeader = ({ children, closeSidebar }: Props) => (
+  <>
+    <div className="components-panel__header interface-complementary-area-header edit-post-sidebar__panel-tabs">
+      { children }
+      <Button
+        onClick={closeSidebar}
+        icon="no-alt"
+      />
+    </div>
+  </>
+);
+
+export default SidebarHeader;
