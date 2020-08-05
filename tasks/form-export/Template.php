4d8a30f98e0de99d70a92eb6e6fbf3fbe76b175e
Rostislav Wolny <rosta@mailpoet.com>
2020-08-05 17:32:01 +0200

    Add simple plugin for exporting forms to Template class
    [MAILPOET-2810]

---
+++ tasks/form-export/Template.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MailPoet\Form\Templates\Templates;
+
+use MailPoet\Form\Templates\FormTemplate;
+
+class Template extends FormTemplate {
+  public function getName(): string {
+    return 'TEMPLATE_NAME';
+  }
+
+  public function getBody(): array {
+    return TEMPLATE_BODY;
+  }
+
+  public function getSettings(): array {
+    return TEMPLATE_SETTINGS;
+  }
+
+  public function getStyles(): string {
+    return <<<EOL
+TEMPLATE_STYLES
+EOL;
+  }
+}
