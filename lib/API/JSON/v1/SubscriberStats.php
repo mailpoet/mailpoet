2155a616e2ed9034ec42311f071d9d82209dc6cb
Pavel Dohnal <pavel@mailpoet.com>
2020-08-05 15:11:22 +0200

    Add endpoint for subscriber statistics
    [MAILPOET-3069]

---
+++ lib/API/JSON/v1/SubscriberStats.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MailPoet\API\JSON\v1;
+
+use MailPoet\API\JSON\Endpoint as APIEndpoint;
+use MailPoet\API\JSON\Error as APIError;
+use MailPoet\Config\AccessControl;
+use MailPoet\Entities\SubscriberEntity;
+use MailPoet\Subscribers\Statistics\SubscriberStatisticsRepository;
+use MailPoet\Subscribers\SubscribersRepository;
+use MailPoet\WP\Functions as WPFunctions;
+
+class SubscriberStats extends APIEndpoint {
+  public $permissions = [
+    'global' => AccessControl::PERMISSION_MANAGE_SUBSCRIBERS,
+  ];
+
+  /** @var SubscribersRepository */
+  private $subscribersRepository;
+
+  /** @var SubscriberStatisticsRepository */
+  private $subscribersStatisticsRepository;
+
+  public function __construct(
+    SubscribersRepository $subscribersRepository,
+    SubscriberStatisticsRepository $subscribersStatisticsRepository
+  ) {
+    $this->subscribersRepository = $subscribersRepository;
+    $this->subscribersStatisticsRepository = $subscribersStatisticsRepository;
+  }
+
+  public function get($data) {
+    $subscriber = isset($data['subscriber_id'])
+      ? $this->subscribersRepository->findOneById((int)$data['subscriber_id'])
+      : null;
+    if (!$subscriber instanceof SubscriberEntity) {
+      return $this->errorResponse([
+        APIError::NOT_FOUND => WPFunctions::get()->__('This subscriber does not exist.', 'mailpoet'),
+      ]);
+    }
+    $statistics = $this->subscribersStatisticsRepository->getStatistics($subscriber);
+    return $this->successResponse([
+      'email' => $subscriber->getEmail(),
+      'total_sent' => $statistics->getTotalSentCount(),
+      'open' => $statistics->getOpenCount(),
+      'click' => $statistics->getClickCount(),
+    ]);
+  }
+}
