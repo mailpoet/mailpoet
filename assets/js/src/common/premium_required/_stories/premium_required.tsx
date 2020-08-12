826ae249556fdfa96765f88fccbf792aa620c802
Rostislav Wolny <rosta@mailpoet.com>
2020-08-12 17:07:00 +0200

    Add simple premium required component
    [MAILPOET-3083]

---
+++ assets/js/src/common/premium_required/_stories/premium_required.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PremiumRequired from '../premium_required';
+import Button from '../../button/button';
+
+export default {
+  title: 'Premium Required',
+  component: PremiumRequired,
+};
+
+export const PremiumsRequired = () => (
+  <>
+    <div>
+      <PremiumRequired
+        title="This is a Premium Feature"
+        message={(
+          <p>
+            Learn more about your subscribers and optimize your campaigns. See who
+            opened your emails, which links they clicked, and then use the data to make y
+            our emails even better. And if you run a WooCommerce store, you will also
+            see the revenue earned per email.
+            <a href="#">
+              Learn more.
+            </a>
+          </p>
+        )}
+        actionButton={(<Button href="#">Sign Up</Button>)}
+      />
+    </div>
+  </>
+);
---
+++ assets/js/src/common/premium_required/premium_required.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Badge from 'common/badge/badge';
+import Heading from 'common/typography/heading/heading';
+
+type Props = {
+  title: string,
+  message: React.ReactNode,
+  actionButton: React.ReactNode,
+};
+
+const PremiumRequired = ({ title, message, actionButton }: Props) => (
+  <div className="mailpoet-premium-required">
+    <div className="mailpoet-premium-required-message">
+      <Heading level={5}>
+        <Badge title="Premium" />
+        {' '}
+        {title}
+      </Heading>
+      {message}
+    </div>
+    <div className="mailpoet-premium-required-button">
+      {actionButton}
+    </div>
+  </div>
+);
+
+export default PremiumRequired;
