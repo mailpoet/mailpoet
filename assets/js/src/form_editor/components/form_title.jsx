fe3c4d2997e02c2333cd438680e7f6d7be594476
Pavel Dohnal <pavel@mailpoet.com>
2020-08-13 14:36:20 +0200

    Remove elements that made input too wide
    [MAILPOET-3086]

--- assets/js/src/form_editor/components/form_title.jsx
+++ assets/js/src/form_editor/components/form_title.jsx
@@ -15,25 +15,21 @@ export default () => {
   const { changeFormName } = useDispatch('mailpoet-form-editor');
 
   return (
-    <div className="edit-post-visual-editor__post-title-wrapper">
-      <div className={titleClass}>
-        <div>
-          <label htmlFor="post-title" className="screen-reader-text">
-            {MailPoet.I18n.t('addFormName')}
-          </label>
-          <input
-            id="form-title"
-            className="form-editor-title"
-            placeholder={MailPoet.I18n.t('addFormName')}
-            data-automation-id="form_title_input"
-            type="text"
-            onKeyPress={() => setIsSelected(false)}
-            onBlur={() => setIsSelected(false)}
-            onChange={(e) => changeFormName(e.target.value)}
-            value={title}
-          />
-        </div>
-      </div>
+    <div className={titleClass}>
+      <label htmlFor="post-title" className="screen-reader-text">
+        {MailPoet.I18n.t('addFormName')}
+      </label>
+      <input
+        id="form-title"
+        className="form-editor-title"
+        placeholder={MailPoet.I18n.t('addFormName')}
+        data-automation-id="form_title_input"
+        type="text"
+        onKeyPress={() => setIsSelected(false)}
+        onBlur={() => setIsSelected(false)}
+        onChange={(e) => changeFormName(e.target.value)}
+        value={title}
+      />
     </div>
   );
 };
