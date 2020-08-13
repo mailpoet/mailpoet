ceb69a58045f8dd9da4db768e82f781f1cd328d0
Rostislav Wolny <rosta@mailpoet.com>
2020-08-13 16:26:57 +0200

    Fix Normalizer polyfill escaping
    [MAILPOET-3105]

--- prefixer/fix-symfony-polyfill.php
+++ prefixer/fix-symfony-polyfill.php
@@ -7,5 +7,10 @@ set_error_handler(function ($severity, $message, $file, $line) {
 
 $file = __DIR__ . '/../vendor-prefixed/symfony/polyfill-intl-idn/Idn.php';
 $data = file_get_contents($file);
-$data = str_replace('use Normalizer;', 'use MailPoetVendor\\Normalizer;', $data);
+$data = str_replace('\\Normalizer::', '\\MailPoetVendor\\Normalizer::', $data);
+file_put_contents($file, $data);
+
+$file = __DIR__ . '/../vendor-prefixed/symfony/polyfill-intl-normalizer/Normalizer.php';
+$data = file_get_contents($file);
+$data = str_replace('\\Normalizer::', '\\MailPoetVendor\\Normalizer::', $data);
 file_put_contents($file, $data);
