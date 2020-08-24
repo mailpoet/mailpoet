a3aedb6805dbc8541861247e53a382f8f332b947
Rostislav Wolny <rosta@mailpoet.com>
2020-08-24 13:46:07 +0200

    Add back button to subscriber stats page
    [PREMIUM-145]

--- assets/js/src/subscribers/stats/heading.tsx
+++ assets/js/src/subscribers/stats/heading.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import MailPoet from 'mailpoet';
+import { Link, useLocation } from 'react-router-dom';
 import Heading from 'common/typography/heading/heading';
+import { LocationState } from 'subscribers/location_state';
 
 export type PropTypes = {
   email: string
 }
 
-export default ({ email }: PropTypes) => (
-  <Heading level={0}>
-    {MailPoet.I18n.t('statsHeading').replace('%s', email)}
-  </Heading>
-);
+export default ({ email }: PropTypes) => {
+  const location = useLocation<LocationState>();
+  const backUrl = location.state?.backUrl || '/';
+  return (
+    <Heading level={0}>
+      {MailPoet.I18n.t('statsHeading').replace('%s', email)}
+      <Link className="page-title-action" to={backUrl}>{MailPoet.I18n.t('backToList')}</Link>
+    </Heading>
+  );
+};
