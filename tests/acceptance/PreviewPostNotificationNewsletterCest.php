98f1b7a78d8ca5f21bce6b2c700cf411f5a109ff
Veljko V <veljano@yahoo.com>
2020-08-26 12:37:06 +0200

    Add 2 new scenarios to newsletter preview testcases
    [MAILPOET-3118]

--- tests/acceptance/PreviewPostNotificationNewsletterCest.php
+++ tests/acceptance/PreviewPostNotificationNewsletterCest.php
@@ -5,7 +5,7 @@ namespace MailPoet\Test\Acceptance;
 use MailPoet\Test\DataFactories\Newsletter;
 
 class PreviewPostNotificationNewsletterCest {
-  public function previewStandardNewsletter(\AcceptanceTester $i) {
+  public function previewPostNotificationNewsletter(\AcceptanceTester $i) {
     $newsletterName = 'Preview in Browser Post Notification';
     $newsletter = new Newsletter();
     $newsletter->withSubject($newsletterName)->withPostNotificationsType()->create();
@@ -15,6 +15,35 @@ class PreviewPostNotificationNewsletterCest {
     $i->click('Post Notifications', '[data-automation-id="newsletters_listing_tabs"]');
     $i->waitForText($newsletterName);
     $i->clickItemRowActionByItemName($newsletterName, 'Preview');
+    $i->switchToNextTab();
+    $i->waitForElement('.mailpoet_template');
+  }
+
+  public function previewPostNotificationNewsletterInEditor(\AcceptanceTester $i) {
+    $i->wantTo('Preview and send post notification newsletter inside editor');
+    $newsletterName = 'Preview in Browser Post Notification';
+    $newsletter = new Newsletter();
+    $newsletter->withSubject($newsletterName)->withPostNotificationsType()->create();
+    $i->login();
+    $i->amOnMailpoetPage('Emails');
+    $i->waitForText('Emails');
+    $i->click('Post Notifications', '[data-automation-id="newsletters_listing_tabs"]');
+    $i->waitForText($newsletterName);
+    $i->clickItemRowActionByItemName($newsletterName, 'Edit');
+    $i->click('[data-automation-id="sidebar_preview_region_heading"]');
+    $i->waitForText('Send preview');
+    $i->click('Send preview');
+    $i->waitForText('Your test email has been sent!');
+    $i->click('.notice-dismiss');
+    $i->clearField('#mailpoet_preview_to_email');
+    $i->click('Send preview');
+    $i->waitForText('Enter an email address to send the preview newsletter to.');
+    $i->fillField('#mailpoet_preview_to_email', 'test2@test.com');
+    $i->click('Send preview');
+    $i->waitForText('Your test email has been sent!');
+    $i->click('View in browser');
+    $i->waitForText('Newsletter Preview');
+    $i->click('Open in new tab');
     $i->switchToNextTab();
     $i->waitForElement('.mailpoet_template');
   }
--- tests/acceptance/PreviewStandardNewsletterCest.php
+++ tests/acceptance/PreviewStandardNewsletterCest.php
@@ -17,4 +17,31 @@ class PreviewStandardNewsletterCest {
     $i->switchToNextTab();
     $i->waitForElement('.mailpoet_template');
   }
+
+  public function previewStandardNewsletterInEditor(\AcceptanceTester $i) {
+    $i->wantTo('Preview and send newsletter inside editor');
+    $newsletterName = 'Preview in Browser Newsletter';
+    $newsletter = new Newsletter();
+    $newsletter->withSubject($newsletterName)->create();
+    $i->login();
+    $i->amOnMailpoetPage('Emails');
+    $i->waitForText($newsletterName);
+    $i->clickItemRowActionByItemName($newsletterName, 'Edit');
+    $i->click('[data-automation-id="sidebar_preview_region_heading"]');
+    $i->waitForText('Send preview');
+    $i->click('Send preview');
+    $i->waitForText('Your test email has been sent!');
+    $i->click('.notice-dismiss');
+    $i->clearField('#mailpoet_preview_to_email');
+    $i->click('Send preview');
+    $i->waitForText('Enter an email address to send the preview newsletter to.');
+    $i->fillField('#mailpoet_preview_to_email', 'test2@test.com');
+    $i->click('Send preview');
+    $i->waitForText('Your test email has been sent!');
+    $i->click('View in browser');
+    $i->waitForText('Newsletter Preview');
+    $i->click('Open in new tab');
+    $i->switchToNextTab();
+    $i->waitForElement('.mailpoet_template');
+  }
 }
