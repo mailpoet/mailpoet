0a0ec7d787b4994366f4211b4c2a4d465d2011c5
Rostislav Wolny <rosta@mailpoet.com>
2020-08-27 16:46:23 +0200

    Add getCount to StatisticsClickEntity
    [PREMIUM-145]

--- lib/Entities/StatisticsClickEntity.php
+++ lib/Entities/StatisticsClickEntity.php
@@ -138,4 +138,8 @@ class StatisticsClickEntity {
   public function getWooCommercePurchases() {
     return $this->wooCommercePurchases;
   }
+
+  public function getCount(): int {
+    return $this->count;
+  }
 }
