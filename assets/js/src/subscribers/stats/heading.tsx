7cca09aa9448683d8338f50cd932ca1989eec53b
Rostislav Wolny <rosta@mailpoet.com>
2020-09-01 15:11:50 +0200

    Adjust subscriber stats main heading
    [PREMIUM-145]

--- assets/js/src/subscribers/stats/heading.tsx
+++ assets/js/src/subscribers/stats/heading.tsx
@@ -12,7 +12,7 @@ export default ({ email }: PropTypes) => {
   const location = useLocation<LocationState>();
   const backUrl = location.state?.backUrl || '/';
   return (
-    <Heading level={0}>
+    <Heading level={1}>
       {MailPoet.I18n.t('statsHeading').replace('%s', email)}
       <Link className="page-title-action" to={backUrl}>{MailPoet.I18n.t('backToList')}</Link>
     </Heading>
