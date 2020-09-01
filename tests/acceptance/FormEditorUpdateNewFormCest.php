7cf2a68019fa819eea4373b1518c9c415fc50f0a
Veljko V <veljano@yahoo.com>
2020-09-01 14:58:53 +0200

    Update scenario of updating the form
    [MAILPOET-3124]

--- tests/acceptance/FormEditorUpdateNewFormCest.php
+++ tests/acceptance/FormEditorUpdateNewFormCest.php
@@ -3,17 +3,45 @@
 namespace MailPoet\Test\Acceptance;
 
 use MailPoet\Test\DataFactories\Segment;
+use MailPoet\Test\DataFactories\Settings;
 
 class FormEditorUpdateNewFormCest {
+
+  const CONFIRMATION_MESSAGE_TIMEOUT = 20;
+
+  public function _before(\AcceptanceTester $i) {
+    $settings = new Settings();
+    $settings
+      ->withConfirmationEmailSubject()
+      ->withConfirmationEmailBody()
+      ->withConfirmationEmailEnabled();
+    $i->havePostInDatabase([
+      'post_author' => 1,
+      'post_type' => 'page',
+      'post_name' => 'form-test',
+      'post_title' => 'Form Test',
+      'post_content' => '
+        Regular form:
+          [mailpoet_form id="2"]
+      ',
+      'post_status' => 'publish',
+    ]);
+  }
+
   public function updateNewForm(\AcceptanceTester $i) {
+    $i->wantTo('Create and update form');
+
+    $newConfMessage = 'Hey, this is the updated conf message.';
+    $subscriberEmail = 'test-form@example.com';
     $segmentFactory = new Segment();
     $segmentName = 'Fancy List';
     $segmentFactory->withName($segmentName)->create();
-    $i->wantTo('Create and update form');
+
     $i->login();
     $i->amOnMailPoetPage('Forms');
     // Create a new form
     $formName = 'My awesome form';
+    $updatedFormName = 'My updated awesome form';
     $i->click('[data-automation-id="create_new_form"]');
     $i->waitForElement('[data-automation-id="form_title_input"]');
     $i->fillField('[data-automation-id="form_title_input"]', $formName);
@@ -27,8 +55,27 @@ class FormEditorUpdateNewFormCest {
     // Reload page and check data were saved
     $i->reloadPage();
     $i->waitForElement('[data-automation-id="form_title_input"]');
-    $i->assertAttributeContains('[data-automation-id="form_title_input"]', 'value', $formName);
+    $i->seeInField('[data-automation-id="form_title_input"]', $formName);
     $i->seeSelectedInSelect2($segmentName);
+    $i->seeNoJSErrors();
+    // Update form name and confirmation message
+    $i->fillField('[data-automation-id="form_title_input"]', $updatedFormName);
+    $i->fillField('.components-textarea-control__input', $newConfMessage);
+    $i->saveFormInEditor();
+    // Reload page and check data were saved
+    $i->reloadPage();
+    $i->waitForElement('[data-automation-id="form_title_input"]');
+    $i->seeInField('[data-automation-id="form_title_input"]', $updatedFormName);
+    $i->seeInField('.components-textarea-control__input', $newConfMessage);
+    $i->seeNoJSErrors();
+    // Verify new form name in Forms list page and also new conf message on the front end
+    $i->amOnMailpoetPage('Forms');
+    $i->waitForText('Forms');
+    $i->waitForText($updatedFormName);
+    $i->amOnPage('/form-test');
+    $i->fillField('[data-automation-id="form_email"]', $subscriberEmail);
+    $i->click('.mailpoet_submit');
+    $i->waitForText($newConfMessage, self::CONFIRMATION_MESSAGE_TIMEOUT, '.mailpoet_validate_success');
     $i->seeNoJSErrors();
   }
 }
