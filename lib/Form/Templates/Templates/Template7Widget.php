8e83eb3493698082b88b2dc43adadaed20cc8653
Rostislav Wolny <rosta@mailpoet.com>
2020-09-03 17:33:12 +0200

    Add form templates based on template 7
    [MAILPOET-3076]

---
+++ lib/Form/Templates/Templates/Template7Widget.php
@@ -0,0 +1,254 @@
+<?php
+
+namespace MailPoet\Form\Templates\Templates;
+
+use MailPoet\Form\Templates\FormTemplate;
+
+class Template7Widget extends FormTemplate {
+  const ID = 'template-7-widget';
+
+  public function getName(): string {
+    return 'Template 7 Widget';
+  }
+
+  public function getBody(): array {
+    return [
+      [
+        'type' => 'divider',
+        'params' => [
+          'class_name' => '',
+          'height' => '10',
+          'type' => 'spacer',
+          'style' => 'solid',
+          'divider_height' => '1',
+          'divider_width' => '100',
+          'color' => 'black',
+        ],
+        'id' => 'divider',
+        'name' => 'Divider',
+      ],
+      [
+        'type' => 'heading',
+        'id' => 'heading',
+        'params' => [
+          'content' => _x('<img draggable="false" role="img" class="emoji" alt="🕶️" src="https://s.w.org/images/core/emoji/13.0.0/svg/1f576.svg"> <strong><span style="font-family: Abril FatFace" data-font="Abril FatFace" class="mailpoet-has-font">Relax!</span></strong>', 'Text in a web form. Keep HTML tags!', 'mailpoet'),
+          'level' => '2',
+          'align' => 'center',
+          'font_size' => '40',
+          'text_color' => '#ffffff',
+          'line_height' => '1.2',
+          'background_color' => '',
+          'anchor' => '',
+          'class_name' => '',
+        ],
+      ],
+      [
+        'type' => 'heading',
+        'id' => 'heading',
+        'params' => [
+          'content' => _x('<span style="font-family: Cairo" data-font="Cairo" class="mailpoet-has-font">Put your feet up and let us do the hard work for you. Sign up to receive our latest deals directly in your inbox.</span>', 'Text in a web form. Keep HTML tags!', 'mailpoet'),
+          'level' => '2',
+          'align' => 'center',
+          'font_size' => '16',
+          'text_color' => '#ffffff',
+          'line_height' => '1.2',
+          'background_color' => '',
+          'anchor' => '',
+          'class_name' => '',
+        ],
+      ],
+      [
+        'type' => 'text',
+        'params' => [
+          'label' => _x('Enter your email', 'Form label', 'mailpoet'),
+          'class_name' => '',
+          'required' => '1',
+          'label_within' => '1',
+        ],
+        'id' => 'email',
+        'name' => 'Email',
+        'styles' => [
+          'full_width' => '1',
+          'bold' => '0',
+          'background_color' => '#ffffff',
+          'font_color' => '#abb8c3',
+          'border_size' => '0',
+          'border_radius' => '6',
+        ],
+      ],
+      [
+        'type' => 'submit',
+        'params' => [
+          'label' => _x('Get the latest deals', 'Form label', 'mailpoet'),
+          'class_name' => '',
+        ],
+        'id' => 'submit',
+        'name' => 'Submit',
+        'styles' => [
+          'full_width' => '1',
+          'bold' => '1',
+          'gradient' => 'linear-gradient(180deg,rgb(0,159,251) 0%,rgb(29,123,164) 100%)',
+          'font_size' => '16',
+          'font_color' => '#ffffff',
+          'border_size' => '1',
+          'border_radius' => '6',
+          'padding' => '11',
+          'font_family' => 'Cairo',
+        ],
+      ],
+      [
+        'type' => 'paragraph',
+        'id' => 'paragraph',
+        'params' => [
+          'content' => _x('<span style="font-family: Cairo" data-font="Cairo" class="mailpoet-has-font">We\'ll never send you spam or share your email address.<br>Find out more in our <a rel="noreferrer noopener" href="https://www.mailpoet.com/privacy-notice/" target="_blank">Privacy Policy</a>.</span>', 'Text in a web form. Keep HTML tags!', 'mailpoet'),
+          'drop_cap' => '0',
+          'align' => 'center',
+          'font_size' => '13',
+          'line_height' => '1.5',
+          'text_color' => '#ffffff',
+          'background_color' => '',
+          'class_name' => '',
+        ],
+      ],
+      [
+        'type' => 'divider',
+        'params' => [
+          'class_name' => '',
+          'height' => '10',
+          'type' => 'spacer',
+          'style' => 'solid',
+          'divider_height' => '1',
+          'divider_width' => '100',
+          'color' => 'black',
+        ],
+        'id' => 'divider',
+        'name' => 'Divider',
+      ],
+    ];
+  }
+
+  public function getSettings(): array {
+    return [
+      'success_message' => '',
+      'segments' => [],
+      'alignment' => 'left',
+      'fontSize' => '14',
+      'form_placement' => [
+        'popup' => ['enabled' => ''],
+        'below_posts' => ['enabled' => ''],
+        'fixed_bar' => ['enabled' => ''],
+        'slide_in' => ['enabled' => ''],
+        'others' => [
+          'styles' => [
+            'width' => [
+              'unit' => 'percent',
+              'value' => '100',
+            ],
+          ],
+        ],
+      ],
+      'border_radius' => '24',
+      'border_size' => '0',
+      'form_padding' => '20',
+      'input_padding' => '12',
+      'font_family' => 'Cairo',
+      'background_image_url' => '',
+      'background_image_display' => 'scale',
+      'close_button' => 'round_white',
+      'segments_selected_by' => 'admin',
+      'gradient' => 'linear-gradient(180deg,rgb(255,233,112) 0%,rgb(230,174,70) 49%,rgb(228,37,111) 100%)',
+      'form_placement_bellow_posts_enabled' => '',
+      'form_placement_popup_enabled' => '',
+      'form_placement_fixed_bar_enabled' => '',
+      'form_placement_slide_in_enabled' => '',
+    ];
+  }
+
+  public function getStyles(): string {
+    return <<<EOL
+/* form */
+.mailpoet_form {
+}
+
+form {
+  margin-bottom: 0;
+}
+
+p.mailpoet_form_paragraph.last {
+    margin-bottom: 0px;
+}
+
+h2.mailpoet-heading {
+    margin: -10px 0 10px 0;
+}
+
+/* columns */
+.mailpoet_column_with_background {
+  padding: 10px;
+}
+/* space between columns */
+.mailpoet_form_column:not(:first-child) {
+  margin-left: 20px;
+}
+
+/* input wrapper (label + input) */
+.mailpoet_paragraph {
+  line-height:20px;
+  margin-bottom: 20px;
+}
+
+/* labels */
+.mailpoet_segment_label,
+.mailpoet_text_label,
+.mailpoet_textarea_label,
+.mailpoet_select_label,
+.mailpoet_radio_label,
+.mailpoet_checkbox_label,
+.mailpoet_list_label,
+.mailpoet_date_label {
+  display:block;
+  font-weight: normal;
+}
+
+/* inputs */
+.mailpoet_text,
+.mailpoet_textarea,
+.mailpoet_select,
+.mailpoet_date_month,
+.mailpoet_date_day,
+.mailpoet_date_year,
+.mailpoet_date {
+  display:block;
+}
+
+.mailpoet_text,
+.mailpoet_textarea {
+  width: 200px;
+}
+
+.mailpoet_checkbox {
+}
+
+.mailpoet_submit {
+}
+
+.mailpoet_divider {
+}
+
+.mailpoet_message {
+}
+
+.mailpoet_form_loading {
+  width: 30px;
+  text-align: center;
+  line-height: normal;
+}
+
+.mailpoet_form_loading > span {
+  width: 5px;
+  height: 5px;
+  background-color: #5b5b5b;
+}
+EOL;
+  }
+}
