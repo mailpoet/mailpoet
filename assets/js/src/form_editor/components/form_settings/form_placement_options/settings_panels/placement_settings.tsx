0cafa1c726615fdc9c12df3e3dd0aa4fcb371fdd
Pavel Dohnal <pavel@mailpoet.com>
2020-09-07 17:01:13 +0200

    Display select field for selecting pages
    [MAILPOET-3120]

--- assets/js/src/form_editor/components/form_settings/form_placement_options/settings_panels/placement_settings.tsx
+++ assets/js/src/form_editor/components/form_settings/form_placement_options/settings_panels/placement_settings.tsx
@@ -3,6 +3,7 @@ import MailPoet from 'mailpoet';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { ToggleControl } from '@wordpress/components';
 import { assocPath, compose, __ } from 'lodash/fp';
+import Selection from 'form/fields/selection.jsx';
 
 type Props = {
   settingsPlacementKey: string
@@ -13,6 +14,10 @@ const PlacementSettings = ({ settingsPlacementKey }: Props) => {
     (select) => select('mailpoet-form-editor').getFormSettings(),
     []
   );
+  const pages = useSelect(
+    (select) => select('mailpoet-form-editor').getAllWPPages(),
+    []
+  );
   const { changeFormSettings } = useDispatch('mailpoet-form-editor');
 
   return (
@@ -21,6 +26,28 @@ const PlacementSettings = ({ settingsPlacementKey }: Props) => {
         label={MailPoet.I18n.t('placeFormOnAllPages')}
         checked={formSettings.formPlacement[settingsPlacementKey].pages.all}
         onChange={compose([changeFormSettings, assocPath(`formPlacement.${settingsPlacementKey}.pages.all`, __, formSettings)])}
+      />
+      <Selection
+        item={{
+          pages: formSettings.formPlacement[settingsPlacementKey].pages.selected,
+        }}
+        onValueChange={(e) => {
+          changeFormSettings(
+            assocPath(
+              `formPlacement.${settingsPlacementKey}.pages.selected`,
+              e.target.value,
+              formSettings
+            )
+          );
+        }}
+        field={{
+          id: 'pages',
+          name: 'pages',
+          values: pages,
+          multiple: true,
+          placeholder: MailPoet.I18n.t('selectPage'),
+          getLabel: (seg) => seg.name,
+        }}
       />
       <ToggleControl
         label={MailPoet.I18n.t('placeFormOnAllPosts')}
