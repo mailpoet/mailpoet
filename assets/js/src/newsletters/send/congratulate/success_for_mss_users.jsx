4fb272f9486665cf1cd06779068335ccba9a1c83
Amine Ben hammou <amine@mailpoet.com>
2020-09-09 22:27:32 +0200

    Redesign congratulation page

--- assets/js/src/newsletters/send/congratulate/success_for_mss_users.jsx
+++ assets/js/src/newsletters/send/congratulate/success_for_mss_users.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MailPoet from 'mailpoet';
+import Heading from 'common/typography/heading/heading';
+import { Button } from 'common';
 
-function renderHeader(newsletter) {
+function getSuccessMessage(newsletter) {
   if (newsletter.type === 'welcome') {
-    return MailPoet.I18n.t('congratulationsWelcomeEmailSuccessHeader');
+    return MailPoet.I18n.t('congratulationsWelcomeEmailSuccessBody');
   }
   if (newsletter.type === 'notification') {
-    return MailPoet.I18n.t('congratulationsPostNotificationSuccessHeader');
+    return MailPoet.I18n.t('congratulationsPostNotificationSuccessBody');
   }
   if (newsletter.type === 'automatic') {
-    return MailPoet.I18n.t('congratulationsWooSuccessHeader');
+    return MailPoet.I18n.t('congratulationsWooSuccessBody');
   }
   if (newsletter.status === 'scheduled') {
-    return MailPoet.I18n.t('congratulationsScheduleSuccessHeader');
+    return MailPoet.I18n.t('congratulationsScheduleSuccessBody');
   }
-  return MailPoet.I18n.t('congratulationsSendSuccessHeader');
+  return MailPoet.I18n.t('congratulationsSendSuccessBody');
 }
 
 function MSSUserSuccess(props) {
   return (
     <div className="mailpoet_congratulate_success">
-      <h1>{renderHeader(props.newsletter)}</h1>
-      <img src={props.illustrationImageUrl} alt="" width="750" height="250" />
-      <button type="button" className="button" onClick={props.successClicked}>{MailPoet.I18n.t('close')}</button>
+      <Heading level={0}>{MailPoet.I18n.t('congratulationsSuccessHeader')}</Heading>
+      <h1>{getSuccessMessage(props.newsletter)}</h1>
+      <img src={props.illustrationImageUrl} alt="" width="500" />
+      <Button type="button" dimension="small" onClick={props.successClicked}>{MailPoet.I18n.t('close')}</Button>
     </div>
   );
 }
