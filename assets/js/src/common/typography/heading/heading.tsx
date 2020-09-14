dbac73fec5ec060ac28b91a91f7e848105a81451
Ján Mikláš <neosinner@gmail.com>
2020-09-14 16:43:59 +0200

    Alllow HTML attributes in <Heading> component
    [MAILPOET-2788]

--- assets/js/src/common/typography/heading/heading.tsx
+++ assets/js/src/common/typography/heading/heading.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 
-type Props = {
+type Props = HTMLAttributes<HTMLHeadingElement> & {
   children: React.ReactNode,
   level: 0 | 1 | 2 | 3 | 4 | 5,
 };
@@ -8,11 +8,12 @@ type Props = {
 const Heading = ({
   children,
   level,
+  ...attributes
 }: Props) => {
   // This is written like this because of linter errors on React's IntrinsicAttributes
   const Element = level === 5 ? 'h5' : level === 4 ? 'h4' : level === 3 ? 'h3' : level === 2 ? 'h2' : 'h1'; // eslint-disable-line no-nested-ternary
   return (
-    <Element className={`mailpoet-h${level}`}>
+    <Element className={`mailpoet-h${level}`} {...attributes}>
       {children}
     </Element>
   );
