8cf33f19f7cfaec8591cf61fbec5d6ccef9c6a6a
Rostislav Wolny <rosta@mailpoet.com>
2020-09-16 16:55:48 +0200

    Implement email actions filter in Doctrine
    [MAILPOET-3077]

--- lib/DI/ContainerConfigurator.php
+++ lib/DI/ContainerConfigurator.php
@@ -257,6 +257,7 @@ class ContainerConfigurator implements IContainerConfigurator {
     $container->autowire(\MailPoet\Segments\SubscribersFinder::class);
     $container->autowire(\MailPoet\Segments\SegmentsRepository::class);
     $container->autowire(\MailPoet\Segments\DynamicSegments\Filters\UserRole::class)->setPublic(true);
+    $container->autowire(\MailPoet\Segments\DynamicSegments\Filters\EmailAction::class)->setPublic(true);
     // Services
     $container->autowire(\MailPoet\Services\Bridge::class)->setPublic(true);
     $container->autowire(\MailPoet\Services\AuthorizedEmailsController::class);
--- lib/Segments/DynamicSegments/Filters/EmailAction.php
+++ lib/Segments/DynamicSegments/Filters/EmailAction.php
@@ -2,7 +2,13 @@
 
 namespace MailPoet\Segments\DynamicSegments\Filters;
 
+use MailPoet\Entities\DynamicSegmentFilterEntity;
+use MailPoet\Entities\StatisticsClickEntity;
+use MailPoet\Entities\StatisticsNewsletterEntity;
+use MailPoet\Entities\StatisticsOpenEntity;
+use MailPoet\Entities\SubscriberEntity;
 use MailPoetVendor\Doctrine\DBAL\Query\QueryBuilder;
+use MailPoetVendor\Doctrine\ORM\EntityManager;
 
 class EmailAction implements Filter {
   const ACTION_OPENED = 'opened';
@@ -10,22 +16,67 @@ class EmailAction implements Filter {
   const ACTION_CLICKED = 'clicked';
   const ACTION_NOT_CLICKED = 'notClicked';
 
-  /** @var string */
-  private $action;
+  /** @var EntityManager */
+  private $entityManager;
 
-  /** @var int */
-  private $newsletterId;
-
-  /** @var int|null */
-  private $linkId;
-
-  public function __construct(string $action, int $newsletterId, int $linkId = null) {
-    $this->action = $action;
-    $this->newsletterId = $newsletterId;
-    $this->linkId = $linkId;
+  public function __construct(EntityManager $entityManager) {
+    $this->entityManager = $entityManager;
   }
 
-  public function apply(QueryBuilder $queryBuilder): QueryBuilder {
+  public function apply(QueryBuilder $queryBuilder, DynamicSegmentFilterEntity $filterEntity): QueryBuilder {
+    $action = $filterEntity->getFilterDataParam('action');
+    $newsletterId = (int)$filterEntity->getFilterDataParam('newsletter_id');
+    $linkId = $filterEntity->getFilterDataParam('link_id') ? (int)$filterEntity->getFilterDataParam('link_id') : null;
+
+    $statsSentTable = $this->entityManager->getClassMetadata(StatisticsNewsletterEntity::class)->getTableName();
+    $subscribersTable = $this->entityManager->getClassMetadata(SubscriberEntity::class)->getTableName();
+    if (($action === self::ACTION_CLICKED) || ($action === self::ACTION_NOT_CLICKED)) {
+      $statsTable = $this->entityManager->getClassMetadata(StatisticsClickEntity::class)->getTableName();
+    } else {
+      $statsTable = $this->entityManager->getClassMetadata(StatisticsOpenEntity::class)->getTableName();
+    }
+
+    $where = '1';
+
+    if (($action === self::ACTION_NOT_CLICKED) || ($action === self::ACTION_NOT_OPENED)) {
+      $queryBuilder = $queryBuilder->innerJoin(
+        $subscribersTable,
+        $statsSentTable,
+        'statssent',
+        "$subscribersTable.id = statssent.subscriber_id AND statssent.newsletter_id = :newsletter"
+      )->leftJoin(
+        'statssent',
+        $statsTable,
+        'stats',
+        $this->createNotStatsJoinCondition($action, $linkId)
+      );
+      $where .= ' AND stats.id IS NULL';
+    } else {
+      $queryBuilder = $queryBuilder->innerJoin(
+        $subscribersTable,
+        $statsTable,
+        'stats',
+        "stats.subscriber_id = $subscribersTable.id AND stats.newsletter_id = :newsletter"
+      );
+    }
+    if ($action === EmailAction::ACTION_CLICKED && $linkId) {
+      $where .= ' AND stats.link_id = :link';
+    }
+    $queryBuilder = $queryBuilder
+      ->andWhere($where)
+      ->setParameter('newsletter', $newsletterId);
+    if (in_array($action, [EmailAction::ACTION_CLICKED, EmailAction::ACTION_NOT_CLICKED]) && $linkId) {
+      $queryBuilder = $queryBuilder
+        ->setParameter('link', $linkId);
+    }
     return $queryBuilder;
+  }
+
+  private function createNotStatsJoinCondition(string $action, int $linkId = null): string {
+    $clause = "statssent.subscriber_id = stats.subscriber_id AND stats.newsletter_id = :newsletter";
+    if ($action === EmailAction::ACTION_NOT_CLICKED && $linkId) {
+      $clause .= ' AND stats.link_id = :link';
+    }
+    return $clause;
   }
 }
