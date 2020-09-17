a1b36772234cb1cc772e176d52437a6c9fd37bc6
Rostislav Wolny <rosta@mailpoet.com>
2020-09-17 15:37:21 +0200

    Implement WooCommerce category purchase segment filter in Doctrine
    [MAILPOET-3077]

--- lib/DI/ContainerConfigurator.php
+++ lib/DI/ContainerConfigurator.php
@@ -259,6 +259,7 @@ class ContainerConfigurator implements IContainerConfigurator {
     $container->autowire(\MailPoet\Segments\DynamicSegments\Filters\UserRole::class)->setPublic(true);
     $container->autowire(\MailPoet\Segments\DynamicSegments\Filters\EmailAction::class)->setPublic(true);
     $container->autowire(\MailPoet\Segments\DynamicSegments\Filters\WooCommerceProduct::class)->setPublic(true);
+    $container->autowire(\MailPoet\Segments\DynamicSegments\Filters\WooCommerceCategory::class)->setPublic(true);
     // Services
     $container->autowire(\MailPoet\Services\Bridge::class)->setPublic(true);
     $container->autowire(\MailPoet\Services\AuthorizedEmailsController::class);
