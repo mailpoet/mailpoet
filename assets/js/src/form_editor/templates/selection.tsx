527ebbd922103cbdf7ea6160f5390fece938c27c
Rostislav Wolny <rosta@mailpoet.com>
2020-09-28 17:13:56 +0200

    Render form teplates selection categories
    [MAILPOET-2988]

--- assets/js/src/form_editor/templates/selection.tsx
+++ assets/js/src/form_editor/templates/selection.tsx
@@ -1,47 +1,72 @@
-import React from 'react';
-import Heading from 'common/typography/heading/heading';
+import React, { useState } from 'react';
 import MailPoet from 'mailpoet';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { Button } from '@wordpress/components';
-
-import { TemplateType } from './store/types';
+import Categories from 'common/categories/categories';
+import Background from 'common/background/background';
+import { TemplateData } from './store/types';
 
 export default () => {
-  const templates: Array<TemplateType> = useSelect(
+  const [selectedCategory, setSelectedCategory] = useState('popup');
+
+  const categories = [
+    {
+      name: 'popup',
+      label: MailPoet.I18n.t('popupCategory'),
+    },
+    {
+      name: 'fixed_bar',
+      label: MailPoet.I18n.t('fixedBarCategory'),
+    },
+    {
+      name: 'below_posts',
+      label: MailPoet.I18n.t('belowPagesCategory'),
+    },
+    {
+      name: 'slide_in',
+      label: MailPoet.I18n.t('slideInCategory'),
+    },
+    {
+      name: 'others',
+      label: MailPoet.I18n.t('othersCategory'),
+    },
+  ];
+
+  const templates: TemplateData = useSelect(
     (select) => select('mailpoet-form-editor-templates').getTemplates(),
     []
   );
-  const selectTemplateFailed: boolean = useSelect(
-    (select) => select('mailpoet-form-editor-templates').getSelectTemplateFailed(),
-    []
-  );
+
+  // const selectTemplateFailed: boolean = useSelect(
+  //   (select) => select('mailpoet-form-editor-templates').getSelectTemplateFailed(),
+  //   []
+  // );
+
   const { selectTemplate } = useDispatch('mailpoet-form-editor-templates');
   return (
     <div className="template-selection" data-automation-id="template_selection_list">
-      <Heading level={1}>{MailPoet.I18n.t('heading')}</Heading>
-      {selectTemplateFailed && (<div className="mailpoet_error">Failed</div>)}
-      <ol>
-        <li>
-          <Button
-            isLink
-            data-automation-id="blank_template"
-            onClick={() => selectTemplate()}
-          >
-            {MailPoet.I18n.t('blankTemplate')}
-          </Button>
-        </li>
-        {templates.map((template, index) => (
-          <li key={template.id}>
-            <Button
-              isLink
-              onClick={() => selectTemplate(template.id)}
-              data-automation-id={`template_index_${index}`}
-            >
-              {template.name}
-            </Button>
-          </li>
-        ))}
-      </ol>
+      <Background color="#fff" />
+      <div className="mailpoet-templates">
+        <Categories
+          categories={categories}
+          active={selectedCategory}
+          onSelect={setSelectedCategory}
+        />
+        <ol>
+          {templates[selectedCategory].map((template, index) => (
+            <li key={template.id}>
+              <Button
+                isLink
+                onClick={() => selectTemplate(template.id)}
+                data-automation-id={`template_index_${index}`}
+              >
+                {template.name}
+              </Button>
+            </li>
+          ))}
+        </ol>
+      </div>
+
     </div>
   );
 };
