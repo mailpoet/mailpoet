714bc641b4c8a494311a2dcae0d9f560de73c71d
Ján Mikláš <neosinner@gmail.com>
2020-09-29 14:35:11 +0200

    Allow rendering array of strings in <Tags>
    [MAILPOET-2782]

--- assets/js/src/common/tag/tags.tsx
+++ assets/js/src/common/tag/tags.tsx
@@ -4,13 +4,20 @@ import Tag from './tag';
 type Props = {
   children?: React.ReactNode,
   dimension?: 'large',
-  segments: any[]
+  segments?: any[],
+  strings?: string[],
 }
 
-const Tags = ({ children, dimension, segments }: Props) => (
+const Tags = ({
+  children,
+  dimension,
+  segments,
+  strings,
+}: Props) => (
   <div className="mailpoet-tags">
     {children}
-    {segments.map((segment) => <Tag key={segment.name} dimension={dimension} variant="list">{segment.name}</Tag>)}
+    {segments && segments.map((segment) => <Tag key={segment.name} dimension={dimension} variant="list">{segment.name}</Tag>)}
+    {strings && strings.map((string) => <Tag key={string} dimension={dimension} variant="list">{string}</Tag>)}
   </div>
 );
 
