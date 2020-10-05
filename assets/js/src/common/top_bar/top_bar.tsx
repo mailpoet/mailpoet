45aa778c3ff0d92dfde52aaaaf71095f05176964
Pavel Dohnal <pavel@mailpoet.com>
2020-10-05 15:01:59 +0200

    Display beamer dot
    [MAILPOET-3102]

--- assets/js/src/common/top_bar/top_bar.tsx
+++ assets/js/src/common/top_bar/top_bar.tsx
@@ -1,29 +1,45 @@
 import React from 'react';
+import classNames from 'classnames';
+import { withFeatureAnnouncement } from 'announcements/with_feature_announcement';
 import { MailPoetLogo } from './mailpoet_logo';
 import { BeamerIcon } from './beamer_icon';
+import MailPoet from '../../mailpoet';
 
 type Props = {
   children?: React.ReactNode
   hasNews: boolean
-  onBeamerClick: () => {}
+  onBeamerClick: () => void
 };
 
 export const TopBar = ({
   children,
   hasNews,
   onBeamerClick,
-}: Props) => (
-  <div className="top-bar">
-    <div className="top-bar-logo">
-      <MailPoetLogo />
-    </div>
-    <div className="top-bar-content">
-      <div className="top-bar-children">
-        {children}
+}: Props) => {
+  const buttonClasses = classNames(
+    'button top-bar-beamer',
+    hasNews ? 'top-bar-beamer-dot' : ''
+  );
+  return (
+    <div className="top-bar">
+      <div className="top-bar-logo">
+        <MailPoetLogo />
       </div>
-      <div className="top-bar-beamer">
-        <BeamerIcon />
+      <div className="top-bar-content">
+        <div className="top-bar-children">
+          {children}
+        </div>
+        <button
+          type="button"
+          onClick={onBeamerClick}
+          className={buttonClasses}
+          title={MailPoet.I18n.t('whatsNew')}
+        >
+          <BeamerIcon />
+        </button>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+export const TopBarWithBeamer = withFeatureAnnouncement(TopBar);
