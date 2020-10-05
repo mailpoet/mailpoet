c0fa7c8283d65e704b6b7db42fc77d3e36469b60
Pavel Dohnal <pavel@mailpoet.com>
2020-10-05 11:28:23 +0200

    Add children to topbar
    [MAILPOET-3102]

--- assets/js/src/common/top_bar/top_bar.tsx
+++ assets/js/src/common/top_bar/top_bar.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { MailPoetLogo } from './mailpoet_logo';
 import { BeamerIcon } from './beamer_icon';
 
-export const TopBar = () => (
+type Props = {
+  children?: React.ReactNode,
+};
+
+export const TopBar = ({
+  children,
+}: Props) => (
   <div className="top-bar">
     <div className="top-bar-logo">
       <MailPoetLogo />
     </div>
-    <div>
+    <div className="top-bar-content">
+      <div className="top-bar-children">
+        {children}
+      </div>
       <div className="top-bar-beamer">
         <BeamerIcon />
       </div>
