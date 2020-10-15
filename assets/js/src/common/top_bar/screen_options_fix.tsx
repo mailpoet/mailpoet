d653619282638c5eb82d3121fe0787710b453795
Ján Mikláš <neosinner@gmail.com>
2020-10-15 14:29:08 +0200

    Fix notices position when <TopBar> is visible
    [MAILPOET-2780]

--- assets/js/src/common/top_bar/screen_options_fix.tsx
+++ assets/js/src/common/top_bar/screen_options_fix.tsx
@@ -8,17 +8,20 @@ export const ScreenOptionsFix = () => (
     {/* eslint-disable-next-line react/no-danger */}
     <style dangerouslySetInnerHTML={
       {
-        __html: `#screen-meta-links {
-          margin-top: -50px;
+        __html: `#screen-meta-links .show-settings {
+          border-color: #e5e9f8;
+          margin-bottom: 10px;
           position: relative;
-          top: 114px;
-          z-index: 10;
+          z-index: 1;
         }`,
       }
     }
     />
 
     {/* eslint-disable-next-line react/no-danger */}
-    <style dangerouslySetInnerHTML={{ __html: '#screen-meta-links .show-settings { border-color: #e5e9f8; }' }} />
+    <style dangerouslySetInnerHTML={{ __html: '#wpbody-content { padding-top: 64px; }' }} />
+
+    {/* eslint-disable-next-line react/no-danger */}
+    <style dangerouslySetInnerHTML={{ __html: '.wrap { margin-top: 20px; }' }} />
   </>
 );
