93dae81a5e5a92ce433c6e18f2ecc8e2f3cf3354
Rostislav Wolny <rosta@mailpoet.com>
2020-10-20 16:54:13 +0200

    Remove unnecessary class alias that breaks PHPStan
    [MAILPOET-3232]

--- prefixer/fix-carbon.php
+++ prefixer/fix-carbon.php
@@ -18,6 +18,17 @@ $replacements = [
       'func_num_args() === 0 ? !$this->invert : ($inverted ? 1 : 0)',
     ],
   ],
+  // Remove unnecessary class alias (fix for Symfony) that breaks PHPStan
+  [
+    'file' => '../vendor-prefixed/nesbot/carbon/src/Carbon/Traits/Localization.php',
+    'find' => [
+      "/if \\(!\\\\interface_exists\\('MailPoetVendor\\\\\\\\Symfony\\\\\\\\Component\\\\\\\\Translation\\\\\\\\TranslatorInterface'\\)\\) {[a-z_(',);\\s\\\\]+}/i",
+    ],
+    'replace' => [
+      '',
+    ],
+    'regular' => true,
+  ],
 ];
 
 // Apply replacements
