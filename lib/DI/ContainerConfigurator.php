32729e082a299f38234335e148b758a388f105f1
Jan Lysý <lysyjan87@gmail.com>
2020-10-21 19:19:12 +0200

    Register Security like a service [MAILPOET-2946]

--- lib/DI/ContainerConfigurator.php
+++ lib/DI/ContainerConfigurator.php
@@ -320,6 +320,7 @@ class ContainerConfigurator implements IContainerConfigurator {
     $container->autowire(\MailPoet\Util\Cookies::class);
     $container->autowire(\MailPoet\Util\Url::class)->setPublic(true);
     $container->autowire(\MailPoet\Util\Installation::class);
+    $container->autowire(\MailPoet\Util\Security::class);
     $container->autowire(\MailPoet\Util\License\Features\Subscribers::class)->setPublic(true);
     $container->autowire(\MailPoet\Util\License\License::class)->setPublic(true);
     $container->register(\MailPoet\Util\CdnAssetUrl::class)
