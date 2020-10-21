bb00f8a6455047e8c41903fca7064d123d9a255b
Ján Mikláš <neosinner@gmail.com>
2020-10-21 07:15:13 +0200

    Remove Typekit, use bundled Nunito font instead
    [MAILPOET-3073]

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { withPerformance } from 'storybook-addon-performance';
+import '../assets/dist/css/mailpoet-font.css';
 import '../assets/dist/css/mailpoet-plugin.css';
 import '../assets/dist/css/mailpoet-form-editor.css';
 
