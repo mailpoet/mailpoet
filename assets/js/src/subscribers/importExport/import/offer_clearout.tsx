46f0b97d06df27c2cc55474d8d47f6a45403fa44
Ján Mikláš <neosinner@gmail.com>
2020-10-22 17:29:57 +0200

    Redesign import clearout step
    [MAILPOET-3073]

--- assets/js/src/subscribers/importExport/import/offer_clearout.tsx
+++ assets/js/src/subscribers/importExport/import/offer_clearout.tsx
@@ -12,20 +12,11 @@ export default ({ onProceed }: Props) => (
     <p>{MailPoet.I18n.t('offerClearoutText2')}</p>
     <p>{MailPoet.I18n.t('offerClearoutText3')}</p>
     <p>
-      <Button
-        dimension="small"
-        variant="dark"
-        target="_blank"
-        href="https://clearout.io/?ref=mailpoet"
-      >
+      <Button target="_blank" href="https://clearout.io/?ref=mailpoet">
         {MailPoet.I18n.t('tryClearout')}
       </Button>
       {onProceed && (
-        <Button
-          onClick={onProceed}
-          variant="link-dark"
-          dimension="small"
-        >
+        <Button onClick={onProceed} variant="link">
           {MailPoet.I18n.t('clearoutGotIt')}
         </Button>
       )}
