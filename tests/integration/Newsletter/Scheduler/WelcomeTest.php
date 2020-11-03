352ff15c25950ab538de6d8c1f4c6513ec551b57
Rostislav Wolny <rosta@mailpoet.com>
2020-11-03 16:32:22 +0100

    Refactor welcome scheduler test to doctrine
    [MAILPOET-3141]

--- tests/integration/Newsletter/Scheduler/WelcomeTest.php
+++ tests/integration/Newsletter/Scheduler/WelcomeTest.php
@@ -5,21 +5,14 @@ namespace MailPoet\Newsletter\Scheduler;
 use MailPoet\Entities\NewsletterEntity;
 use MailPoet\Entities\NewsletterOptionEntity;
 use MailPoet\Entities\NewsletterOptionFieldEntity;
+use MailPoet\Entities\ScheduledTaskEntity;
+use MailPoet\Entities\ScheduledTaskSubscriberEntity;
 use MailPoet\Entities\SegmentEntity;
+use MailPoet\Entities\SendingQueueEntity;
 use MailPoet\Entities\SubscriberEntity;
-use MailPoet\Models\Newsletter;
-use MailPoet\Models\NewsletterOption;
-use MailPoet\Models\NewsletterOptionField;
-use MailPoet\Models\NewsletterPost;
-use MailPoet\Models\ScheduledTask;
-use MailPoet\Models\ScheduledTaskSubscriber;
-use MailPoet\Models\Segment;
-use MailPoet\Models\SendingQueue;
-use MailPoet\Models\Subscriber;
 use MailPoet\Tasks\Sending as SendingTask;
 use MailPoet\WP\Functions as WPFunctions;
 use MailPoetVendor\Carbon\Carbon;
-use MailPoetVendor\Idiorm\ORM;
 
 class WelcomeTest extends \MailPoetTest {
 
@@ -63,12 +56,14 @@ class WelcomeTest extends \MailPoetTest {
 
     // queue is not scheduled
     $this->welcomeScheduler->createWelcomeNotificationSendingTask($newsletter, $existingSubscriber);
-    expect(SendingQueue::findMany())->count(1);
+    $queues = $this->entityManager->getRepository(SendingQueueEntity::class)->findAll();
+    expect($queues)->count(1);
 
     // queue is scheduled
     $unscheduledSubscriber = $this->createSubscriber('welcome_test_2@example.com');
     $this->welcomeScheduler->createWelcomeNotificationSendingTask($newsletter, $unscheduledSubscriber->getId());
-    expect(SendingQueue::findMany())->count(2);
+    $queues = $this->entityManager->getRepository(SendingQueueEntity::class)->findAll();
+    expect($queues)->count(2);
   }
 
   public function testItCreatesWelcomeNotificationSendingTaskScheduledToSendInHours() {
@@ -81,13 +76,17 @@ class WelcomeTest extends \MailPoetTest {
     ]);
 
     $this->welcomeScheduler->createWelcomeNotificationSendingTask($newsletter, $this->subscriber->getId());
-    $queue = SendingQueue::findTaskByNewsletterId(1)
-      ->findOne();
+    $this->entityManager->refresh($newsletter);
+    $queue = $newsletter->getLatestQueue();
     $currentTime = Carbon::createFromTimestamp(WPFunctions::get()->currentTime('timestamp'));
     Carbon::setTestNow($currentTime); // mock carbon to return current time
-    expect($queue->id)->greaterOrEquals(1);
-    expect($queue->priority)->equals(SendingQueue::PRIORITY_HIGH);
-    expect(Carbon::parse($queue->scheduledAt)->format('Y-m-d H:i'))
+    expect($queue->getId())->greaterOrEquals(1);
+    $task = $queue->getTask();
+    assert($task instanceof ScheduledTaskEntity);
+    expect($task->getPriority())->equals(ScheduledTaskEntity::PRIORITY_HIGH);
+    $scheduledAt = $task->getScheduledAt();
+    assert($scheduledAt instanceof \DateTimeInterface);
+    expect($scheduledAt->format('Y-m-d H:i'))
       ->equals($currentTime->addHours(2)->format('Y-m-d H:i'));
   }
 
@@ -103,11 +102,15 @@ class WelcomeTest extends \MailPoetTest {
     $this->welcomeScheduler->createWelcomeNotificationSendingTask($newsletter, $this->subscriber->getId());
     $currentTime = Carbon::createFromTimestamp(WPFunctions::get()->currentTime('timestamp'));
     Carbon::setTestNow($currentTime); // mock carbon to return current time
-    $queue = SendingQueue::findTaskByNewsletterId(1)
-      ->findOne();
-    expect($queue->id)->greaterOrEquals(1);
-    expect($queue->priority)->equals(SendingQueue::PRIORITY_HIGH);
-    expect(Carbon::parse($queue->scheduledAt)->format('Y-m-d H:i'))
+    $this->entityManager->refresh($newsletter);
+    $queue = $newsletter->getLatestQueue();
+    expect($queue->getId())->greaterOrEquals(1);
+    $task = $queue->getTask();
+    assert($task instanceof ScheduledTaskEntity);
+    expect($task->getPriority())->equals(ScheduledTaskEntity::PRIORITY_HIGH);
+    $scheduledAt = $task->getScheduledAt();
+    assert($scheduledAt instanceof \DateTimeInterface);
+    expect($scheduledAt->format('Y-m-d H:i'))
       ->equals($currentTime->addDays(2)->format('Y-m-d H:i'));
   }
 
@@ -121,13 +124,17 @@ class WelcomeTest extends \MailPoetTest {
     ]);
 
     $this->welcomeScheduler->createWelcomeNotificationSendingTask($newsletter, $this->subscriber->getId());
+    $this->entityManager->refresh($newsletter);
+    $queue = $newsletter->getLatestQueue();
     $currentTime = Carbon::createFromTimestamp(WPFunctions::get()->currentTime('timestamp'));
     Carbon::setTestNow($currentTime); // mock carbon to return current time
-    $queue = SendingQueue::findTaskByNewsletterId(1)
-      ->findOne();
-    expect($queue->id)->greaterOrEquals(1);
-    expect($queue->priority)->equals(SendingQueue::PRIORITY_HIGH);
-    expect(Carbon::parse($queue->scheduledAt)->format('Y-m-d H:i'))
+    expect($queue->getId())->greaterOrEquals(1);
+    $task = $queue->getTask();
+    assert($task instanceof ScheduledTaskEntity);
+    expect($task->getPriority())->equals(ScheduledTaskEntity::PRIORITY_HIGH);
+    $scheduledAt = $task->getScheduledAt();
+    assert($scheduledAt instanceof \DateTimeInterface);
+    expect($scheduledAt->format('Y-m-d H:i'))
       ->equals($currentTime->addWeeks(2)->format('Y-m-d H:i'));
   }
 
@@ -141,33 +148,35 @@ class WelcomeTest extends \MailPoetTest {
     ]);
 
     $this->welcomeScheduler->createWelcomeNotificationSendingTask($newsletter, $this->subscriber->getId());
+    $this->entityManager->refresh($newsletter);
+    $queue = $newsletter->getLatestQueue();
     $currentTime = Carbon::createFromTimestamp(WPFunctions::get()->currentTime('timestamp'));
     Carbon::setTestNow($currentTime); // mock carbon to return current time
-    $queue = SendingQueue::findTaskByNewsletterId(1)->findOne();
-    expect($queue->id)->greaterOrEquals(1);
-    expect($queue->priority)->equals(SendingQueue::PRIORITY_HIGH);
-    expect(Carbon::parse($queue->scheduledAt)->format('Y-m-d H:i'))
+    expect($queue->getId())->greaterOrEquals(1);
+    $task = $queue->getTask();
+    assert($task instanceof ScheduledTaskEntity);
+    expect($task->getPriority())->equals(ScheduledTaskEntity::PRIORITY_HIGH);
+    $scheduledAt = $task->getScheduledAt();
+    assert($scheduledAt instanceof \DateTimeInterface);
+    expect($scheduledAt->format('Y-m-d H:i'))
       ->equals($currentTime->format('Y-m-d H:i'));
   }
 
   public function testItDoesNotSchedulesSubscriberWelcomeNotificationWhenSubscriberIsNotInSegment() {
     // do not schedule when subscriber is not in segment
-    $newsletter = $this->_createNewsletter();
     $this->welcomeScheduler->scheduleSubscriberWelcomeNotification(
       $this->subscriber->getId(),
       $segments = []
     );
 
     // queue is not created
-    $queue = SendingQueue::findTaskByNewsletterId($newsletter->id)
-      ->findOne();
-    expect($queue)->false();
+    $queue = $this->newsletter->getLatestQueue();
+    expect($queue)->null();
   }
 
   public function testItSchedulesSubscriberWelcomeNotification() {
-    $newsletter = $this->_createNewsletter();
-    $this->_createNewsletterOptions(
-      $newsletter->id,
+    $newsletter = $this->configureNewsletterWithOptions(
+      $this->newsletter,
       [
         'event' => 'segment',
         'segment' => $this->segment->getId(),
@@ -190,17 +199,20 @@ class WelcomeTest extends \MailPoetTest {
     );
     $currentTime = Carbon::createFromTimestamp(WPFunctions::get()->currentTime('timestamp'));
     Carbon::setTestNow($currentTime); // mock carbon to return current time
-    $queue = SendingQueue::findTaskByNewsletterId($newsletter->id)
-      ->findOne();
-    expect(Carbon::parse($queue->scheduledAt)->format('Y-m-d H:i'))
+    $this->entityManager->refresh($newsletter);
+    $queue = $newsletter->getLatestQueue();
+    $task = $queue->getTask();
+    assert($task instanceof ScheduledTaskEntity);
+    $scheduledAt = $task->getScheduledAt();
+    assert($scheduledAt instanceof \DateTimeInterface);
+    expect($scheduledAt->format('Y-m-d H:i'))
       ->equals($currentTime->addDay()->format('Y-m-d H:i'));
-    expect($result[0]->id())->equals($queue->id());
+    expect($result[0]->id())->equals($queue->getId());
   }
 
   public function testItDoesNotScheduleWelcomeNotificationWhenSubscriberIsInTrash() {
-    $newsletter = $this->_createNewsletter();
-    $this->_createNewsletterOptions(
-      $newsletter->id,
+    $this->configureNewsletterWithOptions(
+      $this->newsletter,
       [
         'event' => 'segment',
         'segment' => $this->segment->getId(),
@@ -220,9 +232,8 @@ class WelcomeTest extends \MailPoetTest {
   }
 
   public function testItDoesNotScheduleWelcomeNotificationWhenSegmentIsInTrash() {
-    $newsletter = $this->_createNewsletter();
-    $this->_createNewsletterOptions(
-      $newsletter->id,
+    $this->configureNewsletterWithOptions(
+      $this->newsletter,
       [
         'event' => 'segment',
         'segment' => $this->segment->getId(),
@@ -257,9 +268,8 @@ class WelcomeTest extends \MailPoetTest {
   }
 
   public function testItDoesNotScheduleWPUserWelcomeNotificationWhenRoleHasNotChanged() {
-    $newsletter = $this->_createNewsletter();
-    $this->_createNewsletterOptions(
-      $newsletter->id,
+    $newsletter = $this->configureNewsletterWithOptions(
+      $this->newsletter,
       [
         'event' => 'user',
         'role' => 'editor',
@@ -274,15 +284,13 @@ class WelcomeTest extends \MailPoetTest {
     );
 
     // queue is not created
-    $queue = SendingQueue::findTaskByNewsletterId($newsletter->id)
-      ->findOne();
-    expect($queue)->false();
+    $queue = $newsletter->getLatestQueue();
+    expect($queue)->null();
   }
 
   public function testItDoesNotScheduleWPUserWelcomeNotificationWhenUserRoleDoesNotMatch() {
-    $newsletter = $this->_createNewsletter();
-    $this->_createNewsletterOptions(
-      $newsletter->id,
+    $newsletter = $this->configureNewsletterWithOptions(
+      $this->newsletter,
       [
         'event' => 'user',
         'role' => 'editor',
@@ -296,15 +304,13 @@ class WelcomeTest extends \MailPoetTest {
     );
 
     // queue is not created
-    $queue = SendingQueue::findTaskByNewsletterId($newsletter->id)
-      ->findOne();
-    expect($queue)->false();
+    $queue = $newsletter->getLatestQueue();
+    expect($queue)->null();
   }
 
   public function testItDoesNotSchedulesWPUserWelcomeNotificationWhenSubscriberIsInTrash() {
-    $newsletter = $this->_createNewsletter();
-    $this->_createNewsletterOptions(
-      $newsletter->id,
+    $newsletter = $this->configureNewsletterWithOptions(
+      $this->newsletter,
       [
         'event' => 'user',
         'role' => 'administrator',
@@ -319,18 +325,14 @@ class WelcomeTest extends \MailPoetTest {
       $subscriberId = $trashedSubscriber->getId(),
       $wpUser = ['roles' => ['administrator']]
     );
-    $currentTime = Carbon::createFromTimestamp(WPFunctions::get()->currentTime('timestamp'));
-    Carbon::setTestNow($currentTime); // mock carbon to return current time
     // queue is created and scheduled for delivery one day later
-    $queue = SendingQueue::findTaskByNewsletterId($newsletter->id)
-      ->findOne();
-    expect($queue)->false();
+    $queue = $newsletter->getLatestQueue();
+    expect($queue)->null();
   }
 
   public function testItDoesNotSchedulesWPUserWelcomeNotificationWhenWpSegmentIsInTrash() {
-    $newsletter = $this->_createNewsletter();
-    $this->_createNewsletterOptions(
-      $newsletter->id,
+    $newsletter = $this->configureNewsletterWithOptions(
+      $this->newsletter,
       [
         'event' => 'user',
         'role' => 'administrator',
@@ -344,18 +346,14 @@ class WelcomeTest extends \MailPoetTest {
       $subscriberId = $this->subscriber->getId(),
       $wpUser = ['roles' => ['administrator']]
     );
-    $currentTime = Carbon::createFromTimestamp(WPFunctions::get()->currentTime('timestamp'));
-    Carbon::setTestNow($currentTime); // mock carbon to return current time
     // queue is created and scheduled for delivery one day later
-    $queue = SendingQueue::findTaskByNewsletterId($newsletter->id)
-      ->findOne();
-    expect($queue)->false();
+    $queue = $newsletter->getLatestQueue();
+    expect($queue)->null();
   }
 
   public function testItSchedulesWPUserWelcomeNotificationWhenUserRolesMatches() {
-    $newsletter = $this->_createNewsletter();
-    $this->_createNewsletterOptions(
-      $newsletter->id,
+    $newsletter = $this->configureNewsletterWithOptions(
+      $this->newsletter,
       [
         'event' => 'user',
         'role' => 'administrator',
@@ -370,16 +368,19 @@ class WelcomeTest extends \MailPoetTest {
     $currentTime = Carbon::createFromTimestamp(WPFunctions::get()->currentTime('timestamp'));
     Carbon::setTestNow($currentTime); // mock carbon to return current time
     // queue is created and scheduled for delivery one day later
-    $queue = SendingQueue::findTaskByNewsletterId($newsletter->id)
-      ->findOne();
-    expect(Carbon::parse($queue->scheduledAt)->format('Y-m-d H:i'))
+    $this->entityManager->refresh($newsletter);
+    $queue = $newsletter->getLatestQueue();
+    $task = $queue->getTask();
+    assert($task instanceof ScheduledTaskEntity);
+    $scheduledAt = $task->getScheduledAt();
+    assert($scheduledAt instanceof \DateTimeInterface);
+    expect($scheduledAt->format('Y-m-d H:i'))
       ->equals($currentTime->addDay()->format('Y-m-d H:i'));
   }
 
   public function testItSchedulesWPUserWelcomeNotificationWhenUserHasAnyRole() {
-    $newsletter = $this->_createNewsletter();
-    $this->_createNewsletterOptions(
-      $newsletter->id,
+    $newsletter = $this->configureNewsletterWithOptions(
+      $this->newsletter,
       [
         'event' => 'user',
         'role' => WelcomeScheduler::WORDPRESS_ALL_ROLES,
@@ -394,41 +395,14 @@ class WelcomeTest extends \MailPoetTest {
     $currentTime = Carbon::createFromTimestamp(WPFunctions::get()->currentTime('timestamp'));
     Carbon::setTestNow($currentTime); // mock carbon to return current time
     // queue is created and scheduled for delivery one day later
-    $queue = SendingQueue::findTaskByNewsletterId($newsletter->id)
-      ->findOne();
-    expect(Carbon::parse($queue->scheduledAt)->format('Y-m-d H:i'))
+    $this->entityManager->refresh($newsletter);
+    $queue = $newsletter->getLatestQueue();
+    $task = $queue->getTask();
+    assert($task instanceof ScheduledTaskEntity);
+    $scheduledAt = $task->getScheduledAt();
+    assert($scheduledAt instanceof \DateTimeInterface);
+    expect($scheduledAt->format('Y-m-d H:i'))
       ->equals($currentTime->addDay()->format('Y-m-d H:i'));
-  }
-
-  public function _createNewsletter(
-    $status = Newsletter::STATUS_ACTIVE
-  ) {
-    $newsletter = Newsletter::create();
-    $newsletter->type = Newsletter::TYPE_WELCOME;
-    $newsletter->status = $status;
-    $newsletter->save();
-    expect($newsletter->getErrors())->false();
-    return $newsletter;
-  }
-
-  public function _createNewsletterOptions($newsletterId, $options) {
-    foreach ($options as $option => $value) {
-      $newsletterOptionField = NewsletterOptionField::where('name', $option)->findOne();
-      if (!$newsletterOptionField) {
-        $newsletterOptionField = NewsletterOptionField::create();
-        $newsletterOptionField->name = $option;
-        $newsletterOptionField->newsletterType = Newsletter::TYPE_WELCOME;
-        $newsletterOptionField->save();
-        expect($newsletterOptionField->getErrors())->false();
-      }
-
-      $newsletterOption = NewsletterOption::create();
-      $newsletterOption->optionFieldId = (int)$newsletterOptionField->id;
-      $newsletterOption->newsletterId = $newsletterId;
-      $newsletterOption->value = $value;
-      $newsletterOption->save();
-      expect($newsletterOption->getErrors())->false();
-    }
   }
 
   private function createWelcomeNewsletter($status = NewsletterEntity::STATUS_ACTIVE): NewsletterEntity {
@@ -480,14 +454,13 @@ class WelcomeTest extends \MailPoetTest {
 
   public function _after() {
     Carbon::setTestNow();
-    ORM::raw_execute('TRUNCATE ' . Newsletter::$_table);
-    ORM::raw_execute('TRUNCATE ' . NewsletterOption::$_table);
-    ORM::raw_execute('TRUNCATE ' . NewsletterOptionField::$_table);
-    ORM::raw_execute('TRUNCATE ' . NewsletterPost::$_table);
-    ORM::raw_execute('TRUNCATE ' . ScheduledTask::$_table);
-    ORM::raw_execute('TRUNCATE ' . ScheduledTaskSubscriber::$_table);
-    ORM::raw_execute('TRUNCATE ' . SendingQueue::$_table);
-    ORM::raw_execute('TRUNCATE ' . Subscriber::$_table);
-    ORM::raw_execute('TRUNCATE ' . Segment::$_table);
+    $this->truncateEntity(NewsletterEntity::class);
+    $this->truncateEntity(NewsletterOptionFieldEntity::class);
+    $this->truncateEntity(NewsletterOptionEntity::class);
+    $this->truncateEntity(ScheduledTaskEntity::class);
+    $this->truncateEntity(ScheduledTaskSubscriberEntity::class);
+    $this->truncateEntity(SendingQueueEntity::class);
+    $this->truncateEntity(SubscriberEntity::class);
+    $this->truncateEntity(SegmentEntity::class);
   }
 }
