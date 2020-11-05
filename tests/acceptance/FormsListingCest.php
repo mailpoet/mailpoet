72dd9135f8bd866cf6d47dc4c07fb61fb84461f0
Veljko V <veljano@yahoo.com>
2020-11-05 10:44:54 +0100

    Update scenario to cover form new button [MAILPOET-3272]
    [MAILPOET-3272](https://mailpoet.atlassian.net/browse/MAILPOET-3272)

--- tests/acceptance/FormsListingCest.php
+++ tests/acceptance/FormsListingCest.php
@@ -6,16 +6,20 @@ use MailPoet\Test\DataFactories\Form;
 
 class FormsListingCest {
   public function formsListing(\AcceptanceTester $i) {
-    $form = new Form();
-    $form->withName('Test Form');
-    $form->create();
-
     $i->wantTo('Open forms listings page');
+    $formName = 'Test Form';
+    $newFormButton = '.mailpoet-button';
+    $form = new Form();
+    $form->withName($formName);
+    $form->create();
 
     $i->login();
     $i->amOnMailpoetPage('Forms');
-
-    $i->waitForText('Test Form', 5, '.mailpoet-listing-table');
+    $i->waitForText($formName, 5, '.mailpoet-listing-table');
     $i->seeNoJSErrors();
+    $i->seeNumberOfElements($newFormButton, 1);
+    $i->clickItemRowActionByItemName($formName, 'Move to trash');
+    $i->waitForText('No forms were found. Why not create a new one?');
+    $i->seeNumberOfElements($newFormButton, 2);
   }
 }
