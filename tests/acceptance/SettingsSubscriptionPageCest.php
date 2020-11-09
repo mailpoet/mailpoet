3735d8a1a2041d2d22648d946d1db6e747264739
Veljko V <veljano@yahoo.com>
2020-11-09 15:19:18 +0100

    Added new scenarios for custom unsubscribe pages [MAILPOET-3283]
    [MAILPOET-3283](https://mailpoet.atlassian.net/browse/MAILPOET-3283)

--- tests/acceptance/SettingsSubscriptionPageCest.php
+++ tests/acceptance/SettingsSubscriptionPageCest.php
@@ -8,6 +8,7 @@ class SettingsSubscriptionPageCest {
     $i->login();
     $i->amOnMailPoetPage('Settings');
     $i->click('[data-automation-id="preview_manage_subscription_page_link"]');
+    $i->switchToNextTab();
     $i->waitForText('Manage your subscription');
   }
 
@@ -20,8 +21,11 @@ class SettingsSubscriptionPageCest {
     $i->cli(['post', 'create', '--post_type=page', "--post_title=$pageTitle", "--post_content=$pageContent"]);
     $i->click(['css' => '[data-automation-id="subscription-manage-page-selection"]']);
     $i->checkOption('[data-automation-id="subscription-manage-page-selection"]', $pageTitle);
+    //save settings and then verify the page
+    $i->click('[data-automation-id="settings-submit-button"]');
+    $i->waitForText('Settings saved');
     $i->click('[data-automation-id="preview_manage_subscription_page_link"]');
+    $i->switchToNextTab();
     $i->waitForText('Manage your subscription');
-
   }
 }
