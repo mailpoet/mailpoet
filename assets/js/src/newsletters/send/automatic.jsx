501f76939eb2283b2c7668b1990e67c0453c8c18
wxa <wxa@users.noreply.github.com>
2020-11-10 12:01:01 +0300

    Merge WC emails JS code info newsletters, don't use hooks [MAILPOET-2433]

--- assets/js/src/newsletters/send/automatic.jsx
+++ assets/js/src/newsletters/send/automatic.jsx
@@ -1,50 +1,14 @@
-import _ from 'underscore';
-import Hooks from 'wp-js-hooks';
 import MailPoet from 'mailpoet';
 import SendEventConditions from 'newsletters/automatic_emails/send_event_conditions.jsx';
 import GATrackingField from 'newsletters/send/ga_tracking.jsx';
 
 const emails = window.mailpoet_woocommerce_automatic_emails || [];
 
-const extendNewsletterEditorConfig = (defaultConfig, newsletter) => {
-  if (newsletter.type !== 'automatic') return defaultConfig;
-
-  const config = defaultConfig;
-
-  return MailPoet.Ajax
-    .post({
-      api_version: window.mailpoet_api_version,
-      endpoint: 'automatic_emails',
-      action: 'get_event_shortcodes',
-      data: {
-        email_slug: newsletter.options.group,
-        event_slug: newsletter.options.event,
-      },
-    })
-    .then((response) => {
-      if (!_.isObject(response) || !response.data) return config;
-      config.shortcodes = { ...config.shortcodes, ...response.data };
-      return config;
-    })
-    .fail((pauseFailResponse) => {
-      if (pauseFailResponse.errors.length > 0) {
-        MailPoet.Notice.error(
-          pauseFailResponse.errors.map((error) => error.message),
-          { scroll: true, static: true }
-        );
-      }
-    });
-};
-
-Hooks.addFilter('mailpoet_newsletters_editor_extend_config', 'mailpoet', extendNewsletterEditorConfig);
-
-const configureSendPageOptions = (defaultFields, newsletter) => {
-  if (newsletter.type !== 'automatic') return defaultFields;
-
+const getAutomaticEmailFields = (newsletter) => {
   const email = emails[newsletter.options.group];
-
-  if (!email) return defaultFields;
-
+  if (!email) {
+    return false;
+  }
   const emailOptions = newsletter.options;
   const fields = [
     {
@@ -138,50 +102,16 @@ const configureSendPageOptions = (defaultFields, newsletter) => {
     },
   ];
 
-  return {
-    getFields: function getFields() {
-      return fields;
-    },
-    getSendButtonOptions: function getSendButtonOptions() {
-      return {
-        value: MailPoet.I18n.t('activate'),
-      };
-    },
-  };
+  return fields;
 };
 
-Hooks.addFilter('mailpoet_newsletters_send_newsletter_fields', 'mailpoet', configureSendPageOptions);
-
-const configureSendPageServerRequest = (defaultParameters, newsletter) => (
-  (newsletter.type === 'automatic') ? {
-    api_version: window.mailpoet_api_version,
-    endpoint: 'newsletters',
-    action: 'setStatus',
-    data: {
-      id: newsletter.id,
-      status: 'active',
-    },
-  } : defaultParameters
-);
-
-Hooks.addFilter('mailpoet_newsletters_send_server_request_parameters', 'mailpoet', configureSendPageServerRequest);
-
-const redirectAfterSendPageServerRequest = (defaultRedirect, newsletter) => (
-  (newsletter.type === 'automatic') ? `/${newsletter.options.group}` : defaultRedirect
-);
-
-Hooks.addFilter('mailpoet_newsletters_send_server_request_response_redirect', 'mailpoet', redirectAfterSendPageServerRequest);
-
-const configureSendPageServerResponse = (newsletter) => {
-  if (newsletter.type !== 'automatic') return null;
-
-  const email = emails[newsletter.options.group];
-
-  if (!email) return null;
-
-  return () => {
-    MailPoet.Notice.success(MailPoet.I18n.t('automaticEmailActivated').replace('%1s', email.title));
-  };
+export default {
+  getFields: function getFields(newsletter) {
+    return getAutomaticEmailFields(newsletter);
+  },
+  getSendButtonOptions: function getSendButtonOptions() {
+    return {
+      value: MailPoet.I18n.t('activate'),
+    };
+  },
 };
-
-Hooks.addFilter('mailpoet_newsletters_send_server_request_response', 'mailpoet', configureSendPageServerResponse);
