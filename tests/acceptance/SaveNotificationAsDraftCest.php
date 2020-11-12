c85430594aa498bf1091bb8cb0d311c215edd7a8
Veljko V <veljano@yahoo.com>
2020-11-12 14:04:13 +0100

    Update existing save as draft tests to be more precise [MAILPOET-3290]
    [MAILPOET-3290](https://mailpoet.atlassian.net/browse/MAILPOET-3290)

--- tests/acceptance/SaveNotificationAsDraftCest.php
+++ tests/acceptance/SaveNotificationAsDraftCest.php
@@ -8,6 +8,7 @@ class SaveNotificationAsDraftCest {
   public function saveNotificationAsDraft(\AcceptanceTester $i) {
     // step 1 - Prepare post notification data
     $i->wantTo('Save post notification email as draft');
+    $inactiveNewsletterUI = '.mailpoet-listing-row-inactive';
     $newsletterTitle = 'Draft Test Post Notification';
     $newsletterFactory = new Newsletter();
     $newsletter = $newsletterFactory->withSubject($newsletterTitle)
@@ -27,7 +28,9 @@ class SaveNotificationAsDraftCest {
     $i->waitForElementVisible($saveAsDraftButton);
     $i->click($saveAsDraftButton);
     $i->waitForElement('[data-automation-id="newsletters_listing_tabs"]');
-    $i->waitForText('Draft Test Post Notification');
+    $i->waitForText($newsletterTitle);
     $i->dontSeeCheckboxIsChecked('.mailpoet-form-toggle input[type="checkbox"]');
+    $i->waitForText('Not sent yet');
+    $i->seeElement($inactiveNewsletterUI);
   }
 }
