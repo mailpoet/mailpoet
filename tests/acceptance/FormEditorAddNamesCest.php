644552ae15709899ca64b44aecfc3e7980acb9a0
Veljko V <veljano@yahoo.com>
2020-11-16 14:45:57 +0100

    Update scenario to cover modifying of the blocks [MAILPOET-3299]
    [MAILPOET-3299](https://mailpoet.atlassian.net/browse/MAILPOET-3299)

--- tests/acceptance/FormEditorAddNamesCest.php
+++ tests/acceptance/FormEditorAddNamesCest.php
@@ -13,21 +13,29 @@ class FormEditorAddNamesCest {
     $formName = 'My fancy form';
     $form = new Form();
     $form->withName($formName)->withSegments([$segment])->create();
+    $firstNameLabelModified = 'Your First Name';
+    $lastNameLabelModified = 'Your Last Name';
     $i->wantTo('Add first and last name to the editor');
     $i->login();
     $i->amOnMailPoetPage('Forms');
     $i->waitForText($formName);
     $i->clickItemRowActionByItemName($formName, 'Edit');
     $i->waitForElement('[data-automation-id="form_title_input"]');
-
+    // Add First & Last name blocks
     $i->addFromBlockInEditor('First name');
     $i->addFromBlockInEditor('Last name');
-
+    // Modify First & Last name blocks
+    $i->click('[data-automation-id="editor_first_name_input"]');
+    $i->fillField('[data-automation-id="settings_first_name_label_input"]', $firstNameLabelModified);
+    $i->click('[data-automation-id="editor_last_name_input"]');
+    $i->fillField('[data-automation-id="settings_last_name_label_input"]', $lastNameLabelModified);
     $i->saveFormInEditor();
     // Reload page and check data were saved
     $i->reloadPage();
     $i->waitForElement('[data-automation-id="form_title_input"]');
-    $i->seeElement('[data-automation-id="editor_first_name_input"]');
-    $i->seeElement('[data-automation-id="editor_last_name_input"]');
+    $i->click('[data-automation-id="editor_first_name_input"]');
+    $i->seeInField('[data-automation-id="settings_first_name_label_input"]', $firstNameLabelModified);
+    $i->click('[data-automation-id="editor_last_name_input"]');
+    $i->seeInField('[data-automation-id="settings_last_name_label_input"]', $lastNameLabelModified);
   }
 }
