16984d30f71a4a71e74cf9efc9ea1b63fe67abcd
Rostislav Wolny <rosta@mailpoet.com>
2020-11-17 12:25:16 +0100

    Release 3.54.3

--- mailpoet.php
+++ mailpoet.php
@@ -2,7 +2,7 @@
 
 /*
  * Plugin Name: MailPoet 3 (New)
- * Version: 3.54.2
+ * Version: 3.54.3
  * Plugin URI: http://www.mailpoet.com
  * Description: Create and send newsletters, post notifications and welcome emails from your WordPress.
  * Author: MailPoet
@@ -16,7 +16,7 @@
  */
 
 $mailpoetPlugin = [
-  'version' => '3.54.2',
+  'version' => '3.54.3',
   'filename' => __FILE__,
   'path' => dirname(__FILE__),
   'autoloader' => dirname(__FILE__) . '/vendor/autoload.php',
