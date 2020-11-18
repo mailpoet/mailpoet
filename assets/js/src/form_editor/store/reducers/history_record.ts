633188a048ba824cc7815e1eecfb93a082fc2d41
Jan Lysý <lysyjan87@gmail.com>
2020-11-18 16:45:53 +0100

    Add comments and little refactor in history record [MAILPOET-3085]

--- assets/js/src/form_editor/store/reducers/history_record.ts
+++ assets/js/src/form_editor/store/reducers/history_record.ts
@@ -1,48 +1,74 @@
 import { isEqual } from 'lodash';
 
-export const createHistoryRecord = (state) => {
-  let editorHistory: any[] = state.editorHistory;
-  let editorHistoryOffset = state.editorHistoryOffset;
-  const lastHistoryRecord = editorHistory[editorHistory.length - 1];
+const HISTORY_LENGTH = 100;
+const HISTORY_DEBOUNCE = 1000; // 1 second
 
-  let newHistoryRecord;
+type HistoryRecord = {
+  blocks: unknown[];
+  data: unknown[];
+  time: number;
+}
+
+const createRecord = (
+  editorHistory: HistoryRecord[],
+  state
+): HistoryRecord => {
+  const lastHistoryRecord = editorHistory[editorHistory.length - 1];
   const time = Date.now();
 
+  // When we would create two almost same records in a row in very short time,
+  // then we overwrite remove the last record
   if (
     lastHistoryRecord !== undefined
-    && (time - lastHistoryRecord.time) < 1000
+    && (time - lastHistoryRecord.time) < HISTORY_DEBOUNCE
     && editorHistory.length > 1
   ) {
     editorHistory.pop();
 
-    newHistoryRecord = {
+    return {
       blocks: state.formBlocks,
       data: state.formData,
       time: lastHistoryRecord.time,
     };
-  } else {
-    newHistoryRecord = {
-      blocks: state.formBlocks,
-      data: state.formData,
-      time,
-    };
-
-    if (
-      lastHistoryRecord !== undefined
-      && isEqual(lastHistoryRecord.data, newHistoryRecord.data)
-      && isEqual(lastHistoryRecord.blocks, newHistoryRecord.blocks)
-    ) {
-      return state;
-    }
   }
 
+  const newHistoryRecord = {
+    blocks: state.formBlocks,
+    data: state.formData,
+    time,
+  };
+
+  // We won't store two same records in a row, then we retur null
+  if (
+    lastHistoryRecord !== undefined
+    && isEqual(lastHistoryRecord.data, newHistoryRecord.data)
+    && isEqual(lastHistoryRecord.blocks, newHistoryRecord.blocks)
+  ) {
+    return null;
+  }
+
+  return newHistoryRecord;
+};
+
+export const createHistoryRecord = (state) => {
+  let editorHistory: HistoryRecord[] = state.editorHistory;
+  let editorHistoryOffset = state.editorHistoryOffset;
+
+  const newHistoryRecord = createRecord(editorHistory, state);
+  if (newHistoryRecord === null) {
+    return state;
+  }
+
+  // When we want to create a history record, and we aren't at the end,
+  // then we have to drop the rest of the history stack
   if (state.editorHistoryOffset !== 0) {
     const offset = state.editorHistory.length - (state.editorHistoryOffset + 1);
     editorHistoryOffset = 0;
     editorHistory = editorHistory.slice(0, offset);
   }
 
-  if (editorHistory.length >= 100) {
+  // We can't store history for whole time of editing
+  if (editorHistory.length >= HISTORY_LENGTH) {
     editorHistory.shift();
   }
 
