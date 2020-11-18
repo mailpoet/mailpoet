189cc1ad3fceb8272827fbee0d2a4b8853d6659d
Ján Mikláš <neosinner@gmail.com>
2020-11-18 12:52:33 +0100

    Show <Loader> when loading sold products in email stats
    [PREMIUM-146]

--- assets/js/src/common/index.ts
+++ assets/js/src/common/index.ts
@@ -1,3 +1,4 @@
+export { default as Button } from './button/button';
+export { default as Loader } from './loader/loader';
 export { default as Tab } from './tabs/tab';
 export { default as Tag } from './tag/tag';
-export { default as Button } from './button/button';
