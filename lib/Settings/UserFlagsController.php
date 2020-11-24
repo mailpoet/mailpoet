9809c0adda9221448dec98490c74f61f357a13e5
Pavel Dohnal <pavel@mailpoet.com>
2020-11-24 11:44:09 +0100

    Add data about form editor tutorial into store
    [MAILPOET-3206]

--- lib/Settings/UserFlagsController.php
+++ lib/Settings/UserFlagsController.php
@@ -20,6 +20,7 @@ class UserFlagsController {
     $this->defaults = [
       'last_announcement_seen' => false,
       'editor_tutorial_seen' => false,
+      'form_editor_tutorial_seen' => false,
       'display_new_form_editor_nps_survey' => false,
       'transactional_emails_opt_in_notice_dismissed' => false,
     ];
