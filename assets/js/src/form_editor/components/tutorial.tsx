bfaa1bbec466a74cee051e3df101f4a355e43ec4
Pavel Dohnal <pavel@mailpoet.com>
2020-11-24 15:08:43 +0100

    Display tutorial only to new users
    [MAILPOET-3206]

--- assets/js/src/form_editor/components/tutorial.tsx
+++ assets/js/src/form_editor/components/tutorial.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Modal from 'common/modal/modal';
 import { useSelect, useDispatch } from '@wordpress/data';
+import MailPoet from 'mailpoet';
 
 export const Tutorial = () => {
   const url = useSelect(
@@ -15,6 +16,13 @@ export const Tutorial = () => {
 
   if (tutorialSeen) {
     return null;
+  }
+
+  const installedAtDate = new Date(MailPoet.installedAt);
+  const sevenDaysAgo = new Date();
+  sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+  if (installedAtDate < sevenDaysAgo) {
+    return null; // only show to new users
   }
 
   function onClose() {
--- lib/AdminPages/Pages/FormEditor.php
+++ lib/AdminPages/Pages/FormEditor.php
@@ -78,6 +78,7 @@ use MailPoet\Models\Segment;
 use MailPoet\Router\Endpoints\FormPreview;
 use MailPoet\Router\Router;
 use MailPoet\Settings\Pages;
+use MailPoet\Settings\SettingsController;
 use MailPoet\Settings\UserFlagsController;
 use MailPoet\WP\Functions as WPFunctions;
 
@@ -111,6 +112,9 @@ class FormEditor {
 
   /** @var UserFlagsController */
   private $userFlags;
+
+  /** @var SettingsController */
+  private $settings;
 
   private $activeTemplates = [
     FormEntity::DISPLAY_TYPE_POPUP => [
@@ -195,6 +199,7 @@ class FormEditor {
     FormFactory $formsFactory,
     Localizer $localizer,
     UserFlagsController $userFlags,
+    SettingsController $settings,
     TemplateRepository $templateRepository
   ) {
     $this->pageRenderer = $pageRenderer;
@@ -207,6 +212,7 @@ class FormEditor {
     $this->localizer = $localizer;
     $this->templatesRepository = $templateRepository;
     $this->userFlags = $userFlags;
+    $this->settings = $settings;
   }
 
   public function render() {
@@ -251,6 +257,7 @@ class FormEditor {
       'products' => $this->getWooCommerceProducts(),
       'product_categories' => $this->getWooCommerceCategories(),
       'product_tags' => $this->getWooCommerceTags(),
+      'installed_at' => $this->settings->get('installed_at'),
     ];
     $this->wp->wpEnqueueMedia();
     $this->pageRenderer->displayPage('form/editor.html', $data);
