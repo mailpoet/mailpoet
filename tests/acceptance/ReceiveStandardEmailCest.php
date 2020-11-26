cf0daa6fcedb07e725be3700059a23b6d18564f5
Pavel Dohnal <pavel@mailpoet.com>
2020-11-26 14:33:27 +0100

    Fix test that is failing on master

--- tests/acceptance/ReceiveStandardEmailCest.php
+++ tests/acceptance/ReceiveStandardEmailCest.php
@@ -21,30 +21,35 @@ class ReceiveStandardEmailCest {
     $specialChars = '… © & ěščřžýáíéůėę€żąß∂‍‍‍';
 
     $newsletterTitle = 'Receive Test ' . $specialChars;
-    $standardTemplate = '[data-automation-id=\'select_template_0\']';
-    $titleElement = '[data-automation-id=\'newsletter_title\']';
+    $standardTemplate = '[data-automation-id="select_template_0"]';
+    $titleElement = '[data-automation-id="newsletter_title"]';
     $sendFormElement = '[data-automation-id="newsletter_send_form"]';
     $segmentName = $i->createListWithSubscriber();
     $i->wantTo('Receive a standard newsletter as a subscriber');
 
-    //create a wp user with wp role subscriber
+    $i->wantTo('Create a wp user with wp role subscriber');
     $i->cli(['user', 'create', 'narwhal', 'standardtest@example.com', '--role=subscriber']);
-    //Create a newsletter with template
+
+    $i->wantTo('Create a newsletter with template');
     $i->login();
     $i->amOnMailpoetPage('Emails');
     $i->click('[data-automation-id="create_standard"]');
+    $i->waitForElement('[data-automation-id="templates-standard"]');
+    $i->click('[data-automation-id="templates-standard"]');
     $i->waitForElement($standardTemplate);
     $i->see('Newsletters', ['css' => '.mailpoet-categories-item.active']);
     $i->click($standardTemplate);
     $i->waitForElement($titleElement);
     $i->fillField($titleElement, $newsletterTitle);
     $i->click('Next');
-    //Choose list and send
+
+    $i->wantTo('Choose list and send');
     $i->waitForElement($sendFormElement);
     $i->selectOptionInSelect2($segmentName);
     $i->click('Send');
     $i->waitForEmailSendingOrSent();
-    //confirm newsletter is received
+
+    $i->wantTo('confirm newsletter is received');
     $i->checkEmailWasReceived($newsletterTitle);
     $i->click(Locator::contains('span.subject', $newsletterTitle));
   }
