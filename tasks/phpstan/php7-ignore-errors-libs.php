9e9063414fe15c7eea5a3d82237118f533181820
Rostislav Wolny <rosta@mailpoet.com>
2020-11-30 17:54:10 +0100

    Ignore some PHPStan errors in libs only for PHP7
    [MAILPOET-3296]

---
+++ tasks/phpstan/php7-ignore-errors-libs.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types = 1);
+# PHPStan gets smarter when runs on PHP8 and some type checks added because of PHP8 are reported as unnecessary when we run PHPStan on PHP7
+# see https://github.com/phpstan/phpstan/issues/4060
+$config = [];
+
+if (PHP_VERSION_ID < 80000) {
+  $config['parameters']['ignoreErrors'][] = [
+    'message' => '#^Else branch is unreachable because ternary operator condition is always true#',
+    'path' => __DIR__ . '/../../lib/AdminPages/Pages/Forms.php',
+    'count' => 1,
+  ];
+  $config['parameters']['ignoreErrors'][] = [
+    'message' => '#^Else branch is unreachable because ternary operator condition is always true#',
+    'path' => __DIR__ . '/../../lib/AdminPages/Pages/Newsletters.php',
+    'count' => 1,
+  ];
+  $config['parameters']['ignoreErrors'][] = [
+    'message' => '#^Strict comparison using === between PDOStatement and false will always evaluate to false#',
+    'path' => __DIR__ . '/../../lib/Doctrine/Driver/PDOConnection.php',
+    'count' => 1,
+  ];
+  $config['parameters']['ignoreErrors'][] = [
+    'message' => '#^Strict comparison using === between string and false will always evaluate to false#',
+    'path' => __DIR__ . '/../../lib/Doctrine/Driver/PDOConnection.php',
+    'count' => 1,
+  ];
+  $config['parameters']['ignoreErrors'][] = [
+    'message' => '#^Cannot access offset \(int\|string\) on array\|false#',
+    'path' => __DIR__ . '/../../lib/Features/FeatureFlagsController.php',
+    'count' => 1,
+  ];
+}
+
+return $config;
