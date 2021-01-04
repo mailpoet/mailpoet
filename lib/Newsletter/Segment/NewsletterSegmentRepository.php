79fe6be3e9ba2c673064df500987897351238f7d
Jan Lysý <lysyjan87@gmail.com>
2021-01-04 13:55:10 +0100

    Add scheduled emails subjects into segments API response
    [MAILPOET-3142]

--- lib/Newsletter/Segment/NewsletterSegmentRepository.php
+++ lib/Newsletter/Segment/NewsletterSegmentRepository.php
@@ -3,6 +3,7 @@
 namespace MailPoet\Newsletter\Segment;
 
 use MailPoet\Doctrine\Repository;
+use MailPoet\Entities\NewsletterEntity;
 use MailPoet\Entities\NewsletterSegmentEntity;
 
 /**
@@ -11,5 +12,23 @@ use MailPoet\Entities\NewsletterSegmentEntity;
 class NewsletterSegmentRepository extends Repository {
   protected function getEntityClassName() {
     return NewsletterSegmentEntity::class;
+  }
+
+  public function getScheduledNewsletterSubjectsBySegmentIds(array $segmentIds): array {
+    $results = $this->doctrineRepository->createQueryBuilder('ns')
+      ->select('IDENTITY(ns.segment) AS segment_id, n.subject')
+      ->join('ns.newsletter', 'n')
+      ->where('n.status = :scheduled')
+      ->andWhere('ns.segment IN (:segmentIds)')
+      ->setParameter('scheduled', NewsletterEntity::STATUS_SCHEDULED)
+      ->setParameter('segmentIds', $segmentIds)
+      ->getQuery()
+      ->getResult();
+    
+    $nameMap = [];
+    foreach ($results as $result) {
+      $nameMap[(string)$result['segment_id']][] = $result['subject'];
+    }
+    return $nameMap;
   }
 }
