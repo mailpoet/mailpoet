2c3f6488c236595a674376e633d7180f860f6ac8
Rostislav Wolny <rosta@mailpoet.com>
2021-01-07 16:16:25 +0100

    Remove no longer needed fetching of WP user count
    [MAILPOET-3143]

--- lib/AdminPages/Pages/Segments.php
+++ lib/AdminPages/Pages/Segments.php
@@ -6,8 +6,6 @@ use MailPoet\AdminPages\PageRenderer;
 use MailPoet\Config\ServicesChecker;
 use MailPoet\Listing\PageLimit;
 use MailPoet\Models\Newsletter;
-use MailPoet\Models\Segment;
-use MailPoet\Models\Subscriber;
 use MailPoet\Services\Bridge;
 use MailPoet\Util\License\Features\Subscribers as SubscribersFeature;
 use MailPoet\WooCommerce\Helper as WooCommerceHelper;
@@ -65,16 +63,6 @@ class Segments {
     $data['has_valid_api_key'] = $this->subscribersFeature->hasValidApiKey();
     $data['subscriber_count'] = $this->subscribersFeature->getSubscribersCount();
     $data['has_premium_support'] = $this->subscribersFeature->hasPremiumSupport();
-
-    $data['wp_users_count'] = false;
-    if (!$data['has_premium_support']) {
-      $wpSegment = Segment::getWPSegment()->withSubscribersCount();
-      $subscribersCount = $wpSegment->subscribersCount;
-      $data['wp_users_count'] = $subscribersCount[Subscriber::STATUS_SUBSCRIBED]
-        + $subscribersCount[Subscriber::STATUS_UNCONFIRMED]
-        + $subscribersCount[Subscriber::STATUS_INACTIVE];
-    }
-
     $data['mss_key_invalid'] = ($this->servicesChecker->isMailPoetAPIKeyValid() === false);
 
     $wpRoles = $this->wp->getEditableRoles();
