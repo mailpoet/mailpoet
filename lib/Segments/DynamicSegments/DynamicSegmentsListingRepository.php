15528d84e0b262d53d1cedb14b077b444aaf107d
Pavel Dohnal <pavel.dohnal@automattic.com>
2021-01-08 15:04:07 +0100

    Fix displaying woocommerce static segment in dynamic segments
    [MAILPOET-3361]

--- lib/Segments/DynamicSegments/DynamicSegmentsListingRepository.php
+++ lib/Segments/DynamicSegments/DynamicSegmentsListingRepository.php
@@ -4,7 +4,12 @@ namespace MailPoet\Segments\DynamicSegments;
 
 use MailPoet\Entities\SegmentEntity;
 use MailPoet\Segments\SegmentListingRepository;
+use MailPoetVendor\Doctrine\ORM\QueryBuilder;
 
 class DynamicSegmentsListingRepository extends SegmentListingRepository {
-  protected $types = [SegmentEntity::TYPE_DYNAMIC];
+  protected function applyParameters(QueryBuilder $queryBuilder, array $parameters) {
+    $queryBuilder
+      ->andWhere('s.type = :type')
+      ->setParameter('type', SegmentEntity::TYPE_DYNAMIC);
+  }
 }
