4d45d21f90f97c7e46745415290ec3c7d9075a56
Veljko <veljano@yahoo.com>
2021-01-13 15:28:21 +0100

    Update test to cover recent bug of form's placement

--- tests/acceptance/FormEditorPlaceFormOnSpecifiedPageCest.php
+++ tests/acceptance/FormEditorPlaceFormOnSpecifiedPageCest.php
@@ -21,6 +21,10 @@ class FormEditorPlaceFormOnSpecifiedPageCest {
     $pageData = $i->cliToArray(['post', 'get', $page[0], '--format=json']);
     $pageData = json_decode($pageData[0], true);
     $pageUrl = $pageData['guid'];
+    $post = $i->cliToArray(['post', 'create', '--format=json', '--porcelain', '--post_status=publish', '--post_type=post', "--post_title=$pageTitle", "--post_content=$pageContent"]);
+    $postData = $i->cliToArray(['post', 'get', $post[0], '--format=json']);
+    $postData = json_decode($postData[0], true);
+    $postUrl = $postData['guid'];
 
     $i->wantTo('Set popup form to display on the created page');
     $i->login();
@@ -33,9 +37,15 @@ class FormEditorPlaceFormOnSpecifiedPageCest {
     $i->checkOption('Enable');
     $i->waitForText('Display on all posts');
     $i->selectOptionInSelect2($pageTitle, '[data-automation-id="form-placement-select-page"] input.select2-search__field');
-
     $i->wantTo('Save the form and check the output');
     $i->saveFormInEditor();
+    $i->amOnUrl($pageUrl);
+    $i->waitForElement('[data-automation-id="form_email"]');
+
+    $i->wantTo('Check if form is not present in a post and recheck appearing in a page again');
+    $i->amOnUrl($postUrl);
+    $i->waitForText($pageTitle);
+    $i->dontSeeElement('[data-automation-id="form_email"]');
     $i->amOnUrl($pageUrl);
     $i->waitForElement('[data-automation-id="form_email"]');
   }
