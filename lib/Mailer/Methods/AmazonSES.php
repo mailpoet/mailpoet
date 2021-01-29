ca76f700891f4e8f4264d07d04dbd16bb55dbeb4
Jan Lysý <lysyjan87@gmail.com>
2021-01-29 08:38:00 +0100

    Add new Amazon SES regions
    [MAILPOET-3369]

--- lib/Mailer/Methods/AmazonSES.php
+++ lib/Mailer/Methods/AmazonSES.php
@@ -27,14 +27,21 @@ class AmazonSES {
   private $availableRegions = [
     'US East (N. Virginia)' => 'us-east-1',
     'US East (Ohio)' => 'us-east-2',
+    'US West (N. California)' => 'us-west-1',
     'US West (Oregon)' => 'us-west-2',
     'EU (Ireland)' => 'eu-west-1',
     'EU (London)' => 'eu-west-2',
     'EU (Frankfurt)' => 'eu-central-1',
+    'EU (Paris)' => 'eu-west-3',
+    'EU (Stockholm)' => 'eu-north-1',
     'Canada (Central)' => 'ca-central-1',
     'Asia Pacific (Mumbai)' => 'ap-south-1',
+    'Asia Pacific (Seoul)' => 'ap-northeast-2',
+    'Asia Pacific (Singapore)' => 'ap-southeast-1',
     'Asia Pacific (Sydney)' => 'ap-southeast-2',
+    'Asia Pacific (Tokyo)' => 'ap-northeast-1',
     'South America (Sao Paulo)' => 'sa-east-1',
+    'AWS GovCloud (US)' => 'us-gov-west-1',
   ];
 
   /** @var AmazonSESMapper */
