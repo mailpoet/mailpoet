8bb84da0ae57960e531bb55480181bf3f51f494f
Rostislav Wolny <rosta@mailpoet.com>
2021-01-29 14:16:30 +0100

    Use BlockFormatControls component instead of Fill in font select
    [MAILPOET-3382]

--- assets/js/src/form_editor/rich_text/font_selection_format.tsx
+++ assets/js/src/form_editor/rich_text/font_selection_format.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { applyFormat } from '@wordpress/rich-text';
 import MailPoet from 'mailpoet';
-import { Fill } from '@wordpress/components';
+import { BlockFormatControls } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
 
 import FontFamilySettings from '../components/font_family_settings';
@@ -29,28 +29,26 @@ const Edit = ({
   }
 
   return (
-    <>
-      <Fill name="BlockFormatControls">
-        <div className="mailpoet_toolbar_item">
-          <FontFamilySettings
-            value={activeAttributes.font}
-            onChange={(font) => {
-              onChange(
-                applyFormat(value, {
-                  type: 'mailpoet-form/font-selection',
-                  attributes: {
-                    style: `font-family: ${font}`,
-                    font,
-                  },
-                })
-              );
-            }}
-            name={MailPoet.I18n.t('formSettingsStylesFontFamily')}
-            hideLabelFromVision
-          />
-        </div>
-      </Fill>
-    </>
+    <BlockFormatControls>
+      <div className="mailpoet_toolbar_item">
+        <FontFamilySettings
+          value={activeAttributes.font}
+          onChange={(font) => {
+            onChange(
+              applyFormat(value, {
+                type: 'mailpoet-form/font-selection',
+                attributes: {
+                  style: `font-family: ${font}`,
+                  font,
+                },
+              })
+            );
+          }}
+          name={MailPoet.I18n.t('formSettingsStylesFontFamily')}
+          hideLabelFromVision
+        />
+      </div>
+    </BlockFormatControls>
   );
 };
 
