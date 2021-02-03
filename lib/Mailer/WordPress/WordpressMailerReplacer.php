83ad751a51c3a784fbf045b2e5f26471a7f06c28
Rostislav Wolny <rosta@mailpoet.com>
2021-02-03 14:36:21 +0100

    Fix crashes on multisite installations
    [MAILPOET-3403]

--- lib/Mailer/WordPress/WordpressMailerReplacer.php
+++ lib/Mailer/WordPress/WordpressMailerReplacer.php
@@ -35,7 +35,14 @@ class WordpressMailerReplacer {
 
   public function replaceWordPressMailer() {
     global $phpmailer;
-    if ($this->settings->get('send_transactional_emails', false)) {
+    // This code needs to be wrapped because it has to run in an early stage of plugin initialisation
+    // and in some cases on multisite instance it may run before DB migrator and settings table is not ready at that time
+    try {
+      $sendTransactional = $this->settings->get('send_transactional_emails', false);
+    } catch (\Exception $e) {
+      $sendTransactional = false;
+    }
+    if ($sendTransactional) {
       $phpmailer = new WordPressMailer(
         $this->mailer,
         new FallbackMailer($this->settings),
