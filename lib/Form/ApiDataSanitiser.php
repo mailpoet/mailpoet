92d9ce05d758acb7a04371c8ad40d9e44a705560
Rostislav Wolny <rosta@mailpoet.com>
2021-02-10 13:47:26 +0100

    Add service for fixing form API data
    [MAILPOET-3415]

---
+++ lib/Form/ApiDataSanitiser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MailPoet\Form;
+
+class ApiDataSanitiser {
+  /** @var FormHtmlSanitiser */
+  private $htmlSanitizer;
+
+  /**
+   * List of blocks and their parameters that will be sanitized
+   * @var string[][]
+   */
+  private $htmlSanitizeConfig = [
+    'paragraph' => [
+      'content',
+    ],
+    'heading' => [
+      'content',
+    ],
+  ];
+
+  public function __construct(FormHtmlSanitiser $htmlSanitiser) {
+    $this->htmlSanitizer = $htmlSanitiser;
+  }
+
+  public function sanitiseBody(array $body): array {
+    foreach ($body as $key => $block) {
+      $sanitizedBlock = $this->sanitiseBlock($block);
+      if (isset($sanitizedBlock['body']) && is_array($sanitizedBlock['body']) && !empty($sanitizedBlock['body'])) {
+        $sanitizedBlock['body'] = $this->sanitiseBody($sanitizedBlock['body']);
+      }
+      $body[$key] = $sanitizedBlock;
+    }
+    return $body;
+  }
+
+  private function sanitiseBlock(array $block): array {
+    if (!isset($this->htmlSanitizeConfig[$block['type']])) {
+      return $block;
+    }
+    $params = $block['params'] ?? [];
+    foreach ($this->htmlSanitizeConfig[$block['type']] as $parameter) {
+      if (!isset($params[$parameter])) continue;
+      $params[$parameter] = $this->htmlSanitizer->sanitise($params[$parameter]);
+    }
+    $block['params'] = $params;
+    return $block;
+  }
+}
