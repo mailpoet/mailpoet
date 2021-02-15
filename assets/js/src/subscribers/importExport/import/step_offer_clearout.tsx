098af570e36891ec6772a944b1be2fabb2a0361f
Pavel Dohnal <pavel.dohnal@automattic.com>
2021-02-15 11:18:34 +0100

    Fix function return types in import
    [MAILPOET-3301]

--- assets/js/src/subscribers/importExport/import/step_offer_clearout.tsx
+++ assets/js/src/subscribers/importExport/import/step_offer_clearout.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import OfferClearout from './offer_clearout';
 
-export default ({ history }: RouteComponentProps) => (
-  <OfferClearout onProceed={() => history.push('step_method_selection')} />
+export default ({ history }: RouteComponentProps): JSX.Element => (
+  <OfferClearout onProceed={(): void => history.push('step_method_selection')} />
 );
