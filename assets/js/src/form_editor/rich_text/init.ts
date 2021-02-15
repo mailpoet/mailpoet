ad9811e1199472e9394329440db6cb9f85c04cbe
Pavel Dohnal <pavel.dohnal@automattic.com>
2021-02-15 10:13:48 +0100

    Fix function return types in rich text
    [MAILPOET-3301]

--- assets/js/src/form_editor/rich_text/init.ts
+++ assets/js/src/form_editor/rich_text/init.ts
@@ -2,6 +2,6 @@ import '@wordpress/format-library'; // load default formats (bold, italic, ...)
 import { registerFormatType } from '@wordpress/rich-text';
 import * as FontSelectionFormat from './font_selection_format';
 
-export default function () {
+export default function (): void {
   registerFormatType(FontSelectionFormat.name, FontSelectionFormat.settings);
 }
