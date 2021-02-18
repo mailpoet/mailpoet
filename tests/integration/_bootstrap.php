5c38069caa13bafcd9a78854fffcfa12d5723e1b
Rostislav Wolny <rosta@mailpoet.com>
2021-02-18 17:13:13 +0100

    Add scheduled tasks cleanup to tests setup
    [MAILPOET-3412]

--- tests/integration/_bootstrap.php
+++ tests/integration/_bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 use MailPoet\DI\ContainerWrapper;
+use MailPoet\Entities\ScheduledTaskEntity;
 use MailPoet\Settings\SettingsController;
 use MailPoetVendor\Doctrine\DBAL\Connection;
 use MailPoetVendor\Doctrine\ORM\EntityManager;
@@ -181,6 +182,8 @@ abstract class MailPoetTest extends \Codeception\TestCase\Test { // phpcs:ignore
     $this->connection = $this->diContainer->get(Connection::class);
     $this->entityManager = $this->diContainer->get(EntityManager::class);
     $this->diContainer->get(SettingsController::class)->resetCache();
+    // Cleanup scheduled tasks from previous tests
+    $this->truncateEntity(ScheduledTaskEntity::class);
     $this->entityManager->clear();
     parent::setUp();
   }
