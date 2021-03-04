4fe9851a2e1308894538489752e8e5deee36e7f6
Pavel Dohnal <pavel.dohnal@automattic.com>
2021-03-04 10:09:25 +0100

    Refactor heading steps to typescript
    [MAILPOET-3436]

--- assets/js/src/newsletters/listings/heading_display.jsx
+++ assets/js/src/newsletters/listings/heading_display.jsx
@@ -1,5 +1,5 @@
 import { withRouter } from 'react-router-dom';
-import { mapPathToSteps } from './heading_steps.jsx';
+import { mapPathToSteps } from './heading_steps.tsx';
 
 const isHeaderHidden = (location) => location.hash.match(new RegExp('^#/new')) || location.pathname.match(new RegExp('^/new'));
 
--- assets/js/src/newsletters/listings/heading_steps_route.jsx
+++ assets/js/src/newsletters/listings/heading_steps_route.jsx
@@ -1,5 +1,5 @@
 import { withRouter } from 'react-router-dom';
-import ListingHeadingSteps from './heading_steps.jsx';
+import ListingHeadingSteps from './heading_steps.tsx';
 
 const ListingHeadingStepsRoute = withRouter(ListingHeadingSteps);
 
