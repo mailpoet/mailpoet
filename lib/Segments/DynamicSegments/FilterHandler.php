7a98abdead1f7a82dd03b810a66433267d19d0e3
Pavel Dohnal <pavel.dohnal@automattic.com>
2021-03-15 14:38:04 +0100

    Connect dynamic segments filters using UNION select
    [MAILPOET-3212]

--- lib/Segments/DynamicSegments/FilterHandler.php
+++ lib/Segments/DynamicSegments/FilterHandler.php
@@ -1,14 +1,17 @@
-<?php
+<?php declare(strict_types = 1);
 
 namespace MailPoet\Segments\DynamicSegments;
 
 use MailPoet\Entities\DynamicSegmentFilterData;
+use MailPoet\Entities\SegmentEntity;
+use MailPoet\Entities\SubscriberEntity;
 use MailPoet\Segments\DynamicSegments\Exceptions\InvalidFilterException;
 use MailPoet\Segments\DynamicSegments\Filters\EmailAction;
 use MailPoet\Segments\DynamicSegments\Filters\UserRole;
 use MailPoet\Segments\DynamicSegments\Filters\WooCommerceCategory;
 use MailPoet\Segments\DynamicSegments\Filters\WooCommerceProduct;
 use MailPoetVendor\Doctrine\DBAL\Query\QueryBuilder;
+use MailPoetVendor\Doctrine\ORM\EntityManager;
 
 class FilterHandler {
   /** @var EmailAction */
@@ -23,7 +26,11 @@ class FilterHandler {
   /** @var WooCommerceCategory */
   private $wooCommerceCategory;
 
+  /** @var EntityManager */
+  private $entityManager;
+
   public function __construct(
+    EntityManager $entityManager,
     EmailAction $emailAction,
     UserRole $userRole,
     WooCommerceProduct $wooCommerceProduct,
@@ -33,9 +40,31 @@ class FilterHandler {
     $this->userRole = $userRole;
     $this->wooCommerceProduct = $wooCommerceProduct;
     $this->wooCommerceCategory = $wooCommerceCategory;
+    $this->entityManager = $entityManager;
   }
 
-  public function apply(QueryBuilder $queryBuilder, DynamicSegmentFilterData $filter): QueryBuilder {
+  public function apply(QueryBuilder $queryBuilder, SegmentEntity $segment): QueryBuilder {
+    $filters = $segment->getDynamicFilters();
+    $filterSelects = [];
+    $subscribersTable = $this->entityManager->getClassMetadata(SubscriberEntity::class)->getTableName();
+    foreach ($filters as $filter) {
+      $subscribersIdsQuery = $this->entityManager
+        ->getConnection()
+        ->createQueryBuilder()
+        ->select("DISTINCT $subscribersTable.id as inner_subscriber_id")
+        ->from($subscribersTable);
+      $this->applyFilter($subscribersIdsQuery, $filter->getFilterData());
+      $filterSelects[] = $subscribersIdsQuery->getSQL();
+      $queryBuilder->setParameters(array_merge(
+        $subscribersIdsQuery->getParameters(),
+        $queryBuilder->getParameters()
+      ));
+    }
+    $queryBuilder->innerJoin($subscribersTable, sprintf('(%s)', join(' UNION ', $filterSelects)), 'filtered_subscribers', 'filtered_subscribers.inner_subscriber_id = id');
+    return $queryBuilder;
+  }
+
+  private function applyFilter(QueryBuilder $queryBuilder, DynamicSegmentFilterData $filter): QueryBuilder {
     switch ($filter->getFilterType()) {
       case DynamicSegmentFilterData::TYPE_USER_ROLE:
         return $this->userRole->apply($queryBuilder, $filter);
