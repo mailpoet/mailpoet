f066bc8fddbbaf91f757ff63c80734b4cc3c4d22
Rostislav Wolny <rosta@mailpoet.com>
2021-03-19 16:31:21 +0100

    Disallow trashing single dynamic segment with active email
    [MAILPOET-3463]

--- lib/Newsletter/Segment/NewsletterSegmentRepository.php
+++ lib/Newsletter/Segment/NewsletterSegmentRepository.php
@@ -14,6 +14,32 @@ class NewsletterSegmentRepository extends Repository {
     return NewsletterSegmentEntity::class;
   }
 
+  public function getSubjectsOfActivelyUsedEmailsForSegments(array $segmentIds): array {
+    $results = $this->doctrineRepository->createQueryBuilder('ns')
+      ->join('ns.newsletter', 'n')
+      ->leftJoin('n.queues', 'q')
+      ->leftJoin('q.task', 't')
+      ->select('IDENTITY(ns.segment) AS segment_id, n.subject')
+      ->where('(n.type IN (:types) OR n.status = :scheduled OR (t.id IS NOT NULL AND t.status IS NULL))')
+      ->andWhere('ns.segment IN (:segmentIds)')
+      ->setParameter('types', [
+        NewsletterEntity::TYPE_AUTOMATIC,
+        NewsletterEntity::TYPE_WELCOME,
+        NewsletterEntity::TYPE_NOTIFICATION,
+      ])
+      ->setParameter('segmentIds', $segmentIds)
+      ->setParameter('scheduled', NewsletterEntity::STATUS_SCHEDULED)
+      ->addGroupBy('n.id, q.id, t.id')
+      ->getQuery()
+      ->getResult();
+
+    $nameMap = [];
+    foreach ($results as $result) {
+      $nameMap[(string)$result['segment_id']][] = $result['subject'];
+    }
+    return $nameMap;
+  }
+
   public function getAutomatedEmailSubjectsBySegmentIds(array $segmentIds): array {
     $results = $this->doctrineRepository->createQueryBuilder('ns')
       ->join('ns.newsletter', 'n')
