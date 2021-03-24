51492c2da8cb31048dac2a57b627d569d8f1cfda
Jan Lysý <jan.lysy@automattic.com>
2021-03-24 10:12:47 +0100

    Add createOrUpdate for SubscriberCustomField
    [MAILPOET-3031]

--- lib/Subscribers/SubscriberCustomFieldRepository.php
+++ lib/Subscribers/SubscriberCustomFieldRepository.php
@@ -3,7 +3,9 @@
 namespace MailPoet\Subscribers;
 
 use MailPoet\Doctrine\Repository;
+use MailPoet\Entities\CustomFieldEntity;
 use MailPoet\Entities\SubscriberCustomFieldEntity;
+use MailPoet\Entities\SubscriberEntity;
 
 /**
  * @extends Repository<SubscriberCustomFieldEntity>
@@ -11,5 +13,20 @@ use MailPoet\Entities\SubscriberCustomFieldEntity;
 class SubscriberCustomFieldRepository extends Repository {
   protected function getEntityClassName() {
     return SubscriberCustomFieldEntity::class;
+  }
+
+  /**
+   * @param string|array|null $value
+   */
+  public function createOrUpdate(SubscriberEntity $subscriber, CustomFieldEntity $customField, $value): SubscriberCustomFieldEntity {
+    $subscriberCustomField = $this->findOneBy(['subscriber' => $subscriber, 'customField' => $customField]);
+    if ($subscriberCustomField instanceof SubscriberCustomFieldEntity) {
+      $subscriberCustomField->setValue($value);
+    } else {
+      $subscriberCustomField = new SubscriberCustomFieldEntity($subscriber, $customField, $value);
+      $this->entityManager->persist($subscriberCustomField);
+    }
+    $this->entityManager->flush();
+    return $subscriberCustomField;
   }
 }
