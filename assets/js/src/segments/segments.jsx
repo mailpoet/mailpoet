eaa79da75698be6a3766f0a684159d1995b39108
Pavel Dohnal <pavel.dohnal@automattic.com>
2021-03-25 10:26:57 +0100

    Refactor form filters to a separate file
    [MAILPOET-3476]

--- assets/js/src/segments/segments.jsx
+++ assets/js/src/segments/segments.jsx
@@ -11,8 +11,8 @@ import SegmentList from 'segments/list.jsx';
 import SegmentForm from 'segments/form.jsx';
 import { GlobalContext, useGlobalContextValue } from 'context/index.jsx';
 import Notices from 'notices/notices.jsx';
-import DynamicSegmentForm from './dynamic/dynamic_segments_form';
-import DynamicSegmentList from './dynamic/dynamic_segments_list.jsx';
+import DynamicSegmentForm from './dynamic/form';
+import DynamicSegmentList from './dynamic/list.jsx';
 import ListHeading from './heading';
 
 const container = document.getElementById('segments_container');
