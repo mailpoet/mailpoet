1548d3fe6fac28c2673070dab812fcee7f573917
Jan Lysý <jan.lysy@automattic.com>
2021-03-30 20:31:52 +0200

    Use Doctrine in SubscriberActions integration tests
    [MAILPOET-3032]

--- lib/Newsletter/Sending/SendingQueuesRepository.php
+++ lib/Newsletter/Sending/SendingQueuesRepository.php
@@ -3,6 +3,7 @@
 namespace MailPoet\Newsletter\Sending;
 
 use MailPoet\Doctrine\Repository;
+use MailPoet\Entities\NewsletterEntity;
 use MailPoet\Entities\SendingQueueEntity;
 
 /**
@@ -11,5 +12,18 @@ use MailPoet\Entities\SendingQueueEntity;
 class SendingQueuesRepository extends Repository {
   protected function getEntityClassName() {
     return SendingQueueEntity::class;
+  }
+
+  public function findOneByNewsletterAndTaskStatus(NewsletterEntity $newsletter, string $status): ?SendingQueueEntity {
+    return $this->entityManager->createQueryBuilder()
+      ->select('s')
+      ->from(SendingQueueEntity::class, 's')
+      ->join('s.task', 't')
+      ->where('t.status = :status')
+      ->andWhere('s.newsletter = :newsletter')
+      ->setParameter('status', $status)
+      ->setParameter('newsletter', $newsletter)
+      ->getQuery()
+      ->getOneOrNullResult();
   }
 }
