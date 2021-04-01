c42992ffc668106b4cab615d858f4434b3b2eb48
Rodrigo Primo <rodrigosprimo@gmail.com>
2021-04-01 13:43:45 -0300

    Refactor Forms::duplicate() to use Doctrine instead of Paris
    [MAILPOET-3038]

---
+++ tests/integration/Form/FormSaveControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MailPoet\Form;
+
+use Codeception\Util\Fixtures;
+use MailPoet\Entities\FormEntity;
+
+class FormSaveControllerTest extends \MailPoetTest {
+  /** @var FormSaveController */
+  private $saveController;
+
+  public function _before() {
+    parent::_before();
+    $this->saveController = $this->diContainer->get(FormSaveController::class);
+  }
+
+  public function testItDuplicatesForms() {
+    $form = $this->createForm();
+    $duplicate = $this->saveController->duplicate($form);
+    expect($duplicate->getName())->equals('Copy of ' . $form->getName());
+    expect($duplicate->getDeletedAt())->equals(null);
+    expect($duplicate->getBody())->equals($form->getBody());
+    expect($duplicate->getStatus())->equals($form->getStatus());
+  }
+
+  public function _after() {
+    $this->cleanup();
+  }
+
+  private function createForm(): FormEntity {
+    $form = new FormEntity('My Form');
+    $form->setBody(Fixtures::get('form_body_template'));
+    $this->entityManager->persist($form);
+    $this->entityManager->flush();
+    return $form;
+  }
+
+  private function cleanup() {
+    $this->truncateEntity(FormEntity::class);
+  }
+}
