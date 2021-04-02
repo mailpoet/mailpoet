6a8dbc6123977fae8d58af26c752ef1b73421edd
Jan Lysý <jan.lysy@automattic.com>
2021-04-02 09:57:33 +0200

    Add StatisticsFormsRepository
    [MAILPOET-3032]

--- lib/DI/ContainerConfigurator.php
+++ lib/DI/ContainerConfigurator.php
@@ -229,6 +229,7 @@ class ContainerConfigurator implements IContainerConfigurator {
     $container->autowire(\MailPoet\Statistics\Track\Opens::class);
     $container->autowire(\MailPoet\Statistics\Track\WooCommercePurchases::class);
     $container->autowire(\MailPoet\Statistics\Track\Unsubscribes::class)->setPublic(true);
+    $container->autowire(\MailPoet\Statistics\StatisticsFormsRepository::class)->setPublic(true);
     $container->autowire(\MailPoet\Statistics\StatisticsUnsubscribesRepository::class);
     $container->autowire(\MailPoet\Statistics\StatisticsWooCommercePurchasesRepository::class)->setPublic(true);
     $container->autowire(\MailPoet\Router\Router::class)
