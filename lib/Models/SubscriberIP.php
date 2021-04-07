24264544ec9ace6feecbd6de7afd0d009efd438d
Jan Lysý <jan.lysy@automattic.com>
2021-04-07 08:13:40 +0200

    Mark old model SubscriberIP as deprecated
    [MAILPOET-3032]

--- lib/Models/SubscriberIP.php
+++ lib/Models/SubscriberIP.php
@@ -3,8 +3,30 @@
 namespace MailPoet\Models;
 
 /**
+ * @deprecated This model is deprecated. Use MailPoet\Subscribers\SubscriberIPsRepository and respective Doctrine entities instead.
+ * This class can be removed after 2021-10-07
  * @property string $ip
  */
 class SubscriberIP extends Model {
   public static $_table = MP_SUBSCRIBER_IPS_TABLE; // phpcs:ignore PSR2.Classes.PropertyDeclaration
+
+  /**
+   * @deprecated This is here for displaying the deprecation warning for properties.
+   */
+  public function __get($key) {
+    self::deprecationError('property "' . $key . '"');
+    return parent::__get($key);
+  }
+
+  /**
+   * @deprecated This is here for displaying the deprecation warning for static calls.
+   */
+  public static function __callStatic($name, $arguments) {
+    self::deprecationError($name);
+    return parent::__callStatic($name, $arguments);
+  }
+
+  private static function deprecationError($methodName) {
+    trigger_error('Calling ' . $methodName . ' is deprecated and will be removed. Use MailPoet\Subscribers\SubscriberIPsRepository and respective Doctrine entities instead.', E_USER_DEPRECATED);
+  }
 }
