8df05700cfa2b54421ca01bfad19b3d170163f0d
Rostislav Wolny <rosta@mailpoet.com>
2021-04-12 16:45:58 +0200

    Add extension to control init state of plugins in acceptance tests
    [MAILPOET-3471]

---
+++ tests/_support/PluginsExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MailPoet\TestsSupport;
+
+use Codeception\Events;
+use Codeception\Extension;
+
+class PluginsExtension extends Extension {
+
+  public static $events = [
+    Events::SUITE_BEFORE => 'setupInitialPluginsState',
+  ];
+
+  public function setupInitialPluginsState() {
+    exec('wp plugin deactivate woocommerce-subscriptions --allow-root');
+    exec('wp plugin deactivate woocommerce --allow-root');
+  }
+}
