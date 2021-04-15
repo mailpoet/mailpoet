5b7258bef037a711676f0fc2135ccb241d80999a
Rodrigo Primo <rodrigosprimo@gmail.com>
2021-04-15 13:46:45 -0300

    Add integration tests for the WooCommerce number of orders segment
    [MAILPOET-3228]

---
+++ tests/integration/Segments/DynamicSegments/Filters/WooCommerceNumberOfOrdersTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace MailPoet\Segments\DynamicSegments\Filters;
+
+use Carbon\Carbon;
+use MailPoet\Entities\DynamicSegmentFilterData;
+use MailPoet\Entities\DynamicSegmentFilterEntity;
+use MailPoet\Entities\SegmentEntity;
+use MailPoet\Entities\SubscriberEntity;
+
+class WooCommerceNumberOfOrdersTest extends \MailPoetTest {
+
+  private $numberOfOrders;
+
+  private $orders;
+
+  public function _before() {
+    $this->numberOfOrders = $this->diContainer->get(WooCommerceNumberOfOrders::class);
+    $this->cleanUp();
+
+    $userId1 = $this->tester->createWordPressUser('customer1@example.com', 'customer');
+    $userId2 = $this->tester->createWordPressUser('customer2@example.com', 'customer');
+    $userId3 = $this->tester->createWordPressUser('customer3@example.com', 'customer');
+
+    $this->orders[] = $this->createOrder(['user_id' => $userId1, 'post_date' => Carbon::now()->subDays(3)->toDateTimeString()]);
+    $this->orders[] = $this->createOrder(['user_id' => $userId2]);
+    $this->orders[] = $this->createOrder(['user_id' => $userId2]);
+    $this->orders[] = $this->createOrder(['user_id' => $userId3]);
+  }
+
+  public function testItGetsCustomersThatPlacedTwoOrdersInTheLastDay() {
+    $segmentFilter = $this->getSegmentFilter('=', 2, 1);
+    $queryBuilder = $this->numberOfOrders->apply($this->getQueryBuilder(), $segmentFilter);
+    $result = $queryBuilder->execute()->fetchAll();
+    expect(count($result))->equals(1);
+    $subscriber1 = $this->entityManager->find(SubscriberEntity::class, $result[0]['inner_subscriber_id']);
+    assert($subscriber1 instanceof SubscriberEntity);
+    expect($subscriber1->getEmail())->equals('customer2@example.com');
+  }
+
+  public function testItGetsCustomersThatPlacedZeroOrdersInTheLastDay() {
+    $segmentFilter = $this->getSegmentFilter('=', 0, 1);
+    $queryBuilder = $this->numberOfOrders->apply($this->getQueryBuilder(), $segmentFilter);
+    $result = $queryBuilder->execute()->fetchAll();
+    expect(count($result))->equals(1);
+    $subscriber1 = $this->entityManager->find(SubscriberEntity::class, $result[0]['inner_subscriber_id']);
+    assert($subscriber1 instanceof SubscriberEntity);
+    expect($subscriber1->getEmail())->equals('customer1@example.com');
+  }
+
+  public function testItGestCustomersThatPlacedAtLeastOneOrderInTheLastWeek() {
+    $segmentFilter = $this->getSegmentFilter('>', 0, 7);
+    $queryBuilder = $this->numberOfOrders->apply($this->getQueryBuilder(), $segmentFilter);
+    $result = $queryBuilder->execute()->fetchAll();
+    expect(count($result))->equals(3);
+  }
+
+  private function getQueryBuilder() {
+    $subscribersTable = $this->entityManager->getClassMetadata(SubscriberEntity::class)->getTableName();
+    return $this->entityManager
+      ->getConnection()
+      ->createQueryBuilder()
+      ->select("$subscribersTable.id as inner_subscriber_id")
+      ->from($subscribersTable);
+  }
+
+  private function getSegmentFilter($comparisonType, $ordersCount, $days): DynamicSegmentFilterEntity {
+    $data = new DynamicSegmentFilterData([
+      'segmentType' => DynamicSegmentFilterData::TYPE_WOOCOMMERCE,
+      'action' => 'numberOfOrders',
+      'number_of_orders_type' => $comparisonType,
+      'number_of_orders_count' => $ordersCount,
+      'number_of_orders_days' => $days,
+    ]);
+
+    $segment = new SegmentEntity('Dynamic Segment', SegmentEntity::TYPE_DYNAMIC, 'description');
+    $this->entityManager->persist($segment);
+    $dynamicSegmentFilter = new DynamicSegmentFilterEntity($segment, $data);
+    $this->entityManager->persist($dynamicSegmentFilter);
+    $segment->addDynamicFilter($dynamicSegmentFilter);
+    return $dynamicSegmentFilter;
+  }
+
+  private function createOrder($data) {
+    $orderData = [
+      'post_type' => 'shop_order',
+      'post_status' => 'wc-completed',
+      'post_date' => $data['post_date'] ?? '',
+      'meta_input' => [
+        '_customer_user' => $data['user_id'] ?? '',
+      ],
+    ];
+
+    return wp_insert_post($orderData);
+  }
+
+  public function _after() {
+    $this->cleanUp();
+  }
+
+  private function cleanUp() {
+    $this->truncateEntity(SegmentEntity::class);
+    $this->truncateEntity(SubscriberEntity::class);
+    $emails = ['customer1@example.com', 'customer2@example.com', 'customer3@example.com'];
+    foreach ($emails as $email) {
+      $this->tester->deleteWordPressUser($email);
+    }
+
+    if (!empty($this->orders)) {
+      foreach ($this->orders as $orderId) {
+        wp_delete_post($orderId);
+      }
+    }
+  }
+}
