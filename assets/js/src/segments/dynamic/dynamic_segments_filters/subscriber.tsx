11acc6146198bf89aaa3697fd6295183d86de3f5
Pavel Dohnal <pavel@mailpoet.com>
2021-04-22 13:48:45 +0200

    Add subscriber date to selection list
    [MAILPOET-3222]

--- assets/js/src/segments/dynamic/dynamic_segments_filters/subscriber.tsx
+++ assets/js/src/segments/dynamic/dynamic_segments_filters/subscriber.tsx
@@ -11,5 +11,5 @@ export function validateWordpressRole(formItems: WordpressRoleFormItem): boolean
 
 export const WordpressRoleSegmentOptions = [
   { value: 'wordpressRole', label: MailPoet.I18n.t('segmentsSubscriber'), group: SegmentTypes.WordPressRole },
-  { value: 'wordpressRole', label: MailPoet.I18n.t('segmentsSubscriber'), group: SegmentTypes.WordPressRole },
+  { value: 'subscribedDate', label: MailPoet.I18n.t('subscribedDate'), group: SegmentTypes.WordPressRole },
 ];
