e94aeeb3f863342e9b4dbeb2b3acd6a480392025
Pavel Dohnal <pavel@mailpoet.com>
2021-04-22 14:38:01 +0200

    Make naming better
    [MAILPOET-3222]

--- assets/js/src/segments/dynamic/dynamic_segments_filters/subscriber.tsx
+++ assets/js/src/segments/dynamic/dynamic_segments_filters/subscriber.tsx
@@ -5,11 +5,11 @@ import {
   SegmentTypes,
 } from '../types';
 
-export function validateWordpressRole(formItems: WordpressRoleFormItem): boolean {
+export function validateSubscriber(formItems: WordpressRoleFormItem): boolean {
   return !!formItems.wordpressRole;
 }
 
-export const WordpressRoleSegmentOptions = [
+export const SubscriberSegmentOptions = [
   { value: 'wordpressRole', label: MailPoet.I18n.t('segmentsSubscriber'), group: SegmentTypes.WordPressRole },
   { value: 'subscribedDate', label: MailPoet.I18n.t('subscribedDate'), group: SegmentTypes.WordPressRole },
 ];
