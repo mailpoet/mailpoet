39f8dfae5b934e060005c7104e75e19808990146
Rostislav Wolny <rosta@mailpoet.com>
2021-05-03 11:06:10 +0200

    Sanitize only specific block types in newsletter API data
    [MAILPOET-3538]

--- lib/Newsletter/ApiDataSanitizer.php
+++ lib/Newsletter/ApiDataSanitizer.php
@@ -6,8 +6,13 @@ class ApiDataSanitizer {
   /** @var NewsletterHtmlSanitizer */
   private $htmlSanitizer;
 
-  private const SANITIZE_KEY_WHITELIST = [
-    'text',
+  /**
+   * Configuration specifies which block types and properties within newsletters content blocks are sanitized
+   */
+  private const SANITIZATION_CONFIG = [
+    'header' => ['text'],
+    'footer' => ['text'],
+    'text' => ['text'],
   ];
 
   public function __construct(NewsletterHtmlSanitizer $htmlSanitizer) {
@@ -15,15 +20,36 @@ class ApiDataSanitizer {
   }
 
   public function sanitizeBody(array $body): array {
-    foreach ($body as $blockName => $block) {
-      if (is_array($block)) {
-        $sanitizedBlock = $this->sanitizeBody($block);
-      } else {
-        $sanitizedBlock = $block && in_array($blockName, self::SANITIZE_KEY_WHITELIST, true) ? $this->htmlSanitizer->sanitize($block) : $block;
-      }
-      $body[$blockName] = $sanitizedBlock;
+    if (isset($body['content']) && isset($body['content']['blocks']) && is_array($body['content']['blocks'])) {
+      $body['content']['blocks'] = $this->sanitizeBlocks($body['content']['blocks']);
     }
-
     return $body;
+  }
+
+  private function sanitizeBlocks(array $blocks): array {
+    foreach ($blocks as $key => $block) {
+      if (!is_array($block) || !isset($block['type'])) {
+        continue;
+      }
+      if (isset($block['blocks']) && is_array($block['blocks'])) {
+        $blocks[$key]['blocks'] = $this->sanitizeBlocks($block['blocks']);
+      } else {
+        $blocks[$key] = $this->sanitizeBlock($block);
+      }
+    };
+    return $blocks;
+  }
+
+  private function sanitizeBlock(array $block): array {
+    if (!isset(self::SANITIZATION_CONFIG[$block['type']])) {
+      return $block;
+    }
+    foreach (self::SANITIZATION_CONFIG[$block['type']] as $property) {
+      if (!isset($block[$property])) {
+        continue;
+      }
+      $block[$property] = $this->htmlSanitizer->sanitize($block[$property]);
+    }
+    return $block;
   }
 }
