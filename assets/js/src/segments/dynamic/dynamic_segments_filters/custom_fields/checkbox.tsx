1a4b6380f64cac4d265029eac4ed51bde0a88ebe
Pavel Dohnal <pavel@mailpoet.com>
2021-05-13 08:32:45 +0200

    Add a component for checkbox
    [MAILPOET-3220]

---
+++ assets/js/src/segments/dynamic/dynamic_segments_filters/custom_fields/checkbox.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import {
+  __,
+  assign,
+  compose,
+  get,
+  set,
+} from 'lodash/fp';
+
+import MailPoet from 'mailpoet';
+import Select from 'common/form/select/select';
+
+import {
+  WordpressRoleFormItem,
+  OnFilterChange,
+} from '../../types';
+
+interface Props {
+  onChange: OnFilterChange;
+  item: WordpressRoleFormItem;
+}
+
+export const Checkbox: React.FunctionComponent<Props> = ({ onChange, item }) => {
+  useEffect(() => {
+    if ((item.value !== '1') && (item.value !== '0')) {
+      onChange(
+        assign(item, { operator: 'equals', value: '1' })
+      );
+    }
+  }, [onChange, item]);
+
+  return (
+    <>
+      <div className="mailpoet-gap" />
+      <Select
+        key="select"
+        value={item.operator}
+        onChange={compose([
+          onChange,
+          assign(item),
+          set('value', __, {}),
+          get('value'),
+          get('target'),
+        ])}
+      >
+        <option value="1">{MailPoet.I18n.t('checked')}</option>
+        <option value="0">{MailPoet.I18n.t('unchecked')}</option>
+      </Select>
+    </>
+  );
+};
