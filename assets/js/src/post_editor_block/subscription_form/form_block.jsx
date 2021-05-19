7642aa648b471de539e579adbffbf71dd875e0fe
Pavel Dohnal <pavel@mailpoet.com>
2021-05-19 15:49:32 +0200

    Fix the post block in the latest Gutenberg
    In the latest Gutenberg right after fetch the data
    are sanitised using __experimentalSanitizeBlockAttributes
    that is trying to get block from teh store
    but our block was not registered in the store.
    
    [MAILPOET-3609]

--- assets/js/src/post_editor_block/subscription_form/form_block.jsx
+++ assets/js/src/post_editor_block/subscription_form/form_block.jsx
@@ -4,6 +4,19 @@ import Edit from './edit.jsx';
 const wp = window.wp;
 const { registerBlockType } = wp.blocks;
 
+registerBlockType('mailpoet/subscription-form-block-render', {
+  title: window.locale.subscriptionForm,
+  attributes: {
+    formId: {
+      type: 'number',
+      default: null,
+    },
+  },
+  supports: {
+    inserter: false,
+  },
+});
+
 registerBlockType('mailpoet/subscription-form-block', {
   title: window.locale.subscriptionForm,
   icon: Icon,
