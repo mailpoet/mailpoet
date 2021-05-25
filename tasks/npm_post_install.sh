38ce4a730818a236ececcfb4de1bc69817d40eb5
Jan Lysý <jan.lysy@automattic.com>
2021-05-25 09:27:15 +0200

    Add replacing invalid CSS from form-editor to postinstall script
    [MAILPOET-3634]

--- tasks/npm_post_install.sh
+++ tasks/npm_post_install.sh
@@ -16,3 +16,8 @@ git apply $INIT_CWD/tasks/patches/spectrum-replace-jquery-deprecated.patch || tr
 # Remove this when a patch is included in a package update.
 # Note: deferred.pipe() fix is not implemented yet, see https://github.com/guillaumepotier/Parsley.js/pull/1347
 sed -i -- "s/_focusedField\.focus()/_focusedField\.trigger('focus')/g" node_modules/parsleyjs/dist/parsley*.js
+
+# The package @wordpress/block-editor contains invalid CSS file. We need to fix it due to sass import.
+# Github issue: https://github.com/WordPress/gutenberg/issues/32181
+sed -i -- "s/box-shadow: 0 0 0 2px-1px #007cba;/box-shadow: 0 0 0 calc(2px-1px) #007cba;/g" node_modules/@wordpress/block-editor/build-style/style.css
+sed -i -- "s/box-shadow: 0 0 0 var(--wp-admin-border-width-focus)-1px var(--wp-admin-theme-color);/box-shadow: 0 0 0 calc(var(--wp-admin-border-width-focus)-1px) var(--wp-admin-theme-color);/g" node_modules/@wordpress/block-editor/build-style/style.css
