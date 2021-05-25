9e44ee74a636ae6a880960f2e45d0da4361c7419
Pavel Dohnal <pavel.dohnal@automattic.com>
2021-05-25 14:43:28 +0200

    Create a doctrine version for isSubscriberProcessed
    [MAILPOET-3267]

--- lib/Newsletter/Sending/ScheduledTaskSubscribersRepository.php
+++ lib/Newsletter/Sending/ScheduledTaskSubscribersRepository.php
@@ -1,9 +1,11 @@
-<?php
+<?php declare(strict_types = 1);
 
 namespace MailPoet\Newsletter\Sending;
 
 use MailPoet\Doctrine\Repository;
+use MailPoet\Entities\ScheduledTaskEntity;
 use MailPoet\Entities\ScheduledTaskSubscriberEntity;
+use MailPoet\Entities\SubscriberEntity;
 
 /**
  * @extends Repository<ScheduledTaskSubscriberEntity>
@@ -11,5 +13,19 @@ use MailPoet\Entities\ScheduledTaskSubscriberEntity;
 class ScheduledTaskSubscribersRepository extends Repository {
   protected function getEntityClassName() {
     return ScheduledTaskSubscriberEntity::class;
+  }
+
+  public function isSubscriberProcessed(ScheduledTaskEntity $task, SubscriberEntity $subscriber): bool {
+    $scheduledTaskSubscriber = $this
+      ->doctrineRepository
+      ->createQueryBuilder('sts')
+      ->andWhere('sts.processed = 1')
+      ->andWhere('sts.task = :task')
+      ->andWhere('sts.subscriber = :subscriber')
+      ->setParameter('subscriber', $subscriber)
+      ->setParameter('task', $task)
+      ->getQuery()
+      ->getOneOrNullResult();
+    return !empty($scheduledTaskSubscriber);
   }
 }
--- lib/Newsletter/Sending/SendingQueuesRepository.php
+++ lib/Newsletter/Sending/SendingQueuesRepository.php
@@ -1,15 +1,28 @@
-<?php
+<?php declare(strict_types = 1);
 
 namespace MailPoet\Newsletter\Sending;
 
 use MailPoet\Doctrine\Repository;
 use MailPoet\Entities\NewsletterEntity;
 use MailPoet\Entities\SendingQueueEntity;
+use MailPoet\Entities\SubscriberEntity;
+use MailPoetVendor\Doctrine\ORM\EntityManager;
 
 /**
  * @extends Repository<SendingQueueEntity>
  */
 class SendingQueuesRepository extends Repository {
+  /** @var ScheduledTaskSubscribersRepository */
+  private $scheduledTaskSubscribersRepository;
+
+  public function __construct(
+    EntityManager $entityManager,
+    ScheduledTaskSubscribersRepository $scheduledTaskSubscribersRepository
+  ) {
+    parent::__construct($entityManager);
+    $this->scheduledTaskSubscribersRepository = $scheduledTaskSubscribersRepository;
+  }
+
   protected function getEntityClassName() {
     return SendingQueueEntity::class;
   }
@@ -25,5 +38,11 @@ class SendingQueuesRepository extends Repository {
       ->setParameter('newsletter', $newsletter)
       ->getQuery()
       ->getOneOrNullResult();
+  }
+
+  public function isSubscriberProcessed(SendingQueueEntity $queue, SubscriberEntity $subscriber): bool {
+    $task = $queue->getTask();
+    if (is_null($task)) return false;
+    return $this->scheduledTaskSubscribersRepository->isSubscriberProcessed($task, $subscriber);
   }
 }
