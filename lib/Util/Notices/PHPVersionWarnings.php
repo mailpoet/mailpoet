b97cb1c3de11afa4d33d7cd2caaffbc98ec612e9
Pavel Dohnal <pavel.dohnal@automattic.com>
2021-06-02 15:02:12 +0200

    Remove upgrade offer
    [MAILPOET-3641]

--- lib/Util/Notices/PHPVersionWarnings.php
+++ lib/Util/Notices/PHPVersionWarnings.php
@@ -22,16 +22,11 @@ class PHPVersionWarnings {
   }
 
   public function display($phpVersion) {
-    $errorString = __('Your website is running on PHP %s which MailPoet does not officially support. Read our [link]simple PHP upgrade guide[/link] or let MailPoet’s support team upgrade it for you.', 'mailpoet');
+    $errorString = __('Your website is running on PHP %s which MailPoet does not officially support. Read our [link]simple PHP upgrade guide.[/link]', 'mailpoet');
     $errorString = sprintf($errorString, $phpVersion);
-    $getInTouchString = __('[link]Yes, I want MailPoet to help me upgrade for free[/link]', 'mailpoet');
     $error = Helpers::replaceLinkTags($errorString, 'https://kb.mailpoet.com/article/251-upgrading-the-websites-php-version', [
       'target' => '_blank',
       'data-beacon-article' => '5ad5f8982c7d3a0e93676666',
-    ]);
-    $error .= '<br><br>' . Helpers::replaceLinkTags($getInTouchString, 'https://www.mailpoet.com/let-us-handle-your-php-upgrade/', [
-      'target' => '_blank',
-      'class' => 'button',
     ]);
 
     $extraClasses = 'mailpoet-dismissible-notice is-dismissible';
