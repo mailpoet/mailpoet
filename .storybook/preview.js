87950ac60a6c432ade4fe961574db988264d5e6e
Ján Mikláš <neosinner@gmail.com>
2021-06-03 13:14:58 +0200

    Add WordPress button styles to Storybook.js
    [MAILPOET-3545]

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { withPerformance } from 'storybook-addon-performance';
+import '../assets/css/src/storybook/wordpress-buttons-5.7.2.css';
 import '../assets/dist/css/mailpoet-font.css';
 import '../assets/dist/css/mailpoet-plugin.css';
 import '../assets/dist/css/mailpoet-form-editor.css';
 
 addDecorator(withPerformance);
-addDecorator(story => <div id="wpbody"><div id="mailpoet-modal"></div>{story()}</div>);
+addDecorator(story => <div class="wp-core-ui" id="wpbody"><div id="mailpoet-modal"></div>{story()}</div>);
