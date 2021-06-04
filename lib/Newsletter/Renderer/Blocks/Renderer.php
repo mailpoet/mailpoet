b2704353c046d9ccdf3aace2a9239303de835328
Jan Lysý <jan.lysy@automattic.com>
2021-06-04 13:55:50 +0200

    Create block placeholder
    [MAILPOET-3638]

--- lib/Newsletter/Renderer/Blocks/Renderer.php
+++ lib/Newsletter/Renderer/Blocks/Renderer.php
@@ -34,6 +34,9 @@ class Renderer {
   /** @var Text */
   private $text;
 
+  /** @var Placeholder */
+  private $placeholder;
+
   public function __construct(
     AutomatedLatestContentBlock $ALC,
     Button $button,
@@ -43,7 +46,8 @@ class Renderer {
     Image $image,
     Social $social,
     Spacer $spacer,
-    Text $text
+    Text $text,
+    Placeholder $placeholder
   ) {
     $this->ALC = $ALC;
     $this->button = $button;
@@ -54,6 +58,7 @@ class Renderer {
     $this->social = $social;
     $this->spacer = $spacer;
     $this->text = $text;
+    $this->placeholder = $placeholder;
   }
 
   public function render(NewsletterEntity $newsletter, $data) {
@@ -110,6 +115,8 @@ class Renderer {
         return $this->spacer->render($block);
       case 'text':
         return $this->text->render($block);
+      case 'placeholder':
+        return $this->placeholder->render($block);
     }
     return '';
   }
