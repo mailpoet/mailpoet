94ac913aa35b3a102d26fd7d2a9b6c1c84b03800
Rostislav Wolny <rosta.wolny@automattic.com>
2021-06-18 11:09:50 +0200

    Update nesbot/carbon
    [MAILPOET-3658]

--- prefixer/fix-carbon.php
+++ prefixer/fix-carbon.php
@@ -5,31 +5,6 @@ set_error_handler(function ($severity, $message, $file, $line) {
   throw new ErrorException($message, 0, $severity, $file, $line);
 });
 
-$replacements = [
-  // Remove unnecessary class alias (fix for Symfony) that breaks PHPStan
-  [
-    'file' => '../vendor-prefixed/nesbot/carbon/src/Carbon/Traits/Localization.php',
-    'find' => [
-      "/if \\(!\\\\interface_exists\\('MailPoetVendor\\\\\\\\Symfony\\\\\\\\Component\\\\\\\\Translation\\\\\\\\TranslatorInterface'\\)\\) {[a-z_(',);\\s\\\\]+}/i",
-    ],
-    'replace' => [
-      '',
-    ],
-    'regular' => true,
-  ],
-];
-
-// Apply replacements
-foreach ($replacements as $singleFile) {
-  $data = file_get_contents($singleFile['file']);
-  if (isset($singleFile['regular']) && $singleFile['regular']) {
-    $data = preg_replace($singleFile['find'], $singleFile['replace'], $data);
-  } else {
-    $data = str_replace($singleFile['find'], $singleFile['replace'], $data);
-  }
-  file_put_contents($singleFile['file'], $data);
-}
-
 // Remove all locales except default english
 // We don't use Carbon translate capabilities so we keep only default english locale to reduce size of the library
 exec('find ' . __DIR__ . "/../vendor-prefixed/nesbot/carbon/src/Carbon/Lang -type f -not -name 'en.php' -delete");
