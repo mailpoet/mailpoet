363657f0beef9ddaf47e5a1f62883e03a468f4ae
Jan Lysý <jan.lysy@automattic.com>
2021-06-22 15:55:45 +0200

    Move error messages from editor to form component
    [MAILPOET-3649]

--- assets/js/src/segments/dynamic/editor.tsx
+++ assets/js/src/segments/dynamic/editor.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelect } from '@wordpress/data';
+import { useDispatch } from '@wordpress/data';
 import { useRouteMatch, Link } from 'react-router-dom';
 
 import MailPoet from 'mailpoet';
@@ -8,18 +8,12 @@ import Heading from 'common/typography/heading/heading';
 import HideScreenOptions from 'common/hide_screen_options/hide_screen_options';
 import { Form } from './form';
 
-import APIErrorsNotice from '../../notices/api_errors_notice';
 import { createStore } from './store/store';
 
 const Editor: React.FunctionComponent = () => {
   const match = useRouteMatch<{id: string}>();
 
   createStore();
-
-  const errors: string[] = useSelect(
-    (select) => select('mailpoet-dynamic-segments-form').getErrors(),
-    []
-  );
 
   const { pageLoaded } = useDispatch('mailpoet-dynamic-segments-form');
 
@@ -31,9 +25,6 @@ const Editor: React.FunctionComponent = () => {
     <>
       <Background color="#fff" />
       <HideScreenOptions />
-      {(errors.length > 0 && (
-        <APIErrorsNotice errors={errors.map((error) => ({ message: error }))} />
-      ))}
 
       <Heading level={1} className="mailpoet-title">
         <span>{MailPoet.I18n.t('formPageTitle')}</span>
