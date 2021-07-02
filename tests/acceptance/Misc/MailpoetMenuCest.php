bcf842f3ef7987088e480b1b0a7b4a5e9132c12e
Veljko <veljano@yahoo.com>
2021-07-02 09:04:54 +0200

    Fix flaky acceptance test mailpoet menus
    [MAILPOET-3677]

--- tests/acceptance/Misc/MailpoetMenuCest.php
+++ tests/acceptance/Misc/MailpoetMenuCest.php
@@ -11,21 +11,27 @@ class MailpoetMenuCest {
     $i->click('MailPoet');
 
     $i->click('Emails');
+    $i->waitForElement('[data-automation-id="tab-Newsletters"]');
     $i->seeInCurrentUrl('?page=mailpoet-newsletters');
 
     $i->click('Forms');
+    $i->waitForElement('[data-automation-id="create_new_form"]');
     $i->seeInCurrentUrl('?page=mailpoet-forms');
 
     $i->click('Subscribers');
+    $i->waitForElement('.mailpoet-subscribers-in-plan');
     $i->seeInCurrentUrl('?page=mailpoet-subscribers');
 
     $i->click('Lists');
+    $i->waitForElement('[data-automation-id="new-segment"]');
     $i->seeInCurrentUrl('?page=mailpoet-segments');
 
     $i->click('Settings');
+    $i->waitForElement('[data-automation-id="basic_settings_tab"]');
     $i->seeInCurrentUrl('?page=mailpoet-settings');
 
     $i->click('Help');
+    $i->waitForElement('.mailpoet-tab-content');
     $i->seeInCurrentUrl('?page=mailpoet-help');
   }
 }
