285b76162ccac2712623d47fd43dbd7b8843297f
Pavel Dohnal <pavel.dohnal@automattic.com>
2021-08-23 13:32:10 +0200

    Run the test only if premium plugin is not present
    [MAILPOET-3723]

--- tests/acceptance/Newsletters/StatsPageCest.php
+++ tests/acceptance/Newsletters/StatsPageCest.php
@@ -7,9 +7,6 @@ use MailPoet\Test\DataFactories\Newsletter;
 class StatsPageCest {
   public function statsPage(\AcceptanceTester $i, $scenario) {
     $i->wantTo('Open stats page of a sent newsletter');
-    if ($i->checkPluginIsActive('mailpoet-premium/mailpoet-premium.php')) {
-      $scenario->skip('We skip this test because the Mailpoet Premium plugin is active!');
-    }
 
     $newsletterTitle = 'Stats Page Test';
     (new Newsletter())->withSubject($newsletterTitle)
@@ -22,11 +19,15 @@ class StatsPageCest {
     $i->waitForText($newsletterTitle);
     $i->clickItemRowActionByItemName($newsletterTitle, 'Statistics');
     $i->waitForText($newsletterTitle);
-    $i->see('This is a Premium feature');
 
-    $href = $i->grabAttributeFrom('//a[span[text()="Sign Up for Free"]]', 'href');
-    expect($href)->stringContainsString('https://www.mailpoet.com/free-plan?utm_medium=stats&utm_campaign=signup&utm_source=plugin');
-    $href = $i->grabAttributeFrom('//a[text()="Learn more"]', 'href');
-    expect($href)->endsWith('page=mailpoet-premium');
+    if (!$i->checkPluginIsActive('mailpoet-premium/mailpoet-premium.php')) {
+      // the premium plugin is not active
+      $i->see('This is a Premium feature');
+
+      $href = $i->grabAttributeFrom('//a[span[text()="Sign Up for Free"]]', 'href');
+      expect($href)->stringContainsString('https://www.mailpoet.com/free-plan?utm_medium=stats&utm_campaign=signup&utm_source=plugin');
+      $href = $i->grabAttributeFrom('//a[text()="Learn more"]', 'href');
+      expect($href)->endsWith('page=mailpoet-premium');
+    }
   }
 }
