13c59396cc66470afeef5d2621a9f3465b8ef6cc
Rostislav Wolny <rosta.wolny@automattic.com>
2021-09-06 13:32:47 +0200

    Use cron worker scheduler service when scheduling Woo sync
    [MAILPOET-3726]

--- lib/API/JSON/v1/ImportExport.php
+++ lib/API/JSON/v1/ImportExport.php
@@ -7,12 +7,11 @@ use MailPoet\API\JSON\Endpoint as APIEndpoint;
 use MailPoet\API\JSON\Error as APIError;
 use MailPoet\API\JSON\ResponseBuilders\SegmentsResponseBuilder;
 use MailPoet\Config\AccessControl;
+use MailPoet\Cron\CronWorkerScheduler;
 use MailPoet\Cron\Workers\WooCommerceSync;
 use MailPoet\CustomFields\CustomFieldsRepository;
 use MailPoet\Doctrine\Validator\ValidationException;
-use MailPoet\Entities\ScheduledTaskEntity;
 use MailPoet\Newsletter\Options\NewsletterOptionsRepository;
-use MailPoet\Newsletter\Sending\ScheduledTasksRepository;
 use MailPoet\Segments\SegmentSaveController;
 use MailPoet\Segments\SegmentsRepository;
 use MailPoet\Segments\WP;
@@ -21,7 +20,6 @@ use MailPoet\Subscribers\ImportExport\Import\Import;
 use MailPoet\Subscribers\ImportExport\Import\MailChimp;
 use MailPoet\Subscribers\ImportExport\ImportExportRepository;
 use MailPoet\Subscribers\SubscribersRepository;
-use MailPoetVendor\Carbon\Carbon;
 
 class ImportExport extends APIEndpoint {
 
@@ -43,14 +41,14 @@ class ImportExport extends APIEndpoint {
   /** @var SubscribersRepository */
   private $subscriberRepository;
 
-  /** @var ScheduledTasksRepository */
-  private $scheduledTasksRepository;
-
   /** @var SegmentSaveController */
   private $segmentSavecontroller;
 
   /** @var SegmentsResponseBuilder */
   private $segmentsResponseBuilder;
+
+  /** @var CronWorkerScheduler */
+  private $cronWorkerScheduler;
 
   public $permissions = [
     'global' => AccessControl::PERMISSION_MANAGE_SUBSCRIBERS,
@@ -62,9 +60,9 @@ class ImportExport extends APIEndpoint {
     ImportExportRepository $importExportRepository,
     NewsletterOptionsRepository $newsletterOptionsRepository,
     SegmentsRepository $segmentsRepository,
-    ScheduledTasksRepository $scheduledTasksRepository,
     SegmentSaveController $segmentSavecontroller,
     SegmentsResponseBuilder $segmentsResponseBuilder,
+    CronWorkerScheduler $cronWorkerScheduler,
     SubscribersRepository $subscribersRepository
   ) {
     $this->wpSegment = $wpSegment;
@@ -73,8 +71,8 @@ class ImportExport extends APIEndpoint {
     $this->newsletterOptionsRepository = $newsletterOptionsRepository;
     $this->segmentsRepository = $segmentsRepository;
     $this->subscriberRepository = $subscribersRepository;
-    $this->scheduledTasksRepository = $scheduledTasksRepository;
     $this->segmentSavecontroller = $segmentSavecontroller;
+    $this->cronWorkerScheduler = $cronWorkerScheduler;
     $this->segmentsResponseBuilder = $segmentsResponseBuilder;
   }
 
@@ -156,18 +154,7 @@ class ImportExport extends APIEndpoint {
 
   public function setupWooCommerceInitialImport() {
     try {
-      $task = $this->scheduledTasksRepository->findScheduledOrRunningTask(WooCommerceSync::TASK_TYPE);
-      if (($task instanceof ScheduledTaskEntity) && $task->getStatus() === null) {
-        return $this->successResponse();
-      }
-      if (!($task instanceof ScheduledTaskEntity)) {
-        $task = new ScheduledTaskEntity();
-        $task->setType(WooCommerceSync::TASK_TYPE);
-        $task->setStatus(ScheduledTaskEntity::STATUS_SCHEDULED);
-        $this->scheduledTasksRepository->persist($task);
-      }
-      $task->setScheduledAt(Carbon::createFromTimestamp((int)current_time('timestamp')));
-      $this->scheduledTasksRepository->flush();
+      $this->cronWorkerScheduler->scheduleImmediatelyIfNotRunning(WooCommerceSync::TASK_TYPE);
       return $this->successResponse();
     } catch (\Exception $e) {
       return $this->errorResponse([
