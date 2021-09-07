ad4b0c47aa1674bba267eda2c638e18b46fac7e1
Pavel Dohnal <pavel@mailpoet.com>
2021-09-07 10:47:40 +0200

    Save bounces statistics
    [MAILPOET-3765]

---
+++ lib/Statistics/StatisticsBouncesRepository.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace MailPoet\Statistics;
+
+use MailPoet\Doctrine\Repository;
+use MailPoet\Entities\StatisticsBounceEntity;
+
+/**
+ * @extends Repository<StatisticsBounceEntity>
+ */
+class StatisticsBouncesRepository extends Repository {
+  protected function getEntityClassName(): string {
+    return StatisticsBounceEntity::class;
+  }
+}
