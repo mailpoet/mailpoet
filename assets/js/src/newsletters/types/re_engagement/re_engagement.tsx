e303df68675e407f8d80f98d3421096bb2e97da7
Pavel Dohnal <pavel@mailpoet.com>
2021-09-09 13:57:07 +0200

    Add title to the re-engagement email
    [MAILPOET-3763]

--- assets/js/src/newsletters/types/re_engagement/re_engagement.tsx
+++ assets/js/src/newsletters/types/re_engagement/re_engagement.tsx
@@ -5,6 +5,7 @@ import Background from 'common/background/background';
 import { Grid } from 'common/grid';
 import Button from 'common/button/button';
 import ListingHeadingStepsRoute from '../../listings/heading_steps_route';
+import Heading from '../../../common/typography/heading/heading';
 
 export function NewsletterTypeReEngagement(): JSX.Element {
   return (
@@ -14,6 +15,8 @@ export function NewsletterTypeReEngagement(): JSX.Element {
       <ListingHeadingStepsRoute emailType="re_engagement" automationId="re_engagement_heading_creation_heading" />
 
       <Grid.Column align="center" className="mailpoet-schedule-email">
+        <Heading level={4}>{MailPoet.I18n.t('selectEventToSendReEngagementEmail')}</Heading>
+
         <Button
           isFullWidth
           onClick={() => 5}
