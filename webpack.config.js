45ad28909fd07dfa1df869940af1c0ec9295e867
Rostislav Wolny <rosta.wolny@automattic.com>
2021-09-10 12:59:49 +0200

    Fix deprecation warning about hashes in JS production build
    [MAILPOET-3214]

--- webpack.config.js
+++ webpack.config.js
@@ -33,8 +33,8 @@ const baseConfig = {
   output: {
     publicPath: '', // This is needed to have correct names in WebpackManifestPlugin
     path: path.join(__dirname, 'assets/dist/js'),
-    filename: (PRODUCTION_ENV) ? '[name].[hash:8].js' : '[name].js',
-    chunkFilename: (PRODUCTION_ENV) ? '[name].[hash:8].chunk.js' : '[name].chunk.js',
+    filename: (PRODUCTION_ENV) ? '[name].[fullhash:8].js' : '[name].js',
+    chunkFilename: (PRODUCTION_ENV) ? '[name].[fullhash:8].chunk.js' : '[name].chunk.js',
   },
   resolve: {
     modules: [
