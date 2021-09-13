6ac7015b5c1c5be06819c076df11e9be318cbac4
Pavel Dohnal <pavel@mailpoet.com>
2021-09-13 12:49:54 +0200

    Save the re-engagement email
    [MAILPOET-3763]

--- assets/js/src/newsletters/types/re_engagement/re_engagement.tsx
+++ assets/js/src/newsletters/types/re_engagement/re_engagement.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { __, assoc, compose } from 'lodash/fp';
+import { useHistory } from 'react-router-dom';
 
 import MailPoet from 'mailpoet';
 import Background from 'common/background/background';
 import { Grid } from 'common/grid';
 import Button from 'common/button/button';
-import ListingHeadingStepsRoute from '../../listings/heading_steps_route';
+import APIErrorsNotice from 'notices/api_errors_notice';
+
 import { Scheduling } from './scheduling';
+import ListingHeadingStepsRoute from '../../listings/heading_steps_route';
 
 interface ReengagementWindow extends Window {
   settings: {
@@ -23,9 +26,38 @@ export function NewsletterTypeReEngagement(): JSX.Element {
     ).toString(),
     afterTimeType: 'months',
   });
+  const [errors, setErrors] = useState([]);
+  const history = useHistory();
+
+  function showTemplateSelection(newsletterId: string) {
+    history.push(`/template/${newsletterId}`);
+  }
+
+  function handleNext() {
+    setErrors([]);
+    MailPoet.Ajax.post({
+      api_version: MailPoet.apiVersion,
+      endpoint: 'newsletters',
+      action: 'create',
+      data: {
+        type: 're_engagement',
+        subject: MailPoet.I18n.t('draftNewsletterTitle'),
+        options,
+      },
+    }).done((response) => {
+      showTemplateSelection(response.data.id);
+    }).fail((response) => {
+      if (response.errors) {
+        setErrors(response.errors);
+      }
+    });
+  }
+
   return (
     <div>
       <Background color="#fff" />
+
+      {errors && (<APIErrorsNotice errors={errors} />)}
 
       <ListingHeadingStepsRoute emailType="re_engagement" automationId="re_engagement_heading_creation_heading" />
 
@@ -42,7 +74,7 @@ export function NewsletterTypeReEngagement(): JSX.Element {
 
         <Button
           isFullWidth
-          onClick={() => 5}
+          onClick={handleNext}
           type="button"
         >
           {MailPoet.I18n.t('next')}
