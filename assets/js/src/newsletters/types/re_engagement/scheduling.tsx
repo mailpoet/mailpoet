d351a38f2775e7c052bdfc330f9659313bc186f0
Pavel Dohnal <pavel@mailpoet.com>
2021-09-13 09:57:29 +0200

    Fix the UI
    [MAILPOET-3763]

--- assets/js/src/newsletters/types/re_engagement/scheduling.tsx
+++ assets/js/src/newsletters/types/re_engagement/scheduling.tsx
@@ -3,6 +3,7 @@ import MailPoet from 'mailpoet';
 import Heading from 'common/typography/heading/heading';
 import Input from 'common/form/input/input';
 import Select from 'common/form/select/select';
+import { Grid } from 'common/grid';
 
 interface Props {
   afterTimeNumber: string;
@@ -20,7 +21,7 @@ export function Scheduling({
   return (
     <>
       <Heading level={4}>{MailPoet.I18n.t('selectEventToSendReEngagementEmail')}</Heading>
-      <div className="mailpoet-grid-column mailpoet-flex mailpoet-re-engagement-scheduling">
+      <Grid.CenteredRow className="mailpoet-re-engagement-scheduling">
         <p>
           {MailPoet.I18n.t('reEngagementTextPre')}
         </p>
@@ -28,17 +29,14 @@ export function Scheduling({
           type="text"
           placeholder={MailPoet.I18n.t('reEngagementAterTimeNumberPlaceholder')}
           value={afterTimeNumber}
-          dimension="small"
         />
         <Select
-          isMinWidth
           value={afterTimeType}
-          dimension="small"
         >
           <option value="weeks">weeks</option>
           <option value="months">months</option>
         </Select>
-      </div>
+      </Grid.CenteredRow>
       {
         (inactiveSubscribersPeriod <= daysSelected) && (
           <div>note</div>
