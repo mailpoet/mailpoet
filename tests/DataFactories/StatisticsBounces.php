73347d6a6aa652253a516b7a8cf830edcfd4ce07
Jan Lysý <jan.lysy@automattic.com>
2021-09-15 16:13:50 +0200

    Create data factory for StatisticsBouncesFactory
    [PREMIUM-154]

---
+++ tests/DataFactories/StatisticsBounces.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace MailPoet\Test\DataFactories;
+
+use MailPoet\DI\ContainerWrapper;
+use MailPoet\Entities\NewsletterEntity;
+use MailPoet\Entities\SendingQueueEntity;
+use MailPoet\Entities\StatisticsBounceEntity;
+use MailPoet\Entities\SubscriberEntity;
+use MailPoetVendor\Doctrine\ORM\EntityManager;
+
+class StatisticsBounces {
+  protected $data;
+
+  /** @var NewsletterEntity */
+  private $newsletter;
+
+  /** @var SubscriberEntity */
+  private $subscriber;
+
+  public function __construct(NewsletterEntity $newsletter, SubscriberEntity $subscriber) {
+    $this->newsletter = $newsletter;
+    $this->subscriber = $subscriber;
+  }
+
+  public function create(): StatisticsBounceEntity {
+    $entityManager = ContainerWrapper::getInstance()->get(EntityManager::class);
+    $queue = $this->newsletter->getLatestQueue();
+    assert($queue instanceof SendingQueueEntity);
+    $entity = new StatisticsBounceEntity(
+      $this->newsletter,
+      $queue,
+      $this->subscriber
+    );
+    $entityManager->persist($entity);
+    $entityManager->flush();
+    return $entity;
+  }
+}
