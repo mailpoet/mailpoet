f3f01e12fe265baedc1baee3ee05fbd31e8cd058
Rostislav Wolny <rosta.wolny@automattic.com>
2021-09-16 09:14:35 +0200

    Remove unnecessary post-instal fix for invalid CSS
    The issue was already fixed in the @wordpress/block-editor
    [MAILPOET-3214]

--- tasks/npm_post_install.sh
+++ tasks/npm_post_install.sh
@@ -17,11 +17,6 @@ git apply $INIT_CWD/tasks/patches/spectrum-replace-jquery-deprecated.patch || tr
 # Note: deferred.pipe() fix is not implemented yet, see https://github.com/guillaumepotier/Parsley.js/pull/1347
 sed -i -- "s/_focusedField\.focus()/_focusedField\.trigger('focus')/g" node_modules/parsleyjs/dist/parsley*.js
 
-# The package @wordpress/block-editor contains invalid CSS file. We need to fix it due to sass import.
-# Github issue: https://github.com/WordPress/gutenberg/issues/32181
-sed -i -- "s/box-shadow: 0 0 0 2px-1px #007cba;/box-shadow: 0 0 0 calc(2px-1px) #007cba;/g" node_modules/@wordpress/block-editor/build-style/style.css
-sed -i -- "s/box-shadow: 0 0 0 var(--wp-admin-border-width-focus)-1px var(--wp-admin-theme-color);/box-shadow: 0 0 0 calc(var(--wp-admin-border-width-focus)-1px) var(--wp-admin-theme-color);/g" node_modules/@wordpress/block-editor/build-style/style.css
-
 # Fix strict mode issues in Backbone.Supermodel
 sed -i -- "s/lastKeyIndex = keyPath.length-1;/var lastKeyIndex = keyPath.length-1;/g" node_modules/backbone.supermodel/build/backbone.supermodel.js
 sed -i -- "s/key = keyPath\[i\];/var key = keyPath\[i\];/g" node_modules/backbone.supermodel/build/backbone.supermodel.js
