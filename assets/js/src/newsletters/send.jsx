48a0989a1ac41035447bc3e448fe502a467416da
Pavel Dohnal <pavel@mailpoet.com>
2021-10-18 15:58:55 +0200

    Make sure the tooltip is not cut in half
    [MAILPOET-3857]

--- assets/js/src/newsletters/send.jsx
+++ assets/js/src/newsletters/send.jsx
@@ -547,7 +547,7 @@ class NewsletterSend extends React.Component {
             }
             { this.state.validationError !== undefined && (
               <HelpTooltip
-                tooltip={this.state.validationError}
+                tooltip={(<div>{this.state.validationError}</div>)}
                 tooltipId={this.state.validationError}
               />
             ) }
