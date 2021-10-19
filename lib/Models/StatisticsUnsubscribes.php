92622f101241d119ac2e54053cf0f347b028b883
wxa <wxa@users.noreply.github.com>
2021-10-19 16:10:38 +0300

    Mark StatisticsUnsubscribes model as deprecated
    [MAILPOET-3819]

--- lib/Models/StatisticsUnsubscribes.php
+++ lib/Models/StatisticsUnsubscribes.php
@@ -3,6 +3,11 @@
 namespace MailPoet\Models;
 
 /**
+ * @deprecated This model is deprecated. Use MailPoet\Statistics\StatisticsUnsubscribesRepository and respective Doctrine entities instead.
+ * This class can be removed after 2022-03-25
+ */
+
+/**
  * @property int $newsletterId
  * @property int $subscriberId
  * @property int $queueId
@@ -11,6 +16,7 @@ class StatisticsUnsubscribes extends Model {
   public static $_table = MP_STATISTICS_UNSUBSCRIBES_TABLE; // phpcs:ignore PSR2.Classes.PropertyDeclaration
 
   public static function getOrCreate($subscriberId, $newsletterId, $queueId) {
+    self::deprecationError(__FUNCTION__);
     $statistics = self::where('subscriber_id', $subscriberId)
       ->where('newsletter_id', $newsletterId)
       ->where('queue_id', $queueId)
@@ -23,5 +29,25 @@ class StatisticsUnsubscribes extends Model {
       $statistics->save();
     }
     return $statistics;
+  }
+
+  /**
+   * @deprecated This is here for displaying the deprecation warning for properties.
+   */
+  public function __get($key) {
+    self::deprecationError('property "' . $key . '"');
+    return parent::__get($key);
+  }
+
+  /**
+   * @deprecated This is here for displaying the deprecation warning for static calls.
+   */
+  public static function __callStatic($name, $arguments) {
+    self::deprecationError($name);
+    return parent::__callStatic($name, $arguments);
+  }
+
+  private static function deprecationError($methodName) {
+    trigger_error('Calling ' . $methodName . ' is deprecated and will be removed. Use MailPoet\Statistics\StatisticsUnsubscribesRepository and respective Doctrine entity instead.', E_USER_DEPRECATED);
   }
 }
