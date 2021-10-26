5284100eb19e04b092e5c6bc74830739eaf1978c
Jan Lysý <jan.lysy@automattic.com>
2021-10-26 14:02:11 +0200

    Add template category Re-engagement Emails
    [MAILPOET-3764]

--- assets/js/src/newsletters/templates.jsx
+++ assets/js/src/newsletters/templates.jsx
@@ -75,6 +75,8 @@ class NewsletterTemplates extends React.Component {
   }
 
   componentDidMount() {
+    this.addReEngagementCategory();
+
     MailPoet.Ajax.get({
       api_version: window.mailpoet_api_version,
       endpoint: 'newsletterTemplates',
@@ -102,6 +104,21 @@ class NewsletterTemplates extends React.Component {
     }).always(() => {
       this.selectInitialTab();
     });
+  }
+
+  // We want temporarily to allow re-engagement emails only when the feature is active
+  addReEngagementCategory() {
+    if (this.context.features.isSupported('re-engagement-email')) {
+      const notificationIndex = templatesCategories.map((category) => category.name).indexOf('notification');
+      const categoriesTmp = templatesCategories.splice(notificationIndex + 1);
+      templatesCategories.push(
+        {
+          name: 're_engagement',
+          label: MailPoet.I18n.t('tabReEngagementTitle'),
+        },
+        ...categoriesTmp
+      );
+    }
   }
 
   addTemplate(template) {
