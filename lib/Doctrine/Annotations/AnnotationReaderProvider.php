86e938b46eb20ee40da8387e29bbda15cbbe58b7
Rostislav Wolny <rosta.wolny@automattic.com>
2021-10-26 15:35:05 +0200

    Provide PSR array cache and switch annotation reader to it
    The annotation reader was using CachedReader which has been deprecated so we need to switch to PsrCachedReader
    Since 2.0 doctrine/cache no longer provides implementations of caches so I also needed to implement a simple PSR array cache.
    [MAILPOET-3892]

--- lib/Doctrine/Annotations/AnnotationReaderProvider.php
+++ lib/Doctrine/Annotations/AnnotationReaderProvider.php
@@ -2,29 +2,28 @@
 
 namespace MailPoet\Doctrine\Annotations;
 
+use MailPoet\Doctrine\PSRArrayCache;
 use MailPoetVendor\Doctrine\Common\Annotations\AnnotationReader;
 use MailPoetVendor\Doctrine\Common\Annotations\AnnotationRegistry;
-use MailPoetVendor\Doctrine\Common\Annotations\CachedReader;
-use MailPoetVendor\Doctrine\Common\Cache\ArrayCache;
+use MailPoetVendor\Doctrine\Common\Annotations\PsrCachedReader;
 
 class AnnotationReaderProvider {
-  /** @var CachedReader */
+  /** @var PsrCachedReader */
   private $annotationReader;
 
   public function __construct() {
     // register annotation reader if doctrine/annotations package is installed
     // (i.e. in dev environment, on production metadata is dumped in the build)
-    $readAnnotations = class_exists(CachedReader::class) && class_exists(AnnotationReader::class);
+    $readAnnotations = class_exists(PsrCachedReader::class) && class_exists(AnnotationReader::class);
     if ($readAnnotations) {
       // autoload all annotation classes using registered loaders (Composer)
       // (needed for Symfony\Validator constraint annotations to be loaded)
       AnnotationRegistry::registerLoader('class_exists');
-      $this->annotationReader = new CachedReader(new AnnotationReader(), new ArrayCache());
+      $this->annotationReader = new PsrCachedReader(new AnnotationReader(), new PSRArrayCache());
     }
   }
 
-  /** @return CachedReader|null */
-  public function getAnnotationReader() {
+  public function getAnnotationReader(): ?PsrCachedReader {
     return $this->annotationReader;
   }
 }
