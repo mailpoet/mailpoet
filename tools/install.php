6824f046053ad40c58e40d71353580b38c18d889
Rostislav Wolny <rosta.wolny@automattic.com>
2021-10-27 13:42:48 +0200

    Update tools (composer and tracy)
    [MAILPOET-3889]

--- tools/install.php
+++ tools/install.php
@@ -1,9 +1,9 @@
 <?php
 
 $tools = [
-  'https://github.com/composer/composer/releases/download/2.1.5/composer.phar' => 'composer.phar',
+  'https://github.com/composer/composer/releases/download/2.1.9/composer.phar' => 'composer.phar',
   'https://github.com/humbug/php-scoper/releases/download/0.15.0/php-scoper.phar' => 'php-scoper.phar',
-  'https://github.com/nette/tracy/releases/download/v2.7.8/tracy.phar' => 'tracy.phar',
+  'https://github.com/nette/tracy/releases/download/v2.8.7/tracy.phar' => 'tracy.phar',
 ];
 // ensure installation in dev-mode only
 $isDevMode = (bool)getenv('COMPOSER_DEV_MODE');
