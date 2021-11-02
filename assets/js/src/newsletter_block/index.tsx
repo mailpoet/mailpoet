0f5b7d0ac7227add07142b903995a31b4c5a8203
Mike Jolley <mike.jolley@me.com>
2021-11-02 18:01:59 +0000

    Checkbox implementation
    [MAILPOET-3920]

--- assets/js/src/newsletter_block/index.tsx
+++ assets/js/src/newsletter_block/index.tsx
@@ -2,19 +2,24 @@
  * External dependencies
  */
 /* eslint-disable react/react-in-jsx-scope */
-import { Icon, button } from '@wordpress/icons';
+import { Icon, megaphone } from '@wordpress/icons';
 import { registerBlockType } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
  */
 import { Edit, Save } from './edit';
+import attributes from './attributes';
 import metadata from './block.json';
 
 registerBlockType(metadata, {
   icon: {
-    src: <Icon icon={button} />,
+    src: <Icon icon={megaphone} />,
     foreground: '#7f54b3',
+  },
+  attributes: {
+    ...metadata.attributes,
+    ...attributes,
   },
   edit: Edit,
   save: Save,
--- lib/PostEditorBlocks/NewsletterBlock.php
+++ lib/PostEditorBlocks/NewsletterBlock.php
@@ -4,15 +4,26 @@ namespace MailPoet\PostEditorBlocks;
 
 use MailPoet\Config\Env;
 use MailPoet\WP\Functions as WPFunctions;
+use MailPoet\Settings\SettingsController;
+use Automattic\WooCommerce\Blocks\Assets\AssetDataRegistry;
+use Automattic\WooCommerce\Blocks\Package;
+use Automattic\WooCommerce\Blocks\Domain\Services\ExtendRestApi;
+use Automattic\WooCommerce\Blocks\StoreApi\Schemas\CheckoutSchema;
 
+// @todo Disable if blocks is inactive
 class NewsletterBlock {
+   /** @var SettingsController */
+  private $settings;
+
   /** @var WPFunctions */
   private $wp;
 
   public function __construct(
-    WPFunctions $wp
+    WPFunctions $wp,
+    SettingsController $settings
   ) {
     $this->wp = $wp;
+    $this->settings = $settings;
   }
 
   public function init() {
@@ -21,10 +32,62 @@ class NewsletterBlock {
       '__experimental_woocommerce_blocks_add_data_attributes_to_block',
       [$this, 'addDataAttributesToBlock']
     );
+    $this->wp->addAction(
+      '__experimental_woocommerce_blocks_checkout_update_order_from_request',
+      [$this, 'process_checkout_block_optin']
+    );
+    $this->addScriptData();
+    $this->extendRestApi();
   }
 
   public function addDataAttributesToBlock( array $blocks ) {
     $blocks[] = 'mailpoet/newsletter-block';
     return $blocks;
+  }
+
+  public function addScriptData() {
+    $data_registry = Package::container()->get(
+      AssetDataRegistry::class
+    );
+    $data_registry->add( 'mailpoet_data', array(
+      'newsletterDefaultText' => $this->settings->get('woocommerce.optin_on_checkout.message', ''),
+			'newsletterEnabled'     => $this->settings->get('woocommerce.optin_on_checkout.enabled', false),
+    ) );
+  }
+
+  public function extendRestApi() {
+    $extend = Package::container()->get(
+			ExtendRestApi::class
+		);
+		$extend->register_endpoint_data(
+			array(
+				'endpoint'        => CheckoutSchema::IDENTIFIER,
+				'namespace'       => 'mailpoet',
+				'schema_callback' => function() {
+					return array(
+						'newsletter-opt-in' => array(
+							'description' => __( 'Subscribe to newsletter opt-in.', 'woo-gutenberg-products-block' ),
+							'type'        => 'boolean',
+						),
+					);
+				},
+			)
+		);
+  }
+
+  public function process_checkout_block_optin( \WC_Order $order, array $request ) {
+    if ( ! $this->settings->get('woocommerce.optin_on_checkout.enabled', false) ) {
+			return;
+		}
+
+		if ( ! $order ) {
+			return;
+		}
+
+		if ( ! isset( $request['extensions']['mailpoet'][ 'optin' ] ) ) {
+			return;
+		}
+
+    // @todo do optin
   }
 }
