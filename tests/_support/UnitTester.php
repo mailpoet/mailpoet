7dea96cb906bbe3d10361e3e283002330d06314b
oluwaseun Olorunsola <triple0t1@gmail.com>
2021-11-04 11:36:42 +0100

    Restore deleted UnitTester.php file due to lint check failing on Circleci
    We discovered that Codeception will regenerate the file during unit testing. Unfortunately, the regenerated file fails lint check which prevents the build from passing on Circleci
    
    We restored the file and left it empty
    
    [MAILPOET-3822]

---
+++ tests/_support/UnitTester.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = null)
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+// phpcs:ignore PSR1.Classes.ClassDeclaration
+class UnitTester extends \Codeception\Actor {
+  use _generated\UnitTesterActions;
+
+  // Can't delete this file because Codeception will regenerate it during unit testing
+  // leaving it empty instead
+}
