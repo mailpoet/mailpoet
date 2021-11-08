74ef883547566823ad544ff9e78589ddf1b5f001
wxa <wxa@users.noreply.github.com>
2021-11-08 10:26:51 +0300

    Fix 'Enable auto-updates' link missing when Premium is up-to-date
    [MAILPOET-3921]

--- tests/integration/Config/UpdaterTest.php
+++ tests/integration/Config/UpdaterTest.php
@@ -74,6 +74,42 @@ class UpdaterTest extends \MailPoetTest {
     expect($result->response[$this->pluginName]->package)->notEmpty();
   }
 
+  public function testItSetsNoupdateKeyIfNoUpdateAvailable() {
+    $updateTransient = new \stdClass;
+    $updateTransient->last_checked = time(); // phpcs:ignore Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps
+    $updater = Stub::construct(
+      $this->updater,
+      [
+        $this->pluginName,
+        $this->slug,
+        $this->version,
+      ],
+      [
+        'getLatestVersion' => function () {
+          return (object)[
+            'id' => 76630,
+            'slug' => $this->slug,
+            'plugin' => $this->pluginName,
+            'new_version' => $this->version,
+            'url' => 'http://www.mailpoet.com/wordpress-newsletter-plugin-premium/',
+            'package' => home_url() . '/wp-content/uploads/mailpoet-premium.zip',
+          ];
+        },
+      ],
+      $this
+    );
+    $result = $updater->checkForUpdate($updateTransient);
+    expect($result->last_checked)->greaterOrEquals($updateTransient->last_checked); // phpcs:ignore Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps
+    expect($result->checked[$this->pluginName])->equals($this->version);
+    expect($result->no_update[$this->pluginName]->slug)->equals($this->slug); // phpcs:ignore Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps
+    expect($result->no_update[$this->pluginName]->plugin)->equals($this->pluginName); // phpcs:ignore Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps
+    expect(version_compare(
+      $this->version,
+      $result->no_update[$this->pluginName]->new_version, // phpcs:ignore Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps
+      '='
+    ))->true();
+  }
+
   public function testItReturnsObjectIfPassedNonObjectWhenCheckingForUpdates() {
     $result = $this->updater->checkForUpdate(null);
     expect($result instanceof \stdClass)->true();
