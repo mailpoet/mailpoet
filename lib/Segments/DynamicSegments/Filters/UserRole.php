c2e841fd3df8f7b0c04acfba9de09ee0e511f601
Pavel Dohnal <pavel.dohnal@automattic.com>
2021-12-02 09:57:20 +0100

    Make role always array
    [MAILPOET-3955]

--- lib/Segments/DynamicSegments/Filters/UserRole.php
+++ lib/Segments/DynamicSegments/Filters/UserRole.php
@@ -2,6 +2,7 @@
 
 namespace MailPoet\Segments\DynamicSegments\Filters;
 
+use MailPoet\Entities\DynamicSegmentFilterData;
 use MailPoet\Entities\DynamicSegmentFilterEntity;
 use MailPoet\Entities\SubscriberEntity;
 use MailPoet\Segments\DynamicSegments\Exceptions\InvalidFilterException;
@@ -25,8 +26,16 @@ class UserRole implements Filter {
     global $wpdb;
     $filterData = $filter->getFilterData();
     $role = $filterData->getParam('wordpressRole');
+    $operator = $filterData->getParam('operator');
     if (!$role) {
       throw new InvalidFilterException('Missing role', InvalidFilterException::MISSING_ROLE);
+    }
+    if (!is_array($role)) {
+      // compatibility with the older segment before multiple roles were added
+      $role = [$role];
+    }
+    if (!$operator) {
+      $operator = DynamicSegmentFilterData::OPERATOR_ANY;
     }
 
     $subscribersTable = $this->entityManager->getClassMetadata(SubscriberEntity::class)->getTableName();
@@ -34,6 +43,7 @@ class UserRole implements Filter {
     return $queryBuilder->join($subscribersTable, $wpdb->users, 'wpusers', "$subscribersTable.wp_user_id = wpusers.id")
       ->join('wpusers', $wpdb->usermeta, 'wpusermeta', 'wpusers.id = wpusermeta.user_id')
       ->andWhere("wpusermeta.meta_key = '{$wpdb->prefix}capabilities' AND wpusermeta.meta_value LIKE :role" . $parameterSuffix)
-      ->setParameter(':role' . $parameterSuffix, '%"' . $role . '"%');
+      ->setParameter(':role' . $parameterSuffix, '%"' . $role[0] . '"%');
   }
+
 }
