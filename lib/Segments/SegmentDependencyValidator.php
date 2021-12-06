657831f0fe298e38e00cd48b1264f1ef0d71b2e4
wxa <wxa@users.noreply.github.com>
2021-12-06 18:08:31 +0300

    Require Premium plugin for multi-condition segments
    [MAILPOET-3929]

--- lib/Segments/SegmentDependencyValidator.php
+++ lib/Segments/SegmentDependencyValidator.php
@@ -5,9 +5,16 @@ namespace MailPoet\Segments;
 use MailPoet\Entities\DynamicSegmentFilterData;
 use MailPoet\Entities\DynamicSegmentFilterEntity;
 use MailPoet\Entities\SegmentEntity;
+use MailPoet\Util\License\Features\Subscribers as SubscribersFeature;
 use MailPoet\WP\Functions as WPFunctions;
+use MailPoetVendor\Doctrine\Common\Collections\Collection;
 
 class SegmentDependencyValidator {
+  private const MAILPOET_PREMIUM_PLUGIN = [
+    'id' => 'mailpoet-premium/mailpoet-premium.php',
+    'name' => 'MailPoet Premium',
+  ];
+
   private const WOOCOMMERCE_PLUGIN = [
     'id' => 'woocommerce/woocommerce.php',
     'name' => 'WooCommerce',
@@ -28,12 +35,17 @@ class SegmentDependencyValidator {
     ],
   ];
 
+  /** @var SubscribersFeature */
+  private $subscribersFeature;
+
   /** @var WPFunctions */
   private $wp;
 
   public function __construct(
+    SubscribersFeature $subscribersFeature,
     WPFunctions $wp
   ) {
+    $this->subscribersFeature = $subscribersFeature;
     $this->wp = $wp;
   }
 
@@ -41,8 +53,9 @@ class SegmentDependencyValidator {
    * @return string[]
    */
   public function getMissingPluginsBySegment(SegmentEntity $segment): array {
-    $missingPluginNames = [];
-    foreach ($segment->getDynamicFilters() as $dynamicFilter) {
+    $dynamicFilters = $segment->getDynamicFilters();
+    $missingPluginNames = $this->getMissingPluginsByAllFilters($dynamicFilters);
+    foreach ($dynamicFilters as $dynamicFilter) {
       $missingPlugins = $this->getMissingPluginsByFilter($dynamicFilter);
       if (!$missingPlugins) {
         continue;
@@ -52,6 +65,21 @@ class SegmentDependencyValidator {
       }
     }
     return array_unique($missingPluginNames);
+  }
+
+  /**
+   * @param Collection<int, DynamicSegmentFilterEntity> $dynamicFilters
+   */
+  public function getMissingPluginsByAllFilters(Collection $dynamicFilters): array {
+    $missingPluginNames = [];
+    if (count($dynamicFilters) > 1
+      && (!$this->wp->isPluginActive(self::MAILPOET_PREMIUM_PLUGIN['id'])
+        || !$this->subscribersFeature->hasValidPremiumKey()
+        || $this->subscribersFeature->check())
+    ) {
+      $missingPluginNames[] = self::MAILPOET_PREMIUM_PLUGIN['name'];
+    }
+    return $missingPluginNames;
   }
 
   public function getMissingPluginsByFilter(DynamicSegmentFilterEntity $dynamicSegmentFilter): array {
@@ -79,5 +107,18 @@ class SegmentDependencyValidator {
       }
     }
     return $missingPlugins;
+  }
+
+  public function getCustomErrorMessage($missingPlugin) {
+    if ($missingPlugin === self::MAILPOET_PREMIUM_PLUGIN['name']
+      && $this->wp->isPluginActive(self::MAILPOET_PREMIUM_PLUGIN['id'])
+      && (!$this->subscribersFeature->hasValidPremiumKey() || $this->subscribersFeature->check())
+    ) {
+      return [
+        'message' => $this->wp->__('Your current MailPoet plan does not support advanced segments. Please [link]upgrade to a MailPoet Premium plan[/link] to reactivate this segment.', 'mailpoet'),
+        'link' => 'https://account.mailpoet.com',
+      ];
+    }
+    return false;
   }
 }
