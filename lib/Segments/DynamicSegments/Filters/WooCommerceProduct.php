217ea35942efbf20cb42d5fd0a28d963f793d5af
Jan Lysý <jan.lysy@automattic.com>
2021-12-08 14:25:45 +0100

    Use WooCommerce tables instead of postmeta
    [MAILPOET-3576]

--- lib/Segments/DynamicSegments/Filters/WooCommerceProduct.php
+++ lib/Segments/DynamicSegments/Filters/WooCommerceProduct.php
@@ -5,6 +5,7 @@ namespace MailPoet\Segments\DynamicSegments\Filters;
 use MailPoet\Entities\DynamicSegmentFilterData;
 use MailPoet\Entities\DynamicSegmentFilterEntity;
 use MailPoet\Entities\SubscriberEntity;
+use MailPoet\Util\DBCollationChecker;
 use MailPoet\Util\Security;
 use MailPoetVendor\Doctrine\DBAL\Connection;
 use MailPoetVendor\Doctrine\DBAL\Query\QueryBuilder;
@@ -16,82 +17,90 @@ class WooCommerceProduct implements Filter {
   /** @var EntityManager */
   private $entityManager;
 
+  /** @var DBCollationChecker */
+  private $collationChecker;
+
   public function __construct(
-    EntityManager $entityManager
+    EntityManager $entityManager,
+    DBCollationChecker $collationChecker
   ) {
     $this->entityManager = $entityManager;
+    $this->collationChecker = $collationChecker;
   }
 
   public function apply(QueryBuilder $queryBuilder, DynamicSegmentFilterEntity $filter): QueryBuilder {
-    global $wpdb;
     $filterData = $filter->getFilterData();
     $operator = $filterData->getOperator();
     $productIds = $filterData->getParam('product_ids');
     $subscribersTable = $this->entityManager->getClassMetadata(SubscriberEntity::class)->getTableName();
     $parameterSuffix = $filter->getId() ?? Security::generateRandomString();
-    $completedOrder = "postmeta.post_id NOT IN ( SELECT id FROM {$wpdb->posts} AS p WHERE p.post_status IN ('wc-cancelled', 'wc-failed'))";
 
     if ($operator === DynamicSegmentFilterData::OPERATOR_ANY) {
-      $this->applyPostmetaJoin($queryBuilder);
-      $this->applyOrderItemsJoin($queryBuilder);
-      $this->applyOrderItemmetaJoin($queryBuilder);
-      $queryBuilder->where("itemmeta.meta_value IN (:products_{$parameterSuffix})");
+      $this->applyCustomerJoin($queryBuilder);
+      $this->applyOrderJoin($queryBuilder);
+      $this->applyProductJoin($queryBuilder);
+      $queryBuilder->where("product.product_id IN (:products_{$parameterSuffix})");
 
     } elseif ($operator === DynamicSegmentFilterData::OPERATOR_ALL) {
-      $this->applyPostmetaJoin($queryBuilder);
-      $this->applyOrderItemsJoin($queryBuilder);
-      $this->applyOrderItemmetaJoin($queryBuilder);
-      $queryBuilder->where("itemmeta.meta_value IN (:products_{$parameterSuffix})")
-        ->groupBy("{$subscribersTable}.id, items.order_id")
-        ->having('COUNT(items.order_id) = :count' . $parameterSuffix)
+      $this->applyCustomerJoin($queryBuilder);
+      $this->applyOrderJoin($queryBuilder);
+      $this->applyProductJoin($queryBuilder);
+      $queryBuilder->where("product.product_id IN (:products_{$parameterSuffix})")
+        ->groupBy("{$subscribersTable}.id, orderStats.order_id")
+        ->having('COUNT(orderStats.order_id) = :count' . $parameterSuffix)
         ->setParameter('count' . $parameterSuffix, count($productIds));
 
     } elseif ($operator === DynamicSegmentFilterData::OPERATOR_NONE) {
-      $this->applyPostmetaJoin($queryBuilder);
-      $this->applyOrderItemsJoin($queryBuilder);
+      $this->applyCustomerJoin($queryBuilder);
+      $this->applyOrderJoin($queryBuilder);
       // subQuery with subscriber ids that bought products
       $subQuery = $this->createQueryBuilder($subscribersTable);
       $subQuery->select("DISTINCT $subscribersTable.id");
-      $subQuery = $this->applyPostmetaJoin($subQuery);
-      $subQuery = $this->applyOrderItemsJoin($subQuery);
-      $subQuery = $this->applyOrderItemmetaJoin($subQuery);
-      $subQuery->where("itemmeta.meta_value IN (:products_{$parameterSuffix})")
-        ->andWhere($completedOrder);
+      $subQuery = $this->applyCustomerJoin($subQuery);
+      $subQuery = $this->applyOrderJoin($subQuery);
+      $subQuery = $this->applyProductJoin($subQuery);
+      $subQuery->where("product.product_id IN (:products_{$parameterSuffix})");
       // application subQuery for negation
       $queryBuilder->where("{$subscribersTable}.id NOT IN ({$subQuery->getSQL()})");
     }
     return $queryBuilder
-      ->andWhere($completedOrder)
       ->setParameter("products_{$parameterSuffix}", $productIds, Connection::PARAM_STR_ARRAY);
   }
 
-  private function applyPostmetaJoin(QueryBuilder $queryBuilder): QueryBuilder {
+  private function applyCustomerJoin(QueryBuilder $queryBuilder): QueryBuilder {
     global $wpdb;
     $subscribersTable = $this->entityManager->getClassMetadata(SubscriberEntity::class)->getTableName();
+    $collation = $this->collationChecker->getCollateIfNeeded(
+      $subscribersTable,
+      'email',
+      $wpdb->prefix . 'wc_customer_lookup',
+      'email'
+    );
     return $queryBuilder->innerJoin(
       $subscribersTable,
-      $wpdb->postmeta,
-      'postmeta',
-      "postmeta.meta_key = '_customer_user' AND $subscribersTable.wp_user_id=postmeta.meta_value"
+      $wpdb->prefix . 'wc_customer_lookup',
+      'customer',
+      "$subscribersTable.email = customer.email $collation"
     );
   }
 
-  private function applyOrderItemsJoin(QueryBuilder $queryBuilder): QueryBuilder {
+  private function applyOrderJoin(QueryBuilder $queryBuilder): QueryBuilder {
     global $wpdb;
-    return $queryBuilder->join('postmeta',
-      $wpdb->prefix . 'woocommerce_order_items',
-      'items',
-      'postmeta.post_id = items.order_id'
+    return $queryBuilder->join(
+      'customer',
+      $wpdb->prefix . 'wc_order_stats',
+      'orderStats',
+      'customer.customer_id = orderStats.customer_id AND orderStats.status NOT IN ("wc-cancelled", "wc-failed")'
     );
   }
 
-  private function applyOrderItemmetaJoin(QueryBuilder $queryBuilder): QueryBuilder {
+  private function applyProductJoin(QueryBuilder $queryBuilder): QueryBuilder {
     global $wpdb;
     return $queryBuilder->innerJoin(
-      'items',
-      $wpdb->prefix . 'woocommerce_order_itemmeta',
-      'itemmeta',
-      "itemmeta.order_item_id=items.order_item_id AND itemmeta.meta_key='_product_id'"
+      'orderStats',
+      $wpdb->prefix . 'wc_order_product_lookup',
+      'product',
+      'orderStats.order_id = product.order_id'
     );
   }
 
