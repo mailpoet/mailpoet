c4520191719acfaecad1f778cca7c2aecee1c262
Rodrigo Primo <rodrigosprimo@gmail.com>
2021-12-14 20:15:22 -0300

    Combine PHPStan config files into one
    Before this commit, we had two different PHPStan configuration files. One
    for the lib directory (phpstan.neon) and another one for the tests directory
    (phpstan-tests.neon). This commit combines both files into one
    (phpstan.neon) to make it easier to maintain and make changes to PHPStan
    configurations.
    
    As part of this process, it was necessary to change the Robo command
    `./do qa:phpstan` to call PHPStan only once and to
    combine php-version-dependent-config-tests.php and
    php-version-dependent-config-libs.php in a new file called
    php-version-dependent-config.php.
    
    Another benefit of this change is that running PHPStan only once with a
    single configuration file means that it runs about 20% faster than
    the previous setup.
    
    [MAILPOET-4024]

--- tasks/phpstan/php-version-dependent-config-tests.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types = 1);
-
-$config = [];
-$phpVersion = (int)getenv('ANALYSIS_PHP_VERSION') ?: PHP_VERSION_ID;
-$config['parameters']['phpVersion'] = $phpVersion;
-
-return $config;
