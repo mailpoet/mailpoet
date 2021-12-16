0246019ed17107ba2c06164476b511bef44beb6d
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2021-12-16 14:15:20 +0100

    Update DB version check
    [MAILPOET-3953]

--- lib/Config/Migrator.php
+++ lib/Config/Migrator.php
@@ -778,8 +778,8 @@ class Migrator {
 
   private function migratePurchasedInCategoryDynamicFilters(): bool {
     global $wpdb;
-    // skip the migration if the DB version is higher than 3.74.4 or is not set (a new install)
-    if (version_compare($this->settings->get('db_version', '3.74.4'), '3.74.3', '>')) {
+    // skip the migration if the DB version is higher than 3.75.1 or is not set (a new install)
+    if (version_compare($this->settings->get('db_version', '3.75.1'), '3.75.0', '>')) {
       return false;
     }
 
