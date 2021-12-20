2ab80b6e9aa51f1ada03ee9111439b05432fb982
Jan Lysý <jan.lysy@automattic.com>
2021-12-20 09:59:41 +0100

    Remove unused import

--- assets/js/src/segments/dynamic/dynamic_segments_filters/woocommerce.tsx
+++ assets/js/src/segments/dynamic/dynamic_segments_filters/woocommerce.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import MailPoet from 'mailpoet';
-import { find, filter } from 'lodash/fp';
+import { filter } from 'lodash/fp';
 import ReactSelect from 'common/form/react_select/react_select';
 import Select from 'common/form/select/select';
 import { useSelect, useDispatch } from '@wordpress/data';
