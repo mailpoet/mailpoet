283c98ca0fb6c75003856116345e14559c41085c
Jan Lysý <jan.lysy@automattic.com>
2022-01-05 13:06:50 +0100

    Remove trimming new lines from PHP files in build script
    [MAILPOET-4001]

--- tasks/strip-whitespaces.php
+++ tasks/strip-whitespaces.php
@@ -11,8 +11,13 @@ if (mb_substr($directory, 0, 1) !== DIRECTORY_SEPARATOR) {
 $iterator = new RecursiveDirectoryIterator($directory, RecursiveDirectoryIterator::SKIP_DOTS);
 $files = new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::SELF_FIRST);
 foreach ($files as $file) {
-  if (substr($file, -3) !== 'php') {
+  if (!is_file($file) || substr($file, -3) !== 'php') {
     continue;
   }
-  file_put_contents($file, php_strip_whitespace($file));
+  $data = file_get_contents($file);
+  $output = preg_replace('/^\s*\/\*(\*)?(((?!\*\/)[\s\S])+)?\*\//m', '', $data); // remove multiline comments
+  $data = $output !== null ? $output : $data;
+  $data = preg_replace("/[\r\n]+/", "\n", $data); // remove redundant new lines
+  $data = preg_replace("/[[:blank:]]+/s", " ", $data); // remove redundant whitespaces
+  file_put_contents($file, $data);
 }
