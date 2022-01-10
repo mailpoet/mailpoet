7abb04d12e65f721e5951b49ccfc9cd7ec32df6f
Rostislav Wolny <rosta.wolny@automattic.com>
2022-01-10 15:37:27 +0100

    Split component for email statistics segment filter configuration
    There was a lot of conditional functionality related only to open statistics.
    Splitting the component into two improves readability.
    [MAILPOET-3951]

---
+++ mailpoet/assets/js/src/segments/dynamic/dynamic_segments_filters/email_statistics_opens.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect } from 'react';
+import MailPoet from 'mailpoet';
+import { filter, map } from 'lodash/fp';
+import { useSelect, useDispatch } from '@wordpress/data';
+
+import ReactSelect from 'common/form/react_select/react_select';
+import Select from 'common/form/select/select';
+import { Grid } from 'common/grid';
+import {
+  AnyValueTypes,
+  EmailFormItem,
+  SelectOption,
+  WindowNewslettersList,
+} from '../types';
+
+type Props = {
+  filterIndex: number;
+}
+
+export const EmailOpenStatisticsFields: React.FunctionComponent<Props> = ({ filterIndex }) => {
+  const segment: EmailFormItem = useSelect(
+    (select) => select('mailpoet-dynamic-segments-form').getSegmentFilter(filterIndex),
+    [filterIndex]
+  );
+
+  const { updateSegmentFilter, updateSegmentFilterFromEvent } = useDispatch('mailpoet-dynamic-segments-form');
+
+  const newslettersList: WindowNewslettersList = useSelect(
+    (select) => select('mailpoet-dynamic-segments-form').getNewslettersList(),
+    []
+  );
+
+  const newsletterOptions = newslettersList?.map((newsletter) => {
+    const sentAt = (newsletter.sent_at) ? MailPoet.Date.format(newsletter.sent_at) : MailPoet.I18n.t('notSentYet');
+    return {
+      label: newsletter.subject,
+      tag: sentAt,
+      value: Number(newsletter.id),
+    };
+  });
+
+  useEffect(() => {
+    if ((segment.operator !== AnyValueTypes.ANY)
+        && (segment.operator !== AnyValueTypes.ALL)
+        && (segment.operator !== AnyValueTypes.NONE)
+    ) {
+      updateSegmentFilter({ operator: AnyValueTypes.ANY }, filterIndex);
+    }
+  }, [
+    segment.newsletter_id,
+    segment.operator,
+    filterIndex,
+    updateSegmentFilter,
+  ]);
+
+  return (
+    <>
+      <Grid.CenteredRow>
+        <Select
+          key="select"
+          isFullWidth
+          value={segment.operator}
+          onChange={(e) => {
+            updateSegmentFilterFromEvent('operator', filterIndex, e);
+          }}
+        >
+          <option value={AnyValueTypes.ANY}>{MailPoet.I18n.t('anyOf')}</option>
+          <option value={AnyValueTypes.ALL}>{MailPoet.I18n.t('allOf')}</option>
+          <option value={AnyValueTypes.NONE}>{MailPoet.I18n.t('noneOf')}</option>
+        </Select>
+      </Grid.CenteredRow>
+      <Grid.CenteredRow>
+        <ReactSelect
+          dimension="small"
+          isFullWidth
+          isMulti
+          placeholder={MailPoet.I18n.t('selectNewsletterPlaceholder')}
+          options={newsletterOptions}
+          automationId="segment-email"
+          value={
+            filter(
+              (option) => {
+                if (!segment.newsletters) return undefined;
+                const newsletterId = option.value;
+                return segment.newsletters.indexOf(newsletterId) !== -1;
+              },
+              newsletterOptions
+            )
+          }
+          onChange={(options: SelectOption[]): void => {
+            updateSegmentFilter(
+              { newsletters: map('value', options) },
+              filterIndex
+            );
+          }}
+        />
+      </Grid.CenteredRow>
+    </>
+  );
+};
