2f4e46b82349dc91a037254a68f3ad618e83ec80
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2022-01-10 14:29:35 +0100

    Fixed PHP 8.1 and wordpress-stubs errors
    [MAILPOET-3932]

---
+++ tasks/phpstan/fix-WPStubs-for-PHP-8_1.php
@@ -0,0 +1,21 @@
+<?php
+
+// throw exception if anything fails
+set_error_handler(function ($severity, $message, $file, $line) {
+  throw new ErrorException($message, 0, $severity, $file, $line);
+});
+
+$file = 'vendor/php-stubs/wordpress-stubs/wordpress-stubs.php';
+
+$data = file_get_contents($file);
+
+$search_term = 'function readonly';
+
+if (! str_contains($data, $search_term)) {
+  return;
+}
+
+// "readonly" is a reserved keyword in PHP 8.1,
+// WP has a "function readonly", hence, we need to replace readonly with __readonly for the stub
+$data = str_replace($search_term, 'function __readonly', $data);
+file_put_contents($file, $data);
