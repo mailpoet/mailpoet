29ea9ca7e34667642637df3724fbf7298d7dc04e
Jan Lysý <jan.lysy@automattic.com>
2022-01-18 13:28:13 +0100

    Add fix for Stub::makeEmptyExcept annotation
    [MAILPOET-4053]

---
+++ mailpoet/tasks/fix-codeception-stub.php
@@ -0,0 +1,33 @@
+<?php
+
+// throw exception if anything fails
+set_error_handler(function ($severity, $message, $file, $line) {
+  throw new ErrorException($message, 0, $severity, $file, $line);
+});
+
+// Skip for production build
+if (!file_exists(__DIR__ . '/../vendor/codeception/stub/src/Stub.php')) {
+  exit;
+}
+
+// Fixes Codeception/Stub annotation for Stub::MakeEmptyExcept.
+// This fix can be deleted when we use Codeception/Stub:4.0.1 or newer.
+$replacements = [
+  [
+    'file' => __DIR__ . '/../vendor/codeception/stub/src/Stub.php',
+    'find' => [
+      '* @template' . PHP_EOL,
+    ],
+    'replace' => [
+      '* @template RealInstanceType of object' . PHP_EOL,
+    ],
+  ],
+
+];
+
+// Apply replacements
+foreach ($replacements as $singleFile) {
+  $data = file_get_contents($singleFile['file']);
+  $data = str_replace($singleFile['find'], $singleFile['replace'], $data);
+  file_put_contents($singleFile['file'], $data);
+}
