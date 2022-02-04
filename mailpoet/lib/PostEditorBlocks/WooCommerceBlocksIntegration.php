38fb681942e020abb55ce38987a72d6a7d157f75
Jan Lysý <jan.lysy@automattic.com>
2022-02-04 14:07:00 +0100

    Improve blocks integration
    [MAILPOET-3937]

--- mailpoet/lib/PostEditorBlocks/WooCommerceBlocksIntegration.php
+++ mailpoet/lib/PostEditorBlocks/WooCommerceBlocksIntegration.php
@@ -110,16 +110,10 @@ class WooCommerceBlocksIntegration {
   }
 
   public function processCheckoutBlockOptin(\WC_Order $order, $request) {
-    $checkoutOptinEnabled = $this->settings->get(WooCommerceSubscription::OPTIN_ENABLED_SETTING_NAME, false);
-    if (!$checkoutOptinEnabled) {
-      return;
-    }
+    $checkoutOptinEnabled = (bool)$this->settings->get(WooCommerceSubscription::OPTIN_ENABLED_SETTING_NAME);
+    $checkoutOptin = isset($request['extensions']['mailpoet']['optin']) ? (bool)$request['extensions']['mailpoet']['optin'] : false;
 
     if (!$order->get_billing_email()) {
-      return;
-    }
-
-    if (!isset($request['extensions']['mailpoet']['optin'])) {
       return;
     }
 
@@ -142,6 +136,6 @@ class WooCommerceBlocksIntegration {
       return null;
     }
 
-    $this->woocommerceSubscription->handleSubscriberOptin($subscriberOldModel, (bool)$checkoutOptinEnabled, (bool)$request['extensions']['mailpoet']['optin']);
+    $this->woocommerceSubscription->handleSubscriberOptin($subscriberOldModel, $checkoutOptinEnabled, $checkoutOptin);
   }
 }
