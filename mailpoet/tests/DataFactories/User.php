9de5a6a8509b7986c1cb0548f5c83251909b33d8
Rostislav Wolny <rosta.wolny@automattic.com>
2022-02-08 09:39:58 +0100

    Use proper function for creating a user in tests on multisite
    [MAILPOET-4123]

--- mailpoet/tests/DataFactories/User.php
+++ mailpoet/tests/DataFactories/User.php
@@ -6,7 +6,11 @@ use WP_User;
 
 class User {
   public function createUser($name, $role, $email): WP_User {
-    $userId = wp_create_user($name, "$name-password", $email);
+    if (is_multisite()) {
+      $userId = wpmu_create_user($name, "$name-password", $email) ;
+    } else {
+      $userId = wp_create_user($name, "$name-password", $email);
+    }
     assert(is_int($userId));
     $user = get_user_by('ID', $userId);
     assert($user instanceof WP_User);
