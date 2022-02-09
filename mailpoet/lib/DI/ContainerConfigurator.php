013bbccb4c0f3bd981fc11356d391b83bd383dca
Jan Jakes <jan.jakes@automattic.com>
2022-02-09 15:14:04 +0100

    Add empty automation initializer
    [MAILPOET-4135]

--- mailpoet/lib/DI/ContainerConfigurator.php
+++ mailpoet/lib/DI/ContainerConfigurator.php
@@ -100,6 +100,8 @@ class ContainerConfigurator implements IContainerConfigurator {
     $container->autowire(\MailPoet\AutomaticEmails\WooCommerce\Events\FirstPurchase::class)->setPublic(true);
     $container->autowire(\MailPoet\AutomaticEmails\WooCommerce\Events\PurchasedInCategory::class)->setPublic(true);
     $container->autowire(\MailPoet\AutomaticEmails\WooCommerce\Events\PurchasedProduct::class)->setPublic(true);
+    // Automation
+    $container->autowire(\MailPoet\Automation\Automation::class)->setPublic(true);
     // Config
     $container->autowire(\MailPoet\Config\AccessControl::class)->setPublic(true);
     $container->autowire(\MailPoet\Config\Activator::class)->setPublic(true);
