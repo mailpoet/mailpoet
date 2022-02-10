70e1c7c6e986e5336536e0b2e396ed6be7f12209
Jan Jakes <jan.jakes@automattic.com>
2022-02-10 09:43:26 +0100

    Add basic request, response, and endpoint definitions for API
    [MAILPOET-4135]

---
+++ mailpoet/lib/Automation/API/Request.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Automation\API;
+
+use MailPoet\Automation\Exceptions;
+use WP_REST_Request;
+
+class Request {
+  /** @var WP_REST_Request */
+  private $wpRequest;
+
+  public function __construct(
+    WP_REST_Request $wpRequest
+  ) {
+    $this->wpRequest = $wpRequest;
+  }
+
+  public function getHeader(string $key): ?string {
+    return $this->wpRequest->get_header($key);
+  }
+
+  public function getUrlParams(): array {
+    return $this->wpRequest->get_url_params();
+  }
+
+  public function getUrlParam(string $name): ?string {
+    return $this->getUrlParams()[$name] ?? null;
+  }
+
+  public function getQueryParams(): array {
+    return $this->wpRequest->get_query_params();
+  }
+
+  public function getQueryParam(string $name): ?string {
+    return $this->getQueryParams()[$name] ?? null;
+  }
+
+  public function getBody(): array {
+    return $this->wpRequest->get_json_params() ?? [];
+  }
+
+  public function getRawBody(): string {
+    return $this->wpRequest->get_body();
+  }
+}
