99a8ee3a46a21cd45697e0c8b0d8b270481512be
Jan Jakes <jan.jakes@automattic.com>
2022-02-10 10:52:32 +0100

    Add basic API route handling wrapper for WP REST API
    Each endpoint will be a class that can implement get/post/put/delete methods.
    The routes are registered to WP REST API automatically based on the existence
    of implemented methods. The endpoint classes are handled standard services and
    therefore can leverage full powers of the DI container.
    
    [MAILPOET-4135]

--- mailpoet/lib/Automation/Automation.php
+++ mailpoet/lib/Automation/Automation.php
@@ -2,7 +2,19 @@
 
 namespace MailPoet\Automation;
 
+use MailPoet\Automation\API\API;
+
 class Automation {
+  /** @var API */
+  private $api;
+
+  public function __construct(
+    API $api
+  ) {
+    $this->api = $api;
+  }
+
   public function initialize(): void {
+    $this->api->initialize();
   }
 }
---
+++ mailpoet/lib/Automation/WordPress.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Automation;
+
+class WordPress {
+  public function addAction(string $hookName, callable $callback, int $priority = 10, int $acceptedArgs = 1): bool {
+    return add_action($hookName, $callback, $priority, $acceptedArgs);
+  }
+
+  public function registerRestRoute(string $namespace, string $route, array $args = [], bool $override = false): bool {
+    return register_rest_route($namespace, $route, $args, $override);
+  }
+}
