b8e2cde04c651784a90a904cae68a804f5dea187
Jan Jakes <jan.jakes@automattic.com>
2022-02-10 11:00:58 +0100

    Add API exception handling and error response
    [MAILPOET-4135]

---
+++ mailpoet/lib/Automation/API/ErrorResponse.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Automation\API;
+
+class ErrorResponse extends Response {
+  public function __construct(
+    int $status,
+    string $message,
+    string $code
+  ) {
+    parent::__construct(null, $status);
+    $this->set_data([
+      'code' => $code,
+      'message' => $message,
+      'data' => ['status' => $status],
+    ]);
+  }
+}
