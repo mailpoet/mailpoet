8841338906e862cc90ef9c4688f604ed5c3a03f6
Jan Jakes <jan.jakes@automattic.com>
2022-02-11 09:53:08 +0100

    Auto-create DB schema on automation page load
    We don't have any hooks for feature activation (yet) so this seems to
    be the most handy place to create the database schema at the moment.
    
    [MAILPOET-4135]

--- mailpoet/lib/AdminPages/Pages/Automation.php
+++ mailpoet/lib/AdminPages/Pages/Automation.php
@@ -3,18 +3,27 @@
 namespace MailPoet\AdminPages\Pages;
 
 use MailPoet\AdminPages\PageRenderer;
+use MailPoet\Automation\Migrations\Migrator;
 
 class Automation {
+  /** @var Migrator */
+  private $migrator;
+
   /** @var PageRenderer */
   private $pageRenderer;
 
   public function __construct(
+    Migrator $migrator,
     PageRenderer $pageRenderer
   ) {
+    $this->migrator = $migrator;
     $this->pageRenderer = $pageRenderer;
   }
 
   public function render() {
+    if (!$this->migrator->hasSchema()) {
+      $this->migrator->createSchema();
+    }
     $this->pageRenderer->displayPage('automation.html', []);
   }
 }
