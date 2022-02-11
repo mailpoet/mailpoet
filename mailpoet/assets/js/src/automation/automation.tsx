d7dad547ec21462646af9f5ed114c1484f4de875
Jan Jakes <jan.jakes@automattic.com>
2022-02-11 10:10:51 +0100

    Add basic check component to test automation API calls
    [MAILPOET-4135]

--- mailpoet/assets/js/src/automation/automation.tsx
+++ mailpoet/assets/js/src/automation/automation.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useQuery } from './api';
+
+function ApiCheck(): JSX.Element {
+  const { data, loading, error } = useQuery('workflows');
+
+  if (!data || loading) {
+    return <div>Calling API...</div>;
+  }
+
+  return <div>{error ? 'API error!' : 'API OK ✓'}</div>;
+}
 
 function App(): JSX.Element {
   return (
-    <div>Hello from React.</div>
+    <ApiCheck />
   );
 }
 
