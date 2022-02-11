7c503a35174d50688462944499da4460adc0ba48
Jan Jakes <jan.jakes@automattic.com>
2022-02-11 09:33:15 +0100

    Add basic migrator to create/delete automation tables for prototyping
    This is a simple service used only for prototyping that will allow us
    to simply change and recreate database schemas. Eventually, we'll need
    to replace it with database migration logic.
    
    [MAILPOET-4135]

---
+++ mailpoet/lib/Automation/Migrations/Migrator.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Automation\Migrations;
+
+use MailPoet\Automation\Exceptions;
+use wpdb;
+
+class Migrator {
+  /** @var string */
+  private $prefix;
+
+  /** @var wpdb */
+  private $wpdb;
+
+  public function __construct() {
+    global $wpdb;
+    $this->prefix = $wpdb->prefix . 'mailpoet_automation_';
+    $this->wpdb = $wpdb;
+  }
+
+  public function createSchema(): void {
+    $this->runQuery("
+      CREATE TABLE {$this->prefix}workflows (
+        id int(11) unsigned NOT NULL AUTO_INCREMENT,
+        name varchar(255) NOT NULL,
+        created_at timestamp NOT NULL,
+        updated_at timestamp NOT NULL,
+        deleted_at timestamp NOT NULL,
+        data longtext,
+        PRIMARY KEY (id)
+      );
+    ");
+  }
+
+  public function deleteSchema(): void {
+    $this->runQuery("DROP TABLE IF EXISTS {$this->prefix}workflows");
+  }
+
+  public function hasSchema(): bool {
+    $pattern = str_replace('_', '\\_', $this->prefix) . '%';
+    return $this->runQuery("SHOW TABLES LIKE '$pattern'") > 0;
+  }
+
+  private function runQuery(string $query): int {
+    $this->wpdb->hide_errors();
+    $result = $this->wpdb->query($query);
+    if ($result === false) {
+      throw Exceptions::migrationFailed($this->wpdb->last_error ?: 'Unknown error');
+    }
+    return $result === true ? 0 : (int)$result;
+  }
+}
