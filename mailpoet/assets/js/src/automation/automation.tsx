2b872f9cca9d14e8f79b519f3d855de2250c6f9f
Jan Jakes <jan.jakes@automattic.com>
2022-02-11 10:12:20 +0100

    Add basic buttons to (re)create/delete automations DB schema
    [MAILPOET-4135]

--- mailpoet/assets/js/src/automation/automation.tsx
+++ mailpoet/assets/js/src/automation/automation.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { useQuery } from './api';
+import { useMutation, useQuery } from './api';
 
 function ApiCheck(): JSX.Element {
   const { data, loading, error } = useQuery('workflows');
@@ -12,9 +12,50 @@ function ApiCheck(): JSX.Element {
   return <div>{error ? 'API error!' : 'API OK ✓'}</div>;
 }
 
+function RecreateSchemaButton(): JSX.Element {
+  const [createSchema, { loading, error }] = useMutation('system/database', { method: 'POST' });
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => createSchema()}
+        disabled={loading}
+      >
+        Recreate DB schema (data will be lost)
+      </button>
+      {error && (<div>{error?.data?.message ?? 'An unknown error occurred'}</div>)}
+    </div>
+  );
+}
+
+function DeleteSchemaButton(): JSX.Element {
+  const [deleteSchema, { loading, error }] = useMutation('system/database', { method: 'DELETE' });
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={async () => {
+          await deleteSchema();
+          window.location.href = '/wp-admin/admin.php?page=mailpoet-experimental';
+        }}
+        disabled={loading}
+      >
+        Delete DB schema & deactivate feature
+      </button>
+      {error && (<div>{error?.data?.message ?? 'An unknown error occurred'}</div>)}
+    </div>
+  );
+}
+
 function App(): JSX.Element {
   return (
-    <ApiCheck />
+    <div>
+      <ApiCheck />
+      <RecreateSchemaButton />
+      <DeleteSchemaButton />
+    </div>
   );
 }
 
