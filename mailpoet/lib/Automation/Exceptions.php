51d6b541e5d2c3b56eabb634d810a28d08b1169a
Jan Jakes <jan.jakes@automattic.com>
2022-02-16 11:00:39 +0100

    Add check that JSON request body exists when required
    [MAILPOET-4135]

--- mailpoet/lib/Automation/Exceptions.php
+++ mailpoet/lib/Automation/Exceptions.php
@@ -3,11 +3,20 @@
 namespace MailPoet\Automation;
 
 use MailPoet\Automation\Exceptions\InvalidStateException;
+use MailPoet\Automation\Exceptions\UnexpectedValueException;
 
 class Exceptions {
+  private const API_NO_JSON_BODY = 'mailpoet_automation_api_no_json_body';
+
   public function __construct() {
     throw new InvalidStateException(
       "This is a static factory class. Use it via 'Exception::someError()' factories."
     );
+  }
+
+  public static function apiNoJsonBody(): UnexpectedValueException {
+    return UnexpectedValueException::create()
+      ->withErrorCode(self::API_NO_JSON_BODY)
+      ->withMessage('No JSON body passed.');
   }
 }
