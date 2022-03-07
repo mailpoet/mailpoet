1e5baa3dfaacb9af3c4f4fb6723041a77d569069
Jan Lysý <jan.lysy@automattic.com>
2022-03-07 11:12:14 +0100

    Add fix script for rmccue/requests
    [MAILPOET-4015]

---
+++ mailpoet/tasks/fix-requests.php
@@ -0,0 +1,56 @@
+<?php
+
+// throw exception if anything fails
+set_error_handler(function ($severity, $message, $file, $line) {
+  throw new ErrorException($message, 0, $severity, $file, $line);
+});
+
+// Skip when requests library is missing
+if (!file_exists(__DIR__ . '/../vendor/rmccue/requests/library/Requests/Cookie/Jar.php')) {
+  exit;
+}
+
+// Add the attribute #[\ReturnTypeWillChange] for compatibility with PHP8.1
+$replacements = [
+  [
+    'file' => __DIR__ . '/../vendor/rmccue/requests/library/Requests/Cookie/Jar.php',
+    'find' => [
+      '*/' . PHP_EOL . '	public function offsetExists($key) {',
+      '*/' . PHP_EOL . '	public function offsetGet($key) {',
+      '*/' . PHP_EOL . '	public function offsetSet($key, $value) {',
+      '*/' . PHP_EOL . '	public function offsetUnset($key) {',
+      '*/' . PHP_EOL . '	public function getIterator() {',
+    ],
+    'replace' => [
+      '*/' . PHP_EOL . '	#[\ReturnTypeWillChange]' . PHP_EOL . '	public function offsetExists($key) {',
+      '*/' . PHP_EOL . '	#[\ReturnTypeWillChange]' . PHP_EOL . '	public function offsetGet($key) {',
+      '*/' . PHP_EOL . '	#[\ReturnTypeWillChange]' . PHP_EOL . '	public function offsetSet($key, $value) {',
+      '*/' . PHP_EOL . '	#[\ReturnTypeWillChange]' . PHP_EOL . '	public function offsetUnset($key) {',
+      '*/' . PHP_EOL . '	#[\ReturnTypeWillChange]' . PHP_EOL . '	public function getIterator() {',
+    ],
+  ],
+  [
+    'file' => __DIR__ . '/../vendor/rmccue/requests/library/Requests/Utility/CaseInsensitiveDictionary.php',
+    'find' => [
+      '*/' . PHP_EOL . '	public function offsetExists($key) {',
+      '*/' . PHP_EOL . '	public function offsetGet($key) {',
+      '*/' . PHP_EOL . '	public function offsetSet($key, $value) {',
+      '*/' . PHP_EOL . '	public function offsetUnset($key) {',
+      '*/' . PHP_EOL . '	public function getIterator() {',
+    ],
+    'replace' => [
+      '*/' . PHP_EOL . '	#[\ReturnTypeWillChange]' . PHP_EOL . '	public function offsetExists($key) {',
+      '*/' . PHP_EOL . '	#[\ReturnTypeWillChange]' . PHP_EOL . '	public function offsetGet($key) {',
+      '*/' . PHP_EOL . '	#[\ReturnTypeWillChange]' . PHP_EOL . '	public function offsetSet($key, $value) {',
+      '*/' . PHP_EOL . '	#[\ReturnTypeWillChange]' . PHP_EOL . '	public function offsetUnset($key) {',
+      '*/' . PHP_EOL . '	#[\ReturnTypeWillChange]' . PHP_EOL . '	public function getIterator() {',
+    ],
+  ],
+];
+
+// Apply replacements
+foreach ($replacements as $singleFile) {
+  $data = file_get_contents($singleFile['file']);
+  $data = str_replace($singleFile['find'], $singleFile['replace'], $data);
+  file_put_contents($singleFile['file'], $data);
+}
