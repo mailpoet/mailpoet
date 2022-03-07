3e6a4fd5573937ca2de7580b64cfacf503bb3b45
Jan Jakes <jan.jakes@automattic.com>
2022-03-07 11:26:15 +0100

    Add workflow data object and an empty step object
    [MAILPOET-4136]

---
+++ mailpoet/lib/Automation/Engine/Workflows/Step.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Automation\Engine\Workflows;
+
+class Step {
+  public const TYPE_TRIGGER = 'trigger';
+  public const TYPE_ACTION = 'action';
+}
