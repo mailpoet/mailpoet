c7213b32f93ee8dfb2e44e5a4650506bc80f9885
Sam Najian <sam.najian@automattic.com>
2022-03-07 17:47:54 +0100

    Add type definition for fields
    - Convert ga_tracking.jsx to tsx
    - Update type definitions in StandardScheduling component
    
    [MAILPOET-4163]

---
+++ mailpoet/assets/js/src/form/types/field.ts
@@ -0,0 +1,29 @@
+import { ComponentType, ReactNode } from 'react';
+import { FieldType } from './fieldType';
+
+export type Segment = unknown; // @TODO: add properties
+
+export type Field = {
+  name: string;
+  id?: string;
+  api_version?: string;
+  endpoint?: string;
+  tooltip?: string;
+  customLabel?: string;
+  className?: string;
+  disabled?: false
+  label?: string;
+  tip?: string | null | ReactNode | Array<ReactNode>;
+  placeholder?: string;
+  type?: FieldType;
+  component?: ComponentType;
+  fields?: Array<Field>;
+  validation?: Record<string, string | boolean | number>;
+  inline?: boolean,
+  multiple?: boolean,
+  onBeforeChange?: () => void,
+  filter?: (segment: Segment) => boolean,
+  getLabel?: (segment: Segment) => string,
+  getCount?: (segment: Segment) => string,
+  transformChangedValue?: (arg: unknown) => Segment,
+};
