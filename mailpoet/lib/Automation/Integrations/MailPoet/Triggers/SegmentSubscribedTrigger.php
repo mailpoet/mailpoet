5ad6aa488a1782b09969bb8aaf26e7737d5dea36
Jan Jakes <jan.jakes@automattic.com>
2022-03-07 15:15:05 +0100

    Pass segment and subscriber subjects with segment subscribed trigger
    [MAILPOET-4136]

--- mailpoet/lib/Automation/Integrations/MailPoet/Triggers/SegmentSubscribedTrigger.php
+++ mailpoet/lib/Automation/Integrations/MailPoet/Triggers/SegmentSubscribedTrigger.php
@@ -4,15 +4,29 @@ namespace MailPoet\Automation\Integrations\MailPoet\Triggers;
 
 use MailPoet\Automation\Engine\Hooks;
 use MailPoet\Automation\Engine\Workflows\Trigger;
+use MailPoet\Automation\Integrations\MailPoet\Subjects\SegmentSubject;
+use MailPoet\Automation\Integrations\MailPoet\Subjects\SubscriberSubject;
+use MailPoet\Entities\SubscriberSegmentEntity;
+use MailPoet\InvalidStateException;
 use MailPoet\WP\Functions as WPFunctions;
 
 class SegmentSubscribedTrigger implements Trigger {
+  /** @var SegmentSubject */
+  private $segmentSubject;
+
+  /** @var SubscriberSubject */
+  private $subscriberSubject;
+
   /** @var WPFunctions */
   private $wp;
 
   public function __construct(
+    SegmentSubject $segmentSubject,
+    SubscriberSubject $subscriberSubject,
     WPFunctions $wp
   ) {
+    $this->segmentSubject = $segmentSubject;
+    $this->subscriberSubject = $subscriberSubject;
     $this->wp = $wp;
   }
 
@@ -25,14 +39,30 @@ class SegmentSubscribedTrigger implements Trigger {
   }
 
   public function getSubjects(): array {
-    return [];
+    return [
+      $this->segmentSubject,
+      $this->subscriberSubject,
+    ];
   }
 
   public function registerHooks(): void {
     $this->wp->addAction('mailpoet_segment_subscribed', [$this, 'handleSubscription'], 10, 2);
   }
 
-  public function handleSubscription(): void {
-    $this->wp->doAction(Hooks::TRIGGER, $this, []);
+  public function handleSubscription(SubscriberSegmentEntity $subscriberSegment): void {
+    $segment = $subscriberSegment->getSegment();
+    $subscriber = $subscriberSegment->getSubscriber();
+
+    if (!$segment || !$subscriber) {
+      throw new InvalidStateException();
+    }
+
+    $this->segmentSubject->load(['segment_id' => $segment->getId()]);
+    $this->subscriberSubject->load(['subscriber_id' => $subscriber->getId()]);
+
+    $this->wp->doAction(Hooks::TRIGGER, $this, [
+      $this->segmentSubject,
+      $this->subscriberSubject,
+    ]);
   }
 }
