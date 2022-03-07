66eaf555b5c7a9ca86ce59200962ce33bb961e9d
Jan Jakes <jan.jakes@automattic.com>
2022-03-07 12:15:09 +0100

    Catch all errors in step runner
    Action Scheduler handles only exceptions, not other errors.
    
    [MAILPOET-4136]

--- mailpoet/lib/Automation/Engine/Control/StepRunner.php
+++ mailpoet/lib/Automation/Engine/Control/StepRunner.php
@@ -2,9 +2,11 @@
 
 namespace MailPoet\Automation\Engine\Control;
 
+use Exception;
 use MailPoet\Automation\Engine\Exceptions\InvalidStateException;
 use MailPoet\Automation\Engine\Hooks;
 use MailPoet\Automation\Engine\WordPress;
+use Throwable;
 
 class StepRunner {
   /** @var WordPress */
@@ -22,6 +24,20 @@ class StepRunner {
 
   /** @param mixed $args */
   public function run($args): void {
+    // Action Scheduler catches only Exception instances, not other errors.
+    // We need to convert them to exceptions to be processed and logged.
+    try {
+      $this->runStep($args);
+    } catch (Throwable $e) {
+      if (!$e instanceof Exception) {
+        throw new Exception($e->getMessage(), intval($e->getCode()), $e);
+      }
+      throw $e;
+    }
+  }
+
+  /** @param mixed $args */
+  private function runStep($args): void {
     // TODO: args validation
     if (!is_array($args)) {
       throw new InvalidStateException();
