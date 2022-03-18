e5823dc74757087bf098b1e4a2ecabf7d39853cf
Jan Jakes <jan.jakes@automattic.com>
2022-03-18 13:54:35 +0100

    Add factory for boolean WP JSON schema
    [MAILPOET-4195]

---
+++ mailpoet/lib/Validator/Schema/BooleanSchema.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Validator\Schema;
+
+use MailPoet\Validator\Schema;
+
+// See: https://developer.wordpress.org/rest-api/extending-the-rest-api/schema/#primitive-types
+class BooleanSchema extends Schema {
+  protected $schema = [
+    'type' => 'boolean',
+  ];
+}
