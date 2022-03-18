e8241466da27da553910ada88f5d5c2961166d57
Jan Jakes <jan.jakes@automattic.com>
2022-03-18 13:54:48 +0100

    Add factory for null WP JSON schema
    [MAILPOET-4195]

---
+++ mailpoet/lib/Validator/Schema/NullSchema.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Validator\Schema;
+
+use MailPoet\Validator\Schema;
+
+// See: https://developer.wordpress.org/rest-api/extending-the-rest-api/schema/#primitive-types
+class NullSchema extends Schema {
+  protected $schema = [
+    'type' => 'null',
+  ];
+}
