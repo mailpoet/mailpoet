6b758d90e66c34a57dc771262714a734f662653d
Rostislav Wolny <rosta.wolny@automattic.com>
2022-03-28 16:28:37 +0200

    Use MailerFactory in Mailer api
    [MAILPOET-4115]

--- mailpoet/tests/integration/API/JSON/v1/MailerTest.php
+++ mailpoet/tests/integration/API/JSON/v1/MailerTest.php
@@ -5,6 +5,7 @@ namespace MailPoet\Test\API\JSON\v1;
 use Codeception\Stub\Expected;
 use MailPoet\API\JSON\Response as APIResponse;
 use MailPoet\API\JSON\v1\Mailer;
+use MailPoet\Mailer\MailerFactory;
 use MailPoet\Mailer\MailerLog;
 use MailPoet\Mailer\MetaInfo;
 use MailPoet\Services\AuthorizedEmailsController;
@@ -23,7 +24,7 @@ class MailerTest extends \MailPoetTest {
     $authorizedEmailsController = $this->makeEmpty(AuthorizedEmailsController::class, ['checkAuthorizedEmailAddresses' => Expected::never()]);
     // resumeSending() method should clear the mailer log's status
     $bridge = new Bridge($settings);
-    $mailerEndpoint = new Mailer($authorizedEmailsController, $settings, $bridge, new MetaInfo);
+    $mailerEndpoint = new Mailer($authorizedEmailsController, $settings, $bridge, $this->diContainer->get(MailerFactory::class), new MetaInfo);
     $response = $mailerEndpoint->resumeSending();
     expect($response->status)->equals(APIResponse::STATUS_OK);
     $mailerLog = MailerLog::getMailerLog();
@@ -35,7 +36,7 @@ class MailerTest extends \MailPoetTest {
     $settings->set(AuthorizedEmailsController::AUTHORIZED_EMAIL_ADDRESSES_ERROR_SETTING, ['invalid_sender_address' => 'a@b.c']);
     $authorizedEmailsController = $this->makeEmpty(AuthorizedEmailsController::class, ['checkAuthorizedEmailAddresses' => Expected::once()]);
     $bridge = new Bridge($settings);
-    $mailerEndpoint = new Mailer($authorizedEmailsController, $settings, $bridge, new MetaInfo);
+    $mailerEndpoint = new Mailer($authorizedEmailsController, $settings, $bridge, $this->diContainer->get(MailerFactory::class), new MetaInfo);
     $mailerEndpoint->resumeSending();
   }
 }
