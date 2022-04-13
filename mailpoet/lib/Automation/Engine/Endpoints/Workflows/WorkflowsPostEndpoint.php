5a5af74c91c4343835fd72735a65b5cffc855d9b
Jan Jakes <jan.jakes@automattic.com>
2022-04-13 13:19:51 +0200

    Add schema for workflow post endpoint
    [MAILPOET-4207]

--- mailpoet/lib/Automation/Engine/Endpoints/Workflows/WorkflowsPostEndpoint.php
+++ mailpoet/lib/Automation/Engine/Endpoints/Workflows/WorkflowsPostEndpoint.php
@@ -6,6 +6,7 @@ use MailPoet\Automation\Engine\API\Endpoint;
 use MailPoet\Automation\Engine\API\Request;
 use MailPoet\Automation\Engine\API\Response;
 use MailPoet\Automation\Engine\Builder\CreateWorkflowController;
+use MailPoet\Validator\Builder;
 
 class WorkflowsPostEndpoint extends Endpoint {
   /** @var CreateWorkflowController */
@@ -18,9 +19,23 @@ class WorkflowsPostEndpoint extends Endpoint {
   }
 
   public function handle(Request $request): Response {
-    // TODO: validation
     $data = $request->getParams();
     $this->createController->createWorkflow($data);
     return new Response();
+  }
+
+  public static function getRequestSchema(): array {
+    $step = Builder::object([
+      'id' => Builder::string()->required(),
+      'type' => Builder::string()->required(),
+      'key' => Builder::string()->required(),
+      'args' => Builder::object(),
+      'next_step_id' => Builder::string(),
+    ]);
+
+    return [
+      'name' => Builder::string()->required(),
+      'steps' => Builder::object()->required()->additionalProperties($step),
+    ];
   }
 }
