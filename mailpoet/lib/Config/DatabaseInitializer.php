c08bde077b684c72d12bc1e2de94b6df17772d19
Jan Lysý <jan.lysy@automattic.com>
2022-04-22 14:12:54 +0200

    Update symfony/dependency-injection package to v5
    [MAILPOET-4227]

--- mailpoet/lib/Config/DatabaseInitializer.php
+++ mailpoet/lib/Config/DatabaseInitializer.php
@@ -1,24 +1,22 @@
-<?php
+<?php declare(strict_types = 1);
 
 namespace MailPoet\Config;
 
 use MailPoetVendor\Doctrine\DBAL\Connection;
-use MailPoetVendor\Psr\Container\ContainerInterface;
 
 class DatabaseInitializer {
-  private $diContainer;
+  /** @var Connection */
+  private $connection;
 
   public function __construct(
-    ContainerInterface $diContainer
+    Connection $connection
   ) {
-    $this->diContainer = $diContainer;
+    $this->connection = $connection;
   }
 
   public function initializeConnection() {
-    $connection = $this->diContainer->get(Connection::class);
-
     // pass the same PDO connection to legacy Database object
     $database = new Database();
-    $database->init($connection->getWrappedConnection());
+    $database->init($this->connection->getWrappedConnection());
   }
 }
