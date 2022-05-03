0e1feba763bfeb531ce7de62e310952fda7ed6b7
Jan Jakes <jan.jakes@automattic.com>
2022-05-03 16:09:37 +0200

    Add automation editor page
    [MAILPOET-4287]

---
+++ mailpoet/lib/AdminPages/Pages/AutomationEditor.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace MailPoet\AdminPages\Pages;
+
+use MailPoet\AdminPages\PageRenderer;
+use MailPoet\WP\Functions as WPFunctions;
+
+class AutomationEditor {
+  /** @var PageRenderer */
+  private $pageRenderer;
+
+  /** @var WPFunctions */
+  private $wp;
+
+  public function __construct(
+    PageRenderer $pageRenderer,
+    WPFunctions $wp
+  ) {
+    $this->pageRenderer = $pageRenderer;
+    $this->wp = $wp;
+  }
+
+  public function render() {
+    $this->pageRenderer->displayPage('automation/editor.html', [
+      'sub_menu' => 'mailpoet-automation',
+      'api' => [
+        'root' => rtrim($this->wp->escUrlRaw($this->wp->restUrl()), '/'),
+        'nonce' => $this->wp->wpCreateNonce('wp_rest'),
+      ],
+    ]);
+  }
+}
