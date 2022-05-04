8a20f57cfa7c2875d9e31e94dda0e557a7db22c6
Rodrigo Primo <rodrigo@automattic.com>
2022-05-04 15:31:21 -0300

    Deprecate DynamicSegmentFilter model as it is not used anymore
    We can remove this model after six months.
    
    [MAILPOET-4150]

--- mailpoet/lib/Models/DynamicSegmentFilter.php
+++ mailpoet/lib/Models/DynamicSegmentFilter.php
@@ -8,12 +8,16 @@ use MailPoet\WP\Functions as WPFunctions;
 /**
  * @property array|string|null $filterData
  * @property string $segmentId
+ * @deprecated This model is deprecated. Use \MailPoet\Segments\DynamicSegments\DynamicSegmentFilterRepository and
+ * \MailPoet\Entities\DynamicSegmentFilterEntity.
+ * This class can be removed after 2022-11-04.
  */
 class DynamicSegmentFilter extends Model {
 
   public static $_table = MP_DYNAMIC_SEGMENTS_FILTERS_TABLE; // phpcs:ignore PSR2.Classes.PropertyDeclaration
 
   public function save() {
+    self::deprecationError(__METHOD__);
     if (is_null($this->filterData)) {
       $this->filterData = [];
     }
@@ -26,6 +30,7 @@ class DynamicSegmentFilter extends Model {
   }
 
   public static function getAllBySegmentIds($segmentIds) {
+    self::deprecationError(__METHOD__);
     if (empty($segmentIds)) return [];
     $query = self::tableAlias('filters')
       ->whereIn('filters.segment_id', $segmentIds);
@@ -35,6 +40,7 @@ class DynamicSegmentFilter extends Model {
   }
 
   public function __get($name) {
+    self::deprecationError($name);
     $name = Helpers::camelCaseToUnderscore($name);
     $value = parent::__get($name);
     if ($name === 'filter_data' && $value !== null && WPFunctions::get()->isSerialized($value)) {
@@ -43,7 +49,16 @@ class DynamicSegmentFilter extends Model {
     return $value;
   }
 
+  /**
+   * @deprecated This is here for displaying the deprecation warning for static calls.
+   */
+  public static function __callStatic($name, $arguments) {
+    self::deprecationError($name);
+    return parent::__callStatic($name, $arguments);
+  }
+
   public static function deleteAllBySegmentIds($segmentIds) {
+    self::deprecationError(__METHOD__);
     if (empty($segmentIds)) return;
 
     $query = self::tableAlias('filters')
@@ -51,5 +66,9 @@ class DynamicSegmentFilter extends Model {
 
     $query->deleteMany();
 
+  }
+
+  private static function deprecationError($methodName) {
+    trigger_error(' Calling ' . esc_html($methodName) . ' is deprecated and will be removed. Use \MailPoet\Segments\DynamicSegments\DynamicSegmentFilterRepository and respective Doctrine entities instead.', E_USER_DEPRECATED);
   }
 }
