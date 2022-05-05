ab609c8867a47bc8746c04fa6adca3dbac048137
John Oleksowicz <johnoleksowicz@icloud.com>
2022-05-05 14:06:54 -0500

    Remove subscriber check from newsletter validator
    Checking the subscriber limit has nothing to do with the newsletter
    itself, so it makes more sense to continue to perform that check as
    needed elsewhere in the codebase.
    
    MAILPOET-4236

--- mailpoet/lib/Newsletter/NewsletterValidator.php
+++ mailpoet/lib/Newsletter/NewsletterValidator.php
@@ -5,7 +5,6 @@ namespace MailPoet\Newsletter;
 use MailPoet\Entities\NewsletterEntity;
 use MailPoet\Services\Bridge;
 use MailPoet\Settings\TrackingConfig;
-use MailPoet\Util\License\Features\Subscribers as SubscribersFeature;
 use MailPoet\Validator\ValidationException;
 
 class NewsletterValidator {
@@ -16,22 +15,16 @@ class NewsletterValidator {
   /** @var TrackingConfig */
   private $trackingConfig;
 
-  /** @var SubscribersFeature */
-  private $subscribersFeature;
-
   public function __construct(
     Bridge $bridge,
-    TrackingConfig $trackingConfig,
-    SubscribersFeature $subscribersFeature
+    TrackingConfig $trackingConfig
   ) {
     $this->bridge = $bridge;
     $this->trackingConfig = $trackingConfig;
-    $this->subscribersFeature = $subscribersFeature;
   }
   
   public function validate(NewsletterEntity $newsletterEntity): ?string {
     try {
-      $this->validateSubscriberLimit();
       $this->validateBody($newsletterEntity);
       $this->validateUnsubscribeRequirements($newsletterEntity);
       $this->validateReEngagementRequirements($newsletterEntity);
@@ -93,12 +86,6 @@ class NewsletterValidator {
       strpos($content, '"type":"automatedLatestContentLayout"') === false
     ) {
       throw new ValidationException(__('Please add an “Automatic Latest Content” widget to the email from the right sidebar.', 'mailpoet'));
-    }
-  }
-
-  private function validateSubscriberLimit(): void {
-    if ($this->subscribersFeature->check()) {
-      throw new ValidationException(__('Subscribers limit reached.', 'mailpoet'));
     }
   }
 }
