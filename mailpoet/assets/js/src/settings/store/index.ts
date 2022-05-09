b12cb1f8ba01791eb74c01bfdccca96ae87dabe3
Jan Jakes <jan.jakes@automattic.com>
2022-05-09 17:34:01 +0200

    Type select and dispatch functions for settings store
    [MAILPOET-4323]

--- mailpoet/assets/js/src/settings/store/index.ts
+++ mailpoet/assets/js/src/settings/store/index.ts
@@ -5,6 +5,12 @@ import * as controls from './controls';
 import { createReducer } from './create_reducer';
 import { makeDefaultState } from './make_default_state';
 import { STORE_NAME } from './store_name';
+import { OmitFirstArgs } from '../../types';
+
+declare module '@wordpress/data' {
+  function select(key: typeof STORE_NAME): OmitFirstArgs<typeof selectors>;
+  function dispatch(key: typeof STORE_NAME): typeof actions;
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const initStore = (window: any) =>
--- mailpoet/assets/js/src/types/index.ts
+++ mailpoet/assets/js/src/types/index.ts
@@ -1,1 +1,13 @@
 export * from '../segments/dynamic/types';
+
+// Inspired by: https://neliosoftware.com/blog/adding-typescript-to-wordpress-data-stores/
+export type OmitFirstArg<F> = F extends (
+  first: unknown,
+  ...args: infer P
+) => infer R
+  ? (...args: P) => R
+  : never;
+
+export type OmitFirstArgs<O extends object> = {
+  [K in keyof O]: OmitFirstArg<O[K]>;
+};
