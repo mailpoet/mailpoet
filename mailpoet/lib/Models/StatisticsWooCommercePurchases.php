7f888b7fbc36a5c9b609b4475a5de03305daf135
Rodrigo Primo <rodrigo@automattic.com>
2022-05-09 17:08:38 -0300

    Deprecate StatisticsWooCommercePurchases model as it is not used anymore
    We can remove this model after six months.
    
    [MAILPOET-4150]

--- mailpoet/lib/Models/StatisticsWooCommercePurchases.php
+++ mailpoet/lib/Models/StatisticsWooCommercePurchases.php
@@ -10,7 +10,33 @@ namespace MailPoet\Models;
  * @property int $orderId
  * @property string $orderCurrency
  * @property float $orderPriceTotal
+ * @deprecated This model is deprecated. Use \MailPoet\Statistics\StatisticsWooCommercePurchasesRepository
+ * and \MailPoet\Entities\StatisticsWooCommercePurchaseEntity
+ * This class can be removed after 2022-11-04.
  */
 class StatisticsWooCommercePurchases extends Model {
   public static $_table = MP_STATISTICS_WOOCOMMERCE_PURCHASES_TABLE; // phpcs:ignore PSR2.Classes.PropertyDeclaration
+
+  /**
+   * @deprecated This is here for displaying the deprecation warning for properties.
+   */
+  public function __get($key) {
+    self::deprecationError('property "' . $key . '"');
+    return parent::__get($key);
+  }
+
+  /**
+   * @deprecated This is here for displaying the deprecation warning for static calls.
+   */
+  public static function __callStatic($name, $arguments) {
+    self::deprecationError($name);
+    return parent::__callStatic($name, $arguments);
+  }
+
+  private static function deprecationError($methodName) {
+    trigger_error(
+      'Calling ' . esc_html($methodName) . ' is deprecated and will be removed. Use \MailPoet\Statistics\StatisticsWooCommercePurchasesRepository and \MailPoet\Entities\StatisticsWooCommercePurchaseEntity.',
+      E_USER_DEPRECATED
+    );
+  }
 }
