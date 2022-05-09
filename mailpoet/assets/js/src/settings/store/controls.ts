7a17440f2004e34ce2c4c1b30ebebd756ea6f607
Jan Jakes <jan.jakes@automattic.com>
2022-05-09 17:39:58 +0200

    Avoid type errors caused by untyped store actions in settings
    [MAILPOET-4323]

--- mailpoet/assets/js/src/settings/store/controls.ts
+++ mailpoet/assets/js/src/settings/store/controls.ts
@@ -1,12 +1,13 @@
 import { MailPoet } from 'mailpoet';
 import { select } from '@wordpress/data';
 import { STORE_NAME } from './store_name';
+import { Settings } from './types';
 
 export { callApi as CALL_API } from 'common/controls/call_api';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export function TRACK_SETTINGS_SAVED() {
-  const settings = select(STORE_NAME).getSettings();
+  const settings: Settings = select(STORE_NAME).getSettings();
   const data = {
     'Sending method type': settings.mta_group || null,
     'Sending frequency (emails)':
@@ -40,16 +41,16 @@ export function TRACK_REINSTALLED() {
 }
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export function TRACK_TEST_EMAIL_SENT({ success, method }) {
+export function TRACK_TEST_EMAIL_SENT(actionData) {
   MailPoet.trackEvent('User has sent a test email from Settings', {
-    'Sending was successful': !!success,
-    'Sending method type': method,
+    'Sending was successful': !!actionData.success,
+    'Sending method type': actionData.method,
   });
 }
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export function TRACK_UNAUTHORIZED_EMAIL({ meta }) {
-  if (meta !== undefined && meta.invalid_sender_address) {
+export function TRACK_UNAUTHORIZED_EMAIL(actionData) {
+  if (actionData.meta !== undefined && actionData.meta.invalid_sender_address) {
     MailPoet.trackEvent('Unauthorized email used', {
       'Unauthorized email source': 'settings',
     });
