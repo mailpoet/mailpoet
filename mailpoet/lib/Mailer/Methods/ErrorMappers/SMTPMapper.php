ecdc7b90ea97fe8f68c9bda1574de047f7cd6a7d
Jan Lysý <jan.lysy@automattic.com>
2022-05-11 09:34:14 +0200

    Move common code into parent class
    [MAILPOET-4142]

--- mailpoet/lib/Mailer/Methods/ErrorMappers/SMTPMapper.php
+++ mailpoet/lib/Mailer/Methods/ErrorMappers/SMTPMapper.php
@@ -1,53 +1,15 @@
-<?php
+<?php declare(strict_types = 1);
 
 namespace MailPoet\Mailer\Methods\ErrorMappers;
 
 use MailPoet\Mailer\Mailer;
-use MailPoet\Mailer\MailerError;
-use MailPoet\Mailer\SubscriberError;
-use MailPoet\WP\Functions as WPFunctions;
-use MailPoetVendor\Swift_RfcComplianceException;
-use MailPoetVendor\Swift_TransportException;
 
-class SMTPMapper {
+class SMTPMapper extends PHPMailerMapper {
   use BlacklistErrorMapperTrait;
-  use ConnectionErrorMapperTrait;
 
-  const METHOD = Mailer::METHOD_SMTP;
+  public const METHOD = Mailer::METHOD_SMTP;
 
-  /**
-   * @see https://swiftmailer.symfony.com/docs/sending.html
-   * @return MailerError
-   */
-  public function getErrorFromException(\Exception $e, $subscriber) {
-    // remove redundant information appended by Swift logger to exception messages
-    $message = explode(PHP_EOL, $e->getMessage());
-    $code = $e->getCode();
-
-    $level = MailerError::LEVEL_HARD;
-    if ($e instanceof Swift_RfcComplianceException) {
-      $level = MailerError::LEVEL_SOFT;
-    }
-
-    if ($e instanceof Swift_TransportException && (($code < 500) || ($code > 504))) {
-      $level = MailerError::LEVEL_SOFT;
-    }
-
-    $subscriberErrors = [new SubscriberError($subscriber, null)];
-    return new MailerError(MailerError::OPERATION_SEND, $level, $message[0], null, $subscriberErrors);
-  }
-
-  public function getErrorFromLog($log, $subscriber) {
-    // extract error message from log
-    preg_match('/!! (.*?)>>/ism', (string)$log, $message);
-    if (!empty($message[1])) {
-      $message = $message[1];
-      // remove line breaks from the message due to how logger's dump() method works
-      $message = preg_replace('/\r|\n/', '', $message);
-    } else {
-      $message = sprintf(WPFunctions::get()->__('%s has returned an unknown error.', 'mailpoet'), Mailer::METHOD_SMTP);
-    }
-    $subscriberErrors = [new SubscriberError($subscriber, null)];
-    return new MailerError(MailerError::OPERATION_SEND, MailerError::LEVEL_HARD, $message, null, $subscriberErrors);
+  protected function getMethodName(): string {
+    return self::METHOD;
   }
 }
--- mailpoet/tests/unit/Mailer/Methods/ErrorMappers/SMTPMapperTest.php
+++ mailpoet/tests/unit/Mailer/Methods/ErrorMappers/SMTPMapperTest.php
@@ -3,12 +3,13 @@
 namespace MailPoet\Test\Mailer\Methods\ErrorMappers;
 
 use Codeception\Stub;
-use MailPoet\Mailer\Mailer;
 use MailPoet\Mailer\MailerError;
 use MailPoet\Mailer\Methods\ErrorMappers\SMTPMapper;
+use MailPoet\Mailer\WordPress\PHPMailerLoader;
 use MailPoet\WP\Functions as WPFunctions;
-use MailPoetVendor\Swift_RfcComplianceException;
-use MailPoetVendor\Swift_TransportException;
+use PHPMailer\PHPMailer\Exception;
+
+PHPMailerLoader::load();
 
 class SMTPMapperTest extends \MailPoetUnitTest {
 
@@ -25,63 +26,10 @@ class SMTPMapperTest extends \MailPoetUnitTest {
     ]));
   }
 
-  public function testItCanProcessExceptionMessage() {
-    $message = 'Connection could not be established with host localhost [Connection refused #111]' . PHP_EOL
-      . 'Log data:' . PHP_EOL
-      . '++ Starting Swift_SmtpTransport' . PHP_EOL
-      . '!! Connection could not be established with host localhost [Connection refused #111] (code: 0)';
-    $error = $this->mapper->getErrorFromException(new \Exception($message), 'john@rambo.com');
-    expect($error->getMessage())
-      ->equals('Connection could not be established with host localhost [Connection refused #111]');
-    expect($error->getLevel())->equals(MailerError::LEVEL_HARD);
-    expect($error->getSubscriberErrors()[0]->getEmail())->equals('john@rambo.com');
-  }
-
   public function testItCreatesSoftErrorForInvalidEmail() {
-    $message = 'Invalid email';
-    $error = $this->mapper->getErrorFromException(new Swift_RfcComplianceException($message), 'john@rambo.com');
+    $message = 'Invalid address (to): john@rambo.com';
+    $error = $this->mapper->getErrorFromException(new Exception($message), 'john@rambo.com');
     expect($error->getLevel())->equals(MailerError::LEVEL_SOFT);
-  }
-
-  public function testItCreatesSoftErrorForMostTransportExceptions() {
-    $message = 'Connection could not be established with host localhost [Connection timed out]' . PHP_EOL
-      . 'Log data:' . PHP_EOL
-      . '++ Starting Swift_SmtpTransport' . PHP_EOL
-      . '!! Connection could not be established with host localhost [Connection timed out] (code: 463)';
-    $error = $this->mapper->getErrorFromException(new Swift_TransportException($message, 463), 'john@rambo.com');
-    expect($error->getLevel())->equals(MailerError::LEVEL_SOFT);
-  }
-
-  public function testItCreatesHardErrorForTransportException500to504() {
-    $message = 'Connection could not be established with host localhost [Bad sequence of commands]' . PHP_EOL
-      . 'Log data:' . PHP_EOL
-      . '++ Starting Swift_SmtpTransport' . PHP_EOL
-      . '!! Connection could not be established with host localhost [Bad sequence of commands] (code: 503)';
-    $error = $this->mapper->getErrorFromException(new Swift_TransportException($message, 503), 'john@rambo.com');
-    expect($error->getLevel())->equals(MailerError::LEVEL_HARD);
-  }
-
-  public function testItCanProcessLogMessageWhenOneExists() {
-    $log = '++ Swift_SmtpTransport started' . PHP_EOL
-      . '>> MAIL FROM:<moi@mrcasual.com>' . PHP_EOL
-      . '<< 250 OK' . PHP_EOL
-      . '>> RCPT TO:<test2@ietsdoenofferte.nl>' . PHP_EOL
-      . '<< 550 No such recipient here' . PHP_EOL
-      . '!! Expected response code 250/251/252 but got code "550", with message "550 No such recipient here' . PHP_EOL
-      . '" (code: 550)' . PHP_EOL
-      . '>> RSET' . PHP_EOL
-      . '<< 250 Reset OK' . PHP_EOL;
-    $error = $this->mapper->getErrorFromLog($log, 'test@example.com');
-    expect($error->getMessage())
-      ->equals('Expected response code 250/251/252 but got code "550", with message "550 No such recipient here" (code: 550)');
-    expect($error->getSubscriberErrors()[0]->getEmail('moi@mrcasual.com'));
-  }
-
-  public function testItReturnsGenericMessageWhenLogMessageDoesNotExist() {
-    $error = $this->mapper->getErrorFromLog(null, 'test@example.com');
-    expect($error->getMessage())
-      ->equals(Mailer::METHOD_SMTP . ' has returned an unknown error.');
-    expect($error->getSubscriberErrors()[0]->getEmail('moi@mrcasual.com'));
   }
 
   public function _after() {
