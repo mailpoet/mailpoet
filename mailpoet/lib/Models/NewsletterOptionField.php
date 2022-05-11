62412fcdcb37ef74ad874d8585c29d148c967cc8
Rodrigo Primo <rodrigo@automattic.com>
2022-05-11 18:02:11 -0300

    Deprecate models NewsletterOption and NewsletterOptionField
    Those models are being deprecated as they are not used anymore in our
    codebase.
    
    [MAILPOET-4150]

--- mailpoet/lib/Models/NewsletterOptionField.php
+++ mailpoet/lib/Models/NewsletterOptionField.php
@@ -7,12 +7,18 @@ use MailPoet\WP\Functions as WPFunctions;
 /**
  * @property string $name
  * @property string $newsletterType
+ *
+ * @deprecated This model is deprecated. Use \MailPoet\Newsletter\Options\NewsletterOptionFieldsRepository and
+ * \MailPoet\Entities\NewsletterOptionFieldEntity instead. This class can be removed after 2022-11-11.
  */
-
 class NewsletterOptionField extends Model {
   public static $_table = MP_NEWSLETTER_OPTION_FIELDS_TABLE; // phpcs:ignore PSR2.Classes.PropertyDeclaration
 
+  /**
+   * @deprecated
+   */
   public function __construct() {
+    self::deprecationError(__METHOD__);
     parent::__construct();
     $this->addValidations('name', [
       'required' => WPFunctions::get()->__('Please specify a name.', 'mailpoet'),
@@ -22,6 +28,9 @@ class NewsletterOptionField extends Model {
     ]);
   }
 
+  /**
+   * @deprecated
+   */
   public function newsletters() {
     return $this->has_many_through(
       __NAMESPACE__ . '\Newsletter',
@@ -29,5 +38,28 @@ class NewsletterOptionField extends Model {
       'option_field_id',
       'newsletter_id'
     )->select_expr(MP_NEWSLETTER_OPTION_TABLE . '.value');
+  }
+
+  /**
+   * @deprecated This is here for displaying the deprecation warning for properties.
+   */
+  public function __get($key) {
+    self::deprecationError('property "' . $key . '"');
+    return parent::__get($key);
+  }
+
+  /**
+   * @deprecated This is here for displaying the deprecation warning for static calls.
+   */
+  public static function __callStatic($name, $arguments) {
+    self::deprecationError($name);
+    return parent::__callStatic($name, $arguments);
+  }
+
+  private static function deprecationError($methodName) {
+    trigger_error(
+      'Calling ' . esc_html($methodName) . ' is deprecated and will be removed. Use \MailPoet\Newsletter\Options\NewsletterOptionFieldsRepository and \MailPoet\Entities\NewsletterOptionFieldEntity instead.',
+      E_USER_DEPRECATED
+    );
   }
 }
