caecdfb026e71b591c81f21ef2e7344bb755606b
Jan Jakes <jan.jakes@automattic.com>
2022-05-13 10:57:58 +0200

    Add basic interface skeleton setup
    [MAILPOET-4287]

--- mailpoet/assets/js/src/automation/editor/index.tsx
+++ mailpoet/assets/js/src/automation/editor/index.tsx
@@ -1,7 +1,24 @@
+import classnames from 'classnames';
 import ReactDOM from 'react-dom';
+import { InterfaceSkeleton } from '@wordpress/interface';
+
+// See: https://github.com/WordPress/gutenberg/blob/9601a33e30ba41bac98579c8d822af63dd961488/packages/edit-post/src/components/layout/index.js
 
 function Editor(): JSX.Element {
-  return <div>Automation editor</div>;
+  const className = classnames(
+    'edit-post-layout',
+    'interface-interface-skeleton',
+  );
+
+  return (
+    <InterfaceSkeleton
+      className={className}
+      header={<div>Header</div>}
+      content={<div>Content</div>}
+      sidebar={<div>Sidebar</div>}
+      secondarySidebar={<div>Secondary sidebar</div>}
+    />
+  );
 }
 
 window.addEventListener('DOMContentLoaded', () => {
