248dd548d39e466afa158273a29e255d8aaa81fb
Jan Jakes <jan.jakes@automattic.com>
2022-05-13 14:28:38 +0200

    Make workflow a composite for keyboard navigation and a11y, add root styles
    [MAILPOET-4287]

--- mailpoet/assets/js/src/automation/editor/components/workflow/index.tsx
+++ mailpoet/assets/js/src/automation/editor/components/workflow/index.tsx
@@ -1,4 +1,9 @@
+import {
+  __unstableComposite as Composite,
+  __unstableUseCompositeState as useCompositeState,
+} from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
+import { WorkflowCompositeContext } from './context';
 import { EmptyWorkflow } from './empty-workflow';
 import { store } from '../../store';
 
@@ -10,9 +15,26 @@ export function Workflow(): JSX.Element {
     [],
   );
 
+  const compositeState = useCompositeState({
+    orientation: 'vertical',
+    wrap: 'horizontal',
+    shift: true,
+  });
+
   if (!workflowData) {
     return <EmptyWorkflow />;
   }
 
-  return <div>{JSON.stringify(workflowData)}</div>;
+  return (
+    <WorkflowCompositeContext.Provider value={compositeState}>
+      <Composite
+        state={compositeState}
+        role="tree"
+        aria-orientation="vertical"
+        className="mailpoet-automation-editor-workflow"
+      >
+        {JSON.stringify(workflowData)}
+      </Composite>
+    </WorkflowCompositeContext.Provider>
+  );
 }
