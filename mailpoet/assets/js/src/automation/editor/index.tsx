16b0229c44968998682a81bd4a2a38f79e4937ab
Jan Jakes <jan.jakes@automattic.com>
2022-05-13 11:27:44 +0200

    Add basic header with heading and save draft & publish buttons
    [MAILPOET-4287]

--- mailpoet/assets/js/src/automation/editor/index.tsx
+++ mailpoet/assets/js/src/automation/editor/index.tsx
@@ -2,6 +2,7 @@ import classnames from 'classnames';
 import ReactDOM from 'react-dom';
 import { useSelect } from '@wordpress/data';
 import { InterfaceSkeleton, FullscreenMode } from '@wordpress/interface';
+import { Header } from './components/header';
 import { store } from './store';
 
 // See: https://github.com/WordPress/gutenberg/blob/9601a33e30ba41bac98579c8d822af63dd961488/packages/edit-post/src/components/layout/index.js
@@ -24,7 +25,7 @@ function Editor(): JSX.Element {
       <FullscreenMode isActive={isFullscreenActive} />
       <InterfaceSkeleton
         className={className}
-        header={<div>Header</div>}
+        header={<Header />}
         content={<div>Content</div>}
         sidebar={<div>Sidebar</div>}
         secondarySidebar={<div>Secondary sidebar</div>}
