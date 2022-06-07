8645213dcb244e7da72c9ec22851191d56717b03
David Remer <webmaster@websupporter.net>
2022-06-07 09:27:36 +0300

    Add short doc to explain, why the methods where altered.
    [MAILPOET-3985]

--- mailpoet/lib/Config/TwigFileSystemCache.php
+++ mailpoet/lib/Config/TwigFileSystemCache.php
@@ -14,6 +14,11 @@ class TwigFileSystemCache extends \MailPoetVendor\Twig\Cache\FilesystemCache {
     parent::__construct($directory, $options);
   }
 
+  /**
+   * The original FileSystemCache of twig generates the key depending on PHP_VERSION.
+   * We need to produce the same key regardless of PHP_VERSION. Therefore, we
+   * overwrite this method.
+   **/
   public function generateKey(string $name, string $className): string {
     $hash = \hash('sha256', $className);
     return $this->directory . $hash[0] . $hash[1] . '/' . $hash . '.php';
