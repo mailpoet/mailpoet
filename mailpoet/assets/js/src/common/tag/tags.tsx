ce799bf0b74bb72d0cabcd991943d98329d68a55
John Oleksowicz <johnoleksowicz@icloud.com>
2022-06-07 13:21:59 -0500

    Add tooltip explaining where the link goes
    MAILPOET-4244

--- mailpoet/assets/js/src/common/tag/tags.tsx
+++ mailpoet/assets/js/src/common/tag/tags.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react';
 import { Tag } from './tag';
+import { Tooltip } from '../tooltip/tooltip';
+import { MailPoet } from '../../mailpoet';
 
 type Segment = {
   name: string;
@@ -18,21 +20,36 @@ function Tags({ children, dimension, segments, strings }: Props) {
     <div className="mailpoet-tags">
       {children}
       {segments &&
-        segments.map((segment) =>
-          segment.id ? (
-            <a
-              href={`admin.php?page=mailpoet-subscribers#/filter[segment=${segment.id}]`}
-            >
-              <Tag key={segment.name} dimension={dimension} variant="list">
-                {segment.name}
-              </Tag>
-            </a>
-          ) : (
+        segments.map((segment) => {
+          const tag = (
             <Tag key={segment.name} dimension={dimension} variant="list">
               {segment.name}
             </Tag>
-          ),
-        )}
+          );
+          if (!segment.id) {
+            return tag;
+          }
+          const randomId = Math.random().toString(36).substring(2, 15);
+          const tooltipId = `segment-tooltip-${randomId}`;
+
+          return (
+            <div key={randomId}>
+              <Tooltip id={tooltipId} place="top">
+                {MailPoet.I18n.t('viewFilteredSubscribersMessage').replace(
+                  '%1$s',
+                  segment.name,
+                )}
+              </Tooltip>
+              <a
+                data-tip=""
+                data-for={tooltipId}
+                href={`admin.php?page=mailpoet-subscribers#/filter[segment=${segment.id}]`}
+              >
+                {tag}
+              </a>
+            </div>
+          );
+        })}
       {strings &&
         strings.map((string) => (
           <Tag key={string} dimension={dimension} variant="list">
