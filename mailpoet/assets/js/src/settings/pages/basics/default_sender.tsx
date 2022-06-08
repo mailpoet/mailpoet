257929ae8e0a615d26a8a44b6ccfd14edfa0ea2f
David Remer <webmaster@websupporter.net>
2022-06-08 12:05:22 +0300

    Display warning and disable save button when from email is not authorized
    [MAILPOET-4253]

--- mailpoet/assets/js/src/settings/pages/basics/default_sender.tsx
+++ mailpoet/assets/js/src/settings/pages/basics/default_sender.tsx
@@ -1,22 +1,45 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Label, Inputs } from 'settings/components';
 import { isEmail, t, onChange, setLowercaseValue } from 'common/functions';
 import { Input } from 'common/form/input/input';
 import { useSetting, useSelector, useAction } from 'settings/store/hooks';
 import { SenderEmailAddressWarning } from 'common/sender_email_address_warning.jsx';
+import ReactStringReplace from 'react-string-replace';
 
 export function DefaultSender() {
   const isMssActive = useSelector('isMssActive')();
   const [senderName, setSenderName] = useSetting('sender', 'name');
   const [senderEmail, setSenderEmail] = useSetting('sender', 'address');
+  const [isAuthorized, setIsAuthorized] = useState(true);
   const [replyToName, setReplyToName] = useSetting('reply_to', 'name');
   const [replyToEmail, setReplyToEmail] = useSetting('reply_to', 'address');
   const setErrorFlag = useAction('setErrorFlag');
   const invalidSenderEmail = senderEmail && !isEmail(senderEmail);
   const invalidReplyToEmail = replyToEmail && !isEmail(replyToEmail);
+
+  const isAuthorizedEmail = (email: string) => {
+    if (!isMssActive) {
+      return;
+    }
+    setIsAuthorized(window.mailpoet_authorized_emails.includes(email));
+  };
+  const updateSenderEmailController = (email: string) => {
+    setIsAuthorized(true);
+    setSenderEmail(email);
+  };
   useEffect(() => {
-    setErrorFlag(invalidSenderEmail || invalidReplyToEmail);
-  }, [invalidReplyToEmail, invalidSenderEmail, setErrorFlag]);
+    setErrorFlag(
+      invalidSenderEmail ||
+        invalidReplyToEmail ||
+        (!isAuthorized && isMssActive),
+    );
+  }, [
+    invalidReplyToEmail,
+    invalidSenderEmail,
+    setErrorFlag,
+    isAuthorized,
+    isMssActive,
+  ]);
   return (
     <>
       <Label
@@ -43,9 +66,27 @@ export function DefaultSender() {
           placeholder="from@mydomain.com"
           data-automation-id="from-email-field"
           value={senderEmail}
-          onChange={onChange(setLowercaseValue(setSenderEmail))}
+          onChange={onChange(setLowercaseValue(updateSenderEmailController))}
+          onBlur={onChange(isAuthorizedEmail)}
         />
         <br />
+        {!isAuthorized && (
+          <span className="mailpoet_error_item mailpoet_error">
+            {ReactStringReplace(
+              t('youNeedToAuthorizeTheEmail'),
+              '[email]',
+              () => senderEmail,
+            )}{' '}
+            <a
+              className="mailpoet-link"
+              href={`https://account.mailpoet.com/authorization?email=${senderEmail}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {t('authorizeMyEmail')}
+            </a>
+          </span>
+        )}
         {invalidSenderEmail && (
           <span className="mailpoet_error_item mailpoet_error">
             {t('invalidEmail')}
