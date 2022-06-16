969fc3743e2ca61f7db3a7788c0078438009c8b0
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2022-06-16 10:54:09 +0100

    Release 3.90.1

--- mailpoet/mailpoet.php
+++ mailpoet/mailpoet.php
@@ -2,7 +2,7 @@
 
 /*
  * Plugin Name: MailPoet 3 (New)
- * Version: 3.90.0
+ * Version: 3.90.1
  * Plugin URI: http://www.mailpoet.com
  * Description: Create and send newsletters, post notifications and welcome emails from your WordPress.
  * Author: MailPoet
@@ -17,7 +17,7 @@
  */
 
 $mailpoetPlugin = [
-  'version' => '3.90.0',
+  'version' => '3.90.1',
   'filename' => __FILE__,
   'path' => dirname(__FILE__),
   'autoloader' => dirname(__FILE__) . '/vendor/autoload.php',
