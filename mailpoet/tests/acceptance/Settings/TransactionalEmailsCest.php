927ab776953a1656ebef3cf1ce7702fe8fbe3072
Rostislav Wolny <rosta.wolny@automattic.com>
2022-06-17 10:17:01 +0200

    Add comments into Transactional email fallback test
    [MAILPOET-4254]

--- mailpoet/tests/acceptance/Settings/TransactionalEmailsCest.php
+++ mailpoet/tests/acceptance/Settings/TransactionalEmailsCest.php
@@ -9,8 +9,10 @@ class TransactionalEmailsCest {
   public function sendTransactionalEmailFallback(\AcceptanceTester $i) {
     $i->wantTo('Check that transactional email are sent even when MailPoet sending doesn‘t send');
     $settings = new Settings();
+    $i->wantTo('Setup MailPoet to send transactional emails but having misconfigured SMTP settings.');
     $settings->withMisconfiguredSendingMethodSmtpMailhog();
     $settings->withTransactionEmailsViaMailPoet();
+    $i->wantTo('Create a new WP user and make sure transactional email were received');
     $i->cli(['user', 'create', 'john_doe', 'john_doe@example.com', '--send-email']);
     $i->amOnMailboxAppPage();
     $i->checkEmailWasReceived('New User Registration');
