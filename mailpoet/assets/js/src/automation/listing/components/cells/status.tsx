642825cfe8f3cb2f40223187a184b38dfd0c408c
Jan Jakes <jan.jakes@automattic.com>
2022-06-29 16:05:01 +0200

    Display current workflow status in automation listing
    [MAILPOET-4454]

--- mailpoet/assets/js/src/automation/listing/components/cells/status.tsx
+++ mailpoet/assets/js/src/automation/listing/components/cells/status.tsx
@@ -8,7 +8,7 @@ type Props = {
 };
 
 export function Status({ workflow }: Props): JSX.Element {
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(workflow.status === 'active');
 
   return (
     <div className="mailpoet-automation-listing-cell-status">
