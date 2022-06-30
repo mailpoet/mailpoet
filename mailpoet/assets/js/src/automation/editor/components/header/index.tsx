5602a73beb1cb2e4f3b1703133460edd8d0dd741
Jan Jakes <jan.jakes@automattic.com>
2022-06-30 10:50:11 +0200

    Display activate/update buttons based on workflow status
    [MAILPOET-4454]

--- mailpoet/assets/js/src/automation/editor/components/header/index.tsx
+++ mailpoet/assets/js/src/automation/editor/components/header/index.tsx
@@ -1,16 +1,42 @@
 import { Button, NavigableMenu } from '@wordpress/components';
-import { useDispatch } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { PinnedItems } from '@wordpress/interface';
 import { InserterToggle } from './inserter_toggle';
 import { MoreMenu } from './more_menu';
 import { store, storeName } from '../../store';
+import { WorkflowStatus } from '../../../listing/workflow';
 
 // See:
 //   https://github.com/WordPress/gutenberg/blob/9601a33e30ba41bac98579c8d822af63dd961488/packages/edit-post/src/components/header/index.js
 //   https://github.com/WordPress/gutenberg/blob/9601a33e30ba41bac98579c8d822af63dd961488/packages/edit-navigation/src/components/header/index.js
 
-export function Header(): JSX.Element {
+function ActivateButton(): JSX.Element {
   const { activate } = useDispatch(store);
+
+  return (
+    <Button isPrimary className="editor-post-publish-button" onClick={activate}>
+      Activate
+    </Button>
+  );
+}
+
+function UpdateButton(): JSX.Element {
+  const { activate } = useDispatch(store);
+
+  return (
+    <Button isPrimary className="editor-post-publish-button" onClick={activate}>
+      Update
+    </Button>
+  );
+}
+
+export function Header(): JSX.Element {
+  const { workflowStatus } = useSelect(
+    (select) => ({
+      workflowStatus: select(store).getWorkflowData().status,
+    }),
+    [],
+  );
 
   return (
     <div className="edit-site-header">
@@ -27,13 +53,8 @@ export function Header(): JSX.Element {
       <div className="edit-site-header_end">
         <div className="edit-site-header__actions">
           <Button isTertiary>Save Draft</Button>
-          <Button
-            isPrimary
-            className="editor-post-publish-button"
-            onClick={activate}
-          >
-            Activate
-          </Button>
+          {workflowStatus !== WorkflowStatus.ACTIVE && <ActivateButton />}
+          {workflowStatus === WorkflowStatus.ACTIVE && <UpdateButton />}
           <PinnedItems.Slot scope={storeName} />
           <MoreMenu />
         </div>
