f4845232d298714fe2e22fb5392f3335401c3c2f
Jan Jakes <jan.jakes@automattic.com>
2022-06-30 11:12:02 +0200

    Improve React key props for automation listing
    [MAILPOET-4454]

--- mailpoet/assets/js/src/automation/listing/get-row.tsx
+++ mailpoet/assets/js/src/automation/listing/get-row.tsx
@@ -4,22 +4,27 @@ import { Edit, More, Name, Status, Subscribers } from './components/cells';
 export function getRow(workflow: Workflow): object[] {
   return [
     {
+      id: workflow.id,
       value: workflow.name,
       display: <Name workflow={workflow} />,
     },
     {
+      id: workflow.id,
       value: null,
       display: <Subscribers workflow={workflow} />,
     },
     {
+      id: workflow.id,
       value: workflow.status,
       display: <Status workflow={workflow} />,
     },
     {
+      id: workflow.id,
       value: null,
       display: <Edit workflow={workflow} />,
     },
     {
+      id: workflow.id,
       value: null,
       display: <More workflow={workflow} />,
     },
