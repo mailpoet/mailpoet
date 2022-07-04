a80cabf59fa919416f713fce5369119fe09c3fa8
Jan Lysý <jan.lysy@automattic.com>
2022-07-04 13:21:05 +0200

    Add possibility to use different variant
    [MAILPOET-4441]

--- mailpoet/assets/js/src/common/tag/_stories/tags.tsx
+++ mailpoet/assets/js/src/common/tag/_stories/tags.tsx
@@ -11,15 +11,22 @@ const segments = [
   { name: 'Unconfirmed' },
 ];
 
+const strings = ['First', 'Second', 'Third tag'];
+
 export function Segments() {
   return (
     <>
-      <Heading level={1}>Segments</Heading>
+      <Heading level={1}>Tags</Heading>
+      <Heading level={2}>Segments</Heading>
       <Tags segments={segments} dimension="large" />
       <div className="mailpoet-gap" />
       <Tags segments={segments}>
         <span>Prefix: </span>
       </Tags>
+      <Heading level={2}>Strings</Heading>
+      <Tags strings={strings} dimension="large" variant="good" />
+      <div className="mailpoet-gap" />
+      <Tags strings={strings} dimension="large" variant="good" isInverted />
     </>
   );
 }
