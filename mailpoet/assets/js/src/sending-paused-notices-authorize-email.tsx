2eb441a7f711e37322f3cee19939e7d426d09b69
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2022-07-05 18:47:18 +0100

    Hijack link to MP Shop, show email authorization modal on Newsletter send page
    MAILPOET-4300

--- mailpoet/assets/js/src/sending-paused-notices-authorize-email.tsx
+++ mailpoet/assets/js/src/sending-paused-notices-authorize-email.tsx
@@ -3,11 +3,23 @@ import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { AuthorizeSenderEmailModal } from 'common/authorize_sender_email_modal';
 
-const performActionOnModalClose = () => {
+/**
+ * Perform action after the email has been successfully Authorized
+ */
+const performSuccessActionOnModalClose = () => {
   // if in Settings, reload page, so the new saved FROM address is loaded
   const isInSettings = window.location.href.includes('?page=mailpoet-settings');
+
+  const isInNewsletterSendPage = window.location.href.includes(
+    '?page=mailpoet-newsletters#/send',
+  );
+
   if (isInSettings) {
     window.location.reload();
+  } else if (isInNewsletterSendPage) {
+    jQuery('#field_sender_address')
+      .parsley()
+      .removeError('invalidFromAddress', { updateClass: true });
   }
 };
 
@@ -15,17 +27,24 @@ function AuthorizeSenderEmailApp() {
   const [showModal, setShowModal] = useState(''); // used to hold the email address as well
 
   useEffect(() => {
+    const performAction = (e) => {
+      e.preventDefault();
+      const email = String(e?.target?.dataset?.email || '');
+      setShowModal(email);
+    };
     // use jQuery since some of the targeted notices are added to the DOM using the old
     // jQuery-based notice implementation which doesn't trigger pure-JS added listeners
     jQuery(($) => {
       $(document).on(
         'click',
         '.notice .mailpoet-js-button-authorize-email',
-        (e) => {
-          e.preventDefault();
-          const email = String(e?.target?.dataset?.email || '');
-          setShowModal(email);
-        },
+        performAction,
+      );
+
+      $(document).on(
+        'click',
+        '.parsley-errors-list .mailpoet-js-button-authorize-email',
+        performAction,
       );
     });
   }, []);
@@ -38,7 +57,7 @@ function AuthorizeSenderEmailApp() {
           onRequestClose={() => {
             setShowModal('');
           }}
-          setAuthorizedAddress={performActionOnModalClose}
+          setAuthorizedAddress={performSuccessActionOnModalClose}
         />
       )}
     </>
