cb68688f1801acd2a4ffb5d87b538547cb06b723
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2022-07-06 19:22:04 +0100

    Refactor: update validateAuthorizedEmail to convert authorizedEmails to lowercase
    MAILPOET-4300

--- mailpoet/lib/Services/Bridge.php
+++ mailpoet/lib/Services/Bridge.php
@@ -125,7 +125,7 @@ class Bridge {
     if ($data && $type === 'all') {
       return $data;
     }
-    return $data ? $data[$type] : [];
+    return $data && isset($data[$type]) ? $data[$type] : [];
   }
 
   /**
