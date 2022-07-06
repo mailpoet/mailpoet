f2dc8e97bb5c19e9e0c64ab9ea08af20b8e93b68
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2022-07-06 12:38:11 +0100

    Refactor email sender field, remove double error notice
    MAILPOET-4300

--- mailpoet/assets/js/src/newsletters/send/sender_address_field.jsx
+++ mailpoet/assets/js/src/newsletters/send/sender_address_field.jsx
@@ -1,51 +1,95 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
+import ReactStringReplace from 'react-string-replace';
+import { MailPoet } from 'mailpoet';
 import { FormFieldText } from 'form/fields/text.jsx';
 import { SenderEmailAddressWarning } from 'common/sender_email_address_warning.jsx';
+import {
+  isFieldValid,
+  addOrUpdateError,
+  removeError,
+  validateField,
+} from 'common/functions/parsley_helper_functions';
 
 class SenderField extends Component {
   constructor(props) {
     super(props);
     this.state = {
       emailAddress: props.item.sender_address,
-      isEmailAuthorized: this.isEmailAddressAuthorized(
-        props.item.sender_address,
-      ),
     };
     this.onChange = this.onChange.bind(this);
     this.onBlur = this.onBlur.bind(this);
+
+    const fieldId = props.field.id || `field_${props.field.name}`;
+    this.domElementSelector = `#${fieldId}`;
+    this.parsleyFieldName = 'invalidFromAddress';
   }
 
   onChange(event) {
+    const emailAddress = event.target.value.toLowerCase();
     this.setState({
-      emailAddress: event.target.value.toLowerCase(),
-      isEmailAuthorized: true, // hide email address warning when user is typing
+      emailAddress,
     });
     this.props.onValueChange({
       ...event,
       target: {
         ...event.target,
         name: event.target.name,
-        value: event.target.value.toLowerCase(),
+        value: emailAddress,
       },
     });
+    // hide email address warning when user is typing
+    removeError(this.domElementSelector, this.parsleyFieldName);
   }
 
-  onBlur(event) {
-    const emailAddress = event.target.value.toLowerCase();
+  onBlur() {
+    const emailAddress = this.state.emailAddress;
     const emailAddressIsAuthorized =
-      event.target.value.length >= 3
-        ? this.isEmailAddressAuthorized(emailAddress)
-        : true;
+      this.isEmailAddressAuthorized(emailAddress);
 
-    this.setState({
-      isEmailAuthorized: emailAddressIsAuthorized,
-      emailAddress,
-    });
+    this.showSenderFieldError(emailAddressIsAuthorized, emailAddress);
   }
 
   isEmailAddressAuthorized = (email) =>
-    window.mailpoet_authorized_emails.includes(email);
+    (window.mailpoet_authorized_emails || []).includes(email);
+
+  showInvalidFromAddressError = (emailAddress) => {
+    const fromAddress = emailAddress;
+    let errorMessage = ReactStringReplace(
+      MailPoet.I18n.t('newsletterInvalidFromAddress'),
+      '%1$s',
+      () => fromAddress,
+    );
+    errorMessage = ReactStringReplace(
+      errorMessage,
+      /\[link\](.*?)\[\/link\]/g,
+      (match) =>
+        `<a href="https://account.mailpoet.com/authorization?email=${encodeURIComponent(
+          fromAddress,
+        )}" target="_blank" class="mailpoet-js-button-authorize-email" data-email="${fromAddress}" rel="noopener noreferrer">${match}</a>`,
+    );
+
+    addOrUpdateError(
+      this.domElementSelector,
+      this.parsleyFieldName,
+      errorMessage.join(''),
+    );
+  };
+
+  showSenderFieldError = (emailAddressIsAuthorized, emailAddress) => {
+    if (!window.mailpoet_mss_active) return;
+
+    removeError(this.domElementSelector, this.parsleyFieldName);
+
+    if (!isFieldValid(this.domElementSelector)) {
+      validateField(this.domElementSelector);
+      return;
+    }
+
+    if (!emailAddressIsAuthorized) {
+      this.showInvalidFromAddressError(emailAddress);
+    }
+  };
 
   render() {
     return (
@@ -59,11 +103,11 @@ class SenderField extends Component {
           onValueChange={this.onChange}
           onBlurEvent={this.onBlur}
         />
-        <div className="regular-text" style={{ marginTop: '2.5rem' }}>
+
+        <div className="regular-text">
           <SenderEmailAddressWarning
             emailAddress={this.state.emailAddress}
             mssActive={window.mailpoet_mss_active}
-            isEmailAuthorized={this.state.isEmailAuthorized}
           />
         </div>
       </>
