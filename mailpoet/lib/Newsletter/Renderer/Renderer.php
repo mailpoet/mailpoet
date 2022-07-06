a2d4c595c41d0237975a41e9d788b5aa3ae47545
Sam Najian <sam.najian@automattic.com>
2022-07-06 12:16:37 +0200

    Make Renderer/Renderer.php method expect NewsletterEntity
    [MAILPOET-4360]

--- mailpoet/lib/Newsletter/Renderer/Renderer.php
+++ mailpoet/lib/Newsletter/Renderer/Renderer.php
@@ -5,11 +5,7 @@ namespace MailPoet\Newsletter\Renderer;
 use MailPoet\Config\Env;
 use MailPoet\Config\ServicesChecker;
 use MailPoet\Entities\NewsletterEntity;
-use MailPoet\InvalidStateException;
-use MailPoet\Models\Newsletter;
-use MailPoet\Newsletter\NewslettersRepository;
 use MailPoet\Newsletter\Renderer\EscapeHelper as EHelper;
-use MailPoet\RuntimeException;
 use MailPoet\Tasks\Sending as SendingTask;
 use MailPoet\Util\pQuery\DomNode;
 use MailPoet\WP\Functions as WPFunctions;
@@ -30,9 +26,6 @@ class Renderer {
   /** @var \MailPoetVendor\CSS */
   private $cSSInliner;
 
-  /** @var NewslettersRepository */
-  private $newslettersRepository;
-
   /** @var ServicesChecker */
   private $servicesChecker;
 
@@ -41,45 +34,24 @@ class Renderer {
     Columns\Renderer $columnsRenderer,
     Preprocessor $preprocessor,
     \MailPoetVendor\CSS $cSSInliner,
-    NewslettersRepository $newslettersRepository,
     ServicesChecker $servicesChecker
   ) {
     $this->blocksRenderer = $blocksRenderer;
     $this->columnsRenderer = $columnsRenderer;
     $this->preprocessor = $preprocessor;
     $this->cSSInliner = $cSSInliner;
-    $this->newslettersRepository = $newslettersRepository;
     $this->servicesChecker = $servicesChecker;
   }
 
-  /**
-   * This is only temporary, when all calls are refactored to doctrine and only entity is passed we don't need this
-   * @param \MailPoet\Models\Newsletter|NewsletterEntity $newsletter
-   * @return NewsletterEntity|null
-   */
-  private function getNewsletter($newsletter) {
-    if ($newsletter instanceof Newsletter) {
-      return $this->newslettersRepository->findOneById($newsletter->id);
-    }
-    if (!$newsletter instanceof NewsletterEntity) {
-      throw new InvalidStateException();
-    }
-    return $newsletter;
-  }
-
-  public function render($newsletter, SendingTask $sendingTask = null, $type = false) {
+  public function render(NewsletterEntity $newsletter, SendingTask $sendingTask = null, $type = false) {
     return $this->_render($newsletter, $sendingTask, $type);
   }
 
-  public function renderAsPreview($newsletter, $type = false, ?string $subject = null) {
+  public function renderAsPreview(NewsletterEntity $newsletter, $type = false, ?string $subject = null) {
     return $this->_render($newsletter, null, $type, true, $subject);
   }
 
-  private function _render($newsletter, SendingTask $sendingTask = null, $type = false, $preview = false, $subject = null) {
-    $newsletter = $this->getNewsletter($newsletter);
-    if (!$newsletter instanceof NewsletterEntity) {
-      throw new RuntimeException('Newsletter was not found');
-    }
+  private function _render(NewsletterEntity $newsletter, SendingTask $sendingTask = null, $type = false, $preview = false, $subject = null) {
     $body = (is_array($newsletter->getBody()))
       ? $newsletter->getBody()
       : [];
