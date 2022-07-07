4e9acb524d8c9a8c8b4efa1909a2f23de4c3836a
Jan Jakes <jan.jakes@automattic.com>
2022-07-07 11:33:44 +0200

    Add step card component and render it in step sidebar
    [MAILPOET-4420]

--- mailpoet/assets/js/src/automation/editor/components/sidebar/step/index.tsx
+++ mailpoet/assets/js/src/automation/editor/components/sidebar/step/index.tsx
@@ -1,11 +1,13 @@
 import { PanelBody } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { store } from '../../../store';
+import { StepCard } from '../../step-card';
 
 export function StepSidebar(): JSX.Element {
-  const { selectedStep } = useSelect(
+  const { selectedStep, selectedStepType } = useSelect(
     (select) => ({
       selectedStep: select(store).getSelectedStep(),
+      selectedStepType: select(store).getSelectedStepType(),
     }),
     [],
   );
@@ -15,19 +17,27 @@ export function StepSidebar(): JSX.Element {
   }
 
   return (
-    <PanelBody>
-      <div>
-        <strong>ID:</strong> {selectedStep.id}
-      </div>
-      <div>
-        <strong>Type:</strong> {selectedStep.type}
-      </div>
-      <div>
-        <strong>Key:</strong> {selectedStep.key}
-      </div>
-      <div>
-        <strong>Args:</strong> {JSON.stringify(selectedStep.args)}
-      </div>
-    </PanelBody>
+    <div className="block-editor-block-inspector">
+      <StepCard
+        title={selectedStepType.title}
+        description={selectedStepType.description}
+        icon={selectedStepType.icon}
+      />
+
+      <PanelBody>
+        <div>
+          <strong>ID:</strong> {selectedStep.id}
+        </div>
+        <div>
+          <strong>Type:</strong> {selectedStep.type}
+        </div>
+        <div>
+          <strong>Key:</strong> {selectedStep.key}
+        </div>
+        <div>
+          <strong>Args:</strong> {JSON.stringify(selectedStep.args)}
+        </div>
+      </PanelBody>
+    </div>
   );
 }
