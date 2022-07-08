f9e46f24a3a920ecdcc8c8c957ce58b4db703004
Rodrigo Primo <rodrigo@automattic.com>
2022-07-08 16:37:39 -0300

    Replace Paris models with Doctrine entities in Subscription\Captcha
    [MAILPOET-4351]

--- mailpoet/lib/Subscription/Captcha.php
+++ mailpoet/lib/Subscription/Captcha.php
@@ -2,8 +2,9 @@
 
 namespace MailPoet\Subscription;
 
-use MailPoet\Models\Subscriber;
+use MailPoet\Entities\SubscriberEntity;
 use MailPoet\Subscribers\SubscriberIPsRepository;
+use MailPoet\Subscribers\SubscribersRepository;
 use MailPoet\Util\Helpers;
 use MailPoet\WP\Functions as WPFunctions;
 use MailPoetVendor\Gregwar\Captcha\CaptchaBuilder;
@@ -23,12 +24,16 @@ class Captcha {
   /** @var SubscriberIPsRepository */
   private $subscriberIPsRepository;
 
+  /** @var SubscribersRepository */
+  private $subscribersRepository;
+
   public static function isReCaptcha(?string $captchaType) {
     return in_array($captchaType, [self::TYPE_RECAPTCHA, self::TYPE_RECAPTCHA_INVISIBLE]);
   }
 
   public function __construct(
     SubscriberIPsRepository $subscriberIPsRepository,
+    SubscribersRepository $subscribersRepository,
     WPFunctions $wp = null,
     CaptchaSession $captchaSession = null
   ) {
@@ -41,6 +46,7 @@ class Captcha {
     $this->wp = $wp;
     $this->captchaSession = $captchaSession;
     $this->subscriberIPsRepository = $subscriberIPsRepository;
+    $this->subscribersRepository = $subscribersRepository;
   }
 
   public function isSupported() {
@@ -59,10 +65,10 @@ class Captcha {
 
     // Check limits per recipient if enabled
     if ($subscriberEmail) {
-      $subscriber = Subscriber::where('email', $subscriberEmail)->findOne();
+      $subscriber = $this->subscribersRepository->findOneBy(['email' => $subscriberEmail]);
       if (
-        $subscriber instanceof Subscriber
-        && $subscriber->countConfirmations >= $subscriptionCaptchaRecipientLimit
+        $subscriber instanceof SubscriberEntity
+        && $subscriber->getConfirmationsCount() >= $subscriptionCaptchaRecipientLimit
       ) {
         return true;
       }
