10d4eb5d7f5d84891c52a1ef1d6ce017cc471dc7
Sam Najian <sam.najian@automattic.com>
2022-07-13 12:13:30 +0200

    Remove old models from SegmentsExporter
    [MAILPOET-4348]

--- mailpoet/lib/Subscribers/ImportExport/PersonalDataExporters/SegmentsExporter.php
+++ mailpoet/lib/Subscribers/ImportExport/PersonalDataExporters/SegmentsExporter.php
@@ -2,22 +2,43 @@
 
 namespace MailPoet\Subscribers\ImportExport\PersonalDataExporters;
 
-use MailPoet\Models\Subscriber;
+use MailPoet\Entities\SubscriberEntity;
+use MailPoet\Entities\SubscriberSegmentEntity;
+use MailPoet\Subscribers\SubscribersRepository;
+use MailPoet\WP\DateTime;
 use MailPoet\WP\Functions as WPFunctions;
 
 class SegmentsExporter {
-  public function export($email) {
+
+  /*** @var SubscribersRepository */
+  private $subscribersRepository;
+
+  public function __construct(
+    SubscribersRepository $subscribersRepository
+  ) {
+    $this->subscribersRepository = $subscribersRepository;
+  }
+
+  /**
+   * @param string $email
+   * @return array(data: mixed[], done: boolean)
+   */
+  public function export(string $email): array {
     return [
-      'data' => $this->exportSubscriber(Subscriber::findOne(trim($email))),
+      'data' => $this->exportSubscriber($this->subscribersRepository->findOneBy(['email' => trim($email)])),
       'done' => true,
     ];
   }
 
-  private function exportSubscriber($subscriber) {
+  /**
+   * @param SubscriberEntity|null $subscriber
+   * @return mixed[]
+   */
+  private function exportSubscriber(?SubscriberEntity $subscriber): array {
     if (!$subscriber) return [];
 
     $result = [];
-    $segments = $subscriber->getAllSegmentNamesWithStatus();
+    $segments = $subscriber->getSubscriberSegments();
 
     foreach ($segments as $segment) {
       $result[] = $this->exportSegment($segment);
@@ -26,24 +47,28 @@ class SegmentsExporter {
     return $result;
   }
 
-  private function exportSegment($segment) {
+  /**
+   * @param SubscriberSegmentEntity $segment
+   * @return mixed[]
+   */
+  private function exportSegment(SubscriberSegmentEntity $segment): array {
     $segmentData = [];
     $segmentData[] = [
       'name' => WPFunctions::get()->__('List name', 'mailpoet'),
-      'value' => $segment['name'],
+      'value' => $segment->getSegment() ? $segment->getSegment()->getName() : '',
     ];
     $segmentData[] = [
       'name' => WPFunctions::get()->__('Subscription status', 'mailpoet'),
-      'value' => $segment['status'],
+      'value' => $segment->getStatus(),
     ];
     $segmentData[] = [
       'name' => WPFunctions::get()->__('Timestamp of the subscription (or last change of the subscription status)', 'mailpoet'),
-      'value' => $segment['updated_at'],
+      'value' => $segment->getUpdatedAt()->format(DateTime::DEFAULT_DATE_TIME_FORMAT),
     ];
     return [
       'group_id' => 'mailpoet-lists',
       'group_label' => WPFunctions::get()->__('MailPoet Mailing Lists', 'mailpoet'),
-      'item_id' => 'list-' . $segment['segment_id'],
+      'item_id' => 'list-' . ($segment->getSegment() ? $segment->getSegment()->getId() : ''),
       'data' => $segmentData,
     ];
   }
