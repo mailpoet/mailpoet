601b3e775b3bb393bd5e0aaa906a4e086cc7549a
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2022-07-19 02:01:17 +0100

    Add DMARC Policy Checker class and getDomainDmarcPolicy method
    MAILPOET-4302

---
+++ mailpoet/lib/Util/DmarcPolicyChecker.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace MailPoet\Util;
+
+class DmarcPolicyChecker {
+  const POLICY_NONE = 'none';
+  const POLICY_REJECT = 'reject';
+  const POLICY_QUARANTINE = 'quarantine';
+
+  /**
+   * Lookup the Domain DMARC Policy
+   * returns reject or quarantine or none
+   */
+  public function getDomainDmarcPolicy(string $domain): string {
+
+    if (!$domain) {
+      throw new \InvalidArgumentException('Domain is Required');
+    }
+
+    $dnsLookup = dns_get_record("_dmarc.$domain", DNS_TXT);
+
+    if (!is_array($dnsLookup)) {
+      return self::POLICY_NONE;
+    }
+
+    $txtRecord = $dnsLookup[0]['txt'] ?? null;
+
+    if (!$txtRecord) {
+        // note
+        // most DNS may not have this record
+        // good to set policy to none in those cases
+        return self::POLICY_NONE;
+    }
+
+    // Check for the presence of v=DMARC1;
+    if (stripos($txtRecord, 'dmarc') === false) {
+      // this is not a dmarc txt record
+      // probably a wrong setup from the user
+      return self::POLICY_NONE;
+    }
+
+    $cache = explode(';', $txtRecord);
+
+    $dmarcInfo = [];
+
+    foreach ($cache as $value) {
+        $item = explode('=', $value);
+        $dKey = $item[0] ?? '';
+        $dValue = $item[1] ?? '';
+        $dmarcInfo[trim($dKey)] = trim($dValue);
+    }
+
+    // policy can either be reject or quarantine or none
+    $dmarcStatus = $dmarcInfo['sp'] ?? $dmarcInfo['p'] ?? self::POLICY_NONE;
+
+    return $dmarcStatus;
+  }
+}
---
+++ mailpoet/tests/integration/Util/DmarcPolicyCheckerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace MailPoet\Test\Util;
+
+use MailPoet\Util\DmarcPolicyChecker;
+
+class DmarcPolicyCheckerTest extends \MailPoetTest {
+  public $dmarcPolicyChecker;
+
+  public function __construct() {
+    parent::__construct();
+    $this->dmarcPolicyChecker = new DmarcPolicyChecker();
+  }
+
+  public function testItReturnsNoneWhenDomainDoesNotHaveDmarc() {
+    $domain = 'example.com'; // has no TXT records
+    $result = $this->dmarcPolicyChecker->getDomainDmarcPolicy($domain);
+    expect($result)->equals('none');
+  }
+
+  public function testItReturnsQuarantineStatus() {
+    $domain = 'automattic.com'; // quarantine
+    $result = $this->dmarcPolicyChecker->getDomainDmarcPolicy($domain);
+    expect($result)->equals('quarantine');
+
+    // testing with mailpoet.com
+    $domain = 'mailpoet.com'; // quarantine
+    $result = $this->dmarcPolicyChecker->getDomainDmarcPolicy($domain);
+    expect($result)->equals('quarantine');
+  }
+
+  public function testItReturnsRejectStatus() {
+    $domain = 'google.com'; // reject
+    $result = $this->dmarcPolicyChecker->getDomainDmarcPolicy($domain);
+    expect($result)->equals('reject');
+  }
+
+  public function testItReturnsNoneForInvalidDomain() {
+    $domain = 'example'; // not a valid domain name
+    $result = $this->dmarcPolicyChecker->getDomainDmarcPolicy($domain);
+    expect($result)->equals('none');
+  }
+
+  public function testItReturnsNoneForImproperDmarcSetup() {
+    $domain = 'mine.com'; // used to be v=spf1 ip4:185.39.48.22/31 ip6:2a04:7a00:0:3948::22/127 -all
+    $result = $this->dmarcPolicyChecker->getDomainDmarcPolicy($domain);
+    expect($result)->equals('none');
+  }
+
+  public function testItReturnsSpStatusBeforePStatus() {
+    $domain = 'gmail.com'; // used to be v=DMARC1; p=none; sp=quarantine; rua=mailto:mailauth-reports@google.com
+    $result = $this->dmarcPolicyChecker->getDomainDmarcPolicy($domain);
+    expect($result)->equals('quarantine');
+  }
+}
