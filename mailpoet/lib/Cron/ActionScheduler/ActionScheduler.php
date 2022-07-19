dfae17ed94240b837420cd49fde91f80b452af83
Rostislav Wolny <rosta.wolny@automattic.com>
2022-07-19 13:19:46 +0200

    Add missing return and parameter types to action scheduler related classes
    [MAILPOET-4274]

--- mailpoet/lib/Cron/ActionScheduler/ActionScheduler.php
+++ mailpoet/lib/Cron/ActionScheduler/ActionScheduler.php
@@ -5,12 +5,13 @@ namespace MailPoet\Cron\ActionScheduler;
 class ActionScheduler {
   public const GROUP_ID = 'mailpoet-cron';
 
-  public function scheduleRecurringAction(int $timestamp, int $interval_in_seconds, $hook, $args = []): int {
+  public function scheduleRecurringAction(int $timestamp, int $interval_in_seconds, string $hook, array $args = []): int {
     return as_schedule_recurring_action($timestamp, $interval_in_seconds, $hook, $args, self::GROUP_ID);
   }
 
-  public function unscheduleAction($hook, $args = []) {
-    return as_unschedule_action($hook, $args, self::GROUP_ID);
+  public function unscheduleAction(string $hook, array $args = []): ?int {
+    $id = as_unschedule_action($hook, $args, self::GROUP_ID);
+    return $id !== null ? intval($id) : null;
   }
 
   public function hasScheduledAction(string $hook, array $args = []): bool {
