4c51001558a5ad62f2c2e2179c226e5b4f7fa55c
Jan Jakes <jan.jakes@automattic.com>
2022-07-20 09:49:03 +0200

    Use pnpm patch for parsleyjs deprecated jQuery methods
    This patch replaces deprecated jQuery methods in parsleyjs. It can be removed this when
    a fix is included in the package update.
    
    Note: deferred.pipe() fix is not implemented yet, see https://github.com/guillaumepotier/Parsley.js/pull/1347
    
    [MAILPOET-4485]

--- mailpoet/tasks/npm_post_install.sh
+++ mailpoet/tasks/npm_post_install.sh
@@ -11,8 +11,3 @@ cat $INIT_CWD/node_modules/tinymce/icons/default/icons.min.js >> $INIT_CWD/asset
 # Replace deprecated jQuery methods in the spectrum-colorpicker dependency
 # Remove this when a patch is included in a package update
 git apply $INIT_CWD/tasks/patches/spectrum-replace-jquery-deprecated.patch || true
-
-# Replace deprecated jQuery methods in the parsley dependency
-# Remove this when a patch is included in a package update.
-# Note: deferred.pipe() fix is not implemented yet, see https://github.com/guillaumepotier/Parsley.js/pull/1347
-sed -i -- "s/_focusedField\.focus()/_focusedField\.trigger('focus')/g" node_modules/parsleyjs/dist/parsley*.js
