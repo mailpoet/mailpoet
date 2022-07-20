296c004c3b443f0bdae3ff6fbfe69ea99522a0cf
Jan Jakes <jan.jakes@automattic.com>
2022-07-20 09:55:32 +0200

    Use pnpm patch for spectrum-colorpicker deprecated jQuery methods
    This patch replaces deprecated jQuery methods in the spectrum-colorpicker dependency.
    It can be removed when a fix is included in a package update.
    See: https://github.com/bgrins/spectrum/pull/564
    
    [MAILPOET-4485]

--- mailpoet/tasks/npm_post_install.sh
+++ mailpoet/tasks/npm_post_install.sh
@@ -7,7 +7,3 @@ echo "import tinymce from 'tinymce/tinymce';
 " >> $INIT_CWD/assets/js/src/newsletter_editor/behaviors/tinymce_icons.js
 
 cat $INIT_CWD/node_modules/tinymce/icons/default/icons.min.js >> $INIT_CWD/assets/js/src/newsletter_editor/behaviors/tinymce_icons.js
-
-# Replace deprecated jQuery methods in the spectrum-colorpicker dependency
-# Remove this when a patch is included in a package update
-git apply $INIT_CWD/tasks/patches/spectrum-replace-jquery-deprecated.patch || true
