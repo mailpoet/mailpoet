bd5ea1041d7be81217b72702154abc04e0966b7b
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2022-07-22 03:58:38 +0100

    Use unidirectional data store, remove unnecessary useState
    MAILPOET-4302

--- mailpoet/assets/js/src/common/authorize_sender_email_and_domain_modal.tsx
+++ mailpoet/assets/js/src/common/authorize_sender_email_and_domain_modal.tsx
@@ -23,40 +23,11 @@ function AuthorizeSenderEmailAndDomainModal({
 }: Props): JSX.Element {
   if (!senderEmail) return null;
 
-  let showSenderEmail = showSenderEmailTab;
-  let showSenderDomain = showSenderDomainTab;
-
-  const updateProperty = (prop: boolean, type: 'email' | 'domain') => {
-    if (type === 'email') showSenderEmail = prop;
-    if (type === 'domain') showSenderDomain = prop;
-  };
-
-  /**
-   *  we want to make sure we have at lease one tab avaliable
-   *  if not, close this modal
-   * @param prop Property
-   * @param type action type
-   */
-  const updateDisplayedTab = (prop: boolean, type: 'email' | 'domain') => {
-    if (type === 'email' && prop === false && showSenderDomain === false) {
-      // domain is currently hidden, trying to hide email, close modal
-      onRequestClose();
-    }
-    if (type === 'domain' && prop === false && showSenderEmail === false) {
-      // email is currently hidden, trying to hide sender domain, close modal
-      onRequestClose();
-    }
-    updateProperty(prop, type);
-  };
-
   const emailAddressDomain = senderEmail.split('@').pop().toLowerCase();
 
-  if (showSenderEmailTab === false && showSenderDomainTab === false) {
-    // show sender_email by default
-    updateDisplayedTab(true, 'email');
-  }
-
-  const defaultTab = showSenderEmail ? 'sender_email' : 'sender_domain';
+  let defaultTab = 'sender_email';
+  defaultTab =
+    showSenderDomainTab && !showSenderEmailTab ? 'sender_domain' : defaultTab;
 
   return (
     <Modal
@@ -67,13 +38,13 @@ function AuthorizeSenderEmailAndDomainModal({
         <Tab
           key="sender_email"
           className={classnames({
-            mailpoet_hidden: !showSenderEmail,
+            mailpoet_hidden: !showSenderEmailTab,
           })}
           title={MailPoet.I18n.t(
             'authorizeSenderEmailAndDomainModalSenderEmailTabTitle',
           )}
         >
-          {showSenderEmail && (
+          {showSenderEmailTab && (
             <AuthorizeSenderEmailModal
               useModal={false}
               senderEmail={senderEmail}
@@ -83,7 +54,6 @@ function AuthorizeSenderEmailAndDomainModal({
                   type: 'email',
                   data: authorizedEmailAddress,
                 });
-                updateDisplayedTab(false, 'email');
               }}
             />
           )}
@@ -91,20 +61,19 @@ function AuthorizeSenderEmailAndDomainModal({
         <Tab
           key="sender_domain"
           className={classnames({
-            mailpoet_hidden: !showSenderDomain,
+            mailpoet_hidden: !showSenderDomainTab,
           })}
           title={MailPoet.I18n.t(
             'authorizeSenderEmailAndDomainModalSenderDomainTabTitle',
           )}
         >
-          {showSenderDomain && (
+          {showSenderDomainTab && (
             <AuthorizeSenderDomainModal
               useModal={false}
               senderDomain={emailAddressDomain}
               onRequestClose={onRequestClose}
               setVerifiedSenderDomain={(verifiedSenderDomain) => {
                 onSuccessAction({ type: 'domain', data: verifiedSenderDomain });
-                updateDisplayedTab(false, 'domain');
               }}
             />
           )}
