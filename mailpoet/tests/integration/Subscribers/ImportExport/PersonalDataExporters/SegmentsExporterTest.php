38c6538da306767c160d43288ea756bbbb446fcc
Sam Najian <sam.najian@automattic.com>
2022-07-27 00:21:45 +0200

    Remove models from SegmentsExporterTest
    [MAILPOET-4348]

--- mailpoet/tests/integration/Subscribers/ImportExport/PersonalDataExporters/SegmentsExporterTest.php
+++ mailpoet/tests/integration/Subscribers/ImportExport/PersonalDataExporters/SegmentsExporterTest.php
@@ -2,10 +2,12 @@
 
 namespace MailPoet\Subscribers\ImportExport\PersonalDataExporters;
 
-use MailPoet\Models\Segment;
-use MailPoet\Models\Subscriber;
-use MailPoet\Models\SubscriberSegment;
+use MailPoet\Entities\SubscriberEntity;
+use MailPoet\Entities\SubscriberSegmentEntity;
 use MailPoet\Subscribers\SubscribersRepository;
+use MailPoet\Test\DataFactories\Subscriber as SubscriberFactory;
+use MailPoet\Test\DataFactories\Segment as SegmentFactory;
+use MailPoet\WP\DateTime;
 
 class SegmentsExporterTest extends \MailPoetTest {
 
@@ -29,9 +31,7 @@ class SegmentsExporterTest extends \MailPoetTest {
   }
 
   public function testExportWorksForSubscriberWithNoSegments() {
-    Subscriber::createOrUpdate([
-      'email' => 'email.that@has.no.segments',
-    ]);
+    (new SubscriberFactory())->withEmail('email.that@has.no.segments')->create();
     $result = $this->exporter->export('email.that@has.no.segments');
     expect($result)->array();
     expect($result)->hasKey('data');
@@ -41,24 +41,25 @@ class SegmentsExporterTest extends \MailPoetTest {
   }
 
   public function testExportWorksForSubscriberWithSegments() {
-    $subscriber = Subscriber::createOrUpdate([
-      'email' => 'email.that@has.some.segments',
-    ]);
-    $segment1 = Segment::createOrUpdate(['name' => 'List 1']);
-    $segment2 = Segment::createOrUpdate(['name' => 'List 2']);
-    SubscriberSegment::createOrUpdate([
-      'subscriber_id' => $subscriber->id(),
-      'segment_id' => $segment1->id(),
-      'status' => Subscriber::STATUS_SUBSCRIBED,
-      'updated_at' => '2018-05-02 15:26:52',
-    ]);
-    SubscriberSegment::createOrUpdate([
-      'subscriber_id' => $subscriber->id(),
-      'segment_id' => $segment2->id(),
-      'status' => Subscriber::STATUS_UNSUBSCRIBED,
-      'updated_at' => '2018-05-02 15:26:00',
-    ]);
-    $result = $this->exporter->export('email.that@has.some.segments');
+    $subscriber = (new SubscriberFactory())->create();
+
+    $segment1 = (new SegmentFactory())->withName('List 1')->create();
+    $segment2 = (new SegmentFactory())->withName('List 2')->create();
+
+    $ss1 = new SubscriberSegmentEntity($segment1, $subscriber, SubscriberEntity::STATUS_SUBSCRIBED);
+    $this->entityManager->persist($ss1);
+    $this->entityManager->flush();
+
+    $ss2 = new SubscriberSegmentEntity($segment2, $subscriber, SubscriberEntity::STATUS_UNSUBSCRIBED);
+    $this->entityManager->persist($ss2);
+    $this->entityManager->flush();
+
+    /**
+     * Make Doctrine update SubscriberSegment collections
+     */
+    $this->entityManager->refresh($subscriber);
+
+    $result = $this->exporter->export($subscriber->getEmail());
     expect($result)->array();
     expect($result)->hasKey('data');
     expect($result)->hasKey('done');
@@ -66,21 +67,27 @@ class SegmentsExporterTest extends \MailPoetTest {
        [
         'group_id' => 'mailpoet-lists',
         'group_label' => 'MailPoet Mailing Lists',
-        'item_id' => 'list-' . $segment1->id(),
+        'item_id' => 'list-' . $segment1->getId(),
         'data' => [
              ['name' => 'List name', 'value' => 'List 1'],
              ['name' => 'Subscription status', 'value' => 'subscribed'],
-             ['name' => 'Timestamp of the subscription (or last change of the subscription status)', 'value' => '2018-05-02 15:26:52'],
+             [
+               'name' => 'Timestamp of the subscription (or last change of the subscription status)',
+               'value' => $ss1->getUpdatedAt()->format(DateTime::DEFAULT_DATE_TIME_FORMAT)
+             ],
           ],
        ],
        [
         'group_id' => 'mailpoet-lists',
         'group_label' => 'MailPoet Mailing Lists',
-        'item_id' => 'list-' . $segment2->id(),
+        'item_id' => 'list-' . $segment2->getId(),
         'data' => [
              ['name' => 'List name', 'value' => 'List 2'],
              ['name' => 'Subscription status', 'value' => 'unsubscribed'],
-             ['name' => 'Timestamp of the subscription (or last change of the subscription status)', 'value' => '2018-05-02 15:26:00'],
+             [
+               'name' => 'Timestamp of the subscription (or last change of the subscription status)',
+               'value' => $ss2->getUpdatedAt()->format(DateTime::DEFAULT_DATE_TIME_FORMAT)
+             ],
           ],
        ],
     ];
