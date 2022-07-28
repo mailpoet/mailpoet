c6f9811c6686721f0afebb489c529bf668aea051
Jan Jakes <jan.jakes@automattic.com>
2022-07-28 16:08:13 +0200

    Add workflow and step before save hooks
    [MAILPOET-4515]

--- mailpoet/lib/Automation/Engine/Hooks.php
+++ mailpoet/lib/Automation/Engine/Hooks.php
@@ -2,10 +2,33 @@
 
 namespace MailPoet\Automation\Engine;
 
+use MailPoet\Automation\Engine\Data\Step;
+use MailPoet\Automation\Engine\Data\Workflow;
+
 class Hooks {
+  /** @var WordPress */
+  private $wordPress;
+
+  public function __construct(
+    WordPress $wordPress
+  ) {
+    $this->wordPress = $wordPress;
+  }
+
   public const INITIALIZE = 'mailpoet/automation/initialize';
   public const API_INITIALIZE = 'mailpoet/automation/api/initialize';
   public const STEP_RUNNER_INITIALIZE = 'mailpoet/automation/step_runner/initialize';
   public const TRIGGER = 'mailpoet/automation/trigger';
   public const WORKFLOW_STEP = 'mailpoet/automation/workflow/step';
+
+  public const WORKFLOW_BEFORE_SAVE = 'mailpoet/automation/workflow/before_save';
+  public const WORKFLOW_STEP_BEFORE_SAVE = 'mailpoet/automation/workflow/step/before_save';
+
+  public function doWorkflowBeforeSave(Workflow $workflow): void {
+    $this->wordPress->doAction(self::WORKFLOW_BEFORE_SAVE, $workflow);
+  }
+
+  public function doWorkflowStepBeforeSave(Step $step): void {
+    $this->wordPress->doAction(self::WORKFLOW_STEP_BEFORE_SAVE, $step);
+  }
 }
