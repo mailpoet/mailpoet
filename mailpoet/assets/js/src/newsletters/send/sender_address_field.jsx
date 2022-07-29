eab3e320e7cfdc4843a763ccbd45be6160c6124e
Rostislav Wolny <rosta.wolny@automattic.com>
2022-07-29 12:32:34 +0200

    Refactor sender address errors rendering on send page
    Both auht. email error and DMARC check warning were rendered via parsley error.
    There was not enough space for both messages to display and also both were rendered
    as errors that block sending. But so far we allow sending when the DMARC warning is present.
    
    This commit changes displaying of both error messages so that they are rendered in
    a custom component and use the parsley only to mark the input red.
    [MAILPOET-4302]

--- mailpoet/assets/js/src/newsletters/send/sender_address_field.jsx
+++ mailpoet/assets/js/src/newsletters/send/sender_address_field.jsx
@@ -1,13 +1,11 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import ReactStringReplace from 'react-string-replace';
-import { MailPoet } from 'mailpoet';
 import { FormFieldText } from 'form/fields/text.jsx';
 import { SenderEmailAddressWarning } from 'common/sender_email_address_warning.jsx';
 import {
   isFieldValid,
   addOrUpdateError,
-  removeError,
+  resetFieldError,
   validateField,
 } from 'common/functions/parsley_helper_functions';
 import { checkSenderEmailDomainDmarcPolicy } from 'common/check_sender_domain_dmarc_policy';
@@ -17,6 +15,8 @@ class SenderField extends Component {
     super(props);
     this.state = {
       emailAddress: props.item.sender_address,
+      showSenderDomainWarning: false,
+      showAuthEmailsError: false,
     };
     this.onChange = this.onChange.bind(this);
     this.onBlur = this.onBlur.bind(this);
@@ -24,7 +24,6 @@ class SenderField extends Component {
     const fieldId = props.field.id || `field_${props.field.name}`;
     this.domElementSelector = `#${fieldId}`;
     this.parsleyFieldName = 'invalidFromAddress';
-    this.parsleySenderDomainFieldName = 'invalidSenderDomain';
   }
 
   onChange(event) {
@@ -41,8 +40,8 @@ class SenderField extends Component {
       },
     });
     // hide email address warning when user is typing
-    removeError(this.domElementSelector, this.parsleyFieldName);
-    removeError(this.domElementSelector, this.parsleySenderDomainFieldName);
+    this.setState({ showAuthEmailsError: false });
+    resetFieldError(this.domElementSelector, this.parsleyFieldName);
   }
 
   onBlur() {
@@ -64,33 +63,17 @@ class SenderField extends Component {
   isEmailAddressAuthorized = (email) =>
     (window.mailpoet_authorized_emails || []).includes(email);
 
-  showInvalidFromAddressError = (emailAddress) => {
-    const fromAddress = emailAddress;
-    let errorMessage = ReactStringReplace(
-      MailPoet.I18n.t('newsletterInvalidFromAddress'),
-      '%1$s',
-      () => fromAddress,
-    );
-    errorMessage = ReactStringReplace(
-      errorMessage,
-      /\[link\](.*?)\[\/link\]/g,
-      (match) =>
-        `<a href="https://account.mailpoet.com/authorization?email=${encodeURIComponent(
-          fromAddress,
-        )}" target="_blank" class="mailpoet-js-button-authorize-email-and-sender-domain" data-email="${fromAddress}" data-type="email" rel="noopener noreferrer">${match}</a>`,
-    );
-
-    addOrUpdateError(
-      this.domElementSelector,
-      this.parsleyFieldName,
-      errorMessage.join(''),
-    );
+  showInvalidFromAddressError = () => {
+    // We add an empty error to the parsley validator on the field
+    // The error message is too big to fit into space we have for parsley errors. We render it in own component SenderEmailAddressWarning.
+    addOrUpdateError(this.domElementSelector, this.parsleyFieldName, ' ');
+    this.setState({ showAuthEmailsError: true });
   };
 
   showSenderFieldError = (emailAddressIsAuthorized, emailAddress) => {
     if (!window.mailpoet_mss_active) return;
 
-    removeError(this.domElementSelector, this.parsleyFieldName);
+    resetFieldError(this.domElementSelector, this.parsleyFieldName);
 
     if (!isFieldValid(this.domElementSelector)) {
       validateField(this.domElementSelector);
@@ -102,21 +85,10 @@ class SenderField extends Component {
     }
   };
 
-  showSenderDomainError = (status, emailAddress) => {
-    if (!status) return;
+  showSenderDomainError = (status) => {
+    if (!status || !window.mailpoet_mss_active) return;
 
-    const errorMessage = ReactStringReplace(
-      MailPoet.I18n.t('authorizeSenderDomain'),
-      /\[link\](.*?)\[\/link\]/g,
-      (match) =>
-        `<a href="https://kb.mailpoet.com/article/295-spf-and-dkim" target="_blank" class="mailpoet-js-button-authorize-email-and-sender-domain" data-email="${emailAddress}" data-type="domain" rel="noopener noreferrer">${match}</a>`,
-    );
-
-    addOrUpdateError(
-      this.domElementSelector,
-      this.parsleySenderDomainFieldName,
-      errorMessage,
-    );
+    this.setState({ showSenderDomainWarning: true });
   };
 
   render() {
@@ -132,10 +104,15 @@ class SenderField extends Component {
           onBlurEvent={this.onBlur}
         />
 
-        <div className="regular-text" style={{ marginTop: '2rem' }}>
+        <div className="regular-text">
           <SenderEmailAddressWarning
             emailAddress={this.state.emailAddress}
             mssActive={window.mailpoet_mss_active}
+            isEmailAuthorized={!this.state.showAuthEmailsError}
+            showSenderDomainWarning={this.state.showSenderDomainWarning}
+            onSuccessfulEmailOrDomainAuthorization={() =>
+              this.setState({ showSenderDomainWarning: false })
+            }
           />
         </div>
       </>
