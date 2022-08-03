c25037ac2bf64a62d5424f44027881d5aa92a0f4
Jan Lysý <jan.lysy@automattic.com>
2022-08-03 13:59:16 +0200

    Add tag filter into subscribers response
    [MAILPOET-4442]

--- mailpoet/lib/Subscribers/SubscribersCountsController.php
+++ mailpoet/lib/Subscribers/SubscribersCountsController.php
@@ -7,6 +7,7 @@ use MailPoet\Entities\SegmentEntity;
 use MailPoet\InvalidStateException;
 use MailPoet\Segments\SegmentsRepository;
 use MailPoet\Segments\SegmentSubscribersRepository;
+use MailPoet\Tags\TagRepository;
 
 class SubscribersCountsController {
   /** @var SegmentsRepository */
@@ -15,18 +16,23 @@ class SubscribersCountsController {
   /** @var SegmentSubscribersRepository */
   private $segmentSubscribersRepository;
 
+  /** @var TagRepository */
+  private $tagRepository;
+
   /** @var TransientCache */
   private $transientCache;
 
   public function __construct(
     SegmentsRepository $segmentsRepository,
     SegmentSubscribersRepository $segmentSubscribersRepository,
+    TagRepository $subscriberTagRepository,
     TransientCache $transientCache
   ) {
 
     $this->segmentSubscribersRepository = $segmentSubscribersRepository;
     $this->transientCache = $transientCache;
     $this->segmentsRepository = $segmentsRepository;
+    $this->tagRepository = $subscriberTagRepository;
   }
 
   public function getSubscribersWithoutSegmentStatisticsCount(): array {
@@ -106,5 +112,12 @@ class SubscribersCountsController {
         $this->transientCache->invalidateItem(TransientCache::SUBSCRIBERS_GLOBAL_STATUS_STATISTICS_COUNT_KEY, $id);
       }
     }
+  }
+
+  /**
+   * @return array<int, array{id: int, name: string, subscribersCount: int}>
+   */
+  public function getTagsStatisticsCount(?string $status, bool $isDeleted): array {
+    return $this->tagRepository->getSubscriberStatisticsCount($status, $isDeleted);
   }
 }
