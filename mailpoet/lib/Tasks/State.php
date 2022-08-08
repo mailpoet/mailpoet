mailpoet/mailpoet

commit f44afbddf27cd9731d5b9ef6da399a11823906d1

Remove SendingQueue model usages
[MAILPOET-4361]

mailpoet/lib/Tasks/State.php
@@ -3,19 +3,23 @@
 namespace MailPoet\Tasks;
 
 use MailPoet\Cron\Workers\Scheduler;
-use MailPoet\Models\Newsletter;
 use MailPoet\Models\ScheduledTask;
-use MailPoet\Models\SendingQueue;
+use MailPoet\Newsletter\Sending\SendingQueuesRepository;
 use MailPoet\Newsletter\Url as NewsletterUrl;
 
 class State {
   /** @var NewsletterUrl */
   private $newsletterUrl;
 
+  /*** @var SendingQueuesRepository */
+  private $sendingQueuesRepository;
+
   public function __construct(
-    NewsletterUrl $newsletterUrl
+    NewsletterUrl $newsletterUrl,
+    SendingQueuesRepository $sendingQueuesRepository
   ) {
     $this->newsletterUrl = $newsletterUrl;
+    $this->sendingQueuesRepository = $sendingQueuesRepository;
   }
 
   /**
@@ -83,8 +87,8 @@ class State {
   private function buildTaskData(ScheduledTask $task) {
     $queue = $newsletter = null;
     if ($task->type === Sending::TASK_TYPE) {
-      $queue = SendingQueue::where('task_id', $task->id)->findOne();
-      $newsletter = $queue instanceof SendingQueue ? $queue->newsletter()->findOne() : null;
+      $queue = $this->sendingQueuesRepository->findOneById($task->id);
+      $newsletter = $queue ? $queue->getNewsletter() : null;
     }
     return [
       'id' => (int)$task->id,
@@ -93,15 +97,11 @@ class State {
       'updated_at' => $task->updatedAt,
       'scheduled_at' => $task->scheduledAt ? $task->scheduledAt : null,
       'status' => $task->status,
-      'newsletter' => (($queue instanceof SendingQueue) && ($newsletter instanceof Newsletter)) ? [
-        'newsletter_id' => (int)$queue->newsletterId,
-        'queue_id' => (int)$queue->id,
-        'subject' => $queue->newsletterRenderedSubject ?: $newsletter->subject,
-        'preview_url' => $this->newsletterUrl->getViewInBrowserUrl(
-          $newsletter,
-          null,
-          $queue
-        ),
+      'newsletter' => $queue && $newsletter ? [
+        'newsletter_id' => $newsletter->getId(),
+        'queue_id' => $queue->getId(),
+        'subject' => $queue->getNewsletterRenderedSubject() ?: $newsletter->getSubject(),
+        'preview_url' => $this->newsletterUrl->getViewInBrowserUrl($newsletter, null, $queue),
       ] : [
         'newsletter_id' => null,
         'queue_id' => null,

mailpoet/lib/Config/Shortcodes.php
@@ -18,7 +18,6 @@ use MailPoet\Segments\SegmentSubscribersRepository;
 use MailPoet\Subscribers\SubscribersRepository;
 use MailPoet\Subscription\Pages;
 use MailPoet\WP\Functions as WPFunctions;
-use MailPoetVendor\Doctrine\ORM\EntityManager;
 
 class Shortcodes {
   /** @var Pages */
@@ -39,9 +38,6 @@ class Shortcodes {
   /** @var NewslettersRepository */
   private $newslettersRepository;
 
-  /** @var EntityManager */
-  private $entityManager;
-
   /** @var Date */
   private $dateCategory;
 
@@ -61,7 +57,6 @@ class Shortcodes {
     SubscribersRepository $subscribersRepository,
     NewsletterUrl $newsletterUrl,
     NewslettersRepository $newslettersRepository,
-    EntityManager $entityManager,
     Date $dateCategory,
     Link $linkCategory,
     Newsletter $newsletterCategory,
@@ -73,7 +68,6 @@ class Shortcodes {
     $this->subscribersRepository = $subscribersRepository;
     $this->newsletterUrl = $newsletterUrl;
     $this->newslettersRepository = $newslettersRepository;
-    $this->entityManager = $entityManager;
     $this->dateCategory = $dateCategory;
     $this->linkCategory = $linkCategory;
     $this->newsletterCategory = $newsletterCategory;
@@ -235,7 +229,7 @@ class Shortcodes {
     $shortcodeProcessor->setQueue($queue);
     return '<a href="' . esc_attr($previewUrl) . '" target="_blank" title="'
       . esc_attr(__('Preview in a new tab', 'mailpoet')) . '">'
-      . esc_attr((string)$shortcodeProcessor->replace($queue->getNewsletterRenderedSubject())) .
+      . esc_attr((string)$shortcodeProcessor->replace($queue ? $queue->getNewsletterRenderedSubject() : '')) .
       '</a>';
   }
 }

mailpoet/lib/Newsletter/Shortcodes/Categories/Link.php
@@ -5,7 +5,6 @@ namespace MailPoet\Newsletter\Shortcodes\Categories;
 use MailPoet\Entities\NewsletterEntity;
 use MailPoet\Entities\SendingQueueEntity;
 use MailPoet\Entities\SubscriberEntity;
-use MailPoet\Models\SendingQueue;
 use MailPoet\Newsletter\Url as NewsletterUrl;
 use MailPoet\Settings\SettingsController;
 use MailPoet\Settings\TrackingConfig;
@@ -48,13 +47,15 @@ class Link implements CategoryInterface {
     bool $wpUserPreview = false
   ): ?string {
     $subscriptionUrlFactory = SubscriptionUrlFactory::getInstance();
-    $queueModel = $this->getQueueModel($queue);
 
     switch ($shortcodeDetails['action']) {
       case 'subscription_unsubscribe_url':
         return self::processUrl(
           $shortcodeDetails['action'],
-          $subscriptionUrlFactory->getConfirmUnsubscribeUrl($wpUserPreview ? null : $subscriber, self::getSendingQueueId($queue)),
+          $subscriptionUrlFactory->getConfirmUnsubscribeUrl(
+            $wpUserPreview ? null : $subscriber,
+            $queue ? $queue->getId() : null
+          ),
           $queue,
           $wpUserPreview
         );
@@ -62,7 +63,10 @@ class Link implements CategoryInterface {
       case 'subscription_instant_unsubscribe_url':
         return self::processUrl(
           $shortcodeDetails['action'],
-          $subscriptionUrlFactory->getUnsubscribeUrl($wpUserPreview ? null : $subscriber, self::getSendingQueueId($queue)),
+          $subscriptionUrlFactory->getUnsubscribeUrl(
+            $wpUserPreview ? null : $subscriber,
+            $queue ? $queue->getId() : null
+          ),
           $queue,
           $wpUserPreview
         );
@@ -79,7 +83,7 @@ class Link implements CategoryInterface {
         $url = $this->newsletterUrl->getViewInBrowserUrl(
           $newsletter,
           $wpUserPreview ? null : $subscriber,
-          $queueModel,
+          $queue,
           $wpUserPreview
         );
         return self::processUrl($shortcodeDetails['action'], $url, $queue, $wpUserPreview);
@@ -106,7 +110,7 @@ class Link implements CategoryInterface {
     }
   }
 
-  public function processUrl($action, $url, $queue, $wpUserPreview = false): string {
+  public function processUrl($action, $url, ?SendingQueueEntity $queue, $wpUserPreview = false): string {
     if ($wpUserPreview) return $url;
     return ($queue && $this->trackingConfig->isEmailTrackingEnabled()) ?
       self::getFullShortcode($action) :
@@ -120,14 +124,19 @@ class Link implements CategoryInterface {
     SendingQueueEntity $queue = null,
     $wpUserPreview = false
   ): ?string {
-    $queueModel = $this->getQueueModel($queue);
     $subscriptionUrlFactory = SubscriptionUrlFactory::getInstance();
     switch ($shortcodeAction) {
       case 'subscription_unsubscribe_url':
-        $url = $subscriptionUrlFactory->getConfirmUnsubscribeUrl($subscriber, self::getSendingQueueId($queue ? $queue) : null);
+        $url = $subscriptionUrlFactory->getConfirmUnsubscribeUrl(
+          $subscriber,
+          $queue ? $queue->getId() : null
+        );
         break;
       case 'subscription_instant_unsubscribe_url':
-        $url = $subscriptionUrlFactory->getUnsubscribeUrl($subscriber, self::getSendingQueueId($queue ? $queue) : null);
+        $url = $subscriptionUrlFactory->getUnsubscribeUrl(
+          $subscriber,
+          $queue ? $queue->getId() : null
+        );
         break;
       case 'subscription_manage_url':
         $url = $subscriptionUrlFactory->getManageUrl($subscriber);
@@ -136,7 +145,7 @@ class Link implements CategoryInterface {
         $url = $this->newsletterUrl->getViewInBrowserUrl(
           $newsletter,
           $subscriber,
-          $queueModel,
+          $queue,
           false
         );
         break;
@@ -161,20 +170,5 @@ class Link implements CategoryInterface {
 
   private function getFullShortcode($action): string {
     return sprintf('[link:%s]', $action);
-  }
-
-  private function getSendingQueueId($queue): ?int {
-    if ($queue instanceof SendingQueueEntity) {
-      return $queue->getId();
-    }
-    return null;
-  }
-
-  // temporary function until Links are refactored to Doctrine
-  private function getQueueModel(SendingQueueEntity $queue = null): ?SendingQueue {
-    if (!$queue) return null;
-    $queueModel = SendingQueue::where('id', $queue->getId())->findOne();
-    if ($queueModel) return $queueModel;
-    return null;
   }
 }

mailpoet/lib/Newsletter/Url.php
@@ -20,9 +20,9 @@ class Url {
   }
 
   public function getViewInBrowserUrl(
-    NewsletterEntity $newsletter = null,
-    SubscriberEntity $subscriber = null,
-    $queue = false,
+    ?NewsletterEntity $newsletter,
+    ?SubscriberEntity $subscriber = null,
+    ?SendingQueueEntity $queue = null,
     bool $preview = true
   ) {
     $data = $this->createUrlDataObject($newsletter, $subscriber, $queue, $preview);
@@ -33,15 +33,15 @@ class Url {
     );
   }
 
-  public function createUrlDataObject(NewsletterEntity $newsletter = null, SubscriberEntity $subscriber = null, $queue, $preview) {
+  public function createUrlDataObject(
+    ?NewsletterEntity $newsletter,
+    ?SubscriberEntity $subscriber,
+    ?SendingQueueEntity $queue,
+    bool $preview
+  ) {
     $newsletterId = $newsletter && $newsletter->getId() ? $newsletter->getId() : 0;
     $newsletterHash = $newsletter && $newsletter->getHash() ? $newsletter->getHash() : 0;
-
-    if ($queue instanceof SendingQueueEntity) {
-      $sendingQueueId = (!empty($queue->getId())) ? (int)$queue->getId() : 0;
-    } else {
-      $sendingQueueId = (!empty($queue->id)) ? (int)$queue->id : 0;
-    }
+    $sendingQueueId = $queue && $queue->getId() ? $queue->getId() : 0;
 
     return [
       $newsletterId,

mailpoet/lib/Newsletter/ViewInBrowser/ViewInBrowserRenderer.php
@@ -78,8 +78,8 @@ class ViewInBrowserRenderer {
     }
     $this->prepareShortcodes(
       $newsletter,
-      $subscriber ?: false,
-      $queue ?: false,
+      $subscriber,
+      $queue,
       $wpUserPreview
     );
     $renderedNewsletter = $this->shortcodes->replace($newsletterBody);
@@ -93,19 +93,15 @@ class ViewInBrowserRenderer {
     return $renderedNewsletter;
   }
 
-  private function prepareShortcodes($newsletter, $subscriber, $queue, $wpUserPreview) {
-    if ($queue instanceof SendingQueueEntity) {
-      $this->shortcodes->setQueue($queue);
-    }
-
-    if ($newsletter instanceof NewsletterEntity) {
-      $this->shortcodes->setNewsletter($newsletter);
-    }
-
+  private function prepareShortcodes(
+    NewsletterEntity $newsletter,
+    ?SubscriberEntity $subscriber,
+    ?SendingQueueEntity $queue,
+    bool $wpUserPreview
+  ) {
+    $this->shortcodes->setQueue($queue);
+    $this->shortcodes->setNewsletter($newsletter);
     $this->shortcodes->setWpUserPreview($wpUserPreview);
-
-    if ($subscriber instanceof SubscriberEntity) {
-      $this->shortcodes->setSubscriber($subscriber);
-    }
+    $this->shortcodes->setSubscriber($subscriber);
   }
 }

mailpoet/tests/integration/Tasks/StateTest.php
@@ -6,6 +6,7 @@ use MailPoet\Cron\Workers\SendingQueue\Migration;
 use MailPoet\Models\Newsletter;
 use MailPoet\Models\ScheduledTask;
 use MailPoet\Models\SendingQueue;
+use MailPoet\Newsletter\Sending\SendingQueuesRepository;
 use MailPoet\Newsletter\Url;
 use MailPoet\Tasks\Sending as SendingTask;
 use MailPoet\Tasks\State;
@@ -19,7 +20,8 @@ class StateTest extends \MailPoetTest {
   public function _before() {
     parent::_before();
     $this->tasksState = new State(
-      $this->diContainer->get(Url::class)
+      $this->diContainer->get(Url::class),
+      $this->diContainer->get(SendingQueuesRepository::class)
     );
   }
 

commit 50c42c0b451f30275b2a9914c400e36f135f04ae

Fix bug when displaying email subjects in the system status page
The commit f44afbddf27cd9731d5b9ef6da399a11823906d1 introduced a bug
when replacing Paris code with Doctrine code in lib/Tasks/State.php.
This bug was causing the system status page to display "Preview" instead
of the subject of the listed emails. The problem was happening because
during the refactor there was a mistake and we were trying to get a
SendingQueueEntity via its ID using the ScheduledTaskEntity ID. This
commit fixes this problem by getting a SendingQueueEntity that matches
the given ScheduledTaskEntity ID.

[MAILPOET-4508]

mailpoet/lib/Tasks/State.php
@@ -87,7 +87,7 @@ class State {
   private function buildTaskData(ScheduledTask $task) {
     $queue = $newsletter = null;
     if ($task->type === Sending::TASK_TYPE) {
-      $queue = $this->sendingQueuesRepository->findOneById($task->id);
+      $queue = $this->sendingQueuesRepository->findOneBy(['task' => $task->id]);
       $newsletter = $queue ? $queue->getNewsletter() : null;
     }
     return [