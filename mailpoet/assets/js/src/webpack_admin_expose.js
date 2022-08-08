a01afc27e8972ce9ef63f98e4c02c5a68c43d16c
Jan Jakes <jan.jakes@automattic.com>
2022-08-08 16:24:40 +0200

    Add entrypoint for premium plugin exposed assets and libs
    This simplifies Webpack configuration, removes duplicities between loaders and
    the "admin_vendor" entrypoint definition, and makes the sharing more transparent.
    
    [MAILPOET-4446]

---
+++ mailpoet/assets/js/src/webpack_admin_expose.js
@@ -0,0 +1,22 @@
+// Modules that are exposed for usage in the premium plugin.
+// The exports below are available via "window.MailPoetLib".
+
+// libs
+export * as ClassNames from 'classnames';
+export * as React from 'react';
+export * as ReactDom from 'react-dom';
+export * as ReactJsxRuntime from 'react/jsx-runtime';
+export * as ReactRouter from 'react-router-dom';
+export * as ReactTooltip from 'react-tooltip';
+export * as ReactStringReplace from 'react-string-replace';
+
+// assets
+export * as Common from 'common';
+export * as CommonFormReactSelect from 'common/form/react_select/react_select';
+export * as CommonFormSelect from 'common/form/select/select';
+export * as CommonGrid from 'common/grid';
+export * as HelpTooltip from 'help-tooltip';
+export * as Hooks from 'hooks.js';
+export * as Listing from 'listing';
+export * as DynamicSegmentsTypes from 'segments/dynamic/types';
+export * as WordPressData from 'wp-data-hooks';
