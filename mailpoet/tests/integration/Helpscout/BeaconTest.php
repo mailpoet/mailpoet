d54129670dca2044d1cef280be8de493e2423557
Rodrigo Primo <rodrigo@automattic.com>
2022-08-09 16:02:41 -0300

    Reorganize information sent to HelpScout
    We are now using the `session-data` method of the Beacon API to send
    site information to HelpScout
    (https://developer.helpscout.com/beacon-2/web/javascript-api/#beacon-session-data).
    This method has a limit of 20 attribute-value pairs and since we had
    more, it was necessary to combinte a few values under the same
    attribute.
    
    [MAILPOET-4525]

--- mailpoet/tests/integration/Helpscout/BeaconTest.php
+++ mailpoet/tests/integration/Helpscout/BeaconTest.php
@@ -58,35 +58,35 @@ class BeaconTest extends \MailPoetTest {
   }
 
   public function testItReturnsWpMemoryLimit() {
-    expect($this->beaconData['WP_MEMORY_LIMIT'])->equals(WP_MEMORY_LIMIT);
+    expect($this->beaconData['WP info'])->stringContainsString('WP_MEMORY_LIMIT: ' . WP_MEMORY_LIMIT);
   }
 
   public function testItReturnsWpMaxMemoryLimit() {
-    expect($this->beaconData['WP_MAX_MEMORY_LIMIT'])->equals(WP_MAX_MEMORY_LIMIT);
+    expect($this->beaconData['WP info'])->stringContainsString('WP_MAX_MEMORY_LIMIT: ' . WP_MAX_MEMORY_LIMIT);
   }
 
   public function testItReturnsWpDebugValue() {
-    expect($this->beaconData['WP_DEBUG'])->equals(WP_DEBUG);
-  }
-
-  public function testItReturnsPhpMaxExecutionTime() {
-    expect($this->beaconData['PHP max_execution_time'])->equals(ini_get('max_execution_time'));
-  }
-
-  public function testItReturnsPhpMemoryLimit() {
-    expect($this->beaconData['PHP memory_limit'])->equals(ini_get('memory_limit'));
-  }
-
-  public function testItReturnsPhpUploadMaxFilesize() {
-    expect($this->beaconData['PHP upload_max_filesize'])->equals(ini_get('upload_max_filesize'));
-  }
-
-  public function testItReturnsPhpPostMaxSize() {
-    expect($this->beaconData['PHP post_max_size'])->equals(ini_get('post_max_size'));
+    expect($this->beaconData['WP info'])->stringContainsString('WP_DEBUG: ' . WP_DEBUG);
   }
 
   public function testItReturnsWpLanguage() {
-    expect($this->beaconData['WordPress language'])->equals(get_locale());
+    expect($this->beaconData['WP info'])->stringContainsString('WordPress language: ' . get_locale());
+  }
+
+  public function testItReturnsPhpMaxExecutionTime() {
+    expect($this->beaconData['PHP info'])->stringContainsString('PHP max_execution_time: ' . ini_get('max_execution_time'));
+  }
+
+  public function testItReturnsPhpMemoryLimit() {
+    expect($this->beaconData['PHP info'])->stringContainsString('PHP memory_limit: ' . ini_get('memory_limit'));
+  }
+
+  public function testItReturnsPhpUploadMaxFilesize() {
+    expect($this->beaconData['PHP info'])->stringContainsString('PHP upload_max_filesize: ' . ini_get('upload_max_filesize'));
+  }
+
+  public function testItReturnsPhpPostMaxSize() {
+    expect($this->beaconData['PHP info'])->stringContainsString('PHP post_max_size: ' . ini_get('post_max_size'));
   }
 
   public function testItReturnsIfWpIsMultisite() {
@@ -116,19 +116,19 @@ class BeaconTest extends \MailPoetTest {
   }
 
   public function testItReturnsSomeSettings() {
-    expect($this->beaconData['Task Scheduler method'])->equals($this->settings->get('cron_trigger.method'));
-    expect($this->beaconData['Default FROM address'])->equals($this->settings->get('sender.address'));
-    expect($this->beaconData['Default Reply-To address'])->equals($this->settings->get('reply_to.address'));
-    expect($this->beaconData['Bounce Email Address'])->equals($this->settings->get('bounce.address'));
+    expect($this->beaconData['MailPoet sending info'])->stringContainsString('Task Scheduler method: ' . $this->settings->get('cron_trigger.method'));
+    expect($this->beaconData['MailPoet sending info'])->stringContainsString('Default FROM address: ' . $this->settings->get('sender.address'));
+    expect($this->beaconData['MailPoet sending info'])->stringContainsString('Default Reply-To address: ' . $this->settings->get('reply_to.address'));
+    expect($this->beaconData['MailPoet sending info'])->stringContainsString('Bounce Email Address: ' . $this->settings->get('bounce.address'));
     expect($this->beaconData['Plugin installed at'])->equals($this->settings->get('installed_at'));
   }
 
   public function testItReturnsTransactionalEmailSendingMethod() {
     $this->settings->set('send_transactional_emails', '');
     $beacon = $this->diContainer->get(Beacon::class);
-    expect($beacon->getData()["Send all site's emails with"])->equals('default WordPress sending method');
+    expect($beacon->getData()['MailPoet sending info'])->stringContainsString("Send all site's emails with: default WordPress sending method");
     $this->settings->set('send_transactional_emails', '1');
-    expect($beacon->getData()["Send all site's emails with"])->equals('current sending method');
+    expect($beacon->getData()['MailPoet sending info'])->stringContainsString("Send all site's emails with: current sending method");
   }
 
   public function testItReturnsTotalNumberOfSubscribers() {
@@ -147,7 +147,7 @@ class BeaconTest extends \MailPoetTest {
   }
 
   public function testItReturnsCronPingUrl() {
-    expect($this->beaconData['Cron ping URL'])->stringContainsString('&action=ping');
+    expect($this->beaconData['MailPoet sending info'])->stringContainsString('&action=ping');
     // cron ping URL should react to custom filters
     $filter = function($url) {
       return str_replace(home_url(), 'http://custom_url/', $url);
@@ -155,7 +155,7 @@ class BeaconTest extends \MailPoetTest {
     $wp = new WPFunctions;
     $wp->addFilter('mailpoet_cron_request_url', $filter);
     $beaconData = $this->beaconData = $this->diContainer->get(Beacon::class)->getData();
-    expect($beaconData['Cron ping URL'])->regExp('!^http:\/\/custom_url\/!');
+    expect($beaconData['MailPoet sending info'])->regExp('!http:\/\/custom_url\/!');
     $wp->removeFilter('mailpoet_cron_request_url', $filter);
   }
 
