1a5952a876a95a8de0b068d4a5ca59d9ee5a70fc
Jan Lysý <jan.lysy@automattic.com>
2022-08-09 18:22:08 +0200

    Mark form editor tutorial as seen on init
    [MAILPOET-4498]

--- mailpoet/assets/js/src/newsletter_editor/tutorial.jsx
+++ mailpoet/assets/js/src/newsletter_editor/tutorial.jsx
@@ -13,7 +13,7 @@ export const displayTutorial = () => {
   MailPoet.Modal.popup({
     title: MailPoet.I18n.t('tutorialVideoTitle'),
     template: `<div class="mailpoet_drag_and_drop_tutorial"><video style="height:640px;" src="${window.config.dragDemoUrl}" controls autoplay></video></div>`,
-    onCancel: () => {
+    onInit: () => {
       MailPoet.Ajax.post({
         api_version: window.mailpoet_api_version,
         endpoint: 'user_flags',
