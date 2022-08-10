8861909859df62fdcfaef9f045896a1d8d00c810
David Remer <webmaster@websupporter.net>
2022-08-10 12:31:05 +0300

    Communicate activated_at to editor
    [MAILPOET-4417]

--- mailpoet/lib/AdminPages/Pages/AutomationEditor.php
+++ mailpoet/lib/AdminPages/Pages/AutomationEditor.php
@@ -61,6 +61,7 @@ class AutomationEditor {
       'status' => $workflow->getStatus(),
       'created_at' => $workflow->getCreatedAt()->format(DateTimeImmutable::W3C),
       'updated_at' => $workflow->getUpdatedAt()->format(DateTimeImmutable::W3C),
+      'activated_at' => $workflow->getActivatedAt() ? $workflow->getActivatedAt()->format(DateTimeImmutable::W3C) : null,
       'steps' => array_map(function (Step $step) {
         return [
           'id' => $step->getId(),
