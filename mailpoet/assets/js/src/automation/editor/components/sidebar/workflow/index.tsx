6e396a5a777aacd26a4e0b749c9cb24b82fcfbb3
David Remer <webmaster@websupporter.net>
2022-08-11 10:40:09 +0300

    Add activated date and author to workflow sidebar
    [MAILPOET-4417]

--- mailpoet/assets/js/src/automation/editor/components/sidebar/workflow/index.tsx
+++ mailpoet/assets/js/src/automation/editor/components/sidebar/workflow/index.tsx
@@ -1,6 +1,7 @@
-import { PanelBody } from '@wordpress/components';
+import { PanelBody, PanelRow } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { store } from '../../../store';
+import { PlainBodyTitle } from '../../panel';
 
 export function WorkflowSidebar(): JSX.Element {
   const { workflowData } = useSelect(
@@ -10,26 +11,42 @@ export function WorkflowSidebar(): JSX.Element {
     [],
   );
 
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
   return (
     <PanelBody>
-      <div>
-        <strong>{workflowData.name}</strong>
-      </div>
-      <br />
-      <div>
-        <strong>ID:</strong> {workflowData.id}
-      </div>
-      <div>
-        <strong>Status:</strong> {workflowData.status}
-      </div>
-      <div>
-        <strong>Created:</strong>{' '}
-        {new Date(Date.parse(workflowData.created_at)).toLocaleString()}
-      </div>
-      <div>
-        <strong>Updated:</strong>{' '}
-        {new Date(Date.parse(workflowData.updated_at)).toLocaleString()}
-      </div>
+      <PlainBodyTitle title="Automation details" />
+      <PanelRow>
+        <strong>Date added</strong>{' '}
+        {new Date(Date.parse(workflowData.created_at)).toLocaleDateString(
+          undefined,
+          dateOptions,
+        )}
+      </PanelRow>
+      <PanelRow>
+        <strong>Activated</strong>{' '}
+        {workflowData.status === 'active' &&
+          new Date(Date.parse(workflowData.updated_at)).toLocaleDateString(
+            undefined,
+            dateOptions,
+          )}
+        {workflowData.status !== 'active' &&
+          workflowData.activated_at &&
+          new Date(Date.parse(workflowData.activated_at)).toLocaleDateString(
+            undefined,
+            dateOptions,
+          )}
+        {workflowData.status !== 'active' && !workflowData.activated_at && (
+          <span className="mailpoet-deactive">Not activated yet.</span>
+        )}
+      </PanelRow>
+      <PanelRow>
+        <strong>Author</strong> {workflowData.author.name}
+      </PanelRow>
     </PanelBody>
   );
 }
