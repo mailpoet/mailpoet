d26c2b29e1b4b7522b231719314f6f6a151803ca
David Remer <webmaster@websupporter.net>
2022-08-18 12:17:44 +0300

    Add test for WorkflowTemplatesGetEndpoint
    [MAILPOET-4538]

---
+++ mailpoet/tests/integration/REST/Automation/Workflows/WorkflowTemplatesGetEndpointTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MailPoet\REST\Automation\Workflows;
+
+use MailPoet\REST\Automation\AutomationTest;
+
+require_once __DIR__ . '/../AutomationTest.php';
+
+class WorkflowTemplatesGetEndpointTest extends AutomationTest
+{
+  private const ENDPOINT_PATH = '/mailpoet/v1/automation/workflow-templates';
+
+  public function testGetAllTemplates() {
+    $result = $this->get(self::ENDPOINT_PATH, []);
+    $this->assertCount(1, $result['data']);
+    $this->assertEquals('simple-welcome-email', $result['data'][0]['slug']);
+  }
+
+  public function testGetTemplatesByCategory() {
+    //@ToDo: Once we have templates in other categories, we should make this test more specific.
+    $result = $this->get(self::ENDPOINT_PATH, [
+      'json' => [
+        'category' => 1,
+      ],
+    ]);
+    $this->assertCount(1, $result['data']);
+    $result = $this->get(self::ENDPOINT_PATH, [
+      'json' => [
+        'category' => 2,
+      ],
+    ]);
+    $this->assertCount(0, $result['data']);
+  }
+}
