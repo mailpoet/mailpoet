538647946ead89a934a05124a361a676f27c0b58
Rodrigo Primo <rodrigo@automattic.com>
2022-08-19 17:17:03 -0300

    Refactor GATracking to use Doctrine instead of Paris
    The test class was also refactored.
    
    [MAILPOET-4592]

--- mailpoet/lib/Cron/Workers/SendingQueue/Tasks/Newsletter.php
+++ mailpoet/lib/Cron/Workers/SendingQueue/Tasks/Newsletter.php
@@ -146,7 +146,7 @@ class Newsletter {
         $renderedNewsletter,
         $newsletter
       );
-      $renderedNewsletter = $this->gaTracking->applyGATracking($renderedNewsletter, $newsletter);
+      $renderedNewsletter = $this->gaTracking->applyGATracking($renderedNewsletter, $newsletterEntity);
       // hash and save all links
       $renderedNewsletter = $this->linksTask->process($renderedNewsletter, $newsletter, $sendingTask);
     } else {
@@ -157,7 +157,7 @@ class Newsletter {
         $renderedNewsletter,
         $newsletter
       );
-      $renderedNewsletter = $this->gaTracking->applyGATracking($renderedNewsletter, $newsletter);
+      $renderedNewsletter = $this->gaTracking->applyGATracking($renderedNewsletter, $newsletterEntity);
     }
     // check if this is a post notification and if it contains at least 1 ALC post
     if (
