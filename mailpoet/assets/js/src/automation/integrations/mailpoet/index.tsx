d8a316ea7b099da358e189da19d2edda80cd40bd
Jan Jakes <jan.jakes@automattic.com>
2022-08-25 15:41:02 +0200

    Add WP user registered trigger type
    [PREMIUM-194]
    [MAILPOET-4585]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/index.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/index.tsx
@@ -1,8 +1,10 @@
 import { registerStepType } from '../../editor/store';
 import { step as SendEmailStep } from './steps/send_email';
 import { step as SomeoneSubscribedTrigger } from './steps/someone-subscribed';
+import { step as WpUserRegisteredTrigger } from './steps/wp-user-registered';
 
 export const initialize = (): void => {
   registerStepType(SendEmailStep);
+  registerStepType(WpUserRegisteredTrigger);
   registerStepType(SomeoneSubscribedTrigger);
 };
---
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/steps/wp-user-registered/index.tsx
@@ -0,0 +1,18 @@
+import { __ } from '@wordpress/i18n';
+import { wordpress } from '@wordpress/icons';
+import { StepType } from '../../../../editor/store/types';
+
+export const step: StepType = {
+  key: 'mailpoet:wp-user-registered',
+  group: 'triggers',
+  title: __('WordPress user registered', 'mailpoet'),
+  foreground: '#2271b1',
+  background: '#f0f6fc',
+  description: __(
+    'Starts the automation when a new user registered in WordPress. Use trigger filters to filter by a specific user role.',
+    'mailpoet',
+  ),
+  subtitle: () => __('Trigger', 'mailpoet'),
+  icon: () => wordpress,
+  edit: () => <div />,
+} as const;
