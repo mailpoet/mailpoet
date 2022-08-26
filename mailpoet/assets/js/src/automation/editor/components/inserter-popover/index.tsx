3d5b2d5880b5604d7f0a080d92c48fd195e979eb
Jan Jakes <jan.jakes@automattic.com>
2022-08-26 11:40:36 +0200

    Show premium upsell modal when trying to add a step
    [PREMIUM-194]

--- mailpoet/assets/js/src/automation/editor/components/inserter-popover/index.tsx
+++ mailpoet/assets/js/src/automation/editor/components/inserter-popover/index.tsx
@@ -1,28 +1,62 @@
+import { useCallback, useRef, useState } from 'react';
 import { Popover } from '@wordpress/components';
-import { dispatch, useSelect } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
+import { Hooks } from 'wp-js-hooks';
+import { PremiumModal } from 'common/premium_modal';
 import { Inserter } from '../inserter';
+import { Item } from '../inserter/item';
 import { store } from '../../store';
 
 export function InserterPopover(): JSX.Element | null {
+  const popoverRef = useRef<HTMLDivElement>();
+  const [showModal, setShowModal] = useState(false);
   const { inserterPopover } = useSelect(
     (select) => ({
       inserterPopover: select(store).getInserterPopover(),
     }),
     [],
   );
+  const { setInserterPopover } = useDispatch(store);
 
-  const { setInserterPopover } = dispatch(store);
+  const onInsert = useCallback((item: Item) => {
+    const addStepCallback = Hooks.applyFilters(
+      'mailpoet.automation.workflow.add_step_callback',
+      () => {
+        setShowModal(true);
+      },
+    );
+    addStepCallback(item);
+  }, []);
 
   if (!inserterPopover) {
     return null;
   }
 
   return (
-    <Popover
-      anchorRect={inserterPopover.anchor.getBoundingClientRect()}
-      onClose={() => setInserterPopover(undefined)}
-    >
-      <Inserter />
-    </Popover>
+    <>
+      <Popover
+        ref={popoverRef}
+        anchorRect={inserterPopover.anchor.getBoundingClientRect()}
+        onClose={() => {
+          if (!showModal) {
+            setInserterPopover(undefined);
+          }
+        }}
+      >
+        <Inserter onInsert={onInsert} />
+      </Popover>
+
+      {showModal && (
+        <PremiumModal
+          onRequestClose={() => {
+            setShowModal(false);
+            popoverRef.current?.focus();
+          }}
+        >
+          {__('You cannot add a new step to the automation.', 'mailpoet')}
+        </PremiumModal>
+      )}
+    </>
   );
 }
