1e464fe99c4e47887080b3a5dd29608183a33a40
Jan Jakes <jan.jakes@automattic.com>
2022-08-26 13:13:27 +0200

    Show premium upsell modal when trying to delete a step
    [PREMIUM-194]

--- mailpoet/assets/js/src/automation/editor/components/workflow/step-more-menu.tsx
+++ mailpoet/assets/js/src/automation/editor/components/workflow/step-more-menu.tsx
@@ -1,6 +1,9 @@
+import { useCallback, useState } from 'react';
 import { DropdownMenu } from '@wordpress/components';
 import { moreVertical, trash } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
+import { Hooks } from 'wp-js-hooks';
+import { PremiumModal } from 'common/premium_modal';
 import { Step as StepData } from './types';
 
 type Props = {
@@ -8,20 +11,45 @@ type Props = {
 };
 
 export function StepMoreMenu({ step }: Props): JSX.Element {
+  const [showModal, setShowModal] = useState(false);
+
+  const onDelete = useCallback((stepData: StepData) => {
+    const deleteStepCallback = Hooks.applyFilters(
+      'mailpoet.automation.workflow.delete_step_callback',
+      () => {
+        setShowModal(true);
+      },
+    );
+    deleteStepCallback(stepData);
+  }, []);
+
   return (
-    <div className="mailpoet-automation-step-more-menu">
-      <DropdownMenu
-        label={__('More', 'mailpoet')}
-        icon={moreVertical}
-        controls={[
-          {
-            title: __('Delete step', 'mailpoet'),
-            icon: trash,
-          },
-        ]}
-        popoverProps={{ position: 'bottom right' }}
-        toggleProps={{ isSmall: true }}
-      />
-    </div>
+    <>
+      <div className="mailpoet-automation-step-more-menu">
+        <DropdownMenu
+          label={__('More', 'mailpoet')}
+          icon={moreVertical}
+          controls={[
+            {
+              title: __('Delete step', 'mailpoet'),
+              icon: trash,
+              onClick: () => onDelete(step),
+            },
+          ]}
+          popoverProps={{ position: 'bottom right' }}
+          toggleProps={{ isSmall: true }}
+        />
+      </div>
+
+      {showModal && (
+        <PremiumModal
+          onRequestClose={() => {
+            setShowModal(false);
+          }}
+        >
+          {__('You cannot remove a new step from the automation.', 'mailpoet')}
+        </PremiumModal>
+      )}
+    </>
   );
 }
