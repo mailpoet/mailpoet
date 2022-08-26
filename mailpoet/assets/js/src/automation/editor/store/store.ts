f8471e66603463ca503bda230d5bdb8b462883f7
Jan Jakes <jan.jakes@automattic.com>
2022-08-26 14:56:03 +0200

    Add hook for editor store extensibility
    This will be used in the premium plugin.
    
    [PREMIUM-194]

--- mailpoet/assets/js/src/automation/editor/store/store.ts
+++ mailpoet/assets/js/src/automation/editor/store/store.ts
@@ -1,5 +1,11 @@
-import { createReduxStore, register, StoreDescriptor } from '@wordpress/data';
+import {
+  createReduxStore,
+  register,
+  StoreConfig,
+  StoreDescriptor,
+} from '@wordpress/data';
 import { controls } from '@wordpress/data-controls';
+import { Hooks } from 'wp-js-hooks';
 import * as actions from './actions';
 import { storeName } from './constants';
 import { getInitialState } from './initial_state';
@@ -13,14 +19,19 @@ type StoreType = Omit<StoreDescriptor, 'name'> & {
 };
 
 export const createStore = (): StoreType => {
-  const store = createReduxStore<State>(storeName, {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any -- the "Action" type is missing thunks with "dispatch"
-    actions: actions as any,
-    controls,
-    selectors,
-    reducer,
-    initialState: getInitialState(),
-  }) as StoreType;
+  const storeConfig = Hooks.applyFilters(
+    'mailpoet.automation.editor.create_store',
+    {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any -- the "Action" type is missing thunks with "dispatch"
+      actions: actions as any,
+      controls,
+      selectors,
+      reducer,
+      initialState: getInitialState(),
+    } as StoreConfig<State>,
+  ) as StoreConfig<State>;
+
+  const store = createReduxStore<State>(storeName, storeConfig) as StoreType;
   register(store);
   return store;
 };
