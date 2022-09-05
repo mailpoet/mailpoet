2b31b02904512e7fe6595c4dbd425e97fa3735ed
Rostislav Wolny <rosta.wolny@automattic.com>
2022-09-05 19:42:55 +0200

    Simplify subscribers last engagement migration by ignoring Woo data
    [MAILPOET-4578]

--- mailpoet/lib/Cron/Workers/SubscribersLastEngagement.php
+++ mailpoet/lib/Cron/Workers/SubscribersLastEngagement.php
@@ -6,8 +6,6 @@ use MailPoet\Entities\ScheduledTaskEntity;
 use MailPoet\Entities\StatisticsClickEntity;
 use MailPoet\Entities\StatisticsOpenEntity;
 use MailPoet\Entities\SubscriberEntity;
-use MailPoet\Util\DBCollationChecker;
-use MailPoet\WooCommerce\Helper as WooCommerceHelper;
 use MailPoetVendor\Doctrine\ORM\EntityManager;
 
 class SubscribersLastEngagement extends SimpleWorker {
@@ -19,24 +17,11 @@ class SubscribersLastEngagement extends SimpleWorker {
   /** @var EntityManager */
   private $entityManager;
 
-  /** @var DBCollationChecker */
-  private $dbCollationChecker;
-
-  /** @var WooCommerceHelper */
-  private $wooCommereHelper;
-
-  /** @var null|string */
-  private $emailCollationCorrection;
-
   public function __construct(
-    EntityManager $entityManager,
-    DBCollationChecker $dbCollationChecker,
-    WooCommerceHelper $wooCommereHelper
+    EntityManager $entityManager
   ) {
     parent::__construct();
     $this->entityManager = $entityManager;
-    $this->dbCollationChecker = $dbCollationChecker;
-    $this->wooCommereHelper = $wooCommereHelper;
   }
 
   public function processTaskStrategy(ScheduledTaskEntity $task, $timer): bool {
@@ -56,22 +41,9 @@ class SubscribersLastEngagement extends SimpleWorker {
   }
 
   private function processBatch(int $minSubscriberId, int $maxSubscriberId): void {
-    global $wpdb;
     $statisticsClicksTable = $this->entityManager->getClassMetadata(StatisticsClickEntity::class)->getTableName();
     $statisticsOpensTable = $this->entityManager->getClassMetadata(StatisticsOpenEntity::class)->getTableName();
     $subscribersTable = $this->entityManager->getClassMetadata(SubscriberEntity::class)->getTableName();
-    $postsTable = $wpdb->posts;
-    $postsmetaTable = $wpdb->postmeta;
-
-    if (is_null($this->emailCollationCorrection)) {
-      $this->emailCollationCorrection = $this->dbCollationChecker->getCollateIfNeeded(
-        $subscribersTable,
-        'email',
-        $postsmetaTable,
-        'meta_value'
-      );
-    }
-    $emailCollate = $this->emailCollationCorrection;
 
     $query = "
       UPDATE $subscribersTable as mps
@@ -81,18 +53,6 @@ class SubscribersLastEngagement extends SimpleWorker {
       WHERE mps.last_engagement_at IS NULL AND mps.id >= $minSubscriberId AND  mps.id < $maxSubscriberId;
     ";
 
-    // Use more complex query that takes into the account also subscriber's latest WooCommerce order
-    if ($this->wooCommereHelper->isWooCommerceActive()) {
-      $query = "
-        UPDATE $subscribersTable as mps
-          LEFT JOIN (SELECT max(created_at) as created_at, subscriber_id FROM $statisticsOpensTable as mpsoinner GROUP BY mpsoinner.subscriber_id) as mpso ON mpso.subscriber_id = mps.id
-          LEFT JOIN (SELECT max(created_at) as created_at, subscriber_id FROM $statisticsClicksTable as mpscinner GROUP BY mpscinner.subscriber_id) as mpsc ON mpsc.subscriber_id = mps.id
-          LEFT JOIN (SELECT MAX(post_id) AS post_id, meta_value as email FROM $postsmetaTable WHERE meta_key = '_billing_email' GROUP BY email) AS newestOrderIds ON newestOrderIds.email $emailCollate = mps.email
-          LEFT JOIN (SELECT ID, post_date FROM $postsTable WHERE post_type = 'shop_order') AS shopOrders ON newestOrderIds.post_id = shopOrders.ID
-        SET mps.last_engagement_at = NULLIF(GREATEST(COALESCE(mpso.created_at, '0'), COALESCE(mpsc.created_at, '0'), COALESCE(shopOrders.post_date, '0')), '0')
-        WHERE mps.last_engagement_at IS NULL AND mps.id >= $minSubscriberId AND  mps.id < $maxSubscriberId;
-      ";
-    }
     $this->entityManager->getConnection()->executeStatement($query);
   }
 
