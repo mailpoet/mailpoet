a0a08c3de2a2c57430e1138a33899474e2dcb436
Jan Lysý <jan.lysy@automattic.com>
2022-09-06 16:47:42 +0200

    Extend tags component by subscriber tags filtering
    [MAILPOET-4564]

--- mailpoet/assets/js/src/common/tag/tags.tsx
+++ mailpoet/assets/js/src/common/tag/tags.tsx
@@ -8,10 +8,18 @@ type Segment = {
   id?: string;
 };
 
+type SubscriberTag = {
+  id: string;
+  name: string;
+  subscriber_id: string;
+  tag_id: string;
+};
+
 type Props = {
   children?: ReactNode;
   dimension?: 'large';
   segments?: Segment[];
+  subscriberTags?: SubscriberTag[];
   strings?: string[];
   variant?: 'average' | 'good' | 'excellent' | 'list' | 'unknown' | 'wordpress';
   isInverted?: boolean;
@@ -21,6 +29,7 @@ function Tags({
   children,
   dimension,
   segments,
+  subscriberTags,
   strings,
   variant,
   isInverted,
@@ -50,6 +59,36 @@ function Tags({
                 data-tip=""
                 data-for={tooltipId}
                 href={`admin.php?page=mailpoet-subscribers#/filter[segment=${segment.id}]`}
+              >
+                {tag}
+              </a>
+            </div>
+          );
+        })}
+      {subscriberTags &&
+        subscriberTags.map((subscriberTag) => {
+          const randomId = Math.random().toString(36).substring(2, 15);
+          const tooltipId = `tag-tooltip-${randomId}`;
+          const tag = (
+            <Tag
+              key={subscriberTag.name}
+              dimension={dimension}
+              variant={variant || 'list'}
+              isInverted={isInverted}
+            >
+              {subscriberTag.name}
+            </Tag>
+          );
+
+          return (
+            <div key={randomId}>
+              <Tooltip id={tooltipId} place="top">
+                {MailPoet.I18n.t('viewFilteredSubscribersMessage')}
+              </Tooltip>
+              <a
+                data-tip=""
+                data-for={tooltipId}
+                href={`admin.php?page=mailpoet-subscribers#/filter[tag=${subscriberTag.tag_id}]`}
               >
                 {tag}
               </a>
