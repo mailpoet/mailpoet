becf82ef8c0ba5fa202ed2fc61b8463f4582c520
Jan Jakes <jan.jakes@automattic.com>
2022-09-06 13:27:58 +0200

    Use confirmation dialog from WP components
    [MAILPOET-4523]

--- mailpoet/assets/js/src/automation/editor/components/actions/trash-button.tsx
+++ mailpoet/assets/js/src/automation/editor/components/actions/trash-button.tsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
 import apiFetch from '@wordpress/api-fetch';
-import { Button } from '@wordpress/components';
+import {
+  Button,
+  __experimentalConfirmDialog as ConfirmDialog,
+} from '@wordpress/components';
 import { StoreDescriptor, useDispatch, useSelect } from '@wordpress/data';
 import { store as noticesStore } from '@wordpress/notices';
 import { addQueryArgs } from '@wordpress/url';
-import { confirmAlert } from '../../../../common/confirm_alert';
 import { storeName } from '../../store';
 import { Workflow } from '../workflow/types';
 import { MailPoet } from '../../../../mailpoet';
 import { LISTING_NOTICE_PARAMETERS } from '../../../listing/workflow-listing-notices';
 
 export function TrashButton(): JSX.Element {
+  const [showConfirmDialog, setShowConfirmDialog] = useState(false);
   const { createErrorNotice } = useDispatch(noticesStore as StoreDescriptor);
   const { workflow } = useSelect(
     (select) => ({
@@ -43,28 +47,34 @@ export function TrashButton(): JSX.Element {
         void createErrorNotice('An error occurred!', {
           explicitDismiss: true,
         });
+      })
+      .finally(() => {
+        setShowConfirmDialog(false);
       });
 
     return true;
   };
 
   return (
-    <Button
-      isSecondary
-      isDestructive
-      onClick={() => {
-        confirmAlert({
-          title: 'Delete workflow',
-          message: `You are about to delete the “${workflow.name}” workflow`,
-          cancelLabel: 'Cancel',
-          confirmLabel: 'Yes, delete',
-          onConfirm: () => {
-            trash();
-          },
-        });
-      }}
-    >
-      Move to Trash
-    </Button>
+    <>
+      <ConfirmDialog
+        isOpen={showConfirmDialog}
+        title="Delete workflow"
+        confirmButtonText="Yes, delete"
+        onConfirm={trash}
+        onCancel={() => setShowConfirmDialog(false)}
+        __experimentalHideHeader={false}
+      >
+        You are about to delete the “{workflow.name}” workflow.
+      </ConfirmDialog>
+
+      <Button
+        isSecondary
+        isDestructive
+        onClick={() => setShowConfirmDialog(true)}
+      >
+        Move to Trash
+      </Button>
+    </>
   );
 }
