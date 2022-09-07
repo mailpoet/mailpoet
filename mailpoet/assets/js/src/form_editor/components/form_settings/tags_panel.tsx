aaf5af2277d532ee911d0a7f8e85e53b5fc7fb7c
Jan Lysý <jan.lysy@automattic.com>
2022-09-07 18:19:19 +0200

    Add token field input into tags panel
    [MAILPOET-4557]

--- mailpoet/assets/js/src/form_editor/components/form_settings/tags_panel.tsx
+++ mailpoet/assets/js/src/form_editor/components/form_settings/tags_panel.tsx
@@ -1,14 +1,38 @@
 import { Panel, PanelBody } from '@wordpress/components';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { isEqual } from 'lodash';
+import { TokenField } from '../../../common/form/tokenField/tokenField';
+import { MailPoet } from '../../../mailpoet';
 
 export function TagsPanel({ onToggle, isOpened }) {
+  const settings = useSelect(
+    (select) => select('mailpoet-form-editor').getFormSettings(),
+    [],
+  );
 
+  const { changeFormSettings } = useDispatch('mailpoet-form-editor');
+
+  const onSegmentsChange = (e) => {
+    if (isEqual(settings.tags, e.value)) {
+      return;
+    }
+
+    void changeFormSettings({
+      ...settings,
+      tags: e.value,
+    });
+  };
+
+  const tags = MailPoet.tags.map((tag) => tag.name);
   return (
     <Panel>
-      <PanelBody
-        title="Tags"
-        opened={isOpened}
-        onToggle={onToggle}
-      >
+      <PanelBody title="Tags" opened={isOpened} onToggle={onToggle}>
+        <TokenField
+          label={MailPoet.I18n.t('addNewTag')}
+          onChange={onSegmentsChange}
+          suggestedValues={tags}
+          selectedValues={settings.tags}
+        />
       </PanelBody>
     </Panel>
   );
