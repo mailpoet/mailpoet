2bcef7629cd44a9732ecf46571fe7b1135b20552
John Oleksowicz <johnoleksowicz@icloud.com>
2022-09-12 16:18:26 -0500

    Mention that shortcodes can be used for subject/preheader
    [MAILPOET-4622]

---
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/steps/send_email/edit/shortcode_help_text.tsx
@@ -0,0 +1,15 @@
+export function ShortcodeHelpText(): JSX.Element {
+  return (
+    <div className="mailpoet-shortcode-selector">
+      You can use{' '}
+      <a
+        href="https://kb.mailpoet.com/article/215-personalize-newsletter-with-shortcodes"
+        target="_blank"
+        rel="noopener noreferrer"
+        data-beacon-article="59d662ef042863379ddc6faa"
+      >
+        MailPoet shortcodes
+      </a>
+    </div>
+  );
+}
