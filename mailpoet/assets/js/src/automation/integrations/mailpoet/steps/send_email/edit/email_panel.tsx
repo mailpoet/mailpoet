44840f3d3416d7f36493f190754445c50262d003
John Oleksowicz <johnoleksowicz@icloud.com>
2022-09-12 15:34:25 -0500

    Skip rendering email preview for now
    I decided to extract the email buttons out of the thumbnail component
    because it didn't seem to make sense to have those things tied together.
    
    [MAILPOET-4622]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/steps/send_email/edit/email_panel.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/steps/send_email/edit/email_panel.tsx
@@ -2,7 +2,7 @@ import { ComponentProps } from 'react';
 import { PanelBody, TextareaControl, TextControl } from '@wordpress/components';
 import { dispatch, useSelect } from '@wordpress/data';
 import { DesignEmailButton } from './design_email_button';
-import { Thumbnail } from './thumbnail';
+import { Button } from '../../../components/button';
 import { PlainBodyTitle } from '../../../../../editor/components/panel';
 import { storeName } from '../../../../../editor/store';
 import { StepName } from '../../../../../editor/components/panel/step-name';
@@ -97,7 +97,20 @@ export function EmailPanel(): JSX.Element {
       <div className="mailpoet-automation-email-content-separator" />
       <PlainBodyTitle title="Email content" />
       {selectedStep.args.email_id ? (
-        <Thumbnail emailId={selectedStep.args.email_id as number} />
+        <div className="mailpoet-automation-email-buttons">
+          <Button
+            variant="sidebar-primary"
+            centered
+            href={`?page=mailpoet-newsletter-editor&id=${
+              selectedStep.args.email_id as string
+            }`}
+          >
+            Edit content
+          </Button>
+          <Button variant="secondary" centered>
+            Preview
+          </Button>
+        </div>
       ) : (
         <DesignEmailButton />
       )}
