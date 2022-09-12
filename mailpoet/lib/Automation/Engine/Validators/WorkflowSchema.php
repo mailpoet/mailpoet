69dce668736f69cc0f858153fe8c95402d809fa9
Jan Jakes <jan.jakes@automattic.com>
2022-09-12 07:39:45 +0200

    Remove duplicit method call
    [MAILPOET-4523]

--- mailpoet/lib/Automation/Engine/Validators/WorkflowSchema.php
+++ mailpoet/lib/Automation/Engine/Validators/WorkflowSchema.php
@@ -37,7 +37,7 @@ class WorkflowSchema {
       'id' => Builder::string()->pattern('^root$'),
       'type' => Builder::string()->pattern('^root$'),
       'key' => Builder::string()->pattern('^core:root$'),
-      'args' => Builder::array()->maxItems(0)->maxItems(0),
+      'args' => Builder::array()->maxItems(0),
       'next_steps' => self::getNextStepsSchema()->required(),
     ]);
   }
