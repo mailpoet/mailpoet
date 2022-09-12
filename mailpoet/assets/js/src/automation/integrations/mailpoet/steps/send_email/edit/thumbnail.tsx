49bb7f35a0048690292764f69122a28630bd1b6a
John Oleksowicz <johnoleksowicz@icloud.com>
2022-09-12 15:34:25 -0500

    Skip rendering email preview for now
    I decided to extract the email buttons out of the thumbnail component
    because it didn't seem to make sense to have those things tied together.
    
    [MAILPOET-4622]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/steps/send_email/edit/thumbnail.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/steps/send_email/edit/thumbnail.tsx
@@ -1,6 +1,5 @@
 import { ComponentProps, ComponentType, useEffect, useState } from 'react';
 import { Spinner as WpSpinner } from '@wordpress/components';
-import { Button } from '../../../components/button';
 import { MailPoetAjax } from '../../../../../../ajax';
 
 // @types/wordpress__components don't define "className", which is supported
@@ -34,33 +33,18 @@ export function Thumbnail({ emailId }: Props): JSX.Element {
   }, [emailId]);
 
   return (
-    <>
-      <div className="mailpoet-automation-thumbnail-box">
-        {thumbnailUrl ? (
-          <div className="mailpoet-automation-thumbnail-wrapper">
-            <img
-              className="mailpoet-automation-thumbnail-image"
-              src={thumbnailUrl}
-              alt="Email thumbnail"
-            />
-          </div>
-        ) : (
-          <Spinner className="mailpoet-automation-thumbnail-spinner" />
-        )}
-      </div>
-
-      <div className="mailpoet-automation-thumbnail-buttons">
-        <Button
-          variant="sidebar-primary"
-          centered
-          href={`?page=mailpoet-newsletter-editor&id=${emailId}`}
-        >
-          Edit content
-        </Button>
-        <Button variant="secondary" centered>
-          Preview
-        </Button>
-      </div>
-    </>
+    <div className="mailpoet-automation-thumbnail-box">
+      {thumbnailUrl ? (
+        <div className="mailpoet-automation-thumbnail-wrapper">
+          <img
+            className="mailpoet-automation-thumbnail-image"
+            src={thumbnailUrl}
+            alt="Email thumbnail"
+          />
+        </div>
+      ) : (
+        <Spinner className="mailpoet-automation-thumbnail-spinner" />
+      )}
+    </div>
   );
 }
