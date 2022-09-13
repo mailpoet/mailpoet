8590cb1409301388712e1654120be3f69ca65fc2
Jan Jakes <jan.jakes@automattic.com>
2022-09-13 15:56:19 +0200

    Rename automation "Validators" namespace to a more universal "Validation"
    [MAILPOET-4629]

--- mailpoet/lib/Automation/Engine/Validation/WorkflowSchema.php
+++ mailpoet/lib/Automation/Engine/Validation/WorkflowSchema.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace MailPoet\Automation\Engine\Validators;
+namespace MailPoet\Automation\Engine\Validation;
 
 use MailPoet\Validator\Builder;
 use MailPoet\Validator\Schema\ArraySchema;
