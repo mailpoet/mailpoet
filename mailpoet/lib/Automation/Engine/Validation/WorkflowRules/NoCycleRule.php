cafe2ed8a7c876f6f44efdf25bbd17d5f6a5df5e
Jan Jakes <jan.jakes@automattic.com>
2022-09-14 15:12:44 +0200

    Add no cycle rule for workflow validation
    [MAILPOET-4629]

---
+++ mailpoet/lib/Automation/Engine/Validation/WorkflowRules/NoCycleRule.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Automation\Engine\Validation\WorkflowRules;
+
+use MailPoet\Automation\Engine\Data\Step;
+use MailPoet\Automation\Engine\Data\Workflow;
+use MailPoet\Automation\Engine\Exceptions;
+use MailPoet\Automation\Engine\Validation\WorkflowGraph\WorkflowNode;
+use MailPoet\Automation\Engine\Validation\WorkflowGraph\WorkflowNodeVisitor;
+
+class NoCycleRule implements WorkflowNodeVisitor {
+  public function initialize(Workflow $workflow): void {
+  }
+
+  public function visitNode(Workflow $workflow, WorkflowNode $node): void {
+    $step = $node->getStep();
+    $parents = $node->getParents();
+    $parentIdsMap = array_combine(
+      array_map(function (Step $parent) {
+        return $parent->getId();
+      }, $node->getParents()),
+      $parents
+    ) ?: [];
+
+    foreach ($step->getNextSteps() as $nextStep) {
+      $nextStepId = $nextStep->getId();
+      if ($nextStepId === $step->getId() || isset($parentIdsMap[$nextStepId])) {
+        throw Exceptions::workflowStructureNotValid(__('Cycle found in workflow graph', 'mailpoet'));
+      }
+    }
+  }
+
+  public function complete(Workflow $workflow): void {
+  }
+}
