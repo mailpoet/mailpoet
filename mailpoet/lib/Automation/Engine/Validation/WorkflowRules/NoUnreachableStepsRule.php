0aa4cd4863c88262251abef35ca58270cdf232fe
Jan Jakes <jan.jakes@automattic.com>
2022-09-14 15:11:07 +0200

    Add no unreachable steps rule for workflow validation
    [MAILPOET-4629]

---
+++ mailpoet/lib/Automation/Engine/Validation/WorkflowRules/NoUnreachableStepsRule.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Automation\Engine\Validation\WorkflowRules;
+
+use MailPoet\Automation\Engine\Data\Workflow;
+use MailPoet\Automation\Engine\Exceptions;
+use MailPoet\Automation\Engine\Validation\WorkflowGraph\WorkflowNode;
+use MailPoet\Automation\Engine\Validation\WorkflowGraph\WorkflowNodeVisitor;
+
+class NoUnreachableStepsRule implements WorkflowNodeVisitor {
+  /** @var WorkflowNode[] */
+  private $visitedNodes = [];
+
+  public function initialize(Workflow $workflow): void {
+    $this->visitedNodes = [];
+  }
+
+  public function visitNode(Workflow $workflow, WorkflowNode $node): void {
+    $this->visitedNodes[] = $node;
+  }
+
+  public function complete(Workflow $workflow): void {
+    if (count($this->visitedNodes) !== count($workflow->getSteps())) {
+      throw Exceptions::workflowStructureNotValid(__('Unreachable steps found in workflow graph', 'mailpoet'));
+    }
+  }
+}
---
+++ mailpoet/tests/unit/Automation/Engine/Validation/WorkflowRules/NoUnreachableStepsRuleTest.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Automation\Engine\Validation\WorkflowRules;
+
+require_once __DIR__ . '/WorkflowRuleTest.php';
+
+use MailPoet\Automation\Engine\Exceptions\UnexpectedValueException;
+use MailPoet\Automation\Engine\Validation\WorkflowGraph\WorkflowWalker;
+
+class NoUnreachableStepsRuleTest extends WorkflowRuleTest {
+  public function testItDetectsUnreachableSteps(): void {
+    $workflow = $this->createWorkflow([
+      'root' => ['a'],
+      'a' => [],
+      'b' => [],
+    ]);
+
+    $this->expectException(UnexpectedValueException::class);
+    $this->expectExceptionMessage('Invalid workflow structure: Unreachable steps found in workflow graph');
+    (new WorkflowWalker())->walk($workflow, [new NoUnreachableStepsRule()]);
+  }
+
+  public function testItDetectsUnreachableSubgraphs(): void {
+    $workflow = $this->createWorkflow([
+      'root' => ['a'],
+      'a' => [],
+      'b' => ['c'],
+      'c' => [],
+    ]);
+
+    $this->expectException(UnexpectedValueException::class);
+    $this->expectExceptionMessage('Invalid workflow structure: Unreachable steps found in workflow graph');
+    $walker = new WorkflowWalker();
+    (new WorkflowWalker())->walk($workflow, [new NoUnreachableStepsRule()]);
+  }
+
+  public function testItPassesWithSimplePath(): void {
+    $workflow = $this->createWorkflow([
+      'root' => ['a'],
+      'a' => ['b'],
+      'b' => ['c'],
+      'c' => [],
+    ]);
+
+    (new WorkflowWalker())->walk($workflow, [new NoUnreachableStepsRule()]);
+    // no exception thrown
+  }
+}
