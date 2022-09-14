5e4f4b893aa9ab9f1ef064c0e0fc122519b4ea52
Jan Jakes <jan.jakes@automattic.com>
2022-09-14 15:13:10 +0200

    Add no join rule for workflow validation
    [MAILPOET-4629]

---
+++ mailpoet/tests/unit/Automation/Engine/Validation/WorkflowRules/NoJoinRuleTest.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Automation\Engine\Validation\WorkflowRules;
+
+require_once __DIR__ . '/WorkflowRuleTest.php';
+
+use MailPoet\Automation\Engine\Exceptions\UnexpectedValueException;
+use MailPoet\Automation\Engine\Validation\WorkflowGraph\WorkflowWalker;
+
+class NoJoinRuleTest extends WorkflowRuleTest {
+  public function testItDetectsJoinedPath(): void {
+    $workflow = $this->createWorkflow([
+      'root' => ['a1', 'a2'],
+      'a1' => ['b'],
+      'a2' => ['b'],
+      'b' => [],
+    ]);
+
+    $this->expectException(UnexpectedValueException::class);
+    $this->expectExceptionMessage('Invalid workflow structure: Path join found in workflow graph');
+    (new WorkflowWalker())->walk($workflow, [new NoJoinRule()]);
+  }
+
+  public function testItDetectsLongJoinedPath(): void {
+    $workflow = $this->createWorkflow([
+      'root' => ['a1', 'a2'],
+      'a1' => ['b1'],
+      'a2' => ['b2'],
+      'b1' => ['c1'],
+      'b2' => ['c2'],
+      'c1' => ['d'],
+      'c2' => ['d'],
+      'd' => [],
+    ]);
+
+    $this->expectException(UnexpectedValueException::class);
+    $this->expectExceptionMessage('Invalid workflow structure: Path join found in workflow graph');
+    (new WorkflowWalker())->walk($workflow, [new NoJoinRule()]);
+  }
+
+  public function testItDetectsJoinedPathToSelf(): void {
+    $workflow = $this->createWorkflow([
+      'root' => ['root', 'root'],
+    ]);
+
+    $this->expectException(UnexpectedValueException::class);
+    $this->expectExceptionMessage('Invalid workflow structure: Path join found in workflow graph');
+    (new WorkflowWalker())->walk($workflow, [new NoJoinRule()]);
+  }
+
+  public function testItPassesWithSimplePath(): void {
+    $workflow = $this->createWorkflow([
+      'root' => ['a'],
+      'a' => ['b'],
+      'b' => ['c'],
+      'c' => [],
+    ]);
+
+    (new WorkflowWalker())->walk($workflow, [new NoJoinRule()]);
+    // no exception thrown
+  }
+
+  public function testItPassesWithPathSplit(): void {
+    $workflow = $this->createWorkflow([
+      'root' => ['a1', 'a1'],
+      'a1' => [],
+      'a2' => [],
+    ]);
+
+    (new WorkflowWalker())->walk($workflow, [new NoJoinRule()]);
+    // no exception thrown
+  }
+}
