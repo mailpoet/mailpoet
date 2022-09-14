0b8461066f5f9185450ba3a0442d4e4173d57380
Jan Jakes <jan.jakes@automattic.com>
2022-09-14 15:11:59 +0200

    Add no duplicate edges rule for workflow validation
    [MAILPOET-4629]

---
+++ mailpoet/tests/unit/Automation/Engine/Validation/WorkflowRules/NoDuplicateEdgesTest.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Automation\Engine\Validation\WorkflowRules;
+
+require_once __DIR__ . '/WorkflowRuleTest.php';
+
+use MailPoet\Automation\Engine\Exceptions\UnexpectedValueException;
+use MailPoet\Automation\Engine\Validation\WorkflowGraph\WorkflowWalker;
+
+class NoDuplicateEdgesTest extends WorkflowRuleTest {
+  public function testItDetectsDuplicateEdges(): void {
+    $workflow = $this->createWorkflow([
+      'root' => ['a', 'a'],
+      'a' => [],
+    ]);
+
+    $this->expectException(UnexpectedValueException::class);
+    $this->expectExceptionMessage('Invalid workflow structure: Duplicate next step definition found');
+    (new WorkflowWalker())->walk($workflow, [new NoDuplicateEdgesRule()]);
+  }
+
+  public function testItPassesWithSingleEdges(): void {
+    $workflow = $this->createWorkflow([
+      'root' => ['a'],
+      'a' => ['b'],
+      'b' => ['c'],
+      'c' => [],
+    ]);
+
+    (new WorkflowWalker())->walk($workflow, [new NoDuplicateEdgesRule()]);
+    // no exception thrown
+  }
+}
