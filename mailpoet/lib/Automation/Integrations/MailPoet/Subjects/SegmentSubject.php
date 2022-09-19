18aedf3d1ad952768aab8398737a741735a59172
Jan Jakes <jan.jakes@automattic.com>
2022-09-19 13:29:53 +0200

    Split segment subject to Subject & Payload
    [MAILPOET-4629]

--- mailpoet/lib/Automation/Integrations/MailPoet/Subjects/SegmentSubject.php
+++ mailpoet/lib/Automation/Integrations/MailPoet/Subjects/SegmentSubject.php
@@ -3,77 +3,73 @@
 namespace MailPoet\Automation\Integrations\MailPoet\Subjects;
 
 use MailPoet\Automation\Engine\Data\Field;
+use MailPoet\Automation\Engine\Data\Subject as SubjectData;
+use MailPoet\Automation\Engine\Workflows\Payload;
 use MailPoet\Automation\Engine\Workflows\Subject;
-use MailPoet\Entities\SegmentEntity;
-use MailPoet\InvalidStateException;
+use MailPoet\Automation\Integrations\MailPoet\Payloads\SegmentPayload;
 use MailPoet\NotFoundException;
 use MailPoet\Segments\SegmentsRepository;
+use MailPoet\Validator\Builder;
+use MailPoet\Validator\Schema\ObjectSchema;
 
+/**
+ * @implements Subject<SegmentPayload>
+ */
 class SegmentSubject implements Subject {
   const KEY = 'mailpoet:segment';
 
-  /** @var Field[] */
-  private $fields;
-
   /** @var SegmentsRepository */
   private $segmentsRepository;
-
-  /** @var SegmentEntity|null */
-  private $segment;
 
   public function __construct(
     SegmentsRepository $segmentsRepository
   ) {
     $this->segmentsRepository = $segmentsRepository;
-
-    $this->fields = [
-      'name' =>
-      new Field(
-        'mailpoet:segment:name',
-        Field::TYPE_STRING,
-        __('Segment name', 'mailpoet'),
-        function () {
-          return $this->getSegment()->getName();
-        }
-      ),
-      'id' =>
-      new Field(
-        'mailpoet:segment:id',
-        Field::TYPE_INTEGER,
-        __('Segment ID', 'mailpoet'),
-        function () {
-          return $this->getSegment()->getId();
-        }
-      ),
-    ];
   }
 
   public function getKey(): string {
     return self::KEY;
   }
 
-  public function getFields(): array {
-    return $this->fields;
+  public function getName(): string {
+    return __('MailPoet segment', 'mailpoet');
   }
 
-  public function load(array $args): void {
-    $id = $args['segment_id'];
-    $this->segment = $this->segmentsRepository->findOneById($args['segment_id']);
-    if (!$this->segment) {
+  public function getArgsSchema(): ObjectSchema {
+    return Builder::object([
+      'segment_id' => Builder::integer()->required(),
+    ]);
+  }
+
+  public function getPayload(SubjectData $subjectData): Payload {
+    $id = $subjectData->getArgs()['segment_id'];
+    $segment = $this->segmentsRepository->findOneById($id);
+    if (!$segment) {
       // translators: %d is the ID.
       throw NotFoundException::create()->withMessage(sprintf(__("Segment with ID '%d' not found.", 'mailpoet'), $id));
     }
+    return new SegmentPayload($segment);
   }
 
-  public function pack(): array {
-    $segment = $this->getSegment();
-    return ['segment_id' => $segment->getId()];
-  }
-
-  public function getSegment(): SegmentEntity {
-    if (!$this->segment) {
-      throw InvalidStateException::create()->withMessage(__('Segment was not loaded.', 'mailpoet'));
-    }
-    return $this->segment;
+  /** @return Field[] */
+  public function getFields(): array {
+    return [
+      new Field(
+        'mailpoet:segment:id',
+        Field::TYPE_INTEGER,
+        __('Segment ID', 'mailpoet'),
+        function (SegmentPayload $payload) {
+          return $payload->getId();
+        }
+      ),
+      new Field(
+        'mailpoet:segment:name',
+        Field::TYPE_STRING,
+        __('Segment name', 'mailpoet'),
+        function (SegmentPayload $payload) {
+          return $payload->getName();
+        }
+      ),
+    ];
   }
 }
