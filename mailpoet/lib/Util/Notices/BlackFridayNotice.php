165e9553385011aec1f30081f90da99c8a9b6385
 Ján Mikláš <neosinner@gmail.com>
2022-09-23 22:32:16 +0200

    Update Black Friday content
    - added timezone to sale duration
    - changed date format
    - link to annual billing period
    - added tracking
    - switch dates in "shouldDisplay" condition, so it reads as "from to" rather than "to from"
    - capitalized button
    
    [MAILPOET-4670]

--- mailpoet/lib/Util/Notices/BlackFridayNotice.php
+++ mailpoet/lib/Util/Notices/BlackFridayNotice.php
@@ -22,8 +22,8 @@ class BlackFridayNotice {
 
   public function init($shouldDisplay) {
     $shouldDisplay = $shouldDisplay
-      && (time() <= strtotime('2022-10-07 12:00:00'))
-      && (time() >= strtotime('2022-10-03 12:00:00'))
+      && (time() >= strtotime('2022-10-03 14:00:00 UTC'))
+      && (time() <= strtotime('2022-10-07 14:00:00 UTC'))
       && !get_transient(self::OPTION_NAME);
     if ($shouldDisplay) {
       $this->display();
@@ -33,9 +33,9 @@ class BlackFridayNotice {
   private function display() {
     $subscribers = $this->subscribersRepository->countBy(['deletedAt' => null]);
     $header = '<h3 class="mailpoet-h3">' . __('Get ready for Black Friday with 40% off MailPoet plans', 'mailpoet') . '</h3>';
-    $body = '<h5 class="mailpoet-h5">' . __('Save 40% on all annual plans and licenses until 2 pm UTC, 7 October. Terms & conditions apply.', 'mailpoet') . '</h5>';
-    $link = "<p><a href='https://account.mailpoet.com/?s=$subscribers' class='mailpoet-button button-primary' target='_blank'>"
-      . __('Shop now', 'mailpoet')
+    $body = '<h5 class="mailpoet-h5">' . __('Save 40% on all annual plans until 2 pm UTC, October 7. Terms & conditions apply.', 'mailpoet') . '</h5>';
+    $link = "<p><a href='https://account.mailpoet.com/?s=$subscribers&billing=yearly&ref=sale-october-2022-plugin&utm_source=MP&utm_medium=plugin&utm_campaign=mp_prebfcm' class='mailpoet-button button-primary' target='_blank'>"
+      . __('Shop Now', 'mailpoet')
       . '</a></p>';
 
     $extraClasses = 'mailpoet-dismissible-notice is-dismissible';
