2761b544f7be8cb0f3f898c24471b21e3c7af9e0
Jan Lysý <jan.lysy@automattic.com>
2022-09-27 10:43:55 +0200

    Add filters into newsletter response
    [MAILPOET-4583]

--- mailpoet/lib/API/JSON/v1/Newsletters.php
+++ mailpoet/lib/API/JSON/v1/Newsletters.php
@@ -156,7 +156,9 @@ class Newsletters extends APIEndpoint {
   public function save($data = []) {
     $data = $this->wp->applyFilters('mailpoet_api_newsletters_save_before', $data);
     $newsletter = $this->newsletterSaveController->save($data);
-    $response = $this->newslettersResponseBuilder->build($newsletter);
+    $response = $this->newslettersResponseBuilder->build($newsletter, [
+      NewslettersResponseBuilder::RELATION_SEGMENTS,
+    ]);
     $previewUrl = $this->getViewInBrowserUrl($newsletter);
     $response = $this->wp->applyFilters('mailpoet_api_newsletters_save_after', $response);
     return $this->successResponse($response, ['preview_url' => $previewUrl]);
