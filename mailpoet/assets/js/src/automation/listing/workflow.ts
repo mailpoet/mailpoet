7dfdb13adac33d5dd2642a2138fa50cf0a1959cd
David Remer <webmaster@websupporter.net>
2022-09-30 10:19:18 +0300

    Extend Workflow type definition
    [MAILPOET-4673]

--- mailpoet/assets/js/src/automation/listing/workflow.ts
+++ mailpoet/assets/js/src/automation/listing/workflow.ts
@@ -9,4 +9,12 @@ export type Workflow = {
   id: number;
   name: string;
   status: WorkflowStatus;
+  stats: {
+    has_values: boolean;
+    totals: {
+      entered: number;
+      in_progress: number;
+      exited: number;
+    };
+  };
 };
