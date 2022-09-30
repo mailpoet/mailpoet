77175e6c1a62478fcaa50e14d5aff9d3f0675b06
David Remer <webmaster@websupporter.net>
2022-09-30 10:21:33 +0300

    Show statistics on listing page
    [MAILPOET-4673]

--- mailpoet/assets/js/src/automation/listing/components/cells/subscribers.tsx
+++ mailpoet/assets/js/src/automation/listing/components/cells/subscribers.tsx
@@ -1,3 +1,4 @@
+import { __ } from '@wordpress/i18n';
 import { Workflow } from '../../workflow';
 
 type Props = {
@@ -5,5 +6,20 @@ type Props = {
 };
 
 export function Subscribers({ workflow }: Props): JSX.Element {
-  return <p>ToDo {workflow.id}</p>;
+  return (
+    <ul className="mailpoet-automation-stats">
+      <li className="mailpoet-automation-stats-entered">
+        {new Intl.NumberFormat().format(workflow.stats.totals.entered)}
+        <span>{__('Entered', 'mailpoet')}</span>
+      </li>
+      <li className="mailpoet-automation-stats-in-process">
+        {new Intl.NumberFormat().format(workflow.stats.totals.in_progress)}
+        <span>{__('Processing', 'mailpoet')}</span>
+      </li>
+      <li className="mailpoet-automation-stats-exited">
+        {new Intl.NumberFormat().format(workflow.stats.totals.exited)}
+        <span>{__('Exited', 'mailpoet')}</span>
+      </li>
+    </ul>
+  );
 }
