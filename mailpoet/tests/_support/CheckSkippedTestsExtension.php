5e1a1a4550559df661d12b765b9c1348a4664626
Rodrigo Primo <rodrigo@automattic.com>
2022-10-04 12:52:41 -0300

    Change codeception extension to catch skipped tests
    This commit changes the Codeception extension CheckSkippedTestsExtension
    that we use to generate an error when tests are skipped in the `trunk` and
    `release` branches. Now this extension accepts a list of tests that can
    be skipped in those two branches. This will be necessary as part of the
    project to support WooCommerce Custom Orders Tables as in some scenarios
    we want to skip WooCommerce Subscriptions tests and in some we don't.
    For now, WooCommerce Subscrptions doesn't support Woo COT, so we want to
    run Woo Subscriptions tests when COT is disabled, but we want to skip
    then when COT is enabled.
    
    The main change is that now we are using the `TEST_SKIPPED` Codeception event
    instead of the `SUITE_AFTER` event. This change was necessary as in the
    latter event we don't have any information about the tests that were
    skipped.
    
    [MAILPOET-4695]

--- mailpoet/tests/_support/CheckSkippedTestsExtension.php
+++ mailpoet/tests/_support/CheckSkippedTestsExtension.php
@@ -1,19 +1,23 @@
 <?php
 
-use Codeception\Event\SuiteEvent;
+use Codeception\Event\FailEvent;
 use Codeception\Events;
 use Codeception\Extension;
 
 // phpcs:ignore PSR1.Classes.ClassDeclaration
 class CheckSkippedTestsExtension extends Extension {
   public static $events = [
-    Events::SUITE_AFTER => 'checkErrorsAfterTests',
+    Events::TEST_SKIPPED => 'checkSkippedTests',
   ];
 
-  public function checkErrorsAfterTests(SuiteEvent $e) {
+  public function checkSkippedTests(FailEvent $event) {
     $branch = getenv('CIRCLE_BRANCH');
-    $skipped = $e->getResult()->skipped();
-    if (in_array($branch, ['trunk', 'release']) && (count($skipped) !== 0)) {
+    $testName = $event->getTest()->getName();
+
+    // list of tests that are allowed to be skipped on trunk and release branches
+    $allowedToSkipList = [];
+
+    if (in_array($branch, ['trunk', 'release']) && !in_array($testName, $allowedToSkipList)) {
       throw new \PHPUnit\Framework\ExpectationFailedException("Failed, Cannot skip tests on branch $branch.");
     }
   }
