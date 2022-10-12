6e3e8c732636af746814ff283453cc0c595b2c95
David Remer <webmaster@websupporter.net>
2022-10-12 09:54:23 +0300

    Test not loggedin users can not get workflow templates
    [MAILPOET-4531]

--- mailpoet/tests/integration/REST/Automation/Workflows/WorkflowTemplatesGetEndpointTest.php
+++ mailpoet/tests/integration/REST/Automation/Workflows/WorkflowTemplatesGetEndpointTest.php
@@ -16,6 +16,17 @@ class WorkflowTemplatesGetEndpointTest extends AutomationTest
     $this->assertEquals('simple-welcome-email', $result['data'][0]['slug']);
   }
 
+  public function testGuestNotAllowed(): void {
+    wp_set_current_user(0);
+    $data = $this->get(self::ENDPOINT_PATH, []);
+
+    $this->assertSame([
+      'code' => 'rest_forbidden',
+      'message' => 'Sorry, you are not allowed to do that.',
+      'data' => ['status' => 401],
+    ], $data);
+  }
+
   public function testGetTemplatesByCategory() {
     //@ToDo: Once we have templates in other categories, we should make this test more specific.
     $result = $this->get(self::ENDPOINT_PATH, [
