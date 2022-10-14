b237c1aa10c9197e709b7017ab7ccdffcf2a605c
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2022-10-14 12:08:12 +0100

    Update First purchase automatic emails
    Add "When to send this email" and "after first purchase" texts to the page
    
    MAILPOET-4658

--- mailpoet/lib/AutomaticEmails/WooCommerce/Events/AbandonedCart.php
+++ mailpoet/lib/AutomaticEmails/WooCommerce/Events/AbandonedCart.php
@@ -55,25 +55,26 @@ class AbandonedCart {
       'title' => _x('Abandoned Shopping Cart', 'This is the name of a type of automatic email for ecommerce. Those emails are sent automatically when a customer adds product to his shopping cart but never complete the checkout process.', 'mailpoet'),
       'description' => __('Send an email to logged-in visitors who have items in their shopping carts but left your website without checking out. Can convert up to 5% of abandoned carts.', 'mailpoet'),
       'listingScheduleDisplayText' => _x('Send the email when a customer abandons their cart.', 'Description of Abandoned Shopping Cart email', 'mailpoet'),
+      'afterDelayText' => __('after cart abandoned', 'mailpoet'),
       'badge' => [
         'text' => __('Must-have', 'mailpoet'),
         'style' => 'red',
       ],
       'timeDelayValues' => [
         'minutes' => [
-          'text' => _x('30 minutes after last page loaded', 'This is a trigger setting. It means that we will send an automatic email to a visitor 30 minutes after this visitor had left the website.', 'mailpoet'),
+          'text' => _x('30 minutes', 'This is a trigger setting. It means that we will send an automatic email to a visitor 30 minutes after this visitor had left the website.', 'mailpoet'),
           'displayAfterTimeNumberField' => false,
         ],
         'hours' => [
-          'text' => __('hour(s) later', 'mailpoet'),
+          'text' => __('hour(s)', 'mailpoet'),
           'displayAfterTimeNumberField' => true,
         ],
         'days' => [
-          'text' => __('day(s) later', 'mailpoet'),
+          'text' => __('day(s)', 'mailpoet'),
           'displayAfterTimeNumberField' => true,
         ],
         'weeks' => [
-          'text' => __('week(s) later', 'mailpoet'),
+          'text' => __('week(s)', 'mailpoet'),
           'displayAfterTimeNumberField' => true,
         ],
       ],
