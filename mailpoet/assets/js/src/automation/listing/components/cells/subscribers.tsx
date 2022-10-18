04f8d29b73c143ed9279edad2b716e66b4990f53
Jan Jakes <jan.jakes@automattic.com>
2022-10-18 15:52:30 +0200

    Reuse stats component in workflow listing
    [MAILPOET-4540]

--- mailpoet/assets/js/src/automation/listing/components/cells/subscribers.tsx
+++ mailpoet/assets/js/src/automation/listing/components/cells/subscribers.tsx
@@ -1,5 +1,6 @@
 import { __ } from '@wordpress/i18n';
 import { Workflow } from '../../workflow';
+import { Statistics } from '../../../components/statistics';
 
 type Props = {
   workflow: Workflow;
@@ -7,25 +8,25 @@ type Props = {
 
 export function Subscribers({ workflow }: Props): JSX.Element {
   return (
-    <ul className="mailpoet-automation-stats">
-      <li className="mailpoet-automation-stats-item">
-        {new Intl.NumberFormat().format(workflow.stats.totals.entered)}
-        <span className="mailpoet-automation-stats-label">
-          {__('Entered', 'mailpoet')}
-        </span>
-      </li>
-      <li className="mailpoet-automation-stats-item">
-        {new Intl.NumberFormat().format(workflow.stats.totals.in_progress)}
-        <span className="mailpoet-automation-stats-label">
-          {__('Processing', 'mailpoet')}
-        </span>
-      </li>
-      <li className="mailpoet-automation-stats-item">
-        {new Intl.NumberFormat().format(workflow.stats.totals.exited)}
-        <span className="mailpoet-automation-stats-label">
-          {__('Exited', 'mailpoet')}
-        </span>
-      </li>
-    </ul>
+    <Statistics
+      labelPosition="after"
+      items={[
+        {
+          key: 'entered',
+          label: __('Entered', 'mailpoet'),
+          value: workflow.stats.totals.entered,
+        },
+        {
+          key: 'processing',
+          label: __('Processing', 'mailpoet'),
+          value: workflow.stats.totals.in_progress,
+        },
+        {
+          key: 'exited',
+          label: __('Exited', 'mailpoet'),
+          value: workflow.stats.totals.exited,
+        },
+      ]}
+    />
   );
 }
