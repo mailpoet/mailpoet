17c7d42eded4245dcbfe3d04ed375cb9ac1712ac
David Remer <webmaster@websupporter.net>
2022-10-18 13:28:18 +0300

    Remove activate toggle
    [MAILPOET-4462]

--- mailpoet/assets/js/src/automation/listing/components/cells/status.tsx
+++ mailpoet/assets/js/src/automation/listing/components/cells/status.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
 import { __ } from '@wordpress/i18n';
-import { ToggleControl } from '@wordpress/components';
 import { Workflow, WorkflowStatus } from '../../workflow';
 
 type Props = {
@@ -8,14 +6,8 @@ type Props = {
 };
 
 export function Status({ workflow }: Props): JSX.Element {
-  const [isActive, setIsActive] = useState(workflow.status === 'active');
-
   return (
     <div className="mailpoet-automation-listing-cell-status">
-      <ToggleControl
-        checked={isActive}
-        onChange={(active) => setIsActive(active)}
-      />
       {workflow.status === WorkflowStatus.ACTIVE
         ? __('Active', 'mailpoet')
         : __('Not active', 'mailpoet')}
