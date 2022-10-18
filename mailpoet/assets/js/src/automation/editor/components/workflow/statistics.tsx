c883eaa3f6aaa671b77a15a907deddf24fd3c88d
Jan Jakes <jan.jakes@automattic.com>
2022-10-18 16:11:21 +0200

    Simplify and fix stats rendering using CSS grid
    Fixed separator rendering, simplified CSS (grid can do this), removed
    had extra paddings that were causing problems on the listing page.
    
    [MAILPOET-4540]

--- mailpoet/assets/js/src/automation/editor/components/workflow/statistics.tsx
+++ mailpoet/assets/js/src/automation/editor/components/workflow/statistics.tsx
@@ -12,24 +12,26 @@ export function Statistics(): JSX.Element {
   );
 
   return (
-    <BaseStatistics
-      items={[
-        {
-          key: 'entered',
-          label: __('Total Entered', 'mailpoet'),
-          value: workflow.stats.totals.entered,
-        },
-        {
-          key: 'processing',
-          label: __('Total Processing', 'mailpoet'),
-          value: workflow.stats.totals.in_progress,
-        },
-        {
-          key: 'exited',
-          label: __('Total Exited', 'mailpoet'),
-          value: workflow.stats.totals.exited,
-        },
-      ]}
-    />
+    <div className="mailpoet-automation-editor-stats">
+      <BaseStatistics
+        items={[
+          {
+            key: 'entered',
+            label: __('Total Entered', 'mailpoet'),
+            value: workflow.stats.totals.entered,
+          },
+          {
+            key: 'processing',
+            label: __('Total Processing', 'mailpoet'),
+            value: workflow.stats.totals.in_progress,
+          },
+          {
+            key: 'exited',
+            label: __('Total Exited', 'mailpoet'),
+            value: workflow.stats.totals.exited,
+          },
+        ]}
+      />
+    </div>
   );
 }
