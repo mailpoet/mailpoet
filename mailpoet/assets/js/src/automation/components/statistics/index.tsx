48dca5e298e120d2784ed45b5f329364152db5b2
Jan Jakes <jan.jakes@automattic.com>
2022-10-18 15:52:30 +0200

    Reuse stats component in workflow listing
    [MAILPOET-4540]

--- mailpoet/assets/js/src/automation/components/statistics/index.tsx
+++ mailpoet/assets/js/src/automation/components/statistics/index.tsx
@@ -6,15 +6,23 @@ type Item = {
 
 type Props = {
   items: Item[];
+  labelPosition?: 'before' | 'after';
 };
 
-export function Statistics({ items }: Props): JSX.Element {
+export function Statistics({
+  items,
+  labelPosition = 'before',
+}: Props): JSX.Element {
   const intl = new Intl.NumberFormat();
   return (
     <ul className="mailpoet-automation-stats">
       {items.map((item) => (
         <li key={item.key} className="mailpoet-automation-stats-item">
-          <span className="mailpoet-automation-stats-label">{item.label}</span>
+          <span
+            className={`mailpoet-automation-stats-label display-${labelPosition}`}
+          >
+            {item.label}
+          </span>
           {intl.format(item.value)}
         </li>
       ))}
