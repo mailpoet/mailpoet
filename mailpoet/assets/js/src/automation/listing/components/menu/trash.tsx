fa3b9ecc4e9198d5474fa301e1584426879e0acb
Jan Jakes <jan.jakes@automattic.com>
2022-10-18 09:42:57 +0200

    Add trash confirmation dialog
    [MAILPOET-4540]

--- mailpoet/assets/js/src/automation/listing/components/menu/trash.tsx
+++ mailpoet/assets/js/src/automation/listing/components/menu/trash.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
+import { __experimentalConfirmDialog as ConfirmDialog } from '@wordpress/components';
 import { useDispatch } from '@wordpress/data';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { Item } from './item';
 import { storeName } from '../../store';
 import { Workflow, WorkflowStatus } from '../../workflow';
 
 export const useTrashButton = (workflow: Workflow): Item | undefined => {
+  const [showDialog, setShowDialog] = useState(false);
   const { trashWorkflow } = useDispatch(storeName);
 
   if (workflow.status === WorkflowStatus.TRASH) {
@@ -16,7 +19,26 @@ export const useTrashButton = (workflow: Workflow): Item | undefined => {
     control: {
       title: __('Trash', 'mailpoet'),
       icon: null,
-      onClick: () => trashWorkflow(workflow),
+      onClick: () => setShowDialog(true),
     },
+    slot: (
+      <ConfirmDialog
+        isOpen={showDialog}
+        title={__('Trash workflow', 'mailpoet')}
+        confirmButtonText={__('Yes, move to trash', 'mailpoet')}
+        __experimentalHideHeader={false}
+        onConfirm={() => trashWorkflow(workflow)}
+        onCancel={() => setShowDialog(false)}
+      >
+        {sprintf(
+          // translators: %s is the workflow name
+          __(
+            'Are you sure you want to move the workflow "%s" to the Trash?',
+            'mailpoet',
+          ),
+          workflow.name,
+        )}
+      </ConfirmDialog>
+    ),
   };
 };
