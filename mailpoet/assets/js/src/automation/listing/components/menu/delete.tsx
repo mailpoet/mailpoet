ba2cb75877eff1e3ea2065196e811c6ede7fc72a
Jan Jakes <jan.jakes@automattic.com>
2022-10-18 09:43:20 +0200

    Add delete confirmation dialog
    [MAILPOET-4540]

--- mailpoet/assets/js/src/automation/listing/components/menu/delete.tsx
+++ mailpoet/assets/js/src/automation/listing/components/menu/delete.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
+import { __experimentalConfirmDialog as ConfirmDialog } from '@wordpress/components';
 import { useDispatch } from '@wordpress/data';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { Item } from './item';
 import { storeName } from '../../store';
 import { Workflow, WorkflowStatus } from '../../workflow';
 
 export const useDeleteButton = (workflow: Workflow): Item | undefined => {
+  const [showDialog, setShowDialog] = useState(false);
   const { deleteWorkflow } = useDispatch(storeName);
 
   if (workflow.status !== WorkflowStatus.TRASH) {
@@ -16,7 +19,26 @@ export const useDeleteButton = (workflow: Workflow): Item | undefined => {
     control: {
       title: __('Delete permanently', 'mailpoet'),
       icon: null,
-      onClick: () => deleteWorkflow(workflow),
+      onClick: () => setShowDialog(true),
     },
+    slot: (
+      <ConfirmDialog
+        isOpen={showDialog}
+        title={__('Permanently delete automation', 'mailpoet')}
+        confirmButtonText={__('Yes, permanently delete', 'mailpoet')}
+        __experimentalHideHeader={false}
+        onConfirm={() => deleteWorkflow(workflow)}
+        onCancel={() => setShowDialog(false)}
+      >
+        {sprintf(
+          // translators: %s is the workflow name
+          __(
+            'Are you sure you want to permanently delete "%s" and all associated data? This cannot be undone!',
+            'mailpoet',
+          ),
+          workflow.name,
+        )}
+      </ConfirmDialog>
+    ),
   };
 };
--- mailpoet/assets/js/src/automation/listing/components/menu/index.ts
+++ mailpoet/assets/js/src/automation/listing/components/menu/index.ts
@@ -1,3 +1,4 @@
+export * from './delete';
 export * from './duplicate';
 export * from './restore';
 export * from './trash';
