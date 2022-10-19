9d8bf3cdca609e68984787a0e39338355e16b53a
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2022-10-19 13:14:53 +0100

    Update default subscription confirmation email content
    MAILPOET-4599

--- mailpoet/lib/Settings/SettingsController.php
+++ mailpoet/lib/Settings/SettingsController.php
@@ -69,7 +69,7 @@ class SettingsController {
           'enabled' => true,
           // translators: %1$s is the name of the blog
           'subject' => sprintf(__('Confirm your subscription to %1$s', 'mailpoet'), WPFunctions::get()->getOption('blogname')),
-          'body' => __("Hello,\n\nWelcome to our newsletter!\n\nPlease confirm your subscription to our list by clicking the link below: \n\n[activation_link]I confirm my subscription![/activation_link]\n\nThank you,\n\nThe Team", 'mailpoet'),
+          'body' => __("Hello [subscriber:firstname | default:there],\n\nYou've received this message because you subscribed to [site:title]. Please confirm your subscription to receive emails from us:\n\n[activation_link]Click here to confirm your subscription.[/activation_link] \n\nIf you received this email by mistake, simply delete it. You won't receive any more emails from us unless you confirm your subscription using the link above.\n\nThank you,\n\n<a target='_blank' href='[site:homepage_link]'>[site:title]</a>", 'mailpoet'),
         ],
         'tracking' => [
           'level' => TrackingConfig::LEVEL_FULL,
