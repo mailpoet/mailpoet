2cacb5809dc50a2a43ad15dc4597f8c865895ea7
David Remer <webmaster@websupporter.net>
2022-10-24 15:01:15 +0300

    Avoid iteration
    [MAILPOET-4700]

--- mailpoet/lib/Automation/Engine/Validation/WorkflowRules/AtLeastOneTriggerRule.php
+++ mailpoet/lib/Automation/Engine/Validation/WorkflowRules/AtLeastOneTriggerRule.php
@@ -2,6 +2,7 @@
 
 namespace MailPoet\Automation\Engine\Validation\WorkflowRules;
 
+use MailPoet\Automation\Engine\Data\Step;
 use MailPoet\Automation\Engine\Data\Workflow;
 use MailPoet\Automation\Engine\Exceptions;
 use MailPoet\Automation\Engine\Validation\WorkflowGraph\WorkflowNode;
@@ -10,19 +11,23 @@ use MailPoet\Automation\Engine\Validation\WorkflowGraph\WorkflowNodeVisitor;
 class AtLeastOneTriggerRule implements WorkflowNodeVisitor {
   public const RULE_ID = 'at-least-one-trigger';
 
-  public function initialize(Workflow $workflow): void {
-    foreach ($workflow->getSteps() as $step) {
-      if ($step->getType() === 'trigger') {
-        return;
-      }
-    }
+  /** @var bool */
+  private $triggerFound = false;
 
-    throw Exceptions::workflowStructureNotValid(__('There must be at least one trigger in the workflow.', 'mailpoet'), self::RULE_ID);
+  public function initialize(Workflow $workflow): void {
+    $this->triggerFound = false;
   }
 
   public function visitNode(Workflow $workflow, WorkflowNode $node): void {
+    if ($node->getStep()->getType() === Step::TYPE_TRIGGER) {
+      $this->triggerFound = true;
+    }
   }
 
   public function complete(Workflow $workflow): void {
+    if ($this->triggerFound) {
+      return;
+    }
+    throw Exceptions::workflowStructureNotValid(__('There must be at least one trigger in the workflow.', 'mailpoet'), self::RULE_ID);
   }
 }
