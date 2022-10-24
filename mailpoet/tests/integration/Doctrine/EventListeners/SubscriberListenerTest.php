3dd872e21180cb325a4e77771a4e1babe30c3560
Jan Lysý <jan.lysy@automattic.com>
2022-10-24 14:06:00 +0200

    Add SubscriberListener integration test
    [MAILPOET-4727]

---
+++ mailpoet/tests/integration/Doctrine/EventListeners/SubscriberListenerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace MailPoet\Test\Doctrine\EventListeners;
+
+use Codeception\Stub\Expected;
+use MailPoet\Config\SubscriberChangesNotifier;
+use MailPoet\Doctrine\EventListeners\SubscriberListener;
+use MailPoet\Doctrine\EventListeners\TimestampListener;
+use MailPoet\Entities\SubscriberEntity;
+use MailPoet\Test\DataFactories\Subscriber as SubscriberFactory;
+use MailPoet\WP\Functions as WPFunctions;
+use MailPoetVendor\Carbon\Carbon;
+use MailPoetVendor\Doctrine\ORM\Events;
+
+require_once __DIR__ . '/EventListenersBaseTest.php';
+require_once __DIR__ . '/TimestampEntity.php';
+
+class SubscriberListenerTest extends EventListenersBaseTest {
+  /** @var WPFunctions */
+  private $wp;
+
+  /** @var SubscriberListener */
+  private $subscriberListener;
+
+  public function _before() {
+    $this->wp = $this->diContainer->get(WPFunctions::class);
+  }
+
+  public function testItNotifiesAboutNewSubscriber(): void {
+    $changesNotifier = $this->make(SubscriberChangesNotifier::class, [
+      'wp' => $this->wp,
+      'subscriberCreated' => Expected::once(),
+    ]);
+    $this->subscriberListener = new SubscriberListener($changesNotifier);
+    $this->replaceEntityListener($this->subscriberListener);
+
+    (new SubscriberFactory())->create();
+  }
+
+  public function testItNotifiesAboutUpdatedSubscriber(): void {
+    $subscriber = (new SubscriberFactory())->create();
+    $changesNotifier = $this->make(SubscriberChangesNotifier::class, [
+      'wp' => $this->wp,
+      'subscriberUpdated' => Expected::once(),
+    ]);
+    $this->subscriberListener = new SubscriberListener($changesNotifier);
+    $this->replaceEntityListener($this->subscriberListener);
+
+    $subscriber->setFirstName('John');
+    $subscriber->setLastName('Doe');
+    $this->entityManager->flush();
+  }
+
+  public function testItNotifiesAboutDeletedSubscriber(): void {
+    $subscriber = (new SubscriberFactory())->create();
+    $changesNotifier = $this->make(SubscriberChangesNotifier::class, [
+      'wp' => $this->wp,
+      'subscriberDeleted' => Expected::once(),
+    ]);
+    $this->subscriberListener = new SubscriberListener($changesNotifier);
+    $this->replaceEntityListener($this->subscriberListener);
+
+    $this->entityManager->remove($subscriber);
+    $this->entityManager->flush();
+  }
+
+  public function _after() {
+    parent::_after();
+    $originalListener = $this->diContainer->get(TimestampListener::class);
+    $this->replaceEntityListener($originalListener);
+    $this->truncateEntity(SubscriberEntity::class);
+  }
+}
