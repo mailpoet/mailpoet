8c1898e46cb0990b94120ca5a62449bfe243a3cb
Jan Jakes <jan.jakes@automattic.com>
2022-10-27 15:58:39 +0200

    Add a migration template that will be used to generate new migrations
    [MAILPOET-4466]

---
+++ mailpoet/lib/Migrator/MigrationTemplate.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Migrations;
+
+use MailPoet\Migrator\Migration;
+
+class MigrationTemplate extends Migration {
+  public function run(): void {
+    /*
+     * TODO: Implement the migration logic here and remove this comment.
+     *
+     * You can use:
+     *   $this->connection      For SQL queries using Doctrine DBAL.
+     *   $this->entityManager   For operations using Doctrine Entity Manager.
+     *   $this->container       For accessing any needed service.
+     */
+  }
+}
