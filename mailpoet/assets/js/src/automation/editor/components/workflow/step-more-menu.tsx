9f4b0d0ca15077dc5dea4793ec5bb79065d6fc36
David Remer <webmaster@websupporter.net>
2022-10-31 10:14:31 +0200

    Add filter so the more menu can be extended by third party
    * Removes deprecated mailpoet.automation.workflow.delete_step_callback filter
    * Adds mailpoet.automation.workflow.step.more-controls filter
    
    [MAILPOET-4674]

--- mailpoet/assets/js/src/automation/editor/components/workflow/step-more-menu.tsx
+++ mailpoet/assets/js/src/automation/editor/components/workflow/step-more-menu.tsx
@@ -1,44 +1,66 @@
-import { useCallback, useState } from 'react';
-import { DropdownMenu } from '@wordpress/components';
+import { useState } from 'react';
+import { Dropdown, DropdownMenu, MenuItem } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
 import { moreVertical, trash } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 import { Hooks } from 'wp-js-hooks';
 import { PremiumModal } from 'common/premium_modal';
 import { Step as StepData } from './types';
+import { storeName } from '../../store';
 
 type Props = {
   step: StepData;
 };
 
+// I do not like this hack.
+// What I want to achieve: With the lazyOnClose method, I want to be able to control to close
+// the menu from within a menuitem.
+// But there must be a better way then storing currentProps like that.
+let currentProps: Dropdown.RenderProps | null = null;
+function setCurrentProps(current) {
+  currentProps = current;
+}
+
 export function StepMoreMenu({ step }: Props): JSX.Element {
+  const { stepType } = useSelect(
+    (select) => ({
+      stepType: select(storeName).getStepType(step.key),
+    }),
+    [step],
+  );
   const [showModal, setShowModal] = useState(false);
 
-  const onDelete = useCallback((stepData: StepData) => {
-    const deleteStepCallback = Hooks.applyFilters(
-      'mailpoet.automation.workflow.delete_step_callback',
-      () => {
-        setShowModal(true);
-      },
-    );
-    deleteStepCallback(stepData);
-  }, []);
-
+  const lazyOnClose = () => {
+    currentProps?.onClose();
+  };
+  type MoreControls = Record<string, JSX.Element>;
+  const controls: MoreControls = Hooks.applyFilters(
+    'mailpoet.automation.workflow.step.more-controls',
+    {
+      delete: (
+        <MenuItem key="delete" icon={trash} onClick={() => setShowModal(true)}>
+          {__('Delete step', 'mailpoet')}
+        </MenuItem>
+      ),
+    },
+    step,
+    stepType,
+    lazyOnClose,
+  );
   return (
     <>
       <div className="mailpoet-automation-step-more-menu">
         <DropdownMenu
           label={__('More', 'mailpoet')}
           icon={moreVertical}
-          controls={[
-            {
-              title: __('Delete step', 'mailpoet'),
-              icon: trash,
-              onClick: () => onDelete(step),
-            },
-          ]}
           popoverProps={{ position: 'bottom right' }}
           toggleProps={{ isSmall: true }}
-        />
+        >
+          {(props) => {
+            setCurrentProps(props);
+            return <>{Object.values(controls)}</>;
+          }}
+        </DropdownMenu>
       </div>
 
       {showModal && (
