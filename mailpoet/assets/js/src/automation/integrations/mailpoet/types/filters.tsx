aa52ca804e12c8f732cec8d7a86c854b8e641440
Jan Jakes <jan.jakes@automattic.com>
2022-11-03 15:20:59 +0100

    Expose send email create step filter
    [MAILPOET-4757]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/types/filters.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/types/filters.tsx
@@ -2,6 +2,11 @@
  * The types in this file document the expected return types of specific
  * filters.
  */
+import { State } from '../../../editor/store';
+import { Step } from '../../../editor/components/workflow/types';
+
+// mailpoet.automation.send_email.create_step
+export type SendEmailCreateStepType = (step: Step, state: State) => Step;
 
 // mailpoet.automation.send_email.google_analytics_panel
 export type GoogleAnalyticsPanelBodyType = JSX.Element;
