0028004bdd628abf880712d7bfa6a81566c107e0
alex-mailpoet <alex-mailpoet@users.noreply.github.com>
2022-11-03 12:38:20 +0300

    Prefix polyfilled normalizer functions
    [MAILPOET-4770]

--- mailpoet/prefixer/fix-symfony-polyfill.php
+++ mailpoet/prefixer/fix-symfony-polyfill.php
@@ -17,6 +17,13 @@ $data = str_replace('\\Normalizer::', '\\MailPoetVendor\\Normalizer::', $data);
 $data = str_replace('\'Normalizer::', '\'\\MailPoetVendor\\Normalizer::', $data); // for use in strings like defined('...')
 file_put_contents($file, $data);
 
+$file = __DIR__ . '/../vendor-prefixed/symfony/polyfill-intl-normalizer/bootstrap.php';
+$data = file_get_contents($file);
+// These unprefixed functions break WP 6.1 compatibility, we don't seem to use them, let's prefix them.
+$data = str_replace('function normalizer_is_normalized', 'function mailpoet_normalizer_is_normalized', $data);
+$data = str_replace('function normalizer_normalize', 'function mailpoet_normalizer_normalize', $data);
+file_put_contents($file, $data);
+
 $file = __DIR__ . '/../vendor-prefixed/symfony/polyfill-iconv/Iconv.php';
 $data = file_get_contents($file);
 $data = str_replace('\\Normalizer::', '\\MailPoetVendor\\Normalizer::', $data);
