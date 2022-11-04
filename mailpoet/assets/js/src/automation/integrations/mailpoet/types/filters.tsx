cf2dc4c3bbe1e57720dca8c1e4ae761371f5b748
Jan Jakes <jan.jakes@automattic.com>
2022-11-04 11:19:25 +0100

    Fix hook type
    [MAILPOET-4757]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/types/filters.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/types/filters.tsx
@@ -2,11 +2,10 @@
  * The types in this file document the expected return types of specific
  * filters.
  */
-import { State } from '../../../editor/store';
 import { Step } from '../../../editor/components/workflow/types';
 
 // mailpoet.automation.send_email.create_step
-export type SendEmailCreateStepType = (step: Step, state: State) => Step;
+export type SendEmailCreateStepType = (step: Step, workflowId: number) => Step;
 
 // mailpoet.automation.send_email.google_analytics_panel
 export type GoogleAnalyticsPanelBodyType = JSX.Element;
