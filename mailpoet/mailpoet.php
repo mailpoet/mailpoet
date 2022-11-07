ec1eb8b54c30aa3b0a6bb8d6c66dd647c31915ed
Rodrigo Primo <rodrigo@automattic.com>
2022-11-07 20:52:19 -0300

    Release 3.103.0

--- mailpoet/mailpoet.php
+++ mailpoet/mailpoet.php
@@ -2,7 +2,7 @@
 
 /*
  * Plugin Name: MailPoet
- * Version: 3.102.1
+ * Version: 3.103.0
  * Plugin URI: https://www.mailpoet.com
  * Description: Create and send newsletters, post notifications and welcome emails from your WordPress.
  * Author: MailPoet
@@ -20,7 +20,7 @@
  */
 
 $mailpoetPlugin = [
-  'version' => '3.102.1',
+  'version' => '3.103.0',
   'filename' => __FILE__,
   'path' => dirname(__FILE__),
   'autoloader' => dirname(__FILE__) . '/vendor/autoload.php',
