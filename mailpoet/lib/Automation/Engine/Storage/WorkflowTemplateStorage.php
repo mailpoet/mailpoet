c239566e1e25954c56c6c15d85920f8f464b4286
David Remer <webmaster@websupporter.net>
2022-11-08 09:21:40 +0200

    Add premium and coming-soon template stubs
    [MAILPOET-4745]

--- mailpoet/lib/Automation/Engine/Storage/WorkflowTemplateStorage.php
+++ mailpoet/lib/Automation/Engine/Storage/WorkflowTemplateStorage.php
@@ -75,8 +75,10 @@ class WorkflowTemplateStorage {
           ],
           [],
         ]
-      )
+      ),
+      WorkflowTemplate::TYPE_FREE_ONLY
     );
+
     $userWelcomeEmail = new WorkflowTemplate(
       'user-welcome-email',
       WorkflowTemplate::CATEGORY_WELCOME,
@@ -99,12 +101,90 @@ class WorkflowTemplateStorage {
           ],
           [],
         ]
-      )
+      ),
+      WorkflowTemplate::TYPE_FREE_ONLY
     );
+
+    $subscriberWelcomeSeries = new WorkflowTemplate(
+      'subscriber-welcome-series',
+      WorkflowTemplate::CATEGORY_WELCOME,
+      __(
+        "Welcome new subscribers and start building a relationship with them. Send an email immediately after someone subscribes to your list to introduce your brand and a follow-up two days later to keep the conversation going.",
+        'mailpoet'
+      ),
+      $this->builder->createFromSequence(
+        __('Welcome series for new subscribers', 'mailpoet'),
+        []
+      ),
+      WorkflowTemplate::TYPE_PREMIUM
+    );
+
+    $userWelcomeSeries = new WorkflowTemplate(
+      'user-welcome-series',
+      WorkflowTemplate::CATEGORY_WELCOME,
+      __(
+        "Welcome new WordPress users to your site. Send an email immediately after a WordPress user registers. Send a follow-up email two days later with more in-depth information.",
+        'mailpoet'
+      ),
+      $this->builder->createFromSequence(
+        __('Welcome series for new WordPress users', 'mailpoet'),
+        []
+      ),
+      WorkflowTemplate::TYPE_PREMIUM
+    );
+
+    $firstPurchase = new WorkflowTemplate(
+      'first-purchase',
+      WorkflowTemplate::CATEGORY_WOOCOMMERCE,
+      __(
+        "Welcome your first-time customers by sending an email with a special offer for their next purchase. Make them feel appreciated within your brand.",
+        'mailpoet'
+      ),
+      $this->builder->createFromSequence(
+        __('Celebrate first-time buyers', 'mailpoet'),
+        []
+      ),
+      WorkflowTemplate::TYPE_COMING_SOON
+    );
+
+    $loyalCustomers = new WorkflowTemplate(
+      'loyal-customers',
+      WorkflowTemplate::CATEGORY_WOOCOMMERCE,
+      __(
+        "These are your most important customers. Make them feel special by sending a thank you note for supporting your brand.",
+        'mailpoet'
+      ),
+      $this->builder->createFromSequence(
+        __('Thank loyal customers', 'mailpoet'),
+        []
+      ),
+      WorkflowTemplate::TYPE_COMING_SOON
+    );
+
+    $abandonedCart = new WorkflowTemplate(
+      'abandoned-cart',
+      WorkflowTemplate::CATEGORY_ABANDONED_CART,
+      __(
+        "Nudge your shoppers to complete the purchase after they added a product to the cart but haven't completed the order. Offer a coupon code as a last resort to convert them to customers.",
+        'mailpoet'
+      ),
+      $this->builder->createFromSequence(
+        __('Abandoned cart reminder', 'mailpoet'),
+        []
+      ),
+      WorkflowTemplate::TYPE_COMING_SOON
+    );
+
+
 
     $templates = $this->wp->applyFilters(Hooks::WORKFLOW_TEMPLATES, [
       $subscriberWelcomeEmail,
       $userWelcomeEmail,
+      $subscriberWelcomeSeries,
+      $userWelcomeSeries,
+      $firstPurchase,
+      $loyalCustomers,
+      $abandonedCart,
     ]);
     return is_array($templates) ? $templates : [];
   }
