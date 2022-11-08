407f3d160993d13f1608c10af1f359a2e750271f
Jan Jakes <jan.jakes@automattic.com>
2022-11-08 10:12:44 +0300

    Hide duplicate workflow button
    [MAILPOET-4786]

--- mailpoet/assets/js/src/automation/listing/components/cells/actions.tsx
+++ mailpoet/assets/js/src/automation/listing/components/cells/actions.tsx
@@ -2,12 +2,7 @@ import { Fragment } from 'react';
 import { __ } from '@wordpress/i18n';
 import { DropdownMenu } from '@wordpress/components';
 import { moreVertical } from '@wordpress/icons';
-import {
-  useDeleteButton,
-  useDuplicateButton,
-  useRestoreButton,
-  useTrashButton,
-} from '../menu';
+import { useDeleteButton, useRestoreButton, useTrashButton } from '../menu';
 import { Workflow } from '../../workflow';
 import { EditWorkflow } from '../actions';
 
@@ -18,12 +13,11 @@ type Props = {
 export function Actions({ workflow }: Props): JSX.Element {
   // Menu items are using custom hooks because the "DropdownMenu" component uses the "controls"
   // attribute rather than child components, but we need to render modal confirmation dialogs.
-  const duplicate = useDuplicateButton(workflow);
   const trash = useTrashButton(workflow);
   const restore = useRestoreButton(workflow);
   const del = useDeleteButton(workflow);
 
-  const menuItems = [duplicate, trash, restore, del].filter((item) => item);
+  const menuItems = [trash, restore, del].filter((item) => item);
 
   return (
     <div className="mailpoet-automation-listing-cell-actions">
