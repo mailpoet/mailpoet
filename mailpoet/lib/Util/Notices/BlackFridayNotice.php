b69315cb106fcde91a734c34a877bd72a64b2e56
Rostislav Wolny <rosta.wolny@automattic.com>
2022-11-10 10:09:53 +0100

    Update sales banner for Black Friday 2022
    [MAILPOET-4712]

--- mailpoet/lib/Util/Notices/BlackFridayNotice.php
+++ mailpoet/lib/Util/Notices/BlackFridayNotice.php
@@ -22,8 +22,8 @@ class BlackFridayNotice {
 
   public function init($shouldDisplay) {
     $shouldDisplay = $shouldDisplay
-      && (time() >= strtotime('2022-10-03 14:00:00 UTC'))
-      && (time() <= strtotime('2022-10-07 14:00:00 UTC'))
+      && (time() >= strtotime('2022-11-23 15:00:00 UTC'))
+      && (time() <= strtotime('2022-11-29 15:00:00 UTC'))
       && !get_transient(self::OPTION_NAME);
     if ($shouldDisplay) {
       $this->display();
@@ -32,9 +32,9 @@ class BlackFridayNotice {
 
   private function display() {
     $subscribers = $this->subscribersRepository->countBy(['deletedAt' => null]);
-    $header = '<h3 class="mailpoet-h3">' . __('Get ready for Black Friday with 40% off MailPoet plans', 'mailpoet') . '</h3>';
-    $body = '<h5 class="mailpoet-h5">' . __('Save 40% on all annual plans until 2 pm UTC, October 7. Terms & conditions apply.', 'mailpoet') . '</h5>';
-    $link = "<p><a href='https://account.mailpoet.com/?s=$subscribers&billing=yearly&ref=sale-october-2022-plugin&utm_source=MP&utm_medium=plugin&utm_campaign=mp_prebfcm' class='mailpoet-button button-primary' target='_blank'>"
+    $header = '<h3 class="mailpoet-h3">' . __('Our Black Friday sale is live! Save 40% for a limited time.', 'mailpoet') . '</h3>';
+    $body = '<h5 class="mailpoet-h5">' . __('Get a 40% discount on all MailPoet plans and upgrades until 3 PM UTC on 29 November. Terms & conditions apply.', 'mailpoet') . '</h5>';
+    $link = "<p><a href='https://account.mailpoet.com/?s=$subscribers&billing=yearly&ref=sale-bfcm-2022-plugin&utm_source=MP&utm_medium=plugin&utm_campaign=mp_bfcm' class='mailpoet-button button-primary' target='_blank'>"
       . __('Shop Now', 'mailpoet')
       . '</a></p>';
 
