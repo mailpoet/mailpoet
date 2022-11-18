40519e8880bbb91ea13ac91ad81055529ac5664f
Rostislav Wolny <rosta.wolny@automattic.com>
2022-11-18 16:42:41 +0100

    Temporarily add static property for MAIN_PAGE_SLUG
    We need to be able to change it when homepage feature is active.
    When we remove the feature flag will switch back to using constant.
    Note I also kept the constant in case a third party plugin uses it.
    [MAILPOET-4824]

--- mailpoet/lib/Config/Menu.php
+++ mailpoet/lib/Config/Menu.php
@@ -29,6 +29,7 @@ use MailPoet\WP\Functions as WPFunctions;
 
 class Menu {
   const MAIN_PAGE_SLUG = 'mailpoet-newsletters';
+  public static $mainPageSlug = 'mailpoet-newsletters';
 
   const EMAILS_PAGE_SLUG = 'mailpoet-newsletters';
   const FORMS_PAGE_SLUG = 'mailpoet-forms';
@@ -175,7 +176,7 @@ class Menu {
       'MailPoet',
       'MailPoet',
       AccessControl::PERMISSION_ACCESS_PLUGIN_ADMIN,
-      self::MAIN_PAGE_SLUG,
+      self::$mainPageSlug,
       null,
       self::ICON_BASE64_SVG,
       30
@@ -183,7 +184,7 @@ class Menu {
 
     // Emails page
     $newslettersPage = $this->wp->addSubmenuPage(
-      self::MAIN_PAGE_SLUG,
+      self::$mainPageSlug,
       $this->setPageTitle(__('Emails', 'mailpoet')),
       esc_html__('Emails', 'mailpoet'),
       AccessControl::PERMISSION_MANAGE_EMAILS,
@@ -197,7 +198,7 @@ class Menu {
     // Homepage
     if ($this->featuresController->isSupported(FeaturesController::FEATURE_HOMEPAGE)) {
       $this->wp->addSubmenuPage(
-        self::MAIN_PAGE_SLUG,
+        self::$mainPageSlug,
         $this->setPageTitle(__('Home', 'mailpoet')),
         esc_html__('Home', 'mailpoet'),
         AccessControl::PERMISSION_ACCESS_PLUGIN_ADMIN,
@@ -238,7 +239,7 @@ class Menu {
 
     // Forms page
     $formsPage = $this->wp->addSubmenuPage(
-      self::MAIN_PAGE_SLUG,
+      self::$mainPageSlug,
       $this->setPageTitle(__('Forms', 'mailpoet')),
       esc_html__('Forms', 'mailpoet'),
       AccessControl::PERMISSION_MANAGE_FORMS,
@@ -304,7 +305,7 @@ class Menu {
 
     // Subscribers page
     $subscribersPage = $this->wp->addSubmenuPage(
-      self::MAIN_PAGE_SLUG,
+      self::$mainPageSlug,
       $this->setPageTitle(__('Subscribers', 'mailpoet')),
       esc_html__('Subscribers', 'mailpoet'),
       AccessControl::PERMISSION_MANAGE_SUBSCRIBERS,
@@ -355,7 +356,7 @@ class Menu {
 
     // Segments page
     $segmentsPage = $this->wp->addSubmenuPage(
-      self::MAIN_PAGE_SLUG,
+      self::$mainPageSlug,
       $this->setPageTitle(__('Lists', 'mailpoet')),
       esc_html__('Lists', 'mailpoet'),
       AccessControl::PERMISSION_MANAGE_SEGMENTS,
@@ -380,7 +381,7 @@ class Menu {
 
     // Settings page
     $this->wp->addSubmenuPage(
-      self::MAIN_PAGE_SLUG,
+      self::$mainPageSlug,
       $this->setPageTitle(__('Settings', 'mailpoet')),
       esc_html__('Settings', 'mailpoet'),
       AccessControl::PERMISSION_MANAGE_SETTINGS,
@@ -393,7 +394,7 @@ class Menu {
 
     // Help page
     $this->wp->addSubmenuPage(
-      self::MAIN_PAGE_SLUG,
+      self::$mainPageSlug,
       $this->setPageTitle(__('Help', 'mailpoet')),
       esc_html__('Help', 'mailpoet'),
       AccessControl::PERMISSION_ACCESS_PLUGIN_ADMIN,
@@ -407,7 +408,7 @@ class Menu {
     // Upgrade page
     // Only show this page in menu if the Premium plugin is not activated
     $this->wp->addSubmenuPage(
-      License::getLicense() ? true : self::MAIN_PAGE_SLUG,
+      License::getLicense() ? true : self::$mainPageSlug,
       $this->setPageTitle(__('Upgrade', 'mailpoet')),
       esc_html__('Upgrade', 'mailpoet'),
       AccessControl::PERMISSION_ACCESS_PLUGIN_ADMIN,
@@ -468,7 +469,7 @@ class Menu {
 
   private function registerAutomationMenu() {
     $automationPage = $this->wp->addSubmenuPage(
-      self::MAIN_PAGE_SLUG,
+      self::$mainPageSlug,
       $this->setPageTitle(__('Automations', 'mailpoet')),
       // @ToDo Remove Beta once Automation is no longer beta.
       '<span>' . esc_html__('Automations', 'mailpoet') . '</span><span class="mailpoet-beta-badge">Beta</span>',
@@ -647,7 +648,7 @@ class Menu {
     // Check if page already exists
     if (
       get_plugin_page_hook($page, '')
-      || WPFunctions::get()->getPluginPageHook($page, self::MAIN_PAGE_SLUG)
+      || WPFunctions::get()->getPluginPageHook($page, self::$mainPageSlug)
     ) {
       return false;
     }
