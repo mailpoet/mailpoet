3c2fb8a910e629d9004d4e2df9b49ca90790cf6a
John Oleksowicz <johnoleksowicz@icloud.com>
2022-11-18 14:17:56 -0600

    Prevent issues if other plugins define WC function
    It's possible that other plugins might define wc_get_page_id, so it's
    more important to know that WC is installed/active than to know if the
    function itself exists.
    
    For good measure I'm including a cast to int even though the WC version
    of the function always returns an integer. This is probably unnecessary
    but it protects us from the possibility of the WC version of the
    function returning something other than an integer in the future.
    
    [MAILPOET-4834]

--- mailpoet/lib/WooCommerce/Helper.php
+++ mailpoet/lib/WooCommerce/Helper.php
@@ -63,8 +63,8 @@ class Helper {
   }
 
   public function wcGetPageId(string $page): ?int {
-    if (function_exists('wc_get_page_id')) {
-      return wc_get_page_id($page);
+    if ($this->isWooCommerceActive()) {
+      return (int)wc_get_page_id($page);
     }
     return null;
   }
