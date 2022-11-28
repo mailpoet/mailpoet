808b705f39239794c7659442ab26257f1bd5b6a5
Sam Najian <sam.najian@automattic.com>
2022-11-28 22:32:28 +0100

    Add error boundaries to logs.tsx
    [MAILPOET-4706]

--- mailpoet/assets/js/src/logs/logs.tsx
+++ mailpoet/assets/js/src/logs/logs.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 
-import { List, Logs, FilterType } from './list';
+import { FilterType, List, Logs } from './list';
+import { ErrorBoundary } from 'common';
 
 interface LogsWindow extends Window {
   mailpoet_logs: Logs;
@@ -14,25 +15,27 @@ if (logsContainer) {
   const url = new URL(window.location.href);
 
   ReactDOM.render(
-    <List
-      logs={window.mailpoet_logs}
-      originalFrom={url.searchParams.get('from')}
-      originalTo={url.searchParams.get('to')}
-      originalSearch={url.searchParams.get('search')}
-      originalOffset={url.searchParams.get('offset')}
-      originalLimit={url.searchParams.get('limit')}
-      onFilter={(data: FilterType): void => {
-        url.searchParams.delete('from');
-        url.searchParams.delete('to');
-        url.searchParams.delete('search');
-        url.searchParams.delete('offset');
-        url.searchParams.delete('limit');
-        Object.entries(data).forEach(([key, value]) => {
-          url.searchParams.append(key, value);
-        });
-        window.location.href = url.href;
-      }}
-    />,
+    <ErrorBoundary>
+      <List
+        logs={window.mailpoet_logs}
+        originalFrom={url.searchParams.get('from')}
+        originalTo={url.searchParams.get('to')}
+        originalSearch={url.searchParams.get('search')}
+        originalOffset={url.searchParams.get('offset')}
+        originalLimit={url.searchParams.get('limit')}
+        onFilter={(data: FilterType): void => {
+          url.searchParams.delete('from');
+          url.searchParams.delete('to');
+          url.searchParams.delete('search');
+          url.searchParams.delete('offset');
+          url.searchParams.delete('limit');
+          Object.entries(data).forEach(([key, value]) => {
+            url.searchParams.append(key, value);
+          });
+          window.location.href = url.href;
+        }}
+      />
+    </ErrorBoundary>,
     logsContainer,
   );
 }
