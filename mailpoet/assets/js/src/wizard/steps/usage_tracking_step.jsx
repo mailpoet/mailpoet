5035d64298809476002ba376c218cad8da41eb93
Rodrigo Primo <rodrigo@automattic.com>
2022-12-07 16:19:52 -0300

    Update the layout of the horizontal lines in the second step of the Welcome Wizard
    In the second step of the wizard, it was necessary to add a <div> as a
    container for all of the options, so that we could use the CSS
    pseudo-class `last-child` to remove the horizontal line from the last
    option.
    
    [MAILPOET-4816]

--- mailpoet/assets/js/src/wizard/steps/usage_tracking_step.jsx
+++ mailpoet/assets/js/src/wizard/steps/usage_tracking_step.jsx
@@ -38,98 +38,100 @@ function WelcomeWizardUsageTrackingStep({ loading, submitForm }) {
       <div className="mailpoet-gap" />
 
       <form onSubmit={submit}>
-        <div className="mailpoet-wizard-woocommerce-option">
-          <div className="mailpoet-wizard-woocommerce-toggle">
-            <YesNo
-              showError={submitted && isNullOrUndefined(state.libs3rdParty)}
-              onCheck={(value) => {
-                const newState = {
-                  libs3rdParty: value,
-                };
-                setState((prevState) => ({ ...prevState, ...newState }));
-              }}
-              checked={state.libs3rdParty}
-              name="mailpoet_libs_3rdParty"
-            />
-          </div>
-          <div>
-            <p>
-              {MailPoet.I18n.t(
-                'welcomeWizardUsageTrackingStepLibs3rdPartyLabel',
-              )}{' '}
-            </p>
-            <div className="mailpoet-wizard-note">
-              <span>
+        <div>
+          <div className="mailpoet-wizard-woocommerce-option">
+            <div className="mailpoet-wizard-woocommerce-toggle">
+              <YesNo
+                showError={submitted && isNullOrUndefined(state.libs3rdParty)}
+                onCheck={(value) => {
+                  const newState = {
+                    libs3rdParty: value,
+                  };
+                  setState((prevState) => ({ ...prevState, ...newState }));
+                }}
+                checked={state.libs3rdParty}
+                name="mailpoet_libs_3rdParty"
+              />
+            </div>
+            <div>
+              <p>
                 {MailPoet.I18n.t(
-                  'welcomeWizardUsageTrackingStepLibs3rdPartyLabelNoteNote',
-                )}
-              </span>
+                  'welcomeWizardUsageTrackingStepLibs3rdPartyLabel',
+                )}{' '}
+              </p>
+              <div className="mailpoet-wizard-note">
+                <span>
+                  {MailPoet.I18n.t(
+                    'welcomeWizardUsageTrackingStepLibs3rdPartyLabelNoteNote',
+                  )}
+                </span>
 
-              {ReactStringReplace(
-                MailPoet.I18n.t(
-                  'welcomeWizardUsageTrackingStepLibs3rdPartyLabelNote',
-                ),
-                /\[link\](.*?)\[\/link\]/g,
-                (match, i) => (
-                  <a
-                    key={i}
-                    href="https://kb.mailpoet.com/article/338-what-3rd-party-libraries-we-use"
-                    data-beacon-article="5f7c7dd94cedfd0017dcece8"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {match}
-                  </a>
-                ),
-              )}
+                {ReactStringReplace(
+                  MailPoet.I18n.t(
+                    'welcomeWizardUsageTrackingStepLibs3rdPartyLabelNote',
+                  ),
+                  /\[link\](.*?)\[\/link\]/g,
+                  (match, i) => (
+                    <a
+                      key={i}
+                      href="https://kb.mailpoet.com/article/338-what-3rd-party-libraries-we-use"
+                      data-beacon-article="5f7c7dd94cedfd0017dcece8"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {match}
+                    </a>
+                  ),
+                )}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="mailpoet-gap" />
+          <div className="mailpoet-gap" />
 
-        <div className="mailpoet-wizard-woocommerce-option">
-          <div className="mailpoet-wizard-woocommerce-toggle">
-            <YesNo
-              showError={submitted && isNullOrUndefined(state.tracking)}
-              onCheck={(value) => {
-                const newState = {
-                  tracking: value,
-                };
-                setState((prevState) => ({ ...prevState, ...newState }));
-              }}
-              checked={state.tracking}
-              name="mailpoet_tracking"
-            />
-          </div>
-          <div>
-            <p>
-              {MailPoet.I18n.t('welcomeWizardUsageTrackingStepTrackingLabel')}{' '}
-            </p>
-            <div className="mailpoet-wizard-note">
-              <span>
-                {MailPoet.I18n.t(
-                  'welcomeWizardUsageTrackingStepTrackingLabelNoteNote',
+          <div className="mailpoet-wizard-woocommerce-option">
+            <div className="mailpoet-wizard-woocommerce-toggle">
+              <YesNo
+                showError={submitted && isNullOrUndefined(state.tracking)}
+                onCheck={(value) => {
+                  const newState = {
+                    tracking: value,
+                  };
+                  setState((prevState) => ({ ...prevState, ...newState }));
+                }}
+                checked={state.tracking}
+                name="mailpoet_tracking"
+              />
+            </div>
+            <div>
+              <p>
+                {MailPoet.I18n.t('welcomeWizardUsageTrackingStepTrackingLabel')}{' '}
+              </p>
+              <div className="mailpoet-wizard-note">
+                <span>
+                  {MailPoet.I18n.t(
+                    'welcomeWizardUsageTrackingStepTrackingLabelNoteNote',
+                  )}
+                </span>
+
+                {ReactStringReplace(
+                  MailPoet.I18n.t(
+                    'welcomeWizardUsageTrackingStepTrackingLabelNote',
+                  ),
+                  /\[link\](.*?)\[\/link\]/g,
+                  (match, i) => (
+                    <a
+                      key={i}
+                      href="https://kb.mailpoet.com/article/130-sharing-your-data-with-us"
+                      data-beacon-article="57ce0aaac6979108399a0454"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {match}
+                    </a>
+                  ),
                 )}
-              </span>
-
-              {ReactStringReplace(
-                MailPoet.I18n.t(
-                  'welcomeWizardUsageTrackingStepTrackingLabelNote',
-                ),
-                /\[link\](.*?)\[\/link\]/g,
-                (match, i) => (
-                  <a
-                    key={i}
-                    href="https://kb.mailpoet.com/article/130-sharing-your-data-with-us"
-                    data-beacon-article="57ce0aaac6979108399a0454"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {match}
-                  </a>
-                ),
-              )}
+              </div>
             </div>
           </div>
         </div>
