74e0774238c7954476ec79615b2ce8a57b1e06ff
Rostislav Wolny <rosta.wolny@automattic.com>
2022-12-08 12:48:54 +0100

    Add email volume limit notice
    [MAILPOET-4825]

--- mailpoet/assets/js/src/homepage/notices.tsx
+++ mailpoet/assets/js/src/homepage/notices.tsx
@@ -1,11 +1,13 @@
 import { Notices } from 'notices/notices';
 import { SubscribersLimitNotice } from 'notices/subscribers_limit_notice';
+import { EmailVolumeLimitNotice } from 'notices/email_volume_limit_notice';
 
 export function HomepageNotices(): JSX.Element {
   return (
     <>
       <Notices />
       <SubscribersLimitNotice />
+      <EmailVolumeLimitNotice />
     </>
   );
 }
