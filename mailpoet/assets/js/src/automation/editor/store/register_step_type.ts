320459eaab55da26f29fe312e40b7cd3c848d646
David Remer <webmaster@websupporter.net>
2022-12-08 10:57:05 +0200

    Add filter mailpoet.register_step_type
    [PREMIUM-213]

--- mailpoet/assets/js/src/automation/editor/store/register_step_type.ts
+++ mailpoet/assets/js/src/automation/editor/store/register_step_type.ts
@@ -1,7 +1,10 @@
 import { dispatch } from '@wordpress/data';
+import { Hooks } from 'wp-js-hooks';
 import { storeName } from './constants';
 import { StepType } from './types';
 
 export const registerStepType = (stepType: StepType): void => {
-  dispatch(storeName).registerStepType(stepType);
+  dispatch(storeName).registerStepType(
+    Hooks.applyFilters('mailpoet.register_step_type', stepType),
+  );
 };
