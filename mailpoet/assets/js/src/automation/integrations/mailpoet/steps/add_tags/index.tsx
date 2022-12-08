d58eb9a28deb12842ccbb2c1d5cb2494aaf91be2
David Remer <webmaster@websupporter.net>
2022-12-08 15:29:55 +0200

    Show Upsell and LockedBadge
    [PREMIUM-213]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/steps/add_tags/index.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/steps/add_tags/index.tsx
@@ -1,21 +1,29 @@
 import { __ } from '@wordpress/i18n';
 import { tag } from '@wordpress/icons';
 import { StepType } from '../../../../editor/store';
+import { LockedBadge } from '../../../../../common/premium_modal/locked_badge';
+import { PremiumModalForStepEdit } from '../../../../../common/premium_modal';
 
 export const step: StepType = {
   key: 'mailpoet:add-tag',
   group: 'actions',
   title: __('Add tag', 'mailpoet'),
   description: __('Add a tag or multiple tags to a subscriber.', 'mailpoet'),
-  subtitle: () =>
-    __(
-      'The premium plugin and your premium license need to be active for this action to work.',
-      'mailpoet',
-    ),
+  subtitle: () => <LockedBadge text={__('Premium', 'mailpoet')} />,
   foreground: '#00A32A',
   background: '#EDFAEF',
   icon: () => (
     <div style={{ width: '100%', height: '100%', scale: '1.4' }}>{tag}</div>
   ),
-  edit: () => null,
+  edit: () => (
+    <PremiumModalForStepEdit
+      tracking={{
+        utm_medium: 'upsell_modal',
+        utm_campaign: 'create_automation_editor_add_tag',
+      }}
+    >
+      {' '}
+      {__('Adding tags is a premium feature.', 'mailpoet')}{' '}
+    </PremiumModalForStepEdit>
+  ),
 } as const;
