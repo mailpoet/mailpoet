4dc31bbdb0fb1d441966b954dcd5e2f0597c4fed
Rostislav Wolny <rosta.wolny@automattic.com>
2022-12-08 13:12:02 +0100

    Add InvalidMssKeyNotice to homepage
    [MAILPOET-4825]

--- mailpoet/assets/js/src/homepage/notices.tsx
+++ mailpoet/assets/js/src/homepage/notices.tsx
@@ -1,6 +1,8 @@
+import { MailPoet } from 'mailpoet';
 import { Notices } from 'notices/notices';
 import { SubscribersLimitNotice } from 'notices/subscribers_limit_notice';
 import { EmailVolumeLimitNotice } from 'notices/email_volume_limit_notice';
+import { InvalidMssKeyNotice } from 'notices/invalid_mss_key_notice';
 
 export function HomepageNotices(): JSX.Element {
   return (
@@ -8,6 +10,10 @@ export function HomepageNotices(): JSX.Element {
       <Notices />
       <SubscribersLimitNotice />
       <EmailVolumeLimitNotice />
+      <InvalidMssKeyNotice
+        mssKeyInvalid={MailPoet.hasInvalidMssApiKey}
+        subscribersCount={MailPoet.subscribersCount}
+      />
     </>
   );
 }
