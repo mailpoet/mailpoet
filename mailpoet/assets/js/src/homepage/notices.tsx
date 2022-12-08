f3e8fb8ae2647427e1ca5a5a150607128f1cafe0
Rostislav Wolny <rosta.wolny@automattic.com>
2022-12-08 12:34:53 +0100

    Add subscribers limit notice to homepage
    [MAILPOET-4825]

--- mailpoet/assets/js/src/homepage/notices.tsx
+++ mailpoet/assets/js/src/homepage/notices.tsx
@@ -1,5 +1,11 @@
 import { Notices } from 'notices/notices';
+import { SubscribersLimitNotice } from 'notices/subscribers_limit_notice';
 
 export function HomepageNotices(): JSX.Element {
-  return <Notices />;
+  return (
+    <>
+      <Notices />
+      <SubscribersLimitNotice />
+    </>
+  );
 }
