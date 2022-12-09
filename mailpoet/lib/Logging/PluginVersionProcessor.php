91a464e96649c87c312d6a0f79d2d5d5c5c3d656
Pavel Dohnal <pavel.dohnal@automattic.com>
2022-12-09 13:09:46 +0100

    Unify inheritance with other processors
    [MAILPOET-4735]

--- mailpoet/lib/Logging/PluginVersionProcessor.php
+++ mailpoet/lib/Logging/PluginVersionProcessor.php
@@ -1,10 +1,11 @@
-<?php declare (strict_types=1);
+<?php declare (strict_types = 1);
 
 namespace MailPoet\Logging;
 
 use MailPoet\Config\Env;
+use MailPoetVendor\Monolog\Processor\ProcessorInterface;
 
-class PluginVersionProcessor {
+class PluginVersionProcessor implements ProcessorInterface {
   public function __invoke(array $record): array {
     $record['extra']['free_plugin_version'] = Env::$version;
     $record['extra']['premium_plugin_version'] = defined('MAILPOET_PREMIUM_VERSION') ? MAILPOET_PREMIUM_VERSION : 'premium not installed';
