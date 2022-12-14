aa118b586e7e7424a55bdaff2e055d5de3c54091
David Remer <webmaster@websupporter.net>
2022-12-14 08:49:26 +0200

    Add RemoveTag action stub
    [PREMIUM-216]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/index.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/index.tsx
@@ -3,6 +3,7 @@ import { step as SendEmailStep } from './steps/send_email';
 import { step as SomeoneSubscribesTrigger } from './steps/someone-subscribes';
 import { step as WpUserRegisteredTrigger } from './steps/wp-user-registered';
 import { step as AddTagsAction } from './steps/add_tags';
+import { step as RemoveTagsAction } from './steps/remove_tags';
 import { registerStepControls } from './step-controls';
 
 export const initialize = (): void => {
@@ -10,5 +11,6 @@ export const initialize = (): void => {
   registerStepType(WpUserRegisteredTrigger);
   registerStepType(SomeoneSubscribesTrigger);
   registerStepType(AddTagsAction);
+  registerStepType(RemoveTagsAction);
   registerStepControls();
 };
