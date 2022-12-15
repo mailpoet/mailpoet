2643cd82449942ae76e4fa1c60da055f625ca292
Rostislav Wolny <rosta.wolny@automattic.com>
2022-12-15 11:14:49 +0100

    Add dummy hide list button
    [MAILPOET-4826]

--- mailpoet/assets/js/src/homepage/components/task-list.tsx
+++ mailpoet/assets/js/src/homepage/components/task-list.tsx
@@ -1,10 +1,23 @@
 import { MailPoet } from 'mailpoet';
+import { DropdownMenu } from '@wordpress/components';
+import { moreVertical } from '@wordpress/icons';
 
 export function TaskList(): JSX.Element {
   return (
     <>
       <h1>{MailPoet.I18n.t('welcomeToMailPoet')}</h1>
       <h2>{MailPoet.I18n.t('beginByCompletingSetup')}</h2>
+      <DropdownMenu
+        label={MailPoet.I18n.t('hideList')}
+        icon={moreVertical}
+        controls={[
+          {
+            title: MailPoet.I18n.t('hideList'),
+            onClick: () => {},
+            icon: null,
+          },
+        ]}
+      />
     </>
   );
 }
