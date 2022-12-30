bb240d8e68072b29b9cb1feafcfa1ee2eb7d6b33
Sam Najian <sam.najian@automattic.com>
2022-12-30 11:01:30 +0100

    Improve call_api return typings
    [MAILPOET-4633]

--- mailpoet/assets/js/src/common/controls/call_api.ts
+++ mailpoet/assets/js/src/common/controls/call_api.ts
@@ -1,17 +1,24 @@
 import { MailPoet } from 'mailpoet';
 import { isErrorResponse } from '../../ajax';
 
-export async function callApi(actionData) {
+export async function callApi<D = unknown, M = unknown>(
+  actionData,
+): Promise<{
+  success: boolean;
+  res: { data: D; meta?: M };
+  error?: unknown;
+}> {
   const { endpoint, action, data } = actionData;
   try {
-    const res = await MailPoet.Ajax.post({
+    const res = await MailPoet.Ajax.post<{ data: D; meta?: M }>({
       api_version: MailPoet.apiVersion,
       endpoint,
       action,
       data,
     });
     return { success: true, res };
-  } catch (res) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (res: any) {
     const error = isErrorResponse(res)
       ? res.errors.map((e) => e.message)
       : null;
