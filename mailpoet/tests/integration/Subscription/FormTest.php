e43fad3f49c43390931b07be0a42a09e68d1f554
Jan Lysý <jan.lysy@automattic.com>
2023-01-03 12:28:38 +0100

    Add log entities cleanup
    [MAILPOET-4104]

--- mailpoet/tests/integration/Subscription/FormTest.php
+++ mailpoet/tests/integration/Subscription/FormTest.php
@@ -8,6 +8,7 @@ use MailPoet\API\JSON\ErrorResponse;
 use MailPoet\API\JSON\Response;
 use MailPoet\DI\ContainerWrapper;
 use MailPoet\Entities\FormEntity;
+use MailPoet\Entities\LogEntity;
 use MailPoet\Entities\SegmentEntity;
 use MailPoet\Entities\SubscriberEntity;
 use MailPoet\Form\Util\FieldNameObfuscator;
@@ -150,6 +151,7 @@ class FormTest extends \MailPoetTest {
     $this->truncateEntity(SegmentEntity::class);
     $this->truncateEntity(FormEntity::class);
     $this->truncateEntity(SubscriberEntity::class);
+    $this->truncateEntity(LogEntity::class);
     $this->diContainer->get(SettingsRepository::class)->truncate();
   }
 }
