be2053758769135ff398ceed8e29e774047bf3b3
Rostislav Wolny <rosta.wolny@automattic.com>
2023-01-05 15:03:53 +0100

    Refactor homepage contents to an extra component
    [MAILPOET-4827]

--- mailpoet/assets/js/src/homepage/components/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-type Props = {
-  children: ReactNode;
-};
-
-export function Layout({ children }: Props): JSX.Element {
-  return <div className="mailpoet-homepage__container">{children}</div>;
-}
