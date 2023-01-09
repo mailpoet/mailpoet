36513dfdb0f28bbe0ba853371a28ca081e809daf
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2023-01-09 09:55:18 +0100

    Fix multi site tests

--- mailpoet/tests/acceptance/Landingpage/LandingpageBasicsCest.php
+++ mailpoet/tests/acceptance/Landingpage/LandingpageBasicsCest.php
@@ -7,15 +7,12 @@ use MailPoet\Test\DataFactories\Features;
 use MailPoet\Test\DataFactories\Settings;
 
 class LandingpageBasicsCest {
-  public function _before() {
-    (new Features())->withFeatureEnabled(FeaturesController::FEATURE_LANDINGPAGE);
-  }
-
   public function landingpageRenders(\AcceptanceTester $i) {
     $i->wantTo('Check landing page renders');
     $i->login();
 
     // show welcome wizard & landing page
+    (new Features())->withFeatureEnabled(FeaturesController::FEATURE_LANDINGPAGE);
     $settings = new Settings();
     $settings->withWelcomeWizard();
 
@@ -28,13 +25,13 @@ class LandingpageBasicsCest {
     $i->wantTo('Check landingpage renders after plugin activation for new users');
     $i->login();
 
-    // show welcome wizard & landing page
-    $settings = new Settings();
-    $settings->withWelcomeWizard();
-
-
     $i->amOnPage('/wp-admin/plugins.php');
     $i->waitForText('Create and send newsletters, post notifications and welcome emails from your WordPress.');
+
+    // show welcome wizard & landing page
+    (new Features())->withFeatureEnabled(FeaturesController::FEATURE_LANDINGPAGE);
+    $settings = new Settings();
+    $settings->withWelcomeWizard();
 
     $i->click('#deactivate-mailpoet');
     $i->waitForText('Plugin deactivated.');
