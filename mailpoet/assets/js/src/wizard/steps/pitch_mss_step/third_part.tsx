ed02327d2fc1babb652de6fa8d51a8a5e95a699c
Rodrigo Primo <rodrigosprimo@gmail.com>
2023-01-10 16:36:31 -0300

    Implement design and content of the third part of the MSS step
    This commit implements the design and content of the third part of the
    MSS step in the welcome wizard. This is a new part of the final step.
    The functionality is not yet implemented and will be in a subsequent
    commit.
    
    [MAILPOET-4818]

---
+++ mailpoet/assets/js/src/wizard/steps/pitch_mss_step/third_part.tsx
@@ -0,0 +1,26 @@
+import { Heading } from 'common/typography/heading/heading';
+import { MailPoet } from 'mailpoet';
+import { Button } from 'common';
+
+function MSSStepThirdPart(): JSX.Element {
+  return (
+    <>
+      <Heading level={1}>
+        {MailPoet.I18n.t('welcomeWizardMSSThirdPartTitle')}
+      </Heading>
+
+      <div className="mailpoet-gap" />
+      <p>{MailPoet.I18n.t('welcomeWizardMSSThirdPartFirstParagraph')}</p>
+      <p>{MailPoet.I18n.t('welcomeWizardMSSThirdPartSecondParagraph')}</p>
+
+      <div className="mailpoet-gap" />
+      <div className="mailpoet-gap" />
+
+      <Button type="button" isFullWidth>
+        {MailPoet.I18n.t('welcomeWizardMSSThirdPartButton')}
+      </Button>
+    </>
+  );
+}
+
+export { MSSStepThirdPart };
