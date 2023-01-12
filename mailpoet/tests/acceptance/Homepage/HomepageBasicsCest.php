e8064cf7a42419fd1d4d5c2354b9ed03137430aa
Rostislav Wolny <rosta.wolny@automattic.com>
2023-01-12 16:13:20 +0100

    Extend homepage acceptance test to check also product discovery section
    [MAILPOET-4827]

--- mailpoet/tests/acceptance/Homepage/HomepageBasicsCest.php
+++ mailpoet/tests/acceptance/Homepage/HomepageBasicsCest.php
@@ -32,17 +32,31 @@ class HomepageBasicsCest {
     $i->dontSee('Sending is broken!');
   }
 
-  public function homepageTaskListRenders(\AcceptanceTester $i) {
-    $i->wantTo('Check homepage renders task list');
+  public function homepageSectionsRender(\AcceptanceTester $i) {
+    $i->wantTo('Check homepage renders all sections');
     $i->login();
     $i->amOnMailpoetPage('Homepage');
+    $i->wantTo('Check homepage renders task list');
     $i->waitForText('Welcome to MailPoet');
     $i->see('Begin by completing your setup');
     $i->see('Sender information added');
     $i->see('Connect MailPoet sending service');
+    $i->wantTo('Hide task list');
     $i->click('button', '.mailpoet-task-list__heading');
     $i->waitForText('Hide setup list', 5, '.mailpoet-task-list__heading .components-popover__content');
     $i->click('.components-popover__content button', '.mailpoet-task-list__heading');
     $i->dontSee('Begin by completing your setup');
+
+    $i->wantTo('Check homepage renders product discovery section');
+    $i->see('Start engaging with your customers');
+    $i->see('Set up a welcome campaign');
+    $i->see('Add a subscription form');
+    $i->see('Send your first newsletter');
+    $i->wantTo('Hide product discovery list');
+    $productDiscoveryHeadingContext = '.mailpoet-homepage-product-discovery .mailpoet-homepage-section__heading';
+    $i->click('button', $productDiscoveryHeadingContext);
+    $i->waitForText('Hide setup list', 5, '.mailpoet-homepage-product-discovery .components-popover__content');
+    $i->click('.components-popover__content button', $productDiscoveryHeadingContext);
+    $i->dontSee('Start engaging with your customers');
   }
 }
