e4c9d037b1260cc7686153ae018ae7d543c0b985
Sam Najian <sam.najian@automattic.com>
2023-01-15 20:06:02 +0100

    Add missing strict_types to Coupon.php
    [MAILPOET-4678]

--- mailpoet/lib/Newsletter/Renderer/Blocks/Coupon.php
+++ mailpoet/lib/Newsletter/Renderer/Blocks/Coupon.php
@@ -1,4 +1,4 @@
-<?php // phpcs:ignore SlevomatCodingStandard.TypeHints.DeclareStrictTypes.DeclareStrictTypesMissing
+<?php declare(strict_types = 1);
 
 namespace MailPoet\Newsletter\Renderer\Blocks;
 
