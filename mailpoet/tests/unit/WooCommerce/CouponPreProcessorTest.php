0e29a3ab0aa5d92b0183351fa0da4c9fc9c820f8
Sam Najian <sam.najian@automattic.com>
2023-01-18 17:30:58 +0100

    Add more unit tests for CouponPreProcessor
    [MAILPOET-4678]

--- mailpoet/tests/unit/WooCommerce/CouponPreProcessorTest.php
+++ mailpoet/tests/unit/WooCommerce/CouponPreProcessorTest.php
@@ -16,6 +16,7 @@ class CouponPreProcessorTest extends \MailPoetUnitTest {
   private $processor;
 
   private static $saveCouponId = 100;
+  private static $updatingCouponId = 5;
 
   public function __construct(
     $name = null,
@@ -23,6 +24,10 @@ class CouponPreProcessorTest extends \MailPoetUnitTest {
     $dataName = ''
   ) {
     parent::__construct($name, $data, $dataName);
+
+    WordPress::interceptFunction('wp_timezone', function() {
+      new \DateTimeZone('UTC');
+    });
 
     $this->processor = new CouponPreProcessor(
       Stub::make(Helper::class),
@@ -44,9 +49,6 @@ class CouponPreProcessorTest extends \MailPoetUnitTest {
   }
 
   public function testEnsureCouponForBlocks() {
-    WordPress::interceptFunction('wp_timezone', function() {
-      new \DateTimeZone('UTC');
-    });
 
     /* @phpstan-ignore-next-line ignoring usage of string instead of class-string */
     $mockedWCCoupon = $this->getMockBuilder('MaybeMissingWC_Coupon')
@@ -59,16 +61,71 @@ class CouponPreProcessorTest extends \MailPoetUnitTest {
 
     $processor = new CouponPreProcessor(
       $wcHelper,
-      Stub::make(NewslettersRepository::class)
+      Stub::make(NewslettersRepository::class, [
+        'flush' => Stub\Expected::never(), // for type = NewsletterEntity::TYPE_AUTOMATIC, the $newsletter->body shouldn't update
+      ], $this)
     );
 
     $newsletter = (new NewsletterEntity());
     $newsletter->setType(NewsletterEntity::TYPE_AUTOMATIC); // so that CouponPreProcessor::processCoupons won't try to persist
     $expiryDay = 5;
+    // NewsletterEntity::TYPE_AUTOMATIC, so that CouponPreProcessor::processCoupons won't try to persist
+    [$newsletter, $blocks] = $this->createNewsletterAndBlockForType(NewsletterEntity::TYPE_AUTOMATIC, $expiryDay);
+    $this->assertWCCouponReceivesCorrectValues($mockedWCCoupon, self::$saveCouponId, $expiryDay);
+
+    $result = $processor->processCoupons($newsletter, $blocks, false);
+    expect($result[0]['blocks'][0]['couponId'])->equals(self::$saveCouponId);
+  }
+
+  public function testEnsureCouponForBlocksSaves() {
+
+    /* @phpstan-ignore-next-line ignoring usage of string instead of class-string */
+    $mockedWCCoupon = $this->getMockBuilder('MaybeMissingWC_Coupon')
+      ->setMethods(['set_code', 'set_discount_type', 'set_amount', 'set_description', 'set_date_expires', 'save'])
+      ->getMock();
+
+    $wcHelper = $this->make(Helper::class, [
+      'createWcCoupon' => $mockedWCCoupon,
+    ]);
+
+    $processor = new CouponPreProcessor(
+      $wcHelper,
+      Stub::make(NewslettersRepository::class, [
+        'flush' => Stub\Expected::once(), // for type != NewsletterEntity::TYPE_AUTOMATIC, the $newsletter->body should update
+      ], $this)
+    );
+
+    $newsletter = (new NewsletterEntity());
+    $newsletter->setType(NewsletterEntity::TYPE_STANDARD); // so that CouponPreProcessor::processCoupons won't try to persist
+    $expiryDay = 10;
+    [$newsletter, $blocks] = $this->createNewsletterAndBlockForType(NewsletterEntity::TYPE_STANDARD, $expiryDay);
+    $this->assertWCCouponReceivesCorrectValues($mockedWCCoupon, self::$updatingCouponId, $expiryDay);
+    /**
+     * If the coupon already is generated for the block, it should not get re-generated
+     */
+    $result = $processor->processCoupons($newsletter, $blocks, false);
+    expect($result[0]['blocks'][0]['couponId'])->equals(self::$updatingCouponId);
+  }
+
+  private function assertWCCouponReceivesCorrectValues($mockedWCCoupon, $expectedCouponId, $expiryDay) {
+    $mockedWCCoupon->method('save')->willReturn($expectedCouponId);
+
+    $mockedWCCoupon->method('set_code')->willReturnCallback(function ($code) {
+      expect($code)->notEmpty();
+    });
+
+    $mockedWCCoupon->method('set_date_expires')->willReturnCallback(function ($date) use ($expiryDay) {
+      expect(\date('Y-m-d', $date))->equals((new \DateTime("now", new \DateTimeZone('UTC')))->modify("+{$expiryDay} days")->format('Y-m-d'));
+    });
+  }
+
+  private function createNewsletterAndBlockForType($newsletterType, $expiryDay): array {
+    $newsletter = (new NewsletterEntity());
+    $newsletter->setType($newsletterType);
     $blocks = [
       [
-      'type' => 'any',
-      'blocks' => [
+        'type' => 'any',
+        'blocks' => [
           [
             'type' => Coupon::TYPE,
             'discountType' => 'percent',
@@ -78,22 +135,7 @@ class CouponPreProcessorTest extends \MailPoetUnitTest {
         ],
       ],
     ];
-    $newsletter->setBody(['blocks' => $blocks]);
-
-    /* @phpstan-ignore-next-line ignoring method of undefined class MaybeMissingWC_Coupon */
-    $mockedWCCoupon->method('save')->willReturn(self::$saveCouponId);
-
-    /* @phpstan-ignore-next-line ignoring method of undefined class MaybeMissingWC_Coupon */
-    $mockedWCCoupon->method('set_code')->willReturnCallback(function ($code) {
-      expect($code)->notEmpty();
-    });
-
-    /* @phpstan-ignore-next-line ignoring method of undefined class MaybeMissingWC_Coupon */
-    $mockedWCCoupon->method('set_date_expires')->willReturnCallback(function ($date) use ($expiryDay) {
-      expect(\date('Y-m-d', $date))->equals((new \DateTime("now", new \DateTimeZone('UTC')))->modify("+{$expiryDay} days")->format('Y-m-d'));
-    });
-
-    $result = $processor->processCoupons($newsletter, $blocks, false);
-    expect($result[0]['blocks'][0]['couponId'])->equals(self::$saveCouponId);
+    $newsletter->setBody(['blocks' => $blocks, 'content' => []]);
+    return [$newsletter, $blocks];
   }
 }
