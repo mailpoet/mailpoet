01042d352f5a62f3129641667c80f347cb4a06c9
Rodrigo Primo <rodrigosprimo@gmail.com>
2023-01-23 16:21:43 -0300

    Implement MSS key verification in the MSS step of the welcome wizard
    [MAILPOET-4818]

--- mailpoet/lib/AdminPages/Pages/WelcomeWizard.php
+++ mailpoet/lib/AdminPages/Pages/WelcomeWizard.php
@@ -4,6 +4,7 @@ namespace MailPoet\AdminPages\Pages;
 
 use MailPoet\AdminPages\PageRenderer;
 use MailPoet\Config\Menu;
+use MailPoet\Config\ServicesChecker;
 use MailPoet\Settings\SettingsController;
 use MailPoet\WooCommerce\Helper as WooCommerceHelper;
 use MailPoet\WP\Functions as WPFunctions;
@@ -23,16 +24,21 @@ class WelcomeWizard {
   /** @var WooCommerceHelper */
   private $wooCommerceHelper;
 
+  /** @var ServicesChecker */
+  private $servicesChecker;
+
   public function __construct(
     PageRenderer $pageRenderer,
     SettingsController $settings,
     WooCommerceHelper $wooCommerceHelper,
-    WPFunctions $wp
+    WPFunctions $wp,
+    ServicesChecker $servicesChecker
   ) {
     $this->pageRenderer = $pageRenderer;
     $this->settings = $settings;
     $this->wooCommerceHelper = $wooCommerceHelper;
     $this->wp = $wp;
+    $this->servicesChecker = $servicesChecker;
   }
 
   public function render() {
@@ -45,12 +51,20 @@ class WelcomeWizard {
       $this->settings->set(WelcomeWizard::TRACK_LOADDED_VIA_WOOCOMMERCE_SETTING_NAME, 1);
     }
 
+    $settings = $this->settings->getAll();
+    $premiumKeyValid = $this->servicesChecker->isPremiumKeyValid(false);
+    // force MSS key check even if the method isn't active
+    $mpApiKeyValid = $this->servicesChecker->isMailPoetAPIKeyValid(false, true);
+
     $data = [
       'finish_wizard_url' => $this->wp->adminUrl('admin.php?page=' . Menu::$mainPageSlug),
       'sender' => $this->settings->get('sender'),
       'admin_email' => $this->wp->getOption('admin_email'),
       'current_wp_user' => $this->wp->wpGetCurrentUser()->to_array(),
       'show_customers_import' => $this->wooCommerceHelper->getCustomersCount() > 0,
+      'settings' => $settings,
+      'premium_key_valid' => !empty($premiumKeyValid),
+      'mss_key_valid' => !empty($mpApiKeyValid),
     ];
     $this->pageRenderer->displayPage('welcome_wizard.html', $data);
   }
