a011a8951e2d66bd0c77eb10951816459fbf55dc
David Remer <webmaster@websupporter.net>
2023-01-25 12:19:42 +0200

    Add MailPoet info to WooCommerce System Status Report
    [MAILPOET-4973]

---
+++ mailpoet/lib/WooCommerce/WooSystemInfo.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\WooCommerce;
+
+use MailPoet\Cron\CronHelper;
+use MailPoet\Router\Endpoints\CronDaemon;
+use MailPoet\Settings\SettingsController;
+
+class WooSystemInfo {
+
+
+  private $cronHelper;
+
+  /** @var SettingsController  */
+  private $settings;
+
+  public function __construct(
+    CronHelper $cronHelper,
+    SettingsController $settings
+  ) {
+    $this->cronHelper = $cronHelper;
+    $this->settings = $settings;
+  }
+
+  public function sendingMethod(): string {
+    return $this->settings->get('mta.method');
+  }
+
+  public function transactionalEmails(): string {
+    return $this->settings->get('send_transactional_emails') ? 'Current sending method' : 'Default WordPress sending method';
+  }
+
+  public function taskSchedulerMethod(): string {
+    return $this->settings->get('cron_trigger.method');
+  }
+
+  public function cronPingUrl(): string {
+    return $this->cronHelper->getCronUrl(CronDaemon::ACTION_PING);
+  }
+
+  public function toArray(): array {
+    return [
+      'sending_method' => $this->sendingMethod(),
+      'transactional_emails' => $this->transactionalEmails(),
+      'task_scheduler_method' => $this->taskSchedulerMethod(),
+      'cron_ping_url' => $this->cronPingUrl(),
+    ];
+  }
+}
