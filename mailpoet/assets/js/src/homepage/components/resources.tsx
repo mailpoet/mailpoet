8c837a4df2d123a1912d7c6333853d958717e3b4
Rostislav Wolny <rosta.wolny@automattic.com>
2023-01-25 16:58:49 +0100

    Add resources simple posts with data
    Resources thumbnails are not available for all posts so as for now I'm
    reusing the thumbnails from the first two posts.
    [MAILPOET-4829]

--- mailpoet/assets/js/src/homepage/components/resources.tsx
+++ mailpoet/assets/js/src/homepage/components/resources.tsx
@@ -1,13 +1,58 @@
 import { MailPoet } from 'mailpoet';
 import { ContentSection } from './content-section';
+import { ResourcePost } from './resource-post';
 
 export function Resources(): JSX.Element {
+  const posts = [
+    <ResourcePost
+      key="createAnEmail"
+      link="https://kb.mailpoet.com/article/141-create-an-email-types-of-campaigns?utm_source=plugin&utm_medium=homepage&utm_campaign=resources"
+      abstract={MailPoet.I18n.t('createAnEmailAbstract')}
+      title={MailPoet.I18n.t('createAnEmailTitle')}
+      imgSrc={`${MailPoet.cdnUrl}homepage/resources/add_form.png`}
+    />,
+    <ResourcePost
+      key="createAForm"
+      link="https://kb.mailpoet.com/article/297-create-a-form-with-our-new-editor?utm_source=plugin&utm_medium=homepage&utm_campaign=resources"
+      abstract={MailPoet.I18n.t('createAFormAbstract')}
+      title={MailPoet.I18n.t('createAFormTitle')}
+      imgSrc={`${MailPoet.cdnUrl}homepage/resources/add_email.png`}
+    />,
+    <ResourcePost
+      key="segmentationGuide"
+      link="https://www.mailpoet.com/blog/email-segmentation/?utm_source=plugin&utm_medium=homepage&utm_campaign=resources"
+      abstract={MailPoet.I18n.t('segmentationGuideAbstract')}
+      title={MailPoet.I18n.t('segmentationGuideTitle')}
+      imgSrc={`${MailPoet.cdnUrl}homepage/resources/add_form.png`}
+    />,
+    <ResourcePost
+      key="reEngagement"
+      link="https://www.mailpoet.com/blog/re-engagement-emails/?utm_source=plugin&utm_medium=homepage&utm_campaign=resources"
+      abstract={MailPoet.I18n.t('reEngagementAbstract')}
+      title={MailPoet.I18n.t('reEngagementTitle')}
+      imgSrc={`${MailPoet.cdnUrl}homepage/resources/add_email.png`}
+    />,
+    <ResourcePost
+      key="marketingStrategy"
+      link="https://www.mailpoet.com/blog/newsletter-marketing-strategy/?utm_source=plugin&utm_medium=homepage&utm_campaign=resources"
+      abstract={MailPoet.I18n.t('marketingStrategyAbstract')}
+      title={MailPoet.I18n.t('marketingStrategyTitle')}
+      imgSrc={`${MailPoet.cdnUrl}homepage/resources/add_form.png`}
+    />,
+    <ResourcePost
+      key="promotingSales"
+      link="https://www.mailpoet.com/blog/how-to-promote-your-sales-with-email-marketing-mailpoet-woocommerce-segmentation/?utm_source=plugin&utm_medium=homepage&utm_campaign=resources"
+      abstract={MailPoet.I18n.t('promotingSalesAbstract')}
+      title={MailPoet.I18n.t('promotingSalesTitle')}
+      imgSrc={`${MailPoet.cdnUrl}homepage/resources/add_email.png`}
+    />,
+  ];
   return (
     <ContentSection
       className="mailpoet-homepage-resources"
       heading={MailPoet.I18n.t('learnMoreAboutEmailMarketing')}
     >
-      Todo
+      {posts.map((post) => post)}
     </ContentSection>
   );
 }
