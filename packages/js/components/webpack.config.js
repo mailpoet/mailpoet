83bac21659a71151306ef79c7413c31a1d1ea597
Jan Jakes <jan.jakes@automattic.com>
2023-01-26 14:56:37 +0100

    Add TypeScript config for @mailpoet/components
    This handles both type checking and type declaration emitting.
    
    [MAILPOET-5015]

--- packages/js/components/webpack.config.js
+++ packages/js/components/webpack.config.js
@@ -1,5 +1,6 @@
 import path from 'node:path';
 import { default as defaultConfig } from '@wordpress/scripts/config/webpack.config.js';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import { WebpackEmitAllPlugin } from '../../../tools/webpack/webpack-emit-all-plugin.js';
 
 const dirname = new URL('.', import.meta.url).pathname;
@@ -9,6 +10,7 @@ const plugins = [
     (plugin) => plugin.constructor.name !== 'DependencyExtractionWebpackPlugin',
   ),
   new WebpackEmitAllPlugin({ context: path.join(dirname, 'src') }),
+  new ForkTsCheckerWebpackPlugin({ typescript: { mode: 'write-dts' } }),
 ];
 
 export default {
