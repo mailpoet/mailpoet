0fb1f4426c4bd27ed3e6d3fbf40f4b12e6309a0e
Rostislav Wolny <rosta.wolny@automattic.com>
2023-01-30 11:23:14 +0100

    Remove currency from the Woo Tracker data
    The currency is already tracked by Woo core under settings.currency.
    [MAILPOET-5037]

--- mailpoet/lib/WooCommerce/Tracker.php
+++ mailpoet/lib/WooCommerce/Tracker.php
@@ -38,7 +38,6 @@ class Tracker {
       $analyticsData = $this->newslettersRepository->getAnalytics();
       $data['extensions']['mailpoet'] = [
         'campaigns_count' => $analyticsData['campaigns_count'],
-        'currency' => $currency,
       ];
       $campaignData = $this->formatCampaignsData($this->wooPurchasesRepository->getRevenuesByCampaigns($currency));
       $data['extensions']['mailpoet'] = array_merge($data['extensions']['mailpoet'], $campaignData);
