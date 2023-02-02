25430762e82a30a5e09a5b4dc24134fe478b4cc9
Jan Lysý <jan.lysy@automattic.com>
2023-02-02 19:29:11 +0100

    Make general panel switchable
    [MAILPOET-4890]

--- mailpoet/assets/js/src/newsletter_editor/blocks/coupon/settings_header.tsx
+++ mailpoet/assets/js/src/newsletter_editor/blocks/coupon/settings_header.tsx
@@ -1,20 +1,14 @@
 import { Button } from '@wordpress/components';
 import classnames from 'classnames';
 import { MailPoet } from 'mailpoet';
-import { useState } from 'react';
 
-function SettingsHeader({ source, onClick }) {
-  const [activeTab, setActiveTab] = useState(source);
-
+function SettingsHeader({ activeTab, onClick }) {
   return (
     <div className="components-panel__header interface-complementary-area-header edit-post-sidebar__panel-tabs">
       <ul>
         <li>
           <Button
-            onClick={() => {
-              onClick('allCoupons');
-              setActiveTab('allCoupons');
-            }}
+            onClick={() => onClick('allCoupons')}
             className={classnames('edit-post-sidebar__panel-tab', {
               'is-active': activeTab === 'allCoupons',
             })}
@@ -25,10 +19,7 @@ function SettingsHeader({ source, onClick }) {
         </li>
         <li>
           <Button
-            onClick={() => {
-              onClick('createNew');
-              setActiveTab('createNew');
-            }}
+            onClick={() => onClick('createNew')}
             className={classnames('edit-post-sidebar__panel-tab', {
               'is-active': activeTab === 'createNew',
             })}
