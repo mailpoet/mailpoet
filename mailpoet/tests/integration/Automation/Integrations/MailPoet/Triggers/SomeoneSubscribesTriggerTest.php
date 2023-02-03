1d1153c0a63dac8ce6d765a5af1b026347e91c0a
David Remer <webmaster@websupporter.net>
2023-02-03 08:15:19 +0200

    Test run_multiple_times setting in SomeoneSubscribesTrigger
    [MAILPOET-4966]

--- mailpoet/tests/integration/Automation/Integrations/MailPoet/Triggers/SomeoneSubscribesTriggerTest.php
+++ mailpoet/tests/integration/Automation/Integrations/MailPoet/Triggers/SomeoneSubscribesTriggerTest.php
@@ -8,20 +8,38 @@ use MailPoet\Automation\Engine\Data\Step;
 use MailPoet\Automation\Engine\Data\StepRunArgs;
 use MailPoet\Automation\Engine\Data\Subject;
 use MailPoet\Automation\Engine\Data\SubjectEntry;
+use MailPoet\Automation\Engine\Storage\AutomationRunStorage;
+use MailPoet\Automation\Engine\Storage\AutomationStorage;
 use MailPoet\Automation\Integrations\MailPoet\Subjects\SegmentSubject;
 use MailPoet\Automation\Integrations\MailPoet\Triggers\SomeoneSubscribesTrigger;
 use MailPoet\Entities\SegmentEntity;
+use MailPoet\Entities\SubscriberEntity;
+use MailPoet\Entities\SubscriberSegmentEntity;
 use MailPoet\Segments\SegmentsRepository;
+use MailPoet\Subscribers\SubscribersRepository;
 
 class SomeoneSubscribesTriggerTest extends \MailPoetTest {
+
+  /** @var AutomationStorage */
+  private $automationStorage;
+
+  /** @var AutomationRunStorage */
+  private $automationRunStorage;
+
   /** @var SegmentsRepository */
   private $segmentRepository;
+
+  /** @var SubscribersRepository */
+  private $subscribersRepository;
 
   /** @var SegmentEntity[] */
   private $segments;
 
   public function _before() {
     $this->segmentRepository = $this->diContainer->get(SegmentsRepository::class);
+    $this->subscribersRepository = $this->diContainer->get(SubscribersRepository::class);
+    $this->automationStorage = $this->diContainer->get(AutomationStorage::class);
+    $this->automationRunStorage = $this->diContainer->get(AutomationRunStorage::class);
     $this->segments = [
       'segment_1' => $this->segmentRepository->createOrUpdate('Segment 1'),
       'segment_2' => $this->segmentRepository->createOrUpdate('Segment 2'),
@@ -80,9 +98,55 @@ class SomeoneSubscribesTriggerTest extends \MailPoetTest {
     ];
   }
 
+  /**
+   * @dataProvider dataForTestItObeysMultipleRunsSetting
+   */
+  public function testItObeysMultipleRunsSetting(bool $runMultipleTimes, int $expectedRuns) {
+    $automation = $this->tester->createAutomation('test',
+      new Step(
+        'trigger',
+        Step::TYPE_TRIGGER,
+        SomeoneSubscribesTrigger::KEY,
+        [
+          'segment_ids' => [],
+          'run_multiple_times' => $runMultipleTimes,
+        ],
+        []
+      )
+    );
+    $this->assertInstanceOf(Automation::class, $automation);
+
+    /** @var SomeoneSubscribesTrigger $testee */
+    $testee = $this->diContainer->get(SomeoneSubscribesTrigger::class);
+    $this->assertCount(0, $this->automationRunStorage->getAutomationRunsForAutomation($automation));
+
+    $subscriber = new SubscriberEntity();
+    $subscriber->setStatus(SubscriberEntity::STATUS_SUBSCRIBED);
+    $subscriber->setEmail('test@mailpoet.com');
+    $this->subscribersRepository->persist($subscriber);
+    $this->subscribersRepository->flush();
+
+    $subscriberSegment1 = new SubscriberSegmentEntity($this->segments['segment_1'], $subscriber, SubscriberEntity::STATUS_SUBSCRIBED);
+    $subscriberSegment2 = new SubscriberSegmentEntity($this->segments['segment_2'], $subscriber, SubscriberEntity::STATUS_SUBSCRIBED);
+    $testee->handleSubscription($subscriberSegment1);
+    $this->assertCount(1, $this->automationRunStorage->getAutomationRunsForAutomation($automation));
+    $testee->handleSubscription($subscriberSegment2);
+    $this->assertCount($expectedRuns, $this->automationRunStorage->getAutomationRunsForAutomation($automation));
+  }
+
+  public function dataForTestItObeysMultipleRunsSetting(): array {
+    return [
+      'run_only_once' => [false, 1],
+      'run_more_often' => [true, 2],
+    ];
+  }
+
   public function _after() {
     $segmentIds = $this->getSegmentIds(array_keys($this->segments));
     $this->segmentRepository->bulkDelete($segmentIds);
+    $this->automationStorage->truncate();
+    $this->automationRunStorage->truncate();
+    $this->subscribersRepository->truncate();
   }
 
   private function getSegmentId(string $index): int {
