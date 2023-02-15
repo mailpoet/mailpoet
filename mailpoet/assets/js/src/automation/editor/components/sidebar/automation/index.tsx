c986b925f924d2eba76e4b5c785c8498d4cc5e56
David Remer <webmaster@websupporter.net>
2023-02-15 11:26:20 +0200

    Render automation sidebar dynamically
    [MAILPOET-4966]

--- mailpoet/assets/js/src/automation/editor/components/sidebar/automation/index.tsx
+++ mailpoet/assets/js/src/automation/editor/components/sidebar/automation/index.tsx
@@ -4,6 +4,27 @@ import { __ } from '@wordpress/i18n';
 import { storeName } from '../../../store';
 import { TrashButton } from '../../actions/trash-button';
 import { locale } from '../../../../config';
+import { Hooks } from '../../../../../hooks';
+import { AutomationSettingElements } from '../../../../types/filters';
+
+function AutomationSettings(): JSX.Element {
+  const settings: AutomationSettingElements = Hooks.applyFilters(
+    'mailpoet.automation.settings.render',
+    {},
+  );
+
+  if (Object.keys(settings).length === 0) {
+    return null;
+  }
+
+  return (
+    <PanelBody title={__('Automation settings', 'mailpoet')} initialOpen>
+      {Object.keys(settings).map((key) => (
+        <PanelRow key={key}>{settings[key]}</PanelRow>
+      ))}
+    </PanelBody>
+  );
+}
 
 export function AutomationSidebar(): JSX.Element {
   const { automationData } = useSelect(
@@ -20,37 +41,40 @@ export function AutomationSidebar(): JSX.Element {
   };
 
   return (
-    <PanelBody title={__('Automation details', 'mailpoet')} initialOpen>
-      <PanelRow>
-        <strong>Date added</strong>{' '}
-        {new Date(Date.parse(automationData.created_at)).toLocaleDateString(
-          locale.toString(),
-          dateOptions,
-        )}
-      </PanelRow>
-      <PanelRow>
-        <strong>Activated</strong>{' '}
-        {automationData.status === 'active' &&
-          new Date(Date.parse(automationData.updated_at)).toLocaleDateString(
+    <>
+      <PanelBody title={__('Automation details', 'mailpoet')} initialOpen>
+        <PanelRow>
+          <strong>Date added</strong>{' '}
+          {new Date(Date.parse(automationData.created_at)).toLocaleDateString(
             locale.toString(),
             dateOptions,
           )}
-        {automationData.status !== 'active' &&
-          automationData.activated_at &&
-          new Date(Date.parse(automationData.activated_at)).toLocaleDateString(
-            locale.toString(),
-            dateOptions,
-          )}
-        {automationData.status !== 'active' && !automationData.activated_at && (
-          <span className="mailpoet-deactive">Not activated yet.</span>
-        )}
-      </PanelRow>
-      <PanelRow>
-        <strong>Author</strong> {automationData.author.name}
-      </PanelRow>
-      <PanelRow>
-        <TrashButton />
-      </PanelRow>
-    </PanelBody>
+        </PanelRow>
+        <PanelRow>
+          <strong>Activated</strong>{' '}
+          {automationData.status === 'active' &&
+            new Date(Date.parse(automationData.updated_at)).toLocaleDateString(
+              locale.toString(),
+              dateOptions,
+            )}
+          {automationData.status !== 'active' &&
+            automationData.activated_at &&
+            new Date(
+              Date.parse(automationData.activated_at),
+            ).toLocaleDateString(locale.toString(), dateOptions)}
+          {automationData.status !== 'active' &&
+            !automationData.activated_at && (
+              <span className="mailpoet-deactive">Not activated yet.</span>
+            )}
+        </PanelRow>
+        <PanelRow>
+          <strong>Author</strong> {automationData.author.name}
+        </PanelRow>
+        <PanelRow>
+          <TrashButton />
+        </PanelRow>
+      </PanelBody>
+      <AutomationSettings />
+    </>
   );
 }
