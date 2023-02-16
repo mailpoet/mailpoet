38e7346b6e8304d3a45fa1568a2ea1de97621d1f
David Remer <webmaster@websupporter.net>
2023-02-16 08:27:16 +0200

    Show setting only in combination with correct triggers
    [MAILPOET-4966]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/automation-sidebar/index.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/automation-sidebar/index.tsx
@@ -1,14 +1,19 @@
 import { Hooks } from 'wp-js-hooks';
 import { AutomationSettingElements } from '../../../types/filters';
-import { RunAutomationOnce } from './run_automation_once';
+import { RunAutomationOnce, showRunOnlyOnce } from './run_automation_once';
 
 export function registerAutomationSidebar() {
   Hooks.addFilter(
     'mailpoet.automation.settings.render',
     'mailpoet',
-    (elements: AutomationSettingElements): AutomationSettingElements => ({
-      ...elements,
-      run_automation_once: <RunAutomationOnce />,
-    }),
+    (elements: AutomationSettingElements): AutomationSettingElements => {
+      if (!showRunOnlyOnce()) {
+        return elements;
+      }
+      return {
+        ...elements,
+        run_automation_once: <RunAutomationOnce />,
+      };
+    },
   );
 }
