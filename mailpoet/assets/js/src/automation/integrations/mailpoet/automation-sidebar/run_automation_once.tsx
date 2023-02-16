3039181454210a5245003af35ae0e3a1786c0e62
David Remer <webmaster@websupporter.net>
2023-02-16 08:27:16 +0200

    Show setting only in combination with correct triggers
    [MAILPOET-4966]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/automation-sidebar/run_automation_once.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/automation-sidebar/run_automation_once.tsx
@@ -1,19 +1,36 @@
 import { ToggleControl } from '@wordpress/components';
-import { dispatch, useSelect } from '@wordpress/data';
+import { dispatch, select, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { storeName } from '../../../editor/store';
 
+export function showRunOnlyOnce(): boolean {
+  const automation = select(storeName).getAutomationData();
+  const triggers = Object.values(automation.steps).filter(
+    (step) => step.type === 'trigger',
+  );
+  if (triggers.length === 0) {
+    return true;
+  }
+
+  const subscriberTriggers = triggers.filter((trigger) =>
+    trigger.subject_keys.includes('mailpoet:subscriber'),
+  );
+  return subscriberTriggers.length > 0;
+}
+
 export function RunAutomationOnce(): JSX.Element {
   const { automationData } = useSelect(
-    (select) => ({
-      automationData: select(storeName).getAutomationData(),
+    (s) => ({
+      automationData: s(storeName).getAutomationData(),
     }),
     [],
   );
+
   const checked =
     (automationData.meta?.run_automation_once as boolean) || false;
   return (
     <ToggleControl
+      className="mailpoet-automation-run-only-once"
       label={__('Run this automation only once per subscriber.', 'mailpoet')}
       checked={checked}
       onChange={(value) => {
