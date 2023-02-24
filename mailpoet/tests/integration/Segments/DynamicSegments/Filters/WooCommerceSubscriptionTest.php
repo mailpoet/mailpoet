6024b5ba42ca1b2beb585e020ed4ae0316fa50e5
Rostislav Wolny <rosta.wolny@automattic.com>
2023-02-24 14:51:26 +0100

    Remove temporary action for disabling Woo compatibility check in test
    WooSubscriptions declared compatibility with HPOS in version 4.9.0
    [MAILPOET-4568]

--- mailpoet/tests/integration/Segments/DynamicSegments/Filters/WooCommerceSubscriptionTest.php
+++ mailpoet/tests/integration/Segments/DynamicSegments/Filters/WooCommerceSubscriptionTest.php
@@ -8,7 +8,6 @@ use MailPoet\Entities\SegmentEntity;
 use MailPoet\Entities\SubscriberEntity;
 use MailPoet\Subscribers\SubscribersRepository;
 use MailPoet\Test\DataFactories\WooCommerceSubscription as WooCommerceSubscriptionFactory;
-use MailPoet\WP\Functions as WPFunctions;
 use MailPoetVendor\Doctrine\DBAL\ForwardCompatibility\DriverStatement;
 use MailPoetVendor\Doctrine\DBAL\Query\QueryBuilder;
 
@@ -38,15 +37,6 @@ class WooCommerceSubscriptionTest extends \MailPoetTest {
   private $subscriptionsFactory;
 
   public function _before(): void {
-    // Temporarily disable the check for incompatible plugins and features in Woo
-    // because WooSubscriptions is still officially not compatible with Woo HPOS
-    $this->diContainer->get(WPFunctions::class)->addAction('woocommerce_init', function () {
-      if (class_exists('\Automattic\WooCommerce\Utilities\FeaturesUtil')) {
-        \Automattic\WooCommerce\Utilities\FeaturesUtil::allow_enabling_features_with_incompatible_plugins();
-        \Automattic\WooCommerce\Utilities\FeaturesUtil::allow_activating_plugins_with_incompatible_features();
-      }
-    });
-
     $this->cleanup();
     $productId = $this->createProduct('Premium Newsletter');
     $this->subscriptionsFactory = new WooCommerceSubscriptionFactory();
