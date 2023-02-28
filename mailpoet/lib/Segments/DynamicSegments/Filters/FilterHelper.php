68902a206d2dfabd51b5d842104110af9f88d54a
John Oleksowicz <johnoleksowicz@icloud.com>
2023-02-28 17:35:50 -0600

    Extract more to helpers
    MAILPOET-4986

---
+++ mailpoet/lib/Segments/DynamicSegments/Filters/FilterHelper.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Segments\DynamicSegments\Filters;
+
+use MailPoet\Entities\SubscriberEntity;
+use MailPoetVendor\Doctrine\DBAL\Query\QueryBuilder;
+use MailPoetVendor\Doctrine\ORM\EntityManager;
+
+class FilterHelper {
+    /** @var EntityManager */
+  private $entityManager;
+
+  public function __construct(
+    EntityManager $entityManager
+  ) {
+    $this->entityManager = $entityManager;
+  }
+
+  public function prefixedTable(string $table): string {
+    global $wpdb;
+    return sprintf('%s%s', $wpdb->prefix, $table);
+  }
+
+  public function getNewSubscribersQueryBuilder(): QueryBuilder {
+    return $this->entityManager
+      ->getConnection()
+      ->createQueryBuilder()
+      ->select('id')
+      ->from($this->getSubscribersTable());
+  }
+
+  public function getSubscribersTable(): string {
+    return $this->entityManager
+      ->getClassMetadata(SubscriberEntity::class)
+      ->getTableName();
+  }
+}
--- mailpoet/lib/Segments/DynamicSegments/Filters/WooCommercePurchaseDate.php
+++ mailpoet/lib/Segments/DynamicSegments/Filters/WooCommercePurchaseDate.php
@@ -3,32 +3,29 @@
 namespace MailPoet\Segments\DynamicSegments\Filters;
 
 use MailPoet\Entities\DynamicSegmentFilterEntity;
-use MailPoet\Entities\SubscriberEntity;
 use MailPoet\Segments\DynamicSegments\Exceptions\InvalidFilterException;
-use MailPoet\Util\DBCollationChecker;
 use MailPoetVendor\Doctrine\DBAL\Query\QueryBuilder;
-use MailPoetVendor\Doctrine\ORM\EntityManager;
 
 class WooCommercePurchaseDate implements Filter {
   const ACTION = 'purchaseDate';
 
-  /** @var EntityManager */
-  private $entityManager;
-
-  /** @var DBCollationChecker */
-  private $collationChecker;
-
   /** @var DateFilterHelper */
   private $dateFilterHelper;
 
+  /** @var FilterHelper */
+  private $filterHelper;
+
+  /** @var WooFilterHelper */
+  private $wooFilterHelper;
+
   public function __construct(
-    EntityManager $entityManager,
-    DBCollationChecker $collationChecker,
-    DateFilterHelper $dateFilterHelper
+    DateFilterHelper $dateFilterHelper,
+    FilterHelper $filterHelper,
+    WooFilterHelper $wooFilterHelper
   ) {
-    $this->entityManager = $entityManager;
-    $this->collationChecker = $collationChecker;
     $this->dateFilterHelper = $dateFilterHelper;
+    $this->filterHelper = $filterHelper;
+    $this->wooFilterHelper = $wooFilterHelper;
   }
 
   public function apply(QueryBuilder $queryBuilder, DynamicSegmentFilterEntity $filter): QueryBuilder {
@@ -36,93 +33,41 @@ class WooCommercePurchaseDate implements Filter {
     $dateValue = $this->dateFilterHelper->getDateValueFromFilter($filter);
     $date = $this->dateFilterHelper->getDateStringForOperator($operator, $dateValue);
     $subQuery = $this->getSubQuery($operator, $date);
+    $subscribersTable = $this->filterHelper->getSubscribersTable();
 
     if (in_array($operator, [DateFilterHelper::NOT_ON, DateFilterHelper::NOT_IN_THE_LAST])) {
-      $queryBuilder->andWhere($queryBuilder->expr()->notIn("{$this->getSubscribersTable()}.id", $subQuery->getSQL()));
+      $queryBuilder->andWhere($queryBuilder->expr()->notIn("{$subscribersTable}.id", $subQuery->getSQL()));
     } else {
-      $queryBuilder->andWhere($queryBuilder->expr()->in("{$this->getSubscribersTable()}.id", $subQuery->getSQL()));
+      $queryBuilder->andWhere($queryBuilder->expr()->in("{$subscribersTable}.id", $subQuery->getSQL()));
     }
 
     return $queryBuilder;
   }
 
   private function getSubQuery(string $operator, string $date): QueryBuilder {
-    $queryBuilder = $this->getNewSubscribersQueryBuilder();
-    $this->applyCustomerLookupJoin($queryBuilder);
-    $this->applyCustomerOrderJoin($queryBuilder);
-    $this->applyOrderStatusFilter($queryBuilder, ['wc-processing', 'wc-completed']);
+    $queryBuilder = $this->filterHelper->getNewSubscribersQueryBuilder();
+    $orderStatsAlias = $this->wooFilterHelper->applyOrderStatusFilter($queryBuilder);
     $quotedDate = $queryBuilder->expr()->literal($date);
 
     switch ($operator) {
       case DateFilterHelper::BEFORE:
-        $queryBuilder->andWhere("DATE(orderStats.date_created) < $quotedDate");
+        $queryBuilder->andWhere("DATE($orderStatsAlias.date_created) < $quotedDate");
         break;
       case DateFilterHelper::AFTER:
-        $queryBuilder->andWhere("DATE(orderStats.date_created) > $quotedDate");
+        $queryBuilder->andWhere("DATE($orderStatsAlias.date_created) > $quotedDate");
         break;
       case DateFilterHelper::IN_THE_LAST:
       case DateFilterHelper::NOT_IN_THE_LAST:
-        $queryBuilder->andWhere("DATE(orderStats.date_created) >= $quotedDate");
+        $queryBuilder->andWhere("DATE($orderStatsAlias.date_created) >= $quotedDate");
         break;
       case DateFilterHelper::ON:
       case DateFilterHelper::NOT_ON:
-        $queryBuilder->andWhere("DATE(orderStats.date_created) = $quotedDate");
+        $queryBuilder->andWhere("DATE($orderStatsAlias.date_created) = $quotedDate");
         break;
       default:
         throw new InvalidFilterException('Incorrect value for operator', InvalidFilterException::MISSING_VALUE);
     }
 
     return $queryBuilder;
-  }
-
-  private function applyOrderStatusFilter(QueryBuilder $queryBuilder, array $allowedStatuses, string $orderStatsAlias = 'orderStats') {
-    $quotedStatus = array_map([$queryBuilder->expr(), 'literal'], $allowedStatuses);
-    $queryBuilder->andWhere($queryBuilder->expr()->in("$orderStatsAlias.status", $quotedStatus));
-  }
-
-  private function applyCustomerOrderJoin(QueryBuilder $queryBuilder, $fromAlias = 'customer', $toAlias = 'orderStats'): QueryBuilder {
-    global $wpdb;
-    $queryBuilder->innerJoin(
-      $fromAlias,
-      $wpdb->prefix . 'wc_order_stats',
-      $toAlias,
-      "$fromAlias.customer_id = $toAlias.customer_id");
-
-    return $queryBuilder;
-  }
-
-  private function applyCustomerLookupJoin(QueryBuilder $queryBuilder, string $alias = 'customer'): QueryBuilder {
-    global $wpdb;
-    $subscribersTable = $this->getSubscribersTable();
-
-    $collation = $this->collationChecker->getCollateIfNeeded(
-      $subscribersTable,
-      'email',
-      $wpdb->prefix . 'wc_customer_lookup',
-      'email'
-    );
-
-    $queryBuilder->innerJoin(
-      $subscribersTable,
-      $wpdb->prefix . 'wc_customer_lookup',
-      $alias,
-      "$subscribersTable.email = $alias.email $collation"
-    );
-
-    return $queryBuilder;
-  }
-
-  private function getNewSubscribersQueryBuilder(): QueryBuilder {
-    return $this->entityManager
-      ->getConnection()
-      ->createQueryBuilder()
-      ->select('id')
-      ->from($this->getSubscribersTable());
-  }
-
-  private function getSubscribersTable(): string {
-    return $this->entityManager
-      ->getClassMetadata(SubscriberEntity::class)
-      ->getTableName();
   }
 }
