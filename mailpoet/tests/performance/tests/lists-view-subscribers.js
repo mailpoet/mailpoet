0a2ccd601583c1c87d7284b0399191b07d3ec6b8
Veljko <veljano@yahoo.com>
2023-02-28 11:58:29 +0100

    Add a new k6 test view subscribers of a list
    [MAILPOET-4957]

---
+++ mailpoet/tests/performance/tests/lists-view-subscribers.js
@@ -0,0 +1,88 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-default-export */
+/**
+ * External dependencies
+ */
+import { sleep, check, group } from 'k6';
+import { chromium } from 'k6/x/browser';
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
+
+/**
+ * Internal dependencies
+ */
+import {
+  baseURL,
+  thinkTimeMin,
+  thinkTimeMax,
+  headlessSet,
+  timeoutSet,
+  defaultListName,
+} from '../config.js';
+import { authenticate } from '../utils/helpers.js';
+/* global Promise */
+
+export function listsViewSubscribers() {
+  const browser = chromium.launch({
+    headless: headlessSet,
+    timeout: timeoutSet,
+  });
+  const page = browser.newPage();
+
+  group('Subscribers - Filter subscribers', () => {
+    page
+      .goto(`${baseURL}/wp-admin/admin.php?page=mailpoet-segments#/lists`, {
+        waitUntil: 'networkidle',
+      })
+
+      .then(() => {
+        authenticate(page);
+      })
+
+      .then(() => {
+        return Promise.all([
+          page.waitForNavigation({ waitUntil: 'networkidle' }),
+        ]);
+      })
+
+      .then(() => {
+        page.waitForSelector('[data-automation-id="dynamic-segments-tab"]');
+        check(page, {
+          'segments tab is visible': page
+            .locator('[data-automation-id="dynamic-segments-tab"]')
+            .isVisible(),
+        });
+      })
+
+      .then(() => {
+        page
+          .locator(
+            '[data-automation-id="segment_name_' + defaultListName + '"]',
+          )
+          .hover();
+        page
+          .locator(
+            '[data-automation-id="view_subscribers_' + defaultListName + '"]',
+          )
+          .click();
+        page.waitForSelector('.mailpoet-listing-no-items');
+        page.waitForSelector('[data-automation-id="filters_subscribed"]');
+        check(page, {
+          'subscribers filter is visible': page
+            .locator('[data-automation-id="listing_filter_segment"]')
+            .isVisible(),
+        });
+        page.waitForLoadState('networkidle');
+      })
+
+      .finally(() => {
+        page.close();
+        browser.close();
+      });
+  });
+
+  sleep(randomIntBetween(thinkTimeMin, thinkTimeMax));
+}
+
+export default function listsViewSubscribersTest() {
+  listsViewSubscribers();
+}
