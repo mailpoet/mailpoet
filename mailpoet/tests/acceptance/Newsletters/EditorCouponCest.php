e8c6f232a74b5549ff2dad670c521aaada79ea7d
Sam Najian <sam.najian@automattic.com>
2023-03-01 21:22:14 +0100

    Acceptance: assert newsletter with coupon is received
    [MAILPOET-5040]

--- mailpoet/tests/acceptance/Newsletters/EditorCouponCest.php
+++ mailpoet/tests/acceptance/Newsletters/EditorCouponCest.php
@@ -2,24 +2,39 @@
 
 namespace MailPoet\Test\Acceptance;
 
+use Codeception\Util\Locator;
 use MailPoet\Newsletter\Renderer\Blocks\Coupon;
 use MailPoet\Test\DataFactories\Newsletter;
+use MailPoet\Test\DataFactories\Settings;
 
 /**
  * @group woo
  */
 class EditorCouponCest {
+
+
+  /** @var Settings */
+  private $settings;
+
+  public function _before() {
+    $this->settings = new Settings();
+  }
+
   public function addCoupon(\AcceptanceTester $i) {
     $couponInEditor = '[data-automation-id="coupon_block"]';
     $couponSettingsHeading = '[data-automation-id="coupon_settings_heading"]';
     $couponSettingsDone = '[data-automation-id="coupon_done_button"]';
     $footer = '[data-automation-id="footer"]';
+    $sendFormElement = '[data-automation-id="newsletter_send_form"]';
+    $emailSubject = 'Newsletter with Coupon';
+    $this->settings->withCronTriggerMethod('Action Scheduler');
 
     $i->activateWooCommerce();
 
     $i->wantTo('Add coupon block to newsletter');
     $newsletter = (new Newsletter())
       ->loadBodyFrom('newsletterWithText.json')
+      ->withSubject($emailSubject)
       ->create();
     $i->login();
     $i->amEditingNewsletter($newsletter->getId());
@@ -36,5 +51,23 @@ class EditorCouponCest {
     $i->wantTo('Close coupon settings panel');
     $i->click($couponSettingsDone);
     $i->seeNoJSErrors();
+
+    $i->wantTo('Send the email with coupon');
+    $i->click('Next');
+    $segmentName = $i->createListWithSubscriber();
+
+    $i->wantTo('Choose list and send');
+    $i->waitForElement($sendFormElement);
+    $i->selectOptionInSelect2($segmentName);
+    $i->click('Send');
+    $i->waitForEmailSendingOrSent();
+    $i->triggerMailPoetActionScheduler();
+
+    $i->wantTo('Verify newsletter with generated coupon is received');
+    $i->checkEmailWasReceived($emailSubject);
+    $i->click('.msglist-message');
+    $i->switchToIFrame("#preview-html");
+    $i->waitForElementVisible('.mailpoet_coupon');
+    $i->dontSee(Locator::contains('.mailpoet_coupon', Coupon::CODE_PLACEHOLDER));
   }
 }
