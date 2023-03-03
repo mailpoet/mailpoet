286d5ab2f9a496f253768ecd35f1a056d3ca0207
Veljko <veljano@yahoo.com>
2023-03-03 18:55:28 +0100

    Refactor formsAdding

--- mailpoet/tests/performance/tests/forms-adding.js
+++ mailpoet/tests/performance/tests/forms-adding.js
@@ -3,7 +3,7 @@
 /**
  * External dependencies
  */
-import { sleep, check, group } from 'k6';
+import { sleep, check } from 'k6';
 import { chromium } from 'k6/experimental/browser';
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
 
@@ -25,59 +25,51 @@ import {
 } from '../utils/helpers.js';
 /* global Promise */
 
-export function formsAdding() {
+export default async function formsAdding() {
   const browser = chromium.launch({
     headless: headlessSet,
     timeout: timeoutSet,
   });
   const page = browser.newPage();
 
-  group('Forms - Add a new form', () => {
-    page
-      .goto(`${baseURL}/wp-admin/admin.php?page=mailpoet-forms`, {
-        waitUntil: 'networkidle',
-      })
+  try {
+    // Go to the page
+    await page.goto(`${baseURL}/wp-admin/admin.php?page=mailpoet-forms`, {
+      waitUntil: 'networkidle',
+    });
 
-      .then(() => {
-        authenticate(page);
-      })
+    // Log in to WP Admin
+    authenticate(page);
 
-      .then(() => {
-        return Promise.all([
-          page.waitForNavigation({ waitUntil: 'networkidle' }),
-        ]);
-      })
+    // Wait for async actions
+    await Promise.all([page.waitForNavigation({ waitUntil: 'networkidle' })]);
 
-      .then(() => {
-        waitAndClick(page, '[data-automation-id="create_new_form"]');
-        sleep(1);
-        page.waitForLoadState('networkidle');
-        waitAndClick(
-          page,
-          '[data-automation-id="select_template_template_1_popup"]',
-        );
-        sleep(1);
-        page.waitForSelector('[data-automation-id="form_title_input"]');
-        selectInSelect2(page, defaultListName);
-        page.waitForSelector('[data-automation-id="form_save_button"]');
-        page.locator('[data-automation-id="form_save_button"]').click();
-        page.waitForSelector('.components-notice');
-        check(page, {
-          'form has been saved notice is present':
-            page.locator('.components-notice__content').textContent() ===
-            'Form saved. Cookies reset — you will see all your dismissed popup forms again.',
-        });
-      })
+    // Wait and click the Add New Form button
+    waitAndClick(page, '[data-automation-id="create_new_form"]');
+    sleep(1);
+    page.waitForLoadState('networkidle');
 
-      .finally(() => {
-        page.close();
-        browser.close();
-      });
-  });
+    // Choose the form template
+    waitAndClick(
+      page,
+      '[data-automation-id="select_template_template_1_popup"]',
+    );
+    sleep(1);
 
+    // Select the list and save the form
+    page.waitForSelector('[data-automation-id="form_title_input"]');
+    selectInSelect2(page, defaultListName);
+    page.waitForSelector('[data-automation-id="form_save_button"]');
+    page.locator('[data-automation-id="form_save_button"]').click();
+    page.waitForSelector('.components-notice');
+    check(page, {
+      'form has been saved notice is visible': page
+        .locator('.components-notice__content')
+        .isVisible(),
+    });
+  } finally {
+    page.close();
+    browser.close();
+  }
   sleep(randomIntBetween(thinkTimeMin, thinkTimeMax));
-}
-
-export default function formsAddingTest() {
-  formsAdding();
 }
