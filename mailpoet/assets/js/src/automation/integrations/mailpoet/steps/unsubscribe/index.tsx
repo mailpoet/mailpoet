5be69b5dd483a55e104a50b35d21c8cd949e78f1
David Remer <webmaster@websupporter.net>
2023-03-03 12:55:50 +0200

    Add Unsubscribe Action Stub
    [PREMIUM-222]

---
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/steps/unsubscribe/index.tsx
@@ -0,0 +1,29 @@
+import { __ } from '@wordpress/i18n';
+import { StepType } from '../../../../editor/store';
+import { LockedBadge } from '../../../../../common/premium_modal/locked_badge';
+import { PremiumModalForStepEdit } from '../../../../../common/premium_modal';
+import { Icon } from './icon';
+
+export const step: StepType = {
+  key: 'mailpoet:unsubscribe',
+  group: 'actions',
+  title: __('Unsubscribe', 'mailpoet'),
+  description: __(
+    'Unsubscribe the subscriber from all marketing emails.',
+    'mailpoet',
+  ),
+  subtitle: () => <LockedBadge text={__('Premium', 'mailpoet')} />,
+  foreground: '#00A32A',
+  background: '#EDFAEF',
+  icon: () => <Icon />,
+  edit: () => (
+    <PremiumModalForStepEdit
+      tracking={{
+        utm_medium: 'upsell_modal',
+        utm_campaign: 'create_automation_editor_unsubscribe',
+      }}
+    >
+      {__('Unsubscribing is a premium feature.', 'mailpoet')}
+    </PremiumModalForStepEdit>
+  ),
+} as const;
