bafbf7a6eaddc2365ae5396c2d69b41d26061702
Veljko <veljano@yahoo.com>
2023-03-06 11:08:22 +0100

    Refactor tests to use k6 browser experimental
    [MAILPOET-5105]

--- mailpoet/tests/performance/tests/subscribers-adding.js
+++ mailpoet/tests/performance/tests/subscribers-adding.js
@@ -3,7 +3,7 @@
 /**
  * External dependencies
  */
-import { sleep, check, group } from 'k6';
+import { sleep, check } from 'k6';
 import { chromium } from 'k6/experimental/browser';
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
 
@@ -23,67 +23,60 @@ import {
 import { authenticate, selectInSelect2 } from '../utils/helpers.js';
 /* global Promise */
 
-export function subscribersAdding() {
+export async function subscribersAdding() {
   const browser = chromium.launch({
     headless: headlessSet,
     timeout: timeoutSet,
   });
   const page = browser.newPage();
 
-  group('Subscribers - Add a new subscriber', () => {
+  try {
     let subscriberEmail =
       'test+' + Math.floor(Math.random() * 9999 + 1) + '@test.com';
 
-    page
-      .goto(`${baseURL}/wp-admin/admin.php?page=mailpoet-subscribers`, {
-        waitUntil: 'networkidle',
-      })
+    // Go to the page
+    await page.goto(`${baseURL}/wp-admin/admin.php?page=mailpoet-subscribers`, {
+      waitUntil: 'networkidle',
+    });
 
-      .then(() => {
-        authenticate(page);
-      })
+    // Log in to WP Admin
+    authenticate(page);
 
-      .then(() => {
-        return Promise.all([
-          page.waitForNavigation({ waitUntil: 'networkidle' }),
-        ]);
-      })
+    // Wait for async actions
+    await Promise.all([page.waitForNavigation({ waitUntil: 'networkidle' })]);
 
-      .then(() => {
-        page
-          .locator('[data-automation-id="add-new-subscribers-button"]')
-          .click();
-        page.locator('input[name="email"]').type(subscriberEmail);
-        page.locator('input[name="first_name"]').type(firstName);
-        page.locator('input[name="last_name"]').type(lastName);
-        selectInSelect2(page, defaultListName);
-        page.locator('button[type="submit"]').click();
-        page.waitForSelector('.mailpoet-listing-no-items');
-        page.waitForSelector('[data-automation-id="filters_subscribed"]');
-        check(page, {
-          'subscribers filter is visible': page
-            .locator('[data-automation-id="listing_filter_segment"]')
-            .isVisible(),
-        });
-      })
+    // Add a new subscriber
+    page.locator('[data-automation-id="add-new-subscribers-button"]').click();
+    page.locator('input[name="email"]').type(subscriberEmail);
+    page.locator('input[name="first_name"]').type(firstName);
+    page.locator('input[name="last_name"]').type(lastName);
+    selectInSelect2(page, defaultListName);
+    page.locator('button[type="submit"]').click();
 
-      .then(() => {
-        page.waitForNavigation({ waitUntil: 'networkidle' });
-        page.locator('#search_input').type(subscriberEmail, { delay: 50 });
-        page.waitForSelector('.mailpoet-listing-no-items');
-        page.waitForSelector('[data-automation-id="filters_subscribed"]');
-        check(page, {
-          'newly added subscriber is present in the listing':
-            page.locator('.mailpoet-listing-title').textContent() ===
-            subscriberEmail,
-        });
-      })
+    // Wait for subscribers listing and verify the filter is visible
+    page.waitForSelector('.mailpoet-listing-no-items');
+    page.waitForSelector('[data-automation-id="filters_subscribed"]');
+    check(page, {
+      'subscribers filter is visible': page
+        .locator('[data-automation-id="listing_filter_segment"]')
+        .isVisible(),
+    });
+    page.waitForLoadState('networkidle');
 
-      .finally(() => {
-        page.close();
-        browser.close();
-      });
-  });
+    // Search for a newly added subscriber and verify
+    page.locator('#search_input').type(subscriberEmail, { delay: 50 });
+    page.waitForSelector('.mailpoet-listing-no-items');
+    page.waitForSelector('[data-automation-id="filters_subscribed"]');
+    page.waitForLoadState('networkidle');
+    check(page, {
+      'newly added subscriber is present in the listing':
+        page.locator('.mailpoet-listing-title').textContent() ===
+        subscriberEmail,
+    });
+  } finally {
+    page.close();
+    browser.close();
+  }
 
   sleep(randomIntBetween(thinkTimeMin, thinkTimeMax));
 }
--- mailpoet/tests/performance/tests/subscribers-listing.js
+++ mailpoet/tests/performance/tests/subscribers-listing.js
@@ -3,7 +3,7 @@
 /**
  * External dependencies
  */
-import { sleep, check, group } from 'k6';
+import { sleep, check } from 'k6';
 import { chromium } from 'k6/experimental/browser';
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
 
@@ -20,48 +20,42 @@ import {
 import { authenticate } from '../utils/helpers.js';
 /* global Promise */
 
-export function subscribersListing() {
+export async function subscribersListing() {
   const browser = chromium.launch({
     headless: headlessSet,
     timeout: timeoutSet,
   });
   const page = browser.newPage();
 
-  group('Subscribers - Load all subscribers', () => {
-    page
-      .goto(`${baseURL}/wp-admin/admin.php?page=mailpoet-subscribers`, {
-        waitUntil: 'networkidle',
-      })
+  try {
+    // Go to the page
+    await page.goto(`${baseURL}/wp-admin/admin.php?page=mailpoet-subscribers`, {
+      waitUntil: 'networkidle',
+    });
 
-      .then(() => {
-        authenticate(page);
-      })
+    // Log in to WP Admin
+    authenticate(page);
 
-      .then(() => {
-        return Promise.all([
-          page.waitForNavigation({ waitUntil: 'networkidle' }),
-        ]);
-      })
+    // Wait for async actions
+    await Promise.all([page.waitForNavigation({ waitUntil: 'networkidle' })]);
 
-      .then(() => {
-        check(page, {
-          'subscribers filter is visible': page
-            .locator('[data-automation-id="listing_filter_segment"]')
-            .isVisible(),
-          'subscribers tag is visible': page
-            .locator('[data-automation-id="listing_filter_tag"]')
-            .isVisible(),
-          'subscribers listing is visible': page
-            .locator('table.mailpoet-listing-table')
-            .isVisible(),
-        });
-      })
-
-      .finally(() => {
-        page.close();
-        browser.close();
-      });
-  });
+    // Verify filter, tag and listing are loaded and visible
+    page.waitForLoadState('networkidle');
+    check(page, {
+      'subscribers filter is visible': page
+        .locator('[data-automation-id="listing_filter_segment"]')
+        .isVisible(),
+      'subscribers tag is visible': page
+        .locator('[data-automation-id="listing_filter_tag"]')
+        .isVisible(),
+      'subscribers listing is visible': page
+        .locator('table.mailpoet-listing-table')
+        .isVisible(),
+    });
+  } finally {
+    page.close();
+    browser.close();
+  }
 
   sleep(randomIntBetween(thinkTimeMin, thinkTimeMax));
 }
