c1ba29de67f0d384df9c026aa12c2599f6eb4d51
Rostislav Wolny <rosta.wolny@automattic.com>
2023-03-09 15:10:43 +0100

    Fix types in form editor hooks
    [MAILPOET-5102]

--- mailpoet/assets/js/src/form_editor/hooks.tsx
+++ mailpoet/assets/js/src/form_editor/hooks.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ComponentType } from 'react';
 // Hook for displaying warning when a block is inserted multiple times
 import '@wordpress/edit-post/build-module/hooks/validate-multiple-use/index.js';
 
@@ -8,7 +8,7 @@ import { addFilter } from '@wordpress/hooks';
 export const initHooks = (): void => {
   // This hook replaces dummy media upload buttons within Gutenberg
   // see https://github.com/WordPress/gutenberg/blob/master/packages/block-editor/src/components/media-upload/README.md
-  const replaceMediaUpload = (): ReactNode => MediaUpload;
+  const replaceMediaUpload = (): ComponentType => MediaUpload;
   addFilter(
     'editor.MediaUpload',
     'mailpoet/form-editor/replace-media-upload',
