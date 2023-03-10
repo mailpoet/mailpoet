1386c221cbbda005d9e0bf59c0d0fb0c1c2dc98b
Rostislav Wolny <rosta.wolny@automattic.com>
2023-03-10 13:33:39 +0100

    Remove unused OmitFirstArgs and OmitFirstArg type helpers
    [MAILPOET-5102]

--- mailpoet/assets/js/src/types/index.ts
+++ mailpoet/assets/js/src/types/index.ts
@@ -34,18 +34,6 @@ import './wordpress_modules';
 
 export * from '../segments/dynamic/types';
 
-// Inspired by: https://neliosoftware.com/blog/adding-typescript-to-wordpress-data-stores/
-export type OmitFirstArg<F> = F extends (
-  first: unknown,
-  ...args: infer P
-) => infer R
-  ? (...args: P) => R
-  : never;
-
-export type OmitFirstArgs<O extends object> = {
-  [K in keyof O]: OmitFirstArg<O[K]>;
-};
-
 declare module '@wordpress/components' {
   export const __experimentalConfirmDialog: ComponentType<
     ComponentProps<typeof ConfirmDialog>
