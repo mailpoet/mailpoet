22cc61971eca6671fc70c0e5cf32759d73c4d1ef
Jan Jakes <jan.jakes@automattic.com>
2023-03-20 11:25:38 +0100

    Remove redundant extensions in imports
    [MAILPOET-5138]

--- mailpoet/assets/js/src/webpack_admin_index.tsx
+++ mailpoet/assets/js/src/webpack_admin_index.tsx
@@ -4,19 +4,19 @@
 // This is to avoid undefined import order & messy WebPack config.
 // Code can be gradually refactored to avoid side effects completely.
 
-import 'homepage/homepage.tsx'; // side effect - renders ReactDOM to document
+import 'homepage/homepage'; // side effect - renders ReactDOM to document
 import 'subscribers/subscribers.jsx'; // side effect - renders ReactDOM to document
 import 'newsletters/newsletters.jsx'; // side effect - renders ReactDOM to window
 import 'segments/segments.jsx'; // side effect - renders ReactDOM to document
 import 'forms/forms.jsx'; // side effect - renders ReactDOM to document
 import 'help/help.jsx'; // side effect - renders ReactDOM to document
 import 'subscribers/importExport/import.jsx'; // side effect - executes on doc ready, adds events
-import 'subscribers/importExport/export.ts'; // side effect - executes on doc ready
-import 'wizard/wizard.tsx'; // side effect - renders ReactDOM to document
+import 'subscribers/importExport/export'; // side effect - executes on doc ready
+import 'wizard/wizard'; // side effect - renders ReactDOM to document
 import 'experimental_features/experimental_features.jsx'; // side effect - renders ReactDOM to document
 import 'logs/logs'; // side effect - renders ReactDOM to document
-import 'sending-paused-notices-fix-button.tsx'; // side effect - renders ReactDOM to document
-import 'sending-paused-notices-resume-button.ts'; // side effect - executes on doc ready, adds events
-import 'sending-paused-notices-authorize-email.tsx'; // side effect - renders ReactDOM to document
+import 'sending-paused-notices-fix-button'; // side effect - renders ReactDOM to document
+import 'sending-paused-notices-resume-button'; // side effect - executes on doc ready, adds events
+import 'sending-paused-notices-authorize-email'; // side effect - renders ReactDOM to document
 import 'landingpage/landingpage'; // side effect - renders ReactDOM to document
-import 'wizard/track_wizard_loaded_via_woocommerce.tsx';
+import 'wizard/track_wizard_loaded_via_woocommerce';
