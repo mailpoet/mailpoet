90a4c982894b741db39b9a83c9e278708b9dd5b1
David Remer <webmaster@websupporter.net>
2023-03-20 08:39:24 +0200

    Provide all possible subjects for a trigger to the UI
    [MAILPOET-4935]

--- mailpoet/lib/AdminPages/Pages/AutomationEditor.php
+++ mailpoet/lib/AdminPages/Pages/AutomationEditor.php
@@ -3,8 +3,10 @@
 namespace MailPoet\AdminPages\Pages;
 
 use MailPoet\AdminPages\PageRenderer;
+use MailPoet\Automation\Engine\Control\SubjectTransformerHandler;
 use MailPoet\Automation\Engine\Data\Automation;
 use MailPoet\Automation\Engine\Hooks;
+use MailPoet\Automation\Engine\Integration\Trigger;
 use MailPoet\Automation\Engine\Mappers\AutomationMapper;
 use MailPoet\Automation\Engine\Registry;
 use MailPoet\Automation\Engine\Storage\AutomationStorage;
@@ -31,13 +33,17 @@ class AutomationEditor {
   /** @var WPFunctions */
   private $wp;
 
+  /** @var SubjectTransformerHandler */
+  private $subjectTransformerHandler;
+
   public function __construct(
     AssetsController $assetsController,
     AutomationMapper $automationMapper,
     AutomationStorage $automationStorage,
     PageRenderer $pageRenderer,
     Registry $registry,
-    WPFunctions $wp
+    WPFunctions $wp,
+    SubjectTransformerHandler $subjectTransformerHandler
   ) {
     $this->assetsController = $assetsController;
     $this->automationMapper = $automationMapper;
@@ -45,6 +51,7 @@ class AutomationEditor {
     $this->pageRenderer = $pageRenderer;
     $this->registry = $registry;
     $this->wp = $wp;
+    $this->subjectTransformerHandler = $subjectTransformerHandler;
   }
 
   public function render() {
@@ -94,7 +101,7 @@ class AutomationEditor {
       $steps[$key] = [
         'key' => $step->getKey(),
         'name' => $step->getName(),
-        'subject_keys' => $step->getSubjectKeys(),
+        'subject_keys' => $step instanceof Trigger ? $this->subjectTransformerHandler->subjectKeysForTrigger($step) : $step->getSubjectKeys(),
         'args_schema' => $step->getArgsSchema()->toArray(),
       ];
     }
