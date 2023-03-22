4af5321d58329a145e1de6bc25d91c8dc1ce843a
Jan Jakes <jan.jakes@automattic.com>
2023-03-22 13:23:34 +0100

    Remove no longer needed hotfixes
    [MAILPOET-5138]

--- mailpoet/assets/js/src/automation/editor/components/form-token-field/index.tsx
+++ mailpoet/assets/js/src/automation/editor/components/form-token-field/index.tsx
@@ -26,9 +26,6 @@ export function FormTokenField({
 }: FormTokenFieldProps): JSX.Element {
   return (
     <WpFormTokenField
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      // The following error seems to be a mismatch. It claims the 'label' prop does not exist, but it does.
       label={label}
       value={value.map((item) => item.name)}
       suggestions={suggestions.map((item) => item.name)}
