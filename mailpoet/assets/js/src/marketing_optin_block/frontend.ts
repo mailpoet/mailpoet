3b321de91e2f368bb4e76952ee9d58bf7a7c6770
Jan Jakes <jan.jakes@automattic.com>
2023-03-24 11:22:43 +0100

    Rewrite .eslintrc.ts.js to new config format
    [MAILPOET-5015]

--- mailpoet/assets/js/src/marketing_optin_block/frontend.ts
+++ mailpoet/assets/js/src/marketing_optin_block/frontend.ts
@@ -6,8 +6,8 @@ import { registerCheckoutBlock } from '@woocommerce/blocks-checkout';
 /**
  * Internal dependencies
  */
-import metadata from './block.json';
-import { FrontendBlock } from './block';
+import metadata from './block.json'; // eslint-disable-line import/no-duplicates -- ESLint detects these two as duplicates of each other
+import { FrontendBlock } from './block'; // eslint-disable-line import/no-duplicates  -- ESLint detects these two as duplicates of each other
 
 registerCheckoutBlock({
   metadata,
