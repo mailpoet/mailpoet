62dcee8ae92ee7af49155da7d94f7d1ecd0918c6
Jan Jakes <jan.jakes@automattic.com>
2023-03-25 22:13:59 +0100

    Merge newsletter tests config into ES5 config
    [MAILPOET-5015]

--- packages/js/eslint-config/eslint-es5.config.js
+++ packages/js/eslint-config/eslint-es5.config.js
@@ -3,6 +3,7 @@ const FlatCompat = require('@eslint/eslintrc').FlatCompat;
 const airbnbLegacyConfig = require('eslint-config-airbnb/legacy');
 const prettierConfig = require('eslint-config-prettier');
 const importPlugin = require('eslint-plugin-import');
+const noOnlyTestsPlugin = require('eslint-plugin-no-only-tests');
 const globals = require('globals');
 
 // compat configs
@@ -26,11 +27,13 @@ module.exports = [
     },
     plugins: {
       import: importPlugin,
+      'no-only-tests': noOnlyTestsPlugin,
     },
     rules: {
       'import/prefer-default-export': 0, // we want to stop using default exports and start using named exports
       'no-underscore-dangle': 0, // Backbone uses underscores, we cannot remove them
       'import/no-default-export': 1, // no default exports
+      'no-only-tests/no-only-tests': 2,
     },
   },
 ];
