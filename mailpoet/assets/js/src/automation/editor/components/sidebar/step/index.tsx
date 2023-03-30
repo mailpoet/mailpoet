6644d42fd718c2706e334671e13a79eb98a2b2e4
Aschepikov <113185437+Aschepikov@users.noreply.github.com>
2023-03-30 12:32:54 +0200

    Revert "Introduce general step error"
    This reverts commit 9738bd3ecd3462b675188f825add69834b62e169.

--- mailpoet/assets/js/src/automation/editor/components/sidebar/step/index.tsx
+++ mailpoet/assets/js/src/automation/editor/components/sidebar/step/index.tsx
@@ -1,33 +1,7 @@
-import { Notice, PanelBody } from '@wordpress/components';
+import { PanelBody } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { storeName } from '../../../store';
 import { StepCard } from '../../step-card';
-
-function StepSidebarGeneralError(): JSX.Element {
-  const { errors } = useSelect(
-    (select) => ({
-      errors: select(storeName).getStepError(
-        select(storeName).getSelectedStep().id,
-      ),
-    }),
-    [],
-  );
-
-  if (!errors || !errors?.fields) {
-    return null;
-  }
-
-  const errorMessage = errors.fields?.general;
-  if (!errorMessage) {
-    return null;
-  }
-
-  return (
-    <Notice isDismissible={false} status="error">
-      {errorMessage}
-    </Notice>
-  );
-}
 
 export function StepSidebar(): JSX.Element {
   const { selectedStep, selectedStepType } = useSelect(
@@ -50,12 +24,12 @@ export function StepSidebar(): JSX.Element {
 
   return (
     <div className="block-editor-block-inspector">
-      <StepSidebarGeneralError />
       <StepCard
         title={selectedStepType.title}
         description={selectedStepType.description}
         icon={selectedStepType.icon}
       />
+
       <Edit
         // Force sidebar remount to avoid different steps mixing their data.
         // This can happen e.g. when having "useState" or "useRef" internally.
