d68fdc76bf1dc6477adf07e6190ef4ad443008a2
David Remer <webmaster@websupporter.net>
2023-03-31 15:09:10 +0300

    Render WooCommerce integration in UI
    [MAILPOET-4999]

--- mailpoet/assets/js/src/automation/integrations/woocommerce/index.tsx
+++ mailpoet/assets/js/src/automation/integrations/woocommerce/index.tsx
@@ -3,9 +3,6 @@ import { step as OrderStatusChanged } from './steps/order_status_changed';
 import { MailPoet } from '../../../mailpoet';
 
 export const initialize = (): void => {
-  // @ToDo Register once transactional emails are implemented
-  return;
-
   if (!MailPoet.isWoocommerceActive) {
     return;
   }
