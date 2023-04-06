6ec53f167ab620efbff2ca40840c86e9194f569a
David Remer <webmaster@websupporter.net>
2023-04-06 13:25:36 +0300

    Introduce ProductsSubject
    [MAILPOET-4934]

--- mailpoet/lib/Automation/Integrations/WooCommerce/WooCommerceIntegration.php
+++ mailpoet/lib/Automation/Integrations/WooCommerce/WooCommerceIntegration.php
@@ -6,6 +6,7 @@ use MailPoet\Automation\Engine\Registry;
 use MailPoet\Automation\Integrations\WooCommerce\Subjects\CustomerSubject;
 use MailPoet\Automation\Integrations\WooCommerce\Subjects\OrderStatusChangeSubject;
 use MailPoet\Automation\Integrations\WooCommerce\Subjects\OrderSubject;
+use MailPoet\Automation\Integrations\WooCommerce\Subjects\ProductsSubject;
 use MailPoet\Automation\Integrations\WooCommerce\Triggers\OrderStatusChangedTrigger;
 
 class WooCommerceIntegration {
@@ -22,6 +23,9 @@ class WooCommerceIntegration {
   /** @var CustomerSubject */
   private $customerSubject;
 
+  /** @var ProductsSubject */
+  private $productsSubject;
+
   /** @var ContextFactory */
   private $contextFactory;
 
@@ -30,12 +34,14 @@ class WooCommerceIntegration {
     OrderStatusChangeSubject $orderStatusChangeSubject,
     OrderSubject $orderSubject,
     CustomerSubject $customerSubject,
+    ProductsSubject $productsSubject,
     ContextFactory $contextFactory
   ) {
     $this->orderStatusChangedTrigger = $orderStatusChangedTrigger;
     $this->orderStatusChangeSubject = $orderStatusChangeSubject;
     $this->orderSubject = $orderSubject;
     $this->customerSubject = $customerSubject;
+    $this->productsSubject = $productsSubject;
     $this->contextFactory = $contextFactory;
   }
 
@@ -48,6 +54,7 @@ class WooCommerceIntegration {
     $registry->addSubject($this->orderSubject);
     $registry->addSubject($this->orderStatusChangeSubject);
     $registry->addSubject($this->customerSubject);
+    $registry->addSubject($this->productsSubject);
     $registry->addTrigger($this->orderStatusChangedTrigger);
   }
 }
