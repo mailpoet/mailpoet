b107546ba39b8d892faa95a6c8d289e4ca36dd8a
John Oleksowicz <johnoleksowicz@icloud.com>
2023-04-06 15:04:53 -0500

    Use correct statuses for product filter
    MAILPOET-5083

--- mailpoet/lib/Segments/DynamicSegments/Filters/WooCommerceProduct.php
+++ mailpoet/lib/Segments/DynamicSegments/Filters/WooCommerceProduct.php
@@ -5,7 +5,6 @@ namespace MailPoet\Segments\DynamicSegments\Filters;
 use MailPoet\Entities\DynamicSegmentFilterData;
 use MailPoet\Entities\DynamicSegmentFilterEntity;
 use MailPoet\Entities\SubscriberEntity;
-use MailPoet\Util\DBCollationChecker;
 use MailPoet\Util\Security;
 use MailPoetVendor\Doctrine\DBAL\Connection;
 use MailPoetVendor\Doctrine\DBAL\Query\QueryBuilder;
@@ -17,15 +16,20 @@ class WooCommerceProduct implements Filter {
   /** @var EntityManager */
   private $entityManager;
 
-  /** @var DBCollationChecker */
-  private $collationChecker;
+  /** @var WooFilterHelper */
+  private $wooFilterHelper;
+
+  /** @var FilterHelper */
+  private $filterHelper;
 
   public function __construct(
     EntityManager $entityManager,
-    DBCollationChecker $collationChecker
+    FilterHelper $filterHelper,
+    WooFilterHelper $wooFilterHelper
   ) {
     $this->entityManager = $entityManager;
-    $this->collationChecker = $collationChecker;
+    $this->wooFilterHelper = $wooFilterHelper;
+    $this->filterHelper = $filterHelper;
   }
 
   public function apply(QueryBuilder $queryBuilder, DynamicSegmentFilterEntity $filter): QueryBuilder {
@@ -36,69 +40,39 @@ class WooCommerceProduct implements Filter {
     $parameterSuffix = $filter->getId() ?? Security::generateRandomString();
 
     if ($operator === DynamicSegmentFilterData::OPERATOR_ANY) {
-      $this->applyCustomerJoin($queryBuilder);
-      $this->applyOrderJoin($queryBuilder);
-      $this->applyProductJoin($queryBuilder);
-      $queryBuilder->where("product.product_id IN (:products_{$parameterSuffix})");
-
+      $orderStatsAlias = $this->wooFilterHelper->applyOrderStatusFilter($queryBuilder);
+      $this->applyProductJoin($queryBuilder, $orderStatsAlias);
+      $queryBuilder->andWhere("product.product_id IN (:products_{$parameterSuffix})");
     } elseif ($operator === DynamicSegmentFilterData::OPERATOR_ALL) {
-      $this->applyCustomerJoin($queryBuilder);
-      $this->applyOrderJoin($queryBuilder);
-      $this->applyProductJoin($queryBuilder);
-      $queryBuilder->where("product.product_id IN (:products_{$parameterSuffix})")
-        ->groupBy("{$subscribersTable}.id, orderStats.order_id")
-        ->having('COUNT(orderStats.order_id) = :count' . $parameterSuffix)
+      $orderStatsAlias = $this->wooFilterHelper->applyOrderStatusFilter($queryBuilder);
+      $this->applyProductJoin($queryBuilder, $orderStatsAlias);
+      $queryBuilder->andWhere("product.product_id IN (:products_{$parameterSuffix})")
+        ->groupBy("{$subscribersTable}.id, $orderStatsAlias.order_id")
+        ->having("COUNT($orderStatsAlias.order_id) = :count" . $parameterSuffix)
         ->setParameter('count' . $parameterSuffix, count($productIds));
 
     } elseif ($operator === DynamicSegmentFilterData::OPERATOR_NONE) {
       // subQuery with subscriber ids that bought products
       $subQuery = $this->createQueryBuilder($subscribersTable);
       $subQuery->select("DISTINCT $subscribersTable.id");
-      $subQuery = $this->applyCustomerJoin($subQuery);
-      $subQuery = $this->applyOrderJoin($subQuery);
-      $subQuery = $this->applyProductJoin($subQuery);
-      $subQuery->where("product.product_id IN (:products_{$parameterSuffix})");
+      $orderStatsAlias = $this->wooFilterHelper->applyOrderStatusFilter($subQuery);
+      $subQuery = $this->applyProductJoin($subQuery, $orderStatsAlias);
+      $subQuery->andWhere("product.product_id IN (:products_{$parameterSuffix})");
       // application subQuery for negation
-      $queryBuilder->where("{$subscribersTable}.id NOT IN ({$subQuery->getSQL()})");
+      $queryBuilder->where("{$subscribersTable}.id NOT IN ({$this->filterHelper->getInterpolatedSQL($subQuery)})");
     }
+    codecept_debug($this->filterHelper->getInterpolatedSQL($queryBuilder));
     return $queryBuilder
       ->setParameter("products_{$parameterSuffix}", $productIds, Connection::PARAM_STR_ARRAY);
   }
 
-  private function applyCustomerJoin(QueryBuilder $queryBuilder): QueryBuilder {
-    global $wpdb;
-    $subscribersTable = $this->entityManager->getClassMetadata(SubscriberEntity::class)->getTableName();
-    $collation = $this->collationChecker->getCollateIfNeeded(
-      $subscribersTable,
-      'email',
-      $wpdb->prefix . 'wc_customer_lookup',
-      'email'
-    );
-    return $queryBuilder->innerJoin(
-      $subscribersTable,
-      $wpdb->prefix . 'wc_customer_lookup',
-      'customer',
-      "$subscribersTable.email = customer.email $collation"
-    );
-  }
-
-  private function applyOrderJoin(QueryBuilder $queryBuilder): QueryBuilder {
-    global $wpdb;
-    return $queryBuilder->join(
-      'customer',
-      $wpdb->prefix . 'wc_order_stats',
-      'orderStats',
-      'customer.customer_id = orderStats.customer_id AND orderStats.status NOT IN ("wc-cancelled", "wc-on-hold", "wc-pending", "wc-failed")'
-    );
-  }
-
-  private function applyProductJoin(QueryBuilder $queryBuilder): QueryBuilder {
+  private function applyProductJoin(QueryBuilder $queryBuilder, string $orderStatsAlias): QueryBuilder {
     global $wpdb;
     return $queryBuilder->innerJoin(
-      'orderStats',
+      $orderStatsAlias,
       $wpdb->prefix . 'wc_order_product_lookup',
       'product',
-      'orderStats.order_id = product.order_id'
+      "$orderStatsAlias.order_id = product.order_id"
     );
   }
 
