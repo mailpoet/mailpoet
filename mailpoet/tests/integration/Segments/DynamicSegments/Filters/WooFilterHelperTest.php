4576dd289fc94e1ebd1188da18727cf914e541ef
John Oleksowicz <johnoleksowicz@icloud.com>
2023-04-07 16:54:24 -0500

    Add woo filter helper tests for order status
    MAILPOET-5083

---
+++ mailpoet/tests/integration/Segments/DynamicSegments/Filters/WooFilterHelperTest.php
@@ -0,0 +1,78 @@
+<?php declare(strict_types = 1);
+
+namespace integration\Segments\DynamicSegments\Filters;
+
+use MailPoet\Segments\DynamicSegments\Filters\WooFilterHelper;
+
+class WooFilterHelperTest extends \MailPoetTest {
+  /** @var WooFilterHelper */
+  private $wooFilterHelper;
+
+  public function _before() {
+    parent::_before();
+    $this->wooFilterHelper = $this->diContainer->get(WooFilterHelper::class);
+  }
+
+  /**
+   * @dataProvider allowedStatuses
+   */
+  public function testItCanJoinCustomersBasedOnPurchaseStatus($status) {
+    $customerId = $this->tester->createCustomer('customer@example.com');
+    $this->createOrder($customerId, $status);
+    $queryBuilder = $this->tester->getSubscribersQueryBuilder();
+    $this->wooFilterHelper->applyOrderStatusFilter($queryBuilder);
+    $emails = $this->tester->getSubscriberEmailsFromQueryBuilder($queryBuilder);
+    expect($emails)->contains('customer@example.com');
+  }
+
+  /**
+   * @dataProvider disallowedStatuses
+   */
+  public function testItExcludesDisallowedOrderStatuses($status) {
+    $customerId = $this->tester->createCustomer('customer@example.com');
+    $this->createOrder($customerId, $status);
+    $queryBuilder = $this->tester->getSubscribersQueryBuilder();
+    $this->wooFilterHelper->applyOrderStatusFilter($queryBuilder);
+    $emails = $this->tester->getSubscriberEmailsFromQueryBuilder($queryBuilder);
+    expect($emails)->notContains('customer@example.com');
+  }
+
+  public function testOrderStatusesCanBeOverridden() {
+    $customerId = $this->tester->createCustomer('refunded@example.com');
+    $this->createOrder($customerId, 'wc-refunded');
+    $customerId2 = $this->tester->createCustomer('completed@example.com');
+    $this->createOrder($customerId2, 'wc-completed');
+    $queryBuilder = $this->tester->getSubscribersQueryBuilder();
+    $this->wooFilterHelper->applyOrderStatusFilter($queryBuilder, ['wc-refunded']);
+    $emails = $this->tester->getSubscriberEmailsFromQueryBuilder($queryBuilder);
+    expect($emails)->contains('refunded@example.com');
+    expect($emails)->notContains('completed@example.com');
+  }
+
+  public function allowedStatuses() {
+    return [
+      'completed' => ['wc-completed'],
+      'processing' => ['wc-processing'],
+    ];
+  }
+
+  public function disallowedStatuses() {
+    return [
+      'refunded' => ['wc-refunded'],
+      'cancelled' => ['wc-cancelled'],
+      'on hold' => ['wc-on-hold'],
+      'pending' => ['wc-pending'],
+      'failed' => ['wc-failed'],
+    ];
+  }
+
+  private function createOrder(int $customerId, string $status): int {
+    $order = $this->tester->createWooCommerceOrder();
+    $order->set_customer_id($customerId);
+    $order->set_status($status);
+    $order->save();
+    $this->tester->updateWooOrderStats($order->get_id());
+
+    return $order->get_id();
+  }
+}
