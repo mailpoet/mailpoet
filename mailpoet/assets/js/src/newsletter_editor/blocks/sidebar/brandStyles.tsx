b276202e45baeb7490c42e7af983b8fb176926c7
Sam Najian <sam.najian@automattic.com>
2023-04-07 20:24:51 +0200

    Add brand/theme colors and typography to boxes
    [MAILPOET-5193]

--- mailpoet/assets/js/src/newsletter_editor/blocks/sidebar/brandStyles.tsx
+++ mailpoet/assets/js/src/newsletter_editor/blocks/sidebar/brandStyles.tsx
@@ -1,6 +1,34 @@
 import { __ } from '@wordpress/i18n';
+import { useState } from 'react';
+
+export const STYLE_TYPES = {
+  brand: 'brand',
+  theme: 'theme',
+};
+
+const templateColors = {
+  selectedStyle: 'brand',
+  brand: {
+    // Todo: get from settings
+    fontFamily: 'SF Pro Text',
+    fontWeight: 'normal',
+    background: '#ffffff',
+    foreground: '#dfa8bb',
+  },
+  theme: {
+    // Todo: get from settings
+    fontFamily: 'SF Pro Text',
+    fontWeight: '800',
+    background: '#ffffff',
+    foreground: '#ad86e9',
+  },
+};
 
 export function BrandStyles() {
+  const { brand, theme, selectedStyle } = templateColors;
+  const [selected, setSelected] = useState(selectedStyle);
+  const selectStyle = (style: string) => () => setSelected(style);
+
   return (
     <>
       <h4>
@@ -12,10 +40,32 @@ export function BrandStyles() {
             type="radio"
             name="style_type"
             id="style_type_brand"
-            value="brand"
-            defaultChecked
+            value={STYLE_TYPES.brand}
+            checked={selected === STYLE_TYPES.brand}
+            onClick={selectStyle(STYLE_TYPES.brand)}
           />
           <label htmlFor="style_type_brand">
+            <h1
+              className="style_type_typography"
+              style={{
+                fontFamily: brand.fontFamily,
+                fontWeight: brand.fontWeight,
+              }}
+            >
+              Aa
+            </h1>
+            <span className="style_type_colors">
+              <span
+                style={{
+                  background: brand.foreground,
+                }}
+              />
+              <span
+                style={{
+                  background: brand.background,
+                }}
+              />
+            </span>
             <i className="mailpoet_checkmark radio_checkmark" />
           </label>
         </div>
@@ -24,9 +74,32 @@ export function BrandStyles() {
             type="radio"
             name="style_type"
             id="style_type_theme"
-            value="theme"
+            value={STYLE_TYPES.theme}
+            checked={selected === STYLE_TYPES.theme}
+            onClick={selectStyle(STYLE_TYPES.theme)}
           />
           <label htmlFor="style_type_theme">
+            <h1
+              className="style_type_typography"
+              style={{
+                fontFamily: theme.fontFamily,
+                fontWeight: theme.fontWeight,
+              }}
+            >
+              Aa
+            </h1>
+            <span className="style_type_colors">
+              <span
+                style={{
+                  background: theme.foreground,
+                }}
+              />
+              <span
+                style={{
+                  background: theme.background,
+                }}
+              />
+            </span>
             <i className="mailpoet_checkmark radio_checkmark" />
           </label>
         </div>
