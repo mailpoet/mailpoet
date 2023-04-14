d0dbe445ca237fd87db7f36d3202284aa0b37534
Jan Jakes <jan.jakes@automattic.com>
2023-04-14 15:23:46 +0200

    Implement datetime filter
    [MAILPOET-5000]

--- mailpoet/lib/Automation/Integrations/Core/CoreIntegration.php
+++ mailpoet/lib/Automation/Integrations/Core/CoreIntegration.php
@@ -4,16 +4,22 @@ namespace MailPoet\Automation\Integrations\Core;
 
 use MailPoet\Automation\Engine\Integration;
 use MailPoet\Automation\Engine\Registry;
+use MailPoet\Automation\Engine\WordPress;
 use MailPoet\Automation\Integrations\Core\Actions\DelayAction;
 
 class CoreIntegration implements Integration {
   /** @var DelayAction */
   private $delayAction;
 
+  /** @var WordPress */
+  private $wordPress;
+
   public function __construct(
-    DelayAction $delayAction
+    DelayAction $delayAction,
+    WordPress $wordPress
   ) {
     $this->delayAction = $delayAction;
+    $this->wordPress = $wordPress;
   }
 
   public function register(Registry $registry): void {
@@ -23,6 +29,7 @@ class CoreIntegration implements Integration {
     $registry->addFilter(new Filters\NumberFilter());
     $registry->addFilter(new Filters\IntegerFilter());
     $registry->addFilter(new Filters\StringFilter());
+    $registry->addFilter(new Filters\DateTimeFilter($this->wordPress->wpTimezone()));
     $registry->addFilter(new Filters\EnumFilter());
     $registry->addFilter(new Filters\EnumArrayFilter());
   }
