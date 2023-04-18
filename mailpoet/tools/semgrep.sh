3fce71e0253e48361cee7c95e39be4b4299acdf3
alex-mailpoet <alex-mailpoet@users.noreply.github.com>
2023-04-18 17:51:34 +0300

    Run Semgrep check on build
    [MAILPOET-5230]

---
+++ mailpoet/tools/semgrep.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Save our starting location, so we can jump back there later.
+scriptdirectory=${PWD}
+rulesdirectory='tools/wpscan-semgrep-rules'
+
+# Make sure we have a copy of WPScan's Semgrep rules.
+if [ ! -d $scriptdirectory/$rulesdirectory ]
+    then
+        echo "Cloning WPScan's Semgrep rules repository..."
+        git clone git@github.com:Automattic/wpscan-semgrep-rules.git $scriptdirectory/$rulesdirectory
+fi
+
+# Run Semgrep
+docker run --rm -v "${scriptdirectory}:/src" returntocorp/semgrep semgrep --error --text --metrics=off -c "/src/${rulesdirectory}/audit" $@
