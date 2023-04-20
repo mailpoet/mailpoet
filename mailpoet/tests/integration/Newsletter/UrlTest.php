2558d99afebdc8f6e09860d6a514a607eb6bdf76
Brezo Cordero <8002881+brezocordero@users.noreply.github.com>
2023-04-20 14:39:35 -0500

    Return same data object on empty or null subscriber
    If subscriber is empty we want to return the same token as if it is null.
    
    [MAILPOET-5248]

---
+++ mailpoet/tests/integration/Newsletter/UrlTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Test\Newsletter;
+
+use MailPoet\Entities\NewsletterEntity;
+use MailPoet\Entities\SubscriberEntity;
+use MailPoet\Newsletter\Url as NewsletterUrl;
+use MailPoet\Util\Security;
+
+class UrlTest extends \MailPoetTest {
+  public function testPreviewUrlIsTheSameForNullOrEmptySubscriber() {
+    $newsletter = new NewsletterEntity();
+    $newsletter->setSubject('some subject');
+    $newsletter->setType(NewsletterEntity::TYPE_STANDARD);
+    $newsletter->setHash(Security::generateHash());
+    $newsletter->setStatus(NewsletterEntity::STATUS_SENT);
+    $newsletterUrl = $this->diContainer->get(NewsletterUrl::class);
+
+    $urlNullSubscriber = $newsletterUrl->getViewInBrowserUrl($newsletter);
+
+    $emptySubscriber = new SubscriberEntity();
+    $urlEmptySubscriber = $newsletterUrl->getViewInBrowserUrl($newsletter, $emptySubscriber);
+
+    expect($urlNullSubscriber)->equals($urlEmptySubscriber);
+  }
+}
