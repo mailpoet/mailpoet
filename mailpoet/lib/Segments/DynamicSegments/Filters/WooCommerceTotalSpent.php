46316b48e2f39615b2158daf9f40d88ec3b990ff
John Oleksowicz <johnoleksowicz@icloud.com>
2023-04-21 16:00:15 -0500

    Clean up code left over after refactoring
    MAILPOET-4987

--- mailpoet/lib/Segments/DynamicSegments/Filters/WooCommerceTotalSpent.php
+++ mailpoet/lib/Segments/DynamicSegments/Filters/WooCommerceTotalSpent.php
@@ -3,38 +3,23 @@
 namespace MailPoet\Segments\DynamicSegments\Filters;
 
 use MailPoet\Entities\DynamicSegmentFilterEntity;
-use MailPoet\Entities\SubscriberEntity;
-use MailPoet\Util\DBCollationChecker;
 use MailPoet\Util\Security;
 use MailPoetVendor\Carbon\Carbon;
 use MailPoetVendor\Doctrine\DBAL\Query\QueryBuilder;
-use MailPoetVendor\Doctrine\ORM\EntityManager;
 
 class WooCommerceTotalSpent implements Filter {
   const ACTION_TOTAL_SPENT = 'totalSpent';
-
-  /** @var EntityManager */
-  private $entityManager;
-
-  /** @var DBCollationChecker */
-  private $collationChecker;
 
   /** @var WooFilterHelper */
   private $wooFilterHelper;
 
   public function __construct(
-    EntityManager $entityManager,
-    DBCollationChecker $collationChecker,
     WooFilterHelper $wooFilterHelper
   ) {
-    $this->entityManager = $entityManager;
-    $this->collationChecker = $collationChecker;
     $this->wooFilterHelper = $wooFilterHelper;
   }
 
   public function apply(QueryBuilder $queryBuilder, DynamicSegmentFilterEntity $filter): QueryBuilder {
-    global $wpdb;
-    $subscribersTable = $this->entityManager->getClassMetadata(SubscriberEntity::class)->getTableName();
     $filterData = $filter->getFilterData();
     $type = $filterData->getParam('total_spent_type');
     $amount = $filterData->getParam('total_spent_amount');
@@ -42,13 +27,6 @@ class WooCommerceTotalSpent implements Filter {
 
     $date = Carbon::now()->subDays($days);
     $parameterSuffix = $filter->getId() ?? Security::generateRandomString();
-    $collation = $this->collationChecker->getCollateIfNeeded(
-      $subscribersTable,
-      'email',
-      $wpdb->prefix . 'wc_customer_lookup',
-      'email'
-    );
-
     $orderStatsAlias = $this->wooFilterHelper->applyOrderStatusFilter($queryBuilder);
     $dateParam = "date_$parameterSuffix";
 
