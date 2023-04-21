a6fcc60de3a32aad32ef9f72f69157038dbb66f1
Jan Jakes <jan.jakes@automattic.com>
2023-04-21 12:34:58 +0200

    Add "of" preposition to matches any/all/none
    [MAILPOET-5257]

--- mailpoet/lib/Automation/Integrations/Core/Filters/EnumArrayFilter.php
+++ mailpoet/lib/Automation/Integrations/Core/Filters/EnumArrayFilter.php
@@ -9,9 +9,9 @@ use MailPoet\Validator\Builder;
 use MailPoet\Validator\Schema\ObjectSchema;
 
 class EnumArrayFilter implements Filter {
-  public const CONDITION_MATCHES_ANY = 'matches-any';
-  public const CONDITION_MATCHES_ALL = 'matches-all';
-  public const CONDITION_MATCHES_NONE = 'matches-none';
+  public const CONDITION_MATCHES_ANY_OF = 'matches-any-of';
+  public const CONDITION_MATCHES_ALL_OF = 'matches-all-of';
+  public const CONDITION_MATCHES_NONE_OF = 'matches-none-of';
 
   public function getFieldType(): string {
     return Field::TYPE_ENUM_ARRAY;
@@ -19,9 +19,9 @@ class EnumArrayFilter implements Filter {
 
   public function getConditions(): array {
     return [
-      self::CONDITION_MATCHES_ANY => __('matches any', 'mailpoet'),
-      self::CONDITION_MATCHES_ALL => __('matches all', 'mailpoet'),
-      self::CONDITION_MATCHES_NONE => __('matches none', 'mailpoet'),
+      self::CONDITION_MATCHES_ANY_OF => __('matches any of', 'mailpoet'),
+      self::CONDITION_MATCHES_ALL_OF => __('matches all of', 'mailpoet'),
+      self::CONDITION_MATCHES_NONE_OF => __('matches none of', 'mailpoet'),
     ];
   }
 
@@ -46,11 +46,11 @@ class EnumArrayFilter implements Filter {
     $filterCount = count($filterValue);
     $matchedCount = count(array_intersect($value, $filterValue));
     switch ($data->getCondition()) {
-      case self::CONDITION_MATCHES_ANY:
+      case self::CONDITION_MATCHES_ANY_OF:
         return $filterCount > 0 && $matchedCount > 0;
-      case self::CONDITION_MATCHES_ALL:
+      case self::CONDITION_MATCHES_ALL_OF:
         return $filterCount > 0 && $matchedCount === count($filterValue);
-      case self::CONDITION_MATCHES_NONE:
+      case self::CONDITION_MATCHES_NONE_OF:
         return $matchedCount === 0;
       default:
         return false;
