d9bec53efa972f847b86141d2729f0b0a8d93519
Rostislav Wolny <rosta.wolny@automattic.com>
2023-04-25 14:02:14 +0200

    Introduce new service for reporting subscriber stats
    Bridge is a low level service that processes requests and responses to/from Bridge API.
    This change is needed so that we can remove dependency on MailPoet\Util\License\Features\Subscribers
    service from the Bridge. The dependecy is a higher level service and may easily cause a circular dependency issue.
    [MAILPOET-5191]

--- mailpoet/lib/DI/ContainerConfigurator.php
+++ mailpoet/lib/DI/ContainerConfigurator.php
@@ -428,6 +428,7 @@ class ContainerConfigurator implements IContainerConfigurator {
     $container->autowire(\MailPoet\Services\AuthorizedEmailsController::class)->setPublic(true);
     $container->autowire(\MailPoet\Services\CongratulatoryMssEmailController::class)->setPublic(true);
     $container->autowire(\MailPoet\Services\AuthorizedSenderDomainController::class)->setPublic(true);
+    $container->autowire(\MailPoet\Services\SubscribersCountReporter::class)->setPublic(true);
     // Settings
     $container->autowire(\MailPoet\Settings\SettingsController::class)->setPublic(true);
     $container->autowire(\MailPoet\Settings\SettingsChangeHandler::class)->setPublic(true);
