476e0603fb5f3ee2387801e2e8fec211e49625b9
Rostislav Wolny <rosta.wolny@automattic.com>
2023-04-28 10:52:59 +0200

    Hide subscriber limit error from mailer log in case plugin display general error
    [MAILPOET-5191]

--- mailpoet/assets/js/src/notices/mailer_error.tsx
+++ mailpoet/assets/js/src/notices/mailer_error.tsx
@@ -112,6 +112,18 @@ export function MailerError({
     return null;
   }
 
+  // When plugin detects that the subscriber limit has been reached (via regular key check)
+  // it displays a notification on all pages (based on MailPoet.subscribersLimitReached).
+  // So in such case we ignore the email volume limit error notification coming from mailer log to avoid duplication.
+  // We still need to display the error in case the plugin doesn't know the limit has been reached from the API key check.
+  if (
+    mtaMethod === 'MailPoet' &&
+    mtaLog.error.operation === 'subscriber_limit_reached' &&
+    MailPoet.subscribersLimitReached
+  ) {
+    return null;
+  }
+
   if (mtaLog.error.operation === 'migration') {
     const className = classnames('mailpoet_notice notice notice-warning', {
       inline: isInline,
