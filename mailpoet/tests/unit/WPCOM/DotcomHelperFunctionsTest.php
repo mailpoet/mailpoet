1eb85c606cd4ac494a247376126bc68e4d0399c0
Brezo Cordero <8002881+brezocordero@users.noreply.github.com>
2023-05-04 12:39:06 -0500

    Formatting and namespace in test
    [MAILPOET-4974]

--- mailpoet/tests/unit/WPCOM/DotcomHelperFunctionsTest.php
+++ mailpoet/tests/unit/WPCOM/DotcomHelperFunctionsTest.php
@@ -1,8 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace unit\WPCOM;
-
-use MailPoet\WPCOM\DotcomHelperFunctions;
+namespace MailPoet\WPCOM;
 
 class DotcomHelperFunctionsTest extends \MailPoetUnitTest {
   /*** @var DotcomHelperFunctions */
