be47e9da4f1d207887147274287a0bd9d4b6c070
David Remer <webmaster@websupporter.net>
2023-05-09 14:16:46 +0300

    Make \WC_Customer nullable in Payload construction
    [MAILPOET-5290]

--- mailpoet/lib/Automation/Integrations/WooCommerce/Payloads/CustomerPayload.php
+++ mailpoet/lib/Automation/Integrations/WooCommerce/Payloads/CustomerPayload.php
@@ -6,20 +6,20 @@ use MailPoet\Automation\Engine\Integration\Payload;
 
 class CustomerPayload implements Payload {
 
-  /** @var \WC_Customer */
+  /** @var \WC_Customer | null */
   private $customer;
 
   public function __construct(
-    \WC_Customer $customer
+    \WC_Customer $customer = null
   ) {
     $this->customer = $customer;
   }
 
-  public function getCustomer(): \WC_Customer {
+  public function getCustomer(): ?\WC_Customer {
     return $this->customer;
   }
 
   public function getId(): int {
-    return $this->customer->get_id();
+    return $this->customer ? $this->customer->get_id() : 0;
   }
 }
