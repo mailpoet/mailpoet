cc352e6e2d0f2f6a756c63fadf65172ab1847328
Jan Jakes <jan.jakes@automattic.com>
2023-05-11 13:44:31 +0200

    Add WordPress integration
    [MAILPOET-5168]

---
+++ mailpoet/lib/Automation/Integrations/WordPress/WordPressIntegration.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Automation\Integrations\WordPress;
+
+use MailPoet\Automation\Engine\Registry;
+
+class WordPressIntegration {
+  public function register(Registry $registry): void {
+  }
+}
