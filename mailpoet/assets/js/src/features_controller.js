fa5c13a73e5281667f5f39a3b3f0315a8f55fb67
Sam Najian <sam.najian@automattic.com>
2023-05-15 19:41:14 +0200

    Add default empty value for features

--- mailpoet/assets/js/src/features_controller.js
+++ mailpoet/assets/js/src/features_controller.js
@@ -1,4 +1,4 @@
-export const FeaturesController = (config) => ({
+export const FeaturesController = (config = {}) => ({
   FEATURE_BRAND_TEMPLATES: 'brand_templates',
 
   isSupported: (feature) => {
