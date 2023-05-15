98b1b8bc4c2863faaf138ee740e7714371f55358
Veljko <veljano@yahoo.com>
2023-05-15 14:31:53 +0200

    Add new test EditorAbandonedCartBlock
    [MAILPOET-5308]

---
+++ mailpoet/tests/acceptance/Newsletters/EditorAbandonedCartBlockCest.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Test\Acceptance;
+
+use MailPoet\Entities\NewsletterEntity;
+use MailPoet\Test\DataFactories\Newsletter;
+
+class EditorAbandonedCartBlockCest {
+  public function addAbandonedCart(\AcceptanceTester $i) {
+    $i->wantTo('add abandoned cart block to newsletter');
+
+    $abandonedCartOverlayMessage = '.mailpoet_overlay_message';
+    $abandonedCartOverlay = '[data-automation-id="acc_overlay"]';
+    $overlayMessage = 'This is only a preview! Your customers will see the product(s) they left in their shopping cart.';
+    $abandonedCartSettingsAssertion = 'Display options';
+    $footer = '[data-automation-id="footer"]';
+
+    // Create newsletter without AC block
+    $newsletter = (new Newsletter())
+      ->loadBodyFrom('newsletterWithText.json')
+      ->withAutomaticTypeWooCommerceAbandonedCart()
+      ->withStatus(NewsletterEntity::STATUS_DRAFT)
+      ->create();
+
+    $i->login();
+    $i->amEditingNewsletter($newsletter->getId());
+
+    // Move the Abandoned Cart block to the editor
+    $i->dragAndDrop('#automation_editor_block_abandoned_cart_content', '#mce_1');
+
+    // Open settings by clicking on the block
+    $i->moveMouseOver($footer, 3, 2);
+    $i->moveMouseOver($abandonedCartOverlay, 3, 2);
+    $i->waitForElementVisible($abandonedCartOverlayMessage);
+    $i->see($overlayMessage, $abandonedCartOverlayMessage);
+    $i->click($abandonedCartOverlayMessage);
+    $i->waitForText($abandonedCartSettingsAssertion);
+  }
+}
