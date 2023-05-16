140375112861ece5bb70c4eaec1dffc4f786d9ac
Jan Jakes <jan.jakes@automattic.com>
2023-05-16 13:08:37 +0200

    Add automations to menu test, improve test
    [MAILPOET-5296]

--- mailpoet/tests/acceptance/Misc/MailpoetMenuCest.php
+++ mailpoet/tests/acceptance/Misc/MailpoetMenuCest.php
@@ -3,16 +3,23 @@
 namespace MailPoet\Test\Acceptance;
 
 class MailpoetMenuCest {
-  public function sendEmail(\AcceptanceTester $i) {
+  public function rootPages(\AcceptanceTester $i) {
     $i->wantTo('Use MailPoet menu in WordPress admin');
 
     $i->login();
     $i->amOnPage('/wp-admin');
+
     $i->click('MailPoet');
+    $i->waitForElement('.mailpoet-subscribers-stats');
+    $i->seeInCurrentUrl('?page=mailpoet-homepage');
 
     $i->click('Emails');
     $i->waitForElement('.mailpoet-newsletter-type');
     $i->seeInCurrentUrl('?page=mailpoet-newsletters');
+
+    $i->click('Automations');
+    $i->waitForElement('.mailpoet-section-build-your-own');
+    $i->seeInCurrentUrl('?page=mailpoet-automation');
 
     $i->click('Forms');
     $i->waitForElement('[data-automation-id="create_new_form"]');
@@ -33,5 +40,11 @@ class MailpoetMenuCest {
     $i->click('Help');
     $i->waitForElement('.mailpoet-tab-content');
     $i->seeInCurrentUrl('?page=mailpoet-help');
+  }
+
+  private function checkHomepage(\AcceptanceTester $i) {
+    $i->click('MailPoet');
+    $i->waitForElement('.mailpoet-subscribers-stats');
+    $i->seeInCurrentUrl('?page=mailpoet-homepage');
   }
 }
