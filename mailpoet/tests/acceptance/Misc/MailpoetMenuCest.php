3f277dce132fdc9ffb30f99d9711e1f7d30c6b22
Jan Jakes <jan.jakes@automattic.com>
2023-05-16 14:14:03 +0200

    Split page asertions into methods
    [MAILPOET-5296]

--- mailpoet/tests/acceptance/Misc/MailpoetMenuCest.php
+++ mailpoet/tests/acceptance/Misc/MailpoetMenuCest.php
@@ -9,42 +9,61 @@ class MailpoetMenuCest {
     $i->login();
     $i->amOnPage('/wp-admin');
 
-    $i->click('MailPoet');
-    $i->waitForElement('.mailpoet-subscribers-stats');
-    $i->seeInCurrentUrl('?page=mailpoet-homepage');
-
-    $i->click('Emails');
-    $i->waitForElement('.mailpoet-newsletter-type');
-    $i->seeInCurrentUrl('?page=mailpoet-newsletters');
-
-    $i->click('Automations');
-    $i->waitForElement('.mailpoet-section-build-your-own');
-    $i->seeInCurrentUrl('?page=mailpoet-automation');
-
-    $i->click('Forms');
-    $i->waitForElement('[data-automation-id="create_new_form"]');
-    $i->seeInCurrentUrl('?page=mailpoet-forms');
-
-    $i->click('Subscribers');
-    $i->waitForElement('.mailpoet-subscribers-in-plan');
-    $i->seeInCurrentUrl('?page=mailpoet-subscribers');
-
-    $i->click('Lists');
-    $i->waitForElement('[data-automation-id="new-segment"]');
-    $i->seeInCurrentUrl('?page=mailpoet-segments');
-
-    $i->click('Settings');
-    $i->waitForElement('[data-automation-id="basic_settings_tab"]');
-    $i->seeInCurrentUrl('?page=mailpoet-settings');
-
-    $i->click('Help');
-    $i->waitForElement('.mailpoet-tab-content');
-    $i->seeInCurrentUrl('?page=mailpoet-help');
+    $this->checkHomepage($i);
+    $this->checkNewsletters($i);
+    $this->checkAutomations($i);
+    $this->checkForms($i);
+    $this->checkSubscribers($i);
+    $this->checkLists($i);
+    $this->checkSettings($i);
+    $this->checkHelp($i);
   }
 
   private function checkHomepage(\AcceptanceTester $i) {
     $i->click('MailPoet');
     $i->waitForElement('.mailpoet-subscribers-stats');
     $i->seeInCurrentUrl('?page=mailpoet-homepage');
+  }
+
+  private function checkNewsletters(\AcceptanceTester $i) {
+    $i->click('Emails');
+    $i->waitForElement('.mailpoet-newsletter-type');
+    $i->seeInCurrentUrl('?page=mailpoet-newsletters');
+  }
+
+  private function checkAutomations(\AcceptanceTester $i) {
+    $i->click('Automations');
+    $i->waitForElement('.mailpoet-section-build-your-own');
+    $i->seeInCurrentUrl('?page=mailpoet-automation');
+  }
+
+  private function checkForms(\AcceptanceTester $i) {
+    $i->click('Forms');
+    $i->waitForElement('[data-automation-id="create_new_form"]');
+    $i->seeInCurrentUrl('?page=mailpoet-forms');
+  }
+
+  private function checkSubscribers(\AcceptanceTester $i) {
+    $i->click('Subscribers');
+    $i->waitForElement('.mailpoet-subscribers-in-plan');
+    $i->seeInCurrentUrl('?page=mailpoet-subscribers');
+  }
+
+  private function checkLists(\AcceptanceTester $i) {
+    $i->click('Lists');
+    $i->waitForElement('[data-automation-id="new-segment"]');
+    $i->seeInCurrentUrl('?page=mailpoet-segments');
+  }
+
+  private function checkSettings(\AcceptanceTester $i) {
+    $i->click('Settings');
+    $i->waitForElement('[data-automation-id="basic_settings_tab"]');
+    $i->seeInCurrentUrl('?page=mailpoet-settings');
+  }
+
+  private function checkHelp(\AcceptanceTester $i) {
+    $i->click('Help');
+    $i->waitForElement('.mailpoet-tab-content');
+    $i->seeInCurrentUrl('?page=mailpoet-help');
   }
 }
