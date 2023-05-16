4fb68388120c76f96a6cba613ce99b78897097c9
Jan Jakes <jan.jakes@automattic.com>
2023-05-16 14:15:05 +0200

    Check for selected menu items, make sure clicks are in the MP menu
    [MAILPOET-5296]

--- mailpoet/tests/acceptance/Misc/MailpoetMenuCest.php
+++ mailpoet/tests/acceptance/Misc/MailpoetMenuCest.php
@@ -2,6 +2,8 @@
 
 namespace MailPoet\Test\Acceptance;
 
+use Codeception\Util\Locator;
+
 class MailpoetMenuCest {
   public function rootPages(\AcceptanceTester $i) {
     $i->wantTo('Use MailPoet menu in WordPress admin');
@@ -20,50 +22,66 @@ class MailpoetMenuCest {
   }
 
   private function checkHomepage(\AcceptanceTester $i) {
-    $i->click('MailPoet');
+    $this->clickMenuItem($i, 'MailPoet');
     $i->waitForElement('.mailpoet-subscribers-stats');
     $i->seeInCurrentUrl('?page=mailpoet-homepage');
+    $this->assertSelectedMenuItem($i, 'Home');
   }
 
   private function checkNewsletters(\AcceptanceTester $i) {
-    $i->click('Emails');
+    $this->clickMenuItem($i, 'Emails');
     $i->waitForElement('.mailpoet-newsletter-type');
     $i->seeInCurrentUrl('?page=mailpoet-newsletters');
+    $this->assertSelectedMenuItem($i, 'Emails');
   }
 
   private function checkAutomations(\AcceptanceTester $i) {
-    $i->click('Automations');
+    $this->clickMenuItem($i, 'Automations');
     $i->waitForElement('.mailpoet-section-build-your-own');
     $i->seeInCurrentUrl('?page=mailpoet-automation');
+    $this->assertSelectedMenuItem($i, 'Automations');
   }
 
   private function checkForms(\AcceptanceTester $i) {
-    $i->click('Forms');
+    $this->clickMenuItem($i, 'Forms');
     $i->waitForElement('[data-automation-id="create_new_form"]');
     $i->seeInCurrentUrl('?page=mailpoet-forms');
+    $this->assertSelectedMenuItem($i, 'Forms');
   }
 
   private function checkSubscribers(\AcceptanceTester $i) {
-    $i->click('Subscribers');
+    $this->clickMenuItem($i, 'Subscribers');
     $i->waitForElement('.mailpoet-subscribers-in-plan');
     $i->seeInCurrentUrl('?page=mailpoet-subscribers');
+    $this->assertSelectedMenuItem($i, 'Subscribers');
   }
 
   private function checkLists(\AcceptanceTester $i) {
-    $i->click('Lists');
+    $this->clickMenuItem($i, 'Lists');
     $i->waitForElement('[data-automation-id="new-segment"]');
     $i->seeInCurrentUrl('?page=mailpoet-segments');
+    $this->assertSelectedMenuItem($i, 'Lists');
   }
 
   private function checkSettings(\AcceptanceTester $i) {
-    $i->click('Settings');
+    $this->clickMenuItem($i, 'Settings');
     $i->waitForElement('[data-automation-id="basic_settings_tab"]');
     $i->seeInCurrentUrl('?page=mailpoet-settings');
+    $this->assertSelectedMenuItem($i, 'Settings');
   }
 
   private function checkHelp(\AcceptanceTester $i) {
-    $i->click('Help');
+    $this->clickMenuItem($i, 'Help');
     $i->waitForElement('.mailpoet-tab-content');
     $i->seeInCurrentUrl('?page=mailpoet-help');
+    $this->assertSelectedMenuItem($i, 'Help');
+  }
+
+  private function clickMenuItem(\AcceptanceTester $i, string $label): void {
+    $i->click($label, '#adminmenu .menu-top.toplevel_page_mailpoet-homepage');
+  }
+
+  private function assertSelectedMenuItem(\AcceptanceTester $i, string $label): void {
+    $i->seeElement(Locator::contains('#toplevel_page_mailpoet-homepage.wp-has-current-submenu > .wp-submenu > .current', $label));
   }
 }
