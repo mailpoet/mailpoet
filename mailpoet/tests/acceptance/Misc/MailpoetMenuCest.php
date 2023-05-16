e8bcbfe618c3b98cdffe370866d866ec3d4ae0a8
Jan Jakes <jan.jakes@automattic.com>
2023-05-16 14:57:22 +0200

    Add checks for subpages to menu test
    [MAILPOET-5296]

--- mailpoet/tests/acceptance/Misc/MailpoetMenuCest.php
+++ mailpoet/tests/acceptance/Misc/MailpoetMenuCest.php
@@ -33,12 +33,35 @@ class MailpoetMenuCest {
     $i->waitForElement('.mailpoet-newsletter-type');
     $i->seeInCurrentUrl('?page=mailpoet-newsletters');
     $this->assertSelectedMenuItem($i, 'Emails');
+
+    // editor
+    $i->waitForElementClickable('[data-automation-id="create_standard"]');
+    $i->click('[data-automation-id="create_standard"]');
+    $i->waitForElementClickable('[data-automation-id="select_template_0"]');
+    $i->click('[data-automation-id="select_template_0"]');
+    $i->waitForElement('#mailpoet_editor');
+    $i->seeInCurrentUrl('?page=mailpoet-newsletter-editor');
+    $this->assertSelectedMenuItem($i, 'Emails');
   }
 
   private function checkAutomations(\AcceptanceTester $i) {
     $this->clickMenuItem($i, 'Automations');
     $i->waitForElement('.mailpoet-section-build-your-own');
     $i->seeInCurrentUrl('?page=mailpoet-automation');
+    $this->assertSelectedMenuItem($i, 'Automations');
+
+    // templates
+    $i->waitForElementClickable(Locator::contains('button', 'Start with a template'));
+    $i->click(Locator::contains('button', 'Start with a template'));
+    $i->waitForElement('.mailpoet-automation-templates');
+    $i->seeInCurrentUrl('?page=mailpoet-automation-templates');
+    $this->assertSelectedMenuItem($i, 'Automations');
+
+    // editor
+    $i->waitForElementClickable(Locator::contains('button', 'Welcome new subscribers'));
+    $i->click(Locator::contains('button', 'Welcome new subscribers'));
+    $i->waitForElement('#mailpoet_automation_editor');
+    $i->seeInCurrentUrl('?page=mailpoet-automation-editor');
     $this->assertSelectedMenuItem($i, 'Automations');
   }
 
@@ -47,12 +70,41 @@ class MailpoetMenuCest {
     $i->waitForElement('[data-automation-id="create_new_form"]');
     $i->seeInCurrentUrl('?page=mailpoet-forms');
     $this->assertSelectedMenuItem($i, 'Forms');
+
+    // templates
+    $i->waitForElementClickable(Locator::contains('button', 'New Form'));
+    $i->click(Locator::contains('button', 'New Form'));
+    $i->waitForElementClickable('[data-automation-id="select_template_template_1_popup"]');
+    $i->seeInCurrentUrl('?page=mailpoet-form-editor-template-selection');
+    $this->assertSelectedMenuItem($i, 'Forms');
+
+    // editor
+    $i->click('[data-automation-id="select_template_template_1_popup"]');
+    $i->waitForElement('#mailpoet_form_edit');
+    $i->seeInCurrentUrl('?page=mailpoet-form-editor');
+    $this->assertSelectedMenuItem($i, 'Forms');
   }
 
   private function checkSubscribers(\AcceptanceTester $i) {
     $this->clickMenuItem($i, 'Subscribers');
     $i->waitForElement('.mailpoet-subscribers-in-plan');
     $i->seeInCurrentUrl('?page=mailpoet-subscribers');
+    $this->assertSelectedMenuItem($i, 'Subscribers');
+
+    // import
+    $i->waitForElementClickable('[data-automation-id="import-subscribers-button"]');
+    $i->click('[data-automation-id="import-subscribers-button"]');
+    $i->waitForElement('#import_container');
+    $i->seeInCurrentUrl('?page=mailpoet-import');
+    $this->assertSelectedMenuItem($i, 'Subscribers');
+
+    // export
+    $this->clickMenuItem($i, 'Subscribers');
+    $i->waitForElement('.mailpoet-subscribers-in-plan');
+    $i->waitForElementClickable('#mailpoet_export_button');
+    $i->click('#mailpoet_export_button');
+    $i->waitForElement('#mailpoet-export');
+    $i->seeInCurrentUrl('?page=mailpoet-export');
     $this->assertSelectedMenuItem($i, 'Subscribers');
   }
 
