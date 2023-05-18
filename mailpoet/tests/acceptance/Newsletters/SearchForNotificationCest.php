5ea51812ca2781b6d752d49bf2d98caf135f8b65
Veljko <veljano@yahoo.com>
2023-05-18 15:00:38 +0200

    Updated 2 tests for searching newsletter and post notification
    [MAILPOET-5311]

--- mailpoet/tests/acceptance/Newsletters/SearchForNotificationCest.php
+++ mailpoet/tests/acceptance/Newsletters/SearchForNotificationCest.php
@@ -3,17 +3,26 @@
 namespace MailPoet\Test\Acceptance;
 
 use MailPoet\Test\DataFactories\Newsletter;
+use MailPoet\Test\DataFactories\Segment;
 
 class SearchForNotificationCest {
   public function searchForStandardNotification(\AcceptanceTester $i) {
-    $i->wantTo('Successfully search for an existing notification');
-    $newsletterTitle = 'Search Test Notification';
+    $i->wantTo('Successfully search for an existing and active notification');
+
+    $newsletterTitle = 'Active Post Notification';
     $failureConditionNewsletter = 'Not Actually Real';
+    $segmentName = 'Fancy List';
+
     // step 1 - Prepare newsletter data
+    $segmentFactory = new Segment();
+    $segment = $segmentFactory->withName($segmentName)->create();
     $newsletterFactory = new Newsletter();
     $newsletterFactory->withSubject($newsletterTitle)
       ->withPostNotificationsType()
+      ->withSegments([$segment])
+      ->withActiveStatus()
       ->create();
+
     // step 2 - Search
     $i->login();
     $i->amOnMailpoetPage('Emails');
@@ -21,7 +30,21 @@ class SearchForNotificationCest {
     $i->waitForListingItemsToLoad();
     $i->searchFor($failureConditionNewsletter);
     $i->waitForText('No emails found.', 15, '[data-automation-id="newsletters_listing_tabs"]');
+
+    // step 3 - Filter by Active and search for the active post notification
+    $i->click('[data-automation-id="filters_active"]');
+    $i->waitForText('No emails found.', 15, '[data-automation-id="newsletters_listing_tabs"]');
     $i->searchFor($newsletterTitle);
     $i->waitForText($newsletterTitle, 15, '[data-automation-id="newsletters_listing_tabs"]');
+
+    // step 4 - Filter by assigned list and make sure the notification is present
+    $i->selectOption('[data-automation-id="listing_filter_segment"]', $segmentName);
+    $i->waitForElement('[data-automation-id="listing_filter_segment"]');
+    $i->waitForText($newsletterTitle, 15, '[data-automation-id="newsletters_listing_tabs"]');
+
+    // step 5 - Filter by Inactive and search for the active post notification
+    $i->click('[data-automation-id="filters_not_active"]');
+    $i->searchFor($newsletterTitle);
+    $i->waitForText('No emails found.', 15, '[data-automation-id="newsletters_listing_tabs"]');
   }
 }
--- mailpoet/tests/acceptance/Newsletters/SearchForStandardNewsletterCest.php
+++ mailpoet/tests/acceptance/Newsletters/SearchForStandardNewsletterCest.php
@@ -3,25 +3,40 @@
 namespace MailPoet\Test\Acceptance;
 
 use MailPoet\Test\DataFactories\Newsletter;
+use MailPoet\Test\DataFactories\Segment;
 
 class SearchForStandardNewsletterCest {
   public function searchForStandardNewsletter(\AcceptanceTester $i) {
-    $i->wantTo('Successfully search for an existing newsletter');
+    $i->wantTo('Successfully search for an existing and sent newsletter');
 
-    $newsletterTitle = 'Search Test Newsletter';
+    $newsletterTitle = 'Sent Newsletter';
     $failureConditionNewsletter = 'Not Actually Real';
-
+    $segmentName = 'Fancy List';
 
     // step 1 - Prepare newsletter data
+    $segmentFactory = new Segment();
+    $segment = $segmentFactory->withName($segmentName)->create();
     $newsletterFactory = new Newsletter();
-    $newsletterFactory->withSubject($newsletterTitle)->create();
+    $newsletterFactory->withSubject($newsletterTitle)
+      ->withSentStatus()
+      ->withSegments([$segment])
+      ->create();
 
     // step 2 - Search
     $i->login();
     $i->amOnMailpoetPage('Emails');
     $i->searchFor($failureConditionNewsletter);
     $i->waitForText('No emails found.', 15, '[data-automation-id="newsletters_listing_tabs"]');
+
+    // step 3 - Filter by Sent and search for the sent newsletter
+    $i->click('[data-automation-id="filters_sent"]');
+    $i->waitForText('No emails found.', 15, '[data-automation-id="newsletters_listing_tabs"]');
     $i->searchFor($newsletterTitle);
+    $i->waitForText($newsletterTitle, 15, '[data-automation-id="newsletters_listing_tabs"]');
+
+    // step 4 - Filter by assigned list and make sure the newsletter is present
+    $i->selectOption('[data-automation-id="listing_filter_segment"]', $segmentName);
+    $i->waitForElement('[data-automation-id="listing_filter_segment"]');
     $i->waitForText($newsletterTitle, 15, '[data-automation-id="newsletters_listing_tabs"]');
   }
 }
