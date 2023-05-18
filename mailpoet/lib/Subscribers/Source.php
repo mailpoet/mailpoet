b11c4b731a52e89459fee556b71250e1d6a84613
Rodrigo Primo <rodrigosprimo@gmail.com>
2023-05-18 16:01:29 -0300

    Remove Subscribers\Source::setSource()
    I found this method in the context of a ticket to refactor Paris code to
    Doctrine. As far as I can tell, it is not used anymore and it is
    safe to delete it.
    
    It was added in 1af5802 and was used only inside
    Models\Subscriber::subscribe(). Then subscribe() was moved to
    Subscriber\SubscriberActions in 7528f0f. Finally, commit 7db2384
    refactored SubscriberActions removing the only two calls to
    Source::setSource().
    
    The test class SourceTest is removed as well as its only purpose was to
    test Source::setSource().
    
    [MAILPOET-5345]

--- mailpoet/lib/Subscribers/Source.php
+++ mailpoet/lib/Subscribers/Source.php
@@ -2,10 +2,7 @@
 
 namespace MailPoet\Subscribers;
 
-use MailPoet\Models\Subscriber;
-
 class Source {
-
   const FORM = 'form';
   const IMPORTED = 'imported';
   const ADMINISTRATOR = 'administrator';
@@ -14,27 +11,4 @@ class Source {
   const WOOCOMMERCE_USER = 'woocommerce_user';
   const WOOCOMMERCE_CHECKOUT = 'woocommerce_checkout';
   const UNKNOWN = 'unknown';
-
-  private static $allowedSources = [
-    Source::FORM,
-    Source::IMPORTED,
-    Source::ADMINISTRATOR,
-    Source::API,
-    Source::WORDPRESS_USER,
-    Source::WOOCOMMERCE_USER,
-    Source::WOOCOMMERCE_CHECKOUT,
-    Source::UNKNOWN,
-  ];
-
-  public static function setSource(Subscriber $subscriber, $source) {
-    if ((isset($subscriber->source)) && ($subscriber->source !== Source::UNKNOWN)) {
-      // we don't want to override source
-      return $subscriber;
-    }
-    if (!in_array($source, Source::$allowedSources)) {
-      throw new \InvalidArgumentException('Invalid source "' . $source . '""');
-    }
-    $subscriber->set('source', $source);
-    return $subscriber;
-  }
 }
