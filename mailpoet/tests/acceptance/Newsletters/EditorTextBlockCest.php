a40e0e9937d97b137dead2c451961f8289235989
Veljko <veljano@yahoo.com>
2023-05-25 11:48:45 +0200

    Update test and add new newsletter json
    [MAILPOET-5309]

--- mailpoet/tests/acceptance/Newsletters/EditorTextBlockCest.php
+++ mailpoet/tests/acceptance/Newsletters/EditorTextBlockCest.php
@@ -11,29 +11,57 @@ class EditorTextBlockCest {
 
   public function addText(\AcceptanceTester $i) {
     $i->wantTo('add Text block to newsletter');
-    $textInEditor = ('[data-automation-id="text_block_in_editor"]');
+
     $newsletter = (new Newsletter())
       ->withSubject('Text Block Newsletter')
       ->loadBodyFrom('newsletterWithText.json')
       ->create();
+
     $i->login();
+
     $i->amEditingNewsletter($newsletter->getId());
     $i->dragAndDrop('#automation_editor_block_text', '#mce_1');
     $i->waitForText('Edit this to insert text.');
+
+    $i->wantTo('add Text block to empty column');
+    $i->dragAndDrop('#automation_editor_block_text', '.mailpoet_container_empty');
+    $i->waitForText('Edit this to insert text.');
+    $i->seeNumberOfElements('[data-automation-id="text_block_in_editor"]', 3);
   }
 
   public function toolbarIsClosing(\AcceptanceTester $i) {
     $i->wantTo('Automatically close TinyMCE toolbar when clicked outside textarea');
-    $textInEditor = ('[data-automation-id="text_block_in_editor"]');
+
     $newsletter = (new Newsletter())
       ->withSubject('TinyMCE toolbar Newsletter')
       ->loadBodyFrom('newsletterThreeCols.json')
       ->create();
+
     $i->login();
+
     $i->amEditingNewsletter($newsletter->getId());
     $i->click(self::TEXT_BLOCK_SELECTOR);
     $i->waitForElementVisible(self::TINYMCE_SELECTOR);
     $i->click(self::CONTAINER_SELECTOR);
     $i->waitForElementNotVisible(self::TINYMCE_SELECTOR);
+  }
+
+  public function verifyText(\AcceptanceTester $i) {
+    $i->wantTo('Verify the content alignment and color inside the text block');
+
+    $textInEditor = ('[data-automation-id="text_block_in_editor"]');
+    $newsletter = (new Newsletter())
+      ->withSubject('Text Block Newsletter')
+      ->loadBodyFrom('newsletterWithTextBlock.json')
+      ->create();
+
+    $i->login();
+
+    $i->amEditingNewsletter($newsletter->getId());
+    $i->assertAttributeContains($textInEditor . ' h1', 'style', 'left');
+    $i->assertAttributeContains($textInEditor . ' h2', 'style', 'center');
+    $i->assertAttributeContains($textInEditor . ' h3', 'style', 'right');
+    $i->assertAttributeContains($textInEditor . ' h2 > strong > span', 'style', 'rgb(253, 0, 0)');
+    $i->seeElement($textInEditor . ' > h3 > em');
   }
 }
