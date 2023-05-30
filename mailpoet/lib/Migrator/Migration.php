e38fd951d1ea2ee0cb2221d225bb0259e89df01b
alex-mailpoet <alex-mailpoet@users.noreply.github.com>
2023-05-30 20:14:43 +0300

    Check indexes exist when adding them
    [MAILPOET-5364]

--- mailpoet/lib/Migrator/Migration.php
+++ mailpoet/lib/Migrator/Migration.php
@@ -53,4 +53,16 @@ abstract class Migration {
       AND column_name = ?
     ", [Env::$dbName, $tableName, $columnName])->fetchOne() !== false;
   }
+
+  protected function indexExists(string $tableName, string $indexName): bool {
+    // We had a problem with the dbName value in ENV for some customers, because it doesn't match DB name in information schema.
+    // So we decided to use the DATABASE() value instead.
+    return $this->connection->executeQuery("
+      SELECT 1
+      FROM information_schema.statistics
+      WHERE table_schema = COALESCE(DATABASE(), ?)
+      AND table_name = ?
+      AND index_name = ?
+    ", [Env::$dbName, $tableName, $indexName])->fetchOne() !== false;
+  }
 }
