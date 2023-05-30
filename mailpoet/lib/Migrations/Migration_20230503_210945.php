2af9830d4b1fae5b5b7bbf884fe0d48a6c32d749
alex-mailpoet <alex-mailpoet@users.noreply.github.com>
2023-05-30 20:14:43 +0300

    Check indexes exist when adding them
    [MAILPOET-5364]

--- mailpoet/lib/Migrations/Migration_20230503_210945.php
+++ mailpoet/lib/Migrations/Migration_20230503_210945.php
@@ -8,10 +8,17 @@ use MailPoet\Migrator\Migration;
 class Migration_20230503_210945 extends Migration {
   public function run(): void {
     $subscribersTable = $this->getTableName(SubscriberEntity::class);
-    $this->connection->executeQuery(
-      "ALTER TABLE `{$subscribersTable}`
-        ADD INDEX `first_name` (`first_name`(10)),
-        ADD INDEX `last_name` (`last_name`(10))"
-    );
+    if (!$this->indexExists($subscribersTable, 'first_name')) {
+      $this->connection->executeQuery(
+        "ALTER TABLE `{$subscribersTable}`
+          ADD INDEX `first_name` (`first_name`(10))"
+      );
+    }
+    if (!$this->indexExists($subscribersTable, 'last_name')) {
+      $this->connection->executeQuery(
+        "ALTER TABLE `{$subscribersTable}`
+          ADD INDEX `last_name` (`last_name`(10))"
+      );
+    }
   }
 }
