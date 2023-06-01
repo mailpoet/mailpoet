367d0331e9c9aa61dfaa47731d6f8e3a14fa127e
Jan Jakes <jan.jakes@automattic.com>
2023-06-01 15:28:42 +0200

    Fix loading external styles

--- mailpoet/tasks/npm_post_install.sh
+++ mailpoet/tasks/npm_post_install.sh
@@ -7,3 +7,6 @@ echo "import tinymce from 'tinymce/tinymce';
 " >> $INIT_CWD/assets/js/src/newsletter_editor/behaviors/tinymce_icons.js
 
 cat $INIT_CWD/node_modules/tinymce/icons/default/icons.min.js >> $INIT_CWD/assets/js/src/newsletter_editor/behaviors/tinymce_icons.js
+
+# fix SCSS file being exposed as CSS file so it can be imported correctly
+cp $INIT_CWD/node_modules/react-dates/lib/css/_datepicker.css $INIT_CWD/node_modules/react-dates/lib/css/_datepicker.scss
