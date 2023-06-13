6b559d33773810f43a9d501903eadb7e2727ab98
Jan Jakes <jan.jakes@automattic.com>
2023-06-13 11:07:47 +0200

    Fix passing empty callback
    [MAILPOET-5383]

--- mailpoet/lib/Config/Menu.php
+++ mailpoet/lib/Config/Menu.php
@@ -174,7 +174,7 @@ class Menu {
       'MailPoet',
       AccessControl::PERMISSION_ACCESS_PLUGIN_ADMIN,
       self::MAIN_PAGE_SLUG,
-      null,
+      '',
       self::ICON_BASE64_SVG,
       30
     );
