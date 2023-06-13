f20878f7f36c6767d804546e4d3d13688b9862e2
Rodrigo Primo <rodrigosprimo@gmail.com>
2023-06-13 12:22:17 -0300

    Update the queries of the 'used shipping method' filter
    This commit changes the logic of the code that builds the queries for
    the 'used shipping method' filter. The initial implementation relied
    only on the shipping method name that is stored in
    wp_woocommerce_order_items.  This implementation did not work as the
    name is not unique. The new implementation use the shipping method id
    and the instance id that are stored in wp_woocommerce_order_itemmeta and
    the combination of both fields is unique.
    
    [MAILPOET-4992]

--- mailpoet/tests/integration/Segments/DynamicSegments/Filters/WooCommerceUsedShippingMethodTest.php
+++ mailpoet/tests/integration/Segments/DynamicSegments/Filters/WooCommerceUsedShippingMethodTest.php
@@ -23,99 +23,104 @@ class WooCommerceUsedShippingMethodTest extends \MailPoetTest {
     $customerId1 = $this->tester->createCustomer('c1@e.com');
     $customerId2 = $this->tester->createCustomer('c2@e.com');
     $customerId3 = $this->tester->createCustomer('c3@e.com');
+    $customerId4 = $this->tester->createCustomer('c4@e.com');
 
-    $this->createOrder($customerId1, Carbon::now(), 'Flat rate');
-    $this->createOrder($customerId2, Carbon::now(), 'Local pickup');
-    $this->createOrder($customerId3, Carbon::now(), 'Flat rate');
-    $this->createOrder($customerId3, Carbon::now(), 'Free shipping');
+    $this->createOrder($customerId1, Carbon::now(), 'flat_rate', 1);
+    $this->createOrder($customerId2, Carbon::now(), 'local_pickup', 2);
+    $this->createOrder($customerId2, Carbon::now(), 'flat_rate', 4);
+    $this->createOrder($customerId3, Carbon::now(), 'flat_rate', 1);
+    $this->createOrder($customerId3, Carbon::now(), 'free_shipping', 3);
+    $this->createOrder($customerId4, Carbon::now(), 'flat_rate', 4);
 
-    $this->assertFilterReturnsEmails('any', ['Flat rate'], 1, ['c1@e.com', 'c3@e.com']);
-    $this->assertFilterReturnsEmails('any', ['Local pickup'], 1, ['c2@e.com']);
-    $this->assertFilterReturnsEmails('any', ['Nonexistent method'], 1000, []);
+    $this->assertFilterReturnsEmails('any', ['flat_rate:1'], 1, ['c1@e.com', 'c3@e.com']);
+    $this->assertFilterReturnsEmails('any', ['local_pickup:2'], 1, ['c2@e.com']);
+    $this->assertFilterReturnsEmails('any', ['local_pickup:2', 'flat_rate:1'], 1, ['c1@e.com', 'c2@e.com', 'c3@e.com']);
+    $this->assertFilterReturnsEmails('any', ['nonexistent_method:1'], 1000, []);
   }
 
   public function testItWorksWithAllOperator(): void {
     $customerId1 = $this->tester->createCustomer('c1@e.com');
-    $this->createOrder($customerId1, Carbon::now(), 'Flat rate');
-    $this->createOrder($customerId1, Carbon::now(), 'Flat rate');
+    $this->createOrder($customerId1, Carbon::now(), 'flat_rate', 1);
+    $this->createOrder($customerId1, Carbon::now(), 'flat_rate', 1);
 
     $customerId2 = $this->tester->createCustomer('c2@e.com');
-    $this->createOrder($customerId2, Carbon::now(), 'Free shipping');
+    $this->createOrder($customerId2, Carbon::now(), 'free_shipping', 2);
 
     $customerId3 = $this->tester->createCustomer('c3@e.com');
-    $this->createOrder($customerId3, Carbon::now(), 'Free shipping');
-    $this->createOrder($customerId3, Carbon::now(), 'Flat rate');
+    $this->createOrder($customerId3, Carbon::now(), 'free_shipping', 2);
+    $this->createOrder($customerId3, Carbon::now(), 'flat_rate', 1);
 
-    $this->assertfilterreturnsemails('all', ['Flat rate'], 1, ['c1@e.com', 'c3@e.com']);
-    $this->assertFilterReturnsEmails('all', ['Free shipping'], 1, ['c2@e.com', 'c3@e.com']);
-    $this->assertFilterReturnsEmails('all', ['Free shipping', 'Flat rate'], 1, ['c3@e.com']);
-    $this->assertFilterReturnsEmails('all', ['Nonexistent method', 'Flat rate'], 1000, []);
+    $this->assertfilterreturnsemails('all', ['flat_rate:1'], 1, ['c1@e.com', 'c3@e.com']);
+    $this->assertFilterReturnsEmails('all', ['free_shipping:2'], 1, ['c2@e.com', 'c3@e.com']);
+    $this->assertFilterReturnsEmails('all', ['free_shipping:2', 'flat_rate:1'], 1, ['c3@e.com']);
+    $this->assertFilterReturnsEmails('all', ['nonexistent_method:1', 'flat_rate:1'], 1000, []);
   }
 
   public function testItWorksWithNoneOperator(): void {
     $customerId1 = $this->tester->createCustomer('c1@e.com');
-    $this->createOrder($customerId1, Carbon::now(), 'Flat rate');
-    $this->createOrder($customerId1, Carbon::now(), 'Flat rate');
+    $this->createOrder($customerId1, Carbon::now(), 'flat_rate', 1);
+    $this->createOrder($customerId1, Carbon::now(), 'flat_rate', 1);
 
     $customerId2 = $this->tester->createCustomer('c2@e.com');
-    $this->createOrder($customerId2, Carbon::now(), 'Free shipping');
+    $this->createOrder($customerId2, Carbon::now(), 'free_shipping', 2);
 
     $customerId3 = $this->tester->createCustomer('c3@e.com');
-    $this->createOrder($customerId3, Carbon::now(), 'Free shipping');
-    $this->createOrder($customerId3, Carbon::now(), 'Flat rate');
+    $this->createOrder($customerId3, Carbon::now(), 'free_shipping', 2);
+    $this->createOrder($customerId3, Carbon::now(), 'flat_rate', 1);
 
     (new Subscriber)->withEmail('sub@e.com')->create();
 
-    $this->assertFilterReturnsEmails('none', ['Flat rate'], 1, ['sub@e.com', 'c2@e.com']);
-    $this->assertFilterReturnsEmails('none', ['Free shipping'], 1, ['sub@e.com', 'c1@e.com']);
-    $this->assertFilterReturnsEmails('none', ['Nonexistent method'], 1000, ['sub@e.com', 'c1@e.com', 'c2@e.com', 'c3@e.com']);
-    $this->assertFilterReturnsEmails('none', ['Flat rate', 'Free shipping'], 1, ['sub@e.com']);
+    $this->assertFilterReturnsEmails('none', ['flat_rate:1'], 1, ['sub@e.com', 'c2@e.com']);
+    $this->assertFilterReturnsEmails('none', ['free_shipping:2'], 1, ['sub@e.com', 'c1@e.com']);
+    $this->assertFilterReturnsEmails('none', ['nonexistent_method:1'], 1000, ['sub@e.com', 'c1@e.com', 'c2@e.com', 'c3@e.com']);
+    $this->assertFilterReturnsEmails('none', ['flat_rate:1', 'free_shipping:2'], 1, ['sub@e.com']);
   }
 
   public function testItWorksWithDateRanges(): void {
     $customerId1 = $this->tester->createCustomer('c1@e.com');
-    $this->createOrder($customerId1, Carbon::now()->subDays(2)->addMinute(), 'Flat rate');
-    $this->createOrder($customerId1, Carbon::now()->subDays(5)->addMinute(), 'Free shipping');
+    $this->createOrder($customerId1, Carbon::now()->subDays(2)->addMinute(), 'flat_rate', 1);
+    $this->createOrder($customerId1, Carbon::now()->subDays(5)->addMinute(), 'free_shipping', 2);
 
     $customerId2 = $this->tester->createCustomer('c2@e.com');
-    $this->createOrder($customerId2, Carbon::now()->subDays(100)->addMinute(), 'Local pickup');
-    $this->assertFilterReturnsEmails('any', ['Flat rate'], 1, []);
-    $this->assertFilterReturnsEmails('any', ['Flat rate'], 2, ['c1@e.com']);
-    $this->assertFilterReturnsEmails('any', ['Free shipping'], 4, []);
-    $this->assertFilterReturnsEmails('any', ['Free shipping'], 5, ['c1@e.com']);
-    $this->assertFilterReturnsEmails('any', ['Local pickup'], 99, []);
-    $this->assertFilterReturnsEmails('any', ['Local pickup'], 100, ['c2@e.com']);
-    $this->assertFilterReturnsEmails('any', ['Local pickup', 'Flat rate'], 100, ['c1@e.com', 'c2@e.com']);
+    $this->createOrder($customerId2, Carbon::now()->subDays(100)->addMinute(), 'local_pickup', 3);
+    $this->assertFilterReturnsEmails('any', ['flat_rate:1'], 1, []);
+    $this->assertFilterReturnsEmails('any', ['flat_rate:1'], 2, ['c1@e.com']);
+    $this->assertFilterReturnsEmails('any', ['free_shipping:2'], 4, []);
+    $this->assertFilterReturnsEmails('any', ['free_shipping:2'], 5, ['c1@e.com']);
+    $this->assertFilterReturnsEmails('any', ['local_pickup:3'], 99, []);
+    $this->assertFilterReturnsEmails('any', ['local_pickup:3'], 100, ['c2@e.com']);
+    $this->assertFilterReturnsEmails('any', ['local_pickup:3', 'flat_rate:1'], 100, ['c1@e.com', 'c2@e.com']);
 
-    $this->assertFilterReturnsEmails('all', ['Flat rate'], 1, []);
-    $this->assertFilterReturnsEmails('all', ['Flat rate'], 2, ['c1@e.com']);
-    $this->assertFilterReturnsEmails('all', ['Flat rate', 'Free shipping'], 2, []);
-    $this->assertFilterReturnsEmails('all', ['Flat rate', 'Free shipping'], 5, ['c1@e.com']);
+    $this->assertFilterReturnsEmails('all', ['flat_rate:1'], 1, []);
+    $this->assertFilterReturnsEmails('all', ['flat_rate:1'], 2, ['c1@e.com']);
+    $this->assertFilterReturnsEmails('all', ['flat_rate:1', 'free_shipping:2'], 2, []);
+    $this->assertFilterReturnsEmails('all', ['flat_rate:1', 'free_shipping:2'], 5, ['c1@e.com']);
 
-    $this->assertFilterReturnsEmails('none', ['Flat rate'], 1, ['c1@e.com', 'c2@e.com']);
-    $this->assertFilterReturnsEmails('none', ['Flat rate'], 2, ['c2@e.com']);
-    $this->assertFilterReturnsEmails('none', ['Free shipping'], 2, ['c1@e.com', 'c2@e.com']);
-    $this->assertFilterReturnsEmails('none', ['Free shipping'], 5, ['c2@e.com']);
+    $this->assertFilterReturnsEmails('none', ['flat_rate:1'], 1, ['c1@e.com', 'c2@e.com']);
+    $this->assertFilterReturnsEmails('none', ['flat_rate:1'], 2, ['c2@e.com']);
+    $this->assertFilterReturnsEmails('none', ['free_shipping:2'], 2, ['c1@e.com', 'c2@e.com']);
+    $this->assertFilterReturnsEmails('none', ['free_shipping:2'], 5, ['c2@e.com']);
   }
 
-  private function assertFilterReturnsEmails(string $operator, array $shippingMethods, int $days, array $expectedEmails): void {
+  private function assertFilterReturnsEmails(string $operator, array $shippingMethodStrings, int $days, array $expectedEmails): void {
     $filterData = new DynamicSegmentFilterData(DynamicSegmentFilterData::TYPE_WOOCOMMERCE, WooCommerceUsedShippingMethod::ACTION, [
       'operator' => $operator,
-      'shipping_methods' => $shippingMethods,
+      'shipping_methods' => $shippingMethodStrings,
       'used_shipping_method_days' => $days,
     ]);
     $emails = $this->tester->getSubscriberEmailsMatchingDynamicFilter($filterData, $this->filter);
     $this->assertEqualsCanonicalizing($expectedEmails, $emails);
   }
 
-  private function createOrder(int $customerId, Carbon $createdAt, string $shippingMethodTitle): int {
+  private function createOrder(int $customerId, Carbon $createdAt, string $shippingMethodId, $shippingInstanceId): int {
     $order = $this->tester->createWooCommerceOrder();
     $order->set_customer_id($customerId);
     $order->set_date_created($createdAt->toDateTimeString());
     $order->set_status('wc-completed');
 
     $shippingItem = new \WC_Order_Item_Shipping();
-    $shippingItem->set_method_title($shippingMethodTitle);
+    $shippingItem->set_method_id($shippingMethodId);
+    $shippingItem->set_instance_id($shippingInstanceId);
     $order->add_item($shippingItem);
 
     $order->save();
