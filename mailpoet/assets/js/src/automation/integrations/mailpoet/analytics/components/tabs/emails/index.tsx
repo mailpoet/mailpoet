57d924e2be598e7145980ad9e4d4f3a968517f8c
David Remer <webmaster@websupporter.net>
2023-06-14 09:39:31 +0300

    Add Email Tab
    [MAILPOET-5092]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/emails/index.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/emails/index.tsx
@@ -1,3 +1,98 @@
+import {TableCard} from "@woocommerce/components/build";
+import {useSelect} from "@wordpress/data";
+import {EmailStats, OverviewSection, storeName} from "../../../store";
+import {__} from "@wordpress/i18n";
+import {useEffect, useState} from "react";
+import {calculateSummary} from "./summary";
+import {transformEmailsToRows} from "./rows";
+
+const headers = [
+  {
+    key: 'email',
+    label: __('Email', 'mailpoet'),
+  },
+  {
+    key: 'sent',
+    label: __('Sent', 'mailpoet'),
+    isLeftAligned:false,
+    isNumeric: true
+  },
+  {
+    key: 'opened',
+    label: __('Opened', 'mailpoet'),
+    isLeftAligned:false,
+    isNumeric: true
+  },
+  {
+    key: 'clicked',
+    label: __('Clicked', 'mailpoet'),
+    isLeftAligned:false,
+    isNumeric: true
+  },
+  {
+    key: 'orders',
+    label: __('Orders', 'mailpoet'),
+    isLeftAligned:false,
+    isNumeric: true
+  },
+  {
+    key: 'revenue',
+    label: __('Revenue', 'mailpoet'),
+    isLeftAligned:false,
+    isNumeric: true
+  },
+  {
+    key: 'unsubscribed',
+    label: __('Unsubscribed', 'mailpoet'),
+    isLeftAligned:false,
+    isNumeric: true
+  },
+  {
+    key: 'actions',
+    label: ''
+  },
+];
+
 export function Emails(): JSX.Element {
-  return <p>Emails</p>;
+  const { overview } = useSelect((s) => ({
+    overview: s(storeName).getSection('overview'),
+  })) as { overview: OverviewSection };
+
+  const [visibleEmails, setVisibleEmails] = useState<EmailStats[] | undefined>(undefined);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage]  = useState(5);
+  //const [rowsPerPage, setRowsPerPage]  = useState(25);
+  useEffect(
+    () => {
+      setVisibleEmails(overview.data !== undefined ? Object.values(overview.data.emails).splice((currentPage-1)*rowsPerPage, rowsPerPage): undefined)
+    }, [overview.data]
+  )
+
+  const rows = visibleEmails !== undefined ? transformEmailsToRows(visibleEmails) : [];
+
+  const summary = calculateSummary(visibleEmails??[]);
+  return <TableCard
+    title=""
+    caption=""
+    onQueryChange={ (type:string) => (param) => {
+      if (type === 'paged') {
+        setCurrentPage(param);
+        setVisibleEmails(overview.data !== undefined ? Object.values(overview.data.emails).splice((param-1)*rowsPerPage, rowsPerPage): undefined)
+      } else if(type==='per_page') {
+        setCurrentPage(1);
+        setRowsPerPage(param);
+        setVisibleEmails(overview.data !== undefined ? Object.values(overview.data.emails).splice(0, param): undefined)
+      }
+    }
+    }
+    query={ {paged: currentPage, sort: {key: 'email', direction: 'asc'}} }
+    rows={ rows }
+    headers={ headers }
+    showMenu={ false }
+    rowsPerPage={ rowsPerPage }
+    onRowClick={ () => {} }
+    totalRows={ overview.data !== undefined ? Object.values(overview.data.emails).length : 0 }
+    summary={ summary }
+    isLoading={ overview.data === undefined }
+  />
 }
