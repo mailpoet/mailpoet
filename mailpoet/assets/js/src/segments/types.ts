4583b80fa698a786c28e221cadb97da2fc4a0bf3
Rodrigo Primo <rodrigosprimo@gmail.com>
2023-06-15 16:28:28 -0300

    Move SegmentResponse type to types.ts to be able to reuse it
    [MAILPOET-5407]

--- mailpoet/assets/js/src/segments/types.ts
+++ mailpoet/assets/js/src/segments/types.ts
@@ -1,1 +1,13 @@
 export * from './dynamic/types';
+
+export type SegmentResponse = {
+  meta: {
+    count: string;
+  };
+  data: {
+    name: string;
+  };
+  errors: {
+    message: string;
+  }[];
+};
