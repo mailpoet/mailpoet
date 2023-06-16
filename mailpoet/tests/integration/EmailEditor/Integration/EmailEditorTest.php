040ed9a98f14bb6cde105934e54ab031e95f85c3
Rostislav Wolny <rosta.wolny@automattic.com>
2023-06-16 16:03:14 +0200

    Save association to WP post with newsletter
    This commit adds a simple implementation of ensuring the mailpoet_email post type
    has associated NewsletterEntity.
    In the future we will need to add support also for saving additional data for
    newsletters (subject, sending settings).
    [MAILPOET-5365]

---
+++ mailpoet/tests/integration/EmailEditor/Integration/EmailEditorTest.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\EmailEditor\Integration;
+
+use MailPoet\Entities\NewsletterEntity;
+use MailPoet\Features\FeatureFlagsController;
+use MailPoet\Features\FeaturesController;
+use MailPoet\Newsletter\NewslettersRepository;
+use MailPoet\WP\Functions as WPFunctions;
+
+class EmailEditorTest extends \MailPoetTest {
+  /** @var EmailEditor */
+  private $emailEditor;
+
+  /** @var FeatureFlagsController */
+  private $featureFlagsController;
+
+  /** @var NewslettersRepository */
+  private $newslettersRepository;
+
+  public function _before() {
+    $this->emailEditor = $this->diContainer->get(EmailEditor::class);
+    $this->newslettersRepository = $this->diContainer->get(NewslettersRepository::class);
+    $this->featureFlagsController = $this->diContainer->get(FeatureFlagsController::class);
+    $this->featureFlagsController->set(FeaturesController::GUTENBERG_EMAIL_EDITOR, true);
+  }
+
+  public function testItRegistersMailPoetEmailPostType() {
+    $this->emailEditor->initialize();
+    $postTypes = get_post_types();
+    $this->assertArrayHasKey('mailpoet_email', $postTypes);
+  }
+
+  public function testItCreatesAssociatedNewsletterEntity() {
+    $this->emailEditor->initialize();
+    $newsletters = $this->newslettersRepository->findAll();
+    expect(count($newsletters))->equals(0);
+    $wp = $this->diContainer->get(WPFunctions::class);
+    // Add email post
+    $postId = $wp->wpInsertPost(['post_type' => 'mailpoet_email']);
+    $newsletters = $this->newslettersRepository->findAll();
+    expect(count($newsletters))->equals(1);
+    expect($newsletters[0]->getWpPostId())->greaterThan(0);
+    expect($newsletters[0]->getWpPostId())->equals($postId);
+    // Add non-email standard post
+    $wp->wpInsertPost(['post_type' => 'post']);
+    $newsletters = $this->newslettersRepository->findAll();
+    // Newsletters count should not change
+    expect(count($newsletters))->equals(1);
+  }
+
+  public function _after() {
+    parent::_after();
+    remove_filter('mailpoet_email_editor_post_types', [$this->emailEditor, 'addEmailPostType']);
+    remove_filter('save_post', [$this->emailEditor, 'onEmailSave']);
+    $this->truncateEntity(NewsletterEntity::class);
+    $this->featureFlagsController->set(FeaturesController::GUTENBERG_EMAIL_EDITOR, false);
+  }
+}
