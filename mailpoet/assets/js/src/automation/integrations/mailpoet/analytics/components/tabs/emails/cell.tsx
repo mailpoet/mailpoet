da62c3da2157bd72a15d0696476bab02bfcb1248
David Remer <webmaster@websupporter.net>
2023-06-16 14:54:23 +0300

    Add interactive elements to email tab
    [MAILPOET-5092]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/emails/cell.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/emails/cell.tsx
@@ -1,23 +1,17 @@
 type CellProps = {
-  value: number | string;
+  value: number | string | JSX.Element
   subValue?: number | string;
-  link?: string;
   badge?: string;
   badgeType?: string;
   className?: string;
 }
-export function Cell({value, subValue, link, badge, badgeType, className}: CellProps): JSX.Element {
+export function Cell({value, subValue, badge, badgeType, className}: CellProps): JSX.Element {
 
-  const badgeElement = badge ? <span className={`mailpoet-analytics-badge`}>{badge}</span> : null
-  const mainElement = link === undefined ?
-    <p className={`mailpoet-analytics-main-value ${className??''} ${badgeType??''}`}>
+  const badgeElement = badge ? <span className="mailpoet-analytics-badge">{badge}</span> : null
+  const mainElement = <div className={`mailpoet-analytics-main-value ${className??''} ${badgeType??''}`}>
       {badgeElement}
       {value}
-    </p> :
-    <p className={`mailpoet-analytics-main-value ${badgeType??''}`}>
-      {badgeElement}
-      <a href={link}>{value}</a>
-    </p>
+    </div>
 
 
   return (
