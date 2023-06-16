2203629991d8d1991d66affa81cd77aa56f5e371
David Remer <webmaster@websupporter.net>
2023-06-16 15:11:57 +0300

    Fix small styling issues
    [MAILPOET-5092]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/emails/actions.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/emails/actions.tsx
@@ -1,37 +1,42 @@
-import {__} from "@wordpress/i18n";
-import {DropdownMenu} from "@wordpress/components";
-import {moreVertical, pencil, seen} from "@wordpress/icons";
+import { __ } from '@wordpress/i18n';
+import { DropdownMenu } from '@wordpress/components';
+import { moreVertical, pencil, seen } from '@wordpress/icons';
 
 type ActionsProps = {
-  id:number;
-  previewUrl:string;
-}
-export function Actions({id, previewUrl}:ActionsProps): JSX.Element {
+  id: number;
+  previewUrl: string;
+};
+export function Actions({ id, previewUrl }: ActionsProps): JSX.Element {
   const controls = [
     {
       title: __('Preview email', 'mailpoet'),
       icon: seen,
       onClick: () => {
-        window.location.href = previewUrl
+        window.location.href = previewUrl;
       },
     },
     {
       title: __('Edit email', 'mailpoet'),
       icon: pencil,
       onClick: () => {
-        window.location.href = `?page=mailpoet-newsletter-editor&id=${id}&context=automation`
+        window.location.href = `?page=mailpoet-newsletter-editor&id=${id}&context=automation`;
       },
     },
-  ]
-  return <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-    <p><a href={`admin.php?page=mailpoet-newsletters#/stats/${id}`}>{__('Statistics', 'mailpoet')}</a></p>
+  ];
+  return (
+    <div className="mailpoet-analytics-email-actions">
+      <p>
+        <a href={`admin.php?page=mailpoet-newsletters#/stats/${id}`}>
+          {__('Statistics', 'mailpoet')}
+        </a>
+      </p>
 
-    <DropdownMenu
-      className="mailpoet-automation-listing-more-button"
-      label={__('More', 'mailpoet')}
-      icon={moreVertical}
-      controls={controls}
-      popoverProps={{ position: 'bottom left' }}
-    />
-  </div>
+      <DropdownMenu
+        label={__('More', 'mailpoet')}
+        icon={moreVertical}
+        controls={controls}
+        popoverProps={{ position: 'bottom left' }}
+      />
+    </div>
+  );
 }
