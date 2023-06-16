0e2b8961f39428a92e483e3e32d51d44909d5b42
Jan Jakes <jan.jakes@automattic.com>
2023-06-16 16:13:13 +0200

    Allow making chip component interactive
    [MAILPOET-4586]

--- mailpoet/assets/js/src/automation/editor/components/chip/index.tsx
+++ mailpoet/assets/js/src/automation/editor/components/chip/index.tsx
@@ -1,17 +1,36 @@
-import { ReactNode } from 'react';
+import { MouseEvent, KeyboardEvent, ReactNode } from 'react';
 import classNames from 'classnames';
 
 type Props = {
   variant?: 'default' | 'danger';
   size?: 'small' | 'medium' | 'large';
+  onClick?: (
+    event: MouseEvent<HTMLDivElement> | KeyboardEvent<HTMLDivElement>,
+  ) => void;
   children?: ReactNode;
 };
 
 export function Chip({
   variant = 'default',
   size = 'medium',
+  onClick,
   children,
 }: Props): JSX.Element {
+  const interactivityProps = onClick
+    ? ({
+        role: 'button',
+        tabIndex: 0,
+        onClick,
+        onKeyDown: (event: KeyboardEvent<HTMLDivElement>) => {
+          event.stopPropagation();
+          event.preventDefault();
+          if (['Enter', ' '].includes(event.key) && onClick) {
+            onClick(event);
+          }
+        },
+      } as const)
+    : {};
+
   return (
     <div
       className={classNames({
@@ -22,6 +41,7 @@ export function Chip({
         'chip-default': variant === 'default',
         'chip-danger': variant === 'danger',
       })}
+      {...interactivityProps}
     >
       {children}
     </div>
