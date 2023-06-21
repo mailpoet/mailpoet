a7c8a4c128ff4185621fce6e1058d47eaac2cb7c
Rostislav Wolny <rosta.wolny@automattic.com>
2023-06-21 10:19:10 +0200

    Fetch MailPoet email data in editor and use id for redirect to send page
    [MAILPOET-5365]

--- mailpoet/assets/js/src/email_editor/email_editor.tsx
+++ mailpoet/assets/js/src/email_editor/email_editor.tsx
@@ -1,13 +1,31 @@
 import { registerPlugin } from '@wordpress/plugins';
 import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
+import { store as editorStore } from '@wordpress/editor';
 import { SendButtonSlot } from './components/send_button_slot';
+import { MailPoetEmailData } from './types';
 
 function Editor() {
+  const { mailpoetData } = useSelect((select) => ({
+    mailpoetData:
+      (select(editorStore).getEditedPostAttribute(
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        // The getEditedPostAttribute accepts an attribute but typescript thinks it doesn't
+        'mailpoet_data',
+      ) as MailPoetEmailData) ?? null,
+  }));
+
   return (
     <SendButtonSlot>
-      {/* eslint-disable-next-line no-console */}
-      <Button variant="primary" onClick={() => console.log('Send Clicked')}>
+      <Button
+        variant="primary"
+        disabled={!mailpoetData}
+        onClick={() => {
+          window.location.href = `admin.php?page=mailpoet-newsletters#/send/${mailpoetData.id}`;
+        }}
+      >
         {__('Send', 'mailpoet')}
       </Button>
     </SendButtonSlot>
