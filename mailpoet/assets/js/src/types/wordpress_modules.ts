fea40076e60a0219cedf91045fd2e280703f405e
Rostislav Wolny <rosta.wolny@automattic.com>
2023-06-21 10:19:10 +0200

    Fetch MailPoet email data in editor and use id for redirect to send page
    [MAILPOET-5365]

--- mailpoet/assets/js/src/types/wordpress_modules.ts
+++ mailpoet/assets/js/src/types/wordpress_modules.ts
@@ -15,6 +15,21 @@ declare module '@wordpress/block-editor' {
   }>;
 }
 
+// We need to use code/editor store but types are not available yet
+declare module '@wordpress/editor' {
+  import * as editorActions from '@wordpress/editor/store/actions';
+  import * as editorSelectors from '@wordpress/editor/store/selectors';
+  import { StoreDescriptor as GenericStoreDescriptor } from '@wordpress/data/build-types/types';
+
+  export * from '@wordpress/editor/index';
+
+  export const store: { name: 'core/editor' } & GenericStoreDescriptor<{
+    reducer: () => unknown;
+    actions: typeof editorActions;
+    selectors: typeof editorSelectors;
+  }>;
+}
+
 // there are no @types/wordpress__interface yet
 declare module '@wordpress/interface' {
   import { StoreDescriptor } from '@wordpress/data/build-types/types';
