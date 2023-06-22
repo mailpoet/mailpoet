55fc75e06eced1a77bc4c18b813139583953b764
David Remer <webmaster@websupporter.net>
2023-06-22 15:39:12 +0300

    Use one formatter
    [MAILPOET-5092]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/emails/summary.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/emails/summary.tsx
@@ -27,37 +27,30 @@ export function calculateSummary(rows: EmailStats[]) {
     },
   );
 
+  const compactFormatter = Intl.NumberFormat(locale.toString(), {
+    notation: 'compact',
+  });
   const summary = [
     {
       label: __('sent', 'mailpoet'),
-      value: Intl.NumberFormat(locale.toString(), {
-        notation: 'compact',
-      }).format(data.sent),
+      value: compactFormatter.format(data.sent),
     },
     {
       label: __('opened', 'mailpoet'),
-      value: Intl.NumberFormat(locale.toString(), {
-        notation: 'compact',
-      }).format(data.opened),
+      value: compactFormatter.format(data.opened),
     },
     {
       label: __('clicked', 'mailpoet'),
-      value: Intl.NumberFormat(locale.toString(), {
-        notation: 'compact',
-      }).format(data.clicked),
+      value: compactFormatter.format(data.clicked),
     },
     {
       label: __('orders', 'mailpoet'),
-      value: Intl.NumberFormat(locale.toString(), {
-        notation: 'compact',
-      }).format(data.orders),
+      value: compactFormatter.format(data.orders),
     },
     { label: __('revenue', 'mailpoet'), value: formattedPrice(data.revenue) },
     {
       label: __('unsubscribed', 'mailpoet'),
-      value: Intl.NumberFormat(locale.toString(), {
-        notation: 'compact',
-      }).format(data.unsubscribed),
+      value: compactFormatter.format(data.unsubscribed),
     },
   ];
 
