cc3112cc8add914a0328b1a61a6859c12d612b61
Jan Jakes <jan.jakes@automattic.com>
2023-06-23 10:12:27 +0200

    Collect filter validation errors
    [MAILPOET-5418]

--- mailpoet/lib/Automation/Engine/Validation/AutomationRules/ValidStepFiltersRule.php
+++ mailpoet/lib/Automation/Engine/Validation/AutomationRules/ValidStepFiltersRule.php
@@ -6,6 +6,7 @@ use MailPoet\Automation\Engine\Data\Automation;
 use MailPoet\Automation\Engine\Registry;
 use MailPoet\Automation\Engine\Validation\AutomationGraph\AutomationNode;
 use MailPoet\Automation\Engine\Validation\AutomationGraph\AutomationNodeVisitor;
+use MailPoet\Validator\ValidationException;
 use MailPoet\Validator\Validator;
 
 class ValidStepFiltersRule implements AutomationNodeVisitor {
@@ -29,14 +30,27 @@ class ValidStepFiltersRule implements AutomationNodeVisitor {
   public function visitNode(Automation $automation, AutomationNode $node): void {
     $filters = $node->getStep()->getFilters();
     $groups = $filters ? $filters->getGroups() : [];
+    $errors = [];
     foreach ($groups as $group) {
       foreach ($group->getFilters() as $filter) {
         $registryFilter = $this->registry->getFilter($filter->getFieldType());
         if (!$registryFilter) {
           continue;
         }
-        $this->validator->validate($registryFilter->getArgsSchema($filter->getCondition()), $filter->getArgs());
+        try {
+          $this->validator->validate($registryFilter->getArgsSchema($filter->getCondition()), $filter->getArgs());
+        } catch (ValidationException $e) {
+          $errors[$filter->getId()] = $e->getWpError()->get_error_code();
+        }
       }
+    }
+
+    if ($errors) {
+      $throwable = ValidationException::create()->withMessage('invalid-automation-filters');
+      foreach ($errors as $errorKey => $errorMsg) {
+        $throwable->withError((string)$errorKey, (string)$errorMsg);
+      }
+      throw $throwable;
     }
   }
 
