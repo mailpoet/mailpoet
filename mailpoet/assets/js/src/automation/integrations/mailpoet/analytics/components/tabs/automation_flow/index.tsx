5e4631ec9d3394f31f9577d65dc9f8f3f1fc238d
David Remer <webmaster@websupporter.net>
2023-06-28 09:53:47 +0300

    Add AutomationPlaceholder
    [MAILPOET-5091]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/automation_flow/index.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/automation_flow/index.tsx
@@ -1,7 +1,10 @@
+import { useSelect } from '@wordpress/data';
 import { Hooks } from 'wp-js-hooks';
 import { Automation } from '../../../../../../editor/components/automation';
 import { StatisticSeparator } from './statistic_separator';
 import { Step as StepData } from '../../../../../../editor/components/automation/types';
+import { storeName } from '../../../store';
+import { AutomationPlaceholder } from './automation_placeholder';
 
 Hooks.addFilter(
   'mailpoet.automation.render_step_separator',
@@ -14,5 +17,14 @@ Hooks.addFilter(
 );
 
 export function AutomationFlow(): JSX.Element {
-  return <Automation context="view" />;
+  const { section } = useSelect(
+    (s) => ({
+      section: s(storeName).getSection('automation_flow'),
+    }),
+    [],
+  );
+
+  const isLoading = section.data === undefined;
+
+  return !isLoading ? <Automation context="view" /> : <AutomationPlaceholder />;
 }
