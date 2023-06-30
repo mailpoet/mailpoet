11acdf4181488a8c72d0de2a44586aae18b5d327
David Remer <webmaster@websupporter.net>
2023-06-30 10:09:02 +0300

    Move hooks
    [MAILPOET-5091]

---
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/automation_flow/hooks/index.tsx
@@ -0,0 +1,48 @@
+import { Hooks } from 'wp-js-hooks';
+import { Step as StepData } from '../../../../../../../editor/components/automation/types';
+import { StepFooter } from '../step_footer';
+import { SendEmailPanel } from '../steps/send_email';
+import { StatisticSeparator } from '../statistic_separator';
+
+export function initHooks() {
+  Hooks.addFilter(
+    'mailpoet.automation.step.footer',
+    'mailpoet',
+    (element: JSX.Element | null, step: StepData, context: string) => {
+      if (context !== 'view') {
+        return element;
+      }
+      return <StepFooter step={step} />;
+    },
+  );
+
+  Hooks.addFilter(
+    'mailpoet.automation.step.more',
+    'mailpoet',
+    (element: JSX.Element | null, step: StepData, context: string) => {
+      if (context !== 'view') {
+        return element;
+      }
+
+      if (step.key === 'mailpoet:send-email') {
+        return <SendEmailPanel step={step} />;
+      }
+
+      return element;
+    },
+  );
+
+  Hooks.addFilter(
+    'mailpoet.automation.render_step_separator',
+    'mailpoet',
+    (filterValue: () => JSX.Element, context) => {
+      if (context !== 'view') {
+        return filterValue;
+      }
+      return function statisticSeperatorWrapper(previousStepData: StepData) {
+        return <StatisticSeparator previousStepId={previousStepData.id} />;
+      };
+    },
+    20,
+  );
+}
--- mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/automation_flow/index.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/automation_flow/index.tsx
@@ -1,55 +1,12 @@
 import { Notice } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
-import { Hooks } from 'wp-js-hooks';
 import { Automation } from '../../../../../../editor/components/automation';
-import { StatisticSeparator } from './statistic_separator';
-import { Step as StepData } from '../../../../../../editor/components/automation/types';
 import { storeName } from '../../../store';
 import { AutomationPlaceholder } from './automation_placeholder';
-import { StepFooter } from './step_footer';
-import { SendEmailPanel } from './steps/send_email';
+import { initHooks } from './hooks';
 
-Hooks.addFilter(
-  'mailpoet.automation.step.footer',
-  'mailpoet',
-  (element: JSX.Element | null, step: StepData, context: string) => {
-    if (context !== 'view') {
-      return element;
-    }
-    return <StepFooter step={step} />;
-  },
-);
-
-Hooks.addFilter(
-  'mailpoet.automation.step.more',
-  'mailpoet',
-  (element: JSX.Element | null, step: StepData, context: string) => {
-    if (context !== 'view') {
-      return element;
-    }
-
-    if (step.key === 'mailpoet:send-email') {
-      return <SendEmailPanel step={step} />;
-    }
-
-    return element;
-  },
-);
-
-Hooks.addFilter(
-  'mailpoet.automation.render_step_separator',
-  'mailpoet',
-  (filterValue: () => JSX.Element, context) => {
-    if (context !== 'view') {
-      return filterValue;
-    }
-    return function statisticSeperatorWrapper(previousStepData: StepData) {
-      return <StatisticSeparator previousStepId={previousStepData.id} />;
-    };
-  },
-  20,
-);
+initHooks();
 
 export function AutomationFlow(): JSX.Element {
   const { section } = useSelect(
