aca358d276e20e8afd4d2c624a69f40401ee6bc0
Rostislav Wolny <rosta.wolny@automattic.com>
2023-07-04 16:18:22 +0200

    Add App level migration and template
    [MAILPOET-5416]

---
+++ mailpoet/lib/Migrator/AppMigration.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Migrator;
+
+use MailPoet\DI\ContainerWrapper;
+use MailPoetVendor\Doctrine\ORM\EntityManager;
+
+abstract class AppMigration {
+  /** @var ContainerWrapper */
+  protected $container;
+
+  /** @var EntityManager */
+  protected $entityManager;
+
+  public function __construct(
+    ContainerWrapper $container
+  ) {
+    $this->container = $container;
+    $this->entityManager = $container->get(EntityManager::class);
+  }
+
+  abstract public function run(): void;
+}
