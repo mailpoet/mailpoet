7b491ce6c75d456ac0bd38d61d9471ad229dd07c
Rostislav Wolny <rosta.wolny@automattic.com>
2023-07-07 14:42:20 +0200

    Refactor Migration_20230111_120000 to work without SettingsController
    [MAILPOET-5416]

--- mailpoet/lib/Migrations/Db/Migration_20230111_120000.php
+++ mailpoet/lib/Migrations/Db/Migration_20230111_120000.php
@@ -3,13 +3,14 @@
 namespace MailPoet\Migrations\Db;
 
 use MailPoet\Entities\SegmentEntity;
+use MailPoet\Entities\SettingEntity;
 use MailPoet\Migrator\DbMigration;
-use MailPoet\Settings\SettingsController;
 use MailPoetVendor\Doctrine\DBAL\Connection;
 
 class Migration_20230111_120000 extends DbMigration {
   public function run(): void {
     $segmentsTable = $this->getTableName(SegmentEntity::class);
+    $settingsTable = $this->getTableName(SettingEntity::class);
     $columnName = 'display_in_manage_subscription_page';
 
     if ($this->columnExists($segmentsTable, $columnName)) {
@@ -21,8 +22,14 @@ class Migration_20230111_120000 extends DbMigration {
       ADD {$columnName} tinyint(1) NOT NULL DEFAULT 0
     ");
 
-    $settings = $this->container->get(SettingsController::class);
-    $segmentIds = $settings->get('subscription.segments', []);
+    $subscriptionSetting = $this->connection->fetchOne("
+      SELECT value
+      FROM {$settingsTable}
+      WHERE name = ?", ['subscription']
+    );
+    $subscriptionSetting = is_string($subscriptionSetting) ? unserialize($subscriptionSetting) : [];
+    $subscriptionSetting = is_array($subscriptionSetting) ? $subscriptionSetting : [];
+    $segmentIds = $subscriptionSetting['segments'] ?? [];
     if ($segmentIds) {
       // display only segments from settings.subscription.segments
       $this->connection->executeStatement("
@@ -31,7 +38,16 @@ class Migration_20230111_120000 extends DbMigration {
         WHERE id IN (?)
       ", [$segmentIds], [Connection::PARAM_INT_ARRAY]);
 
-      $settings->set('subscription.segments', []);
+      $subscriptionSetting['segments'] = [];
+      $this->connection->executeStatement("
+        UPDATE {$settingsTable}
+        SET value = ?
+        WHERE name = ?",
+        [
+          serialize($subscriptionSetting),
+          'subscription',
+        ]
+      );
     } else {
       $this->connection->executeStatement("
         UPDATE {$segmentsTable}
---
+++ mailpoet/tests/integration/Migrations/Migration_20230111_120000_Test.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Migrations;
+
+use MailPoet\Entities\SegmentEntity;
+use MailPoet\Migrations\Db\Migration_20230111_120000;
+use MailPoet\Settings\SettingsController;
+use MailPoet\Test\DataFactories\Segment;
+
+//phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
+class Migration_20230111_120000_Test extends \MailPoetTest {
+  /** @var Migration_20230111_120000 */
+  private $migration;
+
+  /** @var SettingsController */
+  private $settings;
+
+  /** @var SegmentEntity[] */
+  private $segments;
+
+  public function _before() {
+    parent::_before();
+    $this->migration = new Migration_20230111_120000($this->diContainer);
+    $this->settings = $this->diContainer->get(SettingsController::class);
+    $segmentsFactory = new Segment();
+    $this->segments[] = $segmentsFactory->withName('Segment 1')->create();
+    $this->segments[] = $segmentsFactory->withName('Segment 2')->create();
+    $this->segments[] = $segmentsFactory->withName('Segment 3')->create();
+
+    $segmentsTable = $this->entityManager->getClassMetadata(SegmentEntity::class)->getTableName();
+    $this->entityManager->getConnection()->executeStatement("
+      ALTER TABLE {$segmentsTable}
+      DROP COLUMN display_in_manage_subscription_page;
+    ");
+  }
+
+  public function testItSetsDisplayInManageSubscriptionPageProperly() {
+    $this->settings->set('subscription', ['segments' => [$this->segments[0]->getId(), $this->segments[2]->getId()]]);
+    $this->migration->run();
+    foreach ($this->segments as $segment) {
+      $this->entityManager->refresh($segment);
+    }
+    expect($this->segments[0]->getDisplayInManageSubscriptionPage())->equals(true);
+    expect($this->segments[1]->getDisplayInManageSubscriptionPage())->equals(false);
+    expect($this->segments[2]->getDisplayInManageSubscriptionPage())->equals(true);
+    expect($this->settings->fetch('subscription.segments'))->equals([]);
+  }
+
+  public function testItSetsAllToTrueWithoutDefinedSetting() {
+    $this->settings->delete('subscription');
+    $this->migration->run();
+    foreach ($this->segments as $segment) {
+      $this->entityManager->refresh($segment);
+    }
+    expect($this->segments[0]->getDisplayInManageSubscriptionPage())->equals(true);
+    expect($this->segments[1]->getDisplayInManageSubscriptionPage())->equals(true);
+    expect($this->segments[2]->getDisplayInManageSubscriptionPage())->equals(true);
+  }
+}
