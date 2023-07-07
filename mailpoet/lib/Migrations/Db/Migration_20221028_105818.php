8fb91e6047e615aa64d79104d957e2572e8644d7
Rostislav Wolny <rosta.wolny@automattic.com>
2023-07-07 15:19:21 +0200

    Extract App level method from initial Db migration to a new App level migration
    All these extracted methods contain DB version check so for existing
    sites the migration will be just added as completed to the migrations table.
    [MAILPOET-5416]

--- mailpoet/lib/Migrations/Db/Migration_20221028_105818.php
+++ mailpoet/lib/Migrations/Db/Migration_20221028_105818.php
@@ -4,7 +4,6 @@ namespace MailPoet\Migrations\Db;
 
 use MailPoet\Config\Env;
 use MailPoet\Cron\CronTrigger;
-use MailPoet\Cron\Workers\StatsNotifications\Worker;
 use MailPoet\Entities\DynamicSegmentFilterData;
 use MailPoet\Entities\FormEntity;
 use MailPoet\Entities\NewsletterEntity;
@@ -13,19 +12,14 @@ use MailPoet\Entities\NewsletterTemplateEntity;
 use MailPoet\Entities\ScheduledTaskEntity;
 use MailPoet\Entities\SendingQueueEntity;
 use MailPoet\Entities\SubscriberEntity;
-use MailPoet\Form\FormsRepository;
 use MailPoet\Migrator\DbMigration;
 use MailPoet\Segments\DynamicSegments\Filters\EmailAction;
 use MailPoet\Segments\DynamicSegments\Filters\UserRole;
 use MailPoet\Segments\DynamicSegments\Filters\WooCommerceCategory;
 use MailPoet\Segments\DynamicSegments\Filters\WooCommerceProduct;
 use MailPoet\Segments\DynamicSegments\Filters\WooCommerceSubscription;
-use MailPoet\Settings\SettingsChangeHandler;
 use MailPoet\Settings\SettingsController;
-use MailPoet\Settings\TrackingConfig;
 use MailPoet\Util\Helpers;
-use MailPoet\Util\Notices\ChangedTrackingNotice;
-use MailPoet\WP\Functions as WPFunctions;
 
 /**
  * Moved from MailPoet\Config\Migrator.
@@ -81,22 +75,10 @@ class Migration_20221028_105818 extends DbMigration {
   /** @var SettingsController */
   private $settings;
 
-  /** @var SettingsChangeHandler */
-  private $settingsChangeHandler;
-
-  /** @var FormsRepository */
-  private $formsRepository;
-
-  /** @var WPFunctions */
-  private $wp;
-
   public function run(): void {
     $this->prefix = Env::$dbPrefix;
     $this->charsetCollate = Env::$dbCharsetCollate;
     $this->settings = $this->container->get(SettingsController::class);
-    $this->settingsChangeHandler = $this->container->get(SettingsChangeHandler::class);
-    $this->formsRepository = $this->container->get(FormsRepository::class);
-    $this->wp = $this->container->get(WPFunctions::class);
 
     // Ensure dbDelta function
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
@@ -113,21 +95,15 @@ class Migration_20221028_105818 extends DbMigration {
     $this->migrateWooSubscriptionsDynamicFilters();
     $this->migratePurchasedInCategoryDynamicFilters();
     $this->migrateEmailActionsFilters();
-    $this->updateDefaultInactiveSubscriberTimeRange();
-    $this->setDefaultValueForLoadingThirdPartyLibrariesForExistingInstalls();
     $this->disableMailPoetCronTrigger();
 
     // POPULATOR
     $this->updateMetaFields();
     $this->updateLastSubscribedAt();
-    $this->enableStatsNotificationsForAutomatedEmails();
     $this->updateSentUnsubscribeLinksToInstantUnsubscribeLinks();
     $this->pauseTasksForPausedNewsletters();
     $this->moveGoogleAnalyticsFromPremium();
-    $this->addPlacementStatusToForms();
-    $this->migrateFormPlacement();
     $this->moveNewsletterTemplatesThumbnailData();
-    $this->updateToUnifiedTrackingSettings();
     $this->fixNotificationHistoryRecordsStuckAtSending();
   }
 
@@ -997,37 +973,6 @@ class Migration_20221028_105818 extends DbMigration {
     return true;
   }
 
-  private function updateDefaultInactiveSubscriberTimeRange(): bool {
-    // Skip if the installed version is newer than the release that preceded this migration, or if it's a fresh install
-    $currentlyInstalledVersion = (string)$this->settings->get('db_version', '3.78.1');
-    if (version_compare($currentlyInstalledVersion, '3.78.0', '>')) {
-      return false;
-    }
-
-    $currentValue = (int)$this->settings->get('deactivate_subscriber_after_inactive_days');
-    if ($currentValue === 180) {
-      $this->settings->set('deactivate_subscriber_after_inactive_days', 365);
-      $this->settingsChangeHandler->onInactiveSubscribersIntervalChange();
-    }
-
-    return true;
-  }
-
-  private function setDefaultValueForLoadingThirdPartyLibrariesForExistingInstalls(): bool {
-    // skip the migration if the DB version is higher than 3.91.1 or is not set (a new installation)
-    if (version_compare($this->settings->get('db_version', '3.91.2'), '3.91.1', '>')) {
-      return false;
-    }
-
-    $thirdPartyScriptsEnabled = $this->settings->get('3rd_party_libs');
-    if (is_null($thirdPartyScriptsEnabled)) {
-      // keep loading 3rd party libraries for existing users so the functionality is not broken
-      $this->settings->set('3rd_party_libs.enabled', '1');
-    }
-
-    return true;
-  }
-
   private function updateMetaFields() {
     global $wpdb;
     // perform once for versions below or equal to 3.26.0
@@ -1041,15 +986,6 @@ class Migration_20221028_105818 extends DbMigration {
       $wpdb->query("UPDATE `" . esc_sql($table) . "` SET meta = NULL WHERE meta = 'null'");
     }
     return true;
-  }
-
-  private function enableStatsNotificationsForAutomatedEmails() {
-    if (version_compare((string)$this->settings->get('db_version', '3.31.2'), '3.31.1', '>')) {
-      return;
-    }
-    $settings = $this->settings->get(Worker::SETTINGS_KEY);
-    $settings['automated'] = true;
-    $this->settings->set(Worker::SETTINGS_KEY, $settings);
   }
 
   private function updateSentUnsubscribeLinksToInstantUnsubscribeLinks() {
@@ -1091,135 +1027,6 @@ class Migration_20221028_105818 extends DbMigration {
         'nStatusDraft' => NewsletterEntity::STATUS_DRAFT,
       ]
     );
-  }
-
-  private function addPlacementStatusToForms() {
-    if (version_compare((string)$this->settings->get('db_version', '3.49.0'), '3.48.1', '>')) {
-      return;
-    }
-    $forms = $this->formsRepository->findAll();
-    foreach ($forms as $form) {
-      $settings = $form->getSettings();
-      if (
-        (isset($settings['place_form_bellow_all_posts']) && $settings['place_form_bellow_all_posts'] === '1')
-        || (isset($settings['place_form_bellow_all_pages']) && $settings['place_form_bellow_all_pages'] === '1')
-      ) {
-        $settings['form_placement_bellow_posts_enabled'] = '1';
-      } else {
-        $settings['form_placement_bellow_posts_enabled'] = '';
-      }
-      if (
-        (isset($settings['place_popup_form_on_all_posts']) && $settings['place_popup_form_on_all_posts'] === '1')
-        || (isset($settings['place_popup_form_on_all_pages']) && $settings['place_popup_form_on_all_pages'] === '1')
-      ) {
-        $settings['form_placement_popup_enabled'] = '1';
-      } else {
-        $settings['form_placement_popup_enabled'] = '';
-      }
-      if (
-        (isset($settings['place_fixed_bar_form_on_all_posts']) && $settings['place_fixed_bar_form_on_all_posts'] === '1')
-        || (isset($settings['place_fixed_bar_form_on_all_pages']) && $settings['place_fixed_bar_form_on_all_pages'] === '1')
-      ) {
-        $settings['form_placement_fixed_bar_enabled'] = '1';
-      } else {
-        $settings['form_placement_fixed_bar_enabled'] = '';
-      }
-      if (
-        (isset($settings['place_slide_in_form_on_all_posts']) && $settings['place_slide_in_form_on_all_posts'] === '1')
-        || (isset($settings['place_slide_in_form_on_all_pages']) && $settings['place_slide_in_form_on_all_pages'] === '1')
-      ) {
-        $settings['form_placement_slide_in_enabled'] = '1';
-      } else {
-        $settings['form_placement_slide_in_enabled'] = '';
-      }
-      $form->setSettings($settings);
-    }
-    $this->formsRepository->flush();
-  }
-
-  private function migrateFormPlacement() {
-    if (version_compare((string)$this->settings->get('db_version', '3.50.0'), '3.49.1', '>')) {
-      return;
-    }
-    $forms = $this->formsRepository->findAll();
-    foreach ($forms as $form) {
-      $settings = $form->getSettings();
-      if (!is_array($settings)) continue;
-      $settings['form_placement'] = [
-        FormEntity::DISPLAY_TYPE_POPUP => [
-          'enabled' => $settings['form_placement_popup_enabled'],
-          'delay' => $settings['popup_form_delay'] ?? 0,
-          'styles' => $settings['popup_styles'] ?? [],
-          'posts' => [
-            'all' => $settings['place_popup_form_on_all_posts'] ?? '',
-          ],
-          'pages' => [
-            'all' => $settings['place_popup_form_on_all_pages'] ?? '',
-          ],
-        ],
-        FormEntity::DISPLAY_TYPE_FIXED_BAR => [
-          'enabled' => $settings['form_placement_fixed_bar_enabled'],
-          'delay' => $settings['fixed_bar_form_delay'] ?? 0,
-          'styles' => $settings['fixed_bar_styles'] ?? [],
-          'position' => $settings['fixed_bar_form_position'] ?? 'top',
-          'posts' => [
-            'all' => $settings['place_fixed_bar_form_on_all_posts'] ?? '',
-          ],
-          'pages' => [
-            'all' => $settings['place_fixed_bar_form_on_all_pages'] ?? '',
-          ],
-        ],
-        FormEntity::DISPLAY_TYPE_BELOW_POST => [
-          'enabled' => $settings['form_placement_bellow_posts_enabled'],
-          'styles' => $settings['below_post_styles'] ?? [],
-          'posts' => [
-            'all' => $settings['place_form_bellow_all_posts'] ?? '',
-          ],
-          'pages' => [
-            'all' => $settings['place_form_bellow_all_pages'] ?? '',
-          ],
-        ],
-        FormEntity::DISPLAY_TYPE_SLIDE_IN => [
-          'enabled' => $settings['form_placement_slide_in_enabled'],
-          'delay' => $settings['slide_in_form_delay'] ?? 0,
-          'position' => $settings['slide_in_form_position'] ?? 'right',
-          'styles' => $settings['slide_in_styles'] ?? [],
-          'posts' => [
-            'all' => $settings['place_slide_in_form_on_all_posts'] ?? '',
-          ],
-          'pages' => [
-            'all' => $settings['place_slide_in_form_on_all_pages'] ?? '',
-          ],
-        ],
-        FormEntity::DISPLAY_TYPE_OTHERS => [
-          'styles' => $settings['other_styles'] ?? [],
-        ],
-      ];
-      if (isset($settings['form_placement_slide_in_enabled'])) unset($settings['form_placement_slide_in_enabled']);
-      if (isset($settings['form_placement_fixed_bar_enabled'])) unset($settings['form_placement_fixed_bar_enabled']);
-      if (isset($settings['form_placement_popup_enabled'])) unset($settings['form_placement_popup_enabled']);
-      if (isset($settings['form_placement_bellow_posts_enabled'])) unset($settings['form_placement_bellow_posts_enabled']);
-      if (isset($settings['place_form_bellow_all_pages'])) unset($settings['place_form_bellow_all_pages']);
-      if (isset($settings['place_form_bellow_all_posts'])) unset($settings['place_form_bellow_all_posts']);
-      if (isset($settings['place_popup_form_on_all_pages'])) unset($settings['place_popup_form_on_all_pages']);
-      if (isset($settings['place_popup_form_on_all_posts'])) unset($settings['place_popup_form_on_all_posts']);
-      if (isset($settings['popup_form_delay'])) unset($settings['popup_form_delay']);
-      if (isset($settings['place_fixed_bar_form_on_all_pages'])) unset($settings['place_fixed_bar_form_on_all_pages']);
-      if (isset($settings['place_fixed_bar_form_on_all_posts'])) unset($settings['place_fixed_bar_form_on_all_posts']);
-      if (isset($settings['fixed_bar_form_delay'])) unset($settings['fixed_bar_form_delay']);
-      if (isset($settings['fixed_bar_form_position'])) unset($settings['fixed_bar_form_position']);
-      if (isset($settings['place_slide_in_form_on_all_pages'])) unset($settings['place_slide_in_form_on_all_pages']);
-      if (isset($settings['place_slide_in_form_on_all_posts'])) unset($settings['place_slide_in_form_on_all_posts']);
-      if (isset($settings['slide_in_form_delay'])) unset($settings['slide_in_form_delay']);
-      if (isset($settings['slide_in_form_position'])) unset($settings['slide_in_form_position']);
-      if (isset($settings['other_styles'])) unset($settings['other_styles']);
-      if (isset($settings['slide_in_styles'])) unset($settings['slide_in_styles']);
-      if (isset($settings['below_post_styles'])) unset($settings['below_post_styles']);
-      if (isset($settings['fixed_bar_styles'])) unset($settings['fixed_bar_styles']);
-      if (isset($settings['popup_styles'])) unset($settings['popup_styles']);
-      $form->setSettings($settings);
-    }
-    $this->formsRepository->flush();
   }
 
   private function moveGoogleAnalyticsFromPremium() {
@@ -1273,27 +1080,6 @@ class Migration_20221028_105818 extends DbMigration {
       SET thumbnail_data = thumbnail, thumbnail = NULL
       WHERE thumbnail LIKE 'data:image%';"
     );
-  }
-
-  private function updateToUnifiedTrackingSettings() {
-    if (version_compare((string)$this->settings->get('db_version', '3.74.3'), '3.74.2', '>')) {
-      return;
-    }
-    $emailTracking = $this->settings->get('tracking.enabled', true);
-    $wooTrackingCookie = $this->settings->get('woocommerce.accept_cookie_revenue_tracking.enabled');
-    if ($wooTrackingCookie === null) { // No setting for WooCommerce Cookie Tracking - WooCommerce was not active
-      $trackingLevel = $emailTracking ? TrackingConfig::LEVEL_FULL : TrackingConfig::LEVEL_BASIC;
-    } elseif ($wooTrackingCookie) { // WooCommerce Cookie Tracking enabled
-      $trackingLevel = TrackingConfig::LEVEL_FULL;
-      // Cookie was enabled but tracking disabled and we are switching to full.
-      // So we activate an admin notice to let the user know that we activated tracking
-      if (!$emailTracking) {
-        $this->wp->setTransient(ChangedTrackingNotice::OPTION_NAME, true);
-      }
-    } else { // WooCommerce Tracking Cookie Disabled
-      $trackingLevel = $emailTracking ? TrackingConfig::LEVEL_PARTIAL : TrackingConfig::LEVEL_BASIC;
-    }
-    $this->settings->set('tracking.level', $trackingLevel);
   }
 
   private function fixNotificationHistoryRecordsStuckAtSending() {
--- mailpoet/tests/integration/Migrations/Migration_20221028_105818_Test.php
+++ mailpoet/tests/integration/Migrations/Migration_20221028_105818_Test.php
@@ -2,20 +2,11 @@
 
 namespace MailPoet\Migrations;
 
-use Codeception\Stub;
-use MailPoet\Cron\Workers\InactiveSubscribers;
 use MailPoet\Entities\DynamicSegmentFilterData;
 use MailPoet\Entities\DynamicSegmentFilterEntity;
-use MailPoet\Entities\ScheduledTaskEntity;
 use MailPoet\Migrations\Db\Migration_20221028_105818;
-use MailPoet\Newsletter\Sending\ScheduledTasksRepository;
 use MailPoet\Segments\DynamicSegments\Filters\EmailAction;
 use MailPoet\Settings\SettingsController;
-use MailPoet\Settings\TrackingConfig;
-use MailPoet\Util\Notices\ChangedTrackingNotice;
-use MailPoet\WP\Functions;
-use MailPoet\WP\Functions as WPFunctions;
-use MailPoetVendor\Carbon\Carbon;
 
 //phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
 class Migration_20221028_105818_Test extends \MailPoetTest {
@@ -96,149 +87,6 @@ class Migration_20221028_105818_Test extends \MailPoetTest {
     expect($filterNoLink->getFilterData()->getData())->equals(['newsletter_id' => '1', 'operator' => 'none', 'link_ids' => []]);
     expect($filterLink->getFilterData()->getAction())->equals(EmailAction::ACTION_CLICKED);
     expect($filterLink->getFilterData()->getData())->equals(['newsletter_id' => '1', 'operator' => 'none', 'link_ids' => [2]]);
-  }
-
-  public function testItDoesNotUpdateInactiveSubscribersFrequencyValuesOtherThanThePreviousDefault() {
-    $this->settings->delete('deactivate_subscriber_after_inactive_days');
-    $nonDefaultOptions = [ '', '90', '365' ];
-    foreach ($nonDefaultOptions as $option) {
-      $this->settings->set('db_version', '3.78.0');
-      $this->settings->set('deactivate_subscriber_after_inactive_days', $option);
-      $this->migration->run();
-      $this->assertEquals($option, $this->settings->get('deactivate_subscriber_after_inactive_days'));
-    }
-  }
-
-  public function testItDoesUpdateInactiveSubscribersFrequencyPreviousDefaultValue() {
-    $this->settings->delete('deactivate_subscriber_after_inactive_days');
-    $this->settings->set('db_version', '3.78.0');
-    $this->settings->set('deactivate_subscriber_after_inactive_days', '180');
-    $this->migration->run();
-    $this->assertEquals('365', $this->settings->get('deactivate_subscriber_after_inactive_days'));
-  }
-
-  public function testItDoesNotRunInactiveSubscribersFrequencyMigrationForUnexpectedVersions() {
-    $this->settings->delete('deactivate_subscriber_after_inactive_days');
-    $versions = ['3.78.1', '3.900.2', '4.8.0'];
-    foreach ($versions as $version) {
-      $this->settings->set('db_version', $version);
-      $this->settings->set('deactivate_subscriber_after_inactive_days', '180');
-      $this->assertEquals('180', $this->settings->get('deactivate_subscriber_after_inactive_days'));
-      $this->migration->run();
-      $this->assertEquals('180', $this->settings->get('deactivate_subscriber_after_inactive_days'));
-    }
-  }
-
-  public function testItDoesNotRunInactiveSubscribersFrequencyMigrationForNewInstalls() {
-    $this->settings->delete('deactivate_subscriber_after_inactive_days');
-    $this->settings->delete('db_version');
-    $this->migration->run();
-    $setting = $this->settings->get('deactivate_subscriber_after_inactive_days', 'not-set');
-    $this->assertEquals('not-set', $setting);
-  }
-
-  public function testItCreatesInactiveSubscribersTaskIfOneNotAlreadyScheduled() {
-    $this->settings->delete('deactivate_subscriber_after_inactive_days');
-    $currentTime = Carbon::now()->microsecond(0);
-
-    /** @var WPFunctions $wpStub */
-    $wpStub = Stub::make(new WPFunctions(), [
-      'currentTime' => asCallable(function() use ($currentTime) {
-        return $currentTime->getTimestamp();
-      }),
-    ]);
-    WPFunctions::set($wpStub);
-
-    // Double check there isn't already a task in the DB
-    $scheduledTasksRepository = $this->diContainer->get(ScheduledTasksRepository::class);
-    $shouldBeNull = $scheduledTasksRepository->findOneBy([
-      'type' => InactiveSubscribers::TASK_TYPE,
-    ]);
-    $this->assertNull($shouldBeNull);
-
-    // Run the migration
-    $this->settings->set('db_version', '3.78.0');
-    $this->settings->set('deactivate_subscriber_after_inactive_days', '180');
-    $this->migration->run();
-
-    $scheduledTasksRepository = $this->diContainer->get(ScheduledTasksRepository::class);
-    $task = $scheduledTasksRepository->findOneBy([
-      'type' => InactiveSubscribers::TASK_TYPE,
-    ]);
-    $this->assertNotNull($task);
-    $this->assertEquals($currentTime->subMinute(), $task->getScheduledAt());
-  }
-
-  public function testItReschedulesScheduledInactiveSubscribersTask() {
-    $this->settings->delete('deactivate_subscriber_after_inactive_days');
-    $currentTime = Carbon::now()->microsecond(0);
-
-    /** @var WPFunctions $wpStub */
-    $wpStub = Stub::make(new WPFunctions(), [
-      'currentTime' => asCallable(function() use ($currentTime) {
-        return $currentTime->getTimestamp();
-      }),
-    ]);
-    WPFunctions::set($wpStub);
-    $twoHoursFromNow = $currentTime->copy()->addHours(2);
-
-    // Create existing task scheduled for the future
-    $existingTask = new ScheduledTaskEntity();
-    $existingTask->setType(InactiveSubscribers::TASK_TYPE);
-    $existingTask->setStatus(ScheduledTaskEntity::STATUS_SCHEDULED);
-    $existingTask->setScheduledAt($twoHoursFromNow);
-    $scheduledTasksRepository = $this->diContainer->get(ScheduledTasksRepository::class);
-    $scheduledTasksRepository->persist($existingTask);
-    $scheduledTasksRepository->flush();
-
-    // Run the migration
-    $this->settings->set('db_version', '3.78.0');
-    $this->settings->set('deactivate_subscriber_after_inactive_days', '180');
-    $this->migration->run();
-
-    $this->assertEquals($currentTime->subMinute(), $existingTask->getScheduledAt());
-  }
-
-  public function testItMigratesTrackingSettings() {
-    $wp = $this->diContainer->get(Functions::class);
-    $wp->deleteTransient(ChangedTrackingNotice::OPTION_NAME);
-    // WooCommerce disabled and Tracking enabled
-    $this->settings->set('db_version', '3.74.1');
-    $this->settings->set('tracking', ['enabled' => true]);
-    $this->settings->set('woocommerce.accept_cookie_revenue_tracking.enabled', null);
-    $this->migration->run();
-    expect($this->settings->get('tracking.level'))->equals(TrackingConfig::LEVEL_FULL);
-    expect($wp->getTransient(ChangedTrackingNotice::OPTION_NAME))->false();
-    // WooCommerce disabled and Tracking disabled
-    $this->settings->set('tracking', ['enabled' => false]);
-    $this->settings->set('woocommerce.accept_cookie_revenue_tracking.enabled', null);
-    $this->migration->run();
-    expect($this->settings->get('tracking.level'))->equals(TrackingConfig::LEVEL_BASIC);
-    expect($wp->getTransient(ChangedTrackingNotice::OPTION_NAME))->false();
-    // WooCommerce enabled with cookie enabled and Tracking enabled
-    $this->settings->set('tracking', ['enabled' => true]);
-    $this->settings->set('woocommerce.accept_cookie_revenue_tracking.enabled', "1");
-    $this->migration->run();
-    expect($this->settings->get('tracking.level'))->equals(TrackingConfig::LEVEL_FULL);
-    expect($wp->getTransient(ChangedTrackingNotice::OPTION_NAME))->false();
-    // WooCommerce enabled with cookie disabled and Tracking enabled
-    $this->settings->set('tracking', ['enabled' => true]);
-    $this->settings->set('woocommerce.accept_cookie_revenue_tracking.enabled', "");
-    $this->migration->run();
-    expect($this->settings->get('tracking.level'))->equals(TrackingConfig::LEVEL_PARTIAL);
-    expect($wp->getTransient(ChangedTrackingNotice::OPTION_NAME))->false();
-    // WooCommerce enabled with cookie disabled and Tracking disabled
-    $this->settings->set('tracking', ['enabled' => false]);
-    $this->settings->set('woocommerce.accept_cookie_revenue_tracking.enabled', "");
-    $this->migration->run();
-    expect($this->settings->get('tracking.level'))->equals(TrackingConfig::LEVEL_BASIC);
-    expect($wp->getTransient(ChangedTrackingNotice::OPTION_NAME))->false();
-    // WooCommerce enabled with cookie enabled and Tracking disabled
-    $this->settings->set('tracking', ['enabled' => false]);
-    $this->settings->set('woocommerce.accept_cookie_revenue_tracking.enabled', "1");
-    $this->migration->run();
-    expect($this->settings->get('tracking.level'))->equals(TrackingConfig::LEVEL_FULL);
-    expect($wp->getTransient(ChangedTrackingNotice::OPTION_NAME))->equals(true);
   }
 
   private function createSegmentFilter(string $action, array $data, string $type, $segmentId = 1): int {
