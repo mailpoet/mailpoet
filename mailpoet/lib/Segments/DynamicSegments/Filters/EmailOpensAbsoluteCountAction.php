14a2603ac3929ecdba1ef2d73f4215d6c23e0b6d
John Oleksowicz <john.oleksowicz@automattic.com>
2023-07-17 15:58:23 -0500

    Implement lifetime option for email opens
    MAILPOET-4991

--- mailpoet/lib/Segments/DynamicSegments/Filters/EmailOpensAbsoluteCountAction.php
+++ mailpoet/lib/Segments/DynamicSegments/Filters/EmailOpensAbsoluteCountAction.php
@@ -29,16 +29,28 @@ class EmailOpensAbsoluteCountAction implements Filter {
     $days = $filterData->getParam('days');
     $operator = $filterData->getParam('operator');
     $action = $filterData->getAction();
+    $timeframe = $filterData->getParam('timeframe');
     $parameterSuffix = $filter->getId() ?? Security::generateRandomString();
     $statsTable = $this->entityManager->getClassMetadata(StatisticsOpenEntity::class)->getTableName();
     $subscribersTable = $this->entityManager->getClassMetadata(SubscriberEntity::class)->getTableName();
-    $queryBuilder->leftJoin(
-      $subscribersTable,
-      $statsTable,
-      'opens',
-      "{$subscribersTable}.id = opens.subscriber_id AND opens.created_at > :newer{$parameterSuffix} AND opens.user_agent_type = :userAgentType{$parameterSuffix}"
-    );
-    $queryBuilder->setParameter('newer' . $parameterSuffix, CarbonImmutable::now()->subDays($days)->startOfDay());
+
+    if ($timeframe === 'allTime') {
+      $queryBuilder->leftJoin(
+        $subscribersTable,
+        $statsTable,
+        'opens',
+        "{$subscribersTable}.id = opens.subscriber_id AND opens.user_agent_type = :userAgentType{$parameterSuffix}"
+      );
+    } else {
+      $queryBuilder->leftJoin(
+        $subscribersTable,
+        $statsTable,
+        'opens',
+        "{$subscribersTable}.id = opens.subscriber_id AND opens.created_at > :newer{$parameterSuffix} AND opens.user_agent_type = :userAgentType{$parameterSuffix}"
+      );
+      $queryBuilder->setParameter('newer' . $parameterSuffix, CarbonImmutable::now()->subDays($days)->startOfDay());
+    }
+
     $queryBuilder->groupBy("$subscribersTable.id");
     if ($operator === 'equals') {
       $queryBuilder->having("count(opens.id) = :opens" . $parameterSuffix);
