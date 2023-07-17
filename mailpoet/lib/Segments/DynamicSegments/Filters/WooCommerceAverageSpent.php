64094387eba111d5cee82d39eeb8d1902de0a677
John Oleksowicz <john.oleksowicz@automattic.com>
2023-07-17 16:32:09 -0500

    Implement lifetime option for average spent filter
    MAILPOET-4991

--- mailpoet/lib/Segments/DynamicSegments/Filters/WooCommerceAverageSpent.php
+++ mailpoet/lib/Segments/DynamicSegments/Filters/WooCommerceAverageSpent.php
@@ -27,15 +27,20 @@ class WooCommerceAverageSpent implements Filter {
     $filterData = $filter->getFilterData();
     $operator = $filterData->getParam('average_spent_type');
     $amount = $filterData->getParam('average_spent_amount');
-    $days = intval($filterData->getParam('days'));
+    $timeframe = $filterData->getParam('timeframe');
 
-    $date = Carbon::now()->subDays($days);
-    $dateParam = $this->filterHelper->getUniqueParameterName('date');
     $orderStatsAlias = $this->wooFilterHelper->applyOrderStatusFilter($queryBuilder);
 
-    $queryBuilder->andWhere("$orderStatsAlias.date_created >= :$dateParam")
-      ->setParameter($dateParam, $date->toDateTimeString())
-      ->groupBy('inner_subscriber_id');
+    if ($timeframe !== 'allTime') {
+      $days = intval($filterData->getParam('days'));
+      $date = Carbon::now()->subDays($days);
+      $dateParam = $this->filterHelper->getUniqueParameterName('date');
+      $queryBuilder
+        ->andWhere("$orderStatsAlias.date_created >= :$dateParam")
+        ->setParameter($dateParam, $date->toDateTimeString());
+    }
+
+    $queryBuilder->groupBy('inner_subscriber_id');
 
     $amountParam = $this->filterHelper->getUniqueParameterName('amount');
     if ($operator === '=') {
