8451e64589ee96230539bf94bb9d6db3d98a3b86
Jan Jakes <jan.jakes@automattic.com>
2023-07-27 13:30:27 +0200

    Fallback to email when customer has no name
    [MAILPOET-5492]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/orders/cells/customer.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/orders/cells/customer.tsx
@@ -5,13 +5,19 @@ export function CustomerCell({
 }: {
   customer: CustomerData;
 }): JSX.Element {
+  const name = [customer.first_name, customer.last_name]
+    .filter(Boolean)
+    .join(' ');
+
+  const label = name || customer.email;
+
   return (
     <a
       className="mailpoet-analytics-orders__customer"
       href={`?page=mailpoet-subscribers#/edit/${customer.id}`}
     >
-      <img src={customer.avatar} alt={customer.last_name} />
-      {`${customer.first_name} ${customer.last_name}`}
+      <img src={customer.avatar} alt={label} />
+      {label}
     </a>
   );
 }
