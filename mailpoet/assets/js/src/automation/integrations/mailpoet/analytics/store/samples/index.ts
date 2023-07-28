af12dc3fd3bf0c529d27a2476fcc8164c1fbe59f
Jan Jakes <jan.jakes@automattic.com>
2023-07-28 09:41:05 +0200

    Add sample data for subscribers section

--- mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/store/samples/index.ts
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/store/samples/index.ts
@@ -1,10 +1,13 @@
 import { orders } from './orders';
+import { subscribers } from './subscribers';
 import { SectionData } from '../types';
 
 export const getSampleData = (sectionId: string): SectionData | undefined => {
   switch (sectionId) {
     case 'orders':
       return orders;
+    case 'subscribers':
+      return subscribers;
     default:
       return undefined;
   }
