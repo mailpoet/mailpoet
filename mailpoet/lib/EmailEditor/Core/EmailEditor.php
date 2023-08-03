3f166ac3a462878ecb1b7767d0d2c7d61c0af50b
Rostislav Wolny <rosta.wolny@automattic.com>
2023-08-03 08:32:55 +0200

    Refactor assets cleanup to an extra class
    [MAILPOET-5365]

--- mailpoet/lib/EmailEditor/Core/EmailEditor.php
+++ mailpoet/lib/EmailEditor/Core/EmailEditor.php
@@ -9,6 +9,18 @@ namespace MailPoet\EmailEditor\Core;
 class EmailEditor {
   private const ALLOWED_BLOCK_TYPES = ['core/paragraph', 'core/heading', 'core/column', 'core/columns'];
 
+  /** @var AssetsCleaner */
+  private $assetsCleaner;
+
+  /**
+   * @param AssetsCleaner $assetsCleaner
+   */
+  public function __construct(
+    AssetsCleaner $assetsCleaner
+  ) {
+    $this->assetsCleaner = $assetsCleaner;
+  }
+
   public function initialize(): void {
     $this->registerEmailPostTypes();
     add_filter('allowed_block_types_all', [$this, 'setAllowedBlocksInEmails'], 100, 2);
@@ -59,40 +71,12 @@ class EmailEditor {
     return self::ALLOWED_BLOCK_TYPES;
   }
 
-  /**
-   * This method removes all callbacks registered for enqueue_block_editor_assets action
-   * except ones allowed via mailpoet_email_editor_allowed_editor_assets_actions filter.
-   *
-   * This is to prevent 3rd party plugins which don't check post type from breaking the email editor.
-   */
-  public function cleanupBlockEditorAssets(): void {
+  public function cleanupBlockEditorAssets() {
     $emailPostTypes = array_column($this->getPostTypes(), 'name');
     if (!in_array(get_post_type(), $emailPostTypes, true)) {
       return;
     }
-
-    $allowedActions = apply_filters(
-      'mailpoet_email_editor_allowed_editor_assets_actions',
-      [
-        __CLASS__ . '::cleanupBlockEditorAssets',
-        'wp_enqueue_editor_format_library_assets',
-        'wp_enqueue_editor_block_directory_assets',
-        'wp_enqueue_registered_block_scripts_and_styles',
-        'enqueue_editor_block_styles_assets',
-        'wp_enqueue_global_styles_css_custom_properties',
-      ]
-    );
-
-    $assetsActions = $GLOBALS['wp_filter']['enqueue_block_editor_assets']->callbacks;
-    foreach ($assetsActions as $priority => $actions) {
-      foreach ($actions as $action) {
-        $actionName = is_array($action['function']) ? get_class($action['function'][0]) . '::' . $action['function'][1] : $action['function'];
-        if (in_array($actionName, $allowedActions, true)) {
-          continue;
-        }
-        remove_action('enqueue_block_editor_assets', $action['function'], $priority);
-      }
-    }
+    $this->assetsCleaner->cleanupBlockEditorAssets();
   }
 
   public function updateBlockEditorSettings(array $settings, \WP_Block_Editor_Context $blockEditorContext): array {
