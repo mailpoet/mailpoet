8ded40b1d6eed65fc5c28383564bf05c2b6cb231
Veljko <veljano@yahoo.com>
2023-08-21 17:58:50 +0200

    Add new test to cover bug in the past
    [MAILPOET-5020]

---
+++ mailpoet/tests/acceptance/Homepage/HomepageLandingAfterActivatingMailPoetCest.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Test\Acceptance;
+
+class HomepageLandingAfterActivatingMailPoetCest {
+  public function _before(\AcceptanceTester $i) {
+    $i->activateWooCommerce();
+  }
+  
+  public function homepageLanding(\AcceptanceTester $i) {
+    $i->wantTo('Check that Homepage is shown after activating fresh new MailPoet plugin [MAILPOET-5020]');
+
+    $i->login();
+
+    // Workaround to have fresh new MailPoet plugin before activation
+    $i->amOnMailpoetPage('Settings');
+    $i->click('[data-automation-id="settings-advanced-tab"]');
+    $i->click('Reinstall now...');
+    $i->acceptPopup();
+    $i->waitForNoticeAndClose('Better email — without leaving WordPress');
+
+    $i->amOnPluginsPage();
+    $i->deactivatePlugin('mailpoet');
+    $i->waitForNoticeAndClose('Plugin deactivated.');
+
+    $i->activatePlugin('mailpoet');
+    $i->waitForNoticeAndClose('Better email — without leaving WordPress');
+  }
+
+  public function _after(\AcceptanceTester $i) {
+    $i->deactivateWooCommerce();
+  }
+}
