af79e46ed115cad36d2d6468138bf151d145eebc
Jan Jakes <jan.jakes@automattic.com>
2023-08-23 13:21:45 +0200

    Use WP filter default rather than explicitly checking for "isPremiumActive"
    The issue is explained here: https://mailpoet.atlassian.net/browse/MAILPOET-5545
    
    [MAILPOET-5545]

--- mailpoet/assets/js/src/subscribers/stats/opened_email_stats.tsx
+++ mailpoet/assets/js/src/subscribers/stats/opened_email_stats.tsx
@@ -1,3 +1,4 @@
+import { FunctionComponent, useMemo } from 'react';
 import { Hooks } from 'hooks';
 import { Location } from 'history';
 import { MailPoet } from 'mailpoet';
@@ -12,25 +13,30 @@ type Props = {
 };
 
 export function OpenedEmailsStats({ params, location }: Props): JSX.Element {
+  const Content = useMemo(
+    () =>
+      Hooks.applyFilters(
+        'mailpoet_subscribers_opened_emails_stats',
+        () => (
+          <NoAccessInfo
+            limitReached={MailPoet.subscribersLimitReached}
+            limitValue={MailPoet.subscribersLimit}
+            subscribersCountTowardsLimit={MailPoet.subscribersCount}
+            premiumActive={MailPoet.premiumActive}
+            hasValidApiKey={MailPoet.hasValidApiKey}
+            hasPremiumSupport={MailPoet.hasPremiumSupport}
+          />
+        ),
+        params,
+        location,
+      ) as FunctionComponent,
+    [location, params],
+  );
+
   return (
     <>
       <Heading level={4}>{MailPoet.I18n.t('openedEmailsHeading')}</Heading>
-      {!MailPoet.premiumActive || MailPoet.subscribersLimitReached ? (
-        <NoAccessInfo
-          limitReached={MailPoet.subscribersLimitReached}
-          limitValue={MailPoet.subscribersLimit}
-          subscribersCountTowardsLimit={MailPoet.subscribersCount}
-          premiumActive={MailPoet.premiumActive}
-          hasValidApiKey={MailPoet.hasValidApiKey}
-          hasPremiumSupport={MailPoet.hasPremiumSupport}
-        />
-      ) : (
-        Hooks.applyFilters(
-          'mailpoet_subscribers_opened_emails_stats',
-          params,
-          location,
-        )
-      )}
+      <Content />
     </>
   );
 }
