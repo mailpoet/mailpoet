0dfd8c9c320bd81cb6f46538a10b6f8f0531c922
David Remer <webmaster@websupporter.net>
2023-09-04 11:17:36 +0300

    Add Tag Removed Trigger
    [MAILPOET-5515]

---
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/steps/tag_removed/index.tsx
@@ -0,0 +1,32 @@
+import { __ } from '@wordpress/i18n';
+import { StepType } from '../../../../editor/store';
+import { LockedBadge } from '../../../../../common/premium_modal/locked_badge';
+import { PremiumModalForStepEdit } from '../../../../../common/premium_modal';
+
+const keywords = ['tag', 'tags', 'tag removed', 'tags removed'];
+export const step: StepType = {
+  key: 'mailpoet:tag-removed',
+  group: 'triggers',
+  title: () => __('Tag removed', 'mailpoet'),
+  description: () =>
+    __('Triggers when a tag has been removed from a subscriber.', 'mailpoet'),
+
+  subtitle: () => <LockedBadge text={__('Premium', 'mailpoet')} />,
+  keywords,
+  foreground: '#2271b1',
+  background: '#f0f6fc',
+  icon: () => null,
+  edit: () => (
+    <PremiumModalForStepEdit
+      tracking={{
+        utm_medium: 'upsell_modal',
+        utm_campaign: 'create_automation_editor_tag_removed',
+      }}
+    >
+      {__(
+        'Starting an automatoin when a tag has been removed from a subscriber is a premium feature.',
+        'mailpoet',
+      )}
+    </PremiumModalForStepEdit>
+  ),
+} as const;
