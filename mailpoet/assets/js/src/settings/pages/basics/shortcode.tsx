13de5675fa2d1c417849574767e08758d3ee35c9
John Oleksowicz <john.oleksowicz@automattic.com>
2023-09-06 15:21:31 -0500

    Update shortcode generator to support new options
    MAILPOET-5574

--- mailpoet/assets/js/src/settings/pages/basics/shortcode.tsx
+++ mailpoet/assets/js/src/settings/pages/basics/shortcode.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import { t } from 'common/functions';
 import { Input } from 'common/form/input/input';
 import { Label, Inputs, SegmentsSelect } from 'settings/components';
+import { Datepicker } from 'common/datepicker/datepicker';
+import { MailPoet } from 'mailpoet';
+import { __ } from '@wordpress/i18n';
 
 type Props = {
   name: 'mailpoet_archive' | 'mailpoet_subscribers_count';
@@ -11,9 +14,39 @@ type Props = {
 
 export function Shortcode({ name, title, description }: Props) {
   const [segments, setSegments] = useState([]);
-  const shortcode = `[${name}${
-    segments.length ? ` segments="${segments.join(',')}"` : ''
-  }]`;
+  const [lastNDays, setLastNDays] = useState<string>('');
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+  const [subjectContains, setSubjectContains] = useState<string>('');
+
+  let shortcode = `[${name}`;
+
+  if (segments.length) {
+    shortcode += ` segments="${segments.join(',')}"`;
+  }
+
+  if (startDate) {
+    shortcode += ` start_date="${MailPoet.Date.format(startDate, {
+      format: 'Y-m-d',
+    })}"`;
+  }
+
+  if (endDate) {
+    shortcode += ` end_date="${MailPoet.Date.format(endDate, {
+      format: 'Y-m-d',
+    })}"`;
+  }
+
+  if (lastNDays && parseInt(lastNDays, 10) > 0) {
+    shortcode += ` in_the_last_days="${lastNDays}"`;
+  }
+
+  if (subjectContains && subjectContains.length > 0) {
+    shortcode += ` subject_contains="${subjectContains}"`;
+  }
+
+  shortcode += ']';
+
   const selectText = (event) => {
     event.target.focus();
     event.target.select();
@@ -42,6 +75,73 @@ export function Shortcode({ name, title, description }: Props) {
           placeholder={t('leaveEmptyToDisplayAll')}
           segmentsSelector="getSegments"
         />
+        {name === 'mailpoet_archive' && (
+          <>
+            <br />
+            <Input
+              type="number"
+              min="1"
+              max="365000"
+              dimension="small"
+              placeholder={__('In the last days', 'mailpoet')}
+              onChange={(event) => {
+                const inputValue = event.target.value.trim();
+
+                if (inputValue === '') {
+                  setLastNDays('');
+                  return;
+                }
+
+                const newValue = Number(inputValue);
+
+                if (Number.isInteger(newValue) && newValue > 0) {
+                  setStartDate(null);
+                  setEndDate(null);
+                  setLastNDays(inputValue);
+                }
+              }}
+              value={lastNDays}
+            />
+            <br />
+            <Datepicker
+              dimension="small"
+              dateFormat="MMMM d, yyyy"
+              onChange={(value): void => {
+                setStartDate(value);
+                if (value !== null) {
+                  setLastNDays('');
+                }
+              }}
+              selected={startDate}
+              maxDate={endDate}
+              placeholderText={__('Start date', 'mailpoet')}
+              isClearable
+            />
+            <Datepicker
+              dimension="small"
+              dateFormat="MMMM d, yyyy"
+              onChange={(value): void => {
+                setEndDate(value);
+                if (value !== null) {
+                  setLastNDays('');
+                }
+              }}
+              selected={endDate}
+              minDate={startDate}
+              placeholderText={__('End date', 'mailpoet')}
+              isClearable
+            />
+            <br />
+            <Input
+              dimension="small"
+              type="text"
+              value={subjectContains}
+              onChange={(event) => setSubjectContains(event.target.value)}
+              placeholder={__('Subject lines containing', 'mailpoet')}
+            />
+            <br />
+          </>
+        )}
       </Inputs>
     </>
   );
