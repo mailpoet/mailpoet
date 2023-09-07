b798e5ef5e2359efd8cd858c2a1c076526cbeec3
John Oleksowicz <john.oleksowicz@automattic.com>
2023-09-07 15:54:41 -0500

    Extract copy to clipboard into util
    MAILPOET-5574

--- mailpoet/assets/js/src/utils.tsx
+++ mailpoet/assets/js/src/utils.tsx
@@ -1,3 +1,34 @@
 export function isInEnum<T>(value: unknown, enumObj: T): value is T[keyof T] {
   return Object.values(enumObj).includes(value as T[keyof T]);
 }
+
+export async function copyToClipboard(
+  id: string,
+  resultCallback: (success: boolean) => void,
+  alwaysSelectText = false,
+) {
+  const element: HTMLTextAreaElement | null = document.querySelector(`#${id}`);
+  if (!element) {
+    resultCallback(false);
+    return;
+  }
+  if (alwaysSelectText) {
+    element.focus();
+    element.select();
+  }
+  if (navigator.clipboard) {
+    const text = element.value;
+    await navigator.clipboard.writeText(text);
+    resultCallback(true);
+    return;
+  }
+
+  // Fallback if navigator.clipboard does not work.
+  element.focus();
+  element.select();
+  if (document.execCommand('copy')) {
+    resultCallback(true);
+    return;
+  }
+  resultCallback(false);
+}
