45107d3734d2b05edb761415adf1dc1b1a803b49
Jan Lysý <jan.lysy@automattic.com>
2023-09-07 10:34:11 +0200

    Add textarea for subject
    [MAILPOET-5562]

--- mailpoet/assets/js/src/email_editor/integrations/MailPoet/components/settings_sidebar.tsx
+++ mailpoet/assets/js/src/email_editor/integrations/MailPoet/components/settings_sidebar.tsx
@@ -1,19 +1,14 @@
-import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
-import { __ } from '@wordpress/i18n';
+import { DetailsPanel } from './details_panel';
 import { EmailTypeInfo } from './email_type_info';
 
+/**
+ * Component for rendering the sidebar in the email editor
+ */
 export function SettingsSidebar() {
-  // Render email settings panels using PluginDocumentSettingPanel component
   return (
     <>
       <EmailTypeInfo />
-      <PluginDocumentSettingPanel
-        className="mailpoet-email-editor-setting-panel"
-        title={__('Details', 'mailpoet')}
-        name="mailpoet-email-editor-setting-panel"
-      >
-        <p>{__('There should be subject and preview text', 'mailpoet')}</p>
-      </PluginDocumentSettingPanel>
+      <DetailsPanel />
     </>
   );
 }
