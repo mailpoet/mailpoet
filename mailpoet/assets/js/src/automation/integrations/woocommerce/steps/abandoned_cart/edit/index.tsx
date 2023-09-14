f8cab8f88809d2df5d58ae8360775905f5b8c8f0
David Remer <webmaster@websupporter.net>
2023-09-14 12:05:16 +0300

    Fix text-domains
    [MAILPOET-5516]

--- mailpoet/assets/js/src/automation/integrations/woocommerce/steps/abandoned_cart/edit/index.tsx
+++ mailpoet/assets/js/src/automation/integrations/woocommerce/steps/abandoned_cart/edit/index.tsx
@@ -11,7 +11,7 @@ export function Edit(): JSX.Element {
 
   return (
     <PanelBody opened>
-      <PlainBodyTitle title={__('Settings', 'mailpoet-premium')} />
+      <PlainBodyTitle title={__('Settings', 'mailpoet')} />
       <SelectControl
         onChange={(id: string) => {
           dispatch(storeName).updateStepArgs(
@@ -22,11 +22,11 @@ export function Edit(): JSX.Element {
         }}
         defaultValue={(selectedStep.args?.wait as string) ?? '30'}
       >
-        <option value="30">{__('30 minutes', 'mailpoet-premium')}</option>
-        <option value="60">{__('1 hour', 'mailpoet-premium')}</option>
-        <option value="240">{__('4 hours', 'mailpoet-premium')}</option>
-        <option value="1440">{__('1 day', 'mailpoet-premium')}</option>
-        <option value="4320">{__('3 days', 'mailpoet-premium')}</option>
+        <option value="30">{__('30 minutes', 'mailpoet')}</option>
+        <option value="60">{__('1 hour', 'mailpoet')}</option>
+        <option value="240">{__('4 hours', 'mailpoet')}</option>
+        <option value="1440">{__('1 day', 'mailpoet')}</option>
+        <option value="4320">{__('3 days', 'mailpoet')}</option>
       </SelectControl>
     </PanelBody>
   );
