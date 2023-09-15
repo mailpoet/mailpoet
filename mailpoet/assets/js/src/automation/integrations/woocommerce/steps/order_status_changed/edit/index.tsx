63e7db963aa844e044bfd1f2ba2e5450114a3873
David Remer <webmaster@websupporter.net>
2023-09-15 08:41:10 +0300

    Prepare <OrderStatusPanel /> to be used by different steps
    [MAILPOET-5516]

--- mailpoet/assets/js/src/automation/integrations/woocommerce/steps/order_status_changed/edit/index.tsx
+++ mailpoet/assets/js/src/automation/integrations/woocommerce/steps/order_status_changed/edit/index.tsx
@@ -1,5 +1,26 @@
+import { __ } from '@wordpress/i18n';
+import { dispatch, useSelect } from '@wordpress/data';
+import { storeName } from '../../../../../editor/store';
 import { OrderStatusPanel } from './order_status_panel';
 
 export function Edit(): JSX.Element {
-  return <OrderStatusPanel />;
+  const { selectedStep } = useSelect(
+    (select) => ({
+      selectedStep: select(storeName).getSelectedStep(),
+    }),
+    [],
+  );
+
+  return (
+    <OrderStatusPanel
+      label={__('Trigger settings', 'mailpoet')}
+      showFrom
+      showTo
+      toLabel={__('Status changes to:', 'mailpoet')}
+      fromLabel={__('Status changes from:', 'mailpoet')}
+      onChange={(status, property) => {
+        dispatch(storeName).updateStepArgs(selectedStep.id, property, status);
+      }}
+    />
+  );
 }
