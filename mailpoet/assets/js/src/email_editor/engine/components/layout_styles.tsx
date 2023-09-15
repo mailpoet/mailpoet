46d8cb38a0ba0292b3fbec6aa60885b06f6f434c
Rostislav Wolny <rosta.wolny@automattic.com>
2023-09-15 14:45:06 +0200

    Apply layout styles defined in StylesController and also to editor
    [MAILPOET-5540]

---
+++ mailpoet/assets/js/src/email_editor/engine/components/layout_styles.tsx
@@ -0,0 +1,25 @@
+import { useSelect } from '@wordpress/data';
+import { store as editorStore } from '@wordpress/editor';
+import { EmailData } from 'email_editor/engine/types';
+
+export function LayoutStyles() {
+  const { emailData } = useSelect((select) => ({
+    emailData:
+      (select(editorStore).getEditedPostAttribute(
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        // The getEditedPostAttribute accepts an attribute but typescript thinks it doesn't
+        'email_data',
+      ) as EmailData) ?? null,
+  }));
+
+  const css = `
+    .edit-post-visual-editor__content-area {
+        max-width: ${emailData.layout_styles.width}px;
+    }
+    .edit-post-visual-editor {
+    background: ${emailData.layout_styles.background};
+  }`;
+
+  return <style>{css}</style>;
+}
---
+++ mailpoet/lib/EmailEditor/Engine/EmailApiController.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\EmailEditor\Engine;
+
+use MailPoet\Validator\Builder;
+
+class EmailApiController {
+  /** @var StylesController */
+  private $stylesController;
+
+  public function __construct(
+    StylesController $stylesController
+  ) {
+    $this->stylesController = $stylesController;
+  }
+
+  /**
+   * @return array - Email specific data such styles.
+   */
+  public function getEmailData(): array {
+    return [
+      'layout_styles' => $this->stylesController->getEmailLayoutStyles(),
+    ];
+  }
+
+  /**
+   * Update Email specific data we store.
+   */
+  public function saveEmailData(array $data, \WP_Post $emailPost): void {
+    // Here comes code saving of Email specific data that will be passed on 'email_data' attribute
+  }
+
+  public function getEmailDataSchema(): array {
+    return Builder::object([
+      'layout_styles' => Builder::object([
+        'width' => Builder::integer(),
+        'background' => Builder::string(),
+      ]),
+    ])->toArray();
+  }
+}
