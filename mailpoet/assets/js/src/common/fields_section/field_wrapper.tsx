cb310b0d1c5304fcd7309ea4d4051e83c3fd9cd6
Rodrigo Primo <rodrigosprimo@gmail.com>
2023-09-20 13:28:54 -0300

    Implement new design for the main elements of the segment edit page
    [MAILPOET-5398]

---
+++ mailpoet/assets/js/src/common/fields_section/field_wrapper.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+  slug: string;
+  title?: string;
+  description?: string;
+};
+
+export function FieldWrapper({ children, slug, title, description }: Props) {
+  return (
+    <div className="mailpoet-admin-field">
+      {title && (
+        <p>
+          <label htmlFor={`field_${slug}`}>{title}</label>
+        </p>
+      )}
+      <div>{children}</div>
+      {description && (
+        <p className="mailpoet-admin-field-description">{description}</p>
+      )}
+    </div>
+  );
+}
