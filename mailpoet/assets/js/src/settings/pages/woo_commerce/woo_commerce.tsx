d6e30ad969643e339d30f03c33cb6a5b6adb9492
Brezo Cordero <8002881+brezocordero@users.noreply.github.com>
2023-09-21 16:30:03 -0500

    Add automations info notice on Woo Settings tab
    This is for Woo Express customers where the automations are hidden when AutomateWoo is enabled.
    
    [MAILPOET-5598]

--- mailpoet/assets/js/src/settings/pages/woo_commerce/woo_commerce.tsx
+++ mailpoet/assets/js/src/settings/pages/woo_commerce/woo_commerce.tsx
@@ -1,15 +1,19 @@
 import { SaveButton } from 'settings/components';
+import { AutomationsInfoNotice } from 'notices/automations_info_notice';
 import { EmailCustomizer } from './email_customizer';
 import { CheckoutOptin } from './checkout_optin';
 import { SubscribeOldCustomers } from './subscribe_old_customers';
 
 export function WooCommerce() {
   return (
-    <div className="mailpoet-settings-grid">
-      <EmailCustomizer />
-      <CheckoutOptin />
-      <SubscribeOldCustomers />
-      <SaveButton />
-    </div>
+    <>
+      <AutomationsInfoNotice />
+      <div className="mailpoet-settings-grid">
+        <EmailCustomizer />
+        <CheckoutOptin />
+        <SubscribeOldCustomers />
+        <SaveButton />
+      </div>
+    </>
   );
 }
