aac4a2acec5069b37281db015629f5e5a544e744
Rostislav Wolny <rosta.wolny@automattic.com>
2023-09-25 13:27:31 +0200

    Add JS build and an index file for the editor app
    [MAILPOET-5603]

---
+++ mailpoet/assets/js/src/email_editor_custom/index.ts
@@ -0,0 +1,3 @@
+import { initialize } from 'email_editor_custom/engine/editor';
+
+initialize('mailpoet-email-editor');
--- mailpoet/lib/AdminPages/Pages/EmailEditor.php
+++ mailpoet/lib/AdminPages/Pages/EmailEditor.php
@@ -3,18 +3,33 @@
 namespace MailPoet\AdminPages\Pages;
 
 use MailPoet\AdminPages\PageRenderer;
+use MailPoet\Config\Env;
+use MailPoet\WP\Functions as WPFunctions;
 
 class EmailEditor {
   /** @var PageRenderer */
   private $pageRenderer;
 
+  /** @var WPFunctions */
+  private $wp;
+
   public function __construct(
-    PageRenderer $pageRenderer
+    PageRenderer $pageRenderer,
+    WPFunctions $wp
   ) {
     $this->pageRenderer = $pageRenderer;
+    $this->wp = $wp;
   }
 
   public function render() {
+    $assetsParams = require_once Env::$assetsPath . '/dist/js/email_editor_custom/email_editor.asset.php';
+    $this->wp->wpEnqueueScript(
+      'mailpoet_email_editor',
+      Env::$assetsUrl . '/dist/js/email_editor_custom/email_editor.js',
+      $assetsParams['dependencies'],
+      $assetsParams['version'],
+      true
+    );
     $this->pageRenderer->displayPage('email_editor.html', []);
   }
 }
