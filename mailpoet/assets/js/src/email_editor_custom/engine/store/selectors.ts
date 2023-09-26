0d18fa3db82e19e636fd823c7042e5eccc8801c9
Rostislav Wolny <rosta.wolny@automattic.com>
2023-09-26 17:04:15 +0200

    Add inserter sidebar
    [MAILPOET-5603]

--- mailpoet/assets/js/src/email_editor_custom/engine/store/selectors.ts
+++ mailpoet/assets/js/src/email_editor_custom/engine/store/selectors.ts
@@ -1,8 +1,13 @@
 import { createRegistrySelector } from '@wordpress/data';
 import { store as interfaceStore } from '@wordpress/interface';
 import { storeName } from './constants';
+import { State } from './types';
 
 export const isSidebarOpened = createRegistrySelector(
   (select) => (): boolean =>
     !!select(interfaceStore).getActiveComplementaryArea(storeName),
 );
+
+export function isInserterSidebarOpened(state: State): boolean {
+  return state.inserterSidebar.isOpened;
+}
