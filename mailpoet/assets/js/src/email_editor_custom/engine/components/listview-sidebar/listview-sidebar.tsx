2b8ee3207486611bab1ffff752b4bcf0a14f92ef
Rostislav Wolny <rosta.wolny@automattic.com>
2023-09-27 10:25:51 +0200

    Fix Inserter and ListView
    They need to be placed inside the block editor provider
    Another option could be wrapping the layout into the block editor provider
    [MAILPOET-5603]

--- mailpoet/assets/js/src/email_editor_custom/engine/components/listview-sidebar/listview-sidebar.tsx
+++ mailpoet/assets/js/src/email_editor_custom/engine/components/listview-sidebar/listview-sidebar.tsx
@@ -1,27 +1,26 @@
-import { useState } from '@wordpress/element';
 import { useDispatch } from '@wordpress/data';
 import { close } from '@wordpress/icons';
-import { Button } from '@wordpress/components';
-import { __experimentalListView as ListView } from '@wordpress/block-editor';
+import { Button, createSlotFill } from '@wordpress/components';
 import { storeName } from '../../store';
+
+const { Slot: ListviewSlot, Fill: ListviewFill } = createSlotFill(
+  'EmailEditorListview',
+);
 
 export function ListviewSidebar() {
   const { toggleListviewSidebar } = useDispatch(storeName);
-
-  const [dropZoneElement, setDropZoneElement] = useState(null);
   return (
     <div className="edit-post-editor__inserter-panel">
       <div className="edit-post-editor__inserter-panel-header">
         <Button icon={close} onClick={toggleListviewSidebar} />
       </div>
       <div className="edit-post-editor__list-view-panel-content">
-        <div
-          className="edit-post-editor__list-view-container"
-          ref={setDropZoneElement}
-        >
-          <ListView dropZoneElement={dropZoneElement} />
+        <div className="edit-post-editor__list-view-container">
+          <ListviewSlot bubblesVirtually />
         </div>
       </div>
     </div>
   );
 }
+
+ListviewSidebar.ListviewFill = ListviewFill;
