cfb2f7aee8a2637d12e52ce3b91e3a64a6a6d9b3
Rostislav Wolny <rosta.wolny@automattic.com>
2023-09-27 13:19:12 +0200

    Add save draft button without notifications
    [MAILPOET-5603]

--- mailpoet/assets/js/src/email_editor_custom/engine/store/actions.ts
+++ mailpoet/assets/js/src/email_editor_custom/engine/store/actions.ts
@@ -1,5 +1,6 @@
 import { dispatch } from '@wordpress/data';
 import { store as interfaceStore } from '@wordpress/interface';
+import { store as coreDataStore } from '@wordpress/core-data';
 import { storeName } from './constants';
 
 export function toggleInserterSidebar() {
@@ -21,3 +22,14 @@ export const openSidebar = () => {
 export const closeSidebar = () => {
   dispatch(interfaceStore).disableComplementaryArea(storeName);
 };
+
+export function* saveEditedEmail() {
+  // This returns a promise
+  yield dispatch(coreDataStore).saveEditedEntityRecord(
+    'postType',
+    'mailpoet_email',
+    75,
+    {},
+  );
+  // Todo Notice when promise is resolved
+}
