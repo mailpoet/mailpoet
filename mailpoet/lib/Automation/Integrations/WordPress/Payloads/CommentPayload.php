913729673794700e539e39a2776b709ea47dd54b
David Remer <webmaster@websupporter.net>
2023-10-03 09:28:52 +0300

    Add CommentSubject and Payload
    [PREMIUM-248]

---
+++ mailpoet/lib/Automation/Integrations/WordPress/Payloads/CommentPayload.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Automation\Integrations\WordPress\Payloads;
+
+use MailPoet\Automation\Engine\Integration\Payload;
+use MailPoet\Automation\Engine\WordPress;
+
+class CommentPayload implements Payload {
+  /** @var int */
+  private $commentId;
+
+  /** @var WordPress */
+  private $wp;
+
+  public function __construct(
+    int $commentId,
+    WordPress $wp
+  ) {
+    $this->commentId = $commentId;
+    $this->wp = $wp;
+  }
+
+  public function getCommentId(): int {
+    return $this->commentId;
+  }
+
+  public function getComment(): ?\WP_Comment {
+    $comment = $this->wp->getComment($this->commentId);
+    return $comment instanceof \WP_Comment ? $comment : null;
+  }
+}
--- mailpoet/lib/Automation/Integrations/WordPress/WordPressIntegration.php
+++ mailpoet/lib/Automation/Integrations/WordPress/WordPressIntegration.php
@@ -3,19 +3,26 @@
 namespace MailPoet\Automation\Integrations\WordPress;
 
 use MailPoet\Automation\Engine\Registry;
+use MailPoet\Automation\Integrations\WordPress\Subjects\CommentSubject;
 use MailPoet\Automation\Integrations\WordPress\Subjects\UserSubject;
 
 class WordPressIntegration {
   /** @var UserSubject */
   private $userSubject;
 
+  /** @var CommentSubject */
+  private $commentSubject;
+
   public function __construct(
-    UserSubject $userSubject
+    UserSubject $userSubject,
+    CommentSubject $commentSubject
   ) {
     $this->userSubject = $userSubject;
+    $this->commentSubject = $commentSubject;
   }
 
   public function register(Registry $registry): void {
     $registry->addSubject($this->userSubject);
+    $registry->addSubject($this->commentSubject);
   }
 }
