b6b08602fa0277ed6061822bc4660461a8d8b28c
Rodrigo Primo <rodrigosprimo@gmail.com>
2023-10-04 09:57:39 -0300

    Fix layout of the number of reviews segment in the segment edit page
    See https://github.com/mailpoet/mailpoet/pull/5174#issuecomment-1746542729
    
    [MAILPOET-5398]

--- mailpoet/assets/js/src/segments/dynamic/dynamic-segments-filters/fields/woocommerce/number-of-reviews.tsx
+++ mailpoet/assets/js/src/segments/dynamic/dynamic-segments-filters/fields/woocommerce/number-of-reviews.tsx
@@ -54,64 +54,65 @@ export function NumberOfReviewsFields({
 
   return (
     <>
-      <Grid.CenteredRow>
-        <Select
-          key="rating-select"
-          value={segment.rating}
-          onChange={(e): void => {
-            void updateSegmentFilterFromEvent('rating', filterIndex, e);
-          }}
-        >
-          <option value={ReviewRating.ANY}>
-            {MailPoet.I18n.t('wooAnyStarRating')}
-          </option>
-          <option value={ReviewRating.ONE}>
-            {MailPoet.I18n.t('wooOneStarRating')}
-          </option>
-          <option value={ReviewRating.TWO}>
-            {MailPoet.I18n.t('wooTwoStarRating')}
-          </option>
-          <option value={ReviewRating.THREE}>
-            {MailPoet.I18n.t('wooThreeStarRating')}
-          </option>
-          <option value={ReviewRating.FOUR}>
-            {MailPoet.I18n.t('wooFourStarRating')}
-          </option>
-          <option value={ReviewRating.FIVE}>
-            {MailPoet.I18n.t('wooFiveStarRating')}
-          </option>
-        </Select>
-        <Select
-          key="select"
-          value={segment.count_type}
-          onChange={(e): void => {
-            void updateSegmentFilterFromEvent('count_type', filterIndex, e);
-          }}
-          automationId="select-number-of-reviews-type"
-        >
-          <option value={CountType.EQUALS}>{MailPoet.I18n.t('equals')}</option>
-          <option value={CountType.NOT_EQUALS}>
-            {MailPoet.I18n.t('notEquals')}
-          </option>
-          <option value={CountType.MORE_THAN}>
-            {MailPoet.I18n.t('moreThan')}
-          </option>
-          <option value={CountType.LESS_THAN}>
-            {MailPoet.I18n.t('lessThan')}
-          </option>
-        </Select>
-        <Input
-          data-automation-id="input-number-of-reviews-count"
-          type="number"
-          min={0}
-          value={segment.count || ''}
-          placeholder={MailPoet.I18n.t('wooNumberOfOrdersCount')}
-          onChange={(e): void => {
-            void updateSegmentFilterFromEvent('count', filterIndex, e);
-          }}
-        />
-        <div>{MailPoet.I18n.t('wooNumberOfReviewsReviews')}</div>
-      </Grid.CenteredRow>
+      <Select
+        key="rating-select"
+        value={segment.rating}
+        isMinWidth
+        onChange={(e): void => {
+          void updateSegmentFilterFromEvent('rating', filterIndex, e);
+        }}
+      >
+        <option value={ReviewRating.ANY}>
+          {MailPoet.I18n.t('wooAnyStarRating')}
+        </option>
+        <option value={ReviewRating.ONE}>
+          {MailPoet.I18n.t('wooOneStarRating')}
+        </option>
+        <option value={ReviewRating.TWO}>
+          {MailPoet.I18n.t('wooTwoStarRating')}
+        </option>
+        <option value={ReviewRating.THREE}>
+          {MailPoet.I18n.t('wooThreeStarRating')}
+        </option>
+        <option value={ReviewRating.FOUR}>
+          {MailPoet.I18n.t('wooFourStarRating')}
+        </option>
+        <option value={ReviewRating.FIVE}>
+          {MailPoet.I18n.t('wooFiveStarRating')}
+        </option>
+      </Select>
+      <Select
+        key="select"
+        value={segment.count_type}
+        isMinWidth
+        onChange={(e): void => {
+          void updateSegmentFilterFromEvent('count_type', filterIndex, e);
+        }}
+        automationId="select-number-of-reviews-type"
+      >
+        <option value={CountType.EQUALS}>{MailPoet.I18n.t('equals')}</option>
+        <option value={CountType.NOT_EQUALS}>
+          {MailPoet.I18n.t('notEquals')}
+        </option>
+        <option value={CountType.MORE_THAN}>
+          {MailPoet.I18n.t('moreThan')}
+        </option>
+        <option value={CountType.LESS_THAN}>
+          {MailPoet.I18n.t('lessThan')}
+        </option>
+      </Select>
+      <Input
+        className="mailpoet-segments-input-small"
+        data-automation-id="input-number-of-reviews-count"
+        type="number"
+        min={0}
+        value={segment.count || ''}
+        placeholder={MailPoet.I18n.t('wooNumberOfOrdersCount')}
+        onChange={(e): void => {
+          void updateSegmentFilterFromEvent('count', filterIndex, e);
+        }}
+      />
+      <div>{MailPoet.I18n.t('wooNumberOfReviewsReviews')}</div>
       <Grid.CenteredRow>
         <DaysPeriodField filterIndex={filterIndex} />
       </Grid.CenteredRow>
