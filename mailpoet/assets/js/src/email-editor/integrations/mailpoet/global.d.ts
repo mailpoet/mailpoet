eace3140092505f5fd183dab6614d44b2bd1e02a
Jan Lysý <jan.lysy@automattic.com>
2023-10-10 10:11:42 +0200

    Use wpLocalizeScript instead of wpAddInlineScript in Email editor
    [MAILPOET-5563]

--- mailpoet/assets/js/src/email-editor/integrations/mailpoet/global.d.ts
+++ mailpoet/assets/js/src/email-editor/integrations/mailpoet/global.d.ts
@@ -1,6 +1,8 @@
 interface Window {
-  mailpoet_json_api_root: string;
-  mailpoet_token: string;
-  mailpoet_api_version: string;
-  mailpoet_current_wp_user_email: string;
+  MailPoetEmailEditor: {
+    json_api_root: string;
+    api_token: string;
+    api_version: string;
+    current_wp_user_email: string;
+  };
 }
