0113138069d6093e9ce25cedc760f507d746fcd1
Rostislav Wolny <rosta.wolny@automattic.com>
2023-10-11 13:55:34 +0200

    Unify the appearance of inserter and list view sidebars with the post editor
    - fix the width of the list view panel
    - remove close buttons from the panels
    
    [MAILPOET-5637]

--- mailpoet/assets/js/src/email-editor-custom/engine/components/listview-sidebar/listview-sidebar.tsx
+++ mailpoet/assets/js/src/email-editor-custom/engine/components/listview-sidebar/listview-sidebar.tsx
@@ -1,19 +1,12 @@
-import { useDispatch } from '@wordpress/data';
-import { close } from '@wordpress/icons';
-import { Button, createSlotFill } from '@wordpress/components';
-import { storeName } from '../../store';
+import { createSlotFill } from '@wordpress/components';
 
 const { Slot: ListviewSlot, Fill: ListviewFill } = createSlotFill(
   'EmailEditorListview',
 );
 
 export function ListviewSidebar() {
-  const { toggleListviewSidebar } = useDispatch(storeName);
   return (
-    <div className="edit-post-editor__inserter-panel">
-      <div className="edit-post-editor__inserter-panel-header">
-        <Button icon={close} onClick={toggleListviewSidebar} />
-      </div>
+    <div className="edit-post-editor__inserter-panel edit-post-editor__document-overview-panel">
       <div className="edit-post-editor__list-view-panel-content">
         <div className="edit-post-editor__list-view-container">
           <ListviewSlot bubblesVirtually />
