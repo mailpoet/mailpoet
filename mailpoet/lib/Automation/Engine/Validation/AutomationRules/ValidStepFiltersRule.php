52e047fbedf698f87172cba058ad47c82d64261a
Jan Jakes <jan.jakes@automattic.com>
2023-10-12 16:24:24 +0200

    Validate that fields in filters are provided by subjects available in the automation
    [MAILPOET-5586]

--- mailpoet/lib/Automation/Engine/Validation/AutomationRules/ValidStepFiltersRule.php
+++ mailpoet/lib/Automation/Engine/Validation/AutomationRules/ValidStepFiltersRule.php
@@ -2,7 +2,11 @@
 
 namespace MailPoet\Automation\Engine\Validation\AutomationRules;
 
+use MailPoet\Automation\Engine\Control\SubjectTransformerHandler;
 use MailPoet\Automation\Engine\Data\Automation;
+use MailPoet\Automation\Engine\Data\Filter;
+use MailPoet\Automation\Engine\Integration\Payload;
+use MailPoet\Automation\Engine\Integration\Subject;
 use MailPoet\Automation\Engine\Registry;
 use MailPoet\Automation\Engine\Validation\AutomationGraph\AutomationNode;
 use MailPoet\Automation\Engine\Validation\AutomationGraph\AutomationNodeVisitor;
@@ -13,14 +17,19 @@ class ValidStepFiltersRule implements AutomationNodeVisitor {
   /** @var Registry */
   private $registry;
 
+  /** @var SubjectTransformerHandler */
+  private $subjectTransformerHandler;
+
   /** @var Validator */
   private $validator;
 
   public function __construct(
     Registry $registry,
+    SubjectTransformerHandler $subjectTransformerHandler,
     Validator $validator
   ) {
     $this->registry = $registry;
+    $this->subjectTransformerHandler = $subjectTransformerHandler;
     $this->validator = $validator;
   }
 
@@ -41,6 +50,17 @@ class ValidStepFiltersRule implements AutomationNodeVisitor {
           $this->validator->validate($registryFilter->getArgsSchema($filter->getCondition()), $filter->getArgs());
         } catch (ValidationException $e) {
           $errors[$filter->getId()] = $e->getWpError()->get_error_code();
+          continue;
+        }
+
+        // ensure that the field is available with the provided subjects
+        $subjectKeys = $this->subjectTransformerHandler->getSubjectKeysForAutomation($automation);
+        $filterSubject = $this->getFilterSubject($filter);
+        if (!$filterSubject) {
+          $errors[$filter->getId()] = __('Field not found', 'mailpoet');
+        } elseif (!in_array($filterSubject->getKey(), $subjectKeys, true)) {
+          // translators: %s is the name of a subject (data structure) that provides the field
+          $errors[$filter->getId()] = sprintf(__('A trigger that provides %s is required', 'mailpoet'), $filterSubject->getName());
         }
       }
     }
@@ -55,5 +75,17 @@ class ValidStepFiltersRule implements AutomationNodeVisitor {
   }
 
   public function complete(Automation $automation): void {
+  }
+
+  /** @return Subject<Payload> */
+  private function getFilterSubject(Filter $filter): ?Subject {
+    foreach ($this->registry->getSubjects() as $subject) {
+      foreach ($subject->getFields() as $field) {
+        if ($field->getKey() === $filter->getFieldKey()) {
+          return $subject;
+        }
+      }
+    }
+    return null;
   }
 }
