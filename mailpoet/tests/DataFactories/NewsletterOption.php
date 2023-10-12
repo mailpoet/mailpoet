b54b66b5b2b52816c0bd846d863c3da49abd7ab8
John Oleksowicz <john.oleksowicz@automattic.com>
2023-10-12 10:20:28 -0500

    Add NewsletterOptionField factory
    MAILPOET-5161

--- mailpoet/tests/DataFactories/NewsletterOption.php
+++ mailpoet/tests/DataFactories/NewsletterOption.php
@@ -5,19 +5,13 @@ namespace MailPoet\Test\DataFactories;
 use MailPoet\DI\ContainerWrapper;
 use MailPoet\Entities\NewsletterEntity;
 use MailPoet\Entities\NewsletterOptionEntity;
-use MailPoet\Entities\NewsletterOptionFieldEntity;
-use MailPoet\Newsletter\Options\NewsletterOptionFieldsRepository;
 use MailPoet\Newsletter\Options\NewsletterOptionsRepository;
 
 class NewsletterOption {
-  /** @var NewsletterOptionFieldsRepository */
-  private $newsletterOptionFieldsRepository;
-
   /** @var NewsletterOptionsRepository */
   private $newsletterOptionsRepository;
 
   public function __construct() {
-    $this->newsletterOptionFieldsRepository = ContainerWrapper::getInstance()->get(NewsletterOptionFieldsRepository::class);
     $this->newsletterOptionsRepository = ContainerWrapper::getInstance()->get(NewsletterOptionsRepository::class);
   }
 
@@ -31,16 +25,7 @@ class NewsletterOption {
    * @param string|int $optionValue
    */
   public function create(NewsletterEntity $newsletter, string $optionName, $optionValue): NewsletterOptionEntity {
-    $newsletterOptionField = $this->newsletterOptionFieldsRepository->findOneBy([
-      'name' => $optionName,
-      'newsletterType' => $newsletter->getType(),
-    ]);
-    if (!$newsletterOptionField) {
-      $newsletterOptionField = new NewsletterOptionFieldEntity();
-      $newsletterOptionField->setNewsletterType($newsletter->getType());
-      $newsletterOptionField->setName($optionName);
-      $this->newsletterOptionFieldsRepository->persist($newsletterOptionField);
-    }
+    $newsletterOptionField = (new NewsletterOptionField())->findOrCreate($optionName, $newsletter->getType());
 
     $newsletterOption = $this->newsletterOptionsRepository->findOneBy([
       'newsletter' => $newsletter,
