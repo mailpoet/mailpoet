e0916b23ea39bdd2bc4d846caafee15b8bc6a790
John Oleksowicz <john.oleksowicz@automattic.com>
2023-10-13 13:58:54 -0500

    Extract pending approval components
    MAILPOET-5631

--- mailpoet/assets/js/src/common/premium-key/messages.tsx
+++ mailpoet/assets/js/src/common/premium-key/messages.tsx
@@ -1,6 +1,5 @@
 import { MouseEvent } from 'react';
 import { __ } from '@wordpress/i18n';
-import { createInterpolateElement } from '@wordpress/element';
 import {
   KeyActivationState,
   MssStatus,
@@ -13,6 +12,11 @@ import {
   ServiceUnavailableMessage,
   AccessRestrictedMessages,
 } from './key-messages';
+import {
+  ClickToRefresh,
+  PendingApprovalBody,
+  PendingApprovalTitle,
+} from '../pending-approval-notice';
 
 export function Messages(
   state: KeyActivationState,
@@ -66,70 +70,14 @@ export function Messages(
       {showPendingApprovalNotice && (
         <div>
           <div className="pending_approval_heading mailpoet_error">
-            {createInterpolateElement(
-              __(
-                'MailPoet is <link>reviewing your subscription</link>.',
-                'mailpoet',
-              ),
-              {
-                link: (
-                  // eslint-disable-next-line jsx-a11y/anchor-has-content, jsx-a11y/control-has-associated-label
-                  <a
-                    href="https://kb.mailpoet.com/article/379-our-approval-process"
-                    target="_blank"
-                    rel="noreferrer"
-                  />
-                ),
-              },
-            )}
+            <PendingApprovalTitle />
           </div>
           <p>
-            {createInterpolateElement(
-              __(
-                `You can use all MailPoet features and send <link1>email previews</link1> to your <link2>authorized email addresses</link2>, but sending to your email list contacts is temporarily paused until we review your subscription. If you don't hear from us within 48 hours, please check the inbox and spam folders of your MailPoet account email for follow-up emails with the subject "<emailSubject/>" and reply, or <link3>contact us</link3>.`,
-                'mailpoet',
-              ),
-              {
-                link1: (
-                  // eslint-disable-next-line jsx-a11y/anchor-has-content, jsx-a11y/control-has-associated-label
-                  <a
-                    href="https://kb.mailpoet.com/article/290-check-your-newsletter-before-sending-it"
-                    target="_blank"
-                    rel="noreferrer"
-                  />
-                ),
-                link2: (
-                  // eslint-disable-next-line jsx-a11y/anchor-has-content, jsx-a11y/control-has-associated-label
-                  <a
-                    href="https://kb.mailpoet.com/article/266-how-to-add-an-authorized-email-address-as-the-from-address#how-to-authorize-an-email-address"
-                    target="_blank"
-                    rel="noreferrer"
-                  />
-                ),
-                link3: (
-                  // eslint-disable-next-line jsx-a11y/anchor-has-content, jsx-a11y/control-has-associated-label
-                  <a
-                    href="https://www.mailpoet.com/support/"
-                    target="_blank"
-                    rel="noreferrer"
-                  />
-                ),
-                emailSubject: <>Your MailPoet Subscription Review</>,
-              },
-            )}
+            <PendingApprovalBody />
           </p>
           {showRefreshMessage ? (
             <p>
-              {createInterpolateElement(
-                __(
-                  `If you have already received approval email, click <link>here</link> to update the status.`,
-                  'mailpoet',
-                ),
-                {
-                  // eslint-disable-next-line jsx-a11y/anchor-has-content, jsx-a11y/control-has-associated-label
-                  link: <a onClick={onRefreshClick} href="#" />,
-                },
-              )}
+              <ClickToRefresh onRefreshClick={onRefreshClick} />
             </p>
           ) : null}
         </div>
