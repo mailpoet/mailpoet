0c6c6ca023de8c35fe8353325a71a1667348ea04
Rodrigo Primo <rodrigosprimo@gmail.com>
2023-10-16 12:40:23 -0300

    Add acceptance test to check welcome wizard when a MSS key is set
    [MAILPOET-5547]

--- mailpoet/tests/acceptance/WelcomeWizard/WelcomeWizardCest.php
+++ mailpoet/tests/acceptance/WelcomeWizard/WelcomeWizardCest.php
@@ -14,6 +14,11 @@ class WelcomeWizardCest {
    */
   private $settingsRepository;
 
+  /**
+   * @var SettingsFactory
+   */
+  private $settingsFactory;
+
   public function _before(\AcceptanceTester $i) {
     // for some unknown reason the SettingsController doesn't return the expected values here so the code uses the SettingsRepository
     $this->settingsRepository = ContainerWrapper::getInstance()->get(SettingsRepository::class);
@@ -21,10 +26,8 @@ class WelcomeWizardCest {
     $i->login();
     $i->activateWooCommerce();
 
-    $settingsFactory = new SettingsFactory();
-    $settingsFactory->withWelcomeWizard();
-
-    $i->amOnMailpoetPage('Welcome-Wizard');
+    $this->settingsFactory = new SettingsFactory();
+    $this->settingsFactory->withWelcomeWizard();
   }
 
   public function welcomeWizardMainFlow(\AcceptanceTester $i, Scenario $scenario) {
@@ -37,6 +40,7 @@ class WelcomeWizardCest {
     $senderAddress = 'wp@example.com';
 
     $i->wantTo('Check user can go through the welcome wizard');
+    $i->amOnMailpoetPage('Welcome-Wizard');
 
     // First step of the wizard
     $i->fillField('senderName', $senderName);
@@ -64,6 +68,47 @@ class WelcomeWizardCest {
     $i->fillField('#mailpoet_premium_key', $mailPoetSendingKey);
     $i->click('.mailpoet-verify-key-button');
     $i->waitForText('MailPoet account connected', 20);
+    $i->click('.mailpoet-wizard-continue-button');
+
+    // wizard finished and the user was redirect to the home page
+    $i->waitForText('Welcome to MailPoet', 10, '.mailpoet-homepage__container');
+
+    Assert::assertSame($senderName, $this->settingsRepository->findOneByName('sender')->getValue()['name']);
+    Assert::assertSame($senderAddress, $this->settingsRepository->findOneByName('sender')->getValue()['address']);
+    Assert::assertSame(['enabled' => '1'], $this->settingsRepository->findOneByName('3rd_party_libs')->getValue());
+    Assert::assertSame(['enabled' => '1'], $this->settingsRepository->findOneByName('analytics')->getValue());
+    Assert::assertSame(['level' => 'full'], $this->settingsRepository->findOneByName('tracking')->getValue());
+    Assert::assertSame($mailPoetSendingKey, $this->settingsRepository->findOneByName('mta')->getValue()['mailpoet_api_key']);
+  }
+
+  public function welcomeWizardShouldSkipMssStepIfKeyAlreadyExists(\AcceptanceTester $i, Scenario $scenario) {
+    $mailPoetSendingKey = getenv('WP_TEST_MAILER_MAILPOET_API');
+    if (!$mailPoetSendingKey) {
+      $scenario->skip("Skipping, 'WP_TEST_MAILER_MAILPOET_API' not set.");
+    }
+
+    $senderName = 'WP Admin';
+    $senderAddress = 'wp@example.com';
+
+    $this->settingsFactory->withSendingMethodMailPoet();
+
+    $i->wantTo('Check welcome wizard skips MSS step when a MSS key already exists');
+    $i->amOnMailpoetPage('Welcome-Wizard');
+
+    // First step of the wizard
+    $i->fillField('senderName', $senderName);
+    $i->fillField('senderAddress', $senderAddress);
+    $i->click('.mailpoet-wizard-continue-button');
+
+    // Second step of the wizard
+    $i->waitForText('Confirm privacy and data settings');
+    $i->click('#mailpoet-wizard-3rd-party-libs .mailpoet-form-yesno-yes');
+    $i->click('#mailpoet-wizard-tracking .mailpoet-form-yesno-yes');
+    $i->click('.mailpoet-wizard-continue-button');
+
+    // Third step of the wizard
+    $i->waitForText('Power up your WooCommerce store');
+    $i->click('.mailpoet-form-yesno-yes');
     $i->click('.mailpoet-wizard-continue-button');
 
     // wizard finished and the user was redirect to the home page
