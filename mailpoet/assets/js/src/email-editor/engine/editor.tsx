ac075c197e6635af6b6158649446264aabea6e4e
Jan Lysý <jan.lysy@automattic.com>
2023-10-17 16:50:58 +0200

    Add filter for disabling nested columns
    [MAILPOET-5591]

--- mailpoet/assets/js/src/email-editor/engine/editor.tsx
+++ mailpoet/assets/js/src/email-editor/engine/editor.tsx
@@ -5,6 +5,7 @@ import { registerCoreBlocks } from '@wordpress/block-library';
 import { Popover, SlotFillProvider } from '@wordpress/components';
 import { ShortcutProvider } from '@wordpress/keyboard-shortcuts';
 import { EntityProvider } from '@wordpress/core-data';
+import { Hooks } from '../../hooks';
 import { BlockEditor } from './components/block-editor';
 import { createStore, storeName } from './store';
 import { initHooks } from './hooks';
@@ -33,12 +34,41 @@ function Editor() {
   );
 }
 
+/**
+ * Disable nesting columns inside columns by using WP hooks
+ */
+function disableNestedColumns() {
+  Hooks.addFilter(
+    'blocks.registerBlockType',
+    'mailpoet-email-editor/change-columns-allowed-nesting',
+    (settings, name) => {
+      if (name === 'core/column') {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return {
+          ...settings,
+          attributes: {
+            ...settings.attributes,
+            allowedBlocks: {
+              type: 'array',
+              default: ['core/paragraph', 'core/heading'],
+            },
+          },
+        };
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return settings;
+    },
+  );
+}
+
 export function initialize(elementId: string) {
   const container = document.getElementById(elementId);
   if (!container) {
     return;
   }
   createStore();
+  disableNestedColumns();
   registerCoreBlocks();
   initHooks();
   const root = createRoot(container);
