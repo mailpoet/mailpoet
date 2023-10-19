40fde04af58ec82d081f5eb341c427dc1a385607
Jan Jakes <jan.jakes@automattic.com>
2023-10-19 18:02:40 +0200

    Implement new automation scrolling with simultaneous X and Y scroll support
    This fixes horizontal scroll bar appearing only after scrolling till the end vertically.
    Allows for simultaneous X and Y axis scrolling on trackpads and touch devices.
    
    [MAILPOET-5587]

---
+++ mailpoet/assets/js/src/automation/editor/components/automation/use-automation-scroll.ts
@@ -0,0 +1,32 @@
+import { MutableRefObject, useEffect } from 'react';
+
+// Handle automation scrolling, including both X and Y axes simultaneously.
+// Use animation frames to sync with browser repaints for smooth scrolling.
+export const useAutomationScroll = (
+  automationRef: MutableRefObject<HTMLDivElement>,
+): void =>
+  useEffect(() => {
+    const automation = automationRef.current;
+    if (!automation) {
+      return undefined;
+    }
+
+    let frameId: number;
+    const scrollHandler = (event) => {
+      event.preventDefault();
+      frameId = requestAnimationFrame(() => {
+        automation.scrollLeft += event.deltaX;
+        automation.scrollTop += event.deltaY;
+      });
+    };
+
+    automation.addEventListener('wheel', scrollHandler, { passive: false });
+
+    // cleanup
+    return () => {
+      automation.removeEventListener('wheel', scrollHandler);
+      if (frameId) {
+        cancelAnimationFrame(frameId);
+      }
+    };
+  }, [automationRef]);
