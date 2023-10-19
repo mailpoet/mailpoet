10d28da84cff6351856bd493e5f78b2f3918de67
Jan Jakes <jan.jakes@automattic.com>
2023-10-19 18:37:27 +0200

    Implement drag-to-scroll for automation flow
    [MAILPOET-5667]

---
+++ mailpoet/assets/js/src/automation/editor/components/automation/use-automation-drag-to-scroll.ts
@@ -0,0 +1,62 @@
+import { MutableRefObject, useEffect, useRef } from 'react';
+
+// Handle automation dragging to move the flow in a drag-to-scroll fashion.
+// Use animation frames to sync with browser repaints for smooth scrolling.
+// This handles mouse & trackpad. On touch devices, the drag runs natively.
+export const useAutomationDragToScroll = (
+  automationRef: MutableRefObject<HTMLDivElement>,
+): void => {
+  // Using a ref here is crucial to avoid re-rendering the component tree
+  // on every mouse move event. The event handlers must not modify state.
+  const dragInfo = useRef({ isDragging: false, lastX: 0, lastY: 0 });
+
+  useEffect(() => {
+    const automation = automationRef.current;
+    if (!automation) {
+      return undefined;
+    }
+
+    automation.style.cursor = 'grab';
+
+    let frameId: number;
+    const onMouseMove = (event) => {
+      if (!dragInfo.current.isDragging) {
+        return;
+      }
+
+      event.preventDefault();
+      frameId = requestAnimationFrame(() => {
+        automation.scrollLeft += dragInfo.current.lastX - event.clientX;
+        automation.scrollTop += dragInfo.current.lastY - event.clientY;
+        dragInfo.current.lastX = event.clientX;
+        dragInfo.current.lastY = event.clientY;
+      });
+    };
+
+    const onMouseDown = (event) => {
+      dragInfo.current.isDragging = true;
+      dragInfo.current.lastX = event.clientX;
+      dragInfo.current.lastY = event.clientY;
+      automation.style.cursor = 'grabbing';
+    };
+
+    const onMouseUp = () => {
+      dragInfo.current.isDragging = false;
+      automation.style.cursor = 'grab';
+    };
+
+    automation.addEventListener('mousemove', onMouseMove, { passive: false });
+    automation.addEventListener('mousedown', onMouseDown);
+    window.addEventListener('mouseup', onMouseUp);
+
+    // cleanup
+    return () => {
+      automation.removeEventListener('mousemove', onMouseMove);
+      automation.removeEventListener('mousedown', onMouseDown);
+      window.removeEventListener('mouseup', onMouseUp);
+      if (frameId) {
+        cancelAnimationFrame(frameId);
+      }
+    };
+  }, [automationRef]);
+};
