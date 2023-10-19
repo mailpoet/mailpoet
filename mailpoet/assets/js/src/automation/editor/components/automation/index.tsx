412fc16fdad3bfb1456574fddc4c60ef2442a6ea
Jan Jakes <jan.jakes@automattic.com>
2023-10-19 18:10:12 +0200

    Extract automation scroll centering to a custom hook
    [MAILPOET-5587]

--- mailpoet/assets/js/src/automation/editor/components/automation/index.tsx
+++ mailpoet/assets/js/src/automation/editor/components/automation/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useMemo, useRef } from 'react';
 import {
   __unstableComposite as Composite,
   __unstableUseCompositeState as useCompositeState,
@@ -12,6 +12,7 @@ import { storeName } from '../../store';
 import { Statistics } from './statistics';
 import { Flow } from './flow';
 import { useAutomationScroll } from './use-automation-scroll';
+import { useAutomationScrollCenter } from './use-automation-scroll-center';
 
 type AutomationProps = {
   context: 'edit' | 'view';
@@ -31,27 +32,10 @@ export function Automation({ context }: AutomationProps): JSX.Element {
     shift: true,
   });
 
-  // We need to trigger one more render to center the scroll, because the sidebar
-  // is using a React Portal, and it is not in the layout after the first render.
-  const [rendered, setRendered] = useState(false);
-
-  useEffect(() => {
-    // first render
-    if (!rendered) {
-      setRendered(true);
-      return;
-    }
-
-    // center the scroll to the first step
-    const firstStep = document.querySelector('[data-step-id] > *');
-    if (firstStep instanceof HTMLElement) {
-      firstStep.scrollIntoView({ block: 'nearest', inline: 'center' });
-    }
-  }, [rendered]);
-
-  // handle automation scrolling
+  // handle automation scrolling and horizontal scroll centering
   const automationRef = useRef<HTMLDivElement>();
   useAutomationScroll(automationRef);
+  useAutomationScrollCenter(automationRef);
 
   if (!automationData) {
     return <EmptyAutomation />;
---
+++ mailpoet/assets/js/src/automation/editor/components/automation/use-automation-scroll-center.ts
@@ -0,0 +1,24 @@
+import { MutableRefObject, useEffect, useState } from 'react';
+
+export const useAutomationScrollCenter = (
+  automationRef: MutableRefObject<HTMLDivElement>,
+) => {
+  // We need to trigger one more render to center the scroll, because the sidebar
+  // is using a React Portal, and it is not in the layout after the first render.
+  const [rendered, setRendered] = useState(false);
+
+  useEffect(() => {
+    // first render
+    if (!rendered) {
+      setRendered(true);
+      return;
+    }
+
+    // center the scroll to the first step
+    const automation = automationRef.current;
+    const firstStep = automation?.querySelector('[data-step-id] > *');
+    if (firstStep instanceof HTMLElement) {
+      firstStep.scrollIntoView({ block: 'nearest', inline: 'center' });
+    }
+  }, [automationRef, rendered]);
+};
