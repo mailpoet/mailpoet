3235bd704f5334ea7256c89ba7171e0b5a3a8acb
Jan Jakes <jan.jakes@automattic.com>
2023-10-20 10:46:11 +0200

    Improve naming of filter strings
    [MAILPOET-5587]

--- mailpoet/assets/js/src/automation/integrations/core/steps/if-else/strings.tsx
+++ mailpoet/assets/js/src/automation/integrations/core/steps/if-else/strings.tsx
@@ -4,17 +4,17 @@ import { FilterStrings } from '../../../../editor/components/filters';
 export const strings: FilterStrings = {
   title: __('Conditions', 'mailpoet'),
   // translators: %d is the number of conditions that are set up
-  label: __('Conditions: %d', 'mailpoet'),
+  countLabel: __('Conditions: %d', 'mailpoet'),
   addFilter: __('Add condition', 'mailpoet'),
   groupDescription: __(
     'The automation would go through the yes flow if the following conditions are met:',
     'mailpoet',
   ),
-  andDescription: __(
+  andGroupDescription: __(
     'The automation would go through the yes flow if <operator>all of</operator> the following conditions are met:',
     'mailpoet',
   ),
-  orDescription: __(
+  orGroupDescription: __(
     'The automation would go through the yes flow if <operator>any of</operator> the following conditions are met:',
     'mailpoet',
   ),
