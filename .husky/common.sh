ec6d26162b3ae0527b3b7389f63a13daa65b12c8
John Oleksowicz <john.oleksowicz@automattic.com>
2023-10-25 14:45:41 -0500

    Add hooks to install updates automatically
    MAILPOET-4237

---
+++ .husky/common.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+fileChanged() {
+  local filePattern="$1"
+  local changedFiles="$2"
+  if echo "$changedFiles" | grep -qE "$filePattern"; then
+      return 0
+  else
+      return 1
+  fi
+}
+
+installIfUpdates() {
+  local changedFiles="$(git diff-tree -r --name-only --no-commit-id HEAD@{1} HEAD)"
+
+  if fileChanged "pnpm-lock.yaml" "$changedFiles"; then
+    echo "Change detected in pnpm-lock.yaml, running do install:js"
+    pushd mailpoet
+    ./do install:js
+    popd
+  fi
+
+  if fileChanged "mailpoet/composer.lock" "$changedFiles"; then
+    echo "Change detected in mailpoet/composer.lock, running do install:php"
+    pushd mailpoet
+    ./do install:php
+    popd
+  fi
+}
