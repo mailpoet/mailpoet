54b1cff74249f04e3224fadd72eeea01c170872c
David Remer <webmaster@websupporter.net>
2023-10-26 09:06:59 +0300

    Move OrderStatusChangeTrigger into own namespace
    [MAILPOET-5661]

--- mailpoet/lib/Automation/Integrations/WooCommerce/Triggers/Orders/OrderStatusChangedTrigger.php
+++ mailpoet/lib/Automation/Integrations/WooCommerce/Triggers/Orders/OrderStatusChangedTrigger.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace MailPoet\Automation\Integrations\WooCommerce\Triggers;
+namespace MailPoet\Automation\Integrations\WooCommerce\Triggers\Orders;
 
 use MailPoet\Automation\Engine\Data\StepRunArgs;
 use MailPoet\Automation\Engine\Data\StepValidationArgs;
