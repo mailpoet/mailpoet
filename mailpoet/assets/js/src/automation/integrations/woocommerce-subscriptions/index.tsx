7dba2a0ee2230f014e673172fe184238d77606c9
David Remer <webmaster@websupporter.net>
2023-11-01 08:27:06 +0200

    Add subscription-status-changed stub
    [PREMIUM-256]

--- mailpoet/assets/js/src/automation/integrations/woocommerce-subscriptions/index.tsx
+++ mailpoet/assets/js/src/automation/integrations/woocommerce-subscriptions/index.tsx
@@ -1,8 +1,11 @@
 import { MailPoet } from '../../../mailpoet';
+import { step as SubscriptionStatusChanged } from './steps/subscription-status-changed';
+import { registerStepType } from '../../editor/store';
 
 export const initialize = (): void => {
   if (!MailPoet.isWoocommerceSubscriptionsActive) {
     return;
   }
+  registerStepType(SubscriptionStatusChanged);
   // Insert new steps here
 };
