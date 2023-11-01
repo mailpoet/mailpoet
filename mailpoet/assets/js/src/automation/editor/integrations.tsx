0fef69cda7f8b899f079c75b1eed917c871d3e2f
David Remer <webmaster@websupporter.net>
2023-11-01 08:14:17 +0200

    Add WooCommerce Subscriptions integration
    [PREMIUM-256]

--- mailpoet/assets/js/src/automation/editor/integrations.tsx
+++ mailpoet/assets/js/src/automation/editor/integrations.tsx
@@ -2,10 +2,12 @@ import { initialize as initializeCoreIntegration } from '../integrations/core';
 import { initialize as initializeMailPoetIntegration } from '../integrations/mailpoet';
 import { initialize as initializeWordPressIntegration } from '../integrations/wordpress';
 import { initialize as initializeWooCommerceIntegration } from '../integrations/woocommerce';
+import { initialize as initializeWooCommerceSubscriptionsIntegration } from '../integrations/woocommerce-subscriptions';
 
 export function initializeIntegrations() {
   initializeCoreIntegration();
   initializeMailPoetIntegration();
   initializeWordPressIntegration();
   initializeWooCommerceIntegration();
+  initializeWooCommerceSubscriptionsIntegration();
 }
