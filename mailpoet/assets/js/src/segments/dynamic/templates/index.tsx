c6177299eabb0d8e40fb14cda1478cf01b49898f
Jan Jakes <jan.jakes@automattic.com>
2023-11-02 11:54:02 +0100

    Extract header to page-header component, fix spacing as per design
    [MAILPOET-4676]

--- mailpoet/assets/js/src/segments/dynamic/templates/index.tsx
+++ mailpoet/assets/js/src/segments/dynamic/templates/index.tsx
@@ -1,13 +1,5 @@
 import { __ } from '@wordpress/i18n';
-import { chevronLeft } from '@wordpress/icons';
-import {
-  Button,
-  Flex,
-  FlexItem,
-  FlexBlock,
-  SearchControl,
-  TabPanel,
-} from '@wordpress/components';
+import { Button, SearchControl, TabPanel } from '@wordpress/components';
 import { Badge } from '@woocommerce/components';
 import { HideScreenOptions } from 'common/hide-screen-options/hide-screen-options';
 import { TopBarWithBeamer } from 'common/top-bar/top-bar';
@@ -21,6 +13,7 @@ import { useSelect } from '@wordpress/data';
 import { storeName } from 'segments/dynamic/store';
 import { APIErrorsNotice } from 'notices/api-errors-notice';
 import { MailPoet } from 'mailpoet';
+import { BackButton, PageHeader } from '../../../common/page-header';
 import { Footer, Grid } from '../../../common/templates';
 
 const tabs = [
@@ -65,40 +58,27 @@ export function SegmentTemplates(): JSX.Element {
     <div className="mailpoet-main-container">
       <HideScreenOptions />
       <TopBarWithBeamer />
-      <Flex
-        className="mailpoet-heading"
-        direction={['column', 'row'] as any} // eslint-disable-line @typescript-eslint/no-explicit-any -- typed as string but supports string[] and this is needed to make the component responsive
-        gap="16px"
+      <PageHeader
+        className="mailpoet-templates-header"
+        heading={__('Start with a pre-built segment', 'mailpoet')}
+        headingPrefix={
+          <BackButton
+            href="#/"
+            label={__('Segments list', 'mailpoet')}
+            aria-label={__('Navigate to the segments list page', 'mailpoet')}
+          />
+        }
       >
-        <FlexBlock>
-          <h1 className="wp-heading-inline">
-            <Button
-              icon={chevronLeft}
-              aria-label={__('Navigate to the segments list page', 'mailpoet')}
-              href="#/"
-              label={__('Segments list', 'mailpoet')}
-            />
-            {__('Start with a pre-built segment', 'mailpoet')}
-          </h1>
-        </FlexBlock>
-
-        <FlexItem>
-          <SearchControl label={__('Search segment templates', 'mailpoet')} />
-        </FlexItem>
-
-        <FlexItem>
-          <Button
-            variant="secondary"
-            href={`#${ROUTES.NEW_DYNAMIC_SEGMENT}`}
-            data-automation-id="new-custom-segment"
-            onClick={(): void => {
-              trackNewCustomSegment();
-            }}
-          >
-            {__('Create custom segment', 'mailpoet')}
-          </Button>
-        </FlexItem>
-      </Flex>
+        <SearchControl label={__('Search segment templates', 'mailpoet')} />
+        <Button
+          variant="secondary"
+          href={`#${ROUTES.NEW_DYNAMIC_SEGMENT}`}
+          data-automation-id="new-custom-segment"
+          onClick={() => void trackNewCustomSegment()}
+        >
+          {__('Create custom segment', 'mailpoet')}
+        </Button>
+      </PageHeader>
 
       {errors.length > 0 && (
         <APIErrorsNotice errors={errors.map((error) => ({ message: error }))} />
