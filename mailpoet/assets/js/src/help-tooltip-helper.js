7cfe079553f5ec9b69bc697162a4dad179f18909
John Oleksowicz <john.oleksowicz@automattic.com>
2023-11-03 15:18:01 -0500

    Update help tooltip to use createRoot
    MAILPOET-5120

--- mailpoet/assets/js/src/help-tooltip-helper.js
+++ mailpoet/assets/js/src/help-tooltip-helper.js
@@ -1,16 +1,16 @@
 import { Tooltip } from 'help-tooltip.jsx';
 import { createElement } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 export const MailPoetHelpTooltip = {
   show: function show(domContainerNode, opts) {
-    ReactDOM.render(
+    const root = createRoot(domContainerNode);
+    root.render(
       createElement(Tooltip, {
         tooltip: opts.tooltip,
         tooltipId: opts.tooltipId,
         place: opts.place,
       }),
-      domContainerNode,
     );
   },
 };
