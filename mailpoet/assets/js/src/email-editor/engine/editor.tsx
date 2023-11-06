97f49b9c7ba6edff71116e4c1a2d6dd4af27a8af
Jan Lysý <jan.lysy@automattic.com>
2023-11-06 17:54:54 +0100

    Refactor blocks initialization
    [MAILPOET-5591]

--- mailpoet/assets/js/src/email-editor/engine/editor.tsx
+++ mailpoet/assets/js/src/email-editor/engine/editor.tsx
@@ -1,11 +1,10 @@
 import '@wordpress/format-library'; // Enables text formatting capabilities
 import { useSelect } from '@wordpress/data';
 import { StrictMode, createRoot } from '@wordpress/element';
-import { registerCoreBlocks } from '@wordpress/block-library';
 import { Popover, SlotFillProvider } from '@wordpress/components';
 import { ShortcutProvider } from '@wordpress/keyboard-shortcuts';
 import { EntityProvider } from '@wordpress/core-data';
-import { addFilter } from '@wordpress/hooks';
+import { initBlocks } from './blocks';
 import { BlockEditor } from './components/block-editor';
 import { createStore, storeName } from './store';
 import { initHooks } from './hooks';
@@ -34,42 +33,13 @@ function Editor() {
   );
 }
 
-/**
- * Disable nesting columns inside columns by using WP hooks
- */
-function disableNestedColumns() {
-  addFilter(
-    'blocks.registerBlockType',
-    'mailpoet-email-editor/change-columns-allowed-nesting',
-    (settings, name) => {
-      if (name === 'core/column') {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        return {
-          ...settings,
-          attributes: {
-            ...settings.attributes,
-            allowedBlocks: {
-              type: 'array',
-              default: ['core/paragraph', 'core/heading'],
-            },
-          },
-        };
-      }
-
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return settings;
-    },
-  );
-}
-
 export function initialize(elementId: string) {
   const container = document.getElementById(elementId);
   if (!container) {
     return;
   }
   createStore();
-  disableNestedColumns();
-  registerCoreBlocks();
+  initBlocks();
   initHooks();
   const root = createRoot(container);
   root.render(<Editor />);
