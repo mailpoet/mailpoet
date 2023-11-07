103fd665d2bd0bbdfb1b41d4cae8b9ba63eafa69
Jan Lysý <jan.lysy@automattic.com>
2023-11-07 12:02:28 +0100

    Get allowed blocks for core/column from editor config
    [MAILPOET-5591]

--- mailpoet/assets/js/src/email-editor/engine/blocks/core/column.ts
+++ mailpoet/assets/js/src/email-editor/engine/blocks/core/column.ts
@@ -1,4 +1,6 @@
 import { addFilter } from '@wordpress/hooks';
+import { select } from '@wordpress/data';
+import { storeName } from '../../store';
 
 /**
  * Disable nesting columns inside columns by using WP hooks
@@ -9,6 +11,22 @@ function disableNestedColumns() {
     'mailpoet-email-editor/change-columns-allowed-nesting',
     (settings, name) => {
       if (name === 'core/column') {
+        // Filter out core/column and core/columns from supported blocks configured in the editor settings
+        const editorSettings = select(storeName).getInitialEditorSettings();
+        const blockTypes =
+          editorSettings.allowedBlockTypes instanceof Array
+            ? editorSettings.allowedBlockTypes
+            : [];
+        const allowedBlockTypes = [];
+        blockTypes.forEach((allowedBlockType) => {
+          if (
+            allowedBlockType !== 'core/column' &&
+            allowedBlockType !== 'core/columns'
+          ) {
+            allowedBlockTypes.push(allowedBlockType);
+          }
+        });
+
         // eslint-disable-next-line @typescript-eslint/no-unsafe-return
         return {
           ...settings,
@@ -16,7 +34,7 @@ function disableNestedColumns() {
             ...settings.attributes,
             allowedBlocks: {
               type: 'array',
-              default: ['core/paragraph', 'core/heading'],
+              default: allowedBlockTypes,
             },
           },
         };
