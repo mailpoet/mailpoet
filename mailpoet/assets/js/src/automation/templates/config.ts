4c2b4013f1fd84ec3b01294f81ff6309643c08ce
Jan Jakes <jan.jakes@automattic.com>
2023-11-09 11:56:59 +0100

    Add tabs with categories to automation templates
    [MAILPOET-4676]

--- mailpoet/assets/js/src/automation/templates/config.ts
+++ mailpoet/assets/js/src/automation/templates/config.ts
@@ -6,10 +6,19 @@ export type AutomationTemplate = {
   type: 'default' | 'free-only' | 'premium' | 'coming-soon';
 };
 
+export type AutomationTemplateCategory = {
+  slug: string;
+  name: string;
+};
+
 declare global {
   interface Window {
     mailpoet_automation_templates: AutomationTemplate[];
+    mailpoet_automation_template_categories: AutomationTemplateCategory[];
   }
 }
 
 export const automationTemplates = window.mailpoet_automation_templates;
+
+export const automationTemplateCategories =
+  window.mailpoet_automation_template_categories;
--- mailpoet/assets/js/src/automation/templates/index.tsx
+++ mailpoet/assets/js/src/automation/templates/index.tsx
@@ -1,14 +1,38 @@
 import { createRoot } from 'react-dom/client';
 import { __ } from '@wordpress/i18n';
 import { registerTranslations } from 'common';
-import { automationTemplates } from './config';
+import { automationTemplateCategories, automationTemplates } from './config';
 import { TemplateListItem } from './components/template-list-item';
 import { initializeApi } from '../api';
 import { TopBarWithBeamer } from '../../common/top-bar/top-bar';
 import { FromScratchButton } from './components/from-scratch';
 import { BackButton, PageHeader } from '../../common/page-header';
 import { MailPoet } from '../../mailpoet';
-import { Footer } from '../../common/templates';
+import { Footer, Grid, TabPanel, TabTitle } from '../../common/templates';
+
+const tabs = [
+  {
+    name: 'all',
+    title: (
+      <TabTitle
+        title={__('All', 'mailpoet')}
+        count={automationTemplates.length}
+      />
+    ),
+  },
+  ...automationTemplateCategories
+    .map((category) => ({
+      ...category,
+      count: automationTemplates.filter(
+        (template) => template.category === category.slug,
+      ).length,
+    }))
+    .filter(({ count }) => count > 0)
+    .map(({ name, slug, count }) => ({
+      name: slug,
+      title: <TabTitle title={name} count={count} />,
+    })),
+];
 
 function Templates(): JSX.Element {
   return (
@@ -26,11 +50,20 @@ function Templates(): JSX.Element {
         <FromScratchButton />
       </PageHeader>
 
-      <ul className="mailpoet-automation-templates">
-        {automationTemplates.map((template) => (
-          <TemplateListItem key={template.slug} template={template} />
-        ))}
-      </ul>
+      <TabPanel tabs={tabs}>
+        {(tab) => (
+          <Grid>
+            {automationTemplates
+              .filter(
+                (template) =>
+                  tab.name === 'all' || template.category === tab.name,
+              )
+              .map((template) => (
+                <TemplateListItem key={template.slug} template={template} />
+              ))}
+          </Grid>
+        )}
+      </TabPanel>
 
       <Footer>
         <p>{__('Can’t find what you’re looking for?', 'mailpoet')}</p>
