31e2d45e3f88ebb26e96e2bc07c915c156af5257
Jan Jakes <jan.jakes@automattic.com>
2023-11-09 13:42:56 +0100

    Make template card badge colors configurable
    [MAILPOET-4676]

--- mailpoet/assets/js/src/common/templates/item-badge.tsx
+++ mailpoet/assets/js/src/common/templates/item-badge.tsx
@@ -7,27 +7,39 @@ type Props = {
   type: 'essential' | 'coming-soon' | 'premium';
 };
 
-const getBadgeData = (
-  type: Props['type'],
-): { tooltip: string; icon: JSX.Element; color: string } => {
+type BadgeData = {
+  tooltip: string;
+  icon: JSX.Element;
+  foregroundColor: string;
+  backgroundColor: string;
+  borderColor: string;
+};
+
+const getBadgeData = (type: Props['type']): BadgeData | undefined => {
   switch (type) {
     case 'essential':
       return {
         tooltip: __('Essential', 'mailpoet'),
         icon: starFilled,
-        color: '#007cba',
+        foregroundColor: '#fff',
+        backgroundColor: '#007cba',
+        borderColor: '#007cba',
       };
     case 'coming-soon':
       return {
         tooltip: __('Coming soon', 'mailpoet'),
         icon: comingSoon,
-        color: '#949494',
+        foregroundColor: '#fff',
+        backgroundColor: '#949494',
+        borderColor: '#949494',
       };
     case 'premium':
       return {
         tooltip: __('Premium', 'mailpoet'),
         icon: lock,
-        color: '#FF8A40',
+        foregroundColor: '#BD8600',
+        backgroundColor: '#FCF9E8',
+        borderColor: '#F5E6AB',
       };
     default:
       return undefined;
@@ -35,14 +47,16 @@ const getBadgeData = (
 };
 
 export function ItemBadge({ type }: Props): JSX.Element {
-  const { tooltip, icon, color } = getBadgeData(type);
+  const { tooltip, icon, foregroundColor, backgroundColor, borderColor } =
+    getBadgeData(type);
+
   return (
     <Tooltip text={tooltip}>
       <div
         className="mailpoet-templates-badge"
-        style={{ backgroundColor: color }}
+        style={{ backgroundColor, borderColor }}
       >
-        <Icon icon={icon} size={18} />
+        <Icon icon={icon} size={18} style={{ fill: foregroundColor }} />
       </div>
     </Tooltip>
   );
