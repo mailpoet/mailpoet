c7b1587b0e23c9887e0c715e9570a2bc433f76f5
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2023-11-10 13:57:52 +0400

    Add heading block support to email editor
    MAILPOET-5643

--- mailpoet/lib/EmailEditor/Integrations/Core/Renderer/Blocks/Heading.php
+++ mailpoet/lib/EmailEditor/Integrations/Core/Renderer/Blocks/Heading.php
@@ -7,6 +7,38 @@ use MailPoet\EmailEditor\Engine\SettingsController;
 
 class Heading implements BlockRenderer {
   public function render($blockContent, array $parsedBlock, SettingsController $settingsController): string {
-    return $blockContent;
+    $contentStyles = $settingsController->getEmailContentStyles();
+    return str_replace('{heading_content}', $blockContent, $this->prepareColumnTemplate($parsedBlock, $contentStyles));
+  }
+
+  /**
+   * Based on MJML <mj-text>
+   */
+  private function prepareColumnTemplate(array $parsedBlock, array $contentStyles): string {
+    $styles = [];
+    foreach ($parsedBlock['email_attrs'] ?? [] as $property => $value) {
+      $styles[$property] = $value;
+    }
+
+    if (!isset($styles['font-size'])) {
+      $styles['font-size'] = $contentStyles['typography']['fontSize'];
+    }
+    if (!isset($styles['font-family'])) {
+      $styles['font-family'] = $contentStyles['typography']['fontFamily'];
+    }
+
+    return '
+      <div style="' . $this->convertStylesToString($styles) . '">
+        {heading_content}
+      </div>
+    ';
+  }
+
+  private function convertStylesToString(array $styles): string {
+    $cssString = '';
+    foreach ($styles as $property => $value) {
+      $cssString .= $property . ':' . $value . '; ';
+    }
+    return trim($cssString); // Remove trailing space and return the formatted string
   }
 }
