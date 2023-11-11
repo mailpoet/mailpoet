6ba8c7b6a565548b7932476937c897efb20f839d
Jan Lysý <jan.lysy@automattic.com>
2023-11-11 16:48:39 +0400

    Deactivate the option Stack on mobile for columns block
    [MAILPOET-5694]

--- mailpoet/assets/js/src/email-editor/engine/blocks/index.ts
+++ mailpoet/assets/js/src/email-editor/engine/blocks/index.ts
@@ -1,7 +1,9 @@
 import { registerCoreBlocks } from '@wordpress/block-library';
 import { disableNestedColumns } from './core/column';
+import { deactivateStackOnMobile } from './core/columns';
 
 export function initBlocks() {
   disableNestedColumns();
+  deactivateStackOnMobile();
   registerCoreBlocks();
 }
