4dd273b429eaaa238c8b0e4a1dbde6f344a1c4f2
Rostislav Wolny <rosta.wolny@automattic.com>
2023-11-16 15:24:59 +0100

    Add reduced flex layout for buttons block
    [MAILPOET-5644]

--- mailpoet/assets/js/src/email-editor/engine/blocks/core/button.ts
+++ mailpoet/assets/js/src/email-editor/engine/blocks/core/button.ts
@@ -12,6 +12,17 @@ function enhanceButtonBlock() {
       if (name === 'core/button') {
         return { ...settings, styles: [] };
       }
+
+      if (name === 'core/buttons') {
+        return {
+          ...settings,
+          supports: {
+            ...settings.supports,
+            layout: false, // disable block editor's layouts
+            __experimentalEmailFlexLayout: true, // enable MailPoet's reduced flex email layout
+          },
+        };
+      }
       return settings;
     },
   );
