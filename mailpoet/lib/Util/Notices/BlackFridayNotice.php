f53ad4a8b02e57815ed2ed8233a570de742de42d
 Ján Mikláš <neosinner@gmail.com>
2023-11-16 12:49:08 +0100

    Update BFCM sale dates and copy
    [MAILPOET-5707]

--- mailpoet/lib/Util/Notices/BlackFridayNotice.php
+++ mailpoet/lib/Util/Notices/BlackFridayNotice.php
@@ -10,6 +10,8 @@ class BlackFridayNotice {
 
   const OPTION_NAME = 'dismissed-black-friday-notice';
   const DISMISS_NOTICE_TIMEOUT_SECONDS = 2592000; // 30 days
+  const DATE_FROM = '2023-11-22 14:00:00 UTC';
+  const DATE_TO = '2023-11-28 14:00:00 UTC';
 
   /** @var SubscribersRepository */
   private $subscribersRepository;
@@ -22,8 +24,8 @@ class BlackFridayNotice {
 
   public function init($shouldDisplay) {
     $shouldDisplay = $shouldDisplay
-      && (time() >= strtotime('2023-06-14 14:00:00 UTC'))
-      && (time() <= strtotime('2023-06-19 14:00:00 UTC'))
+      && (time() >= strtotime(self::DATE_FROM))
+      && (time() <= strtotime(self::DATE_TO))
       && !get_transient(self::OPTION_NAME);
     if ($shouldDisplay) {
       $this->display();
@@ -32,8 +34,8 @@ class BlackFridayNotice {
 
   private function display() {
     $subscribers = $this->subscribersRepository->countBy(['deletedAt' => null]);
-    $header = '<h3 class="mailpoet-h3">' . __('Get 40% off all MailPoet annual plans and upgrades – no coupon required.', 'mailpoet') . '</h3>';
-    $body = '<h5 class="mailpoet-h5">' . __('Offer ends at 2 pm UTC on Monday, June 19, 2023. Terms and conditions apply.', 'mailpoet') . '</h5>';
+    $header = '<h3 class="mailpoet-h3">' . __('Get up to 40% off all MailPoet annual plans and upgrades', 'mailpoet') . '</h3>';
+    $body = '<h5 class="mailpoet-h5">' . __('Our Black Friday sale is live. Save up to 40% when you switch to or upgrade an annual plan — no coupon needed. Offer ends at 2 PM UTC, 28 November.', 'mailpoet') . '</h5>';
     $link = "<p><a href='https://account.mailpoet.com/?s=$subscribers&billing=yearly&ref=sale-june-2023-plugin&utm_source=MP&utm_medium=plugin&utm_campaign=sale_june_2023' class='mailpoet-button button-primary' target='_blank'>"
       // translators: a button on a sale banner. "Save" meaning to save money - 40% discount
       . __('Pick a plan and save', 'mailpoet')
