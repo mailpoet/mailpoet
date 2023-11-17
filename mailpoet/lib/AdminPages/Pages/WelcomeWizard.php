2a714511c2093257c21ea7c614db40c6c66a4b75
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2023-11-17 11:36:55 +0100

    Add tracking for users arriving to the wizard from WooCommerce Marketing Dashboard
     Users can click on the Finish setup button on the WooCommerce Multichannel Marketing Dashboard which would redirect them to the MailPoet welcome wizard.
    
     We want to track users that arrive to the MP wizard from
    WooCommerce but at this point tracking is not enabled. So we store the
    information in a setting, and send the tracking event to Mixpanel, if it
    is enabled, after the user completes the wizard.
    
    MAILPOET-5695

--- mailpoet/lib/AdminPages/Pages/WelcomeWizard.php
+++ mailpoet/lib/AdminPages/Pages/WelcomeWizard.php
@@ -11,6 +11,7 @@ use MailPoet\WP\Functions as WPFunctions;
 
 class WelcomeWizard {
   const TRACK_LOADDED_VIA_WOOCOMMERCE_SETTING_NAME = 'send_event_that_wizard_was_loaded_via_woocommerce';
+  const TRACK_LOADDED_VIA_WOOCOMMERCE_MARKETING_DASHBOARD_SETTING_NAME = 'wizard_loaded_via_woocommerce_marketing_dashboard';
 
   /** @var PageRenderer */
   private $pageRenderer;
@@ -49,6 +50,13 @@ class WelcomeWizard {
     if (!$loadedViaWooCommerce && isset($_GET['mailpoet_wizard_loaded_via_woocommerce'])) {
       // This setting is used to send an event to Mixpanel in another request as, before completing the wizard, Mixpanel is not enabled.
       $this->settings->set(WelcomeWizard::TRACK_LOADDED_VIA_WOOCOMMERCE_SETTING_NAME, 1);
+    }
+
+    $loadedViaWooCommerceMarketingDashboard = $this->settings->get(WelcomeWizard::TRACK_LOADDED_VIA_WOOCOMMERCE_MARKETING_DASHBOARD_SETTING_NAME, false);
+
+    if (!$loadedViaWooCommerceMarketingDashboard && isset($_GET['mailpoet_wizard_loaded_via_woocommerce_marketing_dashboard'])) {
+      // This setting is used to send an event to Mixpanel in another request as, before completing the wizard, Mixpanel is not enabled.
+      $this->settings->set(WelcomeWizard::TRACK_LOADDED_VIA_WOOCOMMERCE_MARKETING_DASHBOARD_SETTING_NAME, 1);
     }
 
 
