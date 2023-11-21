9122737523be2d75685a95f8f5b14c0e8b605cd3
Jan Lysý <jan.lysy@automattic.com>
2023-11-21 16:55:42 +0100

    Add core/image filter deactivation
    [MAILPOET-5705]

--- mailpoet/assets/js/src/email-editor/engine/blocks/index.ts
+++ mailpoet/assets/js/src/email-editor/engine/blocks/index.ts
@@ -1,13 +1,14 @@
 import { registerCoreBlocks } from '@wordpress/block-library';
 import { disableNestedColumns } from './core/column';
 import { disableColumnsLayout, deactivateStackOnMobile } from './core/columns';
-import { hideExpandOnClick } from './core/image';
+import { disableImageFilter, hideExpandOnClick } from './core/image';
 import { disableCertainRichTextFormats } from './core/rich-text';
 
 export function initBlocks() {
   disableNestedColumns();
   deactivateStackOnMobile();
   hideExpandOnClick();
+  disableImageFilter();
   disableCertainRichTextFormats();
   disableColumnsLayout();
   registerCoreBlocks();
