685af1c1978acf213fb31d06704af9dc84b12142
Jan Jakes <jan.jakes@automattic.com>
2023-11-22 16:16:28 +0100

    Extract template detail dialog to templates grid
    [MAILPOET-4534]

--- mailpoet/assets/js/src/automation/templates/components/templates-grid.tsx
+++ mailpoet/assets/js/src/automation/templates/components/templates-grid.tsx
@@ -1,17 +1,34 @@
+import { useState } from 'react';
 import { AutomationTemplate } from '../config';
 import { Grid } from '../../../common/templates';
 import { TemplateListItem } from './template-list-item';
+import { TemplateDetail } from './template-detail';
 
 type Props = {
   templates: AutomationTemplate[];
 };
 
 export function TemplatesGrid({ templates }: Props): JSX.Element {
+  const [selectedTemplate, setSelectedTemplate] =
+    useState<AutomationTemplate>();
+
   return (
-    <Grid>
-      {templates.map((template) => (
-        <TemplateListItem key={template.slug} template={template} />
-      ))}
-    </Grid>
+    <>
+      {selectedTemplate && (
+        <TemplateDetail
+          template={selectedTemplate}
+          onRequestClose={() => setSelectedTemplate(undefined)}
+        />
+      )}
+      <Grid>
+        {templates.map((template) => (
+          <TemplateListItem
+            key={template.slug}
+            template={template}
+            onSelect={() => setSelectedTemplate(template)}
+          />
+        ))}
+      </Grid>
+    </>
   );
 }
