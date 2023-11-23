c9cb2fe6f4af490a1ccb3d0ef1829d41857464c0
Jan Lysý <jan.lysy@automattic.com>
2023-11-23 10:46:00 +0100

    Add image block integration test
    [MAILPOET-5705]

---
+++ mailpoet/tests/integration/EmailEditor/Integration/Core/Renderer/Blocks/ImageTest.php
@@ -0,0 +1,100 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\EmailEditor\Integrations\Core\Renderer\Blocks;
+
+use MailPoet\EmailEditor\Engine\EmailEditor;
+use MailPoet\EmailEditor\Engine\SettingsController;
+
+class ImageTest extends \MailPoetTest {
+  /** @var Image */
+  private $imageRenderer;
+
+  private $imageContent = '
+    <figure class="wp-block-image alignleft size-full is-style-default">
+        <img src="https://test.com/wp-content/uploads/2023/05/image.jpg" alt="" style=""/>
+    </figure>
+  ';
+
+  /** @var array */
+  private $parsedImage = [
+    'blockName' => 'core/image',
+    'attrs' => [
+      'align' => 'left',
+      'id' => 1,
+      'scale' => 'cover',
+      'sizeSlug' => 'full',
+      'linkDestination' => 'none',
+      'className' => 'is-style-default',
+    ],
+    'email_attrs' => [
+      'width' => '640px',
+    ],
+    'innerBlocks' => [],
+    'innerHTML' => '',
+    'innerContent' => [],
+  ];
+
+  /** @var SettingsController */
+  private $settingsController;
+
+  public function _before() {
+    $this->diContainer->get(EmailEditor::class)->initialize();
+    $this->imageRenderer = new Image();
+    $this->settingsController = $this->diContainer->get(SettingsController::class);
+  }
+
+  public function testItRendersMandatoryImageStyles(): void {
+    $parsedImage = $this->parsedImage;
+    $parsedImage['innerHTML'] = $this->imageContent; // To avoid repetition of the image content in the test we need to add it to the parsed block
+
+    $rendered = $this->imageRenderer->render($this->imageContent, $parsedImage, $this->settingsController);
+    $this->assertStringNotContainsString('<figure', $rendered);
+    $this->assertStringNotContainsString('<figcaption', $rendered);
+    $this->assertStringNotContainsString('</figure>', $rendered);
+    $this->assertStringNotContainsString('</figcaption>', $rendered);
+    $this->assertStringContainsString('width="640"', $rendered);
+    $this->assertStringContainsString('width:640px;', $rendered);
+    $this->assertStringContainsString('<img ', $rendered);
+  }
+
+  public function testItRendersBorderRadiusStyle(): void {
+    $parsedImage = $this->parsedImage;
+    $parsedImage['attrs']['className'] = 'is-style-rounded';
+    $parsedImage['innerHTML'] = $this->imageContent; // To avoid repetition of the image content in the test we need to add it to the parsed block
+
+    $rendered = $this->imageRenderer->render($this->imageContent, $parsedImage, $this->settingsController);
+    $this->assertStringNotContainsString('<figure', $rendered);
+    $this->assertStringNotContainsString('<figcaption', $rendered);
+    $this->assertStringNotContainsString('</figure>', $rendered);
+    $this->assertStringNotContainsString('</figcaption>', $rendered);
+    $this->assertStringContainsString('width="640"', $rendered);
+    $this->assertStringContainsString('width:640px;', $rendered);
+    $this->assertStringContainsString('<img ', $rendered);
+    $this->assertStringContainsString('border-radius: 100%;', $rendered);
+  }
+
+  public function testItRendersCaption(): void {
+    $imageContent = str_replace('</figure>', '<figcaption class="wp-element-caption">Caption</figcaption></figure>', $this->imageContent);
+    $parsedImage = $this->parsedImage;
+    $parsedImage['innerHTML'] = $imageContent; // To avoid repetition of the image content in the test we need to add it to the parsed block
+
+    $rendered = $this->imageRenderer->render($imageContent, $parsedImage, $this->settingsController);
+    $this->assertStringContainsString('>Caption</div>', $rendered);
+    $this->assertStringContainsString('text-align:center;', $rendered);
+  }
+
+  public function testItRendersImageAlignment(): void {
+    $imageContent = str_replace('style=""', 'style="width:400px;height:300px;"', $this->imageContent);
+    $parsedImage = $this->parsedImage;
+    $parsedImage['attrs']['align'] = 'center';
+    $parsedImage['email_attrs']['width'] = '400px';
+    $parsedImage['innerHTML'] = $imageContent; // To avoid repetition of the image content in the test we need to add it to the parsed block
+
+    $rendered = $this->imageRenderer->render($imageContent, $parsedImage, $this->settingsController);
+    $this->assertStringContainsString('align="center"', $rendered);
+    $this->assertStringContainsString('width="400"', $rendered);
+    $this->assertStringContainsString('height="300"', $rendered);
+    $this->assertStringContainsString('height:300px;', $rendered);
+    $this->assertStringContainsString('width:400px;', $rendered);
+  }
+}
