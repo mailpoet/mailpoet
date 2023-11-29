3f30a3ebbbd33b27a4fd1d6c84a3f5bb37380db1
Rodrigo Primo <rodrigosprimo@gmail.com>
2023-11-29 10:36:50 -0300

    Change return type as the method always returns a segment
    [MAILPOET-5752]

--- mailpoet/lib/Segments/SegmentsRepository.php
+++ mailpoet/lib/Segments/SegmentsRepository.php
@@ -69,7 +69,7 @@ class SegmentsRepository extends Repository {
       ->getResult();
   }
 
-  public function getWPUsersSegment(): ?SegmentEntity {
+  public function getWPUsersSegment(): SegmentEntity {
     $segment = $this->findOneBy(['type' => SegmentEntity::TYPE_WP_USERS]);
 
     if (!$segment) {
