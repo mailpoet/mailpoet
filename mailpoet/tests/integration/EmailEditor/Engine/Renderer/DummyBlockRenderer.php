c5ad2405aa3cab5c7bc279cf70aeb873af818283
Rostislav Wolny <rosta.wolny@automattic.com>
2023-12-01 10:59:04 +0100

    Add Flex Renderer test
    [MAILPOET-5644]

--- mailpoet/tests/integration/EmailEditor/Engine/Renderer/DummyBlockRenderer.php
+++ mailpoet/tests/integration/EmailEditor/Engine/Renderer/DummyBlockRenderer.php
@@ -6,10 +6,6 @@ use MailPoet\EmailEditor\Engine\SettingsController;
 
 class DummyBlockRenderer implements BlockRenderer {
   public function render(string $blockContent, array $parsedBlock, SettingsController $settingsController): string {
-    if (!isset($parsedBlock['innerBlocks']) || empty($parsedBlock['innerBlocks'])) {
-      return $parsedBlock['innerHTML'];
-    }
-    // Wrapper is rendered in parent Columns block because it needs to operate with columns count etc.
-    return '[' . $this->render('', $parsedBlock['innerBlocks'], $settingsController) . ']';
+    return $parsedBlock['innerHtml'];
   }
 }
