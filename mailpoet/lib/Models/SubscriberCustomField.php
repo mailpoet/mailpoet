51cf8e0bd203b331fb59516e0f42ae645476acd2
Rodrigo Primo <rodrigosprimo@gmail.com>
2023-12-01 09:54:59 -0300

    Deprecate SubscriberCustomField model as it is not used anymore
    [MAILPOET-5763]

--- mailpoet/lib/Models/SubscriberCustomField.php
+++ mailpoet/lib/Models/SubscriberCustomField.php
@@ -8,11 +8,18 @@ use MailPoet\Util\Helpers;
  * @property int $subscriberId
  * @property int $customFieldId
  * @property string $value
+ *
+ * @deprecated This model is deprecated. Use \MailPoet\Subscribers\SubscriberCustomFieldRepository
+ * and \MailPoet\Entities\SubscriberCustomFieldEntity instead. This class can be removed after 2024-05-30.
  */
 class SubscriberCustomField extends Model {
   public static $_table = MP_SUBSCRIBER_CUSTOM_FIELD_TABLE; // phpcs:ignore PSR2.Classes.PropertyDeclaration
 
+  /**
+   * @deprecated
+   */
   public static function createOrUpdate($data = []) {
+    self::deprecationError(__METHOD__);
     $customField = CustomField::findOne($data['custom_field_id']);
     if ($customField instanceof CustomField) {
       $customField = $customField->asArray();
@@ -47,7 +54,11 @@ class SubscriberCustomField extends Model {
     ]);
   }
 
+  /**
+   * @deprecated
+   */
   public static function createMultiple($values) {
+    self::deprecationError(__METHOD__);
     return self::rawExecute(
       'INSERT IGNORE INTO `' . self::$_table . '` ' .
       '(custom_field_id, subscriber_id, value) ' .
@@ -61,7 +72,11 @@ class SubscriberCustomField extends Model {
     );
   }
 
+  /**
+   * @deprecated
+   */
   public static function updateMultiple($values) {
+    self::deprecationError(__METHOD__);
     $subscriberIds = array_unique(array_column($values, 1));
     $query = sprintf(
       "UPDATE `%s` SET value = (CASE %s ELSE value END) WHERE subscriber_id IN (%s)",
@@ -75,15 +90,46 @@ class SubscriberCustomField extends Model {
     );
   }
 
+  /**
+   * @deprecated
+   */
   public static function deleteSubscriberRelations($subscriber) {
+    self::deprecationError(__METHOD__);
     if ($subscriber === false) return false;
     $relations = self::where('subscriber_id', $subscriber->id);
     return $relations->deleteMany();
   }
 
+  /**
+   * @deprecated
+   */
   public static function deleteManySubscriberRelations(array $subscriberIds) {
+    self::deprecationError(__METHOD__);
     if (empty($subscriberIds)) return false;
     $relations = self::whereIn('subscriber_id', $subscriberIds);
     return $relations->deleteMany();
+  }
+
+  /**
+   * @deprecated This is here for displaying the deprecation warning for properties.
+   */
+  public function __get($key) {
+    self::deprecationError('property "' . $key . '"');
+    return parent::__get($key);
+  }
+
+  /**
+   * @deprecated This is here for displaying the deprecation warning for static calls.
+   */
+  public static function __callStatic($name, $arguments) {
+    self::deprecationError($name);
+    return parent::__callStatic($name, $arguments);
+  }
+
+  private static function deprecationError($methodName) {
+    trigger_error(
+      'Calling ' . esc_html($methodName) . ' is deprecated and will be removed. Use \MailPoet\Subscribers\SubscriberCustomFieldRepository and \MailPoet\Entities\SubscriberCustomFieldEntity instead.',
+      E_USER_DEPRECATED
+    );
   }
 }
