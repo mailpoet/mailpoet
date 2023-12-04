126fe621cb5e59d9ce0852241edee582a5239155
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2023-12-04 09:14:24 +0100

    Add split button component to Automation statistics page
    MAILPOET-5700

--- mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/header/index.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/header/index.tsx
@@ -1,7 +1,14 @@
-import { Button } from '@wordpress/components';
+import {
+  Button,
+  ButtonGroup,
+  Dropdown,
+  MenuGroup,
+  MenuItem,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { addQueryArgs } from '@wordpress/url';
 import { useSelect } from '@wordpress/data';
+import { chevronDown, Icon } from '@wordpress/icons';
 import { Filter } from './filter';
 import { MailPoet } from '../../../../../../mailpoet';
 import { storeName as editorStoreName } from '../../../../../editor/store/constants';
@@ -13,14 +20,36 @@ export function Header(): JSX.Element {
   return (
     <header className="mailpoet-analytics-header">
       <Filter />
-      <Button
-        href={addQueryArgs(MailPoet.urls.automationEditor, {
-          id: automation.id,
-        })}
-        isPrimary
-      >
-        {__('Edit automation', 'mailpoet')}
-      </Button>
+      <Dropdown
+        focusOnMount={false}
+        popoverProps={{ placement: 'bottom-end' }}
+        renderToggle={({ isOpen, onToggle }) => (
+          <ButtonGroup>
+            <Button
+              href={addQueryArgs(MailPoet.urls.automationEditor, {
+                id: automation.id,
+              })}
+              isPrimary
+            >
+              {__('Edit automation', 'mailpoet')}
+            </Button>
+            <Button onClick={onToggle} aria-expanded={isOpen} variant="primary">
+              <br />
+              <Icon icon={chevronDown} size={18} />
+            </Button>
+          </ButtonGroup>
+        )}
+        renderContent={() => (
+          <MenuGroup>
+            <MenuItem variant="tertiary" onClick={() => {}}>
+              {__('Deactivate', 'mailpoet')}
+            </MenuItem>
+            <MenuItem isDestructive onClick={() => {}}>
+              {__('Move to Trash', 'mailpoet')}
+            </MenuItem>
+          </MenuGroup>
+        )}
+      />
     </header>
   );
 }
