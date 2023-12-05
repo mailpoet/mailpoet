ddde17f83a705a112dbcf2274c4f52a0eee4e6ba
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2023-12-05 13:30:49 +0100

    Remove multichannel integration from experimental features
    MAILPOET-5701

--- mailpoet/lib/WooCommerce/MultichannelMarketing/MPMarketingChannelController.php
+++ mailpoet/lib/WooCommerce/MultichannelMarketing/MPMarketingChannelController.php
@@ -2,12 +2,7 @@
 
 namespace MailPoet\WooCommerce\MultichannelMarketing;
 
-use MailPoet\Features\FeaturesController;
-
 class MPMarketingChannelController {
-
-  /** @var FeaturesController */
-  private $featuresController;
 
   /**
    * @var MPMarketingChannelDataController
@@ -15,18 +10,12 @@ class MPMarketingChannelController {
   private $channelDataController;
 
   public function __construct(
-    FeaturesController $featuresController,
     MPMarketingChannelDataController $channelDataController
   ) {
-    $this->featuresController = $featuresController;
     $this->channelDataController = $channelDataController;
   }
 
   public function registerMarketingChannel($registeredMarketingChannels): array {
-    if (!$this->featuresController->isSupported(FeaturesController::MAILPOET_WOOCOMMERCE_MULTICHANNEL_INTEGRATION)) {
-      return $registeredMarketingChannels; // Do not register the marketing channel if the feature flag is not enabled
-    }
-
     return array_merge($registeredMarketingChannels, [
       new MPMarketingChannel(
         $this->channelDataController
