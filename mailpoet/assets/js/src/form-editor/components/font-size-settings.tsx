c576107c1e16fcc58fabdc369781ff82faf63bb5
Rostislav Wolny <rosta.wolny@automattic.com>
2023-12-07 15:41:17 +0100

    Fix remaining Typescript warnings in form-editor
    [MAILPOET-5714]

--- mailpoet/assets/js/src/form-editor/components/font-size-settings.tsx
+++ mailpoet/assets/js/src/form-editor/components/font-size-settings.tsx
@@ -1,7 +1,8 @@
 import { FontSizePicker } from '@wordpress/components';
 import { useSetting } from '@wordpress/block-editor';
+import { FontSizePickerProps } from '@wordpress/components/src/font-size-picker/types';
 
-type Props = Pick<FontSizePicker.Props, 'value' | 'onChange'>;
+type Props = Pick<FontSizePickerProps, 'value' | 'onChange'>;
 
 export function FontSizeSettings({ value, onChange }: Props): JSX.Element {
   const fontSizes = useSetting('typography.fontSizes');
