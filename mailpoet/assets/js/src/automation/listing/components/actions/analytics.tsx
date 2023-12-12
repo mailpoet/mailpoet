26cf6fab2dcf52d7e409fea8871d3b0125fa73e4
Jan Jakes <jan.jakes@automattic.com>
2023-12-12 14:09:33 +0100

    Add link to legacy automatic emails analytics
    [MAILPOET-5779]

--- mailpoet/assets/js/src/automation/listing/components/actions/analytics.tsx
+++ mailpoet/assets/js/src/automation/listing/components/actions/analytics.tsx
@@ -1,16 +1,21 @@
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { addQueryArgs } from '@wordpress/url';
-import { Automation } from '../../automation';
+import { AutomationItem } from '../../store/types';
 import { MailPoet } from '../../../../mailpoet';
 
 type Props = {
-  automation: Automation;
+  automation: AutomationItem;
   label?: string;
 };
 
 export function Analytics({ automation, label }: Props): JSX.Element {
-  return (
+  const { id, isLegacy } = automation;
+  return isLegacy ? (
+    <a href={`?page=mailpoet-newsletters#/stats/${id}`}>
+      {label ?? __('Analytics', 'mailpoet')}
+    </a>
+  ) : (
     <Button
       variant="link"
       href={addQueryArgs(MailPoet.urls.automationAnalytics, {
