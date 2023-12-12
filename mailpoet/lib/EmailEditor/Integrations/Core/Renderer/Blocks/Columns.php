5893ea6a2e70a4183f27261cbe54ff371754be4d
Jan Lysý <jan.lysy@automattic.com>
2023-12-12 20:00:29 +0100

    Add layout left and right padding to columns block
    Because side paddings were removed from template.html it was necessary add them to the columns block to correct compatibility across different email client.
    [MAILPOET-5688]

--- mailpoet/lib/EmailEditor/Integrations/Core/Renderer/Blocks/Columns.php
+++ mailpoet/lib/EmailEditor/Integrations/Core/Renderer/Blocks/Columns.php
@@ -30,9 +30,18 @@ class Columns implements BlockRenderer {
     $paddingRight = $parsedBlock['attrs']['style']['spacing']['padding']['right'] ?? '0px';
     $paddingTop = $parsedBlock['attrs']['style']['spacing']['padding']['top'] ?? '0px';
 
+    $align = $parsedBlock['attrs']['align'] ?? null;
+    if ($align !== 'full') {
+      $layoutPaddingLeft = $settingsController->getEmailLayoutStyles()['padding']['left'];
+      $layoutPaddingRight = $settingsController->getEmailLayoutStyles()['padding']['right'];
+    } else {
+      $layoutPaddingLeft = '0px';
+      $layoutPaddingRight = '0px';
+    }
+
     return '
       <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" style="width:' . $width . ';" width="' . $width . '" bgcolor="' . $backgroundColor . '" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
-      <div style="background:' . $backgroundColor . ';background-color:' . $backgroundColor . ';margin:0px auto;max-width:' . $width . ';width:100%;">
+      <div style="background:' . $backgroundColor . ';background-color:' . $backgroundColor . ';margin:0px auto;max-width:' . $width . ';width:100%;padding-left:' . $layoutPaddingLeft . ';padding-right:' . $layoutPaddingRight . ';">
         <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:' . $backgroundColor . ';background-color:' . $backgroundColor . ';max-width:' . $width . ';width:100%;">
           <tbody>
             <tr>
