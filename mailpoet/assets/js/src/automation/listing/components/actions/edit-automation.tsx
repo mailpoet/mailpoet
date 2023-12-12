f7f75985916f749c4a5071e32b0f0b8d49bc53e4
Jan Jakes <jan.jakes@automattic.com>
2023-12-12 14:09:22 +0100

    Add link to edit legacy automatic emails
    [MAILPOET-5779]

--- mailpoet/assets/js/src/automation/listing/components/actions/edit-automation.tsx
+++ mailpoet/assets/js/src/automation/listing/components/actions/edit-automation.tsx
@@ -1,16 +1,21 @@
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { addQueryArgs } from '@wordpress/url';
-import { Automation } from '../../automation';
+import { AutomationItem } from '../../store/types';
 import { MailPoet } from '../../../../mailpoet';
 
 type Props = {
-  automation: Automation;
+  automation: AutomationItem;
   label?: string;
 };
 
 export function EditAutomation({ automation, label }: Props): JSX.Element {
-  return (
+  const { id, isLegacy } = automation;
+  return isLegacy ? (
+    <a href={`?page=mailpoet-newsletter-editor&id=${id}`}>
+      {label ?? __('Edit', 'mailpoet')}
+    </a>
+  ) : (
     <Button
       variant="link"
       href={addQueryArgs(MailPoet.urls.automationEditor, { id: automation.id })}
