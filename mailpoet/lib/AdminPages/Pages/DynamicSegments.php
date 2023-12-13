966014d71debe58307962ac512144af11b79192d
Rostislav Wolny <rosta.wolny@automattic.com>
2023-12-13 15:52:48 +0100

    Improve name of a getter for campaign name or subject on NewsletterEntity
    [MAILPOET-5646]

--- mailpoet/lib/AdminPages/Pages/DynamicSegments.php
+++ mailpoet/lib/AdminPages/Pages/DynamicSegments.php
@@ -186,7 +186,7 @@ class DynamicSegments {
       $result[] = [
         'id' => (string)$newsletter->getId(),
         'subject' => $newsletter->getSubject(),
-        'name' => $newsletter->getPresentableName(),
+        'name' => $newsletter->getCampaignNameOrSubject(),
         'sent_at' => ($sentAt = $newsletter->getSentAt()) ? $sentAt->format('Y-m-d H:i:s') : null,
       ];
     }
