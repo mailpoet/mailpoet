1de16dc221568de1cbe4459c1879bd4b9c5ab862
Jan Lysý <jan.lysy@automattic.com>
2023-12-14 18:28:19 +0100

    Extend block paragraph integration test
    [MAILPOET-5642]

--- mailpoet/tests/integration/EmailEditor/Integration/Core/Renderer/Blocks/ParagraphTest.php
+++ mailpoet/tests/integration/EmailEditor/Integration/Core/Renderer/Blocks/ParagraphTest.php
@@ -34,9 +34,52 @@ class ParagraphTest extends \MailPoetTest {
 
   public function testItRendersContent(): void {
     $rendered = $this->paragraphRenderer->render('<p>Lorem Ipsum</p>', $this->parsedParagraph, $this->settingsController);
-    verify($rendered)->stringNotContainsString('width:'); // Paragraph should not contain width
-    verify($rendered)->stringContainsString('Lorem Ipsum');
-    verify($rendered)->stringContainsString('font-size:16px;');
-    verify($rendered)->stringContainsString('font-family:Arial;');
+    $this->assertStringContainsString('width:100%', $rendered);
+    $this->assertStringContainsString('Lorem Ipsum', $rendered);
+    $this->assertStringContainsString('font-size:16px;', $rendered);
+    $this->assertStringContainsString('font-family:Arial;', $rendered);
+    $this->assertStringContainsString('text-align:left;', $rendered); // Check the default text-align
+    $this->assertStringContainsString('align="left"', $rendered); // Check the default align
+  }
+
+  public function testItRendersContentWithPadding(): void {
+    $parsedParagraph = $this->parsedParagraph;
+    $parsedParagraph['attrs']['style']['spacing']['padding']['top'] = '10px';
+    $parsedParagraph['attrs']['style']['spacing']['padding']['right'] = '20px';
+    $parsedParagraph['attrs']['style']['spacing']['padding']['bottom'] = '30px';
+    $parsedParagraph['attrs']['style']['spacing']['padding']['left'] = '40px';
+    $parsedParagraph['attrs']['align'] = 'center';
+
+    $rendered = $this->paragraphRenderer->render('<p>Lorem Ipsum</p>', $parsedParagraph, $this->settingsController);
+    $this->assertStringContainsString('padding-top:10px;', $rendered);
+    $this->assertStringContainsString('padding-right:20px;', $rendered);
+    $this->assertStringContainsString('padding-bottom:30px;', $rendered);
+    $this->assertStringContainsString('padding-left:40px;', $rendered);
+    $this->assertStringContainsString('text-align:center;', $rendered);
+    $this->assertStringContainsString('align="center"', $rendered);
+    $this->assertStringContainsString('Lorem Ipsum', $rendered);
+  }
+
+  public function testItConvertsBlockTypography(): void {
+    $parsedParagraph = $this->parsedParagraph;
+    $parsedParagraph['attrs']['style']['typography'] = [
+      'textTransform' => 'uppercase',
+      'letterSpacing' => '1px',
+      'textDecoration' => 'underline',
+      'fontStyle' => 'italic',
+      'fontWeight' => 'bold',
+      'fontSize' => '20px',
+      'fontFamily' => 'Times New Roman',
+    ];
+
+    $rendered = $this->paragraphRenderer->render('<p>Lorem Ipsum</p>', $parsedParagraph, $this->settingsController);
+    $this->assertStringContainsString('text-transform:uppercase;', $rendered);
+    $this->assertStringContainsString('letter-spacing:1px;', $rendered);
+    $this->assertStringContainsString('text-decoration:underline;', $rendered);
+    $this->assertStringContainsString('font-style:italic;', $rendered);
+    $this->assertStringContainsString('font-weight:bold;', $rendered);
+    $this->assertStringContainsString('font-size:20px;', $rendered);
+    $this->assertStringContainsString('font-family:Times New Roman;', $rendered);
+    $this->assertStringContainsString('Lorem Ipsum', $rendered);
   }
 }
