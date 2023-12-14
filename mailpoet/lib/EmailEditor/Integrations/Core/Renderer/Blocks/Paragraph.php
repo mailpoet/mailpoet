61e8d2d2007666ced6b02667e6bc70e880e436ff
Jan Lysý <jan.lysy@automattic.com>
2023-12-14 16:00:49 +0100

    Implement rendering block core/paragraph
    [MAILPOET-5642]

--- mailpoet/lib/EmailEditor/Integrations/Core/Renderer/Blocks/Paragraph.php
+++ mailpoet/lib/EmailEditor/Integrations/Core/Renderer/Blocks/Paragraph.php
@@ -4,9 +4,11 @@ namespace MailPoet\EmailEditor\Integrations\Core\Renderer\Blocks;
 
 use MailPoet\EmailEditor\Engine\Renderer\BlockRenderer;
 use MailPoet\EmailEditor\Engine\SettingsController;
+use MailPoet\Util\Helpers;
 
 class Paragraph implements BlockRenderer {
   public function render($blockContent, array $parsedBlock, SettingsController $settingsController): string {
+    $blockContent = $this->removePaddingFromElement($blockContent, ['tag_name' => 'p']);
     return str_replace('{paragraph_content}', $blockContent, $this->getBlockWrapper($parsedBlock, $settingsController));
   }
 
@@ -15,8 +17,22 @@ class Paragraph implements BlockRenderer {
    */
   private function getBlockWrapper(array $parsedBlock, SettingsController $settingsController): string {
     $contentStyles = $settingsController->getEmailContentStyles();
+    $availableStylesheets = $settingsController->getAvailableStylesheets();
 
-    $styles = [];
+    $align = $parsedBlock['attrs']['align'] ?? 'left';
+    $paddingBottom = $parsedBlock['attrs']['style']['spacing']['padding']['bottom'] ?? '0px';
+    $paddingLeft = $parsedBlock['attrs']['style']['spacing']['padding']['left'] ?? '0px';
+    $paddingRight = $parsedBlock['attrs']['style']['spacing']['padding']['right'] ?? '0px';
+    $paddingTop = $parsedBlock['attrs']['style']['spacing']['padding']['top'] ?? '0px';
+
+    $styles = [
+      'text-align' => $align,
+      'padding-bottom' => $paddingBottom,
+      'padding-left' => $paddingLeft,
+      'padding-right' => $paddingRight,
+      'padding-top' => $paddingTop,
+    ];
+
     foreach ($parsedBlock['email_attrs'] ?? [] as $property => $value) {
       $styles[$property] = $value;
     }
@@ -28,20 +44,78 @@ class Paragraph implements BlockRenderer {
       $styles['font-family'] = $contentStyles['typography']['fontFamily'];
     }
 
+    $styles = array_merge($styles, $this->fetchStylesFromBlockAttrs($availableStylesheets, $parsedBlock['attrs'] ?? []));
+
     return '
       <table
         role="presentation"
         border="0"
         cellpadding="0"
         cellspacing="0"
-        style="' . $settingsController->convertStylesToString($styles) . '"
+        style="width:100%;"
       >
         <tr>
-          <td>
+          <td style="' . $settingsController->convertStylesToString($styles) . '" align="' . $align . '">
             {paragraph_content}
           </td>
         </tr>
       </table>
     ';
+  }
+
+  private function fetchStylesFromBlockAttrs(?string $availableStylesheets, array $attrs = []): array {
+    $styles = [];
+
+    // using custom rules because colors do not automatically resolve to hex value
+    $supportedColorValues = ['backgroundColor', 'textColor'];
+    foreach ($supportedColorValues as $supportedColorValue) {
+      if (array_key_exists($supportedColorValue, $attrs)) {
+        $colorKey = $attrs[$supportedColorValue];
+
+        $cssString = $availableStylesheets ?? '';
+
+        $colorRegex = "/--wp--preset--color--$colorKey: (#[0-9a-fA-F]{6});/";
+
+        // fetch color hex from available stylesheets
+        preg_match($colorRegex, $cssString, $colorMatch);
+
+        $colorValue = '';
+        if ($colorMatch) {
+          $colorValue = $colorMatch[1];
+        }
+
+        if ($supportedColorValue === 'textColor') {
+          $styles['color'] = $colorValue; // use color instead of textColor. textColor not valid CSS property
+        } else {
+          $styles[Helpers::camelCaseToKebabCase($supportedColorValue)] = $colorValue;
+        }
+
+      }
+    }
+
+    // fetch Block Style Typography e.g., fontStyle, fontWeight, etc
+    if (isset($attrs['style']['typography'])) {
+      $blockStyleTypographyKeys = array_keys($attrs['style']['typography']);
+      foreach ($blockStyleTypographyKeys as $blockStyleTypographyKey) {
+        $styles[Helpers::camelCaseToKebabCase($blockStyleTypographyKey)] = $attrs['style']['typography'][$blockStyleTypographyKey];
+      }
+    }
+
+    return $styles;
+  }
+
+  /**
+   * @param array{tag_name: string, class_name?: string} $tag
+   */
+  private function removePaddingFromElement($blockContent, array $tag) {
+    $html = new \WP_HTML_Tag_Processor($blockContent);
+    if ($html->next_tag($tag)) {
+      $elementStyle = $html->get_attribute('style') ?? '';
+      $elementStyle = preg_replace('/padding.*:.?[0-9]+px;?/', '', $elementStyle);
+      $html->set_attribute('style', $elementStyle);
+      $blockContent = $html->get_updated_html();
+    }
+
+    return $blockContent;
   }
 }
