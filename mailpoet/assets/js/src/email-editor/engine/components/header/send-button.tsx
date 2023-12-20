eb744d846d76b0826f2953b727db11103c12c70d
Rostislav Wolny <rosta.wolny@automattic.com>
2023-12-20 15:38:09 +0100

    Disable send button when saved content is empty
    [MAILPOET-5646]

--- mailpoet/assets/js/src/email-editor/engine/components/header/send-button.tsx
+++ mailpoet/assets/js/src/email-editor/engine/components/header/send-button.tsx
@@ -1,7 +1,8 @@
 import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
 import { useEntityProp } from '@wordpress/core-data';
-import { MailPoetEmailData } from 'email-editor/engine/store';
+import { MailPoetEmailData, storeName } from 'email-editor/engine/store';
+import { useSelect } from '@wordpress/data';
 
 export function SendButton() {
   const [mailpoetEmail] = useEntityProp(
@@ -9,6 +10,13 @@ export function SendButton() {
     'mailpoet_email',
     'mailpoet_data',
   );
+  const { hasEmptyContent } = useSelect(
+    (select) => ({
+      hasEmptyContent: select(storeName).hasEmptyContent(),
+    }),
+    [],
+  );
+
   const mailpoetEmailData: MailPoetEmailData = mailpoetEmail;
   return (
     <Button
@@ -16,6 +24,7 @@ export function SendButton() {
       onClick={() => {
         window.location.href = `admin.php?page=mailpoet-newsletters#/send/${mailpoetEmailData.id}`;
       }}
+      disabled={hasEmptyContent}
     >
       {__('Send', 'mailpoet')}
     </Button>
