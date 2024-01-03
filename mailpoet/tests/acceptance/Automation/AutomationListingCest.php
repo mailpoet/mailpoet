632e2d6405b74f33d0c9464310faec3a1d5bed70
Jan Jakes <jan.jakes@automattic.com>
2024-01-03 14:39:12 +0100

    Add acceptance test for legacy automatic emails listing
    [MAILPOET-5779]

--- mailpoet/tests/acceptance/Automation/AutomationListingCest.php
+++ mailpoet/tests/acceptance/Automation/AutomationListingCest.php
@@ -69,4 +69,81 @@ class AutomationListingCest {
     $i->see('Analytics', $automation2row);
     $i->see('Edit', $automation2row);
   }
+
+  public function legacyAutomaticEmailListing(AcceptanceTester $i): void {
+    $i->wantTo('See legacy automatic emails on automation listing page');
+    $i->activateWooCommerce();
+    $i->login();
+
+    // empty state - landing page
+    $i->amOnMailpoetPage('Automation');
+    $i->waitForText('Automations');
+    $i->waitForText('Better engagement begins with automation');
+
+    // non-empty state - listing
+    $product = (new DataFactories\WooCommerceProduct($i))->withName('Test product')->create();
+
+    (new DataFactories\Newsletter())
+      ->withSubject('Welcome')
+      ->withWelcomeTypeForSegment()
+      ->withCreatedAt('2020-01-20 12:00:00')
+      ->create();
+
+    (new DataFactories\Newsletter())
+      ->withSubject('Abandoned cart')
+      ->withAutomaticTypeWooCommerceAbandonedCart()
+      ->withCreatedAt('2020-01-19 12:00:00')
+      ->create();
+
+    (new DataFactories\Newsletter())
+      ->withSubject('First purchase')
+      ->withAutomaticTypeWooCommerceFirstPurchase()
+      ->withCreatedAt('2020-01-18 12:00:00')
+      ->create();
+
+    (new DataFactories\Newsletter())
+      ->withSubject('Product purchased')
+      ->withAutomaticTypeWooCommerceProductPurchased([$product])
+      ->withCreatedAt('2020-01-17 12:00:00')
+      ->create();
+
+    (new DataFactories\Newsletter())
+      ->withSubject('Product purchased in category')
+      ->withAutomaticTypeWooCommerceProductInCategoryPurchased([$product])
+      ->withCreatedAt('2020-01-16 12:00:00')
+      ->create();
+
+    $i->reloadPage();
+    $i->waitForText('Automations');
+    $i->waitForText('Welcome');
+    $i->waitForText('Abandoned cart');
+    $i->waitForText('First purchase');
+    $i->waitForText('Product purchased');
+    $i->waitForText('Product purchased in category');
+
+    // welcome email
+    $welcomeRow = '.mailpoet-automation-listing tr:nth-child(2)';
+    $i->see('Welcome', $welcomeRow);
+    $i->see('Sent when someone subscribes to the list: WooCommerce Customers.', $welcomeRow);
+
+    // abandoned cart email
+    $abandonedCartRow = '.mailpoet-automation-listing tr:nth-child(3)';
+    $i->see('Abandoned cart', $abandonedCartRow);
+    $i->see('Send the email when a customer abandons their cart. 1 week(s) later', $abandonedCartRow);
+
+    // first purchase email
+    $firstPurchaseRow = '.mailpoet-automation-listing tr:nth-child(4)';
+    $i->see('First purchase', $firstPurchaseRow);
+    $i->see('Email sent when a customer makes their first purchase.', $firstPurchaseRow);
+
+    // product purchased email
+    $productPurchasedRow = '.mailpoet-automation-listing tr:nth-child(5)';
+    $i->see('Product purchased', $productPurchasedRow);
+    $i->see('Email sent when a customer buys product: Test product.', $productPurchasedRow);
+
+    // product purchased in category email
+    $productPurchasedInCategoryRow = '.mailpoet-automation-listing tr:nth-child(6)';
+    $i->see('Product purchased in category', $productPurchasedInCategoryRow);
+    $i->see('Email sent when a customer buys a product in category: Uncategorized.', $productPurchasedInCategoryRow);
+  }
 }
