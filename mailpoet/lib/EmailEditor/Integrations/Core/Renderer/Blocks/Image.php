da16f50d33fae5615978b14f1a13a511914a2089
Jan Lysý <jan.lysy@automattic.com>
2024-01-05 15:12:45 +0100

    Refactor core/image renderer for better compatibility with Outlook
    [MAILPOET-5788]

--- mailpoet/lib/EmailEditor/Integrations/Core/Renderer/Blocks/Image.php
+++ mailpoet/lib/EmailEditor/Integrations/Core/Renderer/Blocks/Image.php
@@ -6,30 +6,48 @@ use MailPoet\EmailEditor\Engine\Renderer\BlockRenderer;
 use MailPoet\EmailEditor\Engine\SettingsController;
 
 class Image implements BlockRenderer {
-  public function render(string $blockContent, array $parsedBlock, SettingsController $settingsController): string {
-    // Replacing HTML tags figure and figcaption because they are not supported by all email clients
-    $blockContent = str_replace(
-      ['<figure', '</figure>', '<figcaption', '</figcaption>'],
-      ['<div', '</div>', '<div', '</div>'],
-      $blockContent
-    );
+  public function render($blockContent, array $parsedBlock, SettingsController $settingsController): string {
+    $parsedHtml = $this->parseBlockContent($blockContent);
 
-    $blockContent = $this->applyRoundedStyle($blockContent, $parsedBlock);
-    $blockContent = $this->addImageDimensions($blockContent, $parsedBlock, $settingsController);
-    $blockContent = $this->addWidthToWrapper($blockContent, $parsedBlock, $settingsController);
-    $blockContent = $this->addCaptionFontSize($blockContent, $settingsController);
-    return str_replace('{image_content}', $blockContent, $this->getBlockWrapper($parsedBlock, $settingsController));
+    if (!$parsedHtml) {
+      return '';
+    }
+
+    $imageUrl = $parsedHtml['imageUrl'];
+    $image = $parsedHtml['image'];
+    $caption = $parsedHtml['caption'];
+
+    $parsedBlock = $this->addImageSizeWhenMissing($parsedBlock, $imageUrl);
+    $image = $this->applyRoundedStyle($image, $parsedBlock);
+    $image = $this->addImageDimensions($image, $parsedBlock, $settingsController);
+
+    return str_replace(
+      ['{image_content}', '{caption_content}'],
+      [$image, $caption],
+      $this->getBlockWrapper($parsedBlock, $settingsController)
+    );
   }
 
-  private function applyRoundedStyle($blockContent, array $parsedBlock) {
+  private function applyRoundedStyle(string $blockContent, array $parsedBlock): string {
     // Because the isn't an attribute for definition of rounded style, we have to check the class name
     if (isset($parsedBlock['attrs']['className']) && strpos($parsedBlock['attrs']['className'], 'is-style-rounded') !== false) {
-      // If the image should be in a circle, we need to set the border-radius to 100%
+      // If the image should be in a circle, we need to set the border-radius to 9999px to make it the same as is in the editor
       // This style cannot be applied on the wrapper, and we need to set it directly on the image
-      $blockContent = $this->addStyleToElement($blockContent, ['tag_name' => 'img'], 'border-radius: 100%;');
+      $blockContent = $this->addStyleToElement($blockContent, ['tag_name' => 'img'], 'border-radius: 9999px;');
     }
 
     return $blockContent;
+  }
+
+  /**
+   * When the width is not set, it's important to get it for the image to be displayed correctly
+   */
+  private function addImageSizeWhenMissing(array $parsedBlock, string $imageUrl): array {
+    if (!isset($parsedBlock['attrs']['width'])) {
+      $imageSize = wp_getimagesize($imageUrl);
+      $parsedBlock['attrs']['width'] = $imageSize ? "{$imageSize[0]}px" : '100%';
+    }
+    return $parsedBlock;
   }
 
   /**
@@ -46,7 +64,9 @@ class Image implements BlockRenderer {
         $html->set_attribute('height', $settingsController->parseNumberFromStringWithPixels($height));
       }
 
-      $html->set_attribute('width', $settingsController->parseNumberFromStringWithPixels($parsedBlock['email_attrs']['width']));
+      if (isset($parsedBlock['attrs']['width'])) {
+        $html->set_attribute('width', $settingsController->parseNumberFromStringWithPixels($parsedBlock['attrs']['width']));
+      }
       $blockContent = $html->get_updated_html();
     }
 
@@ -54,54 +74,43 @@ class Image implements BlockRenderer {
   }
 
   /**
-   * We need to reset font-size to avoid unexpected white spaces and set the width of the wrapper
-   * for having caption text under the image.
-   */
-  private function addWidthToWrapper($blockContent, array $parsedBlock, SettingsController $settingsController) {
-    $styles = [
-      'width' => $parsedBlock['email_attrs']['width'] ?? $parsedBlock['attrs']['width'] ?? '100%',
-      'font-size' => '0px',
-    ];
-    return $this->addStyleToElement($blockContent, ['tag_name' => 'div'], $settingsController->convertStylesToString($styles));
-  }
-
-  /**
    * This method configure the font size of the caption because it's set to 0 for the parent element to avoid unexpected white spaces
    */
-  private function addCaptionFontSize($blockContent, $settingsController) {
+  private function getCaptionStyles(SettingsController $settingsController, array $parsedBlock): string {
     $contentStyles = $settingsController->getEmailContentStyles();
 
+    // If the alignment is set, we need to center the caption
+    $styles = [
+      'text-align' => isset($parsedBlock['attrs']['align']) ? 'center' : 'left',
+    ];
+
     if (isset($contentStyles['typography']['fontSize'])) {
-      $styles = [
-        'font-size' => $contentStyles['typography']['fontSize'],
-        'text-align' => 'center',
-      ];
-      $blockContent = $this->addStyleToElement($blockContent, ['tag_name' => 'div', 'class_name' => 'wp-element-caption'], $settingsController->convertStylesToString($styles));
+      $styles['font-size'] = $contentStyles['typography']['fontSize'];
     }
 
-    return $blockContent;
+    return $settingsController->convertStylesToString($styles);
   }
 
   /**
-   * Based on MJML <mj-image>
+   * Based on MJML <mj-image> but because MJML doesn't support captions, our solution is a bit different
    */
   private function getBlockWrapper(array $parsedBlock, SettingsController $settingsController): string {
-    $contentStyles = $settingsController->getEmailContentStyles();
-
     $styles = [
       'border-collapse' => 'collapse',
       'border-spacing' => '0px',
+      'font-size' => '0px',
+      'vertical-align' => 'top',
+      'width' => '100%',
     ];
-    $styles = array_merge($styles, $parsedBlock['email_attrs'] ?? []);
 
-    if (!isset($styles['font-size'])) {
-      $styles['font-size'] = $contentStyles['typography']['fontSize'];
-    }
-    if (!isset($styles['font-family'])) {
-      $styles['font-family'] = $contentStyles['typography']['fontFamily'];
-    }
+    // When the image is not aligned, the wrapper is set to 100% width due to caption that can be longer than the image
+    $wrapperWidth = isset($parsedBlock['attrs']['align']) ? ($parsedBlock['attrs']['width'] ?? '100%') : '100%';
+    $wrapperStyles = $styles;
+    $wrapperStyles['width'] = $wrapperWidth;
 
-    $styles['width'] = '100% !important'; // Using important is necessary for Gmail app on mobile devices
+    $captionStyles = $this->getCaptionStyles($settingsController, $parsedBlock);
+
+    $styles['width'] = '100%';
     $align = $parsedBlock['attrs']['align'] ?? 'left';
 
     return '
@@ -111,10 +120,25 @@ class Image implements BlockRenderer {
         cellpadding="0"
         cellspacing="0"
         style="' . $settingsController->convertStylesToString($styles) . '"
+        width="100%"
       >
         <tr>
           <td align="' . $align . '">
-            {image_content}
+            <table
+              role="presentation"
+              border="0"
+              cellpadding="0"
+              cellspacing="0"
+              style="' . $settingsController->convertStylesToString($wrapperStyles) . '"
+              width="' . $wrapperWidth . '"
+            >
+              <tr>
+                <td>{image_content}</td>
+              </tr>
+              <tr>
+                <td style="' . $captionStyles . '">{caption_content}</td>
+              </tr>
+            </table>
           </td>
         </tr>
       </table>
@@ -136,5 +160,34 @@ class Image implements BlockRenderer {
     }
 
     return $blockContent;
+  }
+
+  /**
+   * @param string $blockContent
+   * @return array{imageUrl: string, image: string, caption: string}|null
+   */
+  private function parseBlockContent(string $blockContent): ?array {
+    // Suppress warnings for invalid HTML tags
+    libxml_use_internal_errors(true);
+    $dom = new \DOMDocument();
+    $dom->loadHTML($blockContent);
+    $figureTag = $dom->getElementsByTagName('figure')->item(0);
+    libxml_clear_errors();
+
+    if (!$figureTag) return null;
+
+    $imgTag = $figureTag->getElementsByTagName('img')->item(0);
+    $image = $dom->saveHTML($imgTag);
+    if (!$image) return null;
+
+    $figcaption = $figureTag->getElementsByTagName('figcaption')->item(0);
+    $figcaptionText = $figcaption ? $dom->saveHTML($figcaption) : '';
+    $figcaptionText = str_replace(['<figcaption', '</figcaption>'], ['<span', '</span>'], (string)$figcaptionText);
+
+    return [
+      'imageUrl' => $imgTag ? $imgTag->getAttribute('src') : '',
+      'image' => $image,
+      'caption' => $figcaptionText ?: '',
+    ];
   }
 }
