4689e3f25b1c8e8315c664898d2adbf2087c0c1b
Rostislav Wolny <rosta.wolny@automattic.com>
2024-01-08 10:56:18 +0100

    Update TablePrefixMetadataFactoryTest to work with multisite configuration
    [MAILPOET-5813]

--- mailpoet/tests/integration/Doctrine/TablePrefixMetadataFactoryTest.php
+++ mailpoet/tests/integration/Doctrine/TablePrefixMetadataFactoryTest.php
@@ -7,9 +7,11 @@ use MailPoet\Entities\WpPostEntity;
 
 class TablePrefixMetadataFactoryTest extends \MailPoetTest {
   public function testItPrefixTablesCorrectly() {
+    global $wpdb;
+    $dbPrefix = $wpdb->prefix;
     $wpPostMetadata = $this->entityManager->getClassMetadata(WpPostEntity::class);
     $newslettersMetadata = $this->entityManager->getClassMetadata(NewsletterEntity::class);
-    verify($wpPostMetadata->getTableName())->equals('mp_posts');
-    verify($newslettersMetadata->getTableName())->equals('mp_mailpoet_newsletters');
+    verify($wpPostMetadata->getTableName())->equals($dbPrefix . 'posts');
+    verify($newslettersMetadata->getTableName())->equals($dbPrefix . 'mailpoet_newsletters');
   }
 }
