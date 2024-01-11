b68df77aee160430e7219a5a7ca2248ad72205e7
Jan Lysý <jan.lysy@automattic.com>
2024-01-11 13:50:59 +0100

    Simplify ListBlock renderer
    Because for nesting is better to doesn't have any wrapper, we add configured styles directly to the list tag
    [MAILPOET-5790]

--- mailpoet/lib/EmailEditor/Integrations/Core/Renderer/Blocks/ListBlock.php
+++ mailpoet/lib/EmailEditor/Integrations/Core/Renderer/Blocks/ListBlock.php
@@ -8,38 +8,30 @@ use MailPoet\EmailEditor\Engine\SettingsController;
 // We have to avoid using keyword `List`
 class ListBlock implements BlockRenderer {
   public function render(string $blockContent, array $parsedBlock, SettingsController $settingsController): string {
-    return str_replace('{list_content}', $blockContent, $this->getBlockWrapper($parsedBlock, $settingsController));
-  }
+    $html = new \WP_HTML_Tag_Processor($blockContent);
+    $tagName = ($parsedBlock['attrs']['ordered'] ?? false) ? 'ol' : 'ul';
+    if ($html->next_tag(['tag_name' => $tagName])) {
+      $styles = $html->get_attribute('style') ?? '';
+      $styles = $settingsController->parseStylesToArray($styles);
+      $styles = array_merge($styles, $parsedBlock['email_attrs'] ?? []);
 
-  private function getBlockWrapper(array $parsedBlock, SettingsController $settingsController): string {
-    $contentStyles = $settingsController->getEmailContentStyles();
+      // List block does not need width specification and it can cause issues for nested lists
+      unset($styles['width'] );
 
-    $styles = [];
-    foreach ($parsedBlock['email_attrs'] ?? [] as $property => $value) {
-      $styles[$property] = $value;
+      // Use font-size and font-family from Settings when those properties are not set
+      $contentStyles = $settingsController->getEmailContentStyles();
+      if (!isset($styles['font-size'])) {
+        $styles['font-size'] = $contentStyles['typography']['fontSize'];
+      }
+      if (!isset($styles['font-family'])) {
+        $styles['font-family'] = $contentStyles['typography']['fontFamily'];
+      }
+
+      $html->set_attribute('style', $settingsController->convertStylesToString($styles));
+      $blockContent = $html->get_updated_html();
     }
 
-    if (!isset($styles['font-size'])) {
-      $styles['font-size'] = $contentStyles['typography']['fontSize'];
-    }
-    if (!isset($styles['font-family'])) {
-      $styles['font-family'] = $contentStyles['typography']['fontFamily'];
-    }
-
-    return '
-      <table
-        border="0"
-        cellpadding="0"
-        cellspacing="0"
-        role="presentation"
-        style="' . $settingsController->convertStylesToString($styles) . '"
-      >
-        <tr>
-          <td>
-            {list_content}
-          </td>
-        </tr>
-      </table>
-    ';
+    // \WP_HTML_Tag_Processor escapes the content, so we have to replace it back
+    return str_replace('&#039;', "'", $blockContent);
   }
 }
