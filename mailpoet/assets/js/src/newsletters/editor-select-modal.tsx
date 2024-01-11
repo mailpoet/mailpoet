7ef288475f0d8de8767d2b252170107de495ba52
Rostislav Wolny <rosta.wolny@automattic.com>
2024-01-11 09:38:10 +0100

    Refactor create newsletter into a function and cache via useCallback
    [MAILPOET-5810]

--- mailpoet/assets/js/src/newsletters/editor-select-modal.tsx
+++ mailpoet/assets/js/src/newsletters/editor-select-modal.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useCallback } from 'react';
 import { Modal, Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { GlobalContext, GlobalContextValue } from 'context';
@@ -15,6 +15,38 @@ export function EditorSelectModal({
 }: EditorSelectModalProps) {
   const [isLoading, setIsLoading] = useState(false);
   const { notices } = useContext<GlobalContextValue>(GlobalContext);
+
+  const createNewsletterAndOpenEditor = useCallback(() => {
+    setIsLoading(true);
+    void MailPoet.Ajax.post({
+      api_version: window.mailpoet_api_version,
+      endpoint: 'newsletters',
+      action: 'create',
+      data: {
+        type: 'standard',
+        subject: __('Subject', 'mailpoet'),
+        new_editor: true,
+      },
+    })
+      .done((response) => {
+        window.location.href = `admin.php?page=mailpoet-email-editor&postId=${
+          response.data.wp_post_id as number
+        }`;
+      })
+      .fail((response) => {
+        setIsLoading(false);
+        onClose();
+        if (response.errors.length > 0) {
+          notices.error(
+            response.errors.map((error) => (
+              <p key={error.message}>{error.message}</p>
+            )),
+            { scroll: true },
+          );
+        }
+      });
+  }, [notices, onClose]);
+
   if (!isModalOpen) {
     return null;
   }
@@ -61,36 +93,7 @@ export function EditorSelectModal({
           type="button"
           variant="primary"
           isBusy={isLoading}
-          onClick={() => {
-            setIsLoading(true);
-            void MailPoet.Ajax.post({
-              api_version: window.mailpoet_api_version,
-              endpoint: 'newsletters',
-              action: 'create',
-              data: {
-                type: 'standard',
-                subject: __('Subject', 'mailpoet'),
-                new_editor: true,
-              },
-            })
-              .done((response) => {
-                window.location.href = `admin.php?page=mailpoet-email-editor&postId=${
-                  response.data.wp_post_id as number
-                }`;
-              })
-              .fail((response) => {
-                setIsLoading(false);
-                onClose();
-                if (response.errors.length > 0) {
-                  notices.error(
-                    response.errors.map((error) => (
-                      <p key={error.message}>{error.message}</p>
-                    )),
-                    { scroll: true },
-                  );
-                }
-              });
-          }}
+          onClick={createNewsletterAndOpenEditor}
         >
           {__('Continue', 'mailpoet')}
         </Button>
