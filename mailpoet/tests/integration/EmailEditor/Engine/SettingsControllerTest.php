2e9f386fa590c810f126538e1805cce27d1a21ca
Rostislav Wolny <rosta.wolny@automattic.com>
2024-01-15 14:08:37 +0100

    Add the basic list of font families
    Add basic font families. For now, we use know-how from the current editor
    and we use the same list of fonts as were used in the current editor
    [MAILPOET-5740]

--- mailpoet/tests/integration/EmailEditor/Engine/SettingsControllerTest.php
+++ mailpoet/tests/integration/EmailEditor/Engine/SettingsControllerTest.php
@@ -13,7 +13,27 @@ class SettingsControllerTest extends \MailPoetTest {
 
   public function testItGeneratesCssStylesForThemeWithFontFamilies() {
     $css = $this->settingsController->getStylesheetForRendering();
-    verify($css)->stringContainsString('.has-system-sans-serif-font-family');
-    verify($css)->stringContainsString('.has-system-Serif-font-family');
+    verify($css)->stringContainsString('.has-arial-font-family');
+    verify($css)->stringContainsString('.has-comic-sans-ms-font-family');
+    verify($css)->stringContainsString('.has-courier-new-font-family');
+    verify($css)->stringContainsString('.has-georgia-font-family');
+    verify($css)->stringContainsString('.has-lucida-font-family');
+    verify($css)->stringContainsString('.has-tahoma-font-family');
+    verify($css)->stringContainsString('.has-times-new-roman-font-family');
+    verify($css)->stringContainsString('.has-trebuchet-ms-font-family');
+    verify($css)->stringContainsString('.has-verdana-font-family');
+    verify($css)->stringContainsString('.has-arvo-font-family');
+    verify($css)->stringContainsString('.has-lato-font-family');
+    verify($css)->stringContainsString('.has-merriweather-font-family');
+    verify($css)->stringContainsString('.has-merriweather-sans-font-family');
+    verify($css)->stringContainsString('.has-noticia-text-font-family');
+    verify($css)->stringContainsString('.has-open-sans-font-family');
+    verify($css)->stringContainsString('.has-playfair-display-font-family');
+    verify($css)->stringContainsString('.has-roboto-font-family');
+    verify($css)->stringContainsString('.has-source-sans-pro-font-family');
+    verify($css)->stringContainsString('.has-oswald-font-family');
+    verify($css)->stringContainsString('.has-raleway-font-family');
+    verify($css)->stringContainsString('.has-permanent-marker-font-family');
+    verify($css)->stringContainsString('.has-pacifico-font-family');
   }
 }
