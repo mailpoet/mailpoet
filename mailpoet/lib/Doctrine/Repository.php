c0f40f9dd21b1c90c5eea63e9b244206d4e409b5
Jan Jakes <jan.jakes@automattic.com>
2024-01-18 14:04:25 +0100

    Remove no longer needed cleanup
    [MAILPOET-5845]

--- mailpoet/lib/Doctrine/Repository.php
+++ mailpoet/lib/Doctrine/Repository.php
@@ -163,24 +163,6 @@ abstract class Repository {
   }
 
   /**
-   * @param class-string<object> $className
-   * @param callable|null $filter
-   */
-  public function detachEntitiesOfType($className, callable $filter = null): void {
-    $rootClassName = $this->entityManager->getClassMetadata($className)->rootEntityName;
-    $entities = $this->entityManager->getUnitOfWork()->getIdentityMap()[$rootClassName] ?? [];
-    foreach ($entities as $entity) {
-      if (!($entity instanceof $className)) {
-        continue;
-      }
-      if ($filter && !$filter($entity)) {
-        continue;
-      }
-      $this->entityManager->detach($entity);
-    }
-  }
-
-  /**
    * @return class-string<T>
    */
   abstract protected function getEntityClassName();
