66a2b812a6a90c7e2b864c40f99d0b0e1948e740
Brezo Cordero <8002881+brezocordero@users.noreply.github.com>
2024-01-18 16:29:01 -0600

    Process Soft error for domain authorization
    Pause the task and do not update counts.
    
    [MAILPOET-5832]

--- mailpoet/lib/Cron/Workers/SendingQueue/SendingErrorHandler.php
+++ mailpoet/lib/Cron/Workers/SendingQueue/SendingErrorHandler.php
@@ -4,6 +4,7 @@ namespace MailPoet\Cron\Workers\SendingQueue;
 
 use MailPoet\Entities\ScheduledTaskEntity;
 use MailPoet\Entities\SendingQueueEntity;
+use MailPoet\Logging\LoggerFactory;
 use MailPoet\Mailer\MailerError;
 use MailPoet\Mailer\MailerLog;
 use MailPoet\Newsletter\Sending\ScheduledTaskSubscribersRepository;
@@ -19,14 +20,19 @@ class SendingErrorHandler {
   /** @var SendingQueuesRepository */
   private $sendingQueuesRepository;
 
+  /** @var LoggerFactory */
+  private $loggerFactory;
+
   public function __construct(
     ScheduledTaskSubscribersRepository $scheduledTaskSubscribersRepository,
     SendingThrottlingHandler $throttlingHandler,
-    SendingQueuesRepository $sendingQueuesRepository
+    SendingQueuesRepository $sendingQueuesRepository,
+    LoggerFactory $loggerFactory
   ) {
     $this->scheduledTaskSubscribersRepository = $scheduledTaskSubscribersRepository;
     $this->throttlingHandler = $throttlingHandler;
     $this->sendingQueuesRepository = $sendingQueuesRepository;
+    $this->loggerFactory = $loggerFactory;
   }
 
   public function processError(
@@ -63,6 +69,14 @@ class SendingErrorHandler {
     $queue = $task->getSendingQueue();
 
     if ($queue instanceof SendingQueueEntity) {
+      if ($error->getOperation() === MailerError::OPERATION_DOMAIN_AUTHORIZATION) {
+        $this->loggerFactory->getLogger(LoggerFactory::TOPIC_NEWSLETTERS)->info(
+          'Paused task in sending queue due to sender domain authorization error',
+          ['task_id' => $task->getId()]
+        );
+        $this->sendingQueuesRepository->pause($queue);
+        return;
+      }
       $this->sendingQueuesRepository->updateCounts($queue);
     }
   }
