4a96f3dfea07ea945515a21f30df7535fc27ac0b
David Remer <webmaster@websupporter.net>
2024-01-26 09:17:20 +0200

    Make shortcode text translateable
    [MAILPOET-5852]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/steps/send-email/edit/shortcode-help-text.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/steps/send-email/edit/shortcode-help-text.tsx
@@ -3,13 +3,12 @@ import { __ } from '@wordpress/i18n';
 export function ShortcodeHelpText(): JSX.Element {
   return (
     <span className="mailpoet-shortcode-selector">
-      You can use{' '}
       <a
         href="https://kb.mailpoet.com/article/215-personalize-newsletter-with-shortcodes"
         target="_blank"
         rel="noopener noreferrer"
       >
-        {__('MailPoet shortcodes', 'mailpoet')}
+        {__('You can use MailPoet shortcodes.', 'mailpoet')}
       </a>
     </span>
   );
