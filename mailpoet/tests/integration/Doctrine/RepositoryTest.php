165e6b27594d9ac161090440f8f274a32098f64c
Jan Jakes <jan.jakes@automattic.com>
2024-01-26 14:47:52 +0100

    Add test for detaching entities by relation
    [MAILPOET-5845]

--- mailpoet/tests/integration/Doctrine/RepositoryTest.php
+++ mailpoet/tests/integration/Doctrine/RepositoryTest.php
@@ -3,12 +3,18 @@
 namespace MailPoet\Test\Doctrine;
 
 use MailPoet\Doctrine\Repository;
+use MailPoet\Entities\CustomFieldEntity;
 use MailPoet\Entities\SettingEntity;
+use MailPoet\Entities\SubscriberCustomFieldEntity;
+use MailPoet\Entities\SubscriberEntity;
+use MailPoet\Test\DataFactories\CustomField;
+use MailPoet\Test\DataFactories\Subscriber;
 use MailPoetVendor\Doctrine\Common\Collections\Criteria;
+use MailPoetVendor\Doctrine\ORM\EntityManager;
 
 class RepositoryTest extends \MailPoetTest {
   public function testItCanPersistAndFlush(): void {
-    $repository = $this->createRepository();
+    $repository = $this->createRepository(SettingEntity::class);
 
     $setting = new SettingEntity();
     $setting->setName('name');
@@ -16,49 +22,84 @@ class RepositoryTest extends \MailPoetTest {
     $repository->persist($setting);
     $repository->flush();
 
-    $this->assertSame($this->getEntityFromIdentityMap($setting->getId()), $setting);
+    $this->assertSame($this->getEntityFromIdentityMap(SettingEntity::class, $setting->getId()), $setting);
   }
 
   public function testItCanDetachAll(): void {
-    $repository = $this->createRepository();
+    $repository = $this->createRepository(SettingEntity::class);
 
     $setting1 = $this->createSetting('name-1', 'value-1');
     $setting2 = $this->createSetting('name-2', 'value-2');
     $setting3 = $this->createSetting('name-3', 'value-3');
 
-    $this->assertSame($this->getEntityFromIdentityMap($setting1->getId()), $setting1);
-    $this->assertSame($this->getEntityFromIdentityMap($setting2->getId()), $setting2);
-    $this->assertSame($this->getEntityFromIdentityMap($setting3->getId()), $setting3);
+    $this->assertSame($this->getEntityFromIdentityMap(SettingEntity::class, $setting1->getId()), $setting1);
+    $this->assertSame($this->getEntityFromIdentityMap(SettingEntity::class, $setting2->getId()), $setting2);
+    $this->assertSame($this->getEntityFromIdentityMap(SettingEntity::class, $setting3->getId()), $setting3);
 
     $repository->detachAll();
 
-    $this->assertNull($this->getEntityFromIdentityMap($setting1->getId()));
-    $this->assertNull($this->getEntityFromIdentityMap($setting2->getId()));
-    $this->assertNull($this->getEntityFromIdentityMap($setting3->getId()));
+    $this->assertNull($this->getEntityFromIdentityMap(SettingEntity::class, $setting1->getId()));
+    $this->assertNull($this->getEntityFromIdentityMap(SettingEntity::class, $setting2->getId()));
+    $this->assertNull($this->getEntityFromIdentityMap(SettingEntity::class, $setting3->getId()));
   }
 
   public function testItCanDetachSelectively(): void {
-    $repository = $this->createRepository();
+    $repository = $this->createRepository(SettingEntity::class);
 
     $setting1 = $this->createSetting('name-1', 'value-1');
     $setting2 = $this->createSetting('name-2', 'value-2');
     $setting3 = $this->createSetting('name-3', 'value-3');
 
-    $this->assertSame($this->getEntityFromIdentityMap($setting1->getId()), $setting1);
-    $this->assertSame($this->getEntityFromIdentityMap($setting2->getId()), $setting2);
-    $this->assertSame($this->getEntityFromIdentityMap($setting3->getId()), $setting3);
+    $this->assertSame($this->getEntityFromIdentityMap(SettingEntity::class, $setting1->getId()), $setting1);
+    $this->assertSame($this->getEntityFromIdentityMap(SettingEntity::class, $setting2->getId()), $setting2);
+    $this->assertSame($this->getEntityFromIdentityMap(SettingEntity::class, $setting3->getId()), $setting3);
 
     $repository->detachAll(
       new Criteria(Criteria::expr()->notIn('id', [$setting1->getId(), $setting3->getId()]))
     );
 
-    $this->assertSame($this->getEntityFromIdentityMap($setting1->getId()), $setting1);
-    $this->assertNull($this->getEntityFromIdentityMap($setting2->getId()));
-    $this->assertSame($this->getEntityFromIdentityMap($setting3->getId()), $setting3);
+    $this->assertSame($this->getEntityFromIdentityMap(SettingEntity::class, $setting1->getId()), $setting1);
+    $this->assertNull($this->getEntityFromIdentityMap(SettingEntity::class, $setting2->getId()));
+    $this->assertSame($this->getEntityFromIdentityMap(SettingEntity::class, $setting3->getId()), $setting3);
+  }
+
+  public function testItCanDetachByRelation(): void {
+    $repository = $this->createRepository(SubscriberCustomFieldEntity::class);
+
+    $subscriber = (new Subscriber())->create();
+    $cf1 = (new CustomField())->withName('cf-1')->create();
+    $cf2 = (new CustomField())->withName('cf-2')->create();
+    $cf3 = (new CustomField())->withName('cf-3')->create();
+
+
+    $cfs1 = $this->createSubscriberCustomField($subscriber, $cf1, 'value-1');
+    $cfs2 = $this->createSubscriberCustomField($subscriber, $cf2, 'value-2');
+    $cfs3 = $this->createSubscriberCustomField($subscriber, $cf3, 'value-3');
+
+    $this->assertSame($this->getEntityFromIdentityMap(SubscriberCustomFieldEntity::class, $cfs1->getId()), $cfs1);
+    $this->assertSame($this->getEntityFromIdentityMap(SubscriberCustomFieldEntity::class, $cfs2->getId()), $cfs2);
+    $this->assertSame($this->getEntityFromIdentityMap(SubscriberCustomFieldEntity::class, $cfs3->getId()), $cfs3);
+
+    // test detaching cfs-3 by cf-3 reference
+    $this->entityManager->detach($cf3);
+    $this->entityManager->detach($cfs3);
+    $cf3Ref = $this->entityManager->getReference(CustomFieldEntity::class, $cf3->getId());
+    $cfs3 = $this->entityManager->find(SubscriberCustomFieldEntity::class, $cfs3->getId());
+
+    $this->assertInstanceOf(SubscriberCustomFieldEntity::class, $cfs3);
+    $this->assertSame($this->getEntityFromIdentityMap(SubscriberCustomFieldEntity::class, $cfs3->getId()), $cfs3);
+
+    $repository->detachAll(
+      new Criteria(Criteria::expr()->notIn('customField', [$cf1, $cf3Ref]))
+    );
+
+    $this->assertSame($this->getEntityFromIdentityMap(SubscriberCustomFieldEntity::class, $cfs1->getId()), $cfs1);
+    $this->assertNull($this->getEntityFromIdentityMap(SubscriberCustomFieldEntity::class, $cfs2->getId()));
+    $this->assertSame($this->getEntityFromIdentityMap(SubscriberCustomFieldEntity::class, $cfs3->getId()), $cfs3);
   }
 
   public function testItCanRefreshAll(): void {
-    $repository = $this->createRepository();
+    $repository = $this->createRepository(SettingEntity::class);
 
     $setting1 = $this->createSetting('name-1', 'value-1');
     $setting2 = $this->createSetting('name-2', 'value-2');
@@ -94,7 +135,7 @@ class RepositoryTest extends \MailPoetTest {
   }
 
   public function testItCanRefreshSelectively(): void {
-    $repository = $this->createRepository();
+    $repository = $this->createRepository(SettingEntity::class);
 
     $setting1 = $this->createSetting('name-1', 'value-1');
     $setting2 = $this->createSetting('name-2', 'value-2');
@@ -140,20 +181,48 @@ class RepositoryTest extends \MailPoetTest {
     return $setting;
   }
 
-  /** @return Repository<SettingEntity> */
-  private function createRepository(): Repository {
-    /** @var Repository<SettingEntity> $repository */
-    $repository = new class($this->entityManager) extends Repository {
+  private function createSubscriberCustomField(SubscriberEntity $subscriber, CustomFieldEntity $customField, string $value): SubscriberCustomFieldEntity {
+    $subscriberCustomField = new SubscriberCustomFieldEntity($subscriber, $customField, $value);
+    $this->entityManager->persist($subscriberCustomField);
+    $this->entityManager->flush();
+    return $subscriberCustomField;
+  }
+
+  /**
+   * @template T of object
+   * @param class-string<T> $entityClass
+   * @return Repository<T>
+   */
+  private function createRepository(string $entityClass): Repository {
+    /** @var Repository<T> $repository */
+    $repository = new class($this->entityManager, $entityClass) extends Repository {
+      private string $entityClass;
+
+      public function __construct(
+        EntityManager $entityManager,
+        string $entityClass
+      ) {
+        $this->entityClass = $entityClass;
+        parent::__construct($entityManager);
+      }
+
       protected function getEntityClassName(): string {
-        return SettingEntity::class;
+        /** @var class-string<T> $entityClass */
+        $entityClass = $this->entityClass;
+        return $entityClass;
       }
     };
     return $repository;
   }
 
-  private function getEntityFromIdentityMap(?int $id): ?SettingEntity {
-    /** @var SettingEntity|false $entity */
-    $entity = $this->entityManager->getUnitOfWork()->tryGetById($id, SettingEntity::class);
+  /**
+   * @template T
+   * @param class-string<T> $entityClass
+   * @return T|null
+   */
+  private function getEntityFromIdentityMap(string $entityClass, ?int $id) {
+    /** @var T|false $entity */
+    $entity = $this->entityManager->getUnitOfWork()->tryGetById($id, $entityClass);
     return $entity ?: null;
   }
 }
