f1852684a5331c17e00d8eca92c15aa0d68d8ee9
Brezo Cordero <8002881+brezocordero@users.noreply.github.com>
2024-01-26 15:41:16 -0600

    Use useSelect for senderDomainsConfig
    [MAILPOET-5796]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/context.ts
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/context.ts
@@ -1,4 +1,4 @@
-import { select } from '@wordpress/data';
+import { select, dispatch, useSelect } from '@wordpress/data';
 import { FormTokenItem } from '../../editor/components';
 import { storeName } from '../../editor/store';
 import { SenderRestrictionsType } from '../../../common';
@@ -23,3 +23,14 @@ export type Context = {
 
 export const getContext = (): Context =>
   select(storeName).getContext('mailpoet') as Context;
+
+export const useSelectContext = (): Context =>
+  useSelect((s) => s(storeName).getContext('mailpoet'), []);
+
+export const updateSenderDomainsConfig = (
+  senderDomainsConfig: SenderDomainsConfig,
+) => {
+  dispatch(storeName).alterContext('mailpoet', 'senderDomainsConfig', {
+    ...senderDomainsConfig,
+  });
+};
