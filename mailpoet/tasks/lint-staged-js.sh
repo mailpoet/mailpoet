a78a31f1109e38409aab8e8d5c47c0cf3930b587
John Oleksowicz <john.oleksowicz@automattic.com>
2024-01-31 13:58:07 -0600

    Fix indentation
    MAILPOET-4237

--- mailpoet/tasks/lint-staged-js.sh
+++ mailpoet/tasks/lint-staged-js.sh
@@ -5,8 +5,8 @@ set -e
 source $PWD/.env
 
 if [ "$MP_GIT_HOOKS_ENABLE" != "true" ]; then
-    echo "MP_GIT_HOOKS_ENABLE is not set to 'true', skipping lint-staged-js"
-    exit 0
+  echo "MP_GIT_HOOKS_ENABLE is not set to 'true', skipping lint-staged-js"
+  exit 0
 fi
 
 if [ "$MP_GIT_HOOKS_ESLINT" = "true" ]; then
