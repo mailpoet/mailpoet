c64335398a0479b210fc97a665b24b2755a87e2a
John Oleksowicz <john.oleksowicz@automattic.com>
2024-01-31 13:55:53 -0600

    Rename scripts and make messages consistent
    MAILPOET-4237

--- mailpoet/tasks/lint-staged-css.sh
+++ mailpoet/tasks/lint-staged-css.sh
@@ -3,12 +3,12 @@
 source $PWD/.env
 
 if [ "$MP_GIT_HOOKS_ENABLE" != "true" ]; then
-    echo "MP_GIT_HOOKS_ENABLE is not set to 'true'. Skipping lint-staged-scripts."
+    echo "MP_GIT_HOOKS_ENABLE is not set to 'true', skipping lint-staged-css."
     exit 0
 fi
 
 if [ "$MP_GIT_HOOKS_STYLELINT" = "true" ]; then
   pnpm run stylelint $@
 else
-  echo "MP_GIT_HOOKS_STYLELINT not set to 'true', skipping stylelint"
+  echo "MP_GIT_HOOKS_STYLELINT is not set to 'true', skipping stylelint."
 fi
--- mailpoet/tasks/lint-staged-js.sh
+++ mailpoet/tasks/lint-staged-js.sh
@@ -3,12 +3,12 @@
 source $PWD/.env
 
 if [ "$MP_GIT_HOOKS_ENABLE" != "true" ]; then
-    echo "MP_GIT_HOOKS_ENABLE is not set to 'true'. Skipping lint-staged-scripts."
+    echo "MP_GIT_HOOKS_ENABLE is not set to 'true', skipping lint-staged-js"
     exit 0
 fi
 
 if [ "$MP_GIT_HOOKS_ESLINT" = "true" ]; then
   eslint --max-warnings 0 $@
 else
-  echo "MP_GIT_HOOKS_ESLINT not set to 'true', skipping eslint"
+  echo "MP_GIT_HOOKS_ESLINT is not set to 'true', skipping eslint"
 fi
