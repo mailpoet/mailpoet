9ac1b84287385aec2e6c97c91eba80d63addd56c
John Oleksowicz <john.oleksowicz@automattic.com>
2024-01-31 14:49:54 -0600

    Add defaults for unset env variables
    These defaults maintain backwards compatibility if developers don't
    change anything
    
    MAILPOET-4237

--- .husky/common.sh
+++ .husky/common.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+. "$(dirname "$0")/../mailpoet/.env"
+
+export MP_GIT_HOOKS_ENABLE="${MP_GIT_HOOKS_ENABLE:-true}"
+export MP_GIT_HOOKS_ESLINT="${MP_GIT_HOOKS_ESLINT:-true}"
+export MP_GIT_HOOKS_STYLELINT="${MP_GIT_HOOKS_STYLELINT:-true}"
+export MP_GIT_HOOKS_PHPLINT="${MP_GIT_HOOKS_PHPLINT:-true}"
+export MP_GIT_HOOKS_CODE_SNIFFER="${MP_GIT_HOOKS_CODE_SNIFFER:-true}"
+export MP_GIT_HOOKS_MINIMAL_PLUGIN_STANDARDS="${MP_GIT_HOOKS_MINIMAL_PLUGIN_STANDARDS:-true}"
+export MP_GIT_HOOKS_PHPSTAN="${MP_GIT_HOOKS_PHPSTAN:-true}"
+export MP_GIT_HOOKS_INSTALL_JS="${MP_GIT_HOOKS_INSTALL_JS:-false}"
+export MP_GIT_HOOKS_INSTALL_PHP="${MP_GIT_HOOKS_INSTALL_PHP:-false}"
+
 fileChanged() {
   local filePattern="$1"
   local changedFiles="$2"
@@ -27,3 +39,4 @@ installIfUpdates() {
     popd
   fi
 }
+
