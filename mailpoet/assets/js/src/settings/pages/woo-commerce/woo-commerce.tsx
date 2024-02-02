e260ac55a7e18d1360096a9807fed070257bb1da
David Remer <webmaster@websupporter.net>
2024-02-02 09:27:43 +0200

    Remove Enable Automations notice
    [MAILPOET-5883]

--- mailpoet/assets/js/src/settings/pages/woo-commerce/woo-commerce.tsx
+++ mailpoet/assets/js/src/settings/pages/woo-commerce/woo-commerce.tsx
@@ -1,19 +1,15 @@
 import { SaveButton } from 'settings/components';
-import { AutomationsInfoNotice } from 'notices/automations-info-notice';
 import { EmailCustomizer } from './email-customizer';
 import { CheckoutOptin } from './checkout-optin';
 import { SubscribeOldCustomers } from './subscribe-old-customers';
 
 export function WooCommerce() {
   return (
-    <>
-      <AutomationsInfoNotice />
-      <div className="mailpoet-settings-grid">
-        <EmailCustomizer />
-        <CheckoutOptin />
-        <SubscribeOldCustomers />
-        <SaveButton />
-      </div>
-    </>
+    <div className="mailpoet-settings-grid">
+      <EmailCustomizer />
+      <CheckoutOptin />
+      <SubscribeOldCustomers />
+      <SaveButton />
+    </div>
   );
 }
