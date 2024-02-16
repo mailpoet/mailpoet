b129903dee4281f43bc5c0c21000c3d8eb5b1d48
John Oleksowicz <john.oleksowicz@automattic.com>
2024-02-16 11:32:27 -0600

    Add explanation of the getLookupData method
    MAILPOET-5467

--- mailpoet/lib/Segments/DynamicSegments/Filters/Filter.php
+++ mailpoet/lib/Segments/DynamicSegments/Filters/Filter.php
@@ -9,5 +9,15 @@ use MailPoetVendor\Doctrine\DBAL\Query\QueryBuilder;
 interface Filter {
   public function apply(QueryBuilder $queryBuilder, DynamicSegmentFilterEntity $filter): QueryBuilder;
 
+  /**
+   * At sending time, we store the current state of every filter so we can tell in the future how it was configured. This
+   * method should be used to return any data that might change after sending time. For example, if a filter stores IDs
+   * of related entities, we should try to look up descriptive names for those entities in case they get deleted or
+   * renamed later.
+   *
+   * @param DynamicSegmentFilterData $filterData
+   *
+   * @return array
+   */
   public function getLookupData(DynamicSegmentFilterData $filterData): array;
 }
