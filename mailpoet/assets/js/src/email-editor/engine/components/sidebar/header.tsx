c71fb555c6053e5ccb45142f8f6e4b3f92c329c5
Rostislav Wolny <rosta.wolny@automattic.com>
2024-02-27 10:03:38 +0100

    Fix sidebar tabs appearance for WP 6.5
    I explored using the private Tabs component and it worked, but it
    doesn't work with WP 6.4 so I went with adding CSS.
    We can switch to Tabs later. Hopefully they won't be private.
    [MAILPOET-5907]

--- mailpoet/assets/js/src/email-editor/engine/components/sidebar/header.tsx
+++ mailpoet/assets/js/src/email-editor/engine/components/sidebar/header.tsx
@@ -32,39 +32,37 @@ export function Header() {
   }, [selectedBlockId, toggleSettingsSidebarActiveTab]);
 
   return (
-    <div className="components-panel__header interface-complementary-area-header edit-post-sidebar__panel-tabs">
-      <ul>
-        <li>
-          <button
-            onClick={() => {
-              void toggleSettingsSidebarActiveTab(mainSidebarEmailTab);
-            }}
-            className={classnames(
-              'components-button edit-post-sidebar__panel-tab',
-              { 'is-active': activeTab === mainSidebarEmailTab },
-            )}
-            data-automation-id="email_settings_tab"
-            type="button"
-          >
-            {__('Email', 'mailpoet')}
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              void toggleSettingsSidebarActiveTab(mainSidebarBlockTab);
-            }}
-            className={classnames(
-              'components-button edit-post-sidebar__panel-tab',
-              { 'is-active': activeTab === mainSidebarBlockTab },
-            )}
-            data-automation-id="mailpoet_block_settings_tab"
-            type="button"
-          >
-            {__('Block')}
-          </button>
-        </li>
-      </ul>
-    </div>
+    <ul>
+      <li>
+        <button
+          onClick={() => {
+            void toggleSettingsSidebarActiveTab(mainSidebarEmailTab);
+          }}
+          className={classnames(
+            'components-button edit-post-sidebar__panel-tab',
+            { 'is-active': activeTab === mainSidebarEmailTab },
+          )}
+          data-automation-id="email_settings_tab"
+          type="button"
+        >
+          {__('Email', 'mailpoet')}
+        </button>
+      </li>
+      <li>
+        <button
+          onClick={() => {
+            void toggleSettingsSidebarActiveTab(mainSidebarBlockTab);
+          }}
+          className={classnames(
+            'components-button edit-post-sidebar__panel-tab',
+            { 'is-active': activeTab === mainSidebarBlockTab },
+          )}
+          data-automation-id="mailpoet_block_settings_tab"
+          type="button"
+        >
+          {__('Block')}
+        </button>
+      </li>
+    </ul>
   );
 }
