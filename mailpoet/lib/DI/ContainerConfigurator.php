0695927e52cb971d54cba779776e48ecd1c72120
Brezo Cordero <8002881+brezocordero@users.noreply.github.com>
2024-02-29 21:52:00 -0600

    Add basic capabilities manager class
    Backwards compatibility will be handled later
    
    [MAILPOET-5898]

--- mailpoet/lib/DI/ContainerConfigurator.php
+++ mailpoet/lib/DI/ContainerConfigurator.php
@@ -610,6 +610,7 @@ class ContainerConfigurator implements IContainerConfigurator {
     $container->autowire(\MailPoet\Util\Url::class)->setPublic(true);
     $container->autowire(\MailPoet\Util\Installation::class);
     $container->autowire(\MailPoet\Util\Security::class);
+    $container->autowire(\MailPoet\Util\License\Features\CapabilitiesManager::class)->setPublic(true);
     $container->autowire(\MailPoet\Util\License\Features\Subscribers::class)->setPublic(true);
     $container->autowire(\MailPoet\Util\License\License::class)->setPublic(true);
     $container->register(\MailPoet\Util\CdnAssetUrl::class)
