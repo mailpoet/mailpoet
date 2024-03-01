135225396306e2e793781428294ae8d44cc53d3e
Jan Lysý <jan.lysy@automattic.com>
2024-03-01 17:54:54 +0100

    Add screen header component
    [MAILPOET-5640]

---
+++ mailpoet/assets/js/src/email-editor/engine/components/styles-sidebar/screen-header.tsx
@@ -0,0 +1,56 @@
+import {
+  __experimentalHStack as HStack,
+  __experimentalVStack as VStack,
+  __experimentalSpacer as Spacer,
+  __experimentalHeading as Heading,
+  __experimentalView as View,
+  __experimentalNavigatorToParentButton as NavigatorToParentButton,
+} from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { chevronLeft } from '@wordpress/icons';
+
+type Props = {
+  title: string;
+  description?: string;
+  onBack?: () => void;
+};
+
+/**
+ * Component for displaying the screen header and optional description based on site editor component:
+ * https://github.com/WordPress/gutenberg/blob/7fa03fafeb421ab4c3604564211ce6007cc38e84/packages/edit-site/src/components/global-styles/header.js
+ */
+export function ScreenHeader({ title, description, onBack }: Props) {
+  return (
+    <VStack spacing={0}>
+      <View>
+        <Spacer marginBottom={0} paddingX={4} paddingY={3}>
+          <HStack spacing={2}>
+            <NavigatorToParentButton
+              style={{ minWidth: 24, padding: 0 }}
+              icon={chevronLeft}
+              size="small"
+              aria-label={__('Navigate to the previous view')}
+              onClick={onBack}
+            />
+            <Spacer>
+              {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+              {/* @ts-ignore */}
+              <Heading
+                className="mailpoet-email-editor__styles-header"
+                level={2}
+                size={13}
+              >
+                {title}
+              </Heading>
+            </Spacer>
+          </HStack>
+        </Spacer>
+      </View>
+      {description && (
+        <p className="mailpoet-email-editor__styles-header-description">
+          {description}
+        </p>
+      )}
+    </VStack>
+  );
+}
