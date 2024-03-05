dba05a1659d575de44f8003c2305e723b9cfc997
Rostislav Wolny <rosta.wolny@automattic.com>
2024-03-05 15:32:12 +0100

    Add dummy esc_attr function for unit tests
    [MAILPOET-5918]

--- mailpoet/tests/unit/_bootstrap.php
+++ mailpoet/tests/unit/_bootstrap.php
@@ -10,6 +10,11 @@ if (!function_exists('_x')) {
     return $text;
   }
 }
+if (!function_exists('esc_attr')) {
+  function esc_attr($attr) {
+    return $attr;
+  }
+}
 
 // Fix for mocking WPFunctions
 // [PHPUnit\Framework\Exception] Use of undefined constant OBJECT - assumed 'OBJECT' (this will throw an Error in a future version of PHP)
