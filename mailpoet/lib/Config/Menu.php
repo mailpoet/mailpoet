3972a4d7e71cc547822d10b38d2a42092d41f9f5
Jan Jakes <jan.jakes@automattic.com>
2024-03-08 14:26:19 +0100

    Remove automations beta badge
    [MAILPOET-5936]

--- mailpoet/lib/Config/Menu.php
+++ mailpoet/lib/Config/Menu.php
@@ -122,22 +122,6 @@ class Menu {
 
     $this->registerMailPoetMenu();
 
-    // @ToDo Remove Beta once Automation is no longer beta.
-    $this->wp->addAction('admin_head', function () {
-      echo '<style>
-#adminmenu .toplevel_page_mailpoet-homepage a[href="admin.php?page=mailpoet-automation"] {
-  white-space: nowrap;
-}
-.mailpoet-beta-badge {
-  text-transform: uppercase;
-  font-size: 9px;
-  position: relative;
-  top: -5px;
-  color: #ffab66;
-}
-</style>';
-    });
-
     if (!self::isOnMailPoetAdminPage()) {
       return;
     }
@@ -531,8 +515,7 @@ class Menu {
     $automationPage = $this->wp->addSubmenuPage(
       self::MAIN_PAGE_SLUG,
       $this->setPageTitle(__('Automations', 'mailpoet')),
-      // @ToDo Remove Beta once Automation is no longer beta.
-      '<span>' . esc_html__('Automations', 'mailpoet') . '</span><span class="mailpoet-beta-badge">Beta</span>',
+      esc_html__('Automations', 'mailpoet'),
       AccessControl::PERMISSION_MANAGE_EMAILS,
       self::AUTOMATIONS_PAGE_SLUG,
       [$this, 'automation']
