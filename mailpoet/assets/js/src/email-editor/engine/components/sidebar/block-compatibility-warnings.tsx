854162f7c24febcf17d3c72a124a7c5c8465835a
Mike Jolley <mike.jolley@me.com>
2024-03-12 17:01:03 +0000

    Add background support notice

--- mailpoet/assets/js/src/email-editor/engine/components/sidebar/block-compatibility-warnings.tsx
+++ mailpoet/assets/js/src/email-editor/engine/components/sidebar/block-compatibility-warnings.tsx
@@ -1,7 +1,18 @@
-import { hasBlockSupport } from '@wordpress/blocks';
+import { hasBlockSupport, getBlockSupport } from '@wordpress/blocks';
 import { Fill, Notice } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
+
+export const hasBackgroundImageSupport = (nameOrType: string) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore not yet supported in the types
+  const backgroundSupport = getBlockSupport(nameOrType, 'background') as Record<
+    string,
+    boolean
+  >;
+
+  return backgroundSupport && backgroundSupport?.backgroundImage !== false;
+};
 
 export function BlockCompatibilityWarnings(): JSX.Element {
   // Select the currently selected block
@@ -19,18 +30,36 @@ export function BlockCompatibilityWarnings(): JSX.Element {
     false,
   );
 
-  return hasBorderSupport ? (
-    <Fill name="InspectorControlsBorder">
-      <Notice
-        className="mailpoet__grid-full-width"
-        status="warning"
-        isDismissible={false}
-      >
-        {__(
-          'Border display may vary or be unsupported in some email clients.',
-          'mailpoet',
-        )}
-      </Notice>
-    </Fill>
-  ) : null;
+  return (
+    <>
+      {hasBorderSupport && (
+        <Fill name="InspectorControlsBorder">
+          <Notice
+            className="mailpoet__grid-full-width"
+            status="warning"
+            isDismissible={false}
+          >
+            {__(
+              'Border display may vary or be unsupported in some email clients.',
+              'mailpoet',
+            )}
+          </Notice>
+        </Fill>
+      )}
+      {hasBackgroundImageSupport(selectedBlock?.name) && (
+        <Fill name="InspectorControlsBackground">
+          <Notice
+            className="mailpoet__grid-full-width"
+            status="warning"
+            isDismissible={false}
+          >
+            {__(
+              'Select a background color for email clients that do not support background images.',
+              'mailpoet',
+            )}
+          </Notice>
+        </Fill>
+      )}
+    </>
+  );
 }
