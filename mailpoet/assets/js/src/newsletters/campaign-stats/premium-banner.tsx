c3e12f8affcfbdcb1b902d4deb37f0e3955b5180
Brezo Cordero <8002881+brezocordero@users.noreply.github.com>
2024-03-15 17:55:59 -0500

    Remove code that is never reached
    If the limit is reached, window.mailpoet_display_detailed_stats is false
    
    [MAILPOET-5938]

--- mailpoet/assets/js/src/newsletters/campaign-stats/premium-banner.tsx
+++ mailpoet/assets/js/src/newsletters/campaign-stats/premium-banner.tsx
@@ -1,7 +1,6 @@
 import { __ } from '@wordpress/i18n';
 import { MailPoet } from 'mailpoet';
 import { Button } from 'common/button/button';
-import { PremiumRequired } from 'common/premium-required/premium-required';
 import { withBoundary } from '../../common';
 import { PremiumBannerWithUpgrade } from '../../common/premium-banner-with-upgrade/premium-banner-with-upgrade';
 
@@ -48,44 +47,6 @@ function SkipDisplayingDetailedStats() {
 function PremiumBanner() {
   if (!window.mailpoet_display_detailed_stats) {
     return <SkipDisplayingDetailedStats />;
-  }
-  if (window.mailpoet_subscribers_limit_reached) {
-    const hasValidApiKey = window.mailpoet_has_valid_api_key;
-    const title = __('Upgrade required', 'mailpoet');
-    const youReachedTheLimit = hasValidApiKey
-      ? __(
-          'Congratulations, you now have [subscribersCount] subscribers! Your plan is limited to [subscribersLimit] subscribers. You need to upgrade now to be able to continue using MailPoet.',
-          'mailpoet',
-        )
-      : __(
-          'Congratulations, you now have [subscribersCount] subscribers! Our free version is limited to [subscribersLimit] subscribers. You need to upgrade now to be able to continue using MailPoet.',
-          'mailpoet',
-        )
-          .replace('[subscribersLimit]', MailPoet.subscribersLimit.toString())
-          .replace('[subscribersCount]', MailPoet.subscribersCount.toString());
-    const upgradeLink = hasValidApiKey
-      ? MailPoet.MailPoetComUrlFactory.getUpgradeUrl(MailPoet.pluginPartialKey)
-      : MailPoet.MailPoetComUrlFactory.getPurchasePlanUrl(
-          window.mailpoet_subscribers_count + 1,
-        );
-
-    return (
-      <div className="mailpoet-stats-premium-required">
-        <PremiumRequired
-          title={title}
-          message={<p>{youReachedTheLimit}</p>}
-          actionButton={
-            <Button
-              target="_blank"
-              rel="noopener noreferrer"
-              href={upgradeLink}
-            >
-              {__('Upgrade Now', 'mailpoet')}
-            </Button>
-          }
-        />
-      </div>
-    );
   }
   return null;
 }
