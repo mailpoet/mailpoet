07f17a6f71700021e923791ad7591c0733c171a0
Jan Jakes <jan.jakes@automattic.com>
2024-03-21 18:00:18 +0100

    Use PageHeader component
    [MAILPOET-5395]

--- mailpoet/assets/js/src/segments/dynamic/list.tsx
+++ mailpoet/assets/js/src/segments/dynamic/list.tsx
@@ -1,13 +1,44 @@
 import { withRouter } from 'react-router-dom';
-import { TopBarWithBeamer } from 'common/top-bar/top-bar';
-import { ListingHeader } from 'segments/dynamic/list/listing-header';
+import { Button } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 import { ListingTabs } from './list/listing-tabs';
+import * as ROUTES from '../routes';
+import { plusIcon } from '../../common/button/icon/plus';
+import { PageHeader } from '../../common/page-header';
+import { SubscribersCacheMessage } from '../../common/subscribers-cache-message';
+import { SubscribersInPlan } from '../../common/subscribers-in-plan';
+import { TopBarWithBeamer } from '../../common/top-bar/top-bar';
+import { MailPoet } from '../../mailpoet';
+import { MssAccessNotices } from '../../notices/mss-access-notices';
 
 function DynamicSegmentListComponent(): JSX.Element {
   return (
     <>
       <TopBarWithBeamer hideScreenOptions />
-      <ListingHeader />
+      <PageHeader heading={__('Segments', 'mailpoet')}>
+        <Button
+          href={`#${ROUTES.DYNAMIC_SEGMENT_TEMPLATES}`}
+          icon={plusIcon}
+          variant="primary"
+          data-automation-id="new-segment"
+          className="mailpoet-add-new-button"
+        >
+          {__('New segment', 'mailpoet')}
+        </Button>
+      </PageHeader>
+
+      <div className="mailpoet-segment-subscriber-count">
+        <SubscribersInPlan
+          subscribersInPlan={MailPoet.subscribersCount}
+          subscribersInPlanLimit={MailPoet.subscribersLimit}
+          design="new"
+        />
+        <SubscribersCacheMessage
+          cacheCalculation={window.mailpoet_subscribers_counts_cache_created_at}
+          design="new"
+        />
+      </div>
+      <MssAccessNotices />
       <ListingTabs />
     </>
   );
