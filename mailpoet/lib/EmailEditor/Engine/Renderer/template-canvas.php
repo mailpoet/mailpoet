1dc5832879213af4de60013e5eeabe1365dfa649
Mike Jolley <mike.jolley@me.com>
2024-03-22 15:48:37 +0000

    Add main template canvas and consolodate styles

---
+++ mailpoet/lib/EmailEditor/Engine/Renderer/template-canvas.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types = 1);
+
+// phpcs:disable Generic.Files.InlineHTML.Found
+// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
+// phpcs:disable WordPressDotOrg.sniffs.OutputEscaping.UnescapedOutputParameter
+/**
+ * Template file to render the current 'wp_template', specifcally for emails.
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>" />
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="format-detection" content="telephone=no" />
+  <style type="text/css">
+    <?php echo $templateStyles; // @phpstan-ignore-line ?>
+  </style>
+</head>
+<body style="word-spacing:normal;background:<?php echo esc_attr($layoutBackground); // @phpstan-ignore-line ?>;">
+    <div class="email_layout_wrapper" style="background:<?php echo esc_attr($layoutBackground); // @phpstan-ignore-line ?>">
+      <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" width="<?php echo esc_attr($width); // @phpstan-ignore-line ?>" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+      <div style="margin:0px auto;max-width:<?php echo esc_attr($width); // @phpstan-ignore-line ?>">
+        <table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation">
+          <tbody>
+            <tr>
+              <td class="email_preheader" height="1">
+                <?php echo $preHeader; // @phpstan-ignore-line ?>
+              </td>
+            </tr>
+            <tr>
+              <td class="email_content_wrapper" style="font-family: <?php echo esc_attr($contentFontFamily); // @phpstan-ignore-line ?>;padding: <?php echo esc_attr(implode(' ', [$padding['top'], $padding['right'], $padding['bottom'], $padding['left']])); // @phpstan-ignore-line ?>;background: <?php echo esc_attr($contentBackground); // @phpstan-ignore-line ?>;">
+                <?php echo $templateHtml; // @phpstan-ignore-line ?>
+              </td>
+            </tr>
+            <tr>
+              <td class="email_footer">
+                <?php echo $logoHtml; // @phpstan-ignore-line ?>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+    <!--[if mso | IE]></td></tr></table><![endif]-->
+  </div>
+</body>
+</html>
