e4b90d7e38afd4dd88758279530bdfa784e3ca60
Brezo Cordero <8002881+brezocordero@users.noreply.github.com>
2024-03-22 12:30:32 -0500

    Add logic to display new upgrade page
    that will have to be modified after results from a/b test
    
    [MAILPOET-5906]

--- mailpoet/lib/AdminPages/Pages/Upgrade.php
+++ mailpoet/lib/AdminPages/Pages/Upgrade.php
@@ -3,6 +3,7 @@
 namespace MailPoet\AdminPages\Pages;
 
 use MailPoet\AdminPages\PageRenderer;
+use MailPoet\Util\License\Features\CapabilitiesManager;
 use MailPoet\WP\Functions as WPFunctions;
 
 class Upgrade {
@@ -12,18 +13,33 @@ class Upgrade {
   /** @var WPFunctions */
   private $wp;
 
+  private CapabilitiesManager $capabilitiesManager;
+
   public function __construct(
     PageRenderer $pageRenderer,
-    WPFunctions $wp
+    WPFunctions $wp,
+    CapabilitiesManager $capabilitiesManager
   ) {
     $this->pageRenderer = $pageRenderer;
     $this->wp = $wp;
+    $this->capabilitiesManager = $capabilitiesManager;
   }
 
   public function render() {
     $data = [
       'current_wp_user' => $this->wp->wpGetCurrentUser()->to_array(),
     ];
+
+    // @todo change this after a/b test to keep only one of the pages
+    if ($this->capabilitiesManager->showNewUpgradePage()) {
+      $data = [
+        'current_mailpoet_plan_tier' => $this->capabilitiesManager->getTier(),
+      ];
+
+      $this->pageRenderer->displayPage('upgrade_tiers.html', $data);
+      return;
+    }
+
     $this->pageRenderer->displayPage('upgrade.html', $data);
   }
 }
