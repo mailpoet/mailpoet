66b383dd224d05e3b7b70d2b6d1de715de1b742d
Mike Jolley <mike.jolley@me.com>
2024-03-28 11:48:54 +0000

    QA

--- mailpoet/assets/js/src/email-editor/engine/components/styles-sidebar/previews/typography-preview.tsx
+++ mailpoet/assets/js/src/email-editor/engine/components/styles-sidebar/previews/typography-preview.tsx
@@ -23,7 +23,7 @@ export default function TypographyPreview({
   } = elementStyles.typography;
 
   const textColor = elementStyles.color?.text || 'inherit';
-  const backgroundColor = elementStyles.color?.background || '#f0f0f0';
+  const background = elementStyles.color?.background || '#f0f0f0';
 
   const extraStyles =
     element === 'link'
@@ -37,7 +37,7 @@ export default function TypographyPreview({
       className="edit-site-typography-preview"
       style={{
         fontFamily: fontFamily ?? 'serif',
-        background: backgroundColor,
+        background,
         color: textColor,
         lineHeight,
         fontSize,
