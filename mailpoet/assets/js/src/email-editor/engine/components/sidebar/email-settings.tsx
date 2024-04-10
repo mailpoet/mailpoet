1b337f6b0cf6a0b36490751accf4251136af9314
Rostislav Wolny <rosta.wolny@automattic.com>
2024-04-10 15:25:19 +0200

    Add templates experiment panel with save component and post/template toggle WIP

--- mailpoet/assets/js/src/email-editor/engine/components/sidebar/email-settings.tsx
+++ mailpoet/assets/js/src/email-editor/engine/components/sidebar/email-settings.tsx
@@ -1,11 +1,13 @@
 import { Panel } from '@wordpress/components';
 import { DetailsPanel } from './details-panel';
 import { EmailTypeInfo } from './email-type-info';
+import { TemplatesPanel } from './templates-panel';
 
 export function EmailSettings() {
   return (
     <Panel>
       <EmailTypeInfo />
+      <TemplatesPanel />
       <DetailsPanel />
     </Panel>
   );
