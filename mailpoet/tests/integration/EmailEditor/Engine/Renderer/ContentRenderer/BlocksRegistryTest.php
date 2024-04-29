76c60aa5e0660e87b5f5d1747c4510e2d8f1eb5d
Rostislav Wolny <rosta.wolny@automattic.com>
2024-04-29 15:44:27 +0200

    Update BlocksRegistryTest
    - testItAllowsToReplaceRendererViaFilter is no longer needed as we removed the filter
    - testItRemovesAllBlockRenderers this responsibility was moved to ContentRenderer
    [MAILPOET-6014]

--- mailpoet/tests/integration/EmailEditor/Engine/Renderer/ContentRenderer/BlocksRegistryTest.php
+++ mailpoet/tests/integration/EmailEditor/Engine/Renderer/ContentRenderer/BlocksRegistryTest.php
@@ -28,35 +28,10 @@ class BlocksRegistryTest extends \MailPoetTest {
     verify($storedRenderer)->equals($renderer);
   }
 
-  public function testItAllowsToReplaceRendererViaFilter() {
+  public function testItReportsWhichRenderersAreRegistered() {
     $renderer = new Text();
-    $dummyRenderer = new DummyBlockRenderer();
     $this->registry->addBlockRenderer('test', $renderer);
-    $callback = function () use ($dummyRenderer) {
-      return $dummyRenderer;
-    };
-    add_filter('mailpoet_block_renderer_test', $callback);
-    $storedRenderer = $this->registry->getBlockRenderer('test');
-    verify($storedRenderer)->equals($dummyRenderer);
-    remove_filter('mailpoet_block_renderer_test', $callback);
-  }
-
-  public function testItRemovesAllBlockRenderers() {
-    $renderer = new Text();
-    verify(has_filter('render_block_test'))->false();
-    verify(has_filter('render_block_test2'))->false();
-
-    $this->registry->addBlockRenderer('test', $renderer);
-    $this->registry->addBlockRenderer('test2', $renderer);
-    verify(has_filter('render_block_test'))->true();
-    verify(has_filter('render_block_test2'))->true();
-    verify($this->registry->getBlockRenderer('test'))->notNull();
-    verify($this->registry->getBlockRenderer('test2'))->notNull();
-
-    $this->registry->removeAllBlockRenderers();
-    verify(has_filter('render_block_test'))->false();
-    verify(has_filter('render_block_test2'))->false();
-    verify($this->registry->getBlockRenderer('test'))->null();
-    verify($this->registry->getBlockRenderer('test2'))->null();
+    verify($this->registry->hasBlockRenderer('test'))->true();
+    verify($this->registry->hasBlockRenderer('unknown'))->false();
   }
 }
