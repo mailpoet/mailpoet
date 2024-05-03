9ce541753a5d399d63ba2eb7be87b6f62f770754
Rostislav Wolny <rosta.wolny@automattic.com>
2024-05-03 15:06:04 +0200

    Update acceptance tests to close the template selection modal
    [MAILPOET-6049]

--- mailpoet/tests/acceptance/EmailEditor/CreateAndSendEmailUsingGutenbergCest.php
+++ mailpoet/tests/acceptance/EmailEditor/CreateAndSendEmailUsingGutenbergCest.php
@@ -26,6 +26,8 @@ class CreateAndSendEmailUsingGutenbergCest {
     $i->waitForText('Create modern, beautiful emails that embody your brand with advanced customization and editing capabilities.');
     $i->click('//button[text()="Continue"]');
 
+    $this->closeTemplateSelectionModal($i);
+
     $i->wantTo('Compose an email');
     $i->waitForElement('.editor-canvas__iframe');
     $i->switchToIFrame('.editor-canvas__iframe');
@@ -94,6 +96,8 @@ class CreateAndSendEmailUsingGutenbergCest {
     $i->waitForText('Create modern, beautiful emails that embody your brand with advanced customization and editing capabilities.');
     $i->click('//button[text()="Continue"]');
 
+    $this->closeTemplateSelectionModal($i);
+
     $i->wantTo('Edit an email');
     $i->waitForElement('.editor-canvas__iframe');
     $i->switchToIFrame('.editor-canvas__iframe');
@@ -136,5 +140,11 @@ class CreateAndSendEmailUsingGutenbergCest {
       return false;
     }
     return true;
+  }
+
+  private function closeTemplateSelectionModal(\AcceptanceTester $i): void {
+    $i->wantTo('Close template selector');
+    $i->waitForElementVisible('.block-editor-block-preview__container');
+    $i->click('[aria-label="Close"]');
   }
 }
