246cb7f3122f6402cdf172356074abab728f5d77
Rostislav Wolny <rosta.wolny@automattic.com>
2024-05-07 11:14:02 +0200

    Configure inserter panel to use post content as a root
    [MAILPOET-6059]

--- mailpoet/assets/js/src/email-editor/engine/components/inserter-sidebar/inserter-sidebar.tsx
+++ mailpoet/assets/js/src/email-editor/engine/components/inserter-sidebar/inserter-sidebar.tsx
@@ -1,10 +1,31 @@
-import { __experimentalLibrary as Library } from '@wordpress/block-editor';
+import {
+  __experimentalLibrary as Library,
+  store as blockEditorStore,
+} from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
+import { store as editorStore } from '@wordpress/editor';
 
 export function InserterSidebar() {
+  const { postContentId, isEditingEmailContent } = useSelect((select) => {
+    const blocks = select(blockEditorStore).getBlocks();
+    return {
+      postContentId: blocks.find((block) => block.name === 'core/post-content')
+        ?.clientId,
+      isEditingEmailContent:
+        // @ts-expect-error No types for this exist yet.
+        select(editorStore).getCurrentPostType() !== 'wp_template',
+    };
+  });
+
   return (
     <div className="edit-post-editor__inserter-panel ">
       <div className="edit-post-editor__inserter-panel-content">
-        <Library showMostUsedBlocks showInserterHelpPanel={false} />
+        <Library
+          showMostUsedBlocks
+          showInserterHelpPanel={false}
+          // In the email content mode we insert primarily into the post content block.
+          rootClientId={isEditingEmailContent ? postContentId : null}
+        />
       </div>
     </div>
   );
