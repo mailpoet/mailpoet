9b921a0cacfc7f5e036e3c2bc8311c3fa9c1258d
Rostislav Wolny <rosta.wolny@automattic.com>
2024-05-13 08:00:16 -0400

    Prevent reopening modal for unsaved email when switching modes
    [MAILPOET-6049]

--- mailpoet/assets/js/src/email-editor/engine/components/template-select/template-selection.tsx
+++ mailpoet/assets/js/src/email-editor/engine/components/template-select/template-selection.tsx
@@ -5,14 +5,14 @@ import { SelectTemplateModal } from './select-modal';
 
 export function TemplateSelection() {
   const [templateSelected, setTemplateSelected] = useState(false);
-  const { emailContentIsEmpty } = useSelect(
+  const { emailContentIsEmpty, emailHasEdits } = useSelect(
     (select) => ({
       emailContentIsEmpty: select(storeName).hasEmptyContent(),
+      emailHasEdits: select(storeName).hasEdits(),
     }),
     [],
   );
-
-  if (!emailContentIsEmpty || templateSelected) {
+  if (!emailContentIsEmpty || emailHasEdits || templateSelected) {
     return null;
   }
 
