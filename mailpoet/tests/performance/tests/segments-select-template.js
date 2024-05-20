f44dd0394f52c00f850f5247821619630d8085a1
Veljko <veljano@yahoo.com>
2024-05-20 15:10:53 +0200

    Add missing comma

--- mailpoet/tests/performance/tests/segments-select-template.js
+++ mailpoet/tests/performance/tests/segments-select-template.js
@@ -23,7 +23,7 @@ import {
 import {
   login,
   focusAndClick,
-  waitForSelectorToBeClickable
+  waitForSelectorToBeClickable,
 } from '../utils/helpers.js';
 
 export async function segmentsSelectTemplate() {
