9cec1b3093879d659babff2c818528753eb5a969
Mike Jolley <mike.jolley@me.com>
2024-05-23 14:26:35 +0100

    Update classnames to fix toolbar alignment

--- mailpoet/assets/js/src/email-editor/engine/components/inserter-sidebar/inserter-sidebar.tsx
+++ mailpoet/assets/js/src/email-editor/engine/components/inserter-sidebar/inserter-sidebar.tsx
@@ -18,7 +18,7 @@ export function InserterSidebar() {
   });
 
   return (
-    <div className="edit-post-editor__inserter-panel ">
+    <div className="edit-post-editor__inserter-panel">
       <div className="edit-post-editor__inserter-panel-content">
         <Library
           showMostUsedBlocks
