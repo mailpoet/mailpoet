53fa0c26ec3edff3afad1c815c1de8ec77fb84a0
alex-mpoet <alex-mpoet@users.noreply.github.com>
2024-05-25 10:04:14 +0300

    Reword sending paused message for Creator plans
    [MAILPOET-6026]

--- mailpoet/assets/js/src/notices/invalid-mss-key-notice.tsx
+++ mailpoet/assets/js/src/notices/invalid-mss-key-notice.tsx
@@ -4,11 +4,67 @@ import ReactStringReplace from 'react-string-replace';
 
 type Props = {
   mssKeyInvalid: boolean;
+  pluginPartialKey: string;
+  premiumKeyValid: boolean;
   subscribersCount: number;
 };
 
-function InvalidMssKeyNotice({ mssKeyInvalid, subscribersCount }: Props) {
+function InvalidMssKeyNotice({
+  mssKeyInvalid,
+  pluginPartialKey,
+  premiumKeyValid,
+  subscribersCount,
+}: Props) {
   if (!mssKeyInvalid) return null;
+  if (premiumKeyValid) {
+    const upgradeLink = `${MailPoet.MailPoetComUrlFactory.getUpgradeUrl(
+      pluginPartialKey,
+      { s: subscribersCount, capabilities: { sendingService: true } },
+    )}&utm_source=plugin&utm_medium=premium&utm_campaign=upgrade&ref=creator-sending-paused-message`;
+    return (
+      <Notice type="error" timeout={false} closable={false} renderInPlace>
+        <h3>{MailPoet.I18n.t('allSendingPausedHeader')}</h3>
+        <p>
+          {ReactStringReplace(
+            MailPoet.I18n.t('allSendingPausedPremiumValidBody'),
+            /\[link\](.*?)\[\/link\]/g,
+            (match, index) => {
+              // first link tag
+              if (index === 1) {
+                return (
+                  <a
+                    href={upgradeLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    key="purchase-plan"
+                  >
+                    {match}
+                  </a>
+                );
+              }
+              // second link tag
+              return (
+                <a href="?page=mailpoet-settings#premium" key="check-sending">
+                  {match}
+                </a>
+              );
+            },
+          )}
+        </p>
+        <p>
+          <a
+            href={upgradeLink}
+            className="button button-primary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {MailPoet.I18n.t('allSendingPausedPremiumValidLink')}
+          </a>
+        </p>
+      </Notice>
+    );
+  }
+  // Premium key invalid
   return (
     <Notice type="error" timeout={false} closable={false} renderInPlace>
       <h3>{MailPoet.I18n.t('allSendingPausedHeader')}</h3>
