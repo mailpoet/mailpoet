ea00c74d469896249863f02b5899768007269833
Veljko <veljano@yahoo.com>
2024-05-29 17:14:28 +0200

    Improve the form test with multiple lists

--- mailpoet/tests/acceptance/Forms/SubscribeToMultipleListsCest.php
+++ mailpoet/tests/acceptance/Forms/SubscribeToMultipleListsCest.php
@@ -3,6 +3,8 @@
 namespace MailPoet\Test\Acceptance;
 
 use Codeception\Util\Locator;
+use MailPoet\DI\ContainerWrapper;
+use MailPoet\Form\FormMessageController;
 use MailPoet\Subscription\Captcha\CaptchaConstants;
 use MailPoet\Test\DataFactories\Form;
 use MailPoet\Test\DataFactories\Segment;
@@ -24,6 +26,7 @@ class SubscribeToMultipleListsCest {
     $formName = 'Multiple Lists Form';
     $formFactory = new Form();
     $form = $formFactory->withName($formName)->withSegments([$segment1, $segment2, $segment3])->create();
+    $formId = $form->getId();
 
     $settings = new Settings();
     $settings
@@ -34,9 +37,30 @@ class SubscribeToMultipleListsCest {
 
     $formFactory->withDefaultSuccessMessage();
 
-    //Add this form to a widget
-    $i->createFormAndSubscribe();
-    //Subscribe via that form
+    $i->havePostInDatabase([
+      'post_author' => 1,
+      'post_type' => 'page',
+      'post_name' => 'form-test',
+      'post_title' => 'Form Test',
+      'post_content' => '
+        Regular form:
+          [mailpoet_form id="' . $formId . '"]
+        Iframe form:
+          <iframe class="mailpoet_form_iframe" id="mailpoet_form_iframe" tabindex="0" src="http://test.local?mailpoet_form_iframe=1" width="100%" height="100%" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+      ',
+      'post_status' => 'publish',
+    ]);
+  
+    $i->wantTo('Subscribe to the form with multiple lists attached');
+    $i->amOnPage('/form-test');
+    $i->waitForElement('[data-automation-id="form_email"]');
+    $i->fillField('[data-automation-id="form_email"]', 'subscriber@example.com');
+    $i->click('[data-automation-id="subscribe-submit-button"]');
+    $messageController = ContainerWrapper::getInstance()->get(FormMessageController::class);
+    $i->waitForText($messageController->getDefaultSuccessMessage(), 30, '.mailpoet_validate_success');
+    $i->seeNoJSErrors();
+
+    $i->wantTo('Confirm subscription to subscribed form');
     $i->amOnMailboxAppPage();
     $i->click(Locator::contains('span.subject', 'Subscribe to multiple test subject'));
     $i->switchToIframe('#preview-html');
@@ -44,5 +68,13 @@ class SubscribeToMultipleListsCest {
     $i->switchToNextTab();
     $i->see('You have subscribed');
     $i->seeNoJSErrors();
+
+    $i->wantTo('Check if the three lists are present for the subscribed user');
+    $i->amOnUrl(\AcceptanceTester::WP_URL);
+    $i->login();
+    $i->amOnMailpoetPage('Subscribers');
+    $i->waitForText($seg1);
+    $i->waitForText($seg2);
+    $i->waitForText($seg3);
   }
 }
