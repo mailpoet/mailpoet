28d09b8b5e3b44df6f459fa4ff536c4a123dd43c
alex-mpoet <alex-mpoet@users.noreply.github.com>
2024-06-03 16:34:53 +0300

    Add cron ping error notice
    [MAILPOET-6029]

---
+++ mailpoet/assets/js/src/notices/cron-ping-error-notice.tsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from 'react';
+import { extractPageNameFromUrl } from 'common/functions';
+import { Notice } from 'notices/notice';
+import { MailPoet } from 'mailpoet';
+import ReactStringReplace from 'react-string-replace';
+
+function CronPingErrorNotice() {
+  const [cronError, setCronError] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Show only when cron trigger method is WordPress
+    if (MailPoet.cronTriggerMethod !== 'WordPress') return;
+    // Do not show on the MailPoet Help page, it has its own cron status indication
+    if (extractPageNameFromUrl() === 'help') return;
+
+    void MailPoet.Ajax.post({
+      api_version: MailPoet.apiVersion,
+      endpoint: 'sendingQueue',
+      action: 'pingCron',
+    })
+      .done(() => {
+        // Do not display the notice if there are no errors
+        setCronError(null);
+      })
+      .fail((errorResponse) => {
+        const errObject = errorResponse?.errors?.[0];
+        if (errObject.error && errObject.message) {
+          // Display cron error if server responded with error message
+          setCronError(errObject.message);
+        } else {
+          // Do not display the notice if there is no server response
+          setCronError(null);
+        }
+      });
+  }, []);
+
+  if (!cronError) return null;
+  return (
+    <Notice type="error" timeout={false} closable={false} renderInPlace>
+      <h3>{MailPoet.I18n.t('cronPingErrorHeader')}</h3>
+      <p>
+        {ReactStringReplace(
+          `${MailPoet.I18n.t(
+            'systemStatusConnectionUnsuccessful',
+          )} ${MailPoet.I18n.t('systemStatusCronConnectionUnsuccessfulInfo')}`,
+          /\[link\](.*?)\[\/link\]/g,
+          (match) => (
+            <a
+              href="https://kb.mailpoet.com/article/231-sending-does-not-work"
+              target="_blank"
+              rel="noopener noreferrer"
+              key="cron-ping-error-kb-link"
+            >
+              {match}
+            </a>
+          ),
+        )}
+      </p>
+      {cronError ? (
+        <p>
+          <i>{cronError}</i>
+        </p>
+      ) : null}
+      <p>{MailPoet.I18n.t('systemStatusIntroCron')}</p>
+    </Notice>
+  );
+}
+
+CronPingErrorNotice.displayName = 'CronPingErrorNotice';
+export { CronPingErrorNotice };
