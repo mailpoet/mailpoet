a31d81d4de52145bf8fba7f85ca249de0187b56a
alex-mpoet <alex-mpoet@users.noreply.github.com>
2024-06-04 12:25:07 +0300

    Cache successful cron ping result
    [MAILPOET-6029]

--- mailpoet/assets/js/src/notices/cron-ping-error-notice.tsx
+++ mailpoet/assets/js/src/notices/cron-ping-error-notice.tsx
@@ -4,6 +4,20 @@ import { Notice } from 'notices/notice';
 import { MailPoet } from 'mailpoet';
 import ReactStringReplace from 'react-string-replace';
 
+const LOCAL_STORAGE_KEY = 'mailpoet_cron_ping_last_success_at';
+const CACHE_TIME = 60 * 60 * 1000; // 1 hour
+
+const cacheSuccessfulResult = () => {
+  window.localStorage.setItem(LOCAL_STORAGE_KEY, Date.now().toString());
+};
+
+const isSuccessfulResultCached = () => {
+  const lastSuccessAt = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+  return (
+    lastSuccessAt && Date.now() - parseInt(lastSuccessAt, 10) <= CACHE_TIME
+  );
+};
+
 function CronPingErrorNotice() {
   const [cronError, setCronError] = useState<string | null>(null);
 
@@ -13,12 +27,16 @@ function CronPingErrorNotice() {
     // Do not show on the MailPoet Help page, it has its own cron status indication
     if (extractPageNameFromUrl() === 'help') return;
 
+    // Don't make a request if we have a recently cached successful result
+    if (isSuccessfulResultCached()) return;
+
     void MailPoet.Ajax.post({
       api_version: MailPoet.apiVersion,
       endpoint: 'sendingQueue',
       action: 'pingCron',
     })
       .done(() => {
+        cacheSuccessfulResult();
         // Do not display the notice if there are no errors
         setCronError(null);
       })
