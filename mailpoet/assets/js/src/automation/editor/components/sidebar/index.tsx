0d62d28d82a2154c55685c7c5954f96205bc0e32
Rostislav Wolny <rosta.wolny@automattic.com>
2024-06-04 15:02:13 +0200

    Refactor automation editor sidebar tabs to use Tabs component
    Gutenberg switched to tabs and removed CSS used for <ul>.
    In this commit, we switched to the Tabs component.
    [MAILPOET-6054]

--- mailpoet/assets/js/src/automation/editor/components/sidebar/index.tsx
+++ mailpoet/assets/js/src/automation/editor/components/sidebar/index.tsx
@@ -1,22 +1,24 @@
 import { ComponentProps } from 'react';
-import { useSelect } from '@wordpress/data';
-import { Platform } from '@wordpress/element';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { Platform, useRef, useContext } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { drawerRight } from '@wordpress/icons';
 import {
   ComplementaryArea,
   store as interfaceStore,
 } from '@wordpress/interface';
+import { privateApis as componentsPrivateApis } from '@wordpress/components';
 import { store as keyboardShortcutsStore } from '@wordpress/keyboard-shortcuts';
 import { Header } from './header';
 import { StepSidebar } from './step';
 import { AutomationSidebar } from './automation';
 import { stepSidebarKey, storeName, automationSidebarKey } from '../../store';
+import { unlock } from '../../../lock-unlock';
+
+const { Tabs } = unlock(componentsPrivateApis);
 
 // See:
-//   https://github.com/WordPress/gutenberg/blob/5caeae34b3fb303761e3b9432311b26f4e5ea3a6/packages/edit-post/src/components/sidebar/plugin-sidebar/index.js
-//   https://github.com/WordPress/gutenberg/blob/5caeae34b3fb303761e3b9432311b26f4e5ea3a6/packages/edit-post/src/components/sidebar/settings-sidebar/index.js
-//   https://github.com/WordPress/gutenberg/blob/0ee78b1bbe9c6f3e6df99f3b967132fa12bef77d/packages/edit-site/src/components/sidebar/index.js
+//   https://github.com/WordPress/gutenberg/blob/e841c9e52d28ba314a535065f9723ec0bc40342c/packages/editor/src/components/sidebar/index.js
 
 const sidebarActiveByDefault = Platform.select({
   web: true,
@@ -25,7 +27,7 @@ const sidebarActiveByDefault = Platform.select({
 
 type Props = ComponentProps<typeof ComplementaryArea>;
 
-export function Sidebar(props: Props): JSX.Element {
+function SidebarContent(props: Props): JSX.Element {
   const { keyboardShortcut, sidebarKey, showIconLabels, automationName } =
     useSelect(
       (select) => ({
@@ -43,12 +45,20 @@ export function Sidebar(props: Props): JSX.Element {
       [],
     );
 
+  const tabListRef = useRef(null);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const tabsContextValue = useContext(Tabs.Context);
+
   return (
     <ComplementaryArea
       identifier={sidebarKey}
-      header={<Header sidebarKey={sidebarKey} />}
+      header={
+        <Tabs.Context.Provider value={tabsContextValue}>
+          <Header ref={tabListRef} />
+        </Tabs.Context.Provider>
+      }
       closeLabel={__('Close settings', 'mailpoet')}
-      headerClassName="edit-site-sidebar-edit-mode__panel-tabs"
+      headerClassName="editor-sidebar__panel-tabs"
       title={__('Settings', 'mailpoet')}
       icon={drawerRight}
       className="edit-site-sidebar mailpoet-automation-sidebar"
@@ -60,8 +70,33 @@ export function Sidebar(props: Props): JSX.Element {
       showIconLabels={showIconLabels}
       {...props}
     >
-      {sidebarKey === automationSidebarKey && <AutomationSidebar />}
-      {sidebarKey === stepSidebarKey && <StepSidebar />}
+      <Tabs.Context.Provider value={tabsContextValue}>
+        <Tabs.TabPanel tabId={automationSidebarKey}>
+          <AutomationSidebar />
+        </Tabs.TabPanel>
+        <Tabs.TabPanel tabId={stepSidebarKey}>
+          <StepSidebar />
+        </Tabs.TabPanel>
+      </Tabs.Context.Provider>
     </ComplementaryArea>
+  );
+}
+
+export function Sidebar(props: Props): JSX.Element {
+  const { openSidebar } = useDispatch(storeName);
+
+  const { sidebarKey } = useSelect(
+    (select) => ({
+      sidebarKey:
+        select(interfaceStore).getActiveComplementaryArea(storeName) ??
+        automationSidebarKey,
+    }),
+    [],
+  );
+
+  return (
+    <Tabs selectedTabId={sidebarKey} onSelect={openSidebar}>
+      <SidebarContent {...props} />
+    </Tabs>
   );
 }
