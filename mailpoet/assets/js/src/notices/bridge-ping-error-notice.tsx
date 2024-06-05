5f598adee262d65c9e6677e89da43ba9605d5f9b
alex-mpoet <alex-mpoet@users.noreply.github.com>
2024-06-05 17:15:29 +0300

    Extract ping notices common code to useServiceCheck hook
    [MAILPOET-6030]

--- mailpoet/assets/js/src/notices/bridge-ping-error-notice.tsx
+++ mailpoet/assets/js/src/notices/bridge-ping-error-notice.tsx
@@ -1,56 +1,19 @@
-import { useState, useEffect } from 'react';
 import { extractPageNameFromUrl } from 'common/functions';
 import { Notice } from 'notices/notice';
 import { MailPoet } from 'mailpoet';
 import ReactStringReplace from 'react-string-replace';
-
-const LOCAL_STORAGE_KEY = 'mailpoet_mss_ping_last_success_at';
-const CACHE_TIME = 60 * 60 * 1000; // 1 hour
-
-const cacheSuccessfulResult = () => {
-  window.localStorage.setItem(LOCAL_STORAGE_KEY, Date.now().toString());
-};
-
-const isSuccessfulResultCached = () => {
-  const lastSuccessAt = window.localStorage.getItem(LOCAL_STORAGE_KEY);
-  return (
-    lastSuccessAt && Date.now() - parseInt(lastSuccessAt, 10) <= CACHE_TIME
-  );
-};
+import { useServiceCheck } from './hooks/use-service-check';
 
 function BridgePingErrorNotice() {
-  const [bridgeError, setBridgeError] = useState<string | null>(null);
-
-  useEffect(() => {
-    // Show only when sending method is MailPoet
-    if (MailPoet.mtaMethod !== 'MailPoet') return;
-    // Do not show on the MailPoet Help page, it has its own MSS status indication
-    if (extractPageNameFromUrl() === 'help') return;
-
-    // Don't make a request if we have a recently cached successful result
-    if (isSuccessfulResultCached()) return;
-
-    void MailPoet.Ajax.post({
-      api_version: MailPoet.apiVersion,
-      endpoint: 'Services',
-      action: 'pingBridge',
-    })
-      .done(() => {
-        cacheSuccessfulResult();
-        // Do not display the notice if there are no errors
-        setBridgeError(null);
-      })
-      .fail((errorResponse) => {
-        const errObject = errorResponse?.errors?.[0];
-        if (errObject.error && errObject.message) {
-          // Display MSS error if server responded with error message
-          setBridgeError(errObject.message);
-        } else {
-          // Do not display the notice if there is no server response
-          setBridgeError(null);
-        }
-      });
-  }, []);
+  const bridgeError = useServiceCheck(
+    'Services',
+    'pingBridge',
+    () =>
+      // Show only when sending method is MailPoet
+      MailPoet.mtaMethod === 'MailPoet' &&
+      // Do not show on the MailPoet Help page, it has its own MSS status indication
+      extractPageNameFromUrl() !== 'help',
+  );
 
   if (!bridgeError) return null;
   return (
--- mailpoet/assets/js/src/notices/cron-ping-error-notice.tsx
+++ mailpoet/assets/js/src/notices/cron-ping-error-notice.tsx
@@ -1,56 +1,19 @@
-import { useState, useEffect } from 'react';
 import { extractPageNameFromUrl } from 'common/functions';
 import { Notice } from 'notices/notice';
 import { MailPoet } from 'mailpoet';
 import ReactStringReplace from 'react-string-replace';
-
-const LOCAL_STORAGE_KEY = 'mailpoet_cron_ping_last_success_at';
-const CACHE_TIME = 60 * 60 * 1000; // 1 hour
-
-const cacheSuccessfulResult = () => {
-  window.localStorage.setItem(LOCAL_STORAGE_KEY, Date.now().toString());
-};
-
-const isSuccessfulResultCached = () => {
-  const lastSuccessAt = window.localStorage.getItem(LOCAL_STORAGE_KEY);
-  return (
-    lastSuccessAt && Date.now() - parseInt(lastSuccessAt, 10) <= CACHE_TIME
-  );
-};
+import { useServiceCheck } from './hooks/use-service-check';
 
 function CronPingErrorNotice() {
-  const [cronError, setCronError] = useState<string | null>(null);
-
-  useEffect(() => {
-    // Show only when cron trigger method is WordPress
-    if (MailPoet.cronTriggerMethod !== 'WordPress') return;
-    // Do not show on the MailPoet Help page, it has its own cron status indication
-    if (extractPageNameFromUrl() === 'help') return;
-
-    // Don't make a request if we have a recently cached successful result
-    if (isSuccessfulResultCached()) return;
-
-    void MailPoet.Ajax.post({
-      api_version: MailPoet.apiVersion,
-      endpoint: 'sendingQueue',
-      action: 'pingCron',
-    })
-      .done(() => {
-        cacheSuccessfulResult();
-        // Do not display the notice if there are no errors
-        setCronError(null);
-      })
-      .fail((errorResponse) => {
-        const errObject = errorResponse?.errors?.[0];
-        if (errObject.error && errObject.message) {
-          // Display cron error if server responded with error message
-          setCronError(errObject.message);
-        } else {
-          // Do not display the notice if there is no server response
-          setCronError(null);
-        }
-      });
-  }, []);
+  const cronError = useServiceCheck(
+    'sendingQueue',
+    'pingCron',
+    () =>
+      // Show only when sending method is MailPoet
+      MailPoet.cronTriggerMethod === 'WordPress' &&
+      // Do not show on the MailPoet Help page, it has its own MSS status indication
+      extractPageNameFromUrl() !== 'help',
+  );
 
   if (!cronError) return null;
   return (
