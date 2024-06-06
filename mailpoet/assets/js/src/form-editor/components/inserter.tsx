d1a4d80459964027285635156e66c24e6da73f67
Rostislav Wolny <rosta.wolny@automattic.com>
2024-06-06 13:01:44 +0200

    Fix the size and scrolling of the inserter sidebar in the form editor
    [[MAILPOET-6054]

--- mailpoet/assets/js/src/form-editor/components/inserter.tsx
+++ mailpoet/assets/js/src/form-editor/components/inserter.tsx
@@ -14,11 +14,11 @@ export function Inserter({ setIsInserterOpened }: Props): JSX.Element {
     [],
   );
   return (
-    <div className="edit-post-editor__inserter-panel">
-      <div className="edit-post-editor__inserter-panel-header">
+    <div className="editor-inserter-sidebar">
+      <div className="editor-inserter-sidebar__header">
         <Button icon={close} onClick={(): void => setIsInserterOpened(false)} />
       </div>
-      <div className="edit-post-editor__inserter-panel-content">
+      <div className="editor-inserter-sidebar__content">
         <Library
           showMostUsedBlocks
           showInserterHelpPanel={false}
