9dda299c581ca030db41a5e73e61f0a8b2f60944
Rostislav Wolny <rosta.wolny@automattic.com>
2024-06-07 14:12:47 +0200

    Fix font size settings in the email editor
    The size picker reads from a different settings property
    [MAILPOET-6054]

--- mailpoet/assets/js/src/form-editor/components/font-size-settings.tsx
+++ mailpoet/assets/js/src/form-editor/components/font-size-settings.tsx
@@ -1,11 +1,11 @@
 import { FontSizePicker } from '@wordpress/components';
-import { useSetting } from '@wordpress/block-editor';
+import { useSettings } from '@wordpress/block-editor';
 import { FontSizePickerProps } from '@wordpress/components/src/font-size-picker/types';
 
 type Props = Pick<FontSizePickerProps, 'value' | 'onChange'>;
 
 export function FontSizeSettings({ value, onChange }: Props): JSX.Element {
-  const fontSizes = useSetting('typography.fontSizes');
+  const [fontSizes] = useSettings('typography.fontSizes');
   return (
     <FontSizePicker
       value={value}
