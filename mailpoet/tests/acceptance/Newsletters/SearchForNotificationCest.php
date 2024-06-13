4c21a257748da18efddefba03b07300304a7d7dd
Oluwaseun Olorunsola <seun.olorunsola@automattic.com>
2024-06-13 18:29:04 +0100

    Add tests for feature and switch to leftJoin instead of InnerJoin because we always want to return Newsletter records when available
    MAILPOET-6072

--- mailpoet/tests/acceptance/Newsletters/SearchForNotificationCest.php
+++ mailpoet/tests/acceptance/Newsletters/SearchForNotificationCest.php
@@ -4,6 +4,7 @@ namespace MailPoet\Test\Acceptance;
 
 use MailPoet\Test\DataFactories\Newsletter;
 use MailPoet\Test\DataFactories\Segment;
+use MailPoet\Test\DataFactories\Subscriber;
 
 class SearchForNotificationCest {
   public function searchForStandardNotification(\AcceptanceTester $i) {
@@ -46,5 +47,52 @@ class SearchForNotificationCest {
     $i->click('[data-automation-id="filters_not_active"]');
     $i->searchFor($newsletterTitle);
     $i->waitForText('No emails found.', 15, '[data-automation-id="newsletters_listing_tabs"]');
+  }
+
+  public function searchForStandardNotificationHistoryItem(\AcceptanceTester $i) {
+    $i->wantTo('Successfully search for an active notification history item');
+
+    $newsletterTitle = 'New Post Alert [newsletter:post_title]';
+    $segmentName = 'New post alert list';
+    $postTitle = 'Hello how do you do';
+
+    // step 1 - Prepare newsletter data
+    $segmentFactory = new Segment();
+    $segment = $segmentFactory->withName($segmentName)->create();
+
+    $subscriberFactory = new Subscriber();
+    $subscriberFactory->withSegments([$segment])->create();
+
+    $newsletterFactory = new Newsletter();
+    $newsletter = $newsletterFactory->withSubject($newsletterTitle)
+      ->withPostNotificationsType()
+      ->withSegments([$segment])
+      ->withActiveStatus()
+      ->withImmediateSendingSettings()
+      ->create();
+
+
+    $postNotificationHistory = (new Newsletter())
+      ->withSubject($postTitle)
+      ->withPostNotificationHistoryType()
+      ->withParent($newsletter)
+      ->create();
+
+    // step 2 - Search post-notification
+    $i->login();
+
+
+    $i->amOnMailpoetPage('Emails');
+    $i->click('Post Notifications', '[data-automation-id="newsletters_listing_tabs"]');
+    $i->waitForListingItemsToLoad();
+    $i->searchFor($newsletterTitle);
+    $i->waitForText($newsletterTitle, 15, '[data-automation-id="newsletters_listing_tabs"]'); // confirm post-notification as created
+
+    // step 3 - Search post-notification history item
+    $selector = sprintf('[data-automation-id="history-%d"]', $newsletter->getId());
+    $i->click($selector);
+     $i->waitForElement(sprintf('[data-automation-id="listing_item_%d"]', $postNotificationHistory->getId()));
+    $i->searchFor($postTitle); // search by rendered post title
+    $i->waitForText($postTitle, 15, '[data-automation-id="newsletters_listing_tabs"]');
   }
 }
