1bb99ad40b1a9706bdc8f9ae7b4bb825a9234375
 Ján Mikláš <neosinner@gmail.com>
2024-06-17 18:01:02 +0200

    Refactor tasks-list-labels-row to TypeScript
    [MAILPOET-5755]

--- mailpoet/assets/js/src/help/tasks-list/tasks-list-labels-row.tsx
+++ mailpoet/assets/js/src/help/tasks-list/tasks-list-labels-row.tsx
@@ -1,35 +1,32 @@
-import PropTypes from 'prop-types';
 import { MailPoet } from 'mailpoet';
 
-function TasksListLabelsRow(props) {
+type Props = {
+  showScheduledAt?: boolean;
+  showCancelledAt?: boolean;
+};
+
+function TasksListLabelsRow({
+  showScheduledAt = false,
+  showCancelledAt = false,
+}: Props): JSX.Element {
   return (
     <tr>
       <th className="row-title">Id</th>
       <th className="row-title">{MailPoet.I18n.t('email')}</th>
       <th className="row-title">{MailPoet.I18n.t('subscriber')}</th>
       <th className="row-title">{MailPoet.I18n.t('priority')}</th>
-      {props.show_scheduled_at ? (
+      {showScheduledAt ? (
         <th className="row-title">{MailPoet.I18n.t('scheduledAt')}</th>
       ) : null}
-      {props.show_cancelled_at ? (
+      {showCancelledAt ? (
         <th className="row-title">{MailPoet.I18n.t('cancelledAt')}</th>
       ) : null}
       <th className="row-title">{MailPoet.I18n.t('updatedAt')}</th>
-      {props.show_scheduled_at || props.show_cancelled_at ? (
+      {showScheduledAt || showCancelledAt ? (
         <th className="row-title">{MailPoet.I18n.t('action')}</th>
       ) : null}
     </tr>
   );
 }
-
-TasksListLabelsRow.propTypes = {
-  show_scheduled_at: PropTypes.bool,
-  show_cancelled_at: PropTypes.bool,
-};
-
-TasksListLabelsRow.defaultProps = {
-  show_scheduled_at: false,
-  show_cancelled_at: false,
-};
 
 export { TasksListLabelsRow };
