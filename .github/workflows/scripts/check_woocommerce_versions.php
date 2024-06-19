2fcd159e7ee142abd32af789aab4728a42bc0665
Jan Lysý <jan.lysy@automattic.com>
2024-06-19 20:35:59 +0200

    Update script getting WooCommerce versions and store them to file
    [MAILPOET-6097]

--- .github/workflows/scripts/check_woocommerce_versions.php
+++ .github/workflows/scripts/check_woocommerce_versions.php
@@ -2,7 +2,10 @@
 
 require_once __DIR__ . '/helpers.php';
 
-function getWooCommerceVersions() {
+/**
+ * We get the official WooCommerce versions from the WordPress API.
+ */
+function getWooCommerceVersions(): array {
   $url = "https://api.wordpress.org/plugins/info/1.0/woocommerce.json";
   $response = file_get_contents($url);
   $data = json_decode($response, true);
@@ -14,7 +17,15 @@ function getWooCommerceVersions() {
   return array_keys($data['versions']);
 }
 
-function replacePreviousVersion($previousVersion) {
+function replaceLatestVersion($latestVersion): void {
+  replaceVersionInFile(
+    __DIR__ . './../../../.circleci/config.yml',
+    '/(.\/do download:woo-commerce-zip )\d+\.\d+\.\d+/',
+    '${1}' . $latestVersion
+  );
+}
+
+function replacePreviousVersion($previousVersion): void {
   replaceVersionInFile(
     __DIR__ . './../../../.circleci/config.yml',
     '/(woo_core_version: )\d+\.\d+\.?\d*/',
@@ -26,9 +37,21 @@ $allVersions = getWooCommerceVersions();
 $stableVersions = filterStableVersions($allVersions);
 [$latestVersion, $previousVersion] = getLatestAndPreviousMinorMajorVersions($stableVersions);
 
-
 echo "Latest WooCommerce version: $latestVersion\n";
 echo "Previous  WooCommerce version: $previousVersion\n";
 
-echo "Replacing the previous version in the config file...\n";
-replacePreviousVersion($previousVersion);
+if ($latestVersion) {
+  echo "Replacing the latest version in the config file...\n";
+  replaceLatestVersion($latestVersion);
+} else {
+  echo "No latest version found.\n";
+}
+
+if ($previousVersion) {
+  echo "Replacing the previous version in the config file...\n";
+  replacePreviousVersion($previousVersion);
+} else {
+  echo "No previous version found.\n";
+}
+
+saveVersionsToFile($latestVersion, $previousVersion, 'woocommerce_versions.txt');
