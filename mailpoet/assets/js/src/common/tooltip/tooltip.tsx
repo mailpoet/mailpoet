a8e7bff850f77f395d009015c9685d6702fa1893
Pavel Dohnal <pavel.dohnal@automattic.com>
2024-06-21 11:43:50 +0200

    Move styling values to CSS in tooltip
    [MAILPOET-5482]

--- mailpoet/assets/js/src/common/tooltip/tooltip.tsx
+++ mailpoet/assets/js/src/common/tooltip/tooltip.tsx
@@ -1,22 +1,9 @@
 import classnames from 'classnames';
 import { Tooltip as ReactTooltip, ITooltip } from 'react-tooltip';
 
-export function Tooltip({
-  effect,
-  textColor,
-  backgroundColor,
-  border,
-  borderColor,
-  className,
-  children,
-  ...props
-}: ITooltip) {
+export function Tooltip({ border, className, children, ...props }: ITooltip) {
   return (
     <ReactTooltip
-      effect={effect || 'solid'}
-      textColor={textColor || '#1d2327'}
-      backgroundColor={backgroundColor || '#fafbfe'}
-      borderColor={borderColor || '#e5e9f8'}
       className={classnames('mailpoet-tooltip', className)}
       border={border === undefined ? true : border}
       {...props}
