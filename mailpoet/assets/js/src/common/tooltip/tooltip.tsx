ec2b077eb3e5a04a704b1c7cf13af5ccb0ed1740
Pavel Dohnal <pavel.dohnal@automattic.com>
2024-06-21 12:18:39 +0200

    Move border values to CSS in tooltip
    [MAILPOET-5482]

--- mailpoet/assets/js/src/common/tooltip/tooltip.tsx
+++ mailpoet/assets/js/src/common/tooltip/tooltip.tsx
@@ -1,11 +1,10 @@
 import classnames from 'classnames';
 import { Tooltip as ReactTooltip, ITooltip } from 'react-tooltip';
 
-export function Tooltip({ border, className, children, ...props }: ITooltip) {
+export function Tooltip({ className, children, ...props }: ITooltip) {
   return (
     <ReactTooltip
       className={classnames('mailpoet-tooltip', className)}
-      border={border === undefined ? true : border}
       {...props}
     >
       {children}
