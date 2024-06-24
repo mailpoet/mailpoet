e833f348262f6c8f833cbb5242a0083f7160b730
 Ján Mikláš <neosinner@gmail.com>
2024-06-24 14:38:44 +0200

    Fetch run data when URL parameter changes
    [MAILPOET-5436]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/subscribers/activity-modal.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/subscribers/activity-modal.tsx
@@ -1,6 +1,8 @@
 import { Modal } from '@wordpress/components';
 import { useHistory, useLocation } from 'react-router-dom';
 import { useEffect, useMemo, useState } from 'react';
+import apiFetch from '@wordpress/api-fetch';
+import { ActivityModalState, RunData } from './modal/types';
 
 export function ActivityModal(): JSX.Element {
   const history = useHistory();
@@ -9,22 +11,58 @@ export function ActivityModal(): JSX.Element {
     () => new URLSearchParams(location.search),
     [location],
   );
-  const [runId, setRunId] = useState(null);
+  const [state, setState] = useState<ActivityModalState>('hidden');
 
-  // Open modal when activity param changes in the URL
-  useEffect(() => {
-    setRunId(pageParams.get('runId'));
-  }, [pageParams]);
+  const [runId, setRunId] = useState<number | null>(null);
+  const [run, setRun] = useState<RunData | null>(null);
 
   const closeModal = () => {
+    setState('hidden');
+    setRun(null);
     pageParams.delete('runId');
     history.push({ search: pageParams.toString() });
   };
 
+  // Open modal when activity param changes in the URL
+  useEffect(() => {
+    setRunId(parseInt(pageParams.get('runId'), 10));
+  }, [pageParams]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const loadRunData = async () => {
+      if (!runId) {
+        return;
+      }
+      setState('loading');
+
+      try {
+        const data = await apiFetch<{ data: RunData }>({
+          path: `/automation/analytics/run-logs&id=${runId}`,
+          method: 'GET',
+          signal: controller.signal,
+        });
+        setRun(data.data);
+        setState('loaded');
+      } catch (error) {
+        if (!controller.signal?.aborted) {
+          setState('error');
+        }
+      }
+    };
+
+    void loadRunData();
+
+    return () => {
+      setState('hidden');
+      controller.abort();
+    };
+  }, [runId]);
+
   return (
     <>
-      {runId && (
-        <Modal onRequestClose={closeModal} title={runId}>
+      {(state === 'loading' || state === 'loaded') && (
+        <Modal onRequestClose={closeModal} title={run?.subscriber.email}>
           <div />
         </Modal>
       )}
