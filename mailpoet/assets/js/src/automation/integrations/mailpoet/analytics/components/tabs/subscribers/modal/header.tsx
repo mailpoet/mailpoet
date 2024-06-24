489877c58828f92c5ae9ccea92ddc4e9cadd99a9
 Ján Mikláš <neosinner@gmail.com>
2024-06-24 15:29:08 +0200

    Add activity modal header with subscriber info
    [MAILPOET-5436]

---
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/subscribers/modal/header.tsx
@@ -0,0 +1,25 @@
+import { __ } from '@wordpress/i18n';
+import { close } from '@wordpress/icons';
+import { Button } from '@wordpress/components';
+import { Subscriber } from '../../../../store';
+import { CustomerCell } from '../../orders/cells/customer';
+
+type HeaderProps = {
+  subscriber: Subscriber | null;
+  onClose: () => void;
+};
+
+export function Header({ subscriber, onClose }: HeaderProps): JSX.Element {
+  return (
+    <div className="components-modal__header mailpoet-analytics-modal-header">
+      <h1 className="components-modal__header-heading">
+        {__('Subscriber activity', 'mailpoet')}
+        {subscriber && ': '}
+      </h1>
+
+      {subscriber && <CustomerCell customer={subscriber} />}
+
+      <Button onClick={onClose} icon={close} label={__('Close')} />
+    </div>
+  );
+}
