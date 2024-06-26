02393dbe4cdf475fdb9fca2995aa7a50149ca346
 Ján Mikláš <neosinner@gmail.com>
2024-06-26 17:49:05 +0200

    Make icons bigger in automation subscribers analytics
    [MAILPOET-6132]

--- mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/subscribers/cells/step.tsx
+++ mailpoet/assets/js/src/automation/integrations/mailpoet/analytics/components/tabs/subscribers/cells/step.tsx
@@ -31,8 +31,8 @@ export function StepCell({
     <div className="mailpoet-analytics-subscribers-step-cell">
       {stepType ? (
         <ColoredIcon
-          width="12px"
-          height="12px"
+          width="16px"
+          height="16px"
           background={stepType.background}
           foreground={stepType.foreground}
           icon={stepType.icon}
