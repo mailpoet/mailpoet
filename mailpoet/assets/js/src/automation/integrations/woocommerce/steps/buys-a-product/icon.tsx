baf8639de5249a5016e2b65b69ca9e3c1a9316bf
 Ján Mikláš <neosinner@gmail.com>
2024-06-26 17:40:58 +0200

    Normalize automation icon sizes
    [MAILPOET-6132]

--- mailpoet/assets/js/src/automation/integrations/woocommerce/steps/buys-a-product/icon.tsx
+++ mailpoet/assets/js/src/automation/integrations/woocommerce/steps/buys-a-product/icon.tsx
@@ -1,6 +1,6 @@
 export function Icon(): JSX.Element {
   return (
-    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="2 2 20 20" xmlns="http://www.w3.org/2000/svg">
       <path
         fillRule="evenodd"
         clipRule="evenodd"
--- mailpoet/assets/js/src/automation/integrations/woocommerce/steps/order-status-changed/icon.tsx
+++ mailpoet/assets/js/src/automation/integrations/woocommerce/steps/order-status-changed/icon.tsx
@@ -1,6 +1,6 @@
 export function Icon(): JSX.Element {
   return (
-    <svg viewBox="0 0 14 16" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="-2 -2 18 20" xmlns="http://www.w3.org/2000/svg">
       <path
         fillRule="evenodd"
         clipRule="evenodd"
