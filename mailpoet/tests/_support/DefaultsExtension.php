06c3891ebad62b958ca6ae895afaac8be945eb96
Pavel Dohnal <pavel.dohnal@automattic.com>
2024-07-12 08:54:24 +0200

    Fix broken HPOS tests
    We complete the actions in the database. However, we don't delete
    the batch processes. Woo thinks those crashed. It tries to log an
    error, but it doens't have suficient rights for that.

--- mailpoet/tests/_support/DefaultsExtension.php
+++ mailpoet/tests/_support/DefaultsExtension.php
@@ -98,6 +98,7 @@ class DefaultsExtension extends Extension {
     delete_transient('_wc_activation_redirect');
 
     // mark all WC cron actions complete
+    update_option('wc_pending_batch_processes', []);
     $tableName = !empty($wpdb->actionscheduler_actions) ? $wpdb->actionscheduler_actions : $wpdb->prefix . 'actionscheduler_actions';// phpcs:ignore Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps
     $sql = "UPDATE {$tableName} SET status = 'complete'";
     $wpdb->query($sql);
