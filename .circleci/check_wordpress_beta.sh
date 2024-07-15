212163dd2031175e880f08a6f9ff46509da114ba
Jan Lysý <jan.lysy@automattic.com>
2024-07-15 13:27:09 +0200

    Fix extraction of RC WordPress versions
    [MAILPOET-6096]

--- .circleci/check_wordpress_beta.sh
+++ .circleci/check_wordpress_beta.sh
@@ -7,14 +7,22 @@ RSS_FEED=$(curl -s https://wordpress.org/news/category/releases/feed/)
 LAST_VERSION=$(echo "$RSS_FEED" | grep -o '<title>WordPress [^<]*</title>' | sed -E 's/<\/?title>//g' | head -n 1 | tr [:upper:] [:lower:])
 
 # Check if a beta or RC version is found
-if [[ $LAST_VERSION != *'beta'* && $LAST_VERSION != *'rc'* ]]; then
-  echo "No WordPress beta/RC version found."
-  echo "LATEST_BETA="
-else
+if [[ $LAST_VERSION == *'beta'* ]]; then
   # Extract titles containing beta versions from the feed
   VERSION_LINE=$(echo "$RSS_FEED" | grep -o '<code>wp core update [^<]*</code>' | sed -E 's/<\/?code>//g' | head -n 1 | grep 'beta')
   LATEST_BETA=$(echo "$VERSION_LINE" | sed -E 's/.*--version=([0-9\.]+-beta[0-9]+).*/\1/')
 
-  echo "Latest WordPress beta/RC version: $LATEST_BETA"
+  echo "Latest WordPress beta version: $LATEST_BETA"
   echo "LATEST_BETA=$LATEST_BETA"
+
+elif [[ $LAST_VERSION == *'release candidate'* ]]; then
+  # Extract titles containing RC versions from the feed
+  VERSION_LINE=$(echo "$RSS_FEED" | grep -o '<code>wp core update [^<]*</code>' | sed -E 's/<\/?code>//g' | head -n 1 | grep 'RC')
+  LATEST_BETA=$(echo "$VERSION_LINE" | sed -E 's/.*--version=([0-9\.]+-RC[0-9]+).*/\1/')
+
+  echo "Latest WordPress RC version: $LATEST_BETA"
+  echo "LATEST_BETA=$LATEST_BETA"
+else
+  echo "No WordPress beta/RC version found."
+  echo "LATEST_BETA="
 fi
