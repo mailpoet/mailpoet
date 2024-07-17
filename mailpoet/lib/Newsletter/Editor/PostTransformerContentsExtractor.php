0f37eb8ebfff977e94811917470375d4bc9a9f81
Pavel Dohnal <pavel.dohnal@automattic.com>
2024-07-17 11:35:11 +0200

    Allow formatting tags in post titles
    [MAILPOET-5920]

--- mailpoet/lib/Newsletter/Editor/PostTransformerContentsExtractor.php
+++ mailpoet/lib/Newsletter/Editor/PostTransformerContentsExtractor.php
@@ -165,6 +165,11 @@ class PostTransformerContentsExtractor {
       'h1' => $commonAttributes,
       'h2' => $commonAttributes,
       'h3' => $commonAttributes,
+      'b' => [],
+      'i' => [],
+      'strong' => [],
+      'em' => [],
+      'small' => [],
     ];
 
     return [
