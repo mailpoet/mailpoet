f506a1f0016c3e6ef9d0388f70438729ecb0482c
 Ján Mikláš <neosinner@gmail.com>
2024-07-31 13:13:55 +0200

    Update migration to only check for presence of the setting, without version check
    [MAILPOET-5807]

--- mailpoet/lib/Migrations/App/Migration_20240730_212419_App.php
+++ mailpoet/lib/Migrations/App/Migration_20240730_212419_App.php
@@ -13,17 +13,15 @@ class Migration_20240730_212419_App extends AppMigration {
   public function run(): void {
     $this->settings = $this->container->get(SettingsController::class);
 
-    // Skip if the installed version is newer than the release that preceded this migration, or if it's a fresh install
-    $currentlyInstalledVersion = (string)$this->settings->get('db_version', '4.58.1');
-    if (version_compare($currentlyInstalledVersion, '4.58.0', '>')) {
+    // Skip if the opt-in checkbox is not enabled or the position is already set.
+    // When enabling, the user chooses the position of the opt-in checkbox
+    // on the same settings page.
+    $optInEnabled = $this->settings->get(Subscription::OPTIN_ENABLED_SETTING_NAME, false);
+    $optInPosition = $this->settings->get(Subscription::OPTIN_POSITION_SETTING_NAME, null);
+    if (!$optInEnabled || $optInPosition) {
       return;
     }
-    // Skip if the opt-in checkbox is not enabled. When enabling, the user chooses
-    // the position of the opt-in checkbox on the same settings page, so no default
-    // value is necessary.
-    if (!$this->settings->get(Subscription::OPTIN_ENABLED_SETTING_NAME, false)) {
-      return;
-    }
+    // Set previous default value for existing installations
     $this->settings->set(Subscription::OPTIN_POSITION_SETTING_NAME, Hooks::OPTIN_POSITION_BEFORE_TERMS_AND_CONDITIONS);
   }
 }
