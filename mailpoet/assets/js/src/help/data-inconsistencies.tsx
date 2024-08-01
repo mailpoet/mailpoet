67a79728e263b757227450e0443c678c8f20cf42
Rostislav Wolny <rosta.wolny@automattic.com>
2024-08-01 10:24:26 +0200

    Add fix button to data inconsistencies table
    [MAILPOET-1587]

--- mailpoet/assets/js/src/help/data-inconsistencies.tsx
+++ mailpoet/assets/js/src/help/data-inconsistencies.tsx
@@ -1,24 +1,74 @@
+import { MailPoet } from 'mailpoet';
+import { Button } from '@wordpress/components';
+import { useState, useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { KeyValueTable } from 'common/key-value-table';
 
-interface DataInconsistenciesProps {
-  dataInconsistencies: {
-    [key: string]: number;
-  };
-}
+type DataInconsistencies = {
+  [key: string]: number;
+};
 
-export function DataInconsistencies({
-  dataInconsistencies,
-}: DataInconsistenciesProps) {
-  if (!dataInconsistencies || dataInconsistencies.total === 0) {
+type Props = {
+  dataInconsistencies: DataInconsistencies;
+};
+
+export function DataInconsistencies({ dataInconsistencies }: Props) {
+  const [data, setData] = useState(dataInconsistencies);
+  const [cleaningKey, setCleaningKey] = useState('');
+
+  const fixInconsistency = useCallback((key) => {
+    setCleaningKey(key as string);
+    MailPoet.Ajax.post({
+      api_version: window.mailpoet_api_version,
+      endpoint: 'help',
+      action: 'fixInconsistentData',
+      data: {
+        inconsistency: key,
+      },
+    })
+      .done((response) => {
+        setCleaningKey('');
+        setData((response.data as DataInconsistencies) || null);
+        MailPoet.Notice.show({
+          message: __('Inconsistency cleaned', 'mailpoet'),
+          scroll: true,
+        });
+      })
+      .catch((e) => {
+        setCleaningKey('');
+        MailPoet.Notice.show({
+          type: 'error',
+          message: e.errors.map((error) => error.message).join(' '),
+          scroll: true,
+        });
+      });
+  }, []);
+
+  if (!data || data.total === 0) {
     return null;
   }
-  const rowsData = Object.entries(dataInconsistencies)
+
+  const rowsData = Object.entries(data)
     .filter(([key]) => key !== 'total')
     .map(([key, value]) => ({
       key,
-      value,
+      value: (
+        <div>
+          {value} (
+          <Button
+            variant="link"
+            label={__('Clean', 'mailpoet')}
+            onClick={() => fixInconsistency(key)}
+            isBusy={cleaningKey === key}
+            disabled={!!cleaningKey}
+          >
+            {__('Fix', 'mailpoet')}
+          </Button>
+          )
+        </div>
+      ),
     }));
+
   return (
     <div>
       <h2>{__('Data Inconsistencies', 'mailpoet')}</h2>
