83eaf69bfbe844b03ae3d044d5564d266cdb7a29
 Ján Mikláš <neosinner@gmail.com>
2024-08-09 14:07:56 +0200

    Track CAPTCHA setting to Mixpanel
    [MAILPOET-6187]

--- mailpoet/lib/Subscription/Captcha/CaptchaConstants.php
+++ mailpoet/lib/Subscription/Captcha/CaptchaConstants.php
@@ -7,6 +7,7 @@ class CaptchaConstants {
   const TYPE_RECAPTCHA = 'recaptcha';
   const TYPE_RECAPTCHA_INVISIBLE = 'recaptcha-invisible';
   const TYPE_DISABLED = null;
+  const TYPE_SETTING_NAME = 'captcha.type';
 
   public static function isReCaptcha(?string $captchaType) {
     return in_array($captchaType, [self::TYPE_RECAPTCHA, self::TYPE_RECAPTCHA_INVISIBLE]);
