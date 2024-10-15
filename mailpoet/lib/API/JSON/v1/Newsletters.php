d4270006d78993bee8a37a718cd4b6e195ef3cf5
Pavel Dohnal <pavel.dohnal@automattic.com>
2024-10-15 11:43:08 +0200

    Fix inactive notifications clog sending queue
    [MAILPOET-6259]

--- mailpoet/lib/API/JSON/v1/Newsletters.php
+++ mailpoet/lib/API/JSON/v1/Newsletters.php
@@ -11,6 +11,7 @@ use MailPoet\Cron\CronHelper;
 use MailPoet\Doctrine\Validator\ValidationException;
 use MailPoet\Entities\NewsletterEntity;
 use MailPoet\Entities\NewsletterOptionFieldEntity;
+use MailPoet\Entities\ScheduledTaskEntity;
 use MailPoet\Entities\SendingQueueEntity;
 use MailPoet\InvalidStateException;
 use MailPoet\Listing;
@@ -211,6 +212,17 @@ class Newsletters extends APIEndpoint {
 
     $this->newslettersRepository->prefetchOptions([$newsletter]);
     $newsletter->setStatus($status);
+
+    // if there are paused tasks unpause them
+    if ($newsletter->getStatus() === NewsletterEntity::STATUS_ACTIVE) {
+      $queues = $newsletter->getQueues();
+      foreach ($queues as $queue) {
+        $task = $queue->getTask();
+        if ($task && $task->getStatus() === ScheduledTaskEntity::STATUS_PAUSED) {
+          $task->setStatus(ScheduledTaskEntity::STATUS_SCHEDULED);
+        }
+      }
+    }
 
     // if there are past due notifications, reschedule them for the next send date
     if ($newsletter->getType() === NewsletterEntity::TYPE_NOTIFICATION && $status === NewsletterEntity::STATUS_ACTIVE) {
