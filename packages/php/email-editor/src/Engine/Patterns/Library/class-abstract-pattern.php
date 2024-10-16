d68b8da14edd321731378407610215740d9b5c2d
Jan Lysý <jan.lysy@automattic.com>
2024-10-16 19:23:15 +0200

    Migrate email editor classes Engine\Patterns directory to WP Coding Standard
    [MAILPOET-6240]

--- packages/php/email-editor/src/Engine/Patterns/Library/class-abstract-pattern.php
+++ packages/php/email-editor/src/Engine/Patterns/Library/class-abstract-pattern.php
@@ -1,41 +1,105 @@
-<?php declare(strict_types = 1);
+<?php
+/**
+ * This file is part of the MailPoet plugin.
+ *
+ * @package MailPoet\EmailEditor
+ */
 
+declare(strict_types = 1);
 namespace MailPoet\EmailEditor\Engine\Patterns\Library;
 
 use MailPoet\EmailEditor\Utils\Cdn_Asset_Url;
 
+/**
+ * Abstract class for block patterns.
+ */
 abstract class Abstract_Pattern {
-	protected $cdnAssetUrl;
-	protected $blockTypes    = array();
-	protected $inserter      = true;
-	protected $source        = 'plugin';
-	protected $categories    = array( 'mailpoet' );
-	protected $viewportWidth = 620;
+	/**
+	 * Cdn_Asset_Url instance.
+	 *
+	 * @var Cdn_Asset_Url $cdn_asset_url
+	 */
+	protected $cdn_asset_url;
+	/**
+	 * List of block types.
+	 *
+	 * @var array $block_types
+	 */
+	protected $block_types = array();
+	/**
+	 * Flag to enable/disable inserter.
+	 *
+	 * @var bool $inserter
+	 */
+	protected $inserter = true;
+	/**
+	 * Source of the pattern.
+	 *
+	 * @var string $source
+	 */
+	protected $source = 'plugin';
+	/**
+	 * List of categories.
+	 *
+	 * @var array $categories
+	 */
+	protected $categories = array( 'mailpoet' );
+	/**
+	 * Viewport width.
+	 *
+	 * @var int $viewport_width
+	 */
+	protected $viewport_width = 620;
 
+	/**
+	 * Constructor.
+	 *
+	 * @param Cdn_Asset_Url $cdn_asset_url Cdn_Asset_Url instance.
+	 */
 	public function __construct(
-		Cdn_Asset_Url $cdnAssetUrl
+		Cdn_Asset_Url $cdn_asset_url
 	) {
-		$this->cdnAssetUrl = $cdnAssetUrl;
+		$this->cdn_asset_url = $cdn_asset_url;
 	}
 
-	public function getProperties() {
+	/**
+	 * Return properties of the pattern.
+	 *
+	 * @return array
+	 */
+	public function get_properties(): array {
 		return array(
-			'title'         => $this->getTitle(),
-			'content'       => $this->getContent(),
-			'description'   => $this->getDescription(),
+			'title'         => $this->get_title(),
+			'content'       => $this->get_content(),
+			'description'   => $this->get_description(),
 			'categories'    => $this->categories,
 			'inserter'      => $this->inserter,
-			'blockTypes'    => $this->blockTypes,
+			'blockTypes'    => $this->block_types,
 			'source'        => $this->source,
-			'viewportWidth' => $this->viewportWidth,
+			'viewportWidth' => $this->viewport_width,
 		);
 	}
 
-	abstract protected function getContent(): string;
+	/**
+	 * Get content.
+	 *
+	 * @return string
+	 */
+	abstract protected function get_content(): string;
 
-	abstract protected function getTitle(): string;
+	/**
+	 * Get title.
+	 *
+	 * @return string
+	 */
+	abstract protected function get_title(): string;
 
-	protected function getDescription(): string {
+	/**
+	 * Get description.
+	 *
+	 * @return string
+	 */
+	protected function get_description(): string {
 		return '';
 	}
 }
--- packages/php/email-editor/src/Engine/Patterns/Library/class-default-content.php
+++ packages/php/email-editor/src/Engine/Patterns/Library/class-default-content.php
@@ -1,25 +1,44 @@
-<?php declare(strict_types = 1);
+<?php
+/**
+ * This file is part of the MailPoet plugin.
+ *
+ * @package MailPoet\EmailEditor
+ */
 
+declare(strict_types = 1);
 namespace MailPoet\EmailEditor\Engine\Patterns\Library;
 
+/**
+ * Default content pattern.
+ */
 class Default_Content extends Abstract_Pattern {
-	protected $blockTypes = array(
+	/**
+	 * List of block types.
+	 *
+	 * @var array $block_types
+	 */
+	protected $block_types = array(
 		'core/post-content',
 	);
 
-	protected function getContent(): string {
+	/**
+	 * Get the content of the pattern.
+	 *
+	 * @return string
+	 */
+	protected function get_content(): string {
 		return '
     <!-- wp:columns {"backgroundColor":"white","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}}} -->
     <div class="wp-block-columns has-white-background-color has-background" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20);padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:column -->
     <div class="wp-block-column">
     <!-- wp:image {"width":"130px","sizeSlug":"large"} -->
-    <figure class="wp-block-image size-large is-resized"><img src="' . esc_url( $this->cdnAssetUrl->generateCdnUrl( 'email-editor/your-logo-placeholder.png' ) ) . '" alt="Your Logo" style="width:130px"/></figure>
+    <figure class="wp-block-image size-large is-resized"><img src="' . esc_url( $this->cdn_asset_url->generateCdnUrl( 'email-editor/your-logo-placeholder.png' ) ) . '" alt="Your Logo" style="width:130px"/></figure>
     <!-- /wp:image -->
     <!-- wp:heading {"fontSize":"medium","style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}}}} -->
     <h2 class="wp-block-heading has-medium-font-size" style="padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10)">' . __( 'One column layout', 'mailpoet' ) . '</h2>
     <!-- /wp:heading -->
     <!-- wp:image {"width":"620px","sizeSlug":"large"} -->
-    <figure class="wp-block-image"><img src="' . esc_url( $this->cdnAssetUrl->generateCdnUrl( 'newsletter/congratulation-page-illustration-transparent-LQ.20181121-1440.png' ) ) . '" alt="Banner Image"/></figure>
+    <figure class="wp-block-image"><img src="' . esc_url( $this->cdn_asset_url->generateCdnUrl( 'newsletter/congratulation-page-illustration-transparent-LQ.20181121-1440.png' ) ) . '" alt="Banner Image"/></figure>
     <!-- /wp:image -->
     <!-- wp:paragraph -->
     <p>' . esc_html__( 'A one-column layout is great for simplified and concise content, like announcements or newsletters with brief updates. Drag blocks to add content and customize your styles from the styles panel on the top right.', 'mailpoet' ) . '</p>
@@ -37,7 +56,12 @@ class Default_Content extends Abstract_Pattern {
     ';
 	}
 
-	protected function getTitle(): string {
+	/**
+	 * Get the title of the pattern.
+	 *
+	 * @return string
+	 */
+	protected function get_title(): string {
 		return __( 'Default Email Content', 'mailpoet' );
 	}
 }
