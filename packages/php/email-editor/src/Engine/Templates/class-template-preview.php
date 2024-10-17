b40732059d4be8061449e878e19bdfcb52516cd9
Jan Lysý <jan.lysy@automattic.com>
2024-10-17 12:12:59 +0200

    Migrate email editor template-preview class to WP Coding Standard
    [MAILPOET-6240]

--- packages/php/email-editor/src/Engine/Templates/class-template-preview.php
+++ packages/php/email-editor/src/Engine/Templates/class-template-preview.php
@@ -1,29 +1,57 @@
-<?php declare(strict_types = 1);
+<?php
+/**
+ * This file is part of the MailPoet plugin.
+ *
+ * @package MailPoet\EmailEditor
+ */
 
+declare(strict_types = 1);
 namespace MailPoet\EmailEditor\Engine\Templates;
 
 use MailPoet\EmailEditor\Engine\Theme_Controller;
 use MailPoet\EmailEditor\Validator\Builder;
 use WP_Theme_JSON;
 
+/**
+ * Template_Preview class.
+ */
 class Template_Preview {
-	private Theme_Controller $themeController;
+	/**
+	 * Provides the theme controller.
+	 *
+	 * @var Theme_Controller
+	 */
+	private Theme_Controller $theme_controller;
+	/**
+	 * Provides the templates.
+	 *
+	 * @var Templates
+	 */
 	private Templates $templates;
 
+	/**
+	 * Template_Preview constructor.
+	 *
+	 * @param Theme_Controller $theme_controller Theme controller.
+	 * @param Templates        $templates Templates.
+	 */
 	public function __construct(
-		Theme_Controller $themeController,
+		Theme_Controller $theme_controller,
 		Templates $templates
 	) {
-		$this->themeController = $themeController;
-		$this->templates       = $templates;
+		$this->theme_controller = $theme_controller;
+		$this->templates        = $templates;
 	}
 
+	/**
+	 * Initializes the class.
+	 */
 	public function initialize(): void {
 		register_rest_field(
 			'wp_template',
 			'email_theme_css',
 			array(
-				'get_callback'    => array( $this, 'getEmailThemePreviewCss' ),
+				'get_callback'    => array( $this, 'get_email_theme_preview_css' ),
 				'update_callback' => null,
 				'schema'          => Builder::string()->toArray(),
 			)
@@ -33,14 +61,18 @@ class Template_Preview {
 	/**
 	 * Generates CSS for preview of email theme
 	 * They are applied in the preview BLockPreview in template selection
+	 *
+	 * @param array $template Template data.
 	 */
-	public function getEmailThemePreviewCss( $template ): string {
-		$editorTheme   = clone $this->themeController->get_theme();
-		$templateTheme = $this->templates->getBlockTemplateTheme( $template['id'], $template['wp_id'] );
-		if ( is_array( $templateTheme ) ) {
-			$editorTheme->merge( new WP_Theme_JSON( $templateTheme, 'custom' ) );
+	public function get_email_theme_preview_css( $template ): string {
+		global $wp_filesystem;
+
+		$editor_theme   = clone $this->theme_controller->get_theme();
+		$template_theme = $this->templates->getBlockTemplateTheme( $template['id'], $template['wp_id'] );
+		if ( is_array( $template_theme ) ) {
+			$editor_theme->merge( new WP_Theme_JSON( $template_theme, 'custom' ) );
 		}
-		$additionalCSS = file_get_contents( __DIR__ . DIRECTORY_SEPARATOR . 'preview.css' );
-		return $editorTheme->get_stylesheet() . $additionalCSS;
+		$additional_css = $wp_filesystem->get_contents( __DIR__ . DIRECTORY_SEPARATOR . 'preview.css' );
+		return $editor_theme->get_stylesheet() . $additional_css;
 	}
 }
