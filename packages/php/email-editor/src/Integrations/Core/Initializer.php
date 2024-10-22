bdda0fb8c297bd9dae1920f97f6ed44af3ece66e
Rostislav Wolny <rosta.wolny@automattic.com>
2024-10-22 13:57:58 +0200

    Prevent block gap for list item
    The list item gets spacing via the fallback renderer.
    To prevent this, I created a renderer that skips spacing.
    [MAILPOET-6249]

--- packages/php/email-editor/src/Integrations/Core/Initializer.php
+++ packages/php/email-editor/src/Integrations/Core/Initializer.php
@@ -21,6 +21,7 @@ class Initializer {
     $blocksRegistry->addBlockRenderer('core/column', new Renderer\Blocks\Column());
     $blocksRegistry->addBlockRenderer('core/columns', new Renderer\Blocks\Columns());
     $blocksRegistry->addBlockRenderer('core/list', new Renderer\Blocks\ListBlock());
+    $blocksRegistry->addBlockRenderer('core/list-item', new Renderer\Blocks\ListItem());
     $blocksRegistry->addBlockRenderer('core/image', new Renderer\Blocks\Image());
     $blocksRegistry->addBlockRenderer('core/buttons', new Renderer\Blocks\Buttons(new FlexLayoutRenderer()));
     $blocksRegistry->addBlockRenderer('core/button', new Renderer\Blocks\Button());
