3b83e5ffca5c0b244b719552af2b6fccb7076857
Rostislav Wolny <rosta.wolny@automattic.com>
2024-10-23 11:29:37 +0200

    Simplify the DefaultContent pattern
    This removes the header and footer from the content pattern.
    The header and footer should be part of the template
    [MAILPOET-6249]

--- packages/php/email-editor/src/Engine/Patterns/Library/DefaultContent.php
+++ packages/php/email-editor/src/Engine/Patterns/Library/DefaultContent.php
@@ -9,12 +9,9 @@ class DefaultContent extends AbstractPattern {
 
   protected function getContent(): string {
     return '
-    <!-- wp:columns {"backgroundColor":"white","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}}} -->
-    <div class="wp-block-columns has-white-background-color has-background" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20);padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:column -->
+    <!-- wp:columns {"backgroundColor":"white","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}}} -->
+    <div class="wp-block-columns has-white-background-color has-background" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:column -->
     <div class="wp-block-column">
-    <!-- wp:image {"width":"130px","sizeSlug":"large"} -->
-    <figure class="wp-block-image size-large is-resized"><img src="' . esc_url($this->cdnAssetUrl->generateCdnUrl("email-editor/your-logo-placeholder.png")) . '" alt="Your Logo" style="width:130px"/></figure>
-    <!-- /wp:image -->
     <!-- wp:heading {"fontSize":"medium","style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}}}} -->
     <h2 class="wp-block-heading has-medium-font-size" style="padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10)">' . __('One column layout', 'mailpoet') . '</h2>
     <!-- /wp:heading -->
@@ -23,12 +20,6 @@ class DefaultContent extends AbstractPattern {
     <!-- /wp:image -->
     <!-- wp:paragraph -->
     <p>' . esc_html__('A one-column layout is great for simplified and concise content, like announcements or newsletters with brief updates. Drag blocks to add content and customize your styles from the styles panel on the top right.', 'mailpoet') . '</p>
-    <!-- /wp:paragraph -->
-    <!-- wp:paragraph {"fontSize":"small"} -->
-    <p class="has-small-font-size">' . esc_html__('You received this email because you are subscribed to the [site:title]', 'mailpoet') . '</p>
-    <!-- /wp:paragraph -->
-    <!-- wp:paragraph {"fontSize":"small"} -->
-    <p class="has-small-font-size"><a href="[link:subscription_unsubscribe_url]">' . esc_html__('Unsubscribe', 'mailpoet') . '</a> | <a href="[link:subscription_manage_url]">' . esc_html__('Manage subscription', 'mailpoet') . '</a></p>
     <!-- /wp:paragraph -->
     </div>
     <!-- /wp:column -->
