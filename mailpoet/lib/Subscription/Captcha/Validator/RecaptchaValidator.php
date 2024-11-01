b47f33d10096b3422be5bfba87782855703deb36
Mustapha Hadid <mustapha.hadeed@gmail.com>
2024-11-01 22:33:58 +0300

    Refactor reCAPTCHA validation in MP forms
    [MAILPOET-6282]

--- mailpoet/lib/Subscription/Captcha/Validator/RecaptchaValidator.php
+++ mailpoet/lib/Subscription/Captcha/Validator/RecaptchaValidator.php
@@ -2,49 +2,26 @@
 
 namespace MailPoet\Subscription\Captcha\Validator;
 
-use MailPoet\Settings\SettingsController;
-use MailPoet\Subscription\Captcha\CaptchaConstants;
-use MailPoet\WP\Functions as WPFunctions;
+use MailPoet\Captcha\ReCaptchaValidator as Validator;
 
 class RecaptchaValidator implements CaptchaValidator {
 
-  /** @var SettingsController */
-  private $settings;
-
-  /** @var WPFunctions */
-  private $wp;
-
-  private const ENDPOINT = 'https://www.google.com/recaptcha/api/siteverify';
+  /** @var Validator */
+  private $validator;
 
   public function __construct(
-    SettingsController $settings,
-    WPFunctions $wp
+    Validator $validator
   ) {
-    $this->settings = $settings;
-    $this->wp = $wp;
+    $this->validator = $validator;
   }
 
   public function validate(array $data): bool {
-    $captchaSettings = $this->settings->get('captcha');
-    if (empty($data['recaptchaResponseToken'])) {
-      throw new ValidationError(__('Please check the CAPTCHA.', 'mailpoet'));
-    }
+    $token = $data['recaptchaResponseToken'] ?? '';
 
-    $secretToken = $captchaSettings['type'] === CaptchaConstants::TYPE_RECAPTCHA_INVISIBLE ? $captchaSettings['recaptcha_invisible_secret_token'] : $captchaSettings['recaptcha_secret_token'];
-
-    $response = $this->wp->wpRemotePost(self::ENDPOINT, [
-      'body' => [
-        'secret' => $secretToken,
-        'response' => $data['recaptchaResponseToken'],
-      ],
-    ]);
-    if ($this->wp->isWpError($response)) {
-      throw new ValidationError(__('Error while validating the CAPTCHA.', 'mailpoet'));
-    }
-    /** @var \stdClass $response */
-    $response = json_decode($this->wp->wpRemoteRetrieveBody($response));
-    if (empty($response->success)) {
-      throw new ValidationError(__('Error while validating the CAPTCHA.', 'mailpoet'));
+    try {
+      $this->validator->validate($token);
+    } catch (\Exception $e) {
+      throw new ValidationError($e->getMessage());
     }
 
     return true;
--- mailpoet/tests/unit/Subscription/Captcha/Validator/RecaptchaValidatorTest.php
+++ mailpoet/tests/unit/Subscription/Captcha/Validator/RecaptchaValidatorTest.php
@@ -3,189 +3,52 @@
 namespace MailPoet\Subscription\Captcha\Validator;
 
 use Codeception\Stub;
-use MailPoet\Settings\SettingsController;
-use MailPoet\Subscription\Captcha\CaptchaConstants;
-use MailPoet\WP\Functions as WPFunctions;
+use MailPoet\Captcha\ReCaptchaValidator as Validator;
 
 class RecaptchaValidatorTest extends \MailPoetUnitTest {
-  public function testSuccessfulInvisibleValidation() {
-
-    $captchaSettings = [
-      'type' => CaptchaConstants::TYPE_RECAPTCHA_INVISIBLE,
-      'recaptcha_invisible_secret_token' => 'recaptcha_invisible_secret_token',
-      'recaptcha_secret_token' => 'recaptcha_secret_token',
-    ];
-    $recaptchaResponseToken = 'recaptchaResponseToken';
-    $response = json_encode(['success' => true]);
-    $settings = Stub::make(
-      SettingsController::class,
-      [
-        'get' => function($key) use ($captchaSettings) {
-          if ($key === 'captcha') {
-            return $captchaSettings;
-          }
-        },
-      ],
-      $this
-    );
-    $wp = Stub::make(
-      WPFunctions::class,
-      [
-        'wpRemotePost' => function($url, $args) use ($recaptchaResponseToken, $captchaSettings, $response) {
-          verify($url)->equals('https://www.google.com/recaptcha/api/siteverify');
-          verify($args['body']['secret'])->equals($captchaSettings['recaptcha_invisible_secret_token']);
-          verify($args['body']['response'])->equals($recaptchaResponseToken);
-          return $response;
-        },
-        'isWpError' => false,
-        'wpRemoteRetrieveBody' => function($data) use ($response) {
-          verify($data)->equals($response);
-          return $response;
-        },
-      ],
-      $this
-    );
-
-    $testee = new RecaptchaValidator($settings, $wp);
-    $data = [
-      'recaptchaResponseToken' => $recaptchaResponseToken,
-    ];
-    verify($testee->validate($data))->true();
-  }
-
   public function testSuccessfulValidation() {
-
-    $captchaSettings = [
-      'type' => CaptchaConstants::TYPE_RECAPTCHA,
-      'recaptcha_invisible_secret_token' => 'recaptcha_invisible_secret_token',
-      'recaptcha_secret_token' => 'recaptcha_secret_token',
-    ];
-    $recaptchaResponseToken = 'recaptchaResponseToken';
-    $response = json_encode(['success' => true]);
-    $settings = Stub::make(
-      SettingsController::class,
+    $responseToken = 'recaptchaResponseToken';
+    $validator = Stub::make(
+      Validator::class,
       [
-        'get' => function($key) use ($captchaSettings) {
-          if ($key === 'captcha') {
-            return $captchaSettings;
-          }
-        },
-      ],
-      $this
-    );
-    $wp = Stub::make(
-      WPFunctions::class,
-      [
-        'wpRemotePost' => function($url, $args) use ($recaptchaResponseToken, $captchaSettings, $response) {
-          verify($url)->equals('https://www.google.com/recaptcha/api/siteverify');
-          verify($args['body']['secret'])->equals($captchaSettings['recaptcha_secret_token']);
-          verify($args['body']['response'])->equals($recaptchaResponseToken);
-          return $response;
-        },
-        'isWpError' => false,
-        'wpRemoteRetrieveBody' => function($data) use ($response) {
-          verify($data)->equals($response);
-          return $response;
+        'validate' => function ($responseToken) {
+          return null;
         },
       ],
       $this
     );
 
-    $testee = new RecaptchaValidator($settings, $wp);
+    $testee = new RecaptchaValidator($validator);
     $data = [
-      'recaptchaResponseToken' => $recaptchaResponseToken,
+      'recaptchaResponseToken' => $responseToken,
     ];
+
     verify($testee->validate($data))->true();
   }
 
   public function testFailingValidation() {
-
-    $captchaSettings = [
-      'type' => CaptchaConstants::TYPE_RECAPTCHA_INVISIBLE,
-      'recaptcha_invisible_secret_token' => 'recaptcha_invisible_secret_token',
-      'recaptcha_secret_token' => 'recaptcha_secret_token',
-    ];
-    $recaptchaResponseToken = 'recaptchaResponseToken';
-    $response = json_encode(['success' => false]);
-    $settings = Stub::make(
-      SettingsController::class,
+    $responseToken = 'recaptchaResponseToken';
+    $exceptionErr = 'Error while validating the CAPTCHA.';
+    $validator = Stub::make(
+      Validator::class,
       [
-        'get' => function($key) use ($captchaSettings) {
-          if ($key === 'captcha') {
-            return $captchaSettings;
-          }
-        },
-      ],
-      $this
-    );
-    $wp = Stub::make(
-      WPFunctions::class,
-      [
-        'wpRemotePost' => function() use ($response) {
-          return $response;
-        },
-        'isWpError' => false,
-        'wpRemoteRetrieveBody' => function() use ($response) {
-          return $response;
+        'validate' => function ($responseToken) use ($exceptionErr) {
+          throw new \Exception($exceptionErr);
         },
       ],
       $this
     );
 
-    $testee = new RecaptchaValidator($settings, $wp);
+    $testee = new RecaptchaValidator($validator);
     $data = [
-      'recaptchaResponseToken' => $recaptchaResponseToken,
+      'recaptchaResponseToken' => $responseToken,
     ];
-    $error = null;
+
     try {
       $testee->validate($data);
-    } catch (ValidationError $error) {
-      verify($error->getMessage())->equals('Error while validating the CAPTCHA.');
+    } catch (\Exception $e) {
+      verify($e)->instanceOf(ValidationError::class);
+      verify($e->getMessage())->equals($exceptionErr);
     }
-    verify($error)->instanceOf(ValidationError::class);
-  }
-
-  public function testConnectionError() {
-
-    $captchaSettings = [
-      'type' => CaptchaConstants::TYPE_RECAPTCHA_INVISIBLE,
-      'recaptcha_invisible_secret_token' => 'recaptcha_invisible_secret_token',
-      'recaptcha_secret_token' => 'recaptcha_secret_token',
-    ];
-    $recaptchaResponseToken = 'recaptchaResponseToken';
-    $response = (object)['wp-error'];
-    $settings = Stub::make(
-      SettingsController::class,
-      [
-        'get' => function($key) use ($captchaSettings) {
-          if ($key === 'captcha') {
-            return $captchaSettings;
-          }
-        },
-      ],
-      $this
-    );
-    $wp = Stub::make(
-      WPFunctions::class,
-      [
-        'wpRemotePost' => function() use ($response) {
-          return $response;
-        },
-        'isWpError' => true,
-      ],
-      $this
-    );
-
-    $testee = new RecaptchaValidator($settings, $wp);
-    $data = [
-      'recaptchaResponseToken' => $recaptchaResponseToken,
-    ];
-    $error = null;
-    try {
-      $testee->validate($data);
-    } catch (ValidationError $error) {
-      verify($error->getMessage())->equals('Error while validating the CAPTCHA.');
-    }
-    verify($error)->instanceOf(ValidationError::class);
   }
 }
