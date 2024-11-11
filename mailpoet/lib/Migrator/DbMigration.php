dccdd2cfbe3c1abc546e7f93b9f1a45bbf76b143
Jan Lysý <jan.lysy@automattic.com>
2024-11-11 18:41:04 +0100

    Add fallback for check indexExists in DbMigrator
    This is a fallback for older DB versions when our approach with adding index does not exist because it returns different error.
    [MAILPOET-6314]

--- mailpoet/lib/Migrator/DbMigration.php
+++ mailpoet/lib/Migrator/DbMigration.php
@@ -74,6 +74,22 @@ abstract class DbMigration {
     try {
       $this->connection->executeStatement("ALTER TABLE $tableName ADD INDEX $indexName (__non__existent__column__name__)");
     } catch (Exception $e) {
+      // Index creating index failed on not existing column we use a fallback. This can happen on MySQL 5.7 and lower and on some MariaDB versions.
+      if ($e->getCode() === 1072) {
+        $database = $wpdb->dbname;
+        $result = $wpdb->get_var($wpdb->prepare(
+          "SELECT count(*)
+         FROM information_schema.statistics
+         WHERE table_schema = COALESCE(DATABASE(), %s)
+         AND table_name = %s
+         AND index_name = %s",
+          $database,
+          $tableName,
+          $indexName
+        ));
+
+        return $result > 0;
+      }
       // Index exists when the error message contains its name. Otherwise, it's the non-existent column error.
       return strpos($e->getMessage(), $indexName) !== false;
     } finally {
