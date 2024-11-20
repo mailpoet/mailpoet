74400a2a08d522f5d03ee552a6ff27b7e4ec9338
Oluwaseun Olorunsola <seun.olorunsola@automattic.com>
2024-11-20 15:57:15 +0100

    Add phpstan command and script to Email editor PHP package
    MAILPOET-6318

---
+++ packages/php/email-editor/tasks/run-phpstan.php
@@ -0,0 +1,39 @@
+<?php // phpcs:ignoreFile
+
+// throw exception if anything fails.
+set_error_handler(
+	function ( $severity, $message, $file, $line ) {
+		throw new ErrorException( $message, 0, $severity, $file, $line );
+	}
+);
+
+$mailpoetRootDir = dirname( __DIR__, 4 );
+
+$emailEditorPhpDir = dirname( __DIR__, 1 );
+
+$phpStanDir = "$mailpoetRootDir/mailpoet/tasks/phpstan";
+$phpStanBin = "$phpStanDir/vendor/bin/phpstan";
+
+
+$extraAgrPhpVersion = '';
+if ( $argc > 1 && isset( $argv[1] ) && stripos( $argv[1], 'php-version' ) !== false ) {
+	$rawArgv            = explode( '=', escapeshellcmd( $argv[1] ) );
+	$value              = $rawArgv[1];
+	$extraAgrPhpVersion = "ANALYSIS_PHP_VERSION=$value ";
+}
+
+$commands = array(
+	"cd $phpStanDir && ", // we run commands from the PHPStan dir because we save MailPoet-specific configuration in it
+	"$extraAgrPhpVersion",
+	'php -d memory_limit=-1 ',
+	"$phpStanBin analyse ",
+	"$emailEditorPhpDir/src ",
+	"$emailEditorPhpDir/tests/integration ",
+	"$emailEditorPhpDir/tests/unit ",
+);
+
+$allCommands = implode( ' ', $commands );
+
+echo "[run-phpstan] Running command: $allCommands \n";
+
+passthru( $allCommands );
