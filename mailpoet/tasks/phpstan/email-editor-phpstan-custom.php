0d207a2afd50e791780d8c69ed0bcf1faa630cbd
Oluwaseun Olorunsola <seun.olorunsola@automattic.com>
2024-11-29 11:15:25 +0100

    Remove unnessary PHPStan ignore errors
    MAILPOET-6318

--- mailpoet/tasks/phpstan/email-editor-phpstan-custom.php
+++ mailpoet/tasks/phpstan/email-editor-phpstan-custom.php
@@ -2,11 +2,6 @@
 
 $config = [];
 $phpVersion = (int)getenv('ANALYSIS_PHP_VERSION') ?: PHP_VERSION_ID;
-$config['parameters']['phpVersion'] = $phpVersion;
-
-// Load PHP 8.2 specific config
-if ($phpVersion >= 80200) {
-   $config['includes'][] = 'email-editor-php-8-config.neon';
-}
+$config['parameters']['phpVersion'] = $phpVersion; // passes the PHP version to PHPStan
 
 return $config;
