634a624ba324d2c29ea5f9c5f596203d32f97e91
Jan Lysý <jan.lysy@automattic.com>
2024-12-03 17:24:56 +0100

    Add filtering personalization tags by category
    [MAILPOET-6354]

--- packages/js/email-editor/src/components/personalization-tags/personalization-tags-modal.tsx
+++ packages/js/email-editor/src/components/personalization-tags/personalization-tags-modal.tsx
@@ -1,11 +1,15 @@
-import { Modal, Button } from '@wordpress/components';
+import * as React from '@wordpress/element';
+import { Modal, Button, MenuGroup, MenuItem } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { PersonalizationTag, storeName } from '../../store';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { external, Icon } from '@wordpress/icons';
 import './index.scss';
+import { useState } from '@wordpress/element';
 
 const PersonalizationTagsModal = () => {
+	const [ activeCategory, setActiveCategory ] = useState( null );
+
 	const { togglePersonalizationTagsModal } = useDispatch( storeName );
 
 	const { isModalOpened, list } = useSelect(
@@ -29,6 +33,11 @@ const PersonalizationTagsModal = () => {
 		{} as Record< string, PersonalizationTag[] >
 	);
 
+	const getMenuItemClass = ( category ) =>
+		category === activeCategory
+			? 'mailpoet-personalization-tags-modal__menu-item-active'
+			: '';
+
 	return (
 		<Modal
 			size="medium"
@@ -41,13 +50,71 @@ const PersonalizationTagsModal = () => {
 				personalize your emails. Learn more{ ' ' }
 				<Icon icon={ external } size={ 16 } />
 			</p>
-			{ Object.entries( groupedTags ).map( ( [ category, items ] ) => (
-				<div key={ category }>
+			<MenuGroup className="mailpoet-personalization-tags-modal__menu">
+				<MenuItem
+					onClick={ () => setActiveCategory( null ) }
+					className={ `${ getMenuItemClass( null ) }` }
+				>
+					{ __( 'All' ) }
+				</MenuItem>
+				<div
+					className="mailpoet-personalization-tags-modal__menu-separator"
+					aria-hidden="true"
+				></div>
+				{ Object.entries( groupedTags ).map(
+					( [ category ], index, array ) => (
+						<React.Fragment key={ category }>
+							<MenuItem
+								onClick={ () => setActiveCategory( category ) }
+								className={ `${ getMenuItemClass(
+									category
+								) }` }
+							>
+								{ category }
+							</MenuItem>
+							{ index < array.length - 1 && (
+								<div
+									className="mailpoet-personalization-tags-modal__menu-separator"
+									aria-hidden="true"
+								></div>
+							) }
+						</React.Fragment>
+					)
+				) }
+			</MenuGroup>
+			{ activeCategory === null ? (
+				// Render all categories
+				Object.entries( groupedTags ).map( ( [ category, items ] ) => (
+					<div key={ category }>
+						<div className="mailpoet-personalization-tags-modal__category">
+							{ category }
+						</div>
+						<div className="mailpoet-personalization-tags-modal__category-group">
+							{ items.map( ( item ) => (
+								<div
+									className="mailpoet-personalization-tags-modal__category-group-item"
+									key={ item.token }
+								>
+									<div className="mailpoet-personalization-tags-modal__item-text">
+										<strong>{ item.name }</strong>
+										{ item.token }
+									</div>
+									<Button variant="link">
+										{ __( 'Insert' ) }
+									</Button>
+								</div>
+							) ) }
+						</div>
+					</div>
+				) )
+			) : (
+				// Render selected category
+				<div>
 					<div className="mailpoet-personalization-tags-modal__category">
-						{ category }
+						{ activeCategory }
 					</div>
 					<div className="mailpoet-personalization-tags-modal__category-group">
-						{ items.map( ( item ) => (
+						{ groupedTags[ activeCategory ]?.map( ( item ) => (
 							<div
 								className="mailpoet-personalization-tags-modal__category-group-item"
 								key={ item.token }
@@ -63,7 +130,7 @@ const PersonalizationTagsModal = () => {
 						) ) }
 					</div>
 				</div>
-			) ) }
+			) }
 		</Modal>
 	);
 };
