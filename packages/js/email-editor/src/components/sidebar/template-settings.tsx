e514365bd7da2aab439d3ebad34090cfbd45a311
Rostislav Wolny <rosta.wolny@automattic.com>
2024-12-04 14:02:00 +0100

    Remove experimental templates panel
    [MAILPOET-6336]

--- packages/js/email-editor/src/components/sidebar/template-settings.tsx
+++ packages/js/email-editor/src/components/sidebar/template-settings.tsx
@@ -1,12 +1,10 @@
 import { Panel } from '@wordpress/components';
 import { TemplateInfo } from './template-info';
-import { TemplatesPanel } from './templates-panel';
 
 export function TemplateSettings() {
 	return (
 		<Panel>
 			<TemplateInfo />
-			<TemplatesPanel />
 		</Panel>
 	);
 }
