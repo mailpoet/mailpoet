2f934b80f0ad41064d34e327529c824e4d83c1ab
Rostislav Wolny <rosta.wolny@automattic.com>
2024-12-09 16:20:53 +0100

    Add hook for working with email global styles
    [MAILPOET-6335]

---
+++ packages/js/email-editor/src/hooks/use-user-theme.ts
@@ -0,0 +1,41 @@
+import { useCallback } from '@wordpress/element';
+import { useSelect, dispatch } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
+import { EmailTheme, storeName } from '../store';
+
+export function useUserTheme() {
+	const { globalStylePost } = useSelect( ( select ) => {
+		const post = select(
+			storeName
+		).getGlobalEmailStylesPost() as EmailTheme & { id: number };
+		return {
+			globalStylePost: post,
+		};
+	}, [] );
+
+	const updateGlobalStylesPost = useCallback(
+		( newTheme: EmailTheme ) => {
+			if ( ! globalStylePost ) {
+				return;
+			}
+			void dispatch( coreStore ).editEntityRecord(
+				'postType',
+				'wp_global_styles',
+				globalStylePost.id,
+				{
+					styles: newTheme.styles,
+					settings: newTheme.settings,
+				}
+			);
+		},
+		[ globalStylePost ]
+	);
+
+	return {
+		userTheme: {
+			settings: globalStylePost?.settings,
+			styles: globalStylePost?.styles,
+		},
+		updateUserTheme: updateGlobalStylesPost,
+	};
+}
