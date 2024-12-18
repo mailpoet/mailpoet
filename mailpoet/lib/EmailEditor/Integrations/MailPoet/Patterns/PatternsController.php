be792bf6b879f68c1831ecbcd307b9453a000b0e
Rostislav Wolny <rosta.wolny@automattic.com>
2024-12-18 11:29:19 +0100

    Remove the old patterns
    [MAILPOET-6334]

--- mailpoet/lib/EmailEditor/Integrations/MailPoet/Patterns/PatternsController.php
+++ mailpoet/lib/EmailEditor/Integrations/MailPoet/Patterns/PatternsController.php
@@ -2,8 +2,6 @@
 
 namespace MailPoet\EmailEditor\Integrations\MailPoet\Patterns;
 
-use MailPoet\EmailEditor\Integrations\MailPoet\Patterns\Library\DefaultContent;
-use MailPoet\EmailEditor\Integrations\MailPoet\Patterns\Library\DefaultContentFull;
 use MailPoet\EmailEditor\Integrations\MailPoet\Patterns\Library\OneColumn;
 use MailPoet\Util\CdnAssetUrl;
 
@@ -18,8 +16,6 @@ class PatternsController {
 
   public function registerPatterns(): void {
     $patterns = [];
-    $patterns[] = new DefaultContentFull($this->cdnAssetUrl);
-    $patterns[] = new DefaultContent($this->cdnAssetUrl);
     $patterns[] = new OneColumn($this->cdnAssetUrl);
     foreach ($patterns as $pattern) {
       register_block_pattern($pattern->get_namespace() . '/' . $pattern->get_name(), $pattern->get_properties());
