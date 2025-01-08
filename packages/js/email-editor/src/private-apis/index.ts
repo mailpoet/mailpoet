4109f25a809210551603da3f715edaea9b06028f
Oluwaseun Olorunsola <seun.olorunsola@automattic.com>
2025-01-08 15:01:11 +0100

    Switching to @wordpress/edit-site to fix incompatibility issues with Jetpack

--- packages/js/email-editor/src/private-apis/index.ts
+++ packages/js/email-editor/src/private-apis/index.ts
@@ -11,7 +11,7 @@ import { store as coreStore } from '@wordpress/core-data';
 
 const { unlock } = __dangerousOptInToUnstableAPIsOnlyForCoreModules(
 	'I acknowledge private features are not for use in themes or plugins and doing so will break in the next version of WordPress.',
-	'@wordpress/edit-post' // The module name must be in the list of allowed, so for now I used the package name of the post editor
+	'@wordpress/edit-site' // The module name must be in the list of allowed, so for now I used the package name of the post editor
 );
 
 /**
