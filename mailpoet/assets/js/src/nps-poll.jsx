d16ce156b2b1ac1f3480a15bc69eecc931db79cd
Rostislav Wolny <rosta.wolny@automattic.com>
2025-01-10 14:48:19 +0100

    Export function for manual initalization of satismeter survey
    We need to export the function to be able to trigger it manually on click.
    [MAILPOET-6418]

--- mailpoet/assets/js/src/nps-poll.jsx
+++ mailpoet/assets/js/src/nps-poll.jsx
@@ -5,78 +5,82 @@ import satismeter from 'satismeter-loader';
 import { ReviewRequest } from 'review-request';
 import { getTrackingData } from 'analytics.js';
 
+export const initializeSatismeterSurvey = () => {
+  const showReviewRequestModal = () => {
+    MailPoet.Modal.popup({
+      width: 800,
+      template: ReactDOMServer.renderToString(
+        ReviewRequest({
+          username:
+            window.mailpoet_current_wp_user_firstname ||
+            window.mailpoet_current_wp_user.user_login,
+          reviewRequestIllustrationUrl:
+            window.mailpoet_review_request_illustration_url,
+          installedDaysAgo: window.mailpoet_installed_days_ago,
+        }),
+      ),
+      onInit: () => {
+        document
+          .getElementById('mailpoet_review_request_not_now')
+          .addEventListener('click', () => MailPoet.Modal.close());
+      },
+    });
+  };
+
+  const callSatismeter = (trackingData) => {
+    const newUsersPollId = '6L479eVPXk7pBn6S';
+    const oldUsersPollId = 'k0aJAsQAWI2ERyGv';
+    const formPollId = 'EqOgKsgZd832Sz9w';
+    const emailEditorPollId = '9qCj2SJBE1s5OhnX5NYfRXu82pEDUB9x';
+    let writeKey;
+    if (window.mailpoet_display_nps_email_editor) {
+      writeKey = emailEditorPollId;
+    } else if (window.mailpoet_display_nps_form) {
+      writeKey = formPollId;
+    } else if (window.mailpoet_is_new_user) {
+      writeKey = newUsersPollId;
+    } else {
+      writeKey = oldUsersPollId;
+    }
+    satismeter({
+      writeKey,
+      userId: window.mailpoet_current_wp_user.ID + window.mailpoet_site_url,
+      traits: {
+        name: window.mailpoet_current_wp_user.user_nicename,
+        email: window.mailpoet_current_wp_user.user_email,
+        mailpoetVersion: window.mailpoet_version,
+        mailpoetPremiumIsActive: window.mailpoet_premium_active,
+        createdAt: trackingData.installedAtIso,
+        newslettersSent: trackingData.newslettersSent,
+        welcomeEmails: trackingData.welcomeEmails,
+        postnotificationEmails: trackingData.postnotificationEmails,
+        woocommerceEmails: trackingData.woocommerceEmails,
+        subscribers: trackingData.subscribers,
+        lists: trackingData.lists,
+        sendingMethod: trackingData.sendingMethod,
+        woocommerceIsInstalled: trackingData.woocommerceIsInstalled,
+      },
+      events: {
+        submit: (response) => {
+          if (response.rating >= 9 && response.completed) {
+            showReviewRequestModal();
+          }
+        },
+      },
+    });
+  };
+
+  if (
+    window.mailpoet_display_nps_poll &&
+    window.mailpoet_3rd_party_libs_enabled
+  ) {
+    getTrackingData().then(({ data }) => callSatismeter(data));
+  }
+};
+
 const useNpsPoll = () => {
   useLayoutEffect(() => {
-    const showReviewRequestModal = () => {
-      MailPoet.Modal.popup({
-        width: 800,
-        template: ReactDOMServer.renderToString(
-          ReviewRequest({
-            username:
-              window.mailpoet_current_wp_user_firstname ||
-              window.mailpoet_current_wp_user.user_login,
-            reviewRequestIllustrationUrl:
-              window.mailpoet_review_request_illustration_url,
-            installedDaysAgo: window.mailpoet_installed_days_ago,
-          }),
-        ),
-        onInit: () => {
-          document
-            .getElementById('mailpoet_review_request_not_now')
-            .addEventListener('click', () => MailPoet.Modal.close());
-        },
-      });
-    };
-
-    const callSatismeter = (trackingData) => {
-      const newUsersPollId = '6L479eVPXk7pBn6S';
-      const oldUsersPollId = 'k0aJAsQAWI2ERyGv';
-      const formPollId = 'EqOgKsgZd832Sz9w';
-      const emailEditorPollId = '9qCj2SJBE1s5OhnX5NYfRXu82pEDUB9x';
-      let writeKey;
-      if (window.mailpoet_display_nps_email_editor) {
-        writeKey = emailEditorPollId;
-      } else if (window.mailpoet_display_nps_form) {
-        writeKey = formPollId;
-      } else if (window.mailpoet_is_new_user) {
-        writeKey = newUsersPollId;
-      } else {
-        writeKey = oldUsersPollId;
-      }
-      satismeter({
-        writeKey,
-        userId: window.mailpoet_current_wp_user.ID + window.mailpoet_site_url,
-        traits: {
-          name: window.mailpoet_current_wp_user.user_nicename,
-          email: window.mailpoet_current_wp_user.user_email,
-          mailpoetVersion: window.mailpoet_version,
-          mailpoetPremiumIsActive: window.mailpoet_premium_active,
-          createdAt: trackingData.installedAtIso,
-          newslettersSent: trackingData.newslettersSent,
-          welcomeEmails: trackingData.welcomeEmails,
-          postnotificationEmails: trackingData.postnotificationEmails,
-          woocommerceEmails: trackingData.woocommerceEmails,
-          subscribers: trackingData.subscribers,
-          lists: trackingData.lists,
-          sendingMethod: trackingData.sendingMethod,
-          woocommerceIsInstalled: trackingData.woocommerceIsInstalled,
-        },
-        events: {
-          submit: (response) => {
-            if (response.rating >= 9 && response.completed) {
-              showReviewRequestModal();
-            }
-          },
-        },
-      });
-    };
-
-    if (
-      window.mailpoet_display_nps_poll &&
-      window.mailpoet_3rd_party_libs_enabled
-    ) {
-      getTrackingData().then(({ data }) => callSatismeter(data));
-    }
+    initializeSatismeterSurvey();
   }, []);
 
   return null;
