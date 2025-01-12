02937a7c274a4d3305d413c2b5358fccd455fb56
 Ján Mikláš <neosinner@gmail.com>
2025-01-12 17:06:56 +0100

    Add test
    [MAILPOET-4160]

--- mailpoet/tests/unit/Entities/SubscriberEntityTest.php
+++ mailpoet/tests/unit/Entities/SubscriberEntityTest.php
@@ -9,6 +9,21 @@ class SubscriberEntityTest extends \MailPoetUnitTest {
     verify($subscriber->wp_user_id)->equals(4);// phpcs:ignore Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps
   }
 
+  public function testMagicGetterThrowsDeprecationWarning() {
+    $expectedMessage = 'Direct access to $subscriber->wp_user_id is deprecated and will be removed after 2026-01-01. Use $subscriber->getWpUserId() instead.';
+    $caughtError = null;
+    set_error_handler(function ($errno, $errstr) use (&$caughtError) {
+      if ($errno === E_USER_DEPRECATED) {
+        $caughtError = $errstr;
+      }
+    });
+    $subscriber = new SubscriberEntity();
+    $subscriber->setWpUserId(4);
+    $subscriber->wp_user_id;// phpcs:ignore Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps
+    restore_error_handler();
+    verify($caughtError)->equals($expectedMessage);
+  }
+
   public function testMagicGetterReturnsNullForUnknown() {
     $subscriber = new SubscriberEntity();
     verify($subscriber->non_existing_property)->null();// phpcs:ignore Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps
