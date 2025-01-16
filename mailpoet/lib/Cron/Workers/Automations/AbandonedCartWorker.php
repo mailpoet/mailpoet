8448365400afc15a4d046d7c8d70bee4fb44365a
Arnaud Berthomier <oz@cyprio.net>
2025-01-16 12:07:09 -0600

    Branch out of AbandonedCart jobs earlier
    If the automation is inactive or there're no subscribers, the worker can
    consider its job done, and return true.
    
    [MAILPOET-6435]

--- mailpoet/lib/Cron/Workers/Automations/AbandonedCartWorker.php
+++ mailpoet/lib/Cron/Workers/Automations/AbandonedCartWorker.php
@@ -45,16 +45,16 @@ class AbandonedCartWorker extends SimpleWorker {
 
     $subscribers = $task->getSubscribers();
     if ($subscribers->count() !== 1) {
-      return false;
+      return true;
     }
     $subscriber = isset($subscribers[0]) ? $subscribers[0]->getSubscriber() : null;
     if (!$subscriber) {
-      return false;
+      return true;
     }
 
     $automation = $this->automationStorage->getAutomation((int)$automationId, (int)$automationVersion);
     if (!$automation || $automation->getStatus() !== Automation::STATUS_ACTIVE) {
-      return false;
+      return true;
     }
 
     $this->wp->doAction(
