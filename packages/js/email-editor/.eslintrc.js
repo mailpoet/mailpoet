16eaa5e5fe02b44090f1a81c8aabf478e80dbb7b
Rostislav Wolny <rosta.wolny@automattic.com>
2025-01-16 11:10:02 +0100

    Configure mailpoet as allowed text domain
    [MAILPOET-6438]

--- packages/js/email-editor/.eslintrc.js
+++ packages/js/email-editor/.eslintrc.js
@@ -5,6 +5,12 @@ module.exports = {
 			files: [ '**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx' ],
 			rules: {
 				'react/react-in-jsx-scope': 'off',
+				'@wordpress/i18n-text-domain': [
+					'error',
+					{
+						allowedTextDomain: [ 'mailpoet' ],
+					},
+				],
 			},
 		},
 	],
