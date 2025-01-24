5351ab24c6d3970900b4c8f192856edfecaf0c83
Rostislav Wolny <rosta.wolny@automattic.com>
2025-01-24 11:24:45 +0100

    Add filtering

--- packages/js/email-editor/src/data-views/listing-view.tsx
+++ packages/js/email-editor/src/data-views/listing-view.tsx
@@ -2,21 +2,22 @@
  * External dependencies
  */
 import { DataViews, View } from '@wordpress/dataviews';
-import { useEntityRecords, Post, store as coreStore } from '@wordpress/core-data';
+import {
+	useEntityRecords,
+	Post,
+	store as coreStore,
+} from '@wordpress/core-data';
 import { useSelect } from '@wordpress/data';
 import { useState, useMemo } from '@wordpress/element';
 import { Icon } from '@wordpress/components';
 import { edit, external } from '@wordpress/icons';
 
 export function ListingView() {
-	const [view, setView] = useState( {
+	const [ view, setView ] = useState< View >( {
 		type: 'table',
 		search: '',
-		fields: ['author', 'status', 'date'],
-		filters: [
-			{ field: 'author', operator: 'is', value: 2 },
-			{ field: 'status', operator: 'isAny', value: [ 'sent', 'draft' ] },
-		],
+		fields: [ 'author', 'status', 'date' ],
+		filters: [],
 		page: 1,
 		perPage: 10,
 		sort: {
@@ -25,9 +26,18 @@ export function ListingView() {
 		},
 		titleField: 'title',
 		showTitle: true,
-	} as View ) ;
+	} );
 
 	const queryArgs = useMemo( () => {
+		const filters = {};
+		view.filters.forEach( ( filter ) => {
+			if ( filter.field === 'status' && filter.operator === 'isAny' ) {
+				filters.status = filter.value;
+			}
+			if ( filter.field === 'author' && filter.operator === 'is' ) {
+				filters.author = filter.value;
+			}
+		} );
 		return {
 			status: 'any',
 			per_page: view.perPage,
@@ -36,22 +46,34 @@ export function ListingView() {
 			order: view.sort?.direction,
 			orderby: view.sort?.field,
 			search: view.search,
+			...filters,
 		};
 	}, [ view ] );
 
-	const { records } = useEntityRecords( 'postType', 'mailpoet_email', queryArgs );
-	const { totalRecords } = useSelect( ( select ) => {
-		return {
-			totalRecords: select( coreStore ).getEntityRecordsTotalItems( 'postType', 'mailpoet_email', queryArgs ),
-		}
-	}, [ queryArgs ] );
+	const { records } = useEntityRecords(
+		'postType',
+		'mailpoet_email',
+		queryArgs
+	);
+	const { totalRecords } = useSelect(
+		( select ) => {
+			return {
+				totalRecords: select( coreStore ).getEntityRecordsTotalItems(
+					'postType',
+					'mailpoet_email',
+					queryArgs
+				),
+			};
+		},
+		[ queryArgs ]
+	);
 
 	const fields = [
 		{
 			id: 'title',
 			label: 'Title',
 			enableHiding: false,
-			render: (item) => {
+			render: ( item ) => {
 				return item.item.title.raw;
 			},
 			Edit: 'text',
@@ -65,27 +87,60 @@ export function ListingView() {
 			id: 'author',
 			label: 'Author',
 			enableHiding: true,
-			render: (item) => {
-				const author = item.item._embedded?.author?.[0] || null;
-				console.log(item.item._embedded)
-				const avatarUrl = author?.avatar_urls?.[24] || null;
-				if (!author) return '-';
-				return <>
-					{ avatarUrl && <img src={ avatarUrl	} alt={author.name} style={{width:'24px', height:'24px', borderRadius: '12px'}}/> } { author.name }
-				</>
+			render: ( item ) => {
+				const author = item.item._embedded?.author?.[ 0 ] || null;
+				const avatarUrl = author?.avatar_urls?.[ 24 ] || null;
+				if ( ! author ) return '-';
+				return (
+					<>
+						{ avatarUrl && (
+							<>
+								<img
+									src={ avatarUrl }
+									alt={ author.name }
+									style={ {
+										width: '24px',
+										height: '24px',
+										borderRadius: '14px',
+										border: '1px solid #ddd',
+									} }
+								/>
+								&nbsp;
+							</>
+						) }
+						{ author.name }
+					</>
+				);
 			},
+			filterBy: {
+				operators: [ 'is' ],
+			},
+			elements: [
+				// @todo Here we need to list all the authors from DB
+				{ value: 1, label: 'Admin' },
+				{ value: 2, label: 'User' },
+			],
 		},
 		{
 			id: 'status',
 			label: 'Status',
 			enableHiding: true,
+			filterBy: {
+				operators: [ 'isAny' ],
+			},
+			elements: [
+				{ value: 'draft', label: 'Draft' },
+				{ value: 'sent', label: 'Sent' },
+				{ value: 'active', label: 'Active' },
+			],
 		},
 		{
 			id: 'date',
 			label: 'Date',
 			enableHiding: false,
 			render: ( { item } ) => {
-				return <time>{ item.date }</time>;
+				const date = new Date( item.date );
+				return <time>{ date.toLocaleString() }</time>;
 			},
 		},
 	];
@@ -97,7 +152,7 @@ export function ListingView() {
 			icon: <Icon icon={ edit } />,
 			supportsBulk: false,
 			callback: ( items ) => {
-				window.location.href = `/wp-admin/post.php?post=${ items[0].id }&action=edit`;
+				window.location.href = `/wp-admin/post.php?post=${ items[ 0 ].id }&action=edit`;
 			},
 			isPrimary: true,
 		},
@@ -107,29 +162,33 @@ export function ListingView() {
 			icon: <Icon icon={ external } />,
 			supportsBulk: false,
 			callback: ( items ) => {
-				window.open(items[0].mailpoet_data.preview_url, '_blank').focus();
-			}
-		}
+				window
+					.open( items[ 0 ].mailpoet_data.preview_url, '_blank' )
+					.focus();
+			},
+		},
 	];
 
-	if ( records === null || totalRecords === null ) {
-		return null;
-	}
+	const form = {
+		type: 'panel',
+		fields: [ 'title' ],
+	};
 
 	console.log( 'emails', records );
 
 	return (
 		<DataViews
 			view={ view }
+			form={ form }
 			actions={ actions }
 			onChangeView={ setView }
 			fields={ fields }
-			data={ records }
+			data={ records ?? [] }
 			paginationInfo={ {
 				totalItems: totalRecords,
 				totalPages: Math.ceil( totalRecords / view.perPage ),
 			} }
-			defaultLayouts={{
+			defaultLayouts={ {
 				table: {
 					showMedia: false,
 				},
@@ -138,8 +197,8 @@ export function ListingView() {
 				},
 				list: {
 					showMedia: true,
-				}
-			}}
+				},
+			} }
 			getItemId={ ( item: Post ) => item.id.toString() }
 		/>
 	);
