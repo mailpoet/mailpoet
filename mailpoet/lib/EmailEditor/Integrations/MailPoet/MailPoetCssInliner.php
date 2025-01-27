edb96befbc252b9f8541dbd84b7494a1ea5e7e1a
Pavel Dohnal <pavel.dohnal@automattic.com>
2025-01-27 12:46:52 +0100

    Ensure the inliner is called correctly
    [MAILPOET-6428]

--- mailpoet/lib/EmailEditor/Integrations/MailPoet/MailPoetCssInliner.php
+++ mailpoet/lib/EmailEditor/Integrations/MailPoet/MailPoetCssInliner.php
@@ -15,11 +15,17 @@ class MailPoetCssInliner implements Css_Inliner {
   }
 
   public function inline_css(string $css = ''): self {// phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps -- we need to match the interface
+    if (!isset($this->inliner)) {
+      throw new \LogicException('You must call from_html before calling inline_css');
+    }
     $this->inliner->inlineCss($css);
     return $this;
   }
 
   public function render(): string {
+    if (!isset($this->inliner)) {
+      throw new \LogicException('You must call from_html before calling inline_css');
+    }
     return $this->inliner->render();
   }
 }
