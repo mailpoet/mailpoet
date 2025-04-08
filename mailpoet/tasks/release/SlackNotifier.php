34a7bec8b71912a1a09d108b474a1f59488caf9c
 Ján Mikláš <neosinner@gmail.com>
2025-04-08 16:07:32 +0200

    Remove Jira link from Slack notifier
    [STOMAIL-7353]

--- mailpoet/tasks/release/SlackNotifier.php
+++ mailpoet/tasks/release/SlackNotifier.php
@@ -26,19 +26,18 @@ class SlackNotifier {
     $this->httpClient = new Client();
   }
 
-  public function notify($version, $changelog, $releaseId) {
-    $message = $this->formatMessage($version, $changelog, $releaseId);
+  public function notify($version, $changelog) {
+    $message = $this->formatMessage($version, $changelog);
     $this->sendMessage($message);
   }
 
-  private function formatMessage($version, $changelog, $releaseId) {
+  private function formatMessage($version, $changelog) {
     $pluginType = $this->project === self::PROJECT_MAILPOET ? 'Free' : 'Premium';
     $githubPath = $this->project === self::PROJECT_MAILPOET ? 'mailpoet' : 'mailpoet-premium';
 
     $message = "*$pluginType plugin `$version` released :tada:!*\n";
     $message .= "\n";
     $message .= "GitHub: https://github.com/mailpoet/$githubPath/releases/tag/$version\n";
-    $message .= "JIRA: https://mailpoet.atlassian.net/projects/$this->project/versions/$releaseId\n";
     if ($this->project === self::PROJECT_MAILPOET) {
       $message .= "WordPress: https://wordpress.org/plugins/mailpoet/#developers\n";
     }
