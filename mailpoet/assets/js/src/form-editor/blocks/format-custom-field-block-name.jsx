56438e4eeec9b7b0a131174a8a03a45afc96ad86
Pavel Dohnal <pavel.dohnal@automattic.com>
2025-04-29 12:53:24 +0200

    Fix handling of Japanese characters in custom field blocks
    [STOMAIL-7383]

--- mailpoet/assets/js/src/form-editor/blocks/format-custom-field-block-name.jsx
+++ mailpoet/assets/js/src/form-editor/blocks/format-custom-field-block-name.jsx
@@ -1,8 +1,15 @@
 import slugify from 'slugify';
 
 export function formatCustomFieldBlockName(blockName, customField) {
-  const name = slugify(customField.name, { lower: true })
+  let name = slugify(customField.name, { lower: true })
     .replace(/[^a-z0-9]+/g, '')
     .replace(/-$/, '');
+
+  // Ensure unique block names by appending ID if the slug is empty or too short
+  // (which can happen with certain character sets)
+  if (!name || name.length < 2) {
+    name = `field${customField.id}`;
+  }
+
   return `${blockName}-${name}`;
 }
