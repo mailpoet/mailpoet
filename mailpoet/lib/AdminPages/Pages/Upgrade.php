25faaf95b8de785261a1402dfdb8e228845da8f8
 Ján Mikláš <neosinner@gmail.com>
2025-05-14 16:05:50 +0200

    Remove old upgrade page
    STOMAIL-7407

--- mailpoet/lib/AdminPages/Pages/Upgrade.php
+++ mailpoet/lib/AdminPages/Pages/Upgrade.php
@@ -3,7 +3,6 @@
 namespace MailPoet\AdminPages\Pages;
 
 use MailPoet\AdminPages\PageRenderer;
-use MailPoet\Util\License\Features\CapabilitiesManager;
 use MailPoet\WP\Functions as WPFunctions;
 
 class Upgrade {
@@ -13,33 +12,18 @@ class Upgrade {
   /** @var WPFunctions */
   private $wp;
 
-  private CapabilitiesManager $capabilitiesManager;
-
   public function __construct(
     PageRenderer $pageRenderer,
-    WPFunctions $wp,
-    CapabilitiesManager $capabilitiesManager
+    WPFunctions $wp
   ) {
     $this->pageRenderer = $pageRenderer;
     $this->wp = $wp;
-    $this->capabilitiesManager = $capabilitiesManager;
   }
 
   public function render() {
     $data = [
       'current_wp_user' => $this->wp->wpGetCurrentUser()->to_array(),
     ];
-
-    // @todo change this after a/b test to keep only one of the pages
-    if ($this->capabilitiesManager->showNewUpgradePage()) {
-      $data = [
-        'current_mailpoet_plan_tier' => $this->capabilitiesManager->getTier(),
-      ];
-
-      $this->pageRenderer->displayPage('upgrade_tiers.html', $data);
-      return;
-    }
-
-    $this->pageRenderer->displayPage('upgrade.html', $data);
+    $this->pageRenderer->displayPage('upgrade_tiers.html', $data);
   }
 }
