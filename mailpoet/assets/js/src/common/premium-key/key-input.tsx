5e582b483945af07e5cf186eb5f2c2ab674e7d5f
Oluwaseun Olorunsola <seun.olorunsola@automattic.com>
2025-07-03 13:53:23 +0100

    Refactor KeyInput component to use PasswordInput and remove toggle button functionality
    - Replaced the existing input type with PasswordInput for better password handling.
    - Removed the toggle button for revealing/hiding the premium key, simplifying the component.
    
    STOMAIL-7318

--- mailpoet/assets/js/src/common/premium-key/key-input.tsx
+++ mailpoet/assets/js/src/common/premium-key/key-input.tsx
@@ -1,7 +1,5 @@
-import { _x } from '@wordpress/i18n';
-import { Button, Input } from 'common/index';
+import { PasswordInput } from 'common/index';
 import { useAction, useSelector } from 'settings/store/hooks';
-import { useState } from 'react';
 
 type KeyInputPropType = {
   placeholder?: string;
@@ -16,25 +14,9 @@ export function KeyInput({
 }: KeyInputPropType) {
   const state = useSelector('getKeyActivationState')();
   const setState = useAction('updateKeyActivationState');
-  const [isRevealed, setIsRevealed] = useState(false);
-  const inputType = forceRevealed || isRevealed ? 'text' : 'password';
-  const toggleButton = !forceRevealed && (
-    <Button
-      className="mailpoet-premium-key-toggle"
-      variant="tertiary"
-      onClick={() => setIsRevealed(!isRevealed)}
-    >
-      {isRevealed
-        ? // translators: Used as a button to show or hide the premium key
-          _x('Hide', 'verb', 'mailpoet')
-        : // translators: Used as a button to show or hide the premium key
-          _x('Show', 'verb', 'mailpoet')}
-    </Button>
-  );
-
   return (
-    <Input
-      type={inputType}
+    <PasswordInput
+      forceRevealed={forceRevealed}
       id="mailpoet_premium_key"
       name="premium[premium_key]"
       placeholder={placeholder}
@@ -48,7 +30,6 @@ export function KeyInput({
           key: event.target.value.trim() || null,
         })
       }
-      iconEnd={toggleButton}
     />
   );
 }
