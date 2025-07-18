62e7644ee85d2e2a2c1cbe81db5042f6d74250f8
Pavel Dohnal <pavel.dohnal@automattic.com>
2025-07-18 09:09:42 +0200

    Add docsblock for Trigger interface
    [STOMAIL-7366]

--- mailpoet/lib/Automation/Engine/Integration/Trigger.php
+++ mailpoet/lib/Automation/Engine/Integration/Trigger.php
@@ -4,8 +4,37 @@ namespace MailPoet\Automation\Engine\Integration;
 
 use MailPoet\Automation\Engine\Data\StepRunArgs;
 
+/**
+ * Trigger Interface
+ *
+ * Triggers are the entry points of automations that determine when an automation
+ * should start executing based on specific events or conditions.
+ *
+ */
 interface Trigger extends Step {
+  /**
+   * Register WordPress hooks to listen for trigger events.
+   *
+   * This method is called when automations using this trigger are activated.
+   * Implementations should use WordPress action/filter hooks to listen for
+   * specific events that should trigger the automation.
+   *
+   * @return void
+   */
   public function registerHooks(): void;
 
+  /**
+   * Determine if the trigger should fire based on the current context.
+   *
+   * This method is called when a trigger event occurs to determine if the
+   * automation should actually start. It receives the step run arguments
+   * containing automation data, subjects, and other context information.
+   *
+   * The method should return true if the automation should proceed, false otherwise.
+   * This allows for fine-grained control over when automations execute.
+   *
+   * @param StepRunArgs $args The step run arguments containing automation context
+   * @return bool True if the automation should proceed, false otherwise
+   */
   public function isTriggeredBy(StepRunArgs $args): bool;
 }
