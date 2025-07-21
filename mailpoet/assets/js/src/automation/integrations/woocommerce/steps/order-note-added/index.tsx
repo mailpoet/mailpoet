266f1ccee55bc52bac5bed700a206263e74e596d
 Ján Mikláš <neosinner@gmail.com>
2025-07-21 14:13:47 +0200

    Unify trigger title case with other triggers
    [STOMAIL-7366]

--- mailpoet/assets/js/src/automation/integrations/woocommerce/steps/order-note-added/index.tsx
+++ mailpoet/assets/js/src/automation/integrations/woocommerce/steps/order-note-added/index.tsx
@@ -5,18 +5,18 @@ import { Edit } from './edit';
 
 const keywords = [
   __('woocommerce', 'mailpoet'),
-  // translators: noun, used as a search keyword for "Order Note Added" trigger
+  // translators: noun, used as a search keyword for "Order note added" trigger
   __('order', 'mailpoet'),
-  // translators: noun, used as a search keyword for "Order Note Added" trigger
+  // translators: noun, used as a search keyword for "Order note added" trigger
   __('note', 'mailpoet'),
-  // translators: noun, used as a search keyword for "Order Note Added" trigger
+  // translators: noun, used as a search keyword for "Order note added" trigger
   __('comment', 'mailpoet'),
 ];
 
 export const step: StepType = {
   key: 'woocommerce:order-note-added',
   group: 'triggers',
-  title: () => __('Order Note Added', 'mailpoet'),
+  title: () => __('Order note added', 'mailpoet'),
   description: () =>
     __(
       'Fires when any note is added to an order, can include both private notes and notes to the customer. These notes appear on the right of the order edit screen.',
