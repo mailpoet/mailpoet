45f6aafed54359427593020424da9ac6806ceaa8
Rostislav Wolny <rosta.wolny@automattic.com>
2025-10-31 12:31:01 +0100

    Point PHPStan to dependencies installed in vendor-prefixed
    PHPStan for Doctrine needs to know the version of the Doctrine packages.
    We need to give the composer the correct data to return the proper versions.
    
    STOMAIL-7513

--- mailpoet/tasks/phpstan/bootstrap.php
+++ mailpoet/tasks/phpstan/bootstrap.php
@@ -15,6 +15,11 @@ define('MAILPOET_PREMIUM_VERSION', '1.0.0');
 // This needs to be set because \MailPoet\Doctrine\TablePrefixMetadataFactory can't construct without it
 MailPoet\Config\Env::$dbPrefix = 'wp_';
 
+// PHPStan for Doctrine needs to know the version of Doctrine packages we use for some checks.
+// We need to point it to the prefixer's installed.php file because that's where the Doctrine packages are installed.
+$data = require __DIR__ . '/../../prefixer/vendor/composer/installed.php';
+\Composer\InstalledVersions::reload($data);
+
 // Load tracy
 $tracyPath = __DIR__ . '/../../tools/vendor/tracy.phar';
 require_once($tracyPath);
