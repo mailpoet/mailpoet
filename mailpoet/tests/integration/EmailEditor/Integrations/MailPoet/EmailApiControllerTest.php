9b6c31b9faafa7ee4bf27d273f02e8748046c2d9
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2025-11-13 13:19:59 +0200

    Update EmailApiController tests

--- mailpoet/tests/integration/EmailEditor/Integrations/MailPoet/EmailApiControllerTest.php
+++ mailpoet/tests/integration/EmailEditor/Integrations/MailPoet/EmailApiControllerTest.php
@@ -4,9 +4,12 @@ namespace MailPoet\EmailEditor\Integrations\MailPoet;
 
 use MailPoet\API\JSON\Response as APIResponse;
 use MailPoet\Entities\NewsletterEntity;
+use MailPoet\Entities\NewsletterOptionFieldEntity;
 use MailPoet\Newsletter\NewslettersRepository;
 use MailPoet\NotFoundException;
 use MailPoet\Test\DataFactories\Newsletter as NewsletterFactory;
+use MailPoet\Test\DataFactories\NewsletterOptionField;
+use MailPoet\Test\DataFactories\Segment as SegmentFactory;
 use MailPoet\UnexpectedValueException;
 
 class EmailApiControllerTest extends \MailPoetTest {
@@ -87,6 +90,186 @@ class EmailApiControllerTest extends \MailPoetTest {
       verify($exception->getHttpStatusCode())->equals(APIResponse::STATUS_BAD_REQUEST);
       verify($exception->getMessage())->stringContainsString('Newsletter ID does not match the post ID');
     }
+  }
+
+  public function testItUpdatesScheduledAtAndSetsIsScheduledTo1(): void {
+    $wpPostId = 7;
+    $newsletter = (new NewsletterFactory())
+      ->withWpPostId($wpPostId)
+      ->create();
+
+    (new NewsletterOptionField())->findOrCreate(
+      NewsletterOptionFieldEntity::NAME_SCHEDULED_AT,
+      NewsletterEntity::TYPE_STANDARD
+    );
+    (new NewsletterOptionField())->findOrCreate(
+      NewsletterOptionFieldEntity::NAME_IS_SCHEDULED,
+      NewsletterEntity::TYPE_STANDARD
+    );
+    $this->entityManager->flush();
+
+    $scheduledAt = '2024-12-25 14:30:00';
+    $this->emailApiController->saveEmailData([
+      'id' => $newsletter->getId(),
+      'subject' => 'Test Subject',
+      'preheader' => 'Test Preheader',
+      'scheduled_at' => $scheduledAt,
+    ], new \WP_Post((object)['ID' => $wpPostId]));
+
+    $this->entityManager->clear();
+    $newsletter = $this->newslettersRepository->findOneById($newsletter->getId());
+    $this->assertInstanceOf(NewsletterEntity::class, $newsletter);
+    verify($newsletter->getOptionValue(NewsletterOptionFieldEntity::NAME_SCHEDULED_AT))->equals($scheduledAt);
+    verify($newsletter->getOptionValue(NewsletterOptionFieldEntity::NAME_IS_SCHEDULED))->equals('1');
+  }
+
+  public function testItUpdatesScheduledAtToNullAndSetsIsScheduledTo0(): void {
+    $wpPostId = 8;
+    $newsletter = (new NewsletterFactory())
+      ->withWpPostId($wpPostId)
+      ->create();
+
+    (new NewsletterOptionField())->findOrCreate(
+      NewsletterOptionFieldEntity::NAME_SCHEDULED_AT,
+      NewsletterEntity::TYPE_STANDARD
+    );
+    (new NewsletterOptionField())->findOrCreate(
+      NewsletterOptionFieldEntity::NAME_IS_SCHEDULED,
+      NewsletterEntity::TYPE_STANDARD
+    );
+    $this->entityManager->flush();
+
+    // First set a scheduled time.
+    $scheduledAt = '2024-12-25 14:30:00';
+    $this->emailApiController->saveEmailData([
+      'id' => $newsletter->getId(),
+      'subject' => 'Test Subject',
+      'preheader' => 'Test Preheader',
+      'scheduled_at' => $scheduledAt,
+    ], new \WP_Post((object)['ID' => $wpPostId]));
+
+    // Then clear it by setting to null.
+    $this->emailApiController->saveEmailData([
+      'id' => $newsletter->getId(),
+      'subject' => 'Test Subject',
+      'preheader' => 'Test Preheader',
+      'scheduled_at' => null,
+    ], new \WP_Post((object)['ID' => $wpPostId]));
+
+    $this->entityManager->clear();
+    $newsletter = $this->newslettersRepository->findOneById($newsletter->getId());
+    $this->assertInstanceOf(NewsletterEntity::class, $newsletter);
+    verify($newsletter->getOptionValue(NewsletterOptionFieldEntity::NAME_SCHEDULED_AT))->null();
+    verify($newsletter->getOptionValue(NewsletterOptionFieldEntity::NAME_IS_SCHEDULED))->equals('0');
+  }
+
+  public function testItUpdatesSegmentIds(): void {
+    $wpPostId = 11;
+    $newsletter = (new NewsletterFactory())
+      ->withWpPostId($wpPostId)
+      ->create();
+
+    $segment1 = (new SegmentFactory())->create();
+    $segment2 = (new SegmentFactory())->create();
+
+    $this->emailApiController->saveEmailData([
+      'id' => $newsletter->getId(),
+      'subject' => 'Test Subject',
+      'preheader' => 'Test Preheader',
+      'segment_ids' => [$segment1->getId(), $segment2->getId()],
+    ], new \WP_Post((object)['ID' => $wpPostId]));
+
+    $this->entityManager->clear();
+    $newsletter = $this->newslettersRepository->findOneById($newsletter->getId());
+    $this->assertInstanceOf(NewsletterEntity::class, $newsletter);
+    $segmentIds = $newsletter->getSegmentIds();
+    verify($segmentIds)->arrayCount(2);
+    $this->assertContains($segment1->getId(), $segmentIds);
+    $this->assertContains($segment2->getId(), $segmentIds);
+  }
+
+  public function testItRemovesSegmentsNotInNewList(): void {
+    $wpPostId = 12;
+    $segment1 = (new SegmentFactory())->create();
+    $segment2 = (new SegmentFactory())->create();
+    $segment3 = (new SegmentFactory())->create();
+
+    $newsletter = (new NewsletterFactory())
+      ->withWpPostId($wpPostId)
+      ->withSegments([$segment1, $segment2])
+      ->create();
+
+    // Update to only include segment3, removing segment1 and segment2.
+    $this->emailApiController->saveEmailData([
+      'id' => $newsletter->getId(),
+      'subject' => 'Test Subject',
+      'preheader' => 'Test Preheader',
+      'segment_ids' => [$segment3->getId()],
+    ], new \WP_Post((object)['ID' => $wpPostId]));
+
+    $this->entityManager->clear();
+    $newsletter = $this->newslettersRepository->findOneById($newsletter->getId());
+    $this->assertInstanceOf(NewsletterEntity::class, $newsletter);
+    $segmentIds = $newsletter->getSegmentIds();
+    verify($segmentIds)->arrayCount(1);
+    $this->assertContains($segment3->getId(), $segmentIds);
+    $this->assertNotContains($segment1->getId(), $segmentIds);
+    $this->assertNotContains($segment2->getId(), $segmentIds);
+  }
+
+  public function testItNormalizesSegmentIdsToIntegers(): void {
+    $wpPostId = 13;
+    $newsletter = (new NewsletterFactory())
+      ->withWpPostId($wpPostId)
+      ->create();
+
+    $segment1 = (new SegmentFactory())->create();
+    $segment2 = (new SegmentFactory())->create();
+
+    // Pass segment IDs as strings to test normalization.
+    $this->emailApiController->saveEmailData([
+      'id' => $newsletter->getId(),
+      'subject' => 'Test Subject',
+      'preheader' => 'Test Preheader',
+      'segment_ids' => [(string)$segment1->getId(), (string)$segment2->getId()],
+    ], new \WP_Post((object)['ID' => $wpPostId]));
+
+    $this->entityManager->clear();
+    $newsletter = $this->newslettersRepository->findOneById($newsletter->getId());
+    $this->assertInstanceOf(NewsletterEntity::class, $newsletter);
+    $segmentIds = $newsletter->getSegmentIds();
+    verify($segmentIds)->arrayCount(2);
+    $this->assertContains($segment1->getId(), $segmentIds);
+    $this->assertContains($segment2->getId(), $segmentIds);
+
+    foreach ($segmentIds as $segmentId) {
+      verify($segmentId)->isInt();
+    }
+  }
+
+  public function testItHandlesEmptySegmentIdsArray(): void {
+    $wpPostId = 14;
+    $segment1 = (new SegmentFactory())->create();
+    $segment2 = (new SegmentFactory())->create();
+
+    $newsletter = (new NewsletterFactory())
+      ->withWpPostId($wpPostId)
+      ->withSegments([$segment1, $segment2])
+      ->create();
+
+    // Clear all segments by passing empty array.
+    $this->emailApiController->saveEmailData([
+      'id' => $newsletter->getId(),
+      'subject' => 'Test Subject',
+      'preheader' => 'Test Preheader',
+      'segment_ids' => [],
+    ], new \WP_Post((object)['ID' => $wpPostId]));
+
+    $this->entityManager->clear();
+    $newsletter = $this->newslettersRepository->findOneById($newsletter->getId());
+    $this->assertInstanceOf(NewsletterEntity::class, $newsletter);
+    $segmentIds = $newsletter->getSegmentIds();
+    verify($segmentIds)->arrayCount(0);
   }
 
   public function _after() {
