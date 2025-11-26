d7b1df71e9fa54bab10932439eaf7b5af5398bd4
 Ján Mikláš <neosinner@gmail.com>
2025-11-26 15:57:48 +0100

    Allow removing opt-in block when opt-in on checkout is disabled
    STOMAIL-7476

--- mailpoet/assets/js/src/marketing-optin-block/attributes.ts
+++ mailpoet/assets/js/src/marketing-optin-block/attributes.ts
@@ -3,10 +3,17 @@
  */
 import { getSetting } from '@woocommerce/settings';
 
-const { defaultText } = getSetting('mailpoet_data');
+const { optinEnabled, defaultText } = getSetting('mailpoet_data');
 export const marketingOptinAttributes = {
   text: {
     type: 'string',
     default: defaultText,
+  },
+  lock: {
+    type: 'object',
+    default: {
+      remove: !!optinEnabled,
+      move: false,
+    },
   },
 };
