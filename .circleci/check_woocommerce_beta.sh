7d7140bf5b481d583f48079fb68226264cb5c74e
 Ján Mikláš <neosinner@gmail.com>
2025-12-11 09:22:01 +0100

    Ensure that RC is not returned when the final version is released
    STOMAIL-7660

--- .circleci/check_woocommerce_beta.sh
+++ .circleci/check_woocommerce_beta.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-# Fetch the versions of WooCommerce from the WordPress API
+# Fetch the latest version of WooCommerce from the WordPress API
+# Sorting: -dev < -alpha < -beta < -rc < final (no suffix)
 LATEST_VERSION=$(
   curl -s https://api.wordpress.org/plugins/info/1.0/woocommerce.json | \
   jq -r '.versions | keys_unsorted | .[]' | \
   grep -v 'trunk' | \
-  sort -V | \
-  tail -n 1
+  awk '{
+    orig = $0
+    v = $0
+    if (v ~ /-dev/) { gsub(/-dev/, "~0dev", v) }
+    else if (v ~ /-alpha/) { gsub(/-alpha/, "~1alpha", v) }
+    else if (v ~ /-beta/) { gsub(/-beta/, "~2beta", v) }
+    else if (v ~ /-rc/) { gsub(/-rc/, "~3rc", v) }
+    else { v = v "~4" }
+    print v "\t" orig
+  }' | \
+  sort -V -k1,1 | \
+  tail -n 1 | \
+  cut -f2
 )
 
 # Check if the latest version is a beta/RC version
