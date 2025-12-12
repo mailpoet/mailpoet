b603eff96e371f3d47770ac4f0d51737952a152c
Jan Lysý <jan.lysy@automattic.com>
2025-12-12 16:33:08 +0100

    Update template selection to use 'Newsletter' category

--- mailpoet/tests/acceptance/EmailEditor/EmailTemplatesCest.php
+++ mailpoet/tests/acceptance/EmailEditor/EmailTemplatesCest.php
@@ -17,7 +17,7 @@ class EmailTemplatesCest {
     $i->waitForText('Take a first look at our new email editor. It introduces a more flexible, modern way to design your emails.');
     $i->click('//button[text()="Try it now"]');
 
-    $this->selectTemplate($i, 'Newsletter - 1 Column');
+    $this->selectTemplate($i, 'Newsletter');
 
     $i->wantTo('Verify settings panel is visible');
     $i->waitForText('Settings', 10, '.woocommerce-email-editor__settings-panel');
@@ -71,7 +71,7 @@ class EmailTemplatesCest {
   private function selectTemplate(\AcceptanceTester $i, string $template): void {
     $i->wantTo("Select template $template");
     $i->waitForElementClickable('.email-editor-start_from_scratch_button');
-    $i->click('[aria-label="Basic"]');
+    $i->click('[aria-label="Newsletter"]');
     $i->waitForElement('.block-editor-block-patterns-list__item-title');
     $i->waitForText($template, 5);
     $i->click("//h4[@class='block-editor-block-patterns-list__item-title' and text()='$template']");
