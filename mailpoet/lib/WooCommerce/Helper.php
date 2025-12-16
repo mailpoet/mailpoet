12603de56e66264a8b829537a8d58927d9e7fa43
 Ján Mikláš <neosinner@gmail.com>
2025-12-16 12:22:01 +0100

    Remove WC version comparison when checking if email improvements are enabled
    STOMAIL-7670

--- mailpoet/lib/WooCommerce/Helper.php
+++ mailpoet/lib/WooCommerce/Helper.php
@@ -349,11 +349,6 @@ class Helper {
     if (!class_exists('\Automattic\WooCommerce\Utilities\FeaturesUtil')) {
       return false;
     }
-    // By this point, the feature should be enabled by default for everyone
-    $wcVersion = $this->getWooCommerceVersion();
-    if (version_compare($wcVersion, '10.0.0', '>')) {
-      return true;
-    }
     return \Automattic\WooCommerce\Utilities\FeaturesUtil::feature_is_enabled('email_improvements');
   }
 
