37c2958d7a26c1641909de116aa2fba7183daa90
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2025-12-18 15:56:55 +0200

    Add filter to modify email editor patterns before registration

--- mailpoet/lib/EmailEditor/Integrations/MailPoet/Patterns/PatternsController.php
+++ mailpoet/lib/EmailEditor/Integrations/MailPoet/Patterns/PatternsController.php
@@ -6,14 +6,18 @@ use MailPoet\EmailEditor\Integrations\MailPoet\Patterns\Library\AbandonedCartPat
 use MailPoet\EmailEditor\Integrations\MailPoet\Patterns\Library\NewsletterPattern;
 use MailPoet\EmailEditor\Integrations\MailPoet\Patterns\Library\WelcomeEmailPattern;
 use MailPoet\Util\CdnAssetUrl;
+use MailPoet\WP\Functions as WPFunctions;
 
 class PatternsController {
   private CdnAssetUrl $cdnAssetUrl;
+  private WPFunctions $wp;
 
   public function __construct(
-    CdnAssetUrl $cdnAssetUrl
+    CdnAssetUrl $cdnAssetUrl,
+    WPFunctions $wp
   ) {
     $this->cdnAssetUrl = $cdnAssetUrl;
+    $this->wp = $wp;
   }
 
   public function registerPatterns(): void {
@@ -23,8 +27,26 @@ class PatternsController {
     $patterns[] = new NewsletterPattern($this->cdnAssetUrl);
     $patterns[] = new WelcomeEmailPattern($this->cdnAssetUrl);
     $patterns[] = new AbandonedCartPattern($this->cdnAssetUrl);
+
     foreach ($patterns as $pattern) {
-      register_block_pattern($pattern->get_namespace() . '/' . $pattern->get_name(), $pattern->get_properties());
+      $patternName = $pattern->get_namespace() . '/' . $pattern->get_name();
+      $patternProperties = $pattern->get_properties();
+
+      /**
+       * Filters pattern data before it is registered as a block pattern.
+       *
+       * @param array{name: string, properties: array} $patternData Pattern name and properties.
+       * @param Pattern $pattern The original Pattern object.
+       * @return array|null Return modified data or null/false to skip registration.
+       */
+      $patternData = $this->wp->applyFilters('mailpoet_email_editor_register_pattern', [
+        'name' => $patternName,
+        'properties' => $patternProperties,
+      ], $pattern);
+
+      if (is_array($patternData) && isset($patternData['name']) && isset($patternData['properties'])) {
+        register_block_pattern($patternData['name'], $patternData['properties']);
+      }
     }
   }
 
