570087eb7aa8b16e8d251147a3b909aa39d245bd
Pavel Dohnal <pavel.dohnal@automattic.com>
2026-02-10 08:34:27 +0100

    Add removeFilter to Engine\WordPress wrapper

--- mailpoet/lib/Automation/Engine/WordPress.php
+++ mailpoet/lib/Automation/Engine/WordPress.php
@@ -24,6 +24,10 @@ class WordPress {
     do_action($hookName, ...$arg);
   }
 
+  public function removeFilter(string $hookName, callable $callback, int $priority = 10): bool {
+    return remove_filter($hookName, $callback, $priority);
+  }
+
   public function addFilter(string $hookName, callable $callback, int $priority = 10, int $acceptedArgs = 1): bool {
     return add_filter($hookName, $callback, $priority, $acceptedArgs);
   }
