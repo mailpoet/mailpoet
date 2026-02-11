e6c25c9b7d0e50a78c41412ae1bfe03276810b7a
Rostislav Wolny <rosta.wolny@automattic.com>
2026-02-11 18:19:18 +0100

    Render ScheduledRow and RecipientsRow to the email editor via the new slot

--- mailpoet/assets/js/src/mailpoet-email-editor-integration/sidebar/email-settings-panel.tsx
+++ mailpoet/assets/js/src/mailpoet-email-editor-integration/sidebar/email-settings-panel.tsx
@@ -1,20 +1,12 @@
-import {
-  // @ts-expect-error Type for PluginDocumentSettingPanel is missing in @types/wordpress__editor
-  PluginDocumentSettingPanel,
-} from '@wordpress/editor';
-import { __ } from '@wordpress/i18n';
+import { EmailActionsFill } from '@woocommerce/email-editor';
 import { ScheduledRow } from './components/scheduled-row';
 import { RecipientsRow } from './components/recipients-row';
 
 export function EmailSettingsPanel() {
   return (
-    <PluginDocumentSettingPanel
-      name="mailpoet-email-settings"
-      title={__('Email settings', 'mailpoet')}
-      className="mailpoet-email-settings-panel"
-    >
+    <EmailActionsFill>
       <ScheduledRow />
       <RecipientsRow />
-    </PluginDocumentSettingPanel>
+    </EmailActionsFill>
   );
 }
