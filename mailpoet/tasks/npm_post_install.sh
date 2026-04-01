2c06ab1cdc121ab76f2b9185f261b916d3819473
Pavel Dohnal <pavel.dohnal@automattic.com>
2026-04-01 10:31:26 +0200

    Fix pnpm 10 compatibility issues found during verification
    - Replace node-linker=hoisted with shamefully-hoist=true in
      .npmrc to properly symlink deps in workspace members
    - Add onlyBuiltDependencies for packages that need lifecycle
      scripts (@parcel/watcher, core-js, core-js-pure)
    - Fix postinstall script to use require.resolve() for finding
      tinymce and react-dates across pnpm's virtual store
    - Regenerate lockfile in pnpm 10 format

--- mailpoet/tasks/npm_post_install.sh
+++ mailpoet/tasks/npm_post_install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Resolve node_modules from workspace root (pnpm hoists packages there)
+NODE_MODULES=$(cd "$INIT_CWD" && node -e "console.log(require.resolve('tinymce/package.json').replace('/package.json',''))" 2>/dev/null)
+REACT_DATES=$(cd "$INIT_CWD" && node -e "console.log(require.resolve('react-dates/package.json').replace('/package.json',''))" 2>/dev/null)
+
 echo "/* eslint-disable */
 " > $INIT_CWD/assets/js/src/newsletter-editor/behaviors/tinymce-icons.js
 
 echo "import tinymce from 'tinymce/tinymce';
 " >> $INIT_CWD/assets/js/src/newsletter-editor/behaviors/tinymce-icons.js
 
-cat $INIT_CWD/node_modules/tinymce/icons/default/icons.min.js >> $INIT_CWD/assets/js/src/newsletter-editor/behaviors/tinymce-icons.js
+cat "$NODE_MODULES/icons/default/icons.min.js" >> $INIT_CWD/assets/js/src/newsletter-editor/behaviors/tinymce-icons.js
 
 # fix SCSS file being exposed as CSS file so it can be imported correctly
-cp $INIT_CWD/node_modules/react-dates/lib/css/_datepicker.css $INIT_CWD/node_modules/react-dates/lib/css/_datepicker.scss
+cp "$REACT_DATES/lib/css/_datepicker.css" "$REACT_DATES/lib/css/_datepicker.scss"
