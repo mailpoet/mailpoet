6729163733f26c5c36d04aba01cf7ad450d79a2b
Chris Moreira <chrism245@Chriss-MacBook-Pro.local>
2026-04-09 10:51:53 -0600

    feat: register AdminNotificationRecipientsSync in DI container (STOMAIL-7770)

--- mailpoet/lib/DI/ContainerConfigurator.php
+++ mailpoet/lib/DI/ContainerConfigurator.php
@@ -646,6 +646,7 @@ class ContainerConfigurator implements IContainerConfigurator {
     $container->autowire(\MailPoet\WooCommerce\Subscription::class)->setPublic(true);
     $container->autowire(\MailPoet\WooCommerce\TransactionalEmailHooks::class)->setPublic(true);
     $container->autowire(\MailPoet\WooCommerce\TransactionalEmails::class)->setPublic(true);
+    $container->autowire(\MailPoet\WooCommerce\AdminNotificationRecipientsSync::class)->setPublic(true);
     $container->autowire(\MailPoet\WooCommerce\Tracker::class)->setPublic(true);
     $container->autowire(\MailPoet\WooCommerce\TransactionalEmails\Template::class)->setPublic(true);
     $container->autowire(\MailPoet\WooCommerce\TransactionalEmails\Renderer::class)->setPublic(true);
