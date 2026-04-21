1ab0228619f4f143af3d8abf60e53a0a854d9eb0
 Ján Mikláš <neosinner@gmail.com>
2026-04-21 12:53:27 +0200

    Add integer casting back

--- mailpoet/lib/API/JSON/ResponseBuilders/SegmentsResponseBuilder.php
+++ mailpoet/lib/API/JSON/ResponseBuilders/SegmentsResponseBuilder.php
@@ -34,7 +34,7 @@ class SegmentsResponseBuilder {
       'deleted_at' => ($deletedAt = $segment->getDeletedAt()) ? $deletedAt->format(self::DATE_FORMAT) : null,
       'average_engagement_score' => $segment->getAverageEngagementScore(),
       'filters_connect' => $segment->getFiltersConnectOperator(),
-      'show_in_manage_subscription_page' => $segment->getDisplayInManageSubscriptionPage(),
+      'show_in_manage_subscription_page' => (int)$segment->getDisplayInManageSubscriptionPage(),
     ];
   }
 
