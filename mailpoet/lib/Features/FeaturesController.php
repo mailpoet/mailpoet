1327fc1386873414d6e83ae06003cf0d1f2c60e6
Pavel Dohnal <pavel.dohnal@automattic.com>
2026-04-23 08:37:24 +0200

    Remove birthday_emails feature flag entirely
    The feature is complete and no longer needs gating.
    Remove the constant and default from both PHP and JS.
    
    STOMAIL-7960

--- mailpoet/lib/Features/FeaturesController.php
+++ mailpoet/lib/Features/FeaturesController.php
@@ -6,13 +6,11 @@ use MailPoetVendor\Doctrine\DBAL\Exception\TableNotFoundException;
 
 class FeaturesController {
   const FEATURE_BRAND_TEMPLATES = 'brand_templates';
-  const FEATURE_BIRTHDAY_EMAILS = 'birthday_emails';
 
   // Define feature defaults in the array below in the following form:
   //   self::FEATURE_NAME_OF_FEATURE => true,
   private $defaults = [
     self::FEATURE_BRAND_TEMPLATES => false,
-    self::FEATURE_BIRTHDAY_EMAILS => true,
   ];
 
   /** @var array|null */
