d5034897decc554a97bc3b442344d3c0af612da3
 Ján Mikláš <neosinner@gmail.com>
2026-04-23 16:34:24 +0200

    Improve REST API route registration, instead or relying on WP's compatibility layer in `do_action()`

--- mailpoet/lib/Tags/RestApi/Api.php
+++ mailpoet/lib/Tags/RestApi/Api.php
@@ -11,22 +11,27 @@ use MailPoet\Tags\RestApi\Endpoints\TagsPostEndpoint;
 use MailPoet\WP\Functions as WPFunctions;
 
 class Api {
+  /** @var RestApi */
+  private $api;
+
   /** @var WPFunctions */
   private $wp;
 
   public function __construct(
+    RestApi $api,
     WPFunctions $wp
   ) {
+    $this->api = $api;
     $this->wp = $wp;
   }
 
   public function initialize(): void {
-    $this->wp->addAction(RestApi::REST_API_INIT_ACTION, function (RestApi $api): void {
-      $api->registerGetRoute('tags', TagsGetEndpoint::class);
-      $api->registerPostRoute('tags', TagsPostEndpoint::class);
-      $api->registerPutRoute('tags/(?P<id>\d+)', TagPutEndpoint::class);
-      $api->registerDeleteRoute('tags/(?P<id>\d+)', TagDeleteEndpoint::class);
-      $api->registerPostRoute('tags/bulk-delete', TagsBulkDeleteEndpoint::class);
+    $this->wp->addAction(RestApi::REST_API_INIT_ACTION, function (): void {
+      $this->api->registerGetRoute('tags', TagsGetEndpoint::class);
+      $this->api->registerPostRoute('tags', TagsPostEndpoint::class);
+      $this->api->registerPutRoute('tags/(?P<id>\d+)', TagPutEndpoint::class);
+      $this->api->registerDeleteRoute('tags/(?P<id>\d+)', TagDeleteEndpoint::class);
+      $this->api->registerPostRoute('tags/bulk-delete', TagsBulkDeleteEndpoint::class);
     });
   }
 }
