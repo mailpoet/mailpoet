48d50186c6caad52bd9d4f733d4af87b0f52e6b7
 Ján Mikláš <neosinner@gmail.com>
2026-04-26 21:57:58 +0200

    Update types for new API response

--- mailpoet/assets/js/src/subscribers/types.tsx
+++ mailpoet/assets/js/src/subscribers/types.tsx
@@ -2,18 +2,20 @@ export type StatsType = {
   email: string;
   engagement_score: number;
   last_engagement?: string;
+  last_engagement_at?: string;
   last_click?: string;
   last_open?: string;
   last_sending?: string;
   last_page_view?: string;
   last_purchase?: string;
-  periodic_stats?: PeriodicStats[];
+  periodic_stats: PeriodicStats[];
   is_woo_active: boolean;
   is_woocommerce_user: boolean;
   avatar_url: string | null;
   subscribed_at: string | null;
   source_label: string | null;
   woocommerce_overview?: WoocommerceOverview;
+  profile: SubscriberProfile;
 };
 
 export type WoocommerceOverview = {
@@ -24,7 +26,9 @@ export type WoocommerceOverview = {
 };
 
 export type PeriodicStats = {
+  key: StatsPeriodKey;
   timeframe: string;
+  label: string;
   total_sent: number;
   open: number;
   machine_open: number;
@@ -36,4 +40,39 @@ export type PeriodicStats = {
     formatted: string;
     formatted_average: string;
   };
+};
+
+export type StatsPeriodKey =
+  | '7_days'
+  | '30_days'
+  | '3_months'
+  | '12_months'
+  | 'lifetime';
+
+export type SubscriberProfile = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  shipping_address: string[];
+  tags: SubscriberTag[];
+  segments: SubscriberSegment[];
+  custom_fields: SubscriberCustomField[];
+};
+
+export type SubscriberTag = {
+  id: string;
+  subscriber_id: string;
+  tag_id: string;
+  name: string;
+};
+
+export type SubscriberSegment = {
+  id: string;
+  name: string;
+};
+
+export type SubscriberCustomField = {
+  id: string;
+  name: string;
+  value: string;
 };
