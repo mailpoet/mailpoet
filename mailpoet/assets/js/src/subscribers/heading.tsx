1f340ba2cb63bba11ba45739329d1dbd9fc2c305
Pavel Dohnal <pavel.dohnal@automattic.com>
2026-05-04 08:26:40 +0200

    Group subscriber header actions
    Keep subscriber data management actions visible while separating them from import and export actions. The header now wraps on narrow screens instead of hiding secondary actions.\n\nSTOMAIL-8002

--- mailpoet/assets/js/src/subscribers/heading.tsx
+++ mailpoet/assets/js/src/subscribers/heading.tsx
@@ -14,50 +14,59 @@ export function SubscribersHeading() {
     <>
       <CompensateScreenOptions />
       <TopBarWithBoundary />
-      <PageHeader heading={__('Subscribers', 'mailpoet')}>
-        <Link
-          className="page-title-action"
-          to={
-            {
-              pathname: '/new',
-              state: {
-                backUrl: location?.pathname,
-              },
-            } as To
-          }
-        >
-          <span data-automation-id="add-new-subscribers-button">
-            {__('Add new subscriber', 'mailpoet')}
+      <PageHeader
+        className="mailpoet-subscribers-page-header"
+        heading={__('Subscribers', 'mailpoet')}
+      >
+        <span className="mailpoet-subscribers-heading-actions">
+          <span className="mailpoet-subscribers-heading-primary-actions">
+            <Link
+              className="page-title-action"
+              to={
+                {
+                  pathname: '/new',
+                  state: {
+                    backUrl: location?.pathname,
+                  },
+                } as To
+              }
+            >
+              <span data-automation-id="add-new-subscribers-button">
+                {__('Add new subscriber', 'mailpoet')}
+              </span>
+            </Link>
+            <a
+              className="page-title-action"
+              href="?page=mailpoet-import"
+              data-automation-id="import-subscribers-button"
+            >
+              {__('Import', 'mailpoet')}
+            </a>
+            <a
+              id="mailpoet_export_button"
+              className="page-title-action"
+              href="?page=mailpoet-export"
+            >
+              {__('Export', 'mailpoet')}
+            </a>
           </span>
-        </Link>
-        <a
-          className="page-title-action not-small-screen"
-          href="?page=mailpoet-import"
-          data-automation-id="import-subscribers-button"
-        >
-          {__('Import', 'mailpoet')}
-        </a>
-        <a
-          id="mailpoet_export_button"
-          className="page-title-action not-small-screen"
-          href="?page=mailpoet-export"
-        >
-          {__('Export', 'mailpoet')}
-        </a>
-        <a
-          className="page-title-action not-small-screen"
-          href="?page=mailpoet-tags"
-          data-automation-id="manage-tags-button"
-        >
-          {__('Tags', 'mailpoet')}
-        </a>
-        <a
-          className="page-title-action not-small-screen"
-          href="?page=mailpoet-custom-fields"
-          data-automation-id="manage-custom-fields-button"
-        >
-          {__('Custom fields', 'mailpoet')}
-        </a>
+          <span className="mailpoet-subscribers-heading-management-actions">
+            <a
+              className="page-title-action"
+              href="?page=mailpoet-tags"
+              data-automation-id="manage-tags-button"
+            >
+              {__('Tags', 'mailpoet')}
+            </a>
+            <a
+              className="page-title-action"
+              href="?page=mailpoet-custom-fields"
+              data-automation-id="manage-custom-fields-button"
+            >
+              {__('Custom fields', 'mailpoet')}
+            </a>
+          </span>
+        </span>
       </PageHeader>
       <div className="mailpoet-segment-subscriber-count">
         <SubscribersInPlan
