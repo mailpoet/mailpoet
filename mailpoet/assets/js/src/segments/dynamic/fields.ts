817d23861b31eb2c412536ddee6079ef2bf781bc
 Ján Mikláš <neosinner@gmail.com>
2026-05-07 09:57:04 +0200

    Format DataViews dates consistently

--- mailpoet/assets/js/src/segments/dynamic/fields.ts
+++ mailpoet/assets/js/src/segments/dynamic/fields.ts
@@ -17,9 +17,7 @@ function dateTime(
         ? `mailpoet_dynamic_segment_updated_at_${segmentId}`
         : 'mailpoet_dynamic_segment_updated_at',
     },
-    createElement('span', null, MailPoet.Date.short(value)),
-    createElement('br', null),
-    createElement('span', null, MailPoet.Date.time(value)),
+    MailPoet.Date.full(value),
   );
 }
 
