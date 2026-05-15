575fad595be602f4e00598d0496bf825d1168710
 Ján Mikláš <neosinner@gmail.com>
2026-05-15 14:20:11 +0200

    Cover public newsletter sharing
    Add integration coverage for visibility, URL generation, metadata, rendering, route fallback behavior, and controller resolution.
    
    STOMAIL-7989

---
+++ mailpoet/tests/integration/Newsletter/Sharing/ShareVisibilityTest.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\Test\Newsletter\Sharing;
+
+use MailPoet\Entities\NewsletterOptionFieldEntity;
+use MailPoet\Newsletter\Sharing\ShareVisibility;
+use MailPoet\Settings\SettingsController;
+use MailPoet\Test\DataFactories\Newsletter;
+
+class ShareVisibilityTest extends \MailPoetTest {
+  public function testItBlocksSentStandardNewslettersByDefault() {
+    $newsletter = (new Newsletter())
+      ->withSentStatus()
+      ->create();
+
+    $shareVisibility = $this->diContainer->get(ShareVisibility::class);
+
+    verify($shareVisibility->getConfiguredVisibility($newsletter))->equals(ShareVisibility::VISIBILITY_DEFAULT);
+    verify($shareVisibility->getDefaultVisibility())->equals(ShareVisibility::VISIBILITY_PRIVATE);
+    verify($shareVisibility->canShare($newsletter))->false();
+  }
+
+  public function testItAllowsNewslettersExplicitlyMarkedPublic() {
+    $newsletter = (new Newsletter())
+      ->withSentStatus()
+      ->withOptions([
+        NewsletterOptionFieldEntity::NAME_SHARE_VISIBILITY => ShareVisibility::VISIBILITY_PUBLIC,
+      ])
+      ->create();
+
+    $shareVisibility = $this->diContainer->get(ShareVisibility::class);
+
+    verify($shareVisibility->canShare($newsletter))->true();
+  }
+
+  public function testItBlocksPrivateNewsletters() {
+    $newsletter = (new Newsletter())
+      ->withSentStatus()
+      ->withOptions([
+        NewsletterOptionFieldEntity::NAME_SHARE_VISIBILITY => ShareVisibility::VISIBILITY_PRIVATE,
+      ])
+      ->create();
+
+    $shareVisibility = $this->diContainer->get(ShareVisibility::class);
+
+    verify($shareVisibility->canShare($newsletter))->false();
+  }
+
+  public function testItUsesGlobalDefaultForDefaultVisibility() {
+    $this->diContainer->get(SettingsController::class)->set(
+      ShareVisibility::SETTING_DEFAULT_VISIBILITY,
+      ShareVisibility::VISIBILITY_PUBLIC
+    );
+    $newsletter = (new Newsletter())
+      ->withSentStatus()
+      ->withOptions([
+        NewsletterOptionFieldEntity::NAME_SHARE_VISIBILITY => ShareVisibility::VISIBILITY_DEFAULT,
+      ])
+      ->create();
+
+    $shareVisibility = $this->diContainer->get(ShareVisibility::class);
+
+    verify($shareVisibility->canShare($newsletter))->true();
+  }
+
+  public function testItBlocksUnsupportedNewsletterTypesAndStatuses() {
+    $draft = (new Newsletter())
+      ->withDraftStatus()
+      ->create();
+    $notificationHistory = (new Newsletter())
+      ->withPostNotificationHistoryType()
+      ->withSentStatus()
+      ->create();
+
+    $shareVisibility = $this->diContainer->get(ShareVisibility::class);
+
+    verify($shareVisibility->canShare($draft))->false();
+    verify($shareVisibility->canShare($notificationHistory))->false();
+  }
+}
--- mailpoet/tests/integration/Newsletter/UrlTest.php
+++ mailpoet/tests/integration/Newsletter/UrlTest.php
@@ -5,7 +5,9 @@ namespace MailPoet\Test\Newsletter;
 use MailPoet\Entities\NewsletterEntity;
 use MailPoet\Entities\SubscriberEntity;
 use MailPoet\Newsletter\Url as NewsletterUrl;
+use MailPoet\Test\DataFactories\Newsletter;
 use MailPoet\Util\Security;
+use MailPoet\WP\Functions as WPFunctions;
 
 class UrlTest extends \MailPoetTest {
   public function testPreviewUrlIsTheSameForNullOrEmptySubscriber() {
@@ -22,5 +24,82 @@ class UrlTest extends \MailPoetTest {
     $urlEmptySubscriber = $newsletterUrl->getViewInBrowserUrl($newsletter, $emptySubscriber);
 
     verify($urlNullSubscriber)->equals($urlEmptySubscriber);
+  }
+
+  public function testItBuildsPublicShareUrlWithNewsletterHashAndSlug() {
+    $this->diContainer->get(WPFunctions::class)->updateOption('permalink_structure', '/%postname%/');
+    $newsletter = (new Newsletter())
+      ->withSubject('Spring Sale & Updates')
+      ->withSentStatus()
+      ->create();
+    $newsletterUrl = $this->diContainer->get(NewsletterUrl::class);
+
+    $url = $newsletterUrl->getPublicShareUrl($newsletter);
+    $identifier = sprintf('%s-spring-sale-updates', $newsletter->getHash());
+
+    verify(parse_url($url, PHP_URL_PATH))->equals(sprintf('/mailpoet-email/%s/', $identifier));
+  }
+
+  public function testItBuildsPublicShareUrlFallbackForPlainPermalinks() {
+    $this->diContainer->get(WPFunctions::class)->updateOption('permalink_structure', '');
+    $newsletter = (new Newsletter())
+      ->withSubject('Plain Permalinks')
+      ->withSentStatus()
+      ->create();
+    $newsletterUrl = $this->diContainer->get(NewsletterUrl::class);
+
+    $url = $newsletterUrl->getPublicShareUrl($newsletter);
+    $identifier = sprintf('%s-plain-permalinks', $newsletter->getHash());
+    parse_str((string)parse_url($url, PHP_URL_QUERY), $queryParams);
+
+    verify($queryParams)->equals(['mailpoet_public_email' => $identifier]);
+  }
+
+  public function testItRespectsThePublicSharePrefixFilter() {
+    $this->diContainer->get(WPFunctions::class)->updateOption('permalink_structure', '/%postname%/');
+    $newsletter = (new Newsletter())
+      ->withSubject('Filtered')
+      ->withSentStatus()
+      ->create();
+    $newsletterUrl = $this->diContainer->get(NewsletterUrl::class);
+
+    $filter = function () {
+      return '/share/email/';
+    };
+    $wp = $this->diContainer->get(WPFunctions::class);
+    $wp->addFilter('mailpoet_public_share_url_prefix', $filter);
+
+    try {
+      verify($newsletterUrl->getPublicSharePathPrefix())->equals('/share/email/');
+      verify($newsletterUrl->getPublicShareUrl($newsletter))
+        ->stringContainsString(sprintf('/share/email/%s-filtered/', $newsletter->getHash()));
+    } finally {
+      $wp->removeFilter('mailpoet_public_share_url_prefix', $filter);
+    }
+  }
+
+  public function testItIgnoresInvalidPublicSharePrefixFilter() {
+    $newsletterUrl = $this->diContainer->get(NewsletterUrl::class);
+
+    $filter = function () {
+      return '/bad prefix?/';
+    };
+    $wp = $this->diContainer->get(WPFunctions::class);
+    $wp->addFilter('mailpoet_public_share_url_prefix', $filter);
+
+    try {
+      verify($newsletterUrl->getPublicSharePathPrefix())->equals('/mailpoet-email/');
+    } finally {
+      $wp->removeFilter('mailpoet_public_share_url_prefix', $filter);
+    }
+  }
+
+  public function testItReturnsEmptyPublicShareUrlWhenNewsletterHasNoHash() {
+    $newsletter = (new Newsletter())->create();
+    $newsletter->setHash(null);
+    $newsletterUrl = $this->diContainer->get(NewsletterUrl::class);
+
+    verify($newsletterUrl->getPublicShareUrl($newsletter))->equals('');
+    verify($newsletterUrl->getPublicShareUrl(null))->equals('');
   }
 }
