98dfed36533e3e377d2aff7a9d691cc0b8823cc4
 Ján Mikláš <neosinner@gmail.com>
2026-06-04 16:19:15 +0200

    Fix automation email menu selection
    STOMAIL-8134

--- mailpoet/tests/integration/Config/MenuTest.php
+++ mailpoet/tests/integration/Config/MenuTest.php
@@ -5,6 +5,9 @@ namespace MailPoet\Test\Config;
 use Codeception\Util\Stub;
 use MailPoet\Config\Menu;
 use MailPoet\Config\ServicesChecker;
+use MailPoet\EmailEditor\Integrations\MailPoet\EmailEditor;
+use MailPoet\Entities\NewsletterEntity;
+use MailPoet\Test\DataFactories\Newsletter;
 
 class MenuTest extends \MailPoetTest {
   public function testItReturnsTrueIfCurrentPageBelongsToMailpoet() {
@@ -55,5 +58,67 @@ class MenuTest extends \MailPoetTest {
     );
     $menu->checkPremiumKey($checker);
     verify($menu->premiumKeyValid)->false();
+  }
+
+  public function testItHighlightsAutomationsWhenEditingAutomationEmail() {
+    $this->assertEmailEditorMenuPage(NewsletterEntity::TYPE_AUTOMATION, Menu::AUTOMATIONS_PAGE_SLUG);
+  }
+
+  public function testItHighlightsEmailsWhenEditingStandardEmail() {
+    $this->assertEmailEditorMenuPage(NewsletterEntity::TYPE_STANDARD, Menu::EMAILS_PAGE_SLUG);
+  }
+
+  private function assertEmailEditorMenuPage(string $newsletterType, string $expectedMenuPageSlug): void {
+    global $plugin_page, $submenu_file;
+
+    $postId = wp_insert_post([
+      'post_title' => 'MailPoet email',
+      'post_type' => EmailEditor::MAILPOET_EMAIL_POST_TYPE,
+      'post_status' => 'private',
+    ]);
+    verify($postId)->greaterThan(0);
+
+    (new Newsletter())
+      ->withType($newsletterType)
+      ->withWpPostId($postId)
+      ->create();
+
+    $previousPost = $_GET['post'] ?? null;
+    $previousAction = $_GET['action'] ?? null;
+    $previousPluginPage = $plugin_page ?? null;
+    $previousSubmenuFile = $submenu_file ?? null;
+    $_GET['post'] = (string)$postId;
+    $_GET['action'] = 'edit';
+
+    $menu = $this->diContainer->get(Menu::class);
+    $emailEditorProperty = new \ReflectionProperty(Menu::class, 'emailEditor');
+    $emailEditorProperty->setAccessible(true);
+    $previousEmailEditor = $emailEditorProperty->getValue($menu);
+
+    try {
+      $emailEditor = Stub::makeEmpty(EmailEditor::class, ['isEditorPage' => true], $this);
+      $emailEditorProperty->setValue($menu, $emailEditor);
+      $parentFile = $menu->highlightNestedMailPoetSubmenus(
+        'edit.php?post_type=' . EmailEditor::MAILPOET_EMAIL_POST_TYPE
+      );
+
+      verify($parentFile)->equals($expectedMenuPageSlug);
+      verify($plugin_page)->equals($expectedMenuPageSlug);
+      verify($submenu_file)->equals($expectedMenuPageSlug);
+    } finally {
+      if ($previousPost === null) {
+        unset($_GET['post']);
+      } else {
+        $_GET['post'] = $previousPost;
+      }
+      if ($previousAction === null) {
+        unset($_GET['action']);
+      } else {
+        $_GET['action'] = $previousAction;
+      }
+      $plugin_page = $previousPluginPage;
+      $submenu_file = $previousSubmenuFile;
+      $emailEditorProperty->setValue($menu, $previousEmailEditor);
+    }
   }
 }
