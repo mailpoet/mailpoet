9f622140facb160d91737c8b7c3c939d26e07ec1
 Ján Mikláš <neosinner@gmail.com>
2026-06-15 15:02:08 +0200

    Fix Woo system status QIT failures
    Catch cron URL generation errors when MailPoet adds WooCommerce system status data so QIT does not receive a 500 response.
    
    STOMAIL-8185

--- mailpoet/lib/WooCommerce/WooSystemInfo.php
+++ mailpoet/lib/WooCommerce/WooSystemInfo.php
@@ -38,7 +38,11 @@ class WooSystemInfo {
   }
 
   public function cronPingUrl(): string {
-    return $this->cronHelper->getCronUrl(CronDaemon::ACTION_PING);
+    try {
+      return $this->cronHelper->getCronUrl(CronDaemon::ACTION_PING);
+    } catch (\Exception $e) {
+      return __('Can‘t generate cron URL.', 'mailpoet') . ' (' . $e->getMessage() . ')';
+    }
   }
 
   public function toArray(): array {
