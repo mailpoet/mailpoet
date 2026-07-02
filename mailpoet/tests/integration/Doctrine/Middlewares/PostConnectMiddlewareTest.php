3329d16ed0ce94befba9cf0a3a65fa4587897245
 Ján Mikláš <neosinner@gmail.com>
2026-07-02 12:52:55 +0200

    Add timezone DB connection test
    STOMAIL-8228

--- mailpoet/tests/integration/Doctrine/Middlewares/PostConnectMiddlewareTest.php
+++ mailpoet/tests/integration/Doctrine/Middlewares/PostConnectMiddlewareTest.php
@@ -15,4 +15,18 @@ class PostConnectMiddlewareTest extends \MailPoetTest {
     $bigSelects = $this->entityManager->getConnection()->fetchOne('SELECT @@SESSION.sql_big_selects');
     verify($bigSelects)->equals('1');
   }
+
+  public function testItUsesUtcDatabaseTimezoneWithInvalidWordPressOffset() {
+    $originalOffset = get_option('gmt_offset');
+    update_option('gmt_offset', -15);
+
+    try {
+      $connection = (new ConnectionFactory())->createConnection();
+      $databaseTimezone = $connection->fetchOne('SELECT @@SESSION.time_zone');
+    } finally {
+      update_option('gmt_offset', $originalOffset);
+    }
+
+    verify($databaseTimezone)->equals('+00:00');
+  }
 }
