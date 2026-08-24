f3b8476ed25155efda6f21b638a790a0e8b8c9a6
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2026-08-24 19:34:51 +0300

    Stop hiding third-party blocks in the post editor

---
+++ mailpoet/changelog/2026-08-24-14-57-41-fixed-blocks-from-other-plugins-missing-in-the-wordpress.md
@@ -0,0 +1,5 @@
+# Type: Fixed
+
+# Description
+
+Blocks from other plugins missing in the WordPress post editor when MailPoet is active
--- mailpoet/lib/EmailEditor/Integrations/MailPoet/Blocks/BlockTypes/LatestPosts.php
+++ mailpoet/lib/EmailEditor/Integrations/MailPoet/Blocks/BlockTypes/LatestPosts.php
@@ -10,7 +10,6 @@ use Automattic\WooCommerce\EmailEditor\Integrations\Core\Renderer\Blocks\Column 
 use Automattic\WooCommerce\EmailEditor\Integrations\Core\Renderer\Blocks\Columns as ColumnsRenderer;
 use Automattic\WooCommerce\EmailEditor\Integrations\Core\Renderer\Blocks\Image as ImageRenderer;
 use MailPoet\Config\Env;
-use MailPoet\EmailEditor\Integrations\MailPoet\EmailEditor;
 use MailPoet\Entities\NewsletterEntity;
 use MailPoet\Entities\NewsletterPostEntity;
 use MailPoet\Newsletter\AutomatedLatestContent;
@@ -132,7 +131,6 @@ class LatestPosts extends AbstractBlock {
     $this->registerTemplateBlock();
     $this->registerPostContentBlock();
     $this->wp->addFilter('block_categories_all', [$this, 'addBlockCategory']);
-    $this->wp->addFilter('allowed_block_types_all', [$this, 'restrictBlocksToEmailEditor'], 10, 2);
     $this->wp->addFilter('block_type_metadata_settings', [$this, 'enableEmailSupportForCoreBlocks']);
     $this->wp->addFilter('woocommerce_email_content_renderer_styles', [$this, 'makeLinkColorStylesInlineable']);
     $this->enableEmailSupportForRegisteredCoreBlocks();
@@ -164,39 +162,6 @@ class LatestPosts extends AbstractBlock {
       'title' => __('MailPoet', 'mailpoet'),
     ];
     return $categories;
-  }
-
-  /**
-   * The block is registered globally so it can render when emails are sent, but
-   * it is only meant to be used inside the MailPoet email editor. Hide it (and
-   * its template block) from the inserter of every other editor.
-   *
-   * @param bool|string[] $allowedBlocks
-   * @param \WP_Block_Editor_Context $editorContext
-   * @return bool|string[]
-   */
-  public function restrictBlocksToEmailEditor($allowedBlocks, $editorContext) {
-    $post = $editorContext->post ?? null;
-    $postType = $post instanceof \WP_Post ? $post->post_type : null; // phpcs:ignore Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps
-    if ($postType === EmailEditor::MAILPOET_EMAIL_POST_TYPE) {
-      return $allowedBlocks;
-    }
-
-    // Another integration may have disallowed every block
-    if ($allowedBlocks === false) {
-      return $allowedBlocks;
-    }
-
-    // When every block is allowed the value is `true` rather than a list. Build
-    // the full list of block names first so we have something to remove ours from.
-    if (!is_array($allowedBlocks)) {
-      $allowedBlocks = array_keys(\WP_Block_Type_Registry::get_instance()->get_all_registered());
-    }
-
-    $ownBlocks = [$this->getBlockType(), self::TEMPLATE_BLOCK, self::POST_CONTENT_BLOCK];
-    return array_values(array_filter($allowedBlocks, static function ($blockName) use ($ownBlocks) {
-      return !in_array($blockName, $ownBlocks, true);
-    }));
   }
 
   /**
@@ -484,8 +449,6 @@ class LatestPosts extends AbstractBlock {
       'render_callback' => static function (): string {
         return '';
       },
-      'editor_script' => $this->getEditorScript('handle'),
-      'editor_style' => $this->getEditorStyle('handle'),
     ]);
   }
 
@@ -499,8 +462,6 @@ class LatestPosts extends AbstractBlock {
     }
     $blockType = register_block_type_from_metadata($metadataPath, [
       'render_callback' => [$this, 'renderPostContent'],
-      'editor_script' => $this->getEditorScript('handle'),
-      'editor_style' => $this->getEditorStyle('handle'),
     ]);
     if ($blockType instanceof \WP_Block_Type) {
       // The render callback already produces email-ready markup; without an
