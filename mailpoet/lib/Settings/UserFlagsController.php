0cd62b167fa2a53b99fd421a653d1379176f0def
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2026-09-15 12:44:25 +0300

    Add "Remember my choice" to the editor choice modal

--- mailpoet/lib/Settings/UserFlagsController.php
+++ mailpoet/lib/Settings/UserFlagsController.php
@@ -28,6 +28,7 @@ class UserFlagsController {
       'legacy_automations_notice_dismissed' => false,
       'legacy_automatic_emails_notice_dismissed' => false,
       'last_email_editor_choice' => null,
+      'editor_choice_modal' => null,
       self::EMAIL_EDITOR_SURVEY => null,
     ];
     $this->userFlagsRepository = $userFlagsRepository;
