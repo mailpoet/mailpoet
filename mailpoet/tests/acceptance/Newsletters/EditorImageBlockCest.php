68260d3bc9b140c299be949ee23d2319698c6bde
Rostislav Wolny <rosta.wolny@automattic.com>
2026-09-22 15:39:32 +0200

    Drop the drag retry from the image block test
    The retry could not recover from the failure it targeted: once the
    empty media modal was open it covered the widget, so repeated drags did
    nothing. With the stray click after a drag swallowed, a single drag is
    enough.
    
    STOMAIL-8524

--- mailpoet/tests/acceptance/Newsletters/EditorImageBlockCest.php
+++ mailpoet/tests/acceptance/Newsletters/EditorImageBlockCest.php
@@ -2,7 +2,6 @@
 
 namespace MailPoet\Test\Acceptance;
 
-use Facebook\WebDriver\Exception\TimeoutException;
 use MailPoet\Test\DataFactories\Newsletter;
 
 class EditorImageBlockCest {
@@ -14,17 +13,8 @@ class EditorImageBlockCest {
       ->create();
     $i->login();
     $i->amEditingNewsletter($newsletter->getId());
-    for ($attempt = 1; $attempt <= 3; $attempt++) {
-      $i->dragAndDrop('#automation_editor_block_image', '#mce_0');
-      try {
-        $i->waitForText('Add images');
-        break;
-      } catch (TimeoutException $e) {
-        if ($attempt === 3) {
-          throw $e;
-        }
-      }
-    }
+    $i->dragAndDrop('#automation_editor_block_image', '#mce_0');
+    $i->waitForText('Add images');
     $i->click('Media Library');
     $i->waitForElementClickable('.thumbnail');
     $i->click('.thumbnail');
